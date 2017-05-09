cac70d9fedb02c85748563cc3e64add5d8a6babb
vaibhav-rbs <vaibhav@ixsystems.com>
2017-05-09 13:05:50 -0700

    Added Dropdown to Edit Zvol section.
    Ticket: #23149

--- src/app/pages/storage/volumes/zvol/zvol-edit/zvol-edit.component.ts
+++ src/app/pages/storage/volumes/zvol/zvol-edit/zvol-edit.component.ts
@@ -41,11 +41,38 @@ export class ZvolEditComponent {
       label: 'zvol name:',
       readOnly: true,
     }),
+    new DynamicSelectModel({
+      id: 'compression',
+      label: 'Compression level:',
+      options: [
+        { label: 'Inherit (lz4)', value: "lz4" },
+        { label: 'Off', value: "off" },
+        { label: 'lz4 (recommended)', value: "lz4" },
+        { label: 'gzip (default level, 6)', value: "gzip" },
+        { label: 'gzip (fastest)', value: "gzip-1" },
+        { label: 'gzip (maximum, slow)', value: "gzip-9" },
+        { label: 'zle (runs of zeros)', value: "zle" },
+        { label: 'lzjb (legacy, not recommended)', value: "lzjb" },
+      ],
+    }),
+    new DynamicSelectModel({
+      id: 'dedup',
+      label: 'ZFS Deduplication:',
+      options: [
+        { label: 'Inherit (off)', value: "inherit" },
+        { label: 'On', value: "on" },
+        { label: 'Verify', value: "verify" },
+        { label: 'Off', value: "off" },
+      ],
+    }),
     new DynamicInputModel({
       id: 'volsize',
       label: 'Size for this zvol:',
     }),
+
   ];
+  private compression: DynamicSelectModel<string>;
+  private dedup: DynamicSelectModel<string>;
 
 
   constructor(protected router: Router, protected route: ActivatedRoute, protected aroute: ActivatedRoute, protected rest: RestService, protected ws: WebSocketService, protected formService: DynamicFormService) {
@@ -53,6 +80,8 @@ export class ZvolEditComponent {
   }
 
   preInit(entityEdit: any) {
+    this.compression = <DynamicSelectModel<string>>this.formService.findById("compression", this.formModel);
+    this.dedup = <DynamicSelectModel<string>>this.formService.findById("dedup", this.formModel);
     this.sub = this.aroute.params.subscribe(params => {
       this.pk = params['pk'];
       this.path = params['path'];
