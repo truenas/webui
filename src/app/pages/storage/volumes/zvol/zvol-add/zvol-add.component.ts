7896f1deb0514ea472bf197716d780a544c37267
vaibhav-rbs <vaibhav@ixsystems.com>
2017-05-09 13:27:51 -0700

    adding compression drop down in zvol add as well.
    Ticket: #23149

--- src/app/pages/storage/volumes/zvol/zvol-add/zvol-add.component.ts
+++ src/app/pages/storage/volumes/zvol/zvol-add/zvol-add.component.ts
@@ -18,6 +18,7 @@ export class ZvolAddComponent {
   protected path: string;
   private sub: Subscription;
   protected route_success: string[] = ['storage', 'volumes'];
+  protected compression: any;
   get resource_name(): string {
     return 'storage/volume/' + this.pk + '/zvols';
   }
@@ -34,6 +35,20 @@ export class ZvolAddComponent {
     new DynamicCheckboxModel({
       id: 'sparse',
       label: 'Sparse Volume:',
+    }),
+    new DynamicSelectModel({
+      id: 'compression',
+      label: 'Compression level:',
+      options: [
+        { label: 'Inherit (lz4)', value: "lz4" },
+        { label: 'Off', value: "off" },
+        { label: 'lz4 (recommended)', value: "lz4" },
+        { label: 'gzip (default level, 6)', value: "gzip" },
+        { label: 'gzip (fastest)', value: "gzip-1" },
+        { label: 'gzip (maximum, slow)', value: "gzip-9" },
+        { label: 'zle (runs of zeros)', value: "zle" },
+        { label: 'lzjb (legacy, not recommended)', value: "lzjb" },
+      ],
     }),
   ];
 
@@ -54,6 +69,7 @@ export class ZvolAddComponent {
     this.sub = this.aroute.params.subscribe(params => {
       this.pk = params['pk'];
       this.path = params['path'];
+      this.compression = <DynamicSelectModel<string>>this.formService.findById("compression", this.formModel);
     });
     // this.rest.get(this.resource_name, {limit: 0, bsdgrp_builtin: false}).subscribe((res) => {
     //   let gid = 999;
