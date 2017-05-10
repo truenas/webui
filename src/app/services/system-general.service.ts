98be084d4a33638e27dfefe366e4a83edea38910
Xin Tan <mactanxin@gmail.com>
2017-05-10 14:07:27 -0700

    Done:  a new systemGeneralService with getCA function ticket: #23131

---
+++ src/app/services/system-general.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable, Subject, Subscription } from 'rxjs/Rx';
+
+import { RestService, WebSocketService } from '../services/';
+
+@Injectable()
+export class SystemGeneralService {
+
+    constructor(protected rest: RestService, protected ws: WebSocketService){
+    };
+
+    getCA() {
+        return this.ws.call('certificateauthority.query', [[['cert_signedby', '=', null]]]);
+    }
+}
