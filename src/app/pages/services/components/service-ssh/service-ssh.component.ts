5e4d44019d57ca07ce908ac8eea0da1cf064692d
Kathleen Hoang <kathleenphoang@gmail.com>
2017-05-16 12:43:13 +0000

    partial commit for ssh service
    Ticket: #23918

---
+++ src/app/pages/services/components/service-ssh/service-ssh.component.ts
@@ -0,0 +1,55 @@
+import {  ApplicationRef, Component, Injector, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
+import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel,    DynamicSelectModel, } from '@ng2-dynamic-forms/core';
+
+
+import { EntityConfigComponent } from '../../../common/entity/entity-config/';
+import { GlobalState } from '../../../../global.state';
+import { RestService, WebSocketService } from '../../../../services/';
+import * as _ from 'lodash';
+
+import { Subscription } from 'rxjs';
+
+@Component ({
+    selector: 'ssh-edit',
+    template: ` <entity-edit [conf]="this"></entity-edit>`,
+})
+
+export class ServiceSSHComponent {
+  // Form Layout
+  protected resource_name: string = 'services';
+  private entityEdit: EntityConfigComponent;
+
+  protected formModel: DynamicFormControlModel[] = [
+    new DynamicInputModel({
+        id: 'ssh_tcpport',
+        label: 'TCP Port',
+    }),
+    new DynamicCheckboxModel({
+      id: 'ssh_rootlogin',
+      label: 'Login as Root with password',
+    }),
+    new DynamicCheckboxModel({
+      id: 'ssh_passwordauth',
+      label: 'Allow Password Authentication',
+    }),
+      new DynamicCheckboxModel({
+      id: 'ssh_tcpfwd',
+      label: 'Allow TCP Port Forwarding',
+    }),
+    new DynamicCheckboxModel({
+      id: 'ssh_compression',
+      label: 'Compress Connections',
+    }),
+  ];
+
+ constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService,  protected ws: WebSocketService, protected formService: DynamicFormService,  protected _injector: Injector, protected _appRef: ApplicationRef,   protected _state: GlobalState) {}
+
+  afterInit(entityEdit: any) {
+    this.entityEdit = entityEdit;
+  }
+
+}
+
+
+
--- src/app/pages/services/services.routing.ts
+++ src/app/pages/services/services.routing.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule }  from '@angular/router';
 import { Services } from './services.component';
 import { ModuleWithProviders } from '@angular/core';
 
+import { ServiceSSHComponent } from './components/service-ssh/';
+
 // noinspection TypeScriptValidateTypes
 export const routes: Routes = [
   {
     path: '', pathMatch: 'full',
     component: Services,
-  }
+  },
+  { // :pk to ssh breaks the functionality 
+    path: 'ssh',
+    component: ServiceSSHComponent,
+  },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
