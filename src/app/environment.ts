4d19967937489e636c083837f4c0b890f88ce4a3
Erin Clark <erin@ixsystems.com>
2017-05-22 17:04:08 -0700

    Add Sentry integration to New GUI production mode

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,8 @@
 // Angular 2
 // rc2 workaround
 import { enableDebugTools, disableDebugTools } from '@angular/platform-browser';
-import { enableProdMode, ApplicationRef } from '@angular/core';
+import { enableProdMode, ApplicationRef, ErrorHandler } from '@angular/core';
+import * as Raven from 'raven-js';
 // Environment Providers
 let PROVIDERS: any[] = [
   // common env directives
@@ -11,14 +12,27 @@ let PROVIDERS: any[] = [
 // https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
 let _decorateModuleRef = function identity<T>(value: T): T { return value; };
 
+// Sentry Error handling for production mode
+
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err:any) : void {
+    Raven.captureException(err.originalError);
+  }
+}
+
 if ('production' === ENV || 'renderer' === ENV) {
   // Production
   // https://github.com/qdouble/angular-webpack2-starter/issues/263
   //disableDebugTools();
   enableProdMode();
 
+  Raven
+    .config('https://e3a2219083e241589cb154b90feeb26c@sentry.ixsystems.com/3')
+    .install();
+
   PROVIDERS = [
     ...PROVIDERS,
+    { provide: ErrorHandler, useClass: RavenErrorHandler }
     // custom providers in production
   ];
 
