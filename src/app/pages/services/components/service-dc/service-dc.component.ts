90506f79e055cde9bb1b1f1e09d1cb5b2e8f63cf
Xin Tan <mactanxin@gmail.com>
2017-05-25 11:40:13 -0700

    Done: adding service template page, api doesn't work yet Ticket:

--- src/app/pages/services/components/service-dc/service-dc.component.ts
+++ src/app/pages/services/components/service-dc/service-dc.component.ts
@@ -5,15 +5,17 @@ import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, Dyna
 
 import { EntityConfigComponent } from '../../../common/entity/entity-config/';
 import { GlobalState } from '../../../../global.state';
-import { RestService, WebSocketService, UserService } from '../../../../services/';
+import { RestService, WebSocketService } from '../../../../services/';
+import { NG_VALIDATORS } from '@angular/forms';
+
 import * as _ from 'lodash';
 
 import { Subscription } from 'rxjs';
 
+
 @Component ({
-    selector: 'dc-edit',
-    template: ` <entity-config [conf]="this"></entity-config>`,
-    providers: [UserService]
+    selector: 'domaincontroller-edit',
+    template: ` <entity-config [conf]="this"></entity-config>`
 })
 
 export class ServiceDCComponent {
@@ -22,71 +24,79 @@ export class ServiceDCComponent {
   protected route_success: string[] = ['services'];
 
   protected formModel: DynamicFormControlModel[] = [
-    new DynamicCheckboxModel({
-      id: 'afp_srv_guest',
-      label: 'Guest Access',
-    }),
-    new DynamicSelectModel({
-      id: 'afp_srv_guest_user',
-      label: 'Guest Account',
+    new DynamicInputModel({
+      id: 'dc_realm',
+      label: 'Realm',
     }),
     new DynamicInputModel({
-        id: 'afp_srv_connections_limit',
-        label: 'Max. Connections',
-    }),
-    new DynamicCheckboxModel({
-      id: 'afp_srv_homedir_enable',
-      label: 'Enable home directories',
-    }),
-   new DynamicInputModel({
-      id: 'afp_srv_homedir',
-      label: 'Home Directories',
-    }),
-    new DynamicInputModel({
-      id: 'afp_srv_homename',
-      label: 'Home share name',
-    }),
-   new DynamicInputModel({
-      id: 'afp_srv_dbpath',
-      label: 'Database Path',
-    }),
-   new DynamicTextAreaModel({
-      id: 'afp_srv_global_aux',
-      label: 'Global auxiliary parameters',
+      id: 'dc_domain',
+      label: 'Domain',
     }),
     new DynamicSelectModel({
-        id: 'afp_srv_chmod_request',
-        label: 'Chmod Request',
+        id: 'dc_role',
+        label: 'Server Roll',
         options: [
-          { label: 'Ignore', value: 'ignore' },
-          { label: 'Preserve', value: 'preserve' },
-          { label: 'Simple', value: 'simple' },
-        ],
+          { label: 'DC', value: 'dc' },
+        ]
+    }),
+    new DynamicInputModel({
+      id: 'dc_dns_forwarder',
+      label: 'DNS Forwarder',
+    }),
+   new DynamicSelectModel({
+      id: 'dc_forest_level',
+      label: 'Forest Level',
+      options: [
+        { label: '2000', value: '2000' },
+        { label: '2003', value: '2003' },
+        { label: '2008', value: '2008' },
+        { label: '2008_R2', value: '2008_R2' },
+      ]
+    }),
+    new DynamicInputModel({
+      id: 'dc_passwd',
+      inputType: 'password',
+      label: 'Administration Password',
+      validators: {
+          required: null,
+          minLength: 3
+      }
+    }),
+    new DynamicInputModel({
+      id: 'confirmPassword',
+      inputType: 'password',
+      label: 'Confirm Administration Password',
+      relation: [
+        {
+          action: "DISABLE",
+          connective: "AND",
+          when: [
+            {
+              id: "dc_passwd",
+              value: null
+            },
+          ]
+        }
+      ]
     }),
     new DynamicSelectModel({
         id: 'afp_srv_map_acls',
-        label: 'Map ACLs',
+        label: 'Kerberos Realm:',
         options: [
           { label: 'Rights', value: 'rights' },
           { label: 'None', value: 'none' },
           { label: 'Mode', value: 'mode' },
         ],
     }),
-    new DynamicInputModel({
-        id: 'afp_srv_bindip',
-        label: 'Bind Interfaces',
-    }),
+
   ];
-
-
-
-  private guestList: DynamicSelectModel<string>;
   
-  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService,  protected ws: WebSocketService, protected formService: DynamicFormService,  protected _injector: Injector, protected _appRef: ApplicationRef,   protected _state: GlobalState, protected userService: UserService) {
+  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService,  protected ws: WebSocketService, protected formService: DynamicFormService,  protected _injector: Injector, protected _appRef: ApplicationRef,   protected _state: GlobalState) {
 
   }
 
   afterInit(entityEdit: any) {
+    this.entityEdit = entityEdit;
   }
 
 }
