b747c96bde3b2f8de0dc321af788750998fa2d2d
Lola Yang <lola.ly27@gmail.com>
2017-05-31 18:25:35 -0700

    Done: update entity-edit, enable to use array form
    Ticket: #24186

--- src/app/pages/common/entity/entity-edit/entity-edit.component.ts
+++ src/app/pages/common/entity/entity-edit/entity-edit.component.ts
@@ -1,8 +1,8 @@
 import { ApplicationRef, Component, Injector, Input, OnDestroy, OnInit, ViewChildren } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { DynamicFormControlModel, DynamicFormService } from '@ng2-dynamic-forms/core';
+import { DynamicFormControlModel, DynamicFormService, DynamicFormArrayModel } from '@ng2-dynamic-forms/core';
 import { RestService, WebSocketService } from '../../../../services/';
 import { Location } from '@angular/common';
 
@@ -30,7 +30,7 @@ export class EntityEditComponent implements OnInit, OnDestroy {
   private sub: any;
   public error: string;
   public data: Object = {};
-
+  public ip_arr_count: number = 0;
   constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService, protected ws: WebSocketService, protected formService: DynamicFormService, protected _injector: Injector, protected _appRef: ApplicationRef, protected location: Location) {
 
   }
@@ -51,7 +51,11 @@ export class EntityEditComponent implements OnInit, OnDestroy {
         for(let i in this.data) {
           let fg = this.formGroup.controls[i];
           if(fg) {
-            fg.setValue(this.data[i]);
+            if(Array.isArray(this.data[i])) {
+              this.setIps(this.data[i]);
+            } else {
+              fg.setValue(this.data[i]);
+            }
           }
         }
         if(this.conf.initial) {
@@ -59,6 +63,7 @@ export class EntityEditComponent implements OnInit, OnDestroy {
         }
       })
     });
+
     if (this.conf.afterInit){
       this.conf.afterInit(this);
     }
@@ -72,6 +77,36 @@ export class EntityEditComponent implements OnInit, OnDestroy {
     this.router.navigate(new Array('/pages').concat(route));
   }
 
+  setIps(data: any[]) {
+    this.ip_arr_count = 0;
+    for(let i in data) {
+      var ip_arr: any[] = _.split(data[i], ':');
+      var ip = ip_arr[0];
+      var port = ip_arr[1];
+
+      var arrayControl = <FormArray> this.formGroup.get("iscsi_target_portal_ips");
+      var arrayModel = <DynamicFormArrayModel> this.formService.findById("iscsi_target_portal_ips", this.conf.formModel);
+      this.ip_arr_count += 1;
+      this.formService.insertFormArrayGroup(this.ip_arr_count, arrayControl, arrayModel);
+
+      arrayControl.controls[i].get("ip").setValue(ip);
+      arrayControl.controls[i].get("port").setValue(port);
+    }
+  }
+
+  getIPs(data: any[]):any[] {
+    var IPs = new Array();
+    for(let i in data) {
+      if('ip' in data[i] && 'port' in data[i] && 'delete' in data[i]) {
+        if(!data[i]['delete']) {
+          let ip = data[i]['ip'] + ':' + data[i]['port'];
+          IPs.push(ip);
+        }
+      }
+    }
+    return IPs;
+  }
+
   onSubmit() {
     this.error = null;
     let value = _.cloneDeep(this.formGroup.value);
@@ -79,6 +114,10 @@ export class EntityEditComponent implements OnInit, OnDestroy {
       let clean = this['clean_' + i];
       if(clean) {
         value = clean(value, i);
+      }
+
+      if(Array.isArray(value[i])) {
+        value[i] = this.getIPs(value[i]);
       }
     }
     if('id' in value) {
