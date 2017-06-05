17a3c0e1137f2b07377477d058764046d79a7aca
Lola Yang <lola.ly27@gmail.com>
2017-06-05 16:08:07 -0700

    Done: updated iscsi service
    Ticket: #24259

--- src/app/services/iscsi.service.ts
+++ src/app/services/iscsi.service.ts
@@ -8,11 +8,26 @@ import { RestService, WebSocketService } from '../services/';
 
 @Injectable()
 export class IscsiService {
+	protected iscsiPortalResource: string = 'services/iscsi/portal';
+	protected iscsiInitiatorResource: string = 'services/iscsi/authorizedinitiator';
+	protected iscsiAuthCredentialResource: string = 'services/iscsi/authcredential';
 
     constructor(protected rest: RestService, protected ws: WebSocketService){
     };
 
     getIpChoices() {
         return this.ws.call('notifier.choices', ['IPChoices', [true, false]]);
+    };
+
+    listPortals() {
+        return this.rest.get(this.iscsiPortalResource, {});
+    };
+
+    listInitiators() {
+        return this.rest.get(this.iscsiInitiatorResource, {});
+    };
+
+    listAuthCredential() {
+        return this.rest.get(this.iscsiAuthCredentialResource, {});
     };
 }
