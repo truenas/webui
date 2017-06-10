45d1df521990c2fdb8640353e9013fed70293316
Erin Clark <erin@ixsystems.com>
2017-06-09 18:21:39 -0700

    Make vlans use the new entity-form component that now supports add/edit
    Ticket: #24416

--- src/app/pages/network/vlans/vlan-edit/vlan-edit.component.ts
+++ src/app/pages/network/vlans/vlan-edit/vlan-edit.component.ts
@@ -1,50 +1,58 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel, DynamicSelectModel, DynamicRadioGroupModel } from '@ng2-dynamic-forms/core';
 import { GlobalState } from '../../../../global.state';
 import { RestService, WebSocketService, NetworkService } from '../../../../services/';
 
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-vlan-edit',
-  template: `<entity-edit [conf]="this"></entity-edit>`
+  template: `<entity-form [conf]="this"></entity-form>`
 })
 export class VlanEditComponent {
 
   protected resource_name: string = 'network/vlan/';
   protected route_success: string[] = ['network', 'vlans'];
+  protected isEntity: boolean = true;
 
-  protected formModel: DynamicFormControlModel[] = [
-    new DynamicInputModel({
-      id: 'vlan_vint',
-      label: 'Virtual Interface',
+  protected fieldConfig: FieldConfig[] = [
+    {
+      type: 'input',
+      name: 'vlan_vint',
+      placeholder: 'Virtual Interface',
       disabled: true
-    }),
-    new DynamicSelectModel({
-      id: 'vlan_pint',
-      label: 'Parent Interface',
-    }),
-    new DynamicInputModel({
-      id: 'vlan_tag',
-      label: 'Vlan Tag',
-    }),
-    new DynamicInputModel({
-      id: 'vlan_description',
-      label: 'Description',
-    }),
+    },
+    {
+      type: 'select',
+      name: 'vlan_pint',
+      placeholder: 'Parent Interface',
+      options: []
+    },
+    {
+      type: 'input',
+      name: 'vlan_tag',
+      placeholder: 'Vlan Tag',
+    },
+    {
+      type: 'input',
+      name: 'vlan_description',
+      placeholder: 'Description',
+    },
   ];
 
-  private vlan_pint: DynamicSelectModel<string>;
+  private vlan_pint: any;
 
-  constructor(protected router: Router, protected rest: RestService, protected ws: WebSocketService, protected formService: DynamicFormService, protected networkService: NetworkService, protected _state: GlobalState) {
+  constructor(protected router: Router, protected rest: RestService, protected ws: WebSocketService, protected networkService: NetworkService, protected _state: GlobalState) {
 
   }
 
   afterInit(entityAdd: any) {
     this.networkService.getVlanNicChoices().subscribe((res) => {
-      this.vlan_pint = <DynamicSelectModel<string>>this.formService.findById("vlan_pint", this.formModel);
+      this.vlan_pint = _.find(this.fieldConfig, {'name': 'vlan_pint'});
       res.forEach((item) => {
-        this.vlan_pint.add({ label: item[1], value: item[0] });
+        this.vlan_pint.options.push({ label: item[1], value: item[0] });
       });
     });
   }
--- src/app/pages/network/vlans/vlan.module.ts
+++ src/app/pages/network/vlans/vlan.module.ts
@@ -2,8 +2,6 @@ import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgaModule } from '../../../theme/nga.module';
-import { DynamicFormsCoreModule } from '@ng2-dynamic-forms/core';
-import { DynamicFormsBootstrapUIModule } from '@ng2-dynamic-forms/ui-bootstrap';
 
 import { EntityModule } from '../../common/entity/entity.module';
 import { routing }       from './vlan.routing';
@@ -18,8 +16,6 @@ import { NetworkService } from '../../../services';
 @NgModule({
   imports: [
     EntityModule,
-    DynamicFormsCoreModule.forRoot(),
-    DynamicFormsBootstrapUIModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
