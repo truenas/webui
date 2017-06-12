d02c11a08ff91419e167520e9ba78a643da67eac
Lola Yang <lola.ly27@gmail.com>
2017-06-12 12:50:34 -0700

    Done: form-input field support basic/advance mode

--- src/app/pages/common/entity/entity-form/components/form-input/form-input.component.ts
+++ src/app/pages/common/entity/entity-form/components/form-input/form-input.component.ts
@@ -7,8 +7,10 @@ import { FieldConfig } from '../../models/field-config.interface';
 @Component({
   selector: 'form-input',
   templateUrl: './form-input.component.html',
+  styleUrls: ['../dynamic-field/dynamic-field.css'],
 })
 export class FormInputComponent implements Field {
   config: FieldConfig;
   group: FormGroup;
+  fieldShow: string;
 }
