e0bdb33a8aa538f6204efd418b9c1051004c0cc5
Lola Yang <lola.ly27@gmail.com>
2017-06-12 12:49:27 -0700

    Done: form-input field support basic/advance mode
    Ticket: #

--- src/app/pages/common/entity/entity-form/components/form-checkbox/form-checkbox.component.ts
+++ src/app/pages/common/entity/entity-form/components/form-checkbox/form-checkbox.component.ts
@@ -6,10 +6,11 @@ import { FieldConfig } from '../../models/field-config.interface';
 
 @Component({
   selector: 'form-checkbox',
-  styleUrls: ['form-checkbox.component.scss'],
+  styleUrls: ['form-checkbox.component.scss', '../dynamic-field/dynamic-field.css'],
   templateUrl: './form-checkbox.component.html'
 })
 export class FormCheckboxComponent implements Field {
   config: FieldConfig;
   group: FormGroup;
+  fieldShow: string;
 }
