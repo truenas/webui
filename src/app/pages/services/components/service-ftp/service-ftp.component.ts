2c844916aaef65896253c2a039e39b4b4b9dc00b
Xin Tan <mactanxin@gmail.com>
2017-06-15 11:39:19 -0700

    updated FTP service with new form

--- src/app/pages/services/components/service-ftp/service-ftp.component.ts
+++ src/app/pages/services/components/service-ftp/service-ftp.component.ts
@@ -1,136 +1,169 @@
-import {  ApplicationRef, Component, Injector, OnInit } from '@angular/core';
+import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
-import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel,    DynamicSelectModel,DynamicTextAreaModel, } from '@ng2-dynamic-forms/core';
-
 
 import { EntityConfigComponent } from '../../../common/entity/entity-config/';
 import { GlobalState } from '../../../../global.state';
-import { RestService, WebSocketService, SystemGeneralService } from '../../../../services/';
-import * as _ from 'lodash';
+import { RestService, WebSocketService, SystemGeneralService} from '../../../../services/';
+import { FormGroup, FormArray, Validators, AbstractControl} from '@angular/forms';
 
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+
+import * as _ from 'lodash';
 import { Subscription } from 'rxjs';
+import { matchOtherValidator } from '../../../common/entity/entity-form/validators/password-validation';
 
 @Component ({
-    selector: 'ftp-edit',
-    template: ` <entity-config [conf]="this"></entity-config>`,
-    providers: [SystemGeneralService]
+    selector: 'dynamicdns-edit',
+    template: `<entity-form [conf]="this"></entity-form>`,
+    providers: [ SystemGeneralService ]
 })
 
-export class ServiceFTPComponent {
+
+export class ServiceFTPComponent implements OnInit {
   protected resource_name: string = 'services/ftp';
-  private entityEdit: EntityConfigComponent;
   protected route_success: string[] = ['services'];
+
+  private entityEdit: EntityConfigComponent;
 
   protected isBasicMode: boolean = true;
 
-  protected formModel: DynamicFormControlModel[] = [
-    new DynamicInputModel({
-      id: 'ftp_port',
-      label: 'Port',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_clients',
-      label: 'Clients',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_ipconnections',
-      label: 'Connections',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_loginattempt',
-      label: 'Login Attempts',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_timeout',
-      label: 'Timeout',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_rootlogin',
-      label: 'Allow Root Login',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_onlyanonymous',
-      label: 'Allow Anonymous Login',
-    }),
-    new DynamicInputModel({
-        id: 'ftp_anonpath',
-        label: 'Path',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_onlylocal',
-      label: 'Allow Local User Login',
-    }),
-    new DynamicTextAreaModel({
-      id: 'ftp_banner',
-      label: 'Display Login',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_resume',
-      label: 'Allow Transfer Resumption',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_defaultroot',
-      label: 'Always Chroot',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_reversedns',
-      label: 'Perform Reverse DNS Lookups',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_masqaddress',
-      label: 'Masquerade Address',
-    }),
-    new DynamicSelectModel({
-        id: 'ftp_ssltls_certfile',
-        label: 'Certificate',
-    }),
-    new DynamicInputModel({
-        id: 'ftp_filemask',
-        label: 'File Permission',
-    }),
-    new DynamicInputModel({
-        id: 'ftp_dirmask',
-        label: 'Directory Permission',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_fxp',
-      label: 'Enable FXP',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_ident',
-      label: 'Require IDENT Authentication',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_passiveportsmin',
-      label: 'Minimum Passive Port',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_passiveportsmax',
-      label: 'Maximum Passive Port',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_localuserbw',
-      label: 'Local User Upload Bandwidth',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_localuserdlbw',
-      label: 'Local User Download Bandwidth',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_anonuserbw',
-      label: 'Anonymous User Upload Bandwidth',
-    }),
-    new DynamicInputModel({
-      id: 'ftp_anonuserdlbw',
-      label: 'Anonymous User Download Bandwidth',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls',
-      label: 'Enable TLS',
-    }),
-    new DynamicSelectModel({
-        id: 'ftp_tls_policy',
-        label: 'TLS Policy',
+  protected fieldConfig: FieldConfig[] = [
+    {
+        type: 'input',
+        name: 'ftp_port',
+        placeholder: 'Port',
+    },
+    {
+        type: 'input',
+        name: 'ftp_clients',
+        placeholder: 'Clients',
+    },
+    {
+        type: 'input',
+        name: 'ftp_ipconnections',
+        placeholder: 'Connections',
+    },
+    {
+        type: 'input',
+        name: 'ftp_loginattempt',
+        placeholder: 'Login Attempts',
+    },
+    {
+        type: 'input',
+        name: 'ftp_timeout',
+        placeholder: 'Timeout',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_rootlogin',
+        placeholder: 'Allow Root Login',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_onlyanonymous',
+        placeholder: 'Allow Anonymous Login',
+    },
+    {
+        type: 'input',
+        name: 'ftp_anonpath',
+        placeholder: 'Path',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_onlylocal',
+        placeholder: 'Allow Local User Login',
+    },
+    {
+        type: 'textarea',
+        name: 'ftp_banner',
+        placeholder: 'Display Login',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_resume',
+        placeholder: 'Allow Transfer Resumption',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_defaultroot',
+        placeholder: 'Always Chroot',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_reversedns',
+        placeholder: 'Perform Reverse DNS Lookups',
+    },
+    {
+        type: 'input',
+        name: 'ftp_masqaddress',
+        placeholder: 'Masquerade Address',
+    },
+    {
+        type: 'select',
+        name: 'ftp_ssltls_certfile',
+        placeholder: 'Certificate',
+        options: [
+        ],
+    },
+    {
+        type: 'input',
+        name: 'ftp_filemask',
+        placeholder: 'File Permission',
+    },
+    {
+        type: 'input',
+        name: 'ftp_dirmask',
+        placeholder: 'Directory Permission',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_fxp',
+        placeholder: 'Enable FXP',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_ident',
+        placeholder: 'Require IDENT Authentication',
+    },
+    {
+        type: 'input',
+        name: 'ftp_passiveportsmin',
+        placeholder: 'Minimum Passive Port',
+    },
+    {
+        type: 'input',
+        name: 'ftp_passiveportsmax',
+        placeholder: 'Maximum Passive Port',
+    },
+    {
+        type: 'input',
+        name: 'ftp_localuserbw',
+        placeholder: 'Local User Upload Bandwidth',
+    },
+    {
+        type: 'input',
+        name: 'ftp_localuserdlbw',
+        placeholder: 'Local User Download Bandwidth',
+    },
+    {
+        type: 'input',
+        name: 'ftp_anonuserbw',
+        placeholder: 'Anonymous User Upload Bandwidth',
+    },
+    {
+        type: 'input',
+        name: 'ftp_anonuserdlbw',
+        placeholder: 'Anonymous User Download Bandwidth',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls',
+        placeholder: 'Enable TLS',
+    },
+    {
+        type: 'select',
+        name: 'ftp_tls_policy',
+        placeholder: 'TLS Policy',
         options: [
           { label: 'On', value: 'on' },
           { label: 'Off', value: 'off' },
@@ -143,55 +176,67 @@ export class ServiceFTPComponent {
           { label: 'Auth + Data', value: 'auth+data' },
           { label: 'Auth + !Data', value: 'auth+!data' },
         ],
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_allow_client_renegotiations',
-      label: 'TLS Allow Client Renegotiations',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_allow_dot_login',
-      label: 'TLS Allow Dot Login',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_allow_per_user',
-      label: 'TLS Allow Per User',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_common_name_required',
-      label: 'TLS Common Name Required',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_enable_diags',
-      label: 'TLS Enable Diagnostics',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_export_cert_data',
-      label: 'TLS Export Certificate Request',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_no_empty_fragments',
-      label: 'TLS No Empty Fragments',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_no_session_reuse_required',
-      label: 'TLS No Session Reuse Required',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_stdenvvars',
-      label: 'TLS Export Standard Vars',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_dns_name_required',
-      label: 'TLS DNS Name Required',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ftp_tls_opt_ip_address_required',
-      label: 'TLS IP Address Required',
-    }),
-    new DynamicTextAreaModel({
-      id: 'ftp_options',
-      label: 'Auxiliary Parameters',
-    }),
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_allow_client_renegotiations',
+        placeholder: 'TLS Allow Client Renegotiations',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_allow_dot_login',
+        placeholder: 'TLS Allow Dot Login',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_allow_per_user',
+        placeholder: 'TLS Allow Per User',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_common_name_required',
+        placeholder: 'TLS Common Name Required',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_enable_diags',
+        placeholder: 'TLS Enable Diagnostics',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_export_cert_data',
+        placeholder: 'TLS Export Certificate Request',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_no_empty_fragments',
+        placeholder: 'TLS No Empty Fragments',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_no_session_reuse_required',
+        placeholder: 'TLS No Session Reuse Required',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_stdenvvars',
+        placeholder: 'TLS Export Standard Vars',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_dns_name_required',
+        placeholder: 'TLS DNS Name Required',
+    },
+    {
+        type: 'checkbox',
+        name: 'ftp_tls_opt_ip_address_required',
+        placeholder: 'TLS IP Address Required',
+    },
+    {
+        type: 'textarea',
+        name: 'ftp_options',
+        placeholder: 'Auxiliary Parameters',
+    },
   ];
 
 
@@ -246,21 +291,31 @@ export class ServiceFTPComponent {
       }
     }
   ];
-  
-  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService,  protected ws: WebSocketService, protected formService: DynamicFormService,  protected _injector: Injector, protected _appRef: ApplicationRef,   protected _state: GlobalState, protected systemGeneralService: SystemGeneralService) {
 
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
+    protected rest: RestService,
+    protected ws: WebSocketService,
+    protected _injector: Injector,
+    protected _appRef: ApplicationRef,
+    protected _state: GlobalState,
+    protected systemGeneralService: SystemGeneralService) {
   }
 
-  private cert_signedby: DynamicSelectModel<string>;
+  private ftp_ssltls_certfile: any;
+
+  ngOnInit() {
+    this.systemGeneralService.getCertificates().subscribe((res) => {
+      this.ftp_ssltls_certfile = _.find(this.fieldConfig, {'name': 'ftp_ssltls_certfile'});
+      res.data.forEach((item) => {
+        this.ftp_ssltls_certfile.options.push({ label: item.cert_common, value: item.id});
+      });
+    });
+  }
 
   afterInit(entityEdit: any) {
     this.entityEdit = entityEdit;
-    this.systemGeneralService.getCA().subscribe((res) => {
-      this.cert_signedby = <DynamicSelectModel<string>>this.formService.findById('ftp_ssltls_certfile', this.formModel);
-      res.forEach((item) => {
-        this.cert_signedby.add({ label: item.cert_name, value: item.id });
-      });
-    });
   }
 
 }
