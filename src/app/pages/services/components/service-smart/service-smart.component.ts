d1d88abd6bffceb4f6b470733b177ecfbd1060eb
Xin Tan <mactanxin@gmail.com>
2017-06-16 13:43:59 -0700

    UPDATED: SMART service with new form

--- src/app/pages/services/components/service-smart/service-smart.component.ts
+++ src/app/pages/services/components/service-smart/service-smart.component.ts
@@ -1,60 +1,75 @@
 import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
-import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel,    DynamicSelectModel,DynamicTextAreaModel, } from '@ng2-dynamic-forms/core';
-
 
 import { EntityConfigComponent } from '../../../common/entity/entity-config/';
 import { GlobalState } from '../../../../global.state';
-import { RestService, WebSocketService } from '../../../../services/';
-import { NG_VALIDATORS } from '@angular/forms';
+import { RestService, WebSocketService, SystemGeneralService} from '../../../../services/';
+import { FormGroup, FormArray, Validators, AbstractControl} from '@angular/forms';
+
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 
 import * as _ from 'lodash';
 import { Subscription } from 'rxjs';
+import { matchOtherValidator } from '../../../common/entity/entity-form/validators/password-validation';
 
 @Component ({
     selector: 'smart-edit',
-    template: ` <entity-config [conf]="this"></entity-config>`
+    template: `<entity-form [conf]="this"></entity-form>`,
 })
 
 export class ServiceSMARTComponent {
-  protected resource_name: string = 'services/smart';
-  private entityEdit: EntityConfigComponent;
-  protected route_success: string[] = ['services'];
 
-  protected formModel: DynamicFormControlModel[] = [
-    new DynamicInputModel({
-      id: 'smart_interval',
-      label: 'Check Interval',
-    }),
-    new DynamicSelectModel({
-      id: 'smart_powermode',
-      label: 'Power Mode',
-      options: [
+  protected resource_name: string = 'services/smart';
+  protected route_success: string[] = ['services'];
+  private entityEdit: EntityConfigComponent;
+
+  protected fieldConfig: FieldConfig[] = [
+    {
+        type: 'input',
+        name: 'smart_interval',
+        placeholder: 'Check Interval',
+    },
+    {
+        type: 'select',
+        name: 'smart_powermode',
+        placeholder: 'Power Mode',
+        options: [
         { label: 'Never', value: 'never'},
         { label: 'Sleep', value: 'sleep'},
         { label: 'Standby', value: 'standby'},
         { label: 'Idle', value: 'idle'},
       ]
-    }),
-    new DynamicInputModel({
-        id: 'smart_difference',
-        label: 'Difference',
-    }),
-    new DynamicInputModel({
-        id: 'smart_informational',
-        label: 'Informational',
-    }),
-    new DynamicInputModel({
-        id: 'smart_critical',
-        label: 'Critical',
-    }),
-    new DynamicInputModel({
-        id: 'smart_email',
-        label: 'Email to Report',
-    }),
+    },
+    {
+        type: 'input',
+        name: 'smart_difference',
+        placeholder: 'Difference',
+    },
+    {
+        type: 'input',
+        name: 'smart_informational',
+        placeholder: 'Informational',
+    },
+    {
+        type: 'input',
+        name: 'smart_critical',
+        placeholder: 'Critical',
+    },
+    {
+        type: 'input',
+        name: 'smart_email',
+        placeholder: 'Email',
+    },
   ];
   
-  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService,  protected ws: WebSocketService, protected formService: DynamicFormService,  protected _injector: Injector, protected _appRef: ApplicationRef,   protected _state: GlobalState) {
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
+    protected rest: RestService,
+    protected ws: WebSocketService,
+    protected _injector: Injector,
+    protected _appRef: ApplicationRef,
+    protected _state: GlobalState) {
   }
 
   afterInit(entityEdit: any) {
