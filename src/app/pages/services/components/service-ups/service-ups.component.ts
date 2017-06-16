4719d9fd6c76190026d478e30ced760f2bf4fa3f
Xin Tan <mactanxin@gmail.com>
2017-06-16 14:36:07 -0700

    UPDATED: UPS service with new form

--- src/app/pages/services/components/service-ups/service-ups.component.ts
+++ src/app/pages/services/components/service-ups/service-ups.component.ts
@@ -1,112 +1,137 @@
 import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
-import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel,    DynamicSelectModel,DynamicTextAreaModel, } from '@ng2-dynamic-forms/core';
-
 
 import { EntityConfigComponent } from '../../../common/entity/entity-config/';
 import { GlobalState } from '../../../../global.state';
-import { RestService, WebSocketService } from '../../../../services/';
-import { NG_VALIDATORS } from '@angular/forms';
+import { RestService, WebSocketService, SystemGeneralService} from '../../../../services/';
+import { FormGroup, FormArray, Validators, AbstractControl} from '@angular/forms';
+
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 
 import * as _ from 'lodash';
 import { Subscription } from 'rxjs';
-
+import { matchOtherValidator } from '../../../common/entity/entity-form/validators/password-validation';
 
 @Component ({
     selector: 'ups-edit',
-    template: ` <entity-config [conf]="this"></entity-config>`
+    template: `<entity-form [conf]="this"></entity-form>`,
 })
 
 export class ServiceUPSComponent {
   protected resource_name: string = 'services/ups';
+  protected route_success: string[] = ['services'];
   private entityEdit: EntityConfigComponent;
-  protected route_success: string[] = ['services','ups'];
 
-  protected formModel: DynamicFormControlModel[] = [
-    new DynamicSelectModel({
-      id: 'ups_mode',
-      label: 'UPS Mode',
-      options: [
-        { label: 'Master', value: 'master'},
-        { label: 'Slave', value: 'slave'},
-      ]
-    }),
-    new DynamicInputModel({
-      id: 'ups_identifier',
-      label: 'Identifier',
-    }),
-    new DynamicSelectModel({
-      id: 'ups_driver',
-      label: 'Driver',
-    }),
-    new DynamicSelectModel({
-      id: 'ups_port',
-      label: 'Port',
-    }),
-    new DynamicTextAreaModel({
-      id: 'lldp_intdesc',
-      label: 'Auxiliary Parameters(ups.conf)',
-    }),
-    new DynamicTextAreaModel({
-      id: 'lldp_intdesc',
-      label: 'Auxiliary Parameters(upsd.conf)',
-    }),
-    new DynamicInputModel({
-      id: 'ups_description',
-      label: 'Description',
-    }),
-    new DynamicSelectModel({
-      id: 'ups_shutdown',
-      label: 'Shutdown Mode',
-      options: [
-        { label: 'UPS reaches low battery', value: 'lowbatt'},
-        { label: 'UPS goes on battery', value: 'batt'},
-      ]
-    }),
-    new DynamicInputModel({
-      id: 'ups_shutdowntimer',
-      label: 'Shutdown Timer',
-    }),
-    new DynamicInputModel({
-      id: 'ups_shutdowncmd',
-      label: 'Shutdown Command',
-    }),
-    new DynamicInputModel({
-      id: 'ups_monuser',
-      label: 'Monitor User',
-    }),
-    new DynamicInputModel({
-      id: 'ups_monpwd',
-      label: 'Monitor Password',
-    }),
-    new DynamicTextAreaModel({
-      id: 'ups_extrausers',
-      label: 'Extra Users(upsd.conf)',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ups_rmonitor',
-      label: 'Remote Monitor',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ups_emailnotify',
-      label: 'Send Email Status Updates',
-    }),
-    new DynamicInputModel({
-      id: 'ups_toemail',
-      label: 'To Email',
-    }),
-    new DynamicInputModel({
-      id: 'ups_subject',
-      label: 'Email Subject',
-    }),
-    new DynamicCheckboxModel({
-      id: 'ups_powerdown',
-      label: 'Power Off UPS',
-    }),
+  protected fieldConfig: FieldConfig[] = [
+    {
+        type: 'select',
+        name: 'ups_mode',
+        placeholder: 'UPS Mode',
+        options: [
+          { label: 'Master', value: 'master'},
+          { label: 'Slave', value: 'slave'},
+        ]
+    },
+    {
+        type: 'input',
+        name: 'ups_identifier',
+        placeholder: 'Identifier',
+    },
+    {
+        type: 'select',
+        name: 'ups_driver',
+        placeholder: 'Driver',
+    },
+    {
+        type: 'select',
+        name: 'ups_port',
+        placeholder: 'Port',
+    },
+    {
+        type: 'textarea',
+        name: 'ups_options',
+        placeholder: 'Auxiliary Parameters(ups.conf)',
+    },
+    {
+        type: 'textarea',
+        name: 'ups_optionsupsd',
+        placeholder: 'Auxiliary Parameters(upsd.conf)',
+    },
+    {
+        type: 'input',
+        name: 'ups_description',
+        placeholder: 'Description',
+    },
+    {
+        type: 'select',
+        name: 'ups_shutdown',
+        placeholder: 'Shutdown Mode',
+        options: [
+          { label: 'UPS reaches low battery', value: 'lowbatt'},
+          { label: 'UPS goes on battery', value: 'batt'},
+        ]
+    },
+    {
+        type: 'input',
+        name: 'ups_shutdowntimer',
+        placeholder: 'Shutdown Timer',
+    },
+    {
+        type: 'input',
+        name: 'ups_shutdowncmd',
+        placeholder: 'Shutdown Command',
+    },
+    {
+        type: 'input',
+        name: 'ups_monuser',
+        placeholder: 'Monitor User',
+    },
+    {
+        type: 'input',
+        name: 'ups_monpwd',
+        placeholder: 'Monitor Password',
+    },
+    {
+        type: 'textarea',
+        name: 'ups_extrausers',
+        placeholder: 'Extra Users(upsd.conf)',
+    },
+    {
+        type: 'checkbox',
+        name: 'ups_rmonitor',
+        placeholder: 'Remote Monitor',
+    },
+    {
+        type: 'checkbox',
+        name: 'ups_emailnotify',
+        placeholder: 'Send Email Status Updates',
+    },
+    {
+        type: 'input',
+        name: 'ups_toemail',
+        placeholder: 'To Email',
+    },
+    {
+        type: 'input',
+        name: 'ups_subject',
+        placeholder: 'Email Subject',
+    },
+    {
+        type: 'checkbox',
+        name: 'ups_powerdown',
+        placeholder: 'Power Off UPS',
+    },
   ];
   
-  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService,  protected ws: WebSocketService, protected formService: DynamicFormService,  protected _injector: Injector, protected _appRef: ApplicationRef,   protected _state: GlobalState) {
-  }
+ constructor(
+    protected router: Router,
+    protected route: ActivatedRoute, 
+    protected rest: RestService,
+    protected ws: WebSocketService,
+    protected _injector: Injector,
+    protected _appRef: ApplicationRef,
+    protected _state: GlobalState
+  ){}
 
   afterInit(entityEdit: any) {
     this.entityEdit = entityEdit;
