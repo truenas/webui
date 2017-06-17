193baefac9d5fe67c41a20f8b4d376d2365b8e94
vaibhav-rbs <vaibhav@ixsystems.com>
2017-06-16 17:24:05 -0700

    WIP commit for NFS service

--- src/app/pages/services/components/service-nfs/service-nfs.component.ts
+++ src/app/pages/services/components/service-nfs/service-nfs.component.ts
@@ -1,126 +1,120 @@
-import { check } from '@angular/tsc-wrapped/src/tsc';
-import { group } from '@angular/core/core';
 import {  ApplicationRef, Component, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
-import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel,
-          DynamicInputModel, DynamicSelectModel, DynamicTextAreaModel,
-          DynamicCheckboxGroupModel } from '@ng2-dynamic-forms/core';
-
-
 import { EntityConfigComponent } from '../../../common/entity/entity-config/';
 import { GlobalState } from '../../../../global.state';
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import { MdInputModule } from '@angular/material';
 import { RestService, WebSocketService, UserService } from '../../../../services/';
+
+
 import * as _ from 'lodash';
 
 import { Subscription } from 'rxjs';
 
 @Component ({
     selector: 'nfs-edit',
-    template: ` <entity-config [conf]="this"></entity-config>`,
+    template: ` <entity-form [conf]="this"></entity-form>`,
     providers: [UserService]
 })
 
-export class ServiceNFSComponent {
+export class ServiceNFSComponent implements OnInit {
   protected resource_name: string = 'services/nfs';
   private entityEdit: EntityConfigComponent;
   protected route_success: string[] = ['services'];
-  protected formModel: DynamicFormControlModel[]
-  protected nfs_srv_bindip: DynamicCheckboxGroupModel;
-
-  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService,
-                protected ws: WebSocketService, protected formService: DynamicFormService,
-                protected _injector: Injector, protected _appRef: ApplicationRef,
-                protected _state: GlobalState, protected userService: UserService) {
-  }
-preInit(entityEdit: any){
-
-  this.formModel = [
-    new DynamicInputModel({
-    id: 'nfs_srv_servers',
-    label: 'Number of servers:',
-    }),
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_udp',
-      label: 'Serve UDP NFS clients:',
-    }),
-    /*
-    new DynamicCheckboxGroupModel({
-      id: 'nfs_srv_bindip',
-      label: 'Bind IP Addresses:',
-      group: [],
-    }),
-    */
-    new DynamicInputModel({
-      id: 'nfs_srv_bindip',
-      label: 'Bind IP Addresses:',
-    }),    
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_allow_nonroot',
-      label: 'Allow non-root mount:',
-    }),
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_v4',
-      label: 'Enable NFSv4:',
-    }),
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_v4_v3owner',
-      label: 'NFSv3 ownership model for NFSv4:',
+  protected fieldConfig: FieldConfig[] = [
+    {
+      type: 'input',
+      name: 'nfs_srv_servers',
+      placeholder: 'Number of servers:',
+    },
+    { type: 'checkbox',
+      name: 'nfs_srv_udp',
+      placeholder: 'Serve UDP NFS clients:',
+    },
+    { type: 'select',
+      name: 'nfs_srv_bindip',
+      placeholder: 'Bind IP Addresses:',
+      options: [],
+      multiple: true
+    },
+    { type: 'checkbox',
+      name: 'nfs_srv_allow_nonroot',
+      placeholder: 'Allow non-root mount:',
+    },
+    { type: 'checkbox',
+      name: 'nfs_srv_v4',
+      placeholder: 'Enable NFSv4:',
+    },
+    { 
+      type: 'checkbox',
+      name: 'nfs_srv_v4_v3owner',
+      placeholder: 'NFSv3 ownership model for NFSv4:',
       relation: [
         {
           action: 'DISABLE',
           when: [
             {
-              id: 'nfs_srv_16',
+              name: 'nfs_srv_16',
               value: true,
             }
           ]
         },
-      ],
-    }),
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_v4_krb',
-      label: 'Require Kerberos for NFSv4:',
-    }),
-    new DynamicInputModel({
-        id: 'nfs_srv_mountd_port',
-        label: 'mountd(8) bind port:',
-    }),
-    new DynamicInputModel({
-        id: 'nfs_srv_rpcstatd_port',
-        label: 'rpc.statd(8) bind port:',
-    }),
-    new DynamicInputModel({
-        id: 'nfs_srv_rpclockd_port',
-        label: 'rpc.lockd(8) bind port:',
-    }),
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_16',
-      label: 'Support >16 groups:',
-    }),
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_mountd_log',
-      label: 'Log mountd(8) requests:',
-    }),
-    new DynamicCheckboxModel({
-      id: 'nfs_srv_statd_lockd_log',
-      label: 'Log rpc.statd(8) and rpc.lockd(8)',
-    }),
+        ],
+    },
+    {
+      type: 'checkbox',
+      name: 'nfs_srv_v4_krb',
+      placeholder: 'Require Kerberos for NFSv4:',
+    },
+    {
+      type: 'input',
+      name: 'nfs_srv_mountd_port',
+      placeholder: 'mountd(8) bind port:',
+    },
+    {
+      type: 'input',
+      name: 'nfs_srv_rpcstatd_port',
+      placeholder: 'rpc.statd(8) bind port:',
+    },
+    {
+      type: 'input',
+      name: 'nfs_srv_rpclockd_port',
+      placeholder: 'rpc.lockd(8) bind port:',
+    },
+    {
+      type: 'checkbox',
+      name: 'nfs_srv_16',
+      placeholder: 'Support >16 groups:',
+    },
+    {
+      type: 'checkbox',
+      name: 'nfs_srv_mountd_log',
+      placeholder: 'Log mountd(8) requests:',
+    },
+    {
+      type: 'checkbox',
+      name: 'nfs_srv_statd_lockd_log',
+      placeholder: 'Log rpc.statd(8) and rpc.lockd(8)',
+    },
   ];
+
+  private nfs_srv_bindip: any;
+  ngOnInit() {
+  this.ws.call('notifier.choices', ['IPChoices']).subscribe((res) => {
+    this.nfs_srv_bindip = _.find(this.fieldConfig, { name :'nfs_srv_bindip'});
+    res.forEach((item) => {
+      this.nfs_srv_bindip.options.push({label: item[0], value: item[0]});
+    });
+  });
 }
+  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService, 
+              protected ws: WebSocketService, protected _injector: Injector, protected _appRef: ApplicationRef,
+              protected _state: GlobalState, protected userService: UserService) {
+  }
 
   afterInit(entityEdit: any) {
     this.entityEdit = entityEdit;
-    /*
-    let self = this
-    this.ws.call('notifier.choices', ['IPChoices']).subscribe((res) => {
-      self.nfs_srv_bindip = <DynamicCheckboxGroupModel>self.formService.findById('nfs_srv_bindip', self.formModel);
-      res.forEach((item) => {
-        self.nfs_srv_bindip;
-      });
-    });
-   */
   }
-
 }
 
 
