e925b649d5fd53af568b5c525dc801cd53141385
Xin Tan <mactanxin@gmail.com>
2017-06-21 14:09:05 -0700

    UPDATED: tunable add with new form

--- src/app/pages/system/tunable/tunable-add/tunable-add.component.ts
+++ src/app/pages/system/tunable/tunable-add/tunable-add.component.ts
@@ -1,46 +1,55 @@
 import { ApplicationRef, Component, Injector, OnInit, ViewContainerRef } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormArray, Validators, AbstractControl} from '@angular/forms';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
+import { EntityConfigComponent } from '../../../common/entity/entity-config/';
 
-import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel, DynamicSelectModel, DynamicRadioGroupModel, DynamicTextAreaModel } from '@ng2-dynamic-forms/core';
 import { GlobalState } from '../../../../global.state';
 import { RestService, WebSocketService } from '../../../../services/';
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import * as _ from 'lodash';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'system-tunable-add',
-  template: `<entity-add [conf]="this"></entity-add>`
+  template: `<entity-form [conf]="this"></entity-form>`
 })
 
 export class TunableAddComponent {
 
   protected resource_name: string = 'system/tunable';
-  protected route_success: string[] = ['system','tunable'];
-  protected formModel: DynamicFormControlModel[] = [
-    new DynamicInputModel({
-        id: 'tun_var',
-        label: 'Variable',
-    }),
-    new DynamicTextAreaModel({
-        id: 'tun_value',
-        label: 'Value',
-    }),
-    new DynamicSelectModel({
-        id: 'tun_type',
-        label: 'Type',
+  protected route_success: string[] = ['system', 'tunable'];
+  protected isEntity: boolean = true;
+  protected fieldConfig: FieldConfig[] = [
+    {
+        type: 'input',
+        name: 'tun_var',
+        placeholder: 'Variable',
+    },
+    {
+        type: 'textarea',
+        name: 'tun_value',
+        placeholder: 'Value',
+    },
+    {
+        type: 'select',
+        name: 'tun_type',
+        placeholder: 'Type',
         options: [
             {label: 'Loader', value: 'loader'},
             {label: 'rc.conf', value: 'rc'},
             {label: 'Sysctl', value: 'sysctl'},
         ]
-    }),
-    new DynamicInputModel({
-        id: 'tun_comment',
-        label: 'Comment',
-    }),
-    new DynamicCheckboxModel({
-        id: 'tun_enabled',
-        label: 'Enable',
-    }),
+    },
+    {
+        type: 'input',
+        name: 'tun_comment',
+        placeholder: 'Comment',
+    },
+    {
+        type: 'checkbox',
+        name: 'tun_enabled',
+        placeholder: 'Enable',
+    },
   ];
 
   afterInit() {
@@ -48,8 +57,14 @@ export class TunableAddComponent {
     });
   }
 
-  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService, protected ws: WebSocketService, protected formService: DynamicFormService, protected _injector: Injector, protected _appRef: ApplicationRef, protected _state: GlobalState) {
-
-  }
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
+    protected rest: RestService,
+    protected ws: WebSocketService,
+    protected _injector: Injector,
+    protected _appRef: ApplicationRef,
+    protected _state: GlobalState
+  ) {}
 
 }
