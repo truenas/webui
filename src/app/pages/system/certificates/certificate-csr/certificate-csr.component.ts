e3ce7e9eabcd1a9a06160ef6a759caa5f6bcdd25
Xin Tan <mactanxin@gmail.com>
2017-06-21 11:14:15 -0700

    UPDATED: refactor csr page with new form

--- src/app/pages/system/certificates/certificate-csr/certificate-csr.component.ts
+++ src/app/pages/system/certificates/certificate-csr/certificate-csr.component.ts
@@ -1,37 +1,45 @@
 import { ApplicationRef, Component, Injector, OnInit, ViewContainerRef } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormArray, Validators, AbstractControl} from '@angular/forms';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
+import { EntityConfigComponent } from '../../../common/entity/entity-config/';
 
-import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel, DynamicSelectModel, DynamicRadioGroupModel, DynamicTextAreaModel } from '@ng2-dynamic-forms/core';
 import { GlobalState } from '../../../../global.state';
-import { RestService, WebSocketService } from '../../../../services/';
+import { RestService, WebSocketService} from '../../../../services/';
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import * as _ from 'lodash';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'system-certificate-csr',
-  template: `<entity-add [conf]="this"></entity-add>`
+  template: `<entity-form [conf]="this"></entity-form>`
 })
 
 export class CertificateCSRComponent {
 
   protected resource_name: string = 'system/certificate/csr';
   protected route_success: string[] = ['system','certificates'];
-  protected formModel: DynamicFormControlModel[] = [
-    new DynamicInputModel({
-        id: 'cert_name',
-        label: 'Identifier',
-    }),
-    new DynamicSelectModel({
-        id: 'cert_key_length',
-        label: 'Key Length',
+  protected isEntity: boolean = true;
+  protected fieldConfig: FieldConfig[] = [
+    {
+        type: 'input',
+        name: 'cert_name',
+        placeholder: 'Identifier',
+    },
+    {
+        type: 'select',
+        name: 'cert_key_length',
+        placeholder: 'Key Length',
         options: [
           { label: '1024', value: 1024 },
           { label: '2048', value: 2048 },
           { label: '4096', value: 4096 },
         ],
-    }),
-    new DynamicSelectModel({
-        id: 'cert_digest_algorithm',
-        label: 'Digest Algorithm',
+    },
+    {
+        type: 'select',
+        name: 'cert_digest_algorithm',
+        placeholder: 'Digest Algorithm',
         options: [
           { label: 'SHA1', value: 'SHA1' },
           { label: 'SHA224', value: 'SHA224' },
@@ -39,43 +47,55 @@ export class CertificateCSRComponent {
           { label: 'SHA384', value: 'SHA384' },
           { label: 'SHA512', value: 'SHA512' },
         ],
-    }),
-    new DynamicSelectModel({
-        id: 'cert_country',
-        label: 'country',
+    },
+    {
+        type: 'select',
+        name: 'cert_country',
+        placeholder: 'Country',
         options: [
           { label: 'US', value: 'US' },
           { label: 'CHINA', value: 'CN' },
           { label: 'RUSSIA', value: 'RU' },
         ],
-    }),
-    new DynamicInputModel({
-        id: 'cert_state',
-        label: 'state',
-    }),
-    new DynamicInputModel({
-        id: 'cert_city',
-        label: 'local',
-    }),
-    new DynamicInputModel({
-        id: 'cert_organization',
-        label: 'organization',
-    }),
-    new DynamicInputModel({
-        id: 'cert_email',
-        label: 'email',
-    }),
-    new DynamicInputModel({
-        id: 'cert_common',
-        label: 'common',
-    }),
+    },
+    {
+        type: 'input',
+        name: 'cert_state',
+        placeholder: 'State',
+    },
+    {
+        type: 'input',
+        name: 'cert_city',
+        placeholder: 'Locality',
+    },
+    {
+        type: 'input',
+        name: 'cert_organization',
+        placeholder: 'Organization',
+    },
+    {
+        type: 'input',
+        name: 'cert_email',
+        placeholder: 'Email',
+    },
+    {
+        type: 'input',
+        name: 'cert_common',
+        placeholder: 'Common Name',
+    },
   ];
 
   afterInit(entityEdit: any) {
   }
 
-  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService, protected ws: WebSocketService, protected formService: DynamicFormService, protected _injector: Injector, protected _appRef: ApplicationRef, protected _state: GlobalState) {
-
-  }
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
+    protected rest: RestService,
+    protected ws: WebSocketService,
+    protected _injector: Injector,
+    protected _appRef: ApplicationRef,
+    protected _state: GlobalState,
+  ) {}
 
 }
