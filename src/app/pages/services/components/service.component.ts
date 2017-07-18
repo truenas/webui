d09f1c1fdcf25acb6e3ae81606821b35105a9d8a
James T. Nixon III <james@ixsystems.com>
2017-07-18 05:14:17 -0700

    Theme enhancements: 	* Palette is Indigo/Light Blue 	* Services Page fixed 	* Theme is still relying on bootstrap 	* Need to see why card styling isnt consistent

--- src/app/pages/services/components/service.component.ts
+++ src/app/pages/services/components/service.component.ts
@@ -6,32 +6,8 @@ import {RestService, WebSocketService} from '../../../services/';
 
 @Component({
   selector : 'service',
-  styleUrls : [ './service.component.scss' ],
-  template : `
-  <md-card class=" col-xlg-4 col-xl-4 col-lg-6 col-sm-12 col-xs-12" title="{{status.label}} is {{status.state}}">
-    <div [ngBusy]="busy" class="row">
-      <div class="col-md-12">
-	<span [ngClass]="status.state == 'RUNNING' ? 'state-label-running' : 'state-label-stopped'" class="v-center">
-	</span>
-      </div>
-      <div class="col-md-4">
-        <ba-checkbox [(ngModel)]="status.enable" (change)="enableToggle($event)" [label]="'Start on Boot'" [baCheckboxClass]="'v-center'"></ba-checkbox>
-      </div>
-
-      <div class="col-md-4">
-        <button class="btn btn-outline-warning" (click)="editService(this.status.service)">
-          <i class="ion-wrench"></i>
-          <span>Edit</span>
-        </button>
-      </div>
-      <div class="col-md-4">
-        <a [ngClass]="status.state == 'RUNNING' ? 'btn btn-danger btn-fab' : 'btn btn-success btn-fab'" (click)="toggle()">
-          <i [ngClass]="status.state == 'RUNNING' ? 'ion-stop' : 'ion-power'"></i>
-        </a>
-      </div>
-    </div>
-  </md-card>
-  `,
+  templateUrl : './service.component.html',
+  styleUrls : [ './service.component.scss' ]
 })
 export class Service {
 
