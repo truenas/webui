2ad8478b498d5c02f4935b86444052eaa8201a2c
Xin Tan <mactanxin@gmail.com>
2017-07-19 13:32:43 -0700

    ADDED: confirm dialog component

---
+++ src/app/pages/common/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,23 @@
+import { MdDialogRef } from '@angular/material';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'confirm-dialog',
+    template: `
+        <p>{{ title }}</p>
+        <p>{{ message }}</p>
+        <button md-raised-button 
+            (click)="dialogRef.close(true)">OK</button>
+        <button md-button 
+            (click)="dialogRef.close(false)">Cancel</button>
+    `,
+})
+export class ConfirmDialog {
+
+    public title: string;
+    public message: string;
+
+    constructor(public dialogRef: MdDialogRef<ConfirmDialog>) {
+
+    }
+}
