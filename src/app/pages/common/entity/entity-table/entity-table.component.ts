af923983166041d8230c18d26fcf98e2162554d7
Xin Tan <mactanxin@gmail.com>
2017-07-21 12:57:52 -0700

    UPDATED: prettify my code

--- src/app/pages/common/entity/entity-table/entity-table.component.ts
+++ src/app/pages/common/entity/entity-table/entity-table.component.ts
@@ -1,15 +1,23 @@
-import {Component, ElementRef, Input, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {Subscription} from 'rxjs';
+import { Component, OnInit, Input, ElementRef, ViewEncapsulation, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataSource } from '@angular/cdk';
+import { MdPaginator, MdSort } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
 
-import {GlobalState} from '../../../../global.state';
-import {RestService} from '../../../../services/rest.service';
-import {EntityUtils} from '../utils';
+//local libs
+import { GlobalState } from '../../../../global.state';
+import { RestService } from '../../../../services/rest.service';
+import { EntityUtils } from '../utils';
 
 @Component({
-  selector : 'entity-table',
-  templateUrl : './entity-table.component.html',
-  styleUrls : [ './entity-table.component.css' ]
+  selector: 'entity-table',
+  templateUrl: './entity-table.component.html',
+  styleUrls: ['./entity-table.component.css']
 })
 export class EntityTableComponent implements OnInit {
 
@@ -17,20 +25,20 @@ export class EntityTableComponent implements OnInit {
 
   public busy: Subscription;
 
-  public rows: Array<any> = [];
-  public columns: Array<any> = [];
+  public rows: Array < any > = [];
+  public columns: Array < any > = [];
   public page: number = 1;
   public itemsPerPage: number = 10;
   public maxSize: number = 5;
   public numPages: number = 1;
   public length: number = 0;
   public config: any = {
-    paging : true,
-    sorting : {columns : this.columns},
+    paging: true,
+    sorting: { columns: this.columns },
   };
 
   constructor(protected rest: RestService, protected router: Router,
-              protected _state: GlobalState, protected _eRef: ElementRef) {}
+    protected _state: GlobalState, protected _eRef: ElementRef) {}
 
   ngOnInit() {
     if (this.conf.preInit) {
@@ -44,7 +52,7 @@ export class EntityTableComponent implements OnInit {
 
   getData() {
     let offset = this.itemsPerPage * (this.page - 1);
-    let sort: Array<String> = [];
+    let sort: Array < String > = [];
     let options: Object = new Object();
 
     for (let i in this.config.sorting.columns) {
@@ -57,21 +65,21 @@ export class EntityTableComponent implements OnInit {
     }
 
     // options = {limit: this.itemsPerPage, offset: offset};
-    options = {limit : 0};
+    options = { limit: 0 };
     if (sort.length > 0) {
       options['sort'] = sort.join(',');
     }
 
     this.busy =
-        this.rest.get(this.conf.resource_name, options).subscribe((res) => {
-          this.length = res.total;
-          this.rows = new EntityUtils().flattenData(res.data);
-        });
+      this.rest.get(this.conf.resource_name, options).subscribe((res) => {
+        this.length = res.total;
+        this.rows = new EntityUtils().flattenData(res.data);
+      });
   }
 
   onChangeTable(
-      config,
-      page: any = {page : this.page, itemsPerPage : this.itemsPerPage}) {
+    config,
+    page: any = { page: this.page, itemsPerPage: this.itemsPerPage }) {
     if (config.filtering) {
       Object.assign(this.config.filtering, config.filtering);
     }
@@ -95,18 +103,15 @@ export class EntityTableComponent implements OnInit {
     if (this.conf.getActions) {
       return this.conf.getActions(row);
     } else {
-      return [
-        {
-          id : "edit",
-          label : "Edit",
-          onClick : (row) => { this.doEdit(row.id); },
-        },
-        {
-          id : "delete",
-          label : "Delete",
-          onClick : (row) => { this.doDelete(row.id); },
-        },
-      ]
+      return [{
+        id: "edit",
+        label: "Edit",
+        onClick: (row) => { this.doEdit(row.id); },
+      }, {
+        id: "delete",
+        label: "Delete",
+        onClick: (row) => { this.doDelete(row.id); },
+      }, ]
     }
   }
 
@@ -131,11 +136,11 @@ export class EntityTableComponent implements OnInit {
 
   doEdit(id) {
     this.router.navigate(
-        new Array('/pages').concat(this.conf.route_edit).concat(id));
+      new Array('/pages').concat(this.conf.route_edit).concat(id));
   }
 
   doDelete(id) {
     this.router.navigate(
-        new Array('/pages').concat(this.conf.route_delete).concat(id));
+      new Array('/pages').concat(this.conf.route_delete).concat(id));
   }
 }
