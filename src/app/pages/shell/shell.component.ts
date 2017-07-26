82d8357a066ff92f37d3e15e06de03eaea290ce2
Xin Tan <mactanxin@gmail.com>
2017-07-26 15:35:46 -0700

    ADDED: connect to websocket, onOpen func with send token to websocket

--- src/app/pages/shell/shell.component.ts
+++ src/app/pages/shell/shell.component.ts
@@ -15,17 +15,37 @@ export class ShellComponent implements OnInit {
   constructor(private rest: RestService, private ws: WebSocketService) {}
   @ViewChild('terminal') container: ElementRef;
   public xterm: xterm;
+  public token: any;
+  public socket: WebSocket;
 
   ngOnInit() {
-    this.xterm = new Terminal();
+    this.xterm = new Terminal({
+    	'cursorBlink': true,
+    	'tabStopWidth': 4,
+    	'cols': 80,
+    	'rows': 50
+    });
+    var protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
+    var socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + "/websocket/shell/";
     this.xterm.open(this.container.nativeElement);
-    // this.xterm.fit();
     this.xterm.writeln('Welcome to Xin-xterm.js');
-    this.xterm.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
-    this.xterm.writeln('Type some keys and commands to play around.');
-
+    this.generateToken();
+    this.socket = new WebSocket(socketURL);
+    this.socket.onopen = this.onOpen;
     this.xterm.on('key', (key, ev) => {
       this.xterm.write(key);
     });
+  }
+
+  generateToken() {
+  	this.ws.call('auth.generate_token').subscribe((res) => {
+  		this.token = res;
+  	});
+  }
+
+  onOpen() {
+	this.socket.send(JSON.stringify({
+		token: this.token
+	}));
   }
 }
