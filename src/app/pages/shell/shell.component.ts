6e50227fd791104f54b09e006e0002ac2ee045b0
Xin Tan <mactanxin@gmail.com>
2017-07-26 17:41:08 -0700

    ADDED: now can connect to webshell with new service, still need stdin/stdout communicate with webshell and userinput

--- src/app/pages/shell/shell.component.ts
+++ src/app/pages/shell/shell.component.ts
@@ -1,51 +1,78 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter,
+  SimpleChange
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 
-import { RestService, WebSocketService } from '../../services/';
+import { WebSocketService, ShellService } from '../../services/';
 import * as xterm from "xterm";
 import * as Terminal from 'xterm/dist/xterm';
 import 'xterm/dist/addons/fit/fit.js';
 
 @Component({
   selector: 'app-shell',
-  templateUrl: './shell.component.html'
+  templateUrl: './shell.component.html',
+  providers: [ShellService],
 })
 
-export class ShellComponent implements OnInit {
-  constructor(private rest: RestService, private ws: WebSocketService) {}
+export class ShellComponent implements OnInit, OnChanges {
+  // This Xterm implementation emits all commands, once the users presses 'Enter'
+  @Output() stdin: EventEmitter < string > = new EventEmitter < string > ();
+  // waits for events that should be printed on the terminal
+  @Input() stdout: EventEmitter < string > ;
+  // sets the shell prompt
+  @Input() prompt: string;
+
+  constructor(private ws: WebSocketService, public ss: ShellService) {
+
+  }
   @ViewChild('terminal') container: ElementRef;
   public xterm: xterm;
   public token: any;
-  public socket: WebSocket;
+
+  clearLine = "\u001b[2K\r"
 
   ngOnInit() {
     this.xterm = new Terminal({
-    	'cursorBlink': true,
-    	'tabStopWidth': 4,
-    	'cols': 80,
-    	'rows': 50
+      'cursorBlink': true,
+      'tabStopWidth': 4,
+      'cols': 80,
+      'rows': 50,
+      'focus': true
     });
-    var protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
-    var socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + "/websocket/shell/";
+
     this.xterm.open(this.container.nativeElement);
     this.xterm.writeln('Welcome to Xin-xterm.js');
+    var protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
     this.generateToken();
-    this.socket = new WebSocket(socketURL);
-    this.socket.onopen = this.onOpen;
     this.xterm.on('key', (key, ev) => {
       this.xterm.write(key);
     });
   }
 
-  generateToken() {
-  	this.ws.call('auth.generate_token').subscribe((res) => {
-  		this.token = res;
-  	});
+  ngOnChanges(changes: {
+    [propKey: string]: SimpleChange }) {
+    let log: string[] = [];
+    for (let propName in changes) {
+      let changedProp = changes[propName];
+      // reprint prompt
+      if (propName == 'prompt' && this.xterm != null) {
+        this.xterm.write(this.clearLine + this.prompt)
+      }
+    }
   }
 
-  onOpen() {
-	this.socket.send(JSON.stringify({
-		token: this.token
-	}));
+  generateToken() {
+    this.ws.call('auth.generate_token').subscribe((res) => {
+      this.ss.token = res;
+      this.ss.connect();
+    });
   }
 }
