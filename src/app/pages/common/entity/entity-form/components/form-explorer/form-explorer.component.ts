7c5a9a421e78b1c840021c37078320306816072b
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-07-31 19:25:15 -0700

    async may work or not work at all but it's worth a try(WIP).

--- src/app/pages/common/entity/entity-form/components/form-explorer/form-explorer.component.ts
+++ src/app/pages/common/entity/entity-form/components/form-explorer/form-explorer.component.ts
@@ -1,11 +1,32 @@
 import {Component, ViewContainerRef, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {EntityFormService} from '../../services/entity-form.service';
-import {TreeModel, NodeEvent} from 'ng2-tree';
+import { TreeNode, TREE_ACTIONS, KEYS, IActionMapping } from 'angular-tree-component';
+
 
 import {FieldConfig} from '../../models/field-config.interface';
 import {Field} from '../../models/field.interface';
 import {TooltipComponent} from '../tooltip/tooltip.component';
+
+const actionMapping:IActionMapping = {
+  mouse: {
+    contextMenu: (tree, node, $event) => {
+      $event.preventDefault();
+      alert(`context menu for ${node.data.name}`);
+    },
+    dblClick: (tree, node, $event) => {
+      if (node.hasChildren) TREE_ACTIONS.TOGGLE_EXPANDED(tree, node, $event);
+    },
+    click: (tree, node, $event) => {
+      $event.shiftKey
+        ? TREE_ACTIONS.TOGGLE_SELECTED_MULTI(tree, node, $event)
+        : TREE_ACTIONS.TOGGLE_SELECTED(tree, node, $event)
+    }
+  },
+  keys: {
+    [KEYS.ENTER]: (tree, node, $event) => alert(`This is ${node.data.name}`)
+  }
+}
 
 @Component({
   selector : 'form-explorer',
@@ -16,28 +37,47 @@ export class FormExplorerComponent implements Field, OnInit {
   config: FieldConfig;
   group: FormGroup;
   fieldShow: string;
+  nodes: any[];
 
-  private tree: TreeModel;
   private treeVisible: boolean = false;
 
 constructor (private entityFormService : EntityFormService){}
 
   ngOnInit() {
-    this.tree = {
-      value: this.config.initial,
-      children: this.entityFormService.getFilesystemListdir(this.config.initial),
-      //loadChildren: (callback) => {
-      //  this.entityFormService.getChildren(callback, this.config.initial)
-      //}
+    this.nodes = [{
+        name: '/mnt',
+        hasChildren: true
+      }];
+  }
+  asyncChildren = [
+    {
+      name: 'child2.1',
+      subTitle: 'new and improved'
+    }, {
+      name: 'child2.2',
+      subTitle: 'new and improved2'
     }
-  };
+  ];
 
-  private onNodeSelected(e: NodeEvent): void {
-    this.config.value = e.node.node['path'];
+  getChildren(node:any) {
+    return new Promise((resolve, reject) => {
+      resolve(this.asyncChildren.map((c) => {
+        return Object.assign({}, c, {
+          hasChildren: node.level < 5
+        });
+      }));
+    });
   }
 
-  private handleExpanded(e: NodeEvent): void {
-    this.config.value = e.node.node['path'];
+  customTemplateStringOptions = {
+    // displayField: 'subTitle',
+    isExpandedField: 'expanded',
+    idField: 'uuid',
+    getChildren: this.getChildren.bind(this),
+    actionMapping,
+    nodeHeight: 23,
+    allowDrag: true,
+    useVirtualScroll: false
   }
   
   private toggleTree() {
