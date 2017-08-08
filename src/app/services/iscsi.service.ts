b9368b95399a343059ca684494dbfb358954a959
Lola Yang <lola.ly27@gmail.com>
2017-08-08 15:12:00 -0700

    Added: get targets/extents from iscsi service

--- src/app/services/iscsi.service.ts
+++ src/app/services/iscsi.service.ts
@@ -10,12 +10,12 @@ import {RestService, WebSocketService} from '../services/';
 @Injectable()
 export class IscsiService {
   protected iscsiPortalResource: string = 'services/iscsi/portal';
-  protected iscsiInitiatorResource: string =
-      'services/iscsi/authorizedinitiator';
-  protected iscsiAuthCredentialResource: string =
-      'services/iscsi/authcredential';
+  protected iscsiInitiatorResource: string = 'services/iscsi/authorizedinitiator';
+  protected iscsiAuthCredentialResource: string = 'services/iscsi/authcredential';
   protected iscsiTargetGroupsResource: string = 'services/iscsi/targetgroup';
   protected volumeResource: string = 'storage/volume';
+  protected iscsiExtentResource: string = 'services/iscsi/extent/';
+  protected iscsiTargetResource: string = 'services/iscsi/target/';
 
   constructor(protected rest: RestService, protected ws: WebSocketService) {};
 
@@ -44,5 +44,13 @@ export class IscsiService {
   getZvols(volume) {
     let rs = this.volumeResource + '/' + volume + '/zvols';
     return this.rest.get(rs, {});
+  }
+
+  getExtents() {
+    return this.rest.get(this.iscsiExtentResource, {});
+  }
+
+  getTargets() {
+    return this.rest.get(this.iscsiTargetResource, {});
   }
 }
