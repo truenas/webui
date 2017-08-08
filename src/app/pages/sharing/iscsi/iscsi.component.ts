8df0726a0750bc6afcb22649b36d38261a03ea60
Lola Yang <lola.ly27@gmail.com>
2017-08-08 16:29:43 -0700

    ISCSI ui using md-tab to show protals/initiators/targets... tables instead of listing all tables at one page

--- src/app/pages/sharing/iscsi/iscsi.component.ts
+++ src/app/pages/sharing/iscsi/iscsi.component.ts
@@ -8,27 +8,32 @@ import {
 @Component({
   selector : 'iscsi',
   template : `
-    <h2>Global Configuration</h2>
-    <app-iscsi-globalconfiguration></app-iscsi-globalconfiguration>
-    
-    <h2>Portals</h2>
-    <app-iscsi-portal-list></app-iscsi-portal-list>
-
-    <h2>Initiators</h2>
-    <app-iscsi-initiator-list></app-iscsi-initiator-list>
-
-    <h2>Authorized Access</h2>
-    <app-iscsi-authorizedaccess-list></app-iscsi-authorizedaccess-list>
-
-    <h2>Targets</h2>
-    <app-iscsi-target-list></app-iscsi-target-list>
-
-    <h2>Extents</h2>
-    <app-iscsi-extent-list></app-iscsi-extent-list>
-
-    <h2>Associated Targets</h2>
-    <app-iscsi-associated-target-list></app-iscsi-associated-target-list>
-    `
+    <md-card>
+      <md-tab-group>
+        <md-tab label="Global Configuration">
+          <app-iscsi-globalconfiguration></app-iscsi-globalconfiguration>
+        </md-tab>
+        <md-tab label="Portals">
+          <app-iscsi-portal-list></app-iscsi-portal-list>
+        </md-tab>
+        <md-tab label="Initiators">
+          <app-iscsi-initiator-list></app-iscsi-initiator-list>
+        </md-tab>
+        <md-tab label="Authorized Access">
+          <app-iscsi-authorizedaccess-list></app-iscsi-authorizedaccess-list>
+        </md-tab>
+        <md-tab label="Targets">
+          <app-iscsi-target-list></app-iscsi-target-list>
+        </md-tab>
+        <md-tab label="Extents">
+          <app-iscsi-extent-list></app-iscsi-extent-list>
+        </md-tab>
+        <md-tab label="Associated Targets">
+          <app-iscsi-associated-target-list></app-iscsi-associated-target-list>
+        </md-tab>
+      </md-tab-group>
+    </md-card>
+  `,
 })
 export class ISCSI {
 
