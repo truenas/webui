ec4c5be7e3419d01375e364c70acf4f42c0ac12d
James T. Nixon III <james@ixsystems.com>
2017-08-14 18:56:12 -0700

    move all entity action buttons to fab bottom right location
    header cleanup

--- src/app/pages/common/entity/entity-table/entity-table-actions.component.ts
+++ src/app/pages/common/entity/entity-table/entity-table-actions.component.ts
@@ -6,42 +6,13 @@ import {GlobalState} from '../../../../global.state';
 import {RestService} from '../../../../services/rest.service';
 
 import {EntityTableComponent} from './entity-table.component';
-import * as _ from 'lodash';
 
 @Component({
   selector : 'app-entity-table-actions',
-  styleUrls: ['./entity-table-actions.component.scss'], 
   template : `
-    <i 
-      *ngIf="(editAction && iconActionVisible(editAction.id)) &&
-        (!entity.conf.isActionVisible 
-        || entity.conf.isActionVisible.bind(entity.conf)(editAction.id, row))"
-      (click)="editAction.onClick(row)"
-      class='ion-wrench'>
-    </i>
-    <i
-      *ngIf="(deleteAction && iconActionVisible(deleteAction.id)) &&
-        (!entity.conf.isActionVisible 
-        || entity.conf.isActionVisible.bind(entity.conf)(deleteAction.id, row))"
-      (click)="deleteAction.onClick(row)"
-      class='ion-trash-a'>
-    </i>
-    <ng2-dropdown *ngIf="showMenu">
-      <ng2-dropdown-button [showCaret]="false" class="ng2-dropdown-button--icon">
-        <a class='ion-android-more-vertical' ng2-menu-item-icon></a>
-      </ng2-dropdown-button>
-      <ng2-dropdown-menu [width]="1" [offset]="'74 -205'">
-        <span *ngFor="let action of actions">
-          <ng2-menu-item
-            *ngIf="!iconActionVisible(action.id) &&
-            (!entity.conf.isActionVisible 
-              || entity.conf.isActionVisible.bind(entity.conf)(action.id, row))"
-              (click)="action.onClick(this.row)">
-            <span>{{ action?.label }}</span>
-          </ng2-menu-item>
-        </span>
-      </ng2-dropdown-menu>
-    </ng2-dropdown>
+    <span *ngFor="let action of entity.getActions(row)">
+      <button *ngIf="!entity.conf.isActionVisible || entity.conf.isActionVisible.bind(entity.conf)(action.id, row)" md-mini-fab color="primary" class="md-fab-bottom-right-2" (click)="action.onClick(this.row)">{{ action?.label }}</button>
+    </span>
   `
 })
 export class EntityTableActionsComponent implements OnInit {
@@ -49,49 +20,17 @@ export class EntityTableActionsComponent implements OnInit {
   @Input('entity') entity: EntityTableComponent;
   @Input('row') row: any;
 
-  public actions: any[];
-  public editAction: any;
-  public deleteAction: any;
-  public actionIconsVisible: number = 0;
-  public showMenu: boolean = true;
-
-  iconActionVisible(id: string) {
-    if (id == 'edit' || id == 'delete') {
-      return this.actionIconsVisible > 0;      
-    } 
-    return false;
-  }
-
-  menuActionVisible(id: string) {
-    if (id == 'edit' || id == 'delete') {
-      false;
-    }
-    return true;
-  }
+  private actions: any[];
 
   ngOnInit() {
     this.actions = this.entity.getActions(this.row);
-    let removeIds = [];
-    for (let i = 0; i < this.actions.length; i++) {
+    this.actions.forEach((action) => {
       if (this.entity.conf.isActionVisible) {
-        this.actions[i].visible = this.entity.conf.isActionVisible.bind(
-            this.entity.conf)(this.actions[i].id, this.row);
+        action.visible = this.entity.conf.isActionVisible.bind(
+            this.entity.conf)(action.id, this.row);
       } else {
-        this.actions[i].visible = true;
+        action.visible = true;
       }
-      
-      if (this.actions[i].id == 'delete') {
-        this.deleteAction = this.actions[i];
-        this.actionIconsVisible += 1;
-      }
-      if (this.actions[i].id == 'edit') {
-        this.editAction = this.actions[i];
-        this.actionIconsVisible += 1;
-      }
-
-    }
-    if (this.actionIconsVisible >= this.actions.length) {
-      this.showMenu = false;
-    }
+    });
   }
 }
--- src/app/pages/common/entity/entity-table/entity-table-add-actions.component.ts
+++ src/app/pages/common/entity/entity-table/entity-table-add-actions.component.ts
@@ -11,7 +11,7 @@ import {EntityTableComponent} from './entity-table.component';
   selector : 'app-entity-table-add-actions',
   template : `
     <span *ngFor="let action of actions">
-        <button md-mini-fab (click)="action.onClick()" color="accent"><md-icon>add_circle</md-icon></button>
+        <button md-mini-fab class="md-fab-bottom-right-2" (click)="action.onClick()" color="accent"><md-icon>add_circle</md-icon></button>
     </span>
   `
 })
