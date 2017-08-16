95d021b4a7978f9393b90d3943a3946174fe34c1
peter_southwell <pete_south@yahoo.com>
2017-08-15 23:11:29 -0700

    this gets the new ng2-chart-line loaded and displayed.  It has NO paint refresh issues at all.. And in my humble opinion.. Looks cool.  TOmorrow morning Ill hook the data up... Took me a bit to get the thing loaded and working.. NExt Ill just shove the WS data into the control.. Replace these three and we are good to go!!!

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {ChartistModule} from 'ng-chartist';
+import { ChartsModule } from 'ng2-charts';
 
 import {NgaModule} from '../../theme/nga.module';
 import { MaterialModule, MdCardModule } from '@angular/material';
@@ -10,13 +11,15 @@ import {Dashboard} from './dashboard.component';
 import {routing} from './dashboard.routing';
 import {LineChart} from './lineChart/lineChart.component';
 import {LineChartService} from './lineChart/lineChart.service';
+import { NglineChartComponent } from './ngline-chart/ngline-chart.component';
 
 @NgModule({
-  imports : [ CommonModule, ChartistModule, FormsModule, NgaModule, routing, 
+  imports : [ CommonModule, ChartistModule, ChartsModule, FormsModule, NgaModule, routing, 
   MaterialModule, MdCardModule ],
   declarations : [
     Dashboard,
     LineChart,
+    NglineChartComponent
   ],
   providers : [
     LineChartService,
