b36486d019da6f055d0391cd511afab7fda85438
Xin Tan <mactanxin@gmail.com>
2017-08-18 15:09:18 -0700

    ADDED: create action icon for adding new tunable

--- src/app/pages/system/tunable/tunable-list/tunable-list.component.ts
+++ src/app/pages/system/tunable/tunable-list/tunable-list.component.ts
@@ -1,5 +1,5 @@
-import {ApplicationRef, Component, Injector, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import {
   DynamicCheckboxModel,
   DynamicFormControlModel,
@@ -9,55 +9,54 @@ import {
   DynamicSelectModel
 } from '@ng2-dynamic-forms/core';
 import * as _ from 'lodash';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 
-import {GlobalState} from '../../../../global.state';
-import {RestService, WebSocketService} from '../../../../services/';
+import { GlobalState } from '../../../../global.state';
+import { RestService, WebSocketService } from '../../../../services/';
 
 @Component({
-  selector : 'system-tunables-list',
-  template : `<entity-table [conf]="this"></entity-table>`
+  selector: 'system-tunables-list',
+  template: `<entity-table [conf]="this"></entity-table>`
 })
 
 export class TunableListComponent {
 
   protected resource_name: string = 'system/tunable';
-  protected route_delete: string[] = [ 'system', 'tunable', 'delete' ];
-  protected route_edit: string[] = [ 'system', 'tunable', 'edit' ];
+  protected route_delete: string[] = ['system', 'tunable', 'delete'];
+  protected route_edit: string[] = ['system', 'tunable', 'edit'];
 
   public busy: Subscription;
   public sub: Subscription;
 
   constructor(protected router: Router, protected aroute: ActivatedRoute,
-              protected rest: RestService, protected ws: WebSocketService,
-              protected formService: DynamicFormService,
-              protected _injector: Injector, protected _appRef: ApplicationRef,
-              protected _state: GlobalState) {}
+    protected rest: RestService, protected ws: WebSocketService,
+    protected formService: DynamicFormService,
+    protected _injector: Injector, protected _appRef: ApplicationRef,
+    protected _state: GlobalState) {}
 
-  public columns: Array<any> = [
-    {name : 'Variable', prop : 'tun_var'},
-    {name : 'Value', prop : 'tun_value'},
-    {name : 'Type', prop : 'tun_type'},
-    {name : 'Comment', prop : 'tun_comment'},
-    {name : 'Enable', prop : 'tun_enabled'},
+  public columns: Array < any > = [
+    { name: 'Variable', prop: 'tun_var' },
+    { name: 'Value', prop: 'tun_value' },
+    { name: 'Type', prop: 'tun_type' },
+    { name: 'Comment', prop: 'tun_comment' },
+    { name: 'Enable', prop: 'tun_enabled' },
   ];
 
   public config: any = {
-    paging : true,
-    sorting : {columns : this.columns},
+    paging: true,
+    sorting: { columns: this.columns },
   }
 
   getAddActions() {
     let actions = [];
-    actions.push(
-        {
-          label : "Add Tunable",
-          onClick : () => {
-            this.router.navigate(
-                new Array('/pages').concat([ "system", "tunable", "add" ]));
-          }
-        },
-    );
+    actions.push({
+      label: "Add Tunable",
+      icon: "fiber_new",
+      onClick: () => {
+        this.router.navigate(
+          new Array('/pages').concat(["system", "tunable", "add"]));
+      }
+    }, );
 
     return actions;
   }
