d2158ee614c9f36c52ff7c6cbc1399f79f96237b
peter_southwell <pete_south@yahoo.com>
2017-08-22 10:02:37 -0700

    moved data out into the service.  Refactored from any to actual types.  Which not apparrently needed.. buy benefit with tools, trackability etc..

--- src/app/pages/reportsdashboard/lineChart/lineChart.service.ts
+++ src/app/pages/reportsdashboard/lineChart/lineChart.service.ts
@@ -4,14 +4,27 @@ import {WebSocketService} from '../../../services/';
 import {BaThemeConfigProvider, colorHelper, layoutPaths} from '../../../theme';
 
 export interface LineChartData {
-    labels: Date[];
-    series: any[];
+  labels: Date[];
+  series: any[];
+}
+
+
+export interface DataListItem {
+  source: string;
+  type: string;
+  dataset: string;
+}
+
+export interface ChartConfigData {
+  title: string;
+  legends: string[];
+  dataList: DataListItem[];
 }
 
 
 export interface HandleDataFunc {
-  handleDataFunc( lineChartData: LineChartData);
-  
+  handleDataFunc(lineChartData: LineChartData);
+
 }
 
 
@@ -19,26 +32,84 @@ export interface HandleDataFunc {
 export class LineChartService {
 
   constructor(private _baConfig: BaThemeConfigProvider,
-              private _ws: WebSocketService) {}
+    private _ws: WebSocketService) {}
 
   getData(dataHandlerInterface: HandleDataFunc, dataList: any[]) {
 
-    this._ws.call('stats.get_data', [ dataList, {} ]).subscribe((res) => {
+    this._ws.call('stats.get_data', [dataList, {}]).subscribe((res) => {
       let linechartData: LineChartData = {
         labels: new Array<Date>(),
         series: new Array<any>()
       }
-      
-      dataList.forEach(() => { linechartData.series.push([]); })
+
+      dataList.forEach(() => {linechartData.series.push([]);})
       res.data.forEach((item, i) => {
         linechartData.labels.push(
-            new Date(res.meta.start * 1000 + i * res.meta.step * 1000));
+          new Date(res.meta.start * 1000 + i * res.meta.step * 1000));
         for (let x in dataList) {
           linechartData.series[x].push(item[x]);
         }
       });
-      
+
       dataHandlerInterface.handleDataFunc(linechartData);
     });
   }
+
+  getChartConfigData(): ChartConfigData[] {
+    return [
+      {
+        title: "Average Load",
+        legends: ['Short Term', ' Mid Term', 'Long Term'],
+        dataList: [
+          {source: 'load', type: 'load', dataset: 'shortterm'},
+          {'source': 'load', 'type': 'load', 'dataset': 'midterm'},
+          {'source': 'load', 'type': 'load', 'dataset': 'longterm'},
+        ],
+      },
+      {
+        title: "Memory",
+        legends: ['Free', 'Active', 'Cache', 'Wired', 'Inactive'],
+        dataList: [
+          {'source': 'memory', 'type': 'memory-free', 'dataset': 'value'},
+          {'source': 'memory', 'type': 'memory-active', 'dataset': 'value'},
+          {'source': 'memory', 'type': 'memory-cache', 'dataset': 'value'},
+          {'source': 'memory', 'type': 'memory-wired', 'dataset': 'value'},
+          {'source': 'memory', 'type': 'memory-inactive', 'dataset': 'value'},
+        ],
+      },
+      {
+        title: "CPU Usage",
+        legends: ['User', 'Interrupt', 'System', 'Idle', 'Nice'],
+        dataList: [
+          {
+            'source': 'aggregation-cpu-sum',
+            'type': 'cpu-user',
+            'dataset': 'value'
+          },
+          {
+            'source': 'aggregation-cpu-sum',
+            'type': 'cpu-interrupt',
+            'dataset': 'value'
+          },
+          {
+            'source': 'aggregation-cpu-sum',
+            'type': 'cpu-system',
+            'dataset': 'value'
+          },
+          {
+            'source': 'aggregation-cpu-sum',
+            'type': 'cpu-idle',
+            'dataset': 'value'
+          },
+          {
+            'source': 'aggregation-cpu-sum',
+            'type': 'cpu-nice',
+            'dataset': 'value'
+          },
+        ],
+      }
+    ];
+  }
+  
+  
 }
--- src/app/pages/reportsdashboard/reportsdashboard.component.ts
+++ src/app/pages/reportsdashboard/reportsdashboard.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import * as _ from 'lodash';
 import {Subscription} from 'rxjs';
+import {LineChartService, ChartConfigData} from './lineChart/lineChart.service';
 
 import {
   RestService,
@@ -20,67 +21,14 @@ export class ReportsDashboard implements OnInit {
   public ipAddress: any = [];
   private allowChartsDisplay: boolean = true;
   
-  public graphs: any[] = [
-    {
-      title : "Average Load",
-      legends : [ 'Short Term', ' Mid Term', 'Long Term' ],
-      dataList : [
-        {'source' : 'load', 'type' : 'load', 'dataset' : 'shortterm'},
-        {'source' : 'load', 'type' : 'load', 'dataset' : 'midterm'},
-        {'source' : 'load', 'type' : 'load', 'dataset' : 'longterm'},
-      ],
-    },
-    {
-      title : "Memory",
-      legends : [ 'Free', 'Active', 'Cache', 'Wired', 'Inactive' ],
-      dataList : [
-        {'source' : 'memory', 'type' : 'memory-free', 'dataset' : 'value'},
-        {'source' : 'memory', 'type' : 'memory-active', 'dataset' : 'value'},
-        {'source' : 'memory', 'type' : 'memory-cache', 'dataset' : 'value'},
-        {'source' : 'memory', 'type' : 'memory-wired', 'dataset' : 'value'},
-        {'source' : 'memory', 'type' : 'memory-inactive', 'dataset' : 'value'},
-      ],
-    },
-    {
-      title : "CPU Usage",
-      legends : [ 'User', 'Interrupt', 'System', 'Idle', 'Nice' ],
-      dataList : [
-        {
-          'source' : 'aggregation-cpu-sum',
-          'type' : 'cpu-user',
-          'dataset' : 'value'
-        },
-        {
-          'source' : 'aggregation-cpu-sum',
-          'type' : 'cpu-interrupt',
-          'dataset' : 'value'
-        },
-        {
-          'source' : 'aggregation-cpu-sum',
-          'type' : 'cpu-system',
-          'dataset' : 'value'
-        },
-        {
-          'source' : 'aggregation-cpu-sum',
-          'type' : 'cpu-idle',
-          'dataset' : 'value'
-        },
-        {
-          'source' : 'aggregation-cpu-sum',
-          'type' : 'cpu-nice',
-          'dataset' : 'value'
-        },
-      ],
-    },
-  ];
+  public graphs: ChartConfigData[] = [];
 
-  constructor(private rest: RestService, private ws: WebSocketService,
-              protected systemGeneralService: SystemGeneralService) {
+  constructor(private _lineChartService: LineChartService) {
     
   }
 
   ngOnInit() {
-   
+     this.graphs = this._lineChartService.getChartConfigData();
   }
   
   tabSelectChangeHandler($event) {
