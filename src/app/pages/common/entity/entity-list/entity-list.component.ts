074aae25a76551d5a7a08c2278a65fcfb16e4880
Xin Tan <mactanxin@gmail.com>
2017-08-24 15:21:49 -0700

    prettify code

--- src/app/pages/common/entity/entity-list/entity-list.component.ts
+++ src/app/pages/common/entity/entity-list/entity-list.component.ts
@@ -1,15 +1,16 @@
-import {Component, ElementRef, Input, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {Subscription} from 'rxjs';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
-import {GlobalState} from '../../../../global.state';
-import {RestService} from '../../../../services/rest.service';
-import {EntityUtils} from '../utils';
+import { GlobalState } from '../../../../global.state';
+import { RestService, DialogService } from '../../../../services/';
+import { EntityUtils } from '../utils';
 
 @Component({
-  selector : 'entity-list',
-  templateUrl : './entity-list.component.html',
-  styleUrls : [ './entity-list.component.css' ]
+  selector: 'entity-list',
+  templateUrl: './entity-list.component.html',
+  styleUrls: ['./entity-list.component.css'],
+  providers: [DialogService]
 })
 export class EntityListComponent implements OnInit {
 
@@ -17,20 +18,20 @@ export class EntityListComponent implements OnInit {
 
   public busy: Subscription;
 
-  public rows: Array<any> = [];
-  public columns: Array<any> = [];
+  public rows: Array < any > = [];
+  public columns: Array < any > = [];
   public page: number = 1;
   public itemsPerPage: number = 10;
   public maxSize: number = 5;
   public numPages: number = 1;
   public length: number = 0;
   public config: any = {
-    paging : true,
-    sorting : {columns : this.columns},
+    paging: true,
+    sorting: { columns: this.columns },
   };
 
   constructor(protected rest: RestService, protected router: Router,
-              protected _state: GlobalState, protected _eRef: ElementRef) {}
+    protected _state: GlobalState, protected _eRef: ElementRef, private dialog: DialogService) {}
 
   ngOnInit() {
     if (this.conf.preInit) {
@@ -44,7 +45,7 @@ export class EntityListComponent implements OnInit {
 
   getData() {
     let offset = this.itemsPerPage * (this.page - 1);
-    let sort: Array<String> = [];
+    let sort: Array < String > = [];
     let options: Object = new Object();
 
     for (let i in this.config.sorting.columns) {
@@ -57,21 +58,21 @@ export class EntityListComponent implements OnInit {
     }
 
     // options = {limit: this.itemsPerPage, offset: offset};
-    options = {limit : 0};
+    options = { limit: 0 };
     if (sort.length > 0) {
       options['sort'] = sort.join(',');
     }
 
     this.busy =
-        this.rest.get(this.conf.resource_name, options).subscribe((res) => {
-          this.length = res.total;
-          this.rows = new EntityUtils().flattenData(res.data);
-        });
+      this.rest.get(this.conf.resource_name, options).subscribe((res) => {
+        this.length = res.total;
+        this.rows = new EntityUtils().flattenData(res.data);
+      });
   }
 
   onChangeTable(
-      config,
-      page: any = {page : this.page, itemsPerPage : this.itemsPerPage}) {
+    config,
+    page: any = { page: this.page, itemsPerPage: this.itemsPerPage }) {
     if (config.filtering) {
       Object.assign(this.config.filtering, config.filtering);
     }
@@ -95,16 +96,15 @@ export class EntityListComponent implements OnInit {
     if (this.conf.getActions) {
       return this.conf.getActions(row);
     } else {
-      return [
-        {
-          id : "edit",
-          label : "Edit",
-          onClick : (row) => { this.doEdit(row.id); },
+      return [{
+          id: "edit",
+          label: "Edit",
+          onClick: (row) => { this.doEdit(row.id); },
         },
         {
-          id : "delete",
-          label : "Delete",
-          onClick : (row) => { this.doDelete(row.id); },
+          id: "delete",
+          label: "Delete",
+          onClick: (row) => { this.doDelete(row.id); },
         },
       ]
     }
@@ -131,11 +131,14 @@ export class EntityListComponent implements OnInit {
 
   doEdit(id) {
     this.router.navigate(
-        new Array('/pages').concat(this.conf.route_edit).concat(id));
+      new Array('/pages').concat(this.conf.route_edit).concat(id)
+    );
   }
 
   doDelete(id) {
-    this.router.navigate(
-        new Array('/pages').concat(this.conf.route_delete).concat(id));
+    // this.router.navigate(
+    //   new Array('/pages').concat(this.conf.route_delete).concat(id)
+    // );
+    this.dialog.confirm("Delete",  "Are you sure you want to delete it?")
   }
 }
