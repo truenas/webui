93b74e07ca4f8fdb4ce6398f82fa595d8125389e
James T. Nixon III <james@ixsystems.com>
2017-08-28 13:54:05 -0700

    in this Theme related update: 	* changes egretThemes to freenasThemes 	* preserved the sanctity of the original egret-themes: blue, indigo, dark-pink, dark-purple 	* added two new themes: freenas-warriors, freenas-sharks...  GO TEAM! 	* made new themes accesible in the theme picker 	* remembered to add the theme files to .angular-cli.json 	* Also removed some bad edits I made last week when trying all this out

--- src/app/services/theme/theme.service.ts
+++ src/app/services/theme/theme.service.ts
@@ -2,28 +2,35 @@ import { Injectable } from '@angular/core';
 import * as domHelper from '../../helpers/dom.helper';
 @Injectable()
 export class ThemeService {
-  egretThemes = [{
+  freenasThemes = [{
     name: 'egret-dark-purple',
     baseColor: '#9c27b0',
     isActive: false
   }, {
     name: 'egret-dark-pink',
     baseColor: '#e91e63',
-    isActive: false
-  },
-  {
-    name: 'freenas-warriors',
-    baseColor: '#fdb927',
+    isActive: true
+  }, {
+    name: 'egret-blue',
+    baseColor: '#247ba0',
     isActive: false
   }, {
     name: 'egret-indigo',
     baseColor: '#3f51b5',
-    isActive: true
+    isActive: false
+  }, {
+    name: 'freenas-warriors',
+    baseColor: '#fdb927',
+    isActive: false
+  }, {
+    name: 'freenas-sharks',
+    baseColor: '#088696',
+    isActive: false
   }];
   constructor() { }
   changeTheme(theme) {
-    domHelper.changeTheme(this.egretThemes, theme.name);
-    this.egretThemes.forEach((t) => {
+    domHelper.changeTheme(this.freenasThemes, theme.name);
+    this.freenasThemes.forEach((t) => {
       t.isActive = false;
       if(t.name === theme.name)
         t.isActive = true;
