02a88a5631cfd06755a0debecc35d5ce7476a72a
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-28 23:42:14 -0700

    remove NgaModule and NT4.

--- src/app/pages/directoryservice/directoryservice.module.ts
+++ src/app/pages/directoryservice/directoryservice.module.ts
@@ -4,7 +4,6 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgUploaderModule} from 'ngx-uploader';
 
 import {SystemGeneralService} from '../../services';
-import {NgaModule} from '../../theme/nga.module';
 import {FnCommonModule} from '../common/common.module';
 import {EntityModule} from '../common/entity/entity.module';
 
@@ -12,14 +11,13 @@ import {ActiveDirectoryComponent} from './activedirectory/';
 import {routing} from './directoryservice.routing';
 import {LdapComponent} from './ldap/';
 import {NISComponent} from './nis/';
-import {NT4Component} from './nt4/nt4.component';
 
 @NgModule({
   imports : [
     EntityModule, CommonModule, FormsModule,
-    ReactiveFormsModule, NgaModule, NgUploaderModule, FnCommonModule,
+    ReactiveFormsModule, NgUploaderModule, FnCommonModule,
     routing
   ],
-  declarations : [ LdapComponent, ActiveDirectoryComponent, NISComponent,NT4Component ],
+  declarations : [ LdapComponent, ActiveDirectoryComponent, NISComponent ],
   providers : [ SystemGeneralService ]
 }) export class DirectoryServiceModule {}
