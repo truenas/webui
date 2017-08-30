d16789950832ab5bcd0b25d0b03dc217ecfa414c
Xin Tan <mactanxin@gmail.com>
2017-08-30 14:39:22 -0700

    UPDATED: fix routing for system section

--- src/app/pages/system/system.routing.ts
+++ src/app/pages/system/system.routing.ts
@@ -2,8 +2,10 @@ import {ModuleWithProviders} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {AdvancedComponent} from './advanced/';
+import {GeneralComponent} from './general/';
 
 export const routes: Routes = [
-  {path : '', component : AdvancedComponent}
+	{path : '', component : GeneralComponent },
+	{path : '', component : AdvancedComponent }
 ];
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
