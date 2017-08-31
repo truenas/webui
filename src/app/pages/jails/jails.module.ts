ee108b60a76c83b5b02435fe9363b0d16108a215
Lola Yang <lola.ly27@gmail.com>
2017-08-30 18:43:08 -0700

    Deleted jails unused routes

--- src/app/pages/jails/jails.module.ts
+++ src/app/pages/jails/jails.module.ts
@@ -7,14 +7,10 @@ import { EntityModule } from '../common/entity/entity.module';
 
 import { routing } from './jails.routing';
 import { JailsConfigurationComponent } from './configuration/';
-// import { JailDeleteComponent } from './jail-delete/';
-
 import { JailListComponent } from './jail-list/';
 import { JailFormComponent } from './jail-form/';
-// import { StorageDeleteComponent } from './storages/storage-delete/';
 import { StorageListComponent } from './storages/storage-list/';
 import { StorageFormComponent } from './storages/storage-form/';
-// import { TemplateDeleteComponent } from './templates/template-delete/';
 import { TemplateListComponent } from './templates/template-list/';
 import { TemplateFormComponent } from './templates/template-form/';
 
@@ -26,13 +22,10 @@ import { TemplateFormComponent } from './templates/template-form/';
     JailsConfigurationComponent,
     JailListComponent,
     JailFormComponent,
-    // JailDeleteComponent,
     StorageListComponent,
     StorageFormComponent,
-    // StorageDeleteComponent,
     TemplateListComponent,
     TemplateFormComponent,
-    // TemplateDeleteComponent,
   ],
   providers : [
     JailService,
--- src/app/pages/jails/jails.routing.ts
+++ src/app/pages/jails/jails.routing.ts
@@ -2,13 +2,10 @@ import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { JailsConfigurationComponent } from './configuration/';
-// import { JailDeleteComponent } from './jail-delete/';
 import { JailListComponent } from './jail-list/';
 import { JailFormComponent } from './jail-form/';
-// import { StorageDeleteComponent } from './storages/storage-delete/';
 import { StorageListComponent } from './storages/storage-list/';
 import { StorageFormComponent } from './storages/storage-form/';
-// import { TemplateDeleteComponent } from './templates/template-delete/';
 import { TemplateListComponent } from './templates/template-list/';
 import { TemplateFormComponent } from './templates/template-form/';
 
@@ -16,16 +13,12 @@ export const routes: Routes = [
   { path : 'jails', component : JailListComponent, pathMatch : 'full' },
   { path : 'add', component : JailFormComponent },
   { path : 'edit/:pk', component : JailFormComponent },
-  // { path : 'delete/:pk', component : JailDeleteComponent },
   { path : 'storage', component : StorageListComponent },
   { path : 'storage/add', component : StorageFormComponent },
   { path : 'storage/edit/:pk', component : StorageFormComponent },
-  // { path : 'storage/delete/:pk', component : StorageDeleteComponent },
   { path : 'configuration', component : JailsConfigurationComponent },
   { path : 'templates', component : TemplateListComponent },
   { path : 'templates/add', component : TemplateFormComponent },
   { path : 'templates/edit/:pk', component : TemplateFormComponent },
-  // { path : 'templates/delete/:pk', component : TemplateDeleteComponent },
-
 ];
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
