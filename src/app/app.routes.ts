bfcba2c40daec7c500acd505592a8e1b54f20117
Xin Tan <mactanxin@gmail.com>
2017-09-05 12:53:32 -0700

    ADDED: everything else you'll ever need for shell component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -145,6 +145,11 @@ export const rootRouterConfig: Routes = [{
         path: 'reportsdashboard',
         loadChildren: './pages/reportsdashboard/reportsdashboard.module#ReportsDashboardModule',
         data: { title: 'reportsdashboard', breadcrumb: 'REPORTING' }
+      },
+      {
+        path : 'shell',
+        loadChildren : './pages/shell/shell.module#ShellModule',
+        data: { title: 'Shell', breadcrumb: 'SHELL'}
       }
     ]
   },
