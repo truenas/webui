587cfed1eccfcea1c35f898ab847a7444f6ff2ec
peter_southwell <pete_south@yahoo.com>
2017-09-08 10:17:58 -0700

    added an interval timer so it polls for new message every 10 seconds.  This can be shut on and off in the class via this. useUpateTimerInterval = true/false defaults now to true.  So... While your logged into the UI and you get an alert.. The UI will reflect the change with out having to reload the page.  Problem is whethor or not we support teh clear button.  Don't see that in the older UI.. So maybe we don't need it.  Seems like we'd need an APi to clear them at the OS.. So that when you reload the applicatoin you don't have to reclear the alerts

--- src/app/components/common/topbar/topbar.component.ts
+++ src/app/components/common/topbar/topbar.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import {Component, OnInit, EventEmitter, Input, Output, OnDestroy} from '@angular/core';
 import * as domHelper from '../../../helpers/dom.helper';
-import { RestService } from '../../../services';
-import { ThemeService } from '../../../services/theme/theme.service';
-import { WebSocketService } from '../../../services/ws.service';
-import { DialogService } from '../../../services/dialog.service';
+import {RestService} from '../../../services';
+import {ThemeService} from '../../../services/theme/theme.service';
+import {WebSocketService} from '../../../services/ws.service';
+import {DialogService} from '../../../services/dialog.service';
 
 @Component({
   selector: 'topbar',
   templateUrl: './topbar.template.html'
 })
-export class TopbarComponent implements OnInit {
+export class TopbarComponent implements OnInit, OnDestroy {
   @Input() sidenav;
   @Input() notificPanel;
-  @Output() onLangChange = new EventEmitter<any>();
-  
+  @Output() onLangChange = new EventEmitter<any>(); 
+
   notificationCount = 0;
-  
+  interval: any;
+  useUpateTimerInterval = true; // Change to false to shut this off.... The thought is.. It updates while
+                                // the user is logged in.
+
   currentLang = 'en';
   availableLangs = [{
     name: 'English',
@@ -25,15 +28,31 @@ export class TopbarComponent implements OnInit {
     code: 'es',
   }]
   freenasThemes;
-  
+
   constructor(private themeService: ThemeService, private rs: RestService, private ws: WebSocketService, private dialogService: DialogService) {}
   ngOnInit() {
     this.freenasThemes = this.themeService.freenasThemes;
-    
-     this.rs.get("system/alert", {}).subscribe((res) => {
-       this.notificationCount = res.data.length;
-     });
+
+    this.rs.get("system/alert", {}).subscribe((res) => {
+      this.notificationCount = res.data.length;
+    });
+     
+    if( this.useUpateTimerInterval === true ) {
+        this.interval = setInterval(() => {
+          this.rs.get("system/alert", {}).subscribe((res) => {
+            this.notificationCount = res.data.length;
+          });
+        }, 10000);
+    }
+
   }
+
+  ngOnDestroy() {
+    if( typeof(this.interval) !== 'undefined') {
+      clearInterval(this.interval);
+    }
+  }
+
   setLang() {
     this.onLangChange.emit(this.currentLang);
   }
@@ -47,9 +66,9 @@ export class TopbarComponent implements OnInit {
     this.sidenav.toggle();
   }
   toggleCollapse() {
-        let appBody = document.body;
-        domHelper.toggleClass(appBody, 'collapsed-menu');
-        domHelper.removeClass(document.getElementsByClassName('has-submenu'), 'open');
+    let appBody = document.body;
+    domHelper.toggleClass(appBody, 'collapsed-menu');
+    domHelper.removeClass(document.getElementsByClassName('has-submenu'), 'open');
   }
   signOut() {
     this.dialogService.confirm("Logout", "You are about to LOGOUT of the FreeNAS WebUI. If unsure, hit 'Cancel', otherwise, press 'OK' to logout.").subscribe((res) => {
@@ -60,7 +79,7 @@ export class TopbarComponent implements OnInit {
   }
   onShutdown() {
     this.dialogService.confirm("Shutdown", "You are about to SHUTDOWN the FreeNAS system. If unsure, hit 'Cancel', otherwise, press 'OK' to shutdown the system.").subscribe((res) => {
-    if (res) {
+      if (res) {
         this.ws.call('system.shutdown', {});
       }
     });
