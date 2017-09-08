80aadc509afe79e7dd8f7e49b63b0c0624a3608c
Lola Yang <lola.ly27@gmail.com>
2017-09-08 14:37:35 -0700

    Jails storage using file explor now

--- src/app/pages/jails/storages/storage-form/storage-form.component.ts
+++ src/app/pages/jails/storages/storage-form/storage-form.component.ts
@@ -7,55 +7,55 @@ import { JailService } from '../../../../services/';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 
 @Component({
-  selector : 'app-storage-add',
-  template : `<entity-form [conf]="this"></entity-form>`
+  selector: 'app-storage-add',
+  template: `<entity-form [conf]="this"></entity-form>`
 })
 export class StorageFormComponent {
 
   protected resource_name: string = 'jails/mountpoints/';
-  protected route_success: string[] = [ 'jails', 'storage' ];
+  protected route_success: string[] = ['jails', 'storage'];
   protected isEntity: boolean = true;
 
-  public fieldConfig: FieldConfig[] = [
-    {
-      type : 'select',
-      name : 'jail',
-      placeholder : 'Jail',
-      options : [],
+  public fieldConfig: FieldConfig[] = [{
+      type: 'select',
+      name: 'jail',
+      placeholder: 'Jail',
+      options: [],
     },
     {
-      type : 'input',
-      name : 'source',
-      placeholder : 'Source',
+      type: 'explorer',
+      initial: '/mnt',
+      name: 'source',
+      placeholder: 'Source',
     },
     {
-      type : 'input',
-      name : 'destination',
-      placeholder : 'Destination',
+      type: 'explorer',
+      initial: '/mnt',
+      name: 'destination',
+      placeholder: 'Destination',
     },
     {
-      type : 'checkbox',
-      name : 'readonly',
-      placeholder : 'Read-Only',
+      type: 'checkbox',
+      name: 'readonly',
+      placeholder: 'Read-Only',
     },
     {
-      type : 'checkbox',
-      name : 'create directory',
-      placeholder : 'Create directory',
+      type: 'checkbox',
+      name: 'create directory',
+      placeholder: 'Create directory',
     }
   ];
 
   private jail: any;
 
   constructor(protected router: Router, protected aroute: ActivatedRoute,
-              protected jailService: JailService) {}
+    protected jailService: JailService) {}
 
   afterInit(entityForm: any) {
     this.jailService.listJails().subscribe((res) => {
-      this.jail = _.find(this.fieldConfig, {'name' : 'jail'});
+      this.jail = _.find(this.fieldConfig, { 'name': 'jail' });
       res.data.forEach((item) => {
-        this.jail.options.push(
-            {label : item.jail_host, value : item.jail_host});
+        this.jail.options.push({ label: item.jail_host, value: item.jail_host });
       });
     });
 
