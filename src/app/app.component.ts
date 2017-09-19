c45a1889348fb9e49f92455d18a1178b5f2c9fe9
Xin Tan <mactanxin@gmail.com>
2017-09-19 11:12:26 -0700

    ADDED: tour your index page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 
 import { RoutePartsService } from "./services/route-parts/route-parts.service";
+import { MdSnackBar } from '@angular/material';
+import * as hopscotch from 'hopscotch';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +18,68 @@ export class AppComponent implements OnInit {
   constructor(public title: Title, 
     private router: Router, 
     private activeRoute: ActivatedRoute,
-    private routePartsService: RoutePartsService) { }
+    private routePartsService: RoutePartsService,
+    public snackBar: MdSnackBar) { }
 
   ngOnInit() {
     this.changePageTitle();
+    setTimeout(() => {
+      hopscotch.startTour(this.tourSteps());
+    }, 2000);
   }
+
+  tourSteps(): any {
+    let self = this;
+    return {
+      id: 'hello-egret',
+      showPrevButton: true,
+      onEnd: function() {
+        self.snackBar.open('Awesome! Now let\'s explore Egret\'s cool features.', 'close', { duration: 5000 });
+      },
+      onClose: function() {
+        self.snackBar.open('You just closed User Tour!', 'close', { duration: 3000 });
+      },
+      steps: [
+        {
+          title: 'Sidebar Controls',
+          content: 'Control left sidebar\'s display style.',
+          target: 'sidenavToggle', // Element ID
+          placement: 'bottom',
+          xOffset: 10
+        },
+        {
+          title: 'Available Themes',
+          content: 'Choose a color scheme.',
+          target: 'schemeToggle', // Element ID
+          placement: 'left',
+          xOffset: 20
+        },
+        {
+          title: 'Language',
+          content: 'Choose your language.',
+          target: document.querySelector('.topbar .mat-select'),
+          placement: 'left',
+          xOffset: 10,
+          yOffset: -5
+        },
+        {
+          title: 'Users & Groups',
+          content: 'Setup Users and Groups.',
+          target: document.querySelector('.sidebar-list-item .mat-list-item-ripple'),
+          placement: 'right',
+          yOffset: 50
+        },
+        {
+          title: 'Volumes & Snapshots',
+          content: 'Create a new Volume.',
+          target: document.querySelector('.sidebar-list-item .mat-list-item-ripple'),
+          placement: 'right',
+          yOffset: 200
+        }
+      ]
+    }
+  }
+
   changePageTitle() {
     this.router.events.filter(event => event instanceof NavigationEnd).subscribe((routeChange) => {
       const routeParts = this.routePartsService.generateRouteParts(this.activeRoute.snapshot);
