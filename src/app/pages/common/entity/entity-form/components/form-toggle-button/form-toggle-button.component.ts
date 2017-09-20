c93fe53713018f75bc6285632fbd240ecf8faa64
Lola Yang <lola.ly27@gmail.com>
2017-09-20 16:23:46 -0700

    Make md-button-toggle-group get selected values

--- src/app/pages/common/entity/entity-form/components/form-toggle-button/form-toggle-button.component.ts
+++ src/app/pages/common/entity/entity-form/components/form-toggle-button/form-toggle-button.component.ts
@@ -4,13 +4,28 @@ import { FormGroup } from '@angular/forms';
 import { FieldConfig } from '../../models/field-config.interface';
 import { Field } from '../../models/field.interface';
 import { TooltipComponent } from '../tooltip/tooltip.component';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'form-toggle-button',
   templateUrl: './form-toggle-button.component.html',
+  styleUrls: ['./form-toggle-button.component.css']
 })
 export class FormToggleButtonComponent implements Field {
   config: FieldConfig;
   group: FormGroup;
   fieldShow: string;
+  public groupValue: Array < any >= [];
+
+  check(item) {
+    let target = _.findIndex(this.groupValue, _.unary(_.partialRight(_.includes, item)));
+    if (target > -1) {
+      this.groupValue.splice(target, 1);
+    } else {
+      this.groupValue.push(item);
+    }
+
+    let control = this.group.controls[this.config.name];
+    control.setValue(this.groupValue);
+  }
 }
