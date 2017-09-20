e017be76f0e6986bd0edb6b832e1ab20291e4510
peter_southwell <pete_south@yahoo.com>
2017-09-20 13:11:14 -0700

    this is how Ill have the chart looking.. now to propogate

--- src/app/pages/reportsdashboard/reportsdashboard.component.ts
+++ src/app/pages/reportsdashboard/reportsdashboard.component.ts
@@ -53,14 +53,11 @@ export class ReportsDashboardComponent implements OnInit, HandleChartConfigDataF
         columns: [
           ['xValues', '01:10', '02:10', '03:10', '04:10', '05:10', '06:10' ],
           ['data1', 30, 200, 100, 400, 150, 250],
-          ['data2', 50, 20, 10, 40, 15, 25]
+          ['data2', 50, 20, 10, 40, 15, 25],
+          ['data3', 20, 80, 60, 20, 15, 45]
         ],
         x: 'xValues',
         xFormat: '%H:%M',
-        axes: {
-          data1: 'y',
-          data2: 'y2'
-        },
         type: 'area-spline'
       },
       axis: {
