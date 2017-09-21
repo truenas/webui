acc73b80f14000fde463e481bacd306df97209af
Lola Yang <lola.ly27@gmail.com>
2017-09-20 18:57:43 -0700

    Make dynamic filed form-slider display value

--- src/app/pages/common/entity/entity-form/components/form-slider/form-slider.component.ts
+++ src/app/pages/common/entity/entity-form/components/form-slider/form-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { FieldConfig } from '../../models/field-config.interface';
@@ -6,11 +6,21 @@ import { Field } from '../../models/field.interface';
 import { TooltipComponent } from '../tooltip/tooltip.component';
 
 @Component({
-  selector: 'form-input',
+  selector: 'form-slider',
   templateUrl: './form-slider.component.html',
+  styleUrls: ['./form-slider.component.css'],
 })
-export class FormSliderComponent implements Field {
+export class FormSliderComponent implements Field, OnInit {
   config: FieldConfig;
   group: FormGroup;
   fieldShow: string;
+  public value: any;
+
+  ngOnInit() {
+    this.value = this.config.min;
+  }
+
+  updateValue($event) {
+    this.value = $event.value;
+  }
 }
