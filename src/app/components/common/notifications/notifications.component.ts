0448d3c57f3ea269608653361c8b71d8e3036fe2
peter_southwell <pete_south@yahoo.com>
2017-10-06 10:05:06 -0700

    Made it so when you click the warning it re-opens or dismisses it.... So.. It's feature per feature what the old one did.  In addition I have the Clear All/Re-Open All thing going on for doing so in mass.  I removed the Routerlink.. Which really was a cool idea.  But there isn't a way YET to map the alert messages to particular screens in the app.. So I left the code in the component.. but took it out of the view... It's a cool idea though for sure that the alerts would based on type invoke deep links in the app

--- src/app/components/common/notifications/notifications.component.ts
+++ src/app/components/common/notifications/notifications.component.ts
@@ -1,7 +1,7 @@
-import {RestService} from '../../../services';
-import {Component, OnInit, ViewChild, Input} from '@angular/core';
-import {MdSidenav} from '@angular/material';
-import {Router, NavigationEnd} from '@angular/router';
+import { RestService } from '../../../services';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { MdSidenav } from '@angular/material';
+import { Router, NavigationEnd } from '@angular/router';
 import { TopbarComponent } from '../topbar/topbar.component';
 import { NotificationsService, NotificationAlert } from 'app/services/notifications.service';
 
@@ -17,11 +17,11 @@ export class NotificationsComponent implements OnInit {
 
   notifications: Array<NotificationAlert> = [];
   dismissedNotifications: Array<NotificationAlert> = []
-  
+
   showMe: Boolean = false;
 
-  constructor(private notificationsService: NotificationsService, private router: Router) {}
-  
+  constructor(private notificationsService: NotificationsService, private router: Router) { }
+
   ngOnInit() {
     this.router.events.subscribe((routeChange) => {
       if (routeChange instanceof NavigationEnd) {
@@ -29,15 +29,15 @@ export class NotificationsComponent implements OnInit {
       }
     });
 
-    this.notificationsService.getNotifications(true).subscribe((notifications)=>{
-        notifications.forEach((notification: NotificationAlert)=>{
-            if( notification.dismissed === false ) {
-              this.notifications.push( notification );
-            } else {
-              this.dismissedNotifications.push( notification );
-            }
-        });
-        this.showMe = true;
+    this.notificationsService.getNotifications(true).subscribe((notifications) => {
+      notifications.forEach((notification: NotificationAlert) => {
+        if (notification.dismissed === false) {
+          this.notifications.push(notification);
+        } else {
+          this.dismissedNotifications.push(notification);
+        }
+      });
+      this.showMe = true;
     });
   }
 
@@ -46,10 +46,10 @@ export class NotificationsComponent implements OnInit {
     e.preventDefault();
 
     this.notificationsService.clearNotifications(this.notifications, true);
-    
-    this.notifications.forEach((notification: NotificationAlert)=>{
+
+    this.notifications.forEach((notification: NotificationAlert) => {
       notification.dismissed = true;
-      this.dismissedNotifications.push( notification );
+      this.dismissedNotifications.push(notification);
     });
 
     this.notifications = [];
@@ -59,12 +59,42 @@ export class NotificationsComponent implements OnInit {
     e.preventDefault();
 
     this.notificationsService.clearNotifications(this.dismissedNotifications, false);
-    
-    this.dismissedNotifications.forEach((notification: NotificationAlert)=>{
+
+    this.dismissedNotifications.forEach((notification: NotificationAlert) => {
       notification.dismissed = false;
-      this.notifications.push( notification );
+      this.notifications.push(notification);
     });
 
     this.dismissedNotifications = [];
+  }
+
+  turnMeOff(notification: NotificationAlert, e) {
+    e.preventDefault();
+    this.notificationsService.clearNotifications([notification], true);
+
+    for (let i = this.notifications.length - 1; i >= 0; i--) {
+      if (this.notifications[i].id === notification.id) {
+        this.notifications.splice(i, 1);
+        break;
+      }
+    }
+
+    notification.dismissed = true;
+    this.dismissedNotifications.push(notification);
+  }
+
+  turnMeOn(notification: NotificationAlert, e) {
+    e.preventDefault();
+    this.notificationsService.clearNotifications([notification], true);
+
+    for (let i = this.dismissedNotifications.length - 1; i >= 0; i--) {
+      if (this.dismissedNotifications[i].id === notification.id) {
+        this.dismissedNotifications.splice(i, 1);
+        break;
+      }
+    }
+
+    notification.dismissed = false;
+    this.notifications.push(notification);
   }
 }
