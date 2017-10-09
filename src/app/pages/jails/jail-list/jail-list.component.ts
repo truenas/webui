c977619598cd9288223ad22ada7d0dd8816eb67b
Lola Yang <lola.ly27@gmail.com>
2017-10-09 11:38:53 -0700

    Jail using iocage websocket api to start/stop/delete a jail

--- src/app/pages/jails/jail-list/jail-list.component.ts
+++ src/app/pages/jails/jail-list/jail-list.component.ts
@@ -1,34 +1,32 @@
-import { RestService } from '../../../services';
+import { RestService, WebSocketService } from '../../../services';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector : 'app-jail-list',
-  template : `<entity-table [conf]="this"></entity-table>`
+  selector: 'app-jail-list',
+  template: `<entity-table [conf]="this"></entity-table>`
 })
 export class JailListComponent {
 
-  protected resource_name = 'jails/jails';
   protected queryCall = 'jail.query';
-  protected route_add: string[] = [ 'jails', 'add' ];
+  protected route_add: string[] = ['jails', 'add'];
   protected route_add_tooltip = "Add Jail";
+  protected wsDelete = 'jail.do_delete';
   protected entityList: any;
 
- 
-
-  public columns: Array<any> = [
-    {name : 'Jail', prop : 'host_hostname'},
-    {name : 'Status', prop : 'jail_status'},
-    {name : 'Release', prop : 'release'},
-    {name : 'IPv4 Address', prop : 'ip4_addr'},
+  public columns: Array < any > = [
+    { name: 'Jail', prop: 'host_hostname' },
+    { name: 'Status', prop: 'jail_status' },
+    { name: 'Release', prop: 'release' },
+    { name: 'IPv4 Address', prop: 'ip4_addr' },
   ];
   public config: any = {
-    paging : true,
-    sorting : {columns : this.columns},
+    paging: true,
+    sorting: { columns: this.columns },
   };
 
-  constructor(protected router: Router, protected rest: RestService) {}
-  
+  constructor(protected router: Router, protected rest: RestService, protected ws: WebSocketService) {}
+
   afterInit(entityList: any) { this.entityList = entityList; }
 
   isActionVisible(actionId: string, row: any) {
@@ -43,57 +41,56 @@ export class JailListComponent {
   }
 
   getActions(parentRow) {
-    return [
-      {
-        id : "edit",
-        label : "Edit",
-        onClick : (row) => {
+    return [{
+        id: "edit",
+        label: "Edit",
+        onClick: (row) => {
           console.log(row.host_hostuuid);
           this.router.navigate(
-              new Array('').concat([ "jails", "edit", row.host_hostuuid ]));
+            new Array('').concat(["jails", "edit", row.host_hostuuid]));
         }
       },
       {
-        id : "start",
-        label : "Start",
-        onClick : (row) => {
+        id: "start",
+        label: "Start",
+        onClick: (row) => {
           this.entityList.busy =
-              this.rest.post(this.resource_name + '/' + row.id + '/start/', {})
-                  .subscribe((res) => { row.jail_status = 'Running'; },
-                             (res) => { console.log(res); });
+            this.ws.call('jail.start', [row.host_hostuuid]).subscribe(
+              (res) => { row.jail_status = 'Up'; },
+              (res) => { console.log(res); });
         }
       },
       {
-        id : "stop",
-        label : "Stop",
-        onClick : (row) => {
+        id: "stop",
+        label: "Stop",
+        onClick: (row) => {
           this.entityList.busy =
-              this.rest.post(this.resource_name + '/' + row.id + '/stop/', {})
-                  .subscribe((res) => { row.jail_status = 'Stopped'; },
-                             (res) => { console.log(res); });
+            this.ws.call('jail.stop', [row.host_hostuuid]).subscribe(
+              (res) => { row.jail_status = 'Down'; },
+              (res) => { console.log(res); });
         }
       },
-      {
-        id : "restart",
-        label : "Restart",
-        onClick : (row) => {
-          this.entityList.busy =
-              this.rest
-                  .post(this.resource_name + '/' + row.id + '/restart/', {})
-                  .subscribe((res) => { row.jail_status = 'Running'; },
-                             (res) => { console.log(res); });
-        }
-      },
+      // {
+      //   id: "restart",
+      //   label: "Restart",
+      //   onClick: (row) => {
+      //     this.entityList.busy =
+      //       this.rest
+      //       .post(this.resource_name + '/' + row.id + '/restart/', {})
+      //       .subscribe((res) => { row.jail_status = 'Running'; },
+      //         (res) => { console.log(res); });
+      //   }
+      // },
       // {
       //   id : "shell",
       //   label : "Shell",
       //   onClick : (row) => {}
       // },
       {
-        id : "delete",
-        label : "Delete",
-        onClick : (row) => {
-          this.entityList.doDelete(row.id);
+        id: "delete",
+        label: "Delete",
+        onClick: (row) => {
+          this.entityList.doDelete(row.host_hostuuid);
         }
       }
     ]
