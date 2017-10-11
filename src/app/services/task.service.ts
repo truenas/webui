7d072b64fe50310e801760b0c1b11f898ae02fb5
Lola Yang <lola.ly27@gmail.com>
2017-10-10 17:18:54 -0700

    Enable to get rsync choices from task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -137,4 +137,12 @@ export class TaskService {
   getVolumeList() {
     return this.rest.get(this.volumeResource, {});
   }
+
+  getRsyncModeChoices() {
+    return this.ws.call('notifier.choices', ['RSYNC_MODE_CHOICES', [true, false]]);
+  };
+
+  getRsyncDirectionChoices() {
+    return this.ws.call('notifier.choices', ['RSYNC_DIRECTION', [true, false]]);
+  };
 }
