a165d9339aa2a7a9dae5ecd51758f1dd1579d683
Lola Yang <lola.ly27@gmail.com>
2017-10-11 16:06:44 -0700

    Now storage service can list all disks

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,11 +1,18 @@
-import {Injectable} from '@angular/core';
-import {WebSocketService} from './ws.service';
+import { Injectable } from '@angular/core';
+import { WebSocketService } from './ws.service';
+import { RestService } from './rest.service';
 
 @Injectable()
 export class StorageService {
-    constructor(protected ws: WebSocketService) {}
+  protected diskResource: string = 'storage/disk';
 
-    filesystemStat(path: string) {
-        return this.ws.call('filesystem.stat', [path])
-    }
+  constructor(protected ws: WebSocketService, protected rest: RestService) {}
+
+  filesystemStat(path: string) {
+    return this.ws.call('filesystem.stat', [path])
+  }
+
+  listDisks() {
+    return this.rest.get(this.diskResource, { limit: 50 });
+  }
 }
