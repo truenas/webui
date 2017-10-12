25309f005d446b3cd35a041569eb7354d7d36341
peter_southwell <pete_south@yahoo.com>
2017-10-12 10:47:01 -0700

    STOP DELETING DIRECTORIES YOU STUPID APP VISUAL STUDIO HATING YOU RIGHT NOW

---
+++ src/app/pages/general/config-save/config-save.component.ts
@@ -0,0 +1,40 @@
+import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormGroup, Validator } from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import * as _ from 'lodash';
+import { Subscription } from 'rxjs';
+
+import { WebSocketService } from '../../../../services/';
+import { MdSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'general-config-save',
+  templateUrl: './config-save.component.html'
+})
+
+export class ConfigSaveComponent {
+
+  public sub: Subscription;
+  public secretseed: boolean = false;
+
+  constructor(protected ws: WebSocketService, protected router: Router, public snackBar: MdSnackBar) {}
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action , {
+    	duration: 5000
+    });
+  }
+
+  doSubmit() {
+    this.sub = this.ws.call('core.download', ['config.save', [{ 'secretseed': this.secretseed }], 'freenas.db'])
+      .subscribe(
+        (res) => {
+          this.openSnackBar("Redirecting to download. Make sure you have pop up enabled in your browser.", "Success");
+          window.open(res[1]);
+        },
+        (err) => {
+          this.openSnackBar("Please check your network connection", "Failed");
+        }
+      );
+  }
+}
---
+++ src/app/pages/general/index.ts
@@ -0,0 +1,4 @@
+// export {ConfigResetComponent} from './config-reset/';
+// export {ConfigSaveComponent} from './config-save/';
+// export {ConfigUploadComponent} from './config-upload/';
+//export {GeneralComponent} from './general.component';
