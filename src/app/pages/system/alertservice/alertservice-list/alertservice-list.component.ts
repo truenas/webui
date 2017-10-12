4fdae4ab7e9752f48761d2d1b0fecf1c52457856
peter_southwell <pete_south@yahoo.com>
2017-10-12 00:31:09 -0700

    This handles the Edit for different types.... And fixed a wee little buggie

--- src/app/pages/system/alertservice/alertservice-list/alertservice-list.component.ts
+++ src/app/pages/system/alertservice/alertservice-list/alertservice-list.component.ts
@@ -2,72 +2,76 @@ import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as _ from 'lodash';
 import { Subscription } from 'rxjs';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import { RestService, WebSocketService } from '../../../../services/';
 
 
 @Component({
-  selector : 'app-alertservice-list',
+  selector: 'app-alertservice-list',
   template: `<entity-table [conf]="this"></entity-table>`
 })
 export class AlertServiceListComponent {
   protected resource_name = 'system/consulalerts';
-  protected route_success: string[] = [ 'system', 'alertservice' ];
+  protected route_success: string[] = ['system', 'alertservice'];
   protected entityList: any;
-  
+
   public busy: Subscription;
   public sub: Subscription;
 
   public columns: Array<any> = [
-    {name : 'Service Name', prop : 'consulalert_type'},
-    {name : 'Enabled', prop : 'enabled'},
+    { name: 'Service Name', prop: 'consulalert_type' },
+    { name: 'Enabled', prop: 'enabled' },
   ];
-    public config: any = {
-      paging : true,
-      sorting : {columns : this.columns},
-    };
+  public config: any = {
+    paging: true,
+    sorting: { columns: this.columns },
+  };
 
   constructor(protected router: Router, protected aroute: ActivatedRoute,
-     protected rest: RestService, protected ws: WebSocketService,
-    protected _injector: Injector, protected _appRef: ApplicationRef) {}
+    protected rest: RestService, protected ws: WebSocketService,
+    protected _injector: Injector, protected _appRef: ApplicationRef) { }
 
 
-  afterInit(entityList: any) { 
-    this.entityList = entityList; 
+  afterInit(entityList: any) {
+    this.entityList = entityList;
   }
-    
+
   preInit(entityList: any) {
-    this.sub = this.aroute.params.subscribe(params => {});
+    this.sub = this.aroute.params.subscribe(params => { });
   }
-  
+
   getAddActions() {
     return [{
-      label: "AWS-SN",
+      label: "AWS-SNS",
       icon: "card_membership",
       onClick: () => {
         this.router.navigate(
           new Array('').concat(["system", "alertservice", "add-aws"]));
       }
-    }]; 
+    }];
   }
 
   getActions(parentRow) {
     return [{
-        id: "edit",
-        label: "Edit",
-        onClick: (row) => {
-          this.router.navigate(
-            new Array('').concat([ 'system', 'alertservice', 'edit-aws', row.id ]));
-        }
-      },
-      {
-        id: "delete",
-        label: "Delete",
-        onClick: (row) => {
-          this.entityList.doDelete(row.id);
+      id: "edit",
+      label: "Edit",
+      onClick: (row) => {
+
+        if (row.consulalert_type === "AWSSNS") {
+          alert("AWSSNS HERE WE GO BIOTCH");
+          const urlNav = new Array<String>('').concat(['system', 'alertservice', 'edit-aws', row.id]);
+          this.router.navigate(urlNav);
         }
       }
+    },
+    {
+      id: "delete",
+      label: "Delete",
+      onClick: (row) => {
+        this.entityList.doDelete(row.id);
+      }
+    }
     ]
   }
- 
+
 }
