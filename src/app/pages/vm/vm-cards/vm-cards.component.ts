bf7a6733bb69e736776ac37ed71464c41276e33e
Damian Szidiropulosz <damianos@knoxbug.org>
2017-10-21 16:51:18 -0400

    vm-cards now have working cancel and delete (close) buttons.

--- src/app/pages/vm/vm-cards/vm-cards.component.ts
+++ src/app/pages/vm/vm-cards/vm-cards.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { EntityModule } from '../../common/entity/entity.module';
-import { RestService } from '../../../services/';
+import { WebSocketService, RestService } from '../../../services/';
+import { DialogService } from '../../../services/dialog.service';
+import { AppLoaderService } from '../../../services/app-loader/app-loader.service';
 
 
 interface VmProfile {
@@ -27,41 +29,110 @@ export class VmCardsComponent implements OnInit {
   @Input() cards = [];
   public lazyLoaded = false;
   public tpl = "edit";
+  protected loaderOpen: boolean = false;
 
-  constructor(protected rest: RestService){}
+  constructor(protected ws: WebSocketService,protected rest: RestService, private dialog: DialogService,protected loader: AppLoaderService){}
 
   ngOnInit() {
-    this.getUserList();
+    this.getVmList();
   }
 
-  getUserList() {
+  parseResponse(data){
+    var card: VmProfile = { 
+      name:data.name,
+      id:data.id,
+      description:data.description,
+      info:data.info,
+      bootloader:data.bootloader,
+      state:data.state,
+      autostart:data.autostart,
+      vcpus:data.vcpus,
+      memory:data.memory
+    }   
+    return card;
+  }
+
+  getVmList() {
     this.rest.get('vm/vm', {}).subscribe((res) => {
-      console.log(res);
+      //console.log(res);
       for(var i = 0; i < res.data.length; i++){
-	var card: VmProfile = {
-	  name:res.data[i].name,
-	  id:res.data[i].id,
-	  description:res.data[i].description,
-	  info:res.data[i].info,
-	  bootloader:res.data[i].bootloader,
-	  state:res.data[i].state,
-	  autostart:res.data[i].autostart,
-	  vcpus:res.data[i].vcpus,
-	  memory:res.data[i].memory
-	}
-	//if(res.data[i].bsdusr_full_name == ""){
-	//card.fullname = res.data[i].bsdusr_username;
-	//}
+	var card = this.parseResponse(res.data[i]);
 	//console.log(card);
 	this.cards.push(card);
+      }   
+    })  
+  }
+
+  getVm(index) {
+    this.rest.get('vm/vm/'+this.cards[index].id, {}).subscribe((res) => {
+      //console.log(res);
+      var card = this.parseResponse(res.data);
+      this.cards[index] = card;
+      //this.cards.push(card);
+    })  
+  }
+
+  refreshVM(index){
+    this.getVm(index);
+  }
+
+/*
+  addVM(){
+    var card: VmProfile = { 
+      name:"",
+      id:"",
+      description:"",
+      info:"",
+      bootloader:"",
+      state:"",
+      autostart:"",
+      vcpus:"",
+      memory:""
+    }   
+    this.cards.push(card);
+  }
+*/
+
+  deleteVM(index) {
+    this.dialog.confirm("Delete", "Are you sure you want to delete it?").subscribe((res) => {
+      if (res) {
+	this.loader.open();
+	this.loaderOpen = true;
+	let data = {};
+	this.rest.delete( 'vm/vm/' + this.cards[index].id, {}).subscribe(
+	  (res) => {
+	    this.cards.splice(index,1);
+	    this.loader.close();
+	    //this.getVmList();
+	  }/*,
+	  (res) => { 
+	    new EntityUtils().handleError(this, res);
+	    this.loader.close(); 
+	  }*/
+	);        
       }
     })
   }
 
-  toggleForm(state, card, template){
+  toggleForm(flipState, card, template){
     // load #cardBack template with code here
     this.tpl = template;
-    card.isFlipped = state;
+    card.isFlipped = flipState;
     this.lazyLoaded = !this.lazyLoaded;
+  }
+
+  toggleVmState(index){
+    let vm = this.cards[index];
+    let action: string;
+    let rpc: string;
+    if (vm.state != 'RUNNING') {
+      rpc = 'vm.start';
+    } else {
+      rpc = 'vm.stop';
+    }
+    this.ws.call(rpc, [ vm.id ]).subscribe((res) => {
+      console.log([vm.id]);
+      this.refreshVM(index);
+    });
   }
 }
