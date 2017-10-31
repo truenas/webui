1a8d6d9effecf30f67352a2db4d28b81bfddcab6
peter_southwell <pete_south@yahoo.com>
2017-10-31 09:16:39 -0700

    new service for replication.public_key and replication.keyscan websocket apis

---
+++ src/app/pages/task-calendar/replication/replication.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+
+import {WebSocketService} from '../../../services';
+import { Observable } from 'rxjs/Observable';
+
+
+
+@Injectable()
+export class ReplicationService {
+ 
+  constructor(private _ws: WebSocketService) {}
+
+  public getSSHKeyscan(title: string, port: number): Observable<any> {
+    return this._ws.call('replication.ssh_keyscan', [[title, port], {}]);
+  }
+
+
+  public getSSHPublicKeyscan(): Observable<any> {
+    return this._ws.call('replication.public_key', [[], {}]);
+  }
+
+
+}
