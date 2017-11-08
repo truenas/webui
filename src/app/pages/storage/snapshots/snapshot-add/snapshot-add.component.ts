349c67bbf341b50704f31b9a906117d1ea000df4
peter_southwell <pete_south@yahoo.com>
2017-11-08 12:31:14 -0800

    This gets snap shots creating/working... Looks like next the clone method isn't working.. Delete is.. So clone is the finishing next checkin

--- src/app/pages/storage/snapshots/snapshot-add/snapshot-add.component.ts
+++ src/app/pages/storage/snapshots/snapshot-add/snapshot-add.component.ts
@@ -7,81 +7,74 @@ import {
   ViewChildren,
   AfterViewInit
 } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import * as moment from 'moment';
 import * as _ from 'lodash';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 
-import {RestService, WebSocketService} from '../../../../services/';
-import {EntityUtils} from '../../../common/entity/utils';
+import { RestService, WebSocketService } from '../../../../services/';
+import { EntityUtils } from '../../../common/entity/utils';
 
 @Component({
-   selector : 'app-snapshot-add',
-   templateUrl : './snapshot-add.component.html'})
+  selector: 'app-snapshot-add',
+  templateUrl: './snapshot-add.component.html'
+})
 
 export class SnapshotAddComponent implements AfterViewInit {
-  
+
 
   protected resource_name: string = 'storage/snapshot';
-  protected route_success: string[] = [ 'storage', 'snapshots' ];
+  protected route_success: string[] = ['storage', 'snapshots'];
   protected isEntity = true;
   protected isNew = true;
   protected fieldConfig: FieldConfig[] = [];
   public initialized = true;
 
   constructor(protected router: Router, protected route: ActivatedRoute,
-              protected rest: RestService, protected ws: WebSocketService,
-              protected _injector: Injector, protected _appRef: ApplicationRef) {
+    protected rest: RestService, protected ws: WebSocketService,
+    protected _injector: Injector, protected _appRef: ApplicationRef) {
 
-                this.fieldConfig = [
-                  {
-                    type: 'select',
-                    name: 'filesystem',
-                    placeholder: 'Volume/Dataset',
-                    options: []
-                  },
-                  {
-                    type: 'input', 
-                    name: 'id', 
-                    placeholder: 'Snapshot Name'
-                  },
-                  {
-                    type: 'input', 
-                    name: 'fullname', 
-                    placeholder: 'Snapshot Full Name'
-                  },
-                  {
-                    type: 'checkbox',
-                    name : 'recursive',
-                    placeholder: 'Recursive'
-                  },
-                ];
+    this.fieldConfig = [
+      {
+        type: 'select',
+        name: 'dataset',
+        placeholder: 'Volume/Dataset',
+        options: []
+      },
+      {
+        type: 'input',
+        name: 'name',
+        placeholder: 'Name',
+        options: []
+      },
+      {
+        type: 'checkbox',
+        name: 'recursive',
+        placeholder: 'Recursive'
+      }
+    ];
 
 
   }
 
   ngAfterViewInit(): void {
 
-    
-  this.rest.get("storage/volume/", {}).subscribe((res)=>{
-      let rows = new EntityUtils().flattenData(res.data);
 
-      rows.forEach((dataItem)=>{
-         let calculatedPath = dataItem.name;
+    this.rest.get("storage/volume/", {}).subscribe((res) => {
+      const rows = new EntityUtils().flattenData(res.data);
 
-         if( typeof(dataItem.path) !== "undefined" && dataItem.path.length > 0 ) {
-            calculatedPath += "/" + dataItem.path;
-         }
+      rows.forEach((dataItem) => {
+        if (typeof (dataItem.path) !== 'undefined' && dataItem.path.length > 0) {
+          this.fieldConfig[0].options.push({
+            label: dataItem.path,
+            value: dataItem.path
+          });
+        }
+      })
 
-         this.fieldConfig[0].options.push({
-             label:  calculatedPath,
-             value:  calculatedPath
-         });
-       })
-
-       this.initialized = true;
-   });
+      this.initialized = true;
+    });
   }
 }
