d4d55810827d0d961f2deff0240aed18b94a5640
Mrt134 <timmoore88@gmail.com>
2017-11-16 09:31:05 -0500

    Update tooltip files and set up example: Remove tooltip messages from tooltips.ts in favor of adding messages to individual files.
    Review and update example tooltips in network config file to use html tags and escape characters for pep8 formatting.
    Build testing: built against latest nightly and found no build errors or render issues. and found no build errors or render issues.

--- src/app/pages/common/tooltips.ts
+++ src/app/pages/common/tooltips.ts
@@ -1,19 +1,3 @@
 import {Tooltip} from './tooltip';
 
-export const TOOLTIPS: Tooltip[] = [
-  {
-    id : 'gc_netwait_enabled',
-    body :
-        'If enabled, delays the start of network-reliant services until interface is up and ICMP packets to a destination defined in netwait ip list are flowing. Link state is examined first, followed by "pinging" an IP address to verify network usability. If no destination can be reached or timeouts are exceeded, network services are started anyway with no guarantee that the network is usable.'
-  },
-  {
-    id : 'gc_netwait_ip',
-    body :
-        'Space-delimited list of IP addresses to ping(8). If multiple IP addresses are specified, each will be tried until one is successful or the list is exhausted. If it is empty the default gateway will be used.'
-  },
-  {
-    id : 'gc_hosts',
-    body :
-        'This field is appended to /etc/hosts which contains information regarding known hosts on the network. hosts(5)'
-  }
-]
+export const TOOLTIPS: Tooltip[] = []
