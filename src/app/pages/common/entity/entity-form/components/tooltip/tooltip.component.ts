ed7fac7743689bc9b776089047914e6ed44112d1
Chentao Wang <allwithyou999@gmail.com>
2017-11-20 04:21:08 -0800

    Fixed Mobile responsive UI, adding an animation, #26723

--- src/app/pages/common/entity/entity-form/components/tooltip/tooltip.component.ts
+++ src/app/pages/common/entity/entity-form/components/tooltip/tooltip.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { TooltipPosition } from '@angular/material';
 
 @Component({
@@ -8,11 +8,29 @@ import { TooltipPosition } from '@angular/material';
 })
 export class TooltipComponent {
   @Input('message') message: string;
+  @ViewChild('tooltip') private tooltip: ElementRef;
 
   public isShowTooltip: Boolean;
+  public tooltipMsgStyle: any;
 
   showTooltip($event) {
     this.isShowTooltip = $event;
+
+    let screenW = document.body.clientWidth;
+    let posX = this.tooltip.nativeElement.getBoundingClientRect().left;
+
+    if((screenW - posX) > 420) {
+      this.tooltipMsgStyle = {'left' : '0px'};
+    }
+    else if(posX > 420) {
+      this.tooltipMsgStyle = {'right' : '0px'};
+    }
+    else {
+      let diffX = 'calc( -45vw - ' + (posX - screenW/2) + 'px )';
+      this.tooltipMsgStyle = {'left' : diffX};
+
+      console.log("------", screenW, diffX);
+    }    
   }
 
 }
