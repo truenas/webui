debcf1dda2c1640f5b27354201281386e421f367
Lola Yang <lola.ly27@gmail.com>
2017-11-20 17:37:07 -0800

    Made left menu same as old ui

--- src/app/services/navigation/navigation.service.ts
+++ src/app/services/navigation/navigation.service.ts
@@ -2,25 +2,24 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 interface IMenuItem {
-  type: string,       // Possible values: link/dropDown/icon/separator/extLink
-  name?: string,      // Used as display text for item and title for separator type
-  state?: string,     // Router state
-  icon?: string,      // Item icon name
-  tooltip?: string,   // Tooltip text 
-  disabled?: boolean, // If true, item will not be appeared in sidenav.
-  sub?: IChildItem[]  // Dropdown items
+  type: string, // Possible values: link/dropDown/icon/separator/extLink
+  name ? : string, // Used as display text for item and title for separator type
+  state ? : string, // Router state
+  icon ? : string, // Item icon name
+  tooltip ? : string, // Tooltip text 
+  disabled ? : boolean, // If true, item will not be appeared in sidenav.
+  sub ? : IChildItem[] // Dropdown items
 }
 interface IChildItem {
-  name: string,       // Display text
-  state: string       // Router state
+  name: string, // Display text
+  state: string // Router state
 }
 
 @Injectable()
 export class NavigationService {
   constructor() {}
 
-  defaultMenu:IMenuItem[] = [
-    {
+  defaultMenu: IMenuItem[] = [{
       name: 'Dashboard',
       type: 'link',
       tooltip: 'Dashboard',
@@ -34,8 +33,8 @@ export class NavigationService {
       icon: 'people',
       state: 'account',
       sub: [
-        {name: 'Users', state: 'users'},
-        {name: 'Groups', state: 'groups'},
+        { name: 'Groups', state: 'groups' },
+        { name: 'Users', state: 'users' },
       ]
     },
     {
@@ -46,37 +45,36 @@ export class NavigationService {
       state: 'system',
       sub: [
         //{name: 'Information', state: 'information'},
-        {name: 'General', state: 'general'},
-        {name: 'Advanced', state: 'advanced'},
-        {name: 'Boot Environments', state: 'bootenv'},
+        { name: 'General', state: 'general' },
+        { name: 'NTP Servers', state: 'ntpservers' },
+        { name: 'Boot', state: 'bootenv' },
+        { name: 'Advanced', state: 'advanced' },
         //{name: 'Email', state: 'email'},
         //{name: 'System Dataset', state: 'systemdataset'},
-        //{name: 'Alert Services', state: 'alertservices'},
-        {name: 'Tunables', state: 'tunable'},
-        {name: 'Update', state: 'update'},
-        {name: 'NTPServers', state: 'ntpservers'},
-        {name: 'AlertService', state: 'alertservice'},
-        {name: 'CloudCredentials', state: 'cloudcredentials'},
-        {name: 'CAs', state: 'ca'},
-        {name: 'Certificates', state: 'certificates'},
-        {name: 'Support', state: 'support'},
+        { name: 'Alert Services', state: 'alertservice' },
+        { name: 'Cloud Credentials', state: 'cloudcredentials' },
+        { name: 'Tunables', state: 'tunable' },
+        { name: 'Update', state: 'update' },
+        { name: 'CAs', state: 'ca' },
+        { name: 'Certificates', state: 'certificates' },
+        { name: 'Support', state: 'support' },
       ]
     },
     {
-     name: 'Tasks',
-     type: 'dropDown',
-     tooltip: 'Tasks',
-     icon: 'date_range',
-     state: 'tasks',
-     sub: [
-       {name: 'Calendar', state: 'calendar'},
-       {name: 'Cron Jobs', state: 'cron'},
-       {name: 'Init/Shutdown Scripts', state: 'initshutdown'},
-       {name: 'Periodic Snapshot Tasks', state: 'snapshot'},
-       {name: 'Replication', state: 'replication'},
-       {name: 'Rsync Tasks', state: 'rsync'},
-       {name: 'S.M.A.R.T Tests', state: 'smart'},
-     ]
+      name: 'Tasks',
+      type: 'dropDown',
+      tooltip: 'Tasks',
+      icon: 'date_range',
+      state: 'tasks',
+      sub: [
+        { name: 'Calendar', state: 'calendar' },
+        { name: 'Cron Jobs', state: 'cron' },
+        { name: 'Init/Shutdown Scripts', state: 'initshutdown' },
+        { name: 'Rsync Tasks', state: 'rsync' },
+        { name: 'S.M.A.R.T Tests', state: 'smart' },
+        { name: 'Periodic Snapshot Tasks', state: 'snapshot' },
+        { name: 'Replication Tasks', state: 'replication' },
+      ]
     },
     {
       name: 'Network',
@@ -85,13 +83,13 @@ export class NavigationService {
       icon: 'device_hub',
       state: 'network',
       sub: [
-        {name: 'Global Configuration', state: 'configuration'},
-        {name: 'Interfaces', state: 'interfaces'},
-       {name: 'IPMI', state: 'ipmi'},
-        {name: 'Link Aggregation', state: 'laggs'},
-//        {name: 'Network Summary', state: 'networksummary'},
-        {name: 'Static Routes', state: 'staticroutes'},
-        {name: 'Vlans', state: 'vlans'},
+        { name: 'Global Configuration', state: 'configuration' },
+        { name: 'Interfaces', state: 'interfaces' },
+        { name: 'IPMI', state: 'ipmi' },
+        { name: 'Link Aggregations', state: 'laggs' },
+        //        {name: 'Network Summary', state: 'networksummary'},
+        { name: 'Static Routes', state: 'staticroutes' },
+        { name: 'VLANs', state: 'vlans' },
       ]
     },
     {
@@ -101,9 +99,9 @@ export class NavigationService {
       icon: 'storage',
       state: 'storage',
       sub: [
-        {name: 'Volumes', state: 'volumes'},
-        {name: 'Snapshots', state: 'snapshots'},
-        {name: 'vmware-Snapshots', state: 'vmware-Snapshots'}
+        { name: 'Volumes', state: 'volumes' },
+        { name: 'Snapshots', state: 'snapshots' },
+        { name: 'VMware-Snapshots', state: 'vmware-Snapshots' }
       ]
     },
     {
@@ -113,9 +111,9 @@ export class NavigationService {
       icon: 'group_work',
       state: 'directoryservice',
       sub: [
-        {name: 'ActiveDirectory', state: 'activedirectory'},
-        {name: 'LDAP', state: 'ldap'},
-        {name: 'NIS', state: 'nis'},
+        { name: 'Active Directory', state: 'activedirectory' },
+        { name: 'LDAP', state: 'ldap' },
+        { name: 'NIS', state: 'nis' },
       ]
     },
     {
@@ -125,11 +123,11 @@ export class NavigationService {
       icon: 'folder_shared',
       state: 'sharing',
       sub: [
-        {name: 'AFP', state: 'afp'},
-        {name: 'NFS', state: 'nfs'},
-        {name: 'WebDAV', state: 'webdav'},
-        {name: 'SMB', state: 'smb'},
-        {name: 'ISCSI', state: 'iscsi'},
+        { name: 'Apple (AFP) Shares', state: 'afp' },
+        { name: 'Unix (NFS) Shares', state: 'nfs' },
+        { name: 'WebDAV Shares', state: 'webdav' },
+        { name: 'Windows (SMB) Shares', state: 'smb' },
+        { name: 'Block (iSCSI)', state: 'iscsi' },
       ]
     },
     {
@@ -148,53 +146,54 @@ export class NavigationService {
     // },
     {
       name: 'Jails',
-      type: 'dropDown',
+      type: 'link',
       tooltip: 'Jails',
       icon: 'apps',
       state: 'jails',
-      sub: [
-        {name: 'Instances', state: 'jails'},
-        // {name: 'Storage', state: 'storage'},
-        // {name: 'Templates', state: 'templates'},
-        // {name: 'Configuration', state: 'configuration'},
-      ]
+      // sub: [
+      //   { name: 'Instances', state: 'jails' },
+      //   // {name: 'Storage', state: 'storage'},
+      //   // {name: 'Templates', state: 'templates'},
+      //   // {name: 'Configuration', state: 'configuration'},
+      // ]
     },
     {
-      name: 'Virtualization',
+      name: 'VMs',
       type: 'link',
       tooltip: 'Virtualization',
       icon: 'laptop_windows',
       state: 'vm'
-    },{
+    },
+    {
       name: 'Reporting',
       type: 'link',
       tooltip: 'Reports',
       icon: 'insert_chart',
       state: 'reportsdashboard',
     },
-//  {
-//    name: 'GUIDE',
-//    type: 'link',
-//    tooltip: 'Storage',
-//    icon: 'storage',
-//    state: 'storage'
-//  },
-//    {
-//      name: 'WIZARD',
-//      type: 'link',
-//      tooltip: 'Wizard',
-//     icon: 'cake',
-//    state: 'wizard'
-//  }
+    //  {
+    //    name: 'GUIDE',
+    //    type: 'link',
+    //    tooltip: 'Storage',
+    //    icon: 'storage',
+    //    state: 'storage'
+    //  },
+    //    {
+    //      name: 'WIZARD',
+    //      type: 'link',
+    //      tooltip: 'Wizard',
+    //     icon: 'cake',
+    //    state: 'wizard'
+    //  }
     {
-      name: 'System Processes',
+      name: 'Display System Processes',
       type: 'link',
       tooltip: 'System Processes',
-      icon: 'code',
+      icon: 'perm_data_setting',
       state: 'systemprocesses'
     },
     {
-      name: 'SHELL',
+      name: 'Shell',
       type: 'link',
       tooltip: 'Shell',
       icon: 'code',
@@ -208,12 +207,12 @@ export class NavigationService {
       state: 'guide',
     }
   ]
-  
+
   // Icon menu TITLE at the very top of navigation.
   // This title will appear if any icon type item is present in menu.
-  iconTypeMenuTitle:string = 'Frequently Accessed';
+  iconTypeMenuTitle: string = 'Frequently Accessed';
   // sets defaultMenu as default;
-  menuItems = new BehaviorSubject<IMenuItem[]>(this.defaultMenu);
+  menuItems = new BehaviorSubject < IMenuItem[] > (this.defaultMenu);
   // navigation component has subscribed this Observable
   menuItems$ = this.menuItems.asObservable();
 
