e50ef2be866681c427ed14a4a7f118b5f0f34e2d
Daisuke Imai <da.Imai.mobile@gmail.com>
2017-11-23 20:51:58 +0900

    Added the translate module on dialog pages

--- src/app/services/app-loader/app-loader.service.ts
+++ src/app/services/app-loader/app-loader.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import { Observable } from 'rxjs/Rx';
 import { AppLoaderComponent } from './app-loader.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class AppLoaderService {
   dialogRef: MdDialogRef<AppLoaderComponent>;
-  constructor(private dialog: MdDialog) { }
+  constructor(private dialog: MdDialog,
+    public translate: TranslateService) { }
 
   public open(title: string = 'Please wait'): Observable<boolean> {
     this.dialogRef = this.dialog.open(AppLoaderComponent, {disableClose: true});
     this.dialogRef.updateSize('200px');
-    this.dialogRef.componentInstance.title = title;
+    this.dialogRef.componentInstance.title = this.translate.instant(title);
     return this.dialogRef.afterClosed();
   }
 
