3dc5998520d4fed8af414f7ac0c47feec60117b5
Chentao Wang <allwithyou999@gmail.com>
2017-12-05 19:23:22 -0800

    Integrated with API on Wipe Disk

--- src/app/pages/storage/volumes/disks/disk-wipe/disk-wipe.component.ts
+++ src/app/pages/storage/volumes/disks/disk-wipe/disk-wipe.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from "@angular/router";
 import { FormArray, FormGroup } from '@angular/forms';
+import { MdSnackBar } from '@angular/material';
 import * as _ from 'lodash';
 
 import { WebSocketService } from "../../../../../services/ws.service";
 import { RestService } from "../../../../../services/rest.service";
 import { FieldConfig } from '../../../../common/entity/entity-form/models/field-config.interface';
 import { EntityFormService } from '../../../../common/entity/entity-form/services/entity-form.service';
+import { AppLoaderService } from '../../../../../services/app-loader/app-loader.service';
 
 
 @Component({
@@ -34,13 +36,13 @@ export class DiskWipeComponent implements OnInit {
       placeholder: 'Method',
       options: [{
         label: 'Quick',
-        value: 'quick',
+        value: 'QUICK',
       }, {
         label: 'Full with zeros',
-        value: 'full',
+        value: 'FULL',
       }, {
         label: 'Full with random data',
-        value: 'fullrandom',
+        value: 'FULL_RANDOM',
       }],
     }
   ];
@@ -51,7 +53,15 @@ export class DiskWipeComponent implements OnInit {
   constructor(private ws: WebSocketService,
               private router: Router,
               private activatedRoute: ActivatedRoute,
-              protected entityFormService: EntityFormService) {
+              protected entityFormService: EntityFormService,
+              protected loader: AppLoaderService,
+              public snackBar: MdSnackBar) {
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action , {
+      duration: 5000
+    });
   }
 
   preInit() {
@@ -69,6 +79,31 @@ export class DiskWipeComponent implements OnInit {
 
   goBack() {
     this.router.navigate(new Array('').concat(this.route_success));
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.loader.open();
+
+    let formValue = _.cloneDeep(this.formGroup.value);
+
+    this.ws.job('disk.wipe', [formValue.disk_name, formValue.wipe_method]).subscribe(
+      (res) => {
+        this.loader.close();
+        if (res.progress) {
+          console.log(res.progress.percent);
+        }
+        if (res.state == 'SUCCESS') {
+          this.openSnackBar("Disk successfully wiped", "Success");
+        } else if (res.state == 'FAILED') {
+          this.openSnackBar("Please retry.", "Failed");
+        }
+      },
+      (res) => {
+        console.log("Wipe disk error: ", res);
+        this.loader.close();
+      });
   }
 
 }
