f5ac3d0d2e9837f6111ab9abb0e3eb0d240da585
peter_southwell <pete_south@yahoo.com>
2017-12-12 16:20:00 -0800

    got zfs pool being the grouped by column as well as zfs pool status in the expansion title

--- src/app/pages/common/entity/entity-table/entity-group-table.component.ts
+++ src/app/pages/common/entity/entity-table/entity-group-table.component.ts
@@ -18,16 +18,6 @@ import { DialogService } from 'app/services';
 import { EntityTableComponent } from 'app/pages/common/entity/entity-table';
 
 
-export interface Node {
-  id?: number;
-  type?: string;
-  name?: string;
-  fullpath?: string;
-  parent?: string;
-  children?: Node[];
-};
-
-
 @Component({
   selector: 'entity-group-table',
   templateUrl: './entity-group-table.component.html',
@@ -35,27 +25,11 @@ export interface Node {
   providers: [DialogService]
 })
 export class EntityGroupTableComponent extends EntityTableComponent implements OnInit {
-  readonly parentId = -666;
-  readonly parentRootPath = "/ROOT/";
-  readonly parentName = "All";
   
-  groups = [];
 
-
-
-   @ViewChild('myTable') table: any;
-  parent: Node = {
-    id: this.parentId,
-    name: this.parentName,
-    fullpath: this.parentRootPath,
-    type: this.parentRootPath,
-    parent: null,
-    children: []
-  };
-
-  parentMap: Map<string, Node> = new Map<string, Node>();
-
-
+  
+  @ViewChild('myTable') table: any;
+  
   constructor(protected rest: RestService, protected router: Router, protected ws: WebSocketService,
     protected _eRef: ElementRef, protected dialog: DialogService, protected loader: AppLoaderService) {
     super(rest, router, ws, _eRef, dialog, loader);
@@ -69,94 +43,9 @@ export class EntityGroupTableComponent extends EntityTableComponent implements O
   handleData(res): any {
 
     res = super.handleData(res);
-
-    this.rows.forEach((row) => {
-      const newNode: Node = {
-        id: row.id,
-        type: row.type,
-        name: row.path,
-        parent: this.parentRootPath,
-        fullpath: row.path
-
-      };
-      this.parentMap.set(row.path, newNode);
-
-
-      if (row.path.indexOf("/") === - 1) {
-
-        if( newNode.type === "zpool" ) {
-          newNode.name += "(zpool)";
-        }
-        this.parent.children.push(newNode);
-      } else {
-        let lastIndexOf: number = row.path.lastIndexOf("/");
-        if (lastIndexOf === -1) {
-          lastIndexOf = row.path.length;
-        }
-
-        const key: string = row.path.substr(0, lastIndexOf);
-
-        if (this.parentMap.has(key)) {
-          const parentNode: Node = this.parentMap.get(key);
-
-
-          if (typeof (parentNode.children) === "undefined") {
-            parentNode.children = [];
-          }
-
-          newNode.name = newNode.name.substr((key + "/").length);
-          newNode.parent = key;
-          parentNode.children.push(newNode);
-        }
-      }
-
-    });
-
     return res;
   }
 
-
-  treeOnActiveChanged($event) {
-    $event.preventDefault();
-
-    const newData: any[] = [];
-
-    this.rows.forEach((row) => {
-
-      if( row.path === $event.node.data.fullpath || $event.node.data.fullpath === this.parentRootPath) {
-        newData.push(row);
-      } else {
-        let lastIndexOf: number = row.path.lastIndexOf("/");
-        if (lastIndexOf === -1) {
-          lastIndexOf = row.path.length;
-        }
-
-        let key: string = row.path.substr(0, lastIndexOf);
-        const filteredParentRootNode: Node = this.parentMap.get($event.node.data.fullpath);
-
-        let found = false;
-        while (this.parentMap.has(key) && key !== this.parentRootPath) {
-          const node = this.parentMap.get(key);
-
-          if (node.id === filteredParentRootNode.id) { 
-            found = true;
-            break;
-          }
-
-          key = node.parent;
-        }
-
-        if (found === true) {
-          newData.push(row);
-        }
-      }
-
-    });
-
-    this.currentRows = newData;
-    this.paginationPageIndex = 0;
-    this.setPaginationInfo();
-  }
 
 
   toggleExpandGroup($event, group) {
@@ -169,4 +58,6 @@ export class EntityGroupTableComponent extends EntityTableComponent implements O
     console.log('Detail Toggled', $event);
     $event.preventDefault();
   }
+
+  
 }
