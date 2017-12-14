68800ecc181b480eb0af95ccf1340242ad07061c
Lola Yang <lola.ly27@gmail.com>
2017-12-14 15:21:15 -0800

    Added kerberos realms list component

---
+++ src/app/pages/directoryservice/kerberosrealms/kerberosrealms-list/kerberosrealms-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RestService } from '../../../../services/';
+
+@Component({
+  selector: 'app-user-list',
+  template: `<entity-table [title]="title" [conf]="this"></entity-table>`
+})
+export class KerberosRealmsListComponent {
+
+  public title = "Kerberos Realms";
+  protected resource_name: string = 'directoryservice/kerberosrealm';
+  protected route_add: string[] = ['kerberosrealms', 'add'];
+  protected route_add_tooltip: string = "Add Kerberos Realm";
+  protected route_edit: string[] = ['kerberosrealms', 'edit'];
+
+  public columns: Array < any > = [
+    { name: 'Realm', prop: 'krb_realm' },
+    { name: 'KDC', prop: 'krb_kdc' },
+    { name: 'Admin Server', prop: 'krb_admin_server' },
+    { name: 'Password Server', prop: 'krb_kpasswd_server' },
+  ];
+  public config: any = {
+    paging: true,
+    sorting: { columns: this.columns },
+  };
+
+  constructor(protected rest: RestService, private router: Router){}
+}
