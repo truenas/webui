1531b926f29a60d5a4e9a36b8d8a58c2a7965335
Lola Yang <lola.ly27@gmail.com>
2018-01-05 12:29:16 -0800

    Enable to edit mountpoint

--- src/app/pages/jails/storages/storage-form/storage-form.component.ts
+++ src/app/pages/jails/storages/storage-form/storage-form.component.ts
@@ -11,16 +11,46 @@ import { DialogService } from '../../../../services/';
 import { JailService } from '../../../../services/';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 
+interface MountPoint {
+  action: string,
+  source: string,
+  destination: string,
+  fstype: string,
+  fsoptions: string,
+  dump: string,
+  pass: string,
+  index ? : string,
+}
 @Component({
   selector: 'app-storage-add',
   template: `<entity-form [conf]="this"></entity-form>`
 })
 export class StorageFormComponent {
 
-  protected addCall = 'jail.fstab';
+  protected queryCall = "jail.fstab";
   protected route_success: string[] = ['jails', 'storage'];
   protected isEntity: boolean = true;
-
+  protected pk: string;
+  protected mountpointId: string;
+  protected queryCallOption: any;
+  protected mountPointAdd: MountPoint = {
+    action: "ADD",
+    source: "",
+    destination: "",
+    fstype: "",
+    fsoptions: "",
+    dump: "",
+    pass: "",
+  };
+  protected mountPointEdit: MountPoint = {
+    action: "REPLACE",
+    source: "",
+    destination: "",
+    fstype: "",
+    fsoptions: "",
+    dump: "",
+    pass: "",
+  };
   public fieldConfig: FieldConfig[] = [{
       type: 'select',
       name: 'jail',
@@ -47,6 +77,7 @@ export class StorageFormComponent {
   protected formGroup: any;
   protected error: any;
   protected jailID: any;
+
   constructor(protected router: Router, protected aroute: ActivatedRoute,
     protected jailService: JailService, protected loader: AppLoaderService, protected ws: WebSocketService,
     private dialog: DialogService) {}
@@ -55,8 +86,11 @@ export class StorageFormComponent {
     this.jail = _.find(this.fieldConfig, { 'name': 'jail' });
     this.aroute.params.subscribe(params => {
       this.route_success.push(params['jail']);
+      this.mountpointId = params['pk'];
       this.jailID = params['jail'];
-      if(this.jailID) {
+      this.pk = params['jail'];
+      this.queryCallOption = { "action": "LIST", "source": "", "destination": "", "fstype": "", "fsoptions": "", "dump": "", "pass": "" };
+      if (this.jailID) {
         this.jail.value = this.jailID;
       }
     });
@@ -68,6 +102,9 @@ export class StorageFormComponent {
     entityForm.error = this.error;
     entityForm.route_success = this.route_success;
     entityForm.jailID = this.jailID;
+    entityForm.mountPointEdit = this.mountPointEdit;
+    entityForm.mountPointAdd = this.mountPointAdd;
+    entityForm.mountpointId = this.mountpointId;
 
     this.jailService.listJails().subscribe((res) => {
       res.forEach((item) => {
@@ -80,22 +117,44 @@ export class StorageFormComponent {
     }
   }
 
+  dataAttributeHandler(entityList: any) {
+    entityList.formGroup.controls['source'].setValue(entityList.queryResponse[this.mountpointId][0]);
+    entityList.formGroup.controls['destination'].setValue(entityList.queryResponse[this.mountpointId][1]);
+
+    this.mountPointEdit.source = entityList.queryResponse[this.mountpointId][0];
+    this.mountPointEdit.destination = entityList.queryResponse[this.mountpointId][1];
+    this.mountPointEdit.fstype = entityList.queryResponse[this.mountpointId][2];
+    this.mountPointEdit.fsoptions = entityList.queryResponse[this.mountpointId][3];
+    this.mountPointEdit.dump = entityList.queryResponse[this.mountpointId][4];
+    this.mountPointEdit.pass = entityList.queryResponse[this.mountpointId][5];
+  }
+
   onSubmit(event: Event) {
     event.preventDefault();
     event.stopPropagation();
     this.error = null;
     let value = _.cloneDeep(this.formGroup.value);
+    let mountPoint: MountPoint;
 
-    let jail = this.jailID;
-
-    value['action'] = "ADD";
-    value['fstype'] = "nullfs";
-    value['fsoptions'] = "ro";
-    value['dump'] = "0";
-    value['pass'] = "0";
+    if (this.mountpointId) {
+      //edit mode
+      this.mountPointEdit.source = value['source'];
+      this.mountPointEdit.destination = value['destination'];
+      this.mountPointEdit.index = this.mountpointId;
+      mountPoint = this.mountPointEdit;
+    } else {
+      // add mode
+      this.mountPointAdd.source = value['source'];
+      this.mountPointAdd.destination = value['destination'];
+      this.mountPointAdd.fstype = "nullfs";
+      this.mountPointAdd.fsoptions = "ro";
+      this.mountPointAdd.dump = "0";
+      this.mountPointAdd.pass = "0";
+      mountPoint = this.mountPointAdd;
+    }
 
     this.loader.open();
-    this.ws.call('jail.fstab', [jail, value]).subscribe(
+    this.ws.call('jail.fstab', [this.jailID, mountPoint]).subscribe(
       (res) => {
         this.loader.close();
         this.router.navigate(new Array('/').concat(this.route_success));
