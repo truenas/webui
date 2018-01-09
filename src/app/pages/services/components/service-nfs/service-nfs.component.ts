b02f9d3e289846fd8a85363e324411c553c3846a
Samantha Bonham <sam.a.bonham@gmail.com>
2018-01-09 14:35:36 -0800

    Added the tooltips for Services->NFS->Configure page.

--- src/app/pages/services/components/service-nfs/service-nfs.component.ts
+++ src/app/pages/services/components/service-nfs/service-nfs.component.ts
@@ -25,16 +25,23 @@ export class ServiceNFSComponent{
       type : 'input',
       name : 'nfs_srv_servers',
       placeholder : 'Number of servers:',
+      tooltip: 'Specifies how many servers to create. The number of\
+ servers can be increased if NFS client responses are slow; to limit\
+ CPU context switching, keep this number less than or equal to the\
+ number of CPUs reported by <b> sysctl -n kern.smp.cpus </b>.',
     },
     {
       type : 'checkbox',
       name : 'nfs_srv_udp',
       placeholder : 'Serve UDP NFS clients:',
+      tooltip: 'Checks if NFS clients need to use UDP.',
     },
     {
       type : 'select',
       name : 'nfs_srv_bindip',
       placeholder : 'Bind IP Addresses:',
+      tooltip: 'Select the IP addresses to listen to for NFS requests.\
+ When unchecked, NFS listens on all available addresses.',
       options : [],
       multiple : true
     },
@@ -42,16 +49,23 @@ export class ServiceNFSComponent{
       type : 'checkbox',
       name : 'nfs_srv_allow_nonroot',
       placeholder : 'Allow non-root mount:',
+      tooltip: 'Check this box only if the NFS client requires it.\
+ When checked non-root mount requests are allowed to be served.',
     },
     {
       type : 'checkbox',
       name : 'nfs_srv_v4',
       placeholder : 'Enable NFSv4:',
+      tooltip: 'NFSv3 is the default, check this box to switch to NFSv4.',
     },
     {
       type : 'checkbox',
       name : 'nfs_srv_v4_v3owner',
       placeholder : 'NFSv3 ownership model for NFSv4:',
+      tooltip: 'Grayed out unless <b>Enable NFSv4 </b> is checked and, in turn, will\
+ gray out <b>Support>16</b> groups which is incompatible; check this box if\
+ NFSv4 ACL support is needed without requiring the client and the\
+ server to sync users and groups.',
       relation : [
         {
           action : 'DISABLE',
@@ -66,36 +80,51 @@ export class ServiceNFSComponent{
       type : 'checkbox',
       name : 'nfs_srv_v4_krb',
       placeholder : 'Require Kerberos for NFSv4:',
+      tooltip: 'When checked, NFS shares will fail if the Kerberos ticket is unavailable.',
     },
     {
       type : 'input',
       name : 'nfs_srv_mountd_port',
       placeholder : 'mountd(8) bind port:',
+      tooltip: 'Optional; specify port that <a href="https://www.freebsd.org/cgi/man.cgi?query=mountd" target="_blank">mountd(8)</a>\
+ binds to.',
     },
     {
       type : 'input',
       name : 'nfs_srv_rpcstatd_port',
       placeholder : 'rpc.statd(8) bind port:',
+      tooltip: 'Optional; specify port that <a href="https://www.freebsd.org/cgi/man.cgi?query=rpc.statd" target="_blank">rpc.statd(8)</a>\
+ binds to.',
     },
     {
       type : 'input',
       name : 'nfs_srv_rpclockd_port',
       placeholder : 'rpc.lockd(8) bind port:',
+      tooltip: 'Optional; specify port that <a href="https://www.freebsd.org/cgi/man.cgi?query=rpc.lockd" target="_blank">rpc.lockd(8)</a>\
+ binds to.',
     },
     {
       type : 'checkbox',
       name : 'nfs_srv_16',
       placeholder : 'Support >16 groups:',
+      tooltip: 'Check this box if any users are members of more than\
+ 16 groups (useful in AD environments); note that this assumes that\
+ group membership has been configured correctly on the NFS server.',
     },
     {
       type : 'checkbox',
       name : 'nfs_srv_mountd_log',
       placeholder : 'Log mountd(8) requests:',
+      tooltip: 'Enable logging of <a href="https://www.freebsd.org/cgi/man.cgi?query=mountd" target="_blank">mountd(8)</a>\
+ requests by syslog.',
     },
     {
       type : 'checkbox',
       name : 'nfs_srv_statd_lockd_log',
       placeholder : 'Log rpc.statd(8) and rpc.lockd(8)',
+      tooltip: 'Enable logging of <a href="https://www.freebsd.org/cgi/man.cgi?query=rpc.statd" target="_blank">rpc.statd(8)</a>\
+ and <a href="https://www.freebsd.org/cgi/man.cgi?query=rpc.lockd" target="_blank">rpc.lockd(8)</a>\
+ requests by syslog.',
     },
   ];
 
