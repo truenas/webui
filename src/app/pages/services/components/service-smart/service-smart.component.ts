d4f55d976dcf363ebe41d8b2b2f5b930b1b7d703
Samantha Bonham <sam.a.bonham@gmail.com>
2018-01-08 17:48:35 -0800

    Added the tooltips for Services->Smart page.

--- src/app/pages/services/components/service-smart/service-smart.component.ts
+++ src/app/pages/services/components/service-smart/service-smart.component.ts
@@ -37,11 +37,16 @@ export class ServiceSMARTComponent {
       type : 'input',
       name : 'smart_interval',
       placeholder : 'Check Interval',
+      tooltip: 'In minutes, how often smartd wakes up to check if\
+      any tests have been configured to run.',
     },
     {
       type : 'select',
       name : 'smart_powermode',
       placeholder : 'Power Mode',
+      tooltip: 'Tests are not performed if the system enters the\
+ specified power mode; choices\
+ are: <i>Never</i>, <i>Sleep</i>, <i>Standby</i>, or <i>Idle</i>.',
       options : [
         {label : 'Never', value : 'never'},
         {label : 'Sleep', value : 'sleep'},
@@ -53,21 +58,31 @@ export class ServiceSMARTComponent {
       type : 'input',
       name : 'smart_difference',
       placeholder : 'Difference',
+      tooltip: 'Default of <i>0</i> disables this\
+ check, otherwise reports if the temperature of a drive has\
+ changed by N degrees Celsius since last report.',
     },
     {
       type : 'input',
       name : 'smart_informational',
       placeholder : 'Informational',
+      tooltip: 'default of 0 disables this check, otherwise will\
+ message with a log level of LOG_INFO if the temperature is higher\
+ than specified degrees in Celsius.',
     },
     {
       type : 'input',
       name : 'smart_critical',
       placeholder : 'Critical',
+      tooltip: 'default of 0 disables this check, otherwise will\
+ message with a log level of LOG_CRIT and send an email if the\
+ temperature is higher than specified degrees in Celsius.',
     },
     {
       type : 'input',
       name : 'smart_email',
       placeholder : 'Email',
+      tooltip: 'Email address of person or alias to receive S.M.A.R.T. alerts.',
     },
   ];
 
