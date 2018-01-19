dc34366e35e683b6b50298910c966350a6cf334f
a-stjohn <aaron.m.stjohn@gmail.com>
2018-01-19 12:58:15 -0500

    Added 4/93 tool tips.

--- src/app/pages/jails/jail-add/jail-add.component.ts
+++ src/app/pages/jails/jail-add/jail-add.component.ts
@@ -33,17 +33,23 @@ export class JailAddComponent implements OnInit {
       type: 'input',
       name: 'uuid',
       placeholder: 'Jails Name',
+      tooltip : 'Mandatory. Can only contain letters, numbers, dashes,\
+ or the underscore character.',
     },
     {
       type: 'select',
       name: 'release',
       placeholder: 'Release',
+      tooltip : 'Select the release for the jail.',
       options: [],
     },
     {
       type: 'input',
       name: 'ip4_addr',
       placeholder: 'IPv4 Address',
+      tooltip : 'This and the other IPv4 settings are grayed out if\
+ <b>IPv4 DHCP</b> is checked. Enter a unique IP address that is in the\
+ local network and not already used by any other computer.',
     },
     {
       type: 'input',
@@ -54,6 +60,9 @@ export class JailAddComponent implements OnInit {
       type: 'input',
       name: 'ip6_addr',
       placeholder: 'IPv6 Address',
+      tooltip : 'This and other IPv6 settings are grayed out if\
+ <b>IPv6 Autoconfigure</b> is checked; enter a unique IPv6 address that\
+ is in the local network and not already used by any other computer',
     },
     {
       type: 'input',
