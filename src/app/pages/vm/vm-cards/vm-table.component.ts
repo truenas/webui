e7e510e3d5b14e00fac3299387ed501c3bf79f71
Lola Yang <lola.ly27@gmail.com>
2018-01-23 14:58:19 -0800

    using material 5 in app/pages/vm

--- src/app/pages/vm/vm-cards/vm-table.component.ts
+++ src/app/pages/vm/vm-cards/vm-table.component.ts
@@ -5,7 +5,8 @@ import {Observable} from 'rxjs/Observable';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import {RestService, WebSocketService} from '../../../services/';
-import { MaterialModule, MdButtonToggleGroup } from '@angular/material';
+import { MaterialModule } from '../../../appMaterial.module';
+import { MatButtonToggleGroup } from '@angular/material/button-toggle';
 
 export class Page {
     //The number of elements in the page
