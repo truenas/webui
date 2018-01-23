6fae370f524f6f53221527664efd0c516c1235b6
Lola Yang <lola.ly27@gmail.com>
2018-01-23 10:26:01 -0800

    using material 5 in app/pages/plugins

--- src/app/pages/plugins/plugins.module.ts
+++ src/app/pages/plugins/plugins.module.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgUploaderModule} from 'ngx-uploader';
-import { MaterialModule, MdTableModule } from '@angular/material';
+import { MaterialModule } from '../../appMaterial.module';
 
 import {EntityModule} from '../common/entity/entity.module';
 
