b11bda4d766db1be32724e34fd517735b42d9ca9
rishabh27892 <rishabh.chauhan27892@gmail.com>
2018-01-29 12:53:35 -0800

    silly mistake fixed

--- tests/acc_group.py
+++ tests/acc_group.py
@@ -31,7 +31,7 @@ xpaths = {
           'fabTrigger': "//*[@id='myFab']/div/smd-fab-trigger/button",
           'fabAction': "//*[@id='add_action_button']/span/mat-icon",
           'savButton': "//*[@id='save_button']",
-          'permitsudoCheckbox: "//*[@id='bsdgrp_sudo']/mat-checkbox/label/div"'
+          'permitsudoCheckbox': "//*[@id='bsdgrp_sudo']/mat-checkbox/label/div"
         }
 
 class create_group_test(unittest.TestCase):
