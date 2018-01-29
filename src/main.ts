1d3aef2d79c5fc9ac27fba0b1e25fd99c95f0996
peter_southwell <pete_south@yahoo.com>
2018-01-28 20:12:00 -0800

    think this actually works in aot

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
-
 import '../node_modules/@angular/material/prebuilt-themes/indigo-pink.css';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from 'environments/environment';
+
+
+if (environment.production) {
+    enableProdMode();
+  }
+
+  
+platformBrowserDynamic().bootstrapModule(AppModule);
