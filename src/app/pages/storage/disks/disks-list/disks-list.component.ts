0f6edadc264a747877cea8f68d30fd914837210b
peter_southwell <pete_south@yahoo.com>
2018-01-29 17:49:45 -0800

    Syntax error duringmerge

--- src/app/pages/storage/disks/disks-list/disks-list.component.ts
+++ src/app/pages/storage/disks/disks-list/disks-list.component.ts
@@ -116,7 +116,7 @@ export class DisksListComponent extends EntityTableComponent implements OnInit, 
 
   constructor(protected rest: RestService, protected router: Router, protected ws: WebSocketService,
     protected _eRef: ElementRef, protected dialogService: DialogService, protected loader: AppLoaderService, protected erdService: ErdService) {
-    super(rest, router, ws, _eRef, dialog, loader, erdService);
+    super(rest, router, ws, _eRef, dialogService, loader, erdService);
       this.conf = new DisksListConfig(this.router, "", "");
   }
 
