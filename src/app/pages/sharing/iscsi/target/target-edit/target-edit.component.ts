9c52a8910e1087647c0569cbe2c39a0c54e49557
Erin Clark <erin@ixsystems.com>
2018-03-03 11:20:26 -0800

    CSS align the buttons in iscsi targets
    Ticket: #27263

--- src/app/pages/sharing/iscsi/target/target-edit/target-edit.component.ts
+++ src/app/pages/sharing/iscsi/target/target-edit/target-edit.component.ts
@@ -14,6 +14,7 @@ import { EntityFormService } from '../../../../common/entity/entity-form/service
 @Component({
   selector : 'app-iscsi-target-edit',
   templateUrl : './target-edit.component.html',
+  styleUrls: ['./target-edit.component.css'],
   providers : [ IscsiService, EntityFormService ],
 })
 export class TargetEditComponent implements OnInit {
