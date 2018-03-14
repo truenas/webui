489f6623d57ac08cea3111f5a0bf1d909ac0dc9a
Lola Yang <lola.ly27@gmail.com>
2018-03-14 14:04:26 -0700

    Added T() marker to iscsi globalconfiguration;extent;authorizedaccess;associated-target

--- src/app/pages/sharing/iscsi/authorizedaccess/authorizedaccess-form/authorizedaccess-form.component.ts
+++ src/app/pages/sharing/iscsi/authorizedaccess/authorizedaccess-form/authorizedaccess-form.component.ts
@@ -7,6 +7,7 @@ import { FieldConfig } from '../../../../common/entity/entity-form/models/field-
 import {
   matchOtherValidator
 } from '../../../../common/entity/entity-form/validators/password-validation';
+import { T } from '../../../../../translate-marker';
 
 @Component({
   selector : 'app-iscsi-authorizedaccess-form',
@@ -22,27 +23,27 @@ export class AuthorizedAccessFormComponent {
     {
       type : 'input',
       name : 'iscsi_target_auth_tag',
-      placeholder : 'Group ID',
-      tooltip: 'Allows different groups to be configured with different\
+      placeholder : T('Group ID'),
+      tooltip: T('Allows different groups to be configured with different\
  authentication profiles. For instance, all users with a group ID of\
  <i>1</i> will inherit the authentication profile associated with Group\
- <i>1</i>.',
+ <i>1</i>.'),
       inputType : 'integer',
     },
     {
       type : 'input',
       name : 'iscsi_target_auth_user',
-      placeholder : 'User',
-      tooltip: 'Name of user account to create for CHAP authentication\
+      placeholder : T('User'),
+      tooltip: T('Name of user account to create for CHAP authentication\
  with the user on the remote system. Many initiators default to using\
- the initiator name as the user.',
+ the initiator name as the user.'),
     },
     {
       type : 'input',
       name : 'iscsi_target_auth_secret',
-      placeholder : 'Secret',
-      tooltip: 'Password to be associated with <b>User</b>. The iSCSI\
- standard requires that this be between 12 and 16 characters.',
+      placeholder : T('Secret'),
+      tooltip: T('Password to be associated with <b>User</b>. The iSCSI\
+ standard requires that this be between 12 and 16 characters.'),
       inputType : 'password',
       validation : [
         Validators.minLength(8),
@@ -52,23 +53,23 @@ export class AuthorizedAccessFormComponent {
     {
       type : 'input',
       name : 'iscsi_target_auth_secret_confirm',
-      placeholder : 'Secret (Confirm)',
+      placeholder : T('Secret (Confirm)'),
       inputType : 'password',
     },
     {
       type : 'input',
       name : 'iscsi_target_auth_peeruser',
-      placeholder : 'Peer User',
-      tooltip: 'Only input when configuring mutual CHAP. In most cases\
- it will need to be the same value as <b>User</b>.',
+      placeholder : T('Peer User'),
+      tooltip: T('Only input when configuring mutual CHAP. In most cases\
+ it will need to be the same value as <b>User</b>.'),
     },
     {
       type : 'input',
       name : 'iscsi_target_auth_peersecret',
-      placeholder : 'Peer Secret',
-      tooltip: 'The mutual secret password which\
+      placeholder : T('Peer Secret'),
+      tooltip: T('The mutual secret password which\
  <b>must be different than the <i>Secret</i></b>. Required if\
- <b>Peer User</b> is set.',
+ <b>Peer User</b> is set.'),
       inputType : 'password',
       validation : [
         Validators.minLength(8),
@@ -78,7 +79,7 @@ export class AuthorizedAccessFormComponent {
     {
       type : 'input',
       name : 'iscsi_target_auth_peersecret_confirm',
-      placeholder : 'Peer Secret (Confirm)',
+      placeholder : T('Peer Secret (Confirm)'),
       inputType : 'password',
     },
   ];
