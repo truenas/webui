7c9229ee711feeeab6bb19e8170c4b33637c25d7
Damian Szidiropulosz <damianos@knoxbug.org>
2018-03-15 12:02:18 -0400

    WIP: This commit fixes ticket 28407. Entity tables having unnecessary fab dial when only a single action is listed. Will refine this further so we have a better option for entity-tables with multiple actions listed. Right now there isn't enough time before code freeze to go through all the pages.

--- src/app/pages/common/entity/entity-table/entity-table-add-actions.component.ts
+++ src/app/pages/common/entity/entity-table/entity-table-add-actions.component.ts
@@ -10,7 +10,16 @@ import {EntityTableComponent} from './entity-table.component';
 @Component({
   selector : 'app-entity-table-add-actions',
   template : `
-	<div *ngIf="this.entity.conf.route_add || this.actions.length > 0">
+	<div *ngIf="this.entity.conf.route_add || this.actions.length == 1">
+	  <button class="mat-card-primary-action" id="add_action_button" *ngIf="this.entity.conf.route_add" mat-mini-fab (click)="this.entity.doAdd()" matTooltip="{{this.entity.conf.route_add_tooltip | translate}}">
+	    <mat-icon>add</mat-icon>
+          </button>
+	  <button mat-mini-fab class="mat-card-primary-action card-action" id="add_action_button_{{action?.label}}" *ngFor="let action of actions" (click)="action.onClick()" matTooltip="{{action.label | translate}}">
+	    <mat-icon>{{action.icon}}</mat-icon>
+          </button>
+              </div>
+        
+	<div *ngIf="this.actions.length > 1">
 		<smd-fab-speed-dial id="myFab" #myFab [direction]="direction" [animationMode]="animationMode"
 				(mouseenter)="myFab.open = true" (mouseleave)="myFab.open = false">
 			<smd-fab-trigger [spin]="spin">
