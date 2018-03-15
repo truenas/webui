ae5ec2cc4b8e9762268cc98132aaf0c650bf677d
Xin Tan <mactanxin@gmail.com>
2018-03-15 14:28:02 -0700

    ADDED: translation marker for ntp edit

--- src/app/pages/system/ntpservers/ntpserver-edit/ntpserver-edit.component.ts
+++ src/app/pages/system/ntpservers/ntpserver-edit/ntpserver-edit.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AbstractControl, FormArray, FormGroup, Validators } from '@angular/forms';
 import { RestService } from '../../../../services/rest.service';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import { T } from '../../../../translate-marker';
 
 @Component({
   selector: 'app-ntpserver-edit',
@@ -17,55 +18,55 @@ export class NTPServerEditComponent {
   public fieldConfig: FieldConfig[] = [{
       type: 'input',
       name: 'ntp_address',
-      placeholder: 'Address',
-      tooltip: 'Enter the hostname or IP address of the <b>NTP</b> server.',
+      placeholder: T('Address'),
+      tooltip: T('Enter the hostname or IP address of the <b>NTP</b> server.'),
     },
     {
       type: 'checkbox',
       name: 'ntp_burst',
-      placeholder: 'Burst',
-      tooltip: 'Recommended when <i>Max. Poll</i> is greater\
- than 10; only use on your own servers i.e. do not use with\
- a public NTP server.',
+      placeholder: T('Burst'),
+      tooltip: T('Recommended when <i>Max. Poll</i> is greater\
+       than 10; only use on your own servers i.e. do not use with\
+       a public NTP server.'),
     },
     {
       type: 'checkbox',
       name: 'ntp_iburst',
-      placeholder: 'IBurst',
-      tooltip: 'Speeds the initial synchronization\
- (seconds instead of minutes).',
+      placeholder: T('IBurst'),
+      tooltip: T('Speeds the initial synchronization\
+       (seconds instead of minutes).'),
     },
     {
       type: 'checkbox',
       name: 'ntp_prefer',
-      placeholder: 'Prefer',
-      tooltip: 'Should only be used for <b>NTP</b> servers known to\
- be highly accurate, such as those with time monitoring hardware.',
+      placeholder: T('Prefer'),
+      tooltip: T('Should only be used for <b>NTP</b> servers known to\
+       be highly accurate, such as those with time monitoring hardware.'),
     },
     {
       type: 'input',
       name: 'ntp_minpoll',
-      placeholder: 'Min. Poll',
+      placeholder: T('Min. Poll'),
       inputType: 'number',
       validation: [Validators.required, Validators.min(0)],
-      tooltip: 'Power of 2 in seconds; cannot be lower than 4 or\
- higher than <i>Max. Poll</i> which is 17.',
+      tooltip: T('Power of 2 in seconds; cannot be lower than 4 or\
+       higher than <i>Max. Poll</i> which is 17.'),
     },
     {
       type: 'input',
       name: 'ntp_maxpoll',
-      placeholder: 'Max. Poll',
+      placeholder: T('Max. Poll'),
       inputType: 'number',
       validation: [Validators.required, Validators.min(0)],
-      tooltip: 'Power of 2 in seconds; cannot be higher than 17 or\
- lower than <i>Min. Poll</i>.',
+      tooltip: T('Power of 2 in seconds; cannot be higher than 17 or\
+       lower than <i>Min. Poll</i>.'),
     },
     {
       type: 'checkbox',
       name: 'force',
-      placeholder: 'Force',
-      tooltip: 'Forces the addition of the <b>NTP</b> server,\
- even if it is currently unreachable.',
+      placeholder: T('Force'),
+      tooltip: T('Forces the addition of the <b>NTP</b> server,\
+       even if it is currently unreachable.'),
     }
   ];
 
