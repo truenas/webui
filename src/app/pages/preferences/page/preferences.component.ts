a3d18c1a4644792aa685da1a4ac7c50bc12d3974
Damian Szidiropulosz <damianos@knoxbug.org>
2018-03-19 17:38:41 -0400

    WIP: Fixed global css rule that caused overlapping icons on Storage/Volumes page.

--- src/app/pages/preferences/page/preferences.component.ts
+++ src/app/pages/preferences/page/preferences.component.ts
@@ -13,93 +13,51 @@ import { Subject } from 'rxjs/Subject';
 @Component({
   selector : 'ui-preferences',
   template:`
-    <mat-card>
-      <mat-toolbar-row>
-        <h3>User Preferences</h3>
-      </mat-toolbar-row>
-      <mat-card-content fxLayout="row wrap" fxLayoutAlign="space-between start">
-        <entity-form-embedded fxFlex="100" fxFlex.gt-xs="300px" [target]="target" [data]="values" [conf]="this"></entity-form-embedded>
-        <div fxFlex="100" fxFlex.gt-xs="calc(100% - 300px)"></div>
-      </mat-card-content>
-    </mat-card>
-  `
-  //templateUrl : './preferences.component.html',
-  //styleUrls: ['./preferences.component.css'],
-})
+  <mat-card>
+  <mat-toolbar-row style="margin-bottom:16px;">
+  <h4>User Preferences</h4>
+  </mat-toolbar-row>
+  <mat-divider></mat-divider>
+  <mat-card-content fxLayout="row wrap" fxLayoutAlign="space-between start" style="margin-top:32px;">
+    <entity-form-embedded fxFlex="100" fxFlex.gt-xs="300px" [target]="target" [data]="values" [conf]="this"></entity-form-embedded>
+      <div fxFlex="100" fxFlex.gt-xs="calc(100% - 300px)"></div>
+        </mat-card-content>
+        </mat-card>
+        `
+        //templateUrl : './preferences.component.html',
+        //styleUrls: ['./preferences.component.css'],
+        })
 export class PreferencesPage implements OnInit, OnChanges {
 
   /*
-  //Preferences Object Structure
-  platform:string; // FreeNAS || TrueNAS
-  timestamp:Date;
-  userTheme:string; // Theme name
-  customThemes?: Theme[]; 
-  favoriteThemes?: string[]; // Theme Names
-  showGuide:boolean; // Guided Tour on/off
-  showTooltips:boolean; // Form Tooltips on/off
-  metaphor:string; // Prefer Cards || Tables || Auto (gui decides based on data array length)
+   //Preferences Object Structure
+   platform:string; // FreeNAS || TrueNAS
+   timestamp:Date;
+   userTheme:string; // Theme name
+   customThemes?: Theme[]; 
+   favoriteThemes?: string[]; // Theme Names
+   showGuide:boolean; // Guided Tour on/off
+   showTooltips:boolean; // Form Tooltips on/off
+   metaphor:string; // Prefer Cards || Tables || Auto (gui decides based on data array length)
 
    */
 
   public target: Subject<CoreEvent> = new Subject();
   public values = [];
-  public saveSubmitText = "Save Changes";
+  public saveSubmitText = "Update Settings";
   protected isEntity: boolean = true; // was true
-  private colorOptions: any[] = []; 
+    private colorOptions: any[] = []; 
   private themeOptions: any[] = [];
   private favoriteFields: any[] = []
-  public fieldConfig:FieldConfig[] = [];
+    public fieldConfig:FieldConfig[] = [];
   public fieldSetDisplay:string = 'no-margins';//default | carousel | stepper
-  public fieldSets: FieldSet[] = [
-    {
-      name:'General Preferences',
-      class:'general-prefs',
-      width:'300px',
-      config:[
-        { 
-          type: 'select', 
-          name: 'accent', 
-          width:'300px',
-          placeholder: 'Choose Theme', 
-          options: this.themeOptions,
-          value:this.themeService.activeTheme,
-          tooltip: "Choose which color from the palette will be the theme's accent color",
-          class:'inline'
-        },
-        { 
-          type: 'radio', 
-          name: 'metaphor', 
-          width:'300px',
-          placeholder: 'View Type Preference',
-          options:[{label:'Cards',value:'cards'},{label:'Tables',value:'tables'},{label:'Auto',value:'auto'}],
-          value:'cards',
-          tooltip: 'Choose the view metaphor you prefer.',
-        },
-        { 
-          type: 'checkbox', 
-          name: 'showGuide', 
-          width:'300px',
-          placeholder: 'Enable Guided Tour on Startup',
-          value:true,
-          tooltip: 'Pick a color, any color!',
-          class:'inline'
-        },
-        { 
-          type: 'checkbox', 
-          name: 'showTooltips', 
-          width: '300px',
-          placeholder: 'Enable Helpful Tooltips in Forms',
-          value:true,
-          tooltip: 'Pick a color, any color!',
-          class:'inline'
-        }
-      ]
-    }/*,
-    {
-      name:'Appearance',
-      class:'appearance',
-      width:'calc(100% - 600px)',
-      config:[
+    public fieldSets: FieldSet[] = [
+      {
+        name:'General Preferences',
+        class:'general-prefs',
+        label:true,
+        width:'300px',
+        config:[
           { 
             type: 'select', 
             name: 'accent', 
@@ -109,24 +67,52 @@ export class PreferencesPage implements OnInit, OnChanges {
             value:this.themeService.activeTheme,
             tooltip: "Choose which color from the palette will be the theme's accent color",
             class:'inline'
+          },
+          { 
+            type: 'radio', 
+            name: 'metaphor', 
+            width:'300px',
+            placeholder: 'View Type Preference',
+            options:[{label:'Cards',value:'cards'},{label:'Tables',value:'tables'},{label:'Auto',value:'auto'}],
+            value:'cards',
+            tooltip: 'Choose the view metaphor you prefer.',
+          },
+          { 
+            type: 'checkbox', 
+            name: 'showGuide', 
+            width:'300px',
+            placeholder: 'Enable Guided Tour on Startup',
+            value:true,
+            tooltip: 'Pick a color, any color!',
+            class:'inline'
+          },
+          { 
+            type: 'checkbox', 
+            name: 'showTooltips', 
+            width: '300px',
+            placeholder: 'Enable Helpful Tooltips in Forms',
+            value:true,
+            tooltip: 'Pick a color, any color!',
+            class:'inline'
           }
         ]
-      },
+      }/*,
       {
-      name:'Favorite Themes',
-      class:'favorite-themes',
-      width:'240px',
-      config:this.favoriteFields 
-    }*/
-  ]
+        name:'Manage Themes',
+        class:'theme-manager',
+        width:'calc(100% - 300px)',
+        label:true,
+        config:this.favoriteFields 
+      }*/
+    ]
 
-  custActions: any[] = [
-    {
-      id: 'create-theme-link',
-      name: 'Create Theme',
-      eventName:"CreateTheme"
-    }
-  ]
+    custActions: any[] = [
+      {
+        id: 'create-theme-link',
+        name: 'Create Theme',
+        eventName:"CreateTheme"
+      }
+    ]
 
     constructor(
       protected router: Router, 
@@ -153,13 +139,13 @@ export class PreferencesPage implements OnInit, OnChanges {
       this.loadValues();
       this.target.subscribe((evt:CoreEvent) => {
         switch(evt.name){
-          case "FormSubmitted":
-            console.log("Form Submitted");
-            console.log(evt.data);
-            
+        case "FormSubmitted":
+          console.log("Form Submitted");
+          console.log(evt.data);
+
           break;
-          case "CreateTheme":
-            this.router.navigate(new Array('').concat(['ui-preferences', 'create-theme']));
+        case "CreateTheme":
+          this.router.navigate(new Array('').concat(['ui-preferences', 'create-theme']));
           break;
         }
       });
@@ -167,44 +153,44 @@ export class PreferencesPage implements OnInit, OnChanges {
     }
 
     /*afterInit(entityForm: any) {
-    }*/
+     }*/
 
-    setFavoriteFields(){
-      for(let i = 0; i < this.themeService.freenasThemes.length; i++){
-        let theme = this.themeService.freenasThemes[i];
-        let field = { 
-          type: 'checkbox', 
-          name: theme.name,
-          width: '200px',
-          placeholder:theme.label,
-          value: false,
-          tooltip: 'Add ' + theme.label + ' to your favorites',
-          class:'inline'
-        }
-        this.favoriteFields.push(field);
-      }   
-    }
+     setFavoriteFields(){
+       for(let i = 0; i < this.themeService.freenasThemes.length; i++){
+         let theme = this.themeService.freenasThemes[i];
+         let field = { 
+           type: 'checkbox', 
+           name: theme.name,
+           width: '200px',
+           placeholder:theme.label,
+           value: false,
+           tooltip: 'Add ' + theme.label + ' to your favorites',
+           class:'inline'
+         }
+         this.favoriteFields.push(field);
+       }   
+     }
 
-    setThemeOptions(){
-      for(let i = 0; i < this.themeService.freenasThemes.length; i++){
-        let theme = this.themeService.freenasThemes[i];
-        this.themeOptions.push({label:theme.label, value: theme.name});
-      }
-    }
+     setThemeOptions(){
+       for(let i = 0; i < this.themeService.freenasThemes.length; i++){
+         let theme = this.themeService.freenasThemes[i];
+         this.themeOptions.push({label:theme.label, value: theme.name});
+       }
+     }
 
-    processSubmission(obj:any){
-    }
+     processSubmission(obj:any){
+     }
 
-    loadValues(themeName?:string){
-      
-    }
+     loadValues(themeName?:string){
 
-    generateFieldConfig(){
-      for(let i in this.fieldSets){
-        for(let ii in this.fieldSets[i].config){
-          this.fieldConfig.push(this.fieldSets[i].config[ii]);
-        }
-      }
-    }
+     }
+
+     generateFieldConfig(){
+       for(let i in this.fieldSets){
+         for(let ii in this.fieldSets[i].config){
+           this.fieldConfig.push(this.fieldSets[i].config[ii]);
+         }
+       }
+     }
 
 }
