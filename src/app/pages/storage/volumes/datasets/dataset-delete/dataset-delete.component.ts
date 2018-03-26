4ae12346caae4ff87f1d5fe2f82967a0076f57b3
unknown <peter.southwell1970@gmail.com>
2018-03-26 15:07:20 -0700

    Re-Wrote dataset-delete.component to utilize =entity-form instaed of entity-delete so I cold customize the submit and ask more ten one are you sure question as per ticket feature

--- src/app/pages/storage/volumes/datasets/dataset-delete/dataset-delete.component.ts
+++ src/app/pages/storage/volumes/datasets/dataset-delete/dataset-delete.component.ts
@@ -1,29 +1,52 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import {
   FormGroup,
 } from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Subscription} from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
-import {RestService, WebSocketService} from '../../../../../services/';
+import { RestService, WebSocketService, DialogService } from '../../../../../services/';
+import { Formconfiguration } from '../../../../common/entity/entity-form/entity-form.component';
+import { AppLoaderService } from '../../../../../services/app-loader/app-loader.service';
+import { T } from '../../../../../translate-marker';
+import { FieldConfig } from '../../../../common/entity/entity-form/models/field-config.interface';
 
 @Component({
-  selector : 'app-dataset-delete',
-  template : `<entity-delete [conf]="this"></entity-delete>`
+  selector: 'app-dataset-delete',
+  template: `form-delete [conf]="this"></entity-form>`
 })
-export class DatasetDeleteComponent {
+export class DatasetDeleteComponent implements Formconfiguration {
 
-  protected pk: any;
-  protected path: string;
+  public pk: any;
+  public path: string;
   public sub: Subscription;
   public deleteSnapshot: boolean = true;
-  protected route_success: string[] = [ 'storage', 'volumes' ];
-  get resource_name(): string {
-    return 'storage/volume/' + this.pk + '/datasets/';
-  }
+  public route_success: string[] = ['storage', 'volumes'];
+  public isNew: boolean = true;
+  public isEntity: boolean = false;
+
+  public resource_name = 'storage/volume';
+
+  public fieldConfig: FieldConfig[] = [
+    {
+      type : 'checkbox',
+      name : 'areyousure',
+      placeholder : T("Are you sure you want to delete?"),
+      tooltip : T('Are you sure you want to delete? the data will be lost.'),
+    },
+
+    {
+      type : 'checkbox',
+      name : 'imaware',
+      placeholder : T('Im aware that snapsots within this data set will be deleted.'),
+      tooltip : T('Im aware that snapsots within this data set will be deleted.   This meas they will not be restorable.'),
+    }
+
+  ];
+
 
   constructor(protected router: Router, protected aroute: ActivatedRoute,
-              protected rest: RestService, protected ws: WebSocketService) {}
+    protected rest: RestService, protected ws: WebSocketService, protected dialogService: DialogService, protected loader: AppLoaderService) { }
 
   clean_name(value) {
     let start = this.path.split('/').splice(1).join('/');
@@ -41,5 +64,23 @@ export class DatasetDeleteComponent {
   }
 
   afterInit(entityAdd: any) {
+  }
+
+  customSubmit(body) { 
+    const url = this.resource_name + "/" + this.pk + "/datasets/";
+    this.loader.open();
+
+    this.rest.delete(url, {}).subscribe((res) => {
+      this.loader.close();
+
+      if( body.areyousure === true && body.imaware === true  ) {
+          this.dialogService.Info(T("Detach Volue"), T("Detached Volume:" + this.pk + " successfully"));
+      } else {
+        this.dialogService.Info(T("Action cancled: Detach Volue"), T("Detached Volume was NOT executed:" + this.pk + " action detach was cancled."));
+      }
+    }, (error) => {
+      this.loader.close();
+      this.dialogService.errorReport(T("Error Detaching volume"), error.message, error.stack);
+    });
   }
 }
