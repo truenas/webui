1f9637dac417e4df94ac5662f2c77290f56cff33
Erin Clark <erin@ixsystems.com>
2018-03-28 11:18:03 -0700

    Add missing space in dialog

--- src/app/pages/storage/volumes/volume-delete/volume-delete.component.ts
+++ src/app/pages/storage/volumes/volume-delete/volume-delete.component.ts
@@ -87,7 +87,7 @@ export class VolumeDeleteComponent implements Formconfiguration {
         return this.rest.delete(this.resource_name + "/" + value.name, { body: JSON.stringify({}) }).subscribe((restPostResp) => {
           console.log("restPostResp", restPostResp);
           this.loader.close();
-          this.dialogService.Info(T("Detach/Delete Pool"), T("Successfully deleted pool") + value.name);
+          this.dialogService.Info(T("Detach/Delete Pool"), T("Successfully deleted pool ") + value.name);
     
           this.router.navigate(new Array('/').concat(
             this.route_success));
