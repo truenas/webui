e96b7ab83608c1d1e5daf3a070832d0b806df9e6
Erin Clark <erin@ixsystems.com>
2018-04-17 13:39:41 -0700

    Add lagg members button/crud
    Ticket: #31701

--- src/app/pages/network/laggs/lagg-list/lagg-list.component.ts
+++ src/app/pages/network/laggs/lagg-list/lagg-list.component.ts
@@ -12,7 +12,7 @@ import { T } from '../../../../translate-marker';
 })
 export class LaggListComponent {
 
-  public title = "Link Aggregations";
+  public title = T("Link Aggregations");
   protected resource_name: string = 'network/lagg/';
   protected route_add: string[] = [ 'network', 'laggs', 'add' ];
   protected route_add_tooltip: string = "Add Link Aggregation";
@@ -44,9 +44,17 @@ export class LaggListComponent {
       }
     });
     actions.push({
+      label : T("Edit Members"),
+      onClick : (row) => {
+        this.router.navigate(new Array('').concat([
+          "network", "laggs", row.id, "members"
+        ]));
+      }
+    });
+    actions.push({
       label : T("Delete"),
       onClick : (row) => {
-        this.entityList.doDelete(row.id);;
+        this.entityList.doDelete(row.id);
       },
     });
     return actions;
