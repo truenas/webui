a8ccfdc1dc9834f9925d07941bd3ee2251b43351
Lola Yang <lola.ly27@gmail.com>
2018-04-27 17:03:10 -0700

    enable to let user create/active pool on plugins avaliable ui

--- src/app/pages/plugins/plugins-available/plugins-available-list.component.ts
+++ src/app/pages/plugins/plugins-available/plugins-available-list.component.ts
@@ -7,7 +7,8 @@ import { T } from '../../../translate-marker';
 
 @Component({
   selector: 'app-plugins-available-list',
-  template: `<entity-table [title]="title" [conf]="this"></entity-table>`
+  templateUrl: './plugins-available-list.component.html',
+  // template: `<entity-table [title]="title" [conf]="this"></entity-table>`
 })
 export class PluginsAvailabelListComponent {
 
@@ -26,7 +27,15 @@ export class PluginsAvailabelListComponent {
     sorting: { columns: this.columns },
   };
 
-  constructor(protected router: Router, protected rest: RestService, protected ws: WebSocketService, protected loader: AppLoaderService) {}
+  public isPoolActivated: boolean = true;
+  public selectedPool;
+  public activatedPool: any;
+  public availablePools: any = [];
+
+  constructor(protected router: Router, protected rest: RestService, protected ws: WebSocketService, protected loader: AppLoaderService) {
+    this.getActivatedPool();
+    this.getAvailablePools();
+  }
 
   getActions(parentRow) {
     return [{
@@ -38,5 +47,29 @@ export class PluginsAvailabelListComponent {
         }
       }
     ]
+  }
+
+  getActivatedPool(){
+    this.ws.call('jail.get_activated_pool').subscribe((res)=>{
+      if (res != null) {
+        this.activatedPool = res;
+        this.isPoolActivated = true;
+      } else {
+        this.isPoolActivated = false;
+      }
+    })
+  }
+
+  getAvailablePools(){
+    this.ws.call('pool.query').subscribe( (res)=> {
+      this.availablePools = res;
+    })
+  }
+
+  activatePool(event: Event){
+    this.ws.call('jail.activate', [this.selectedPool.name]).subscribe(
+      (res)=>{
+        this.isPoolActivated = true;
+      });
   }
 }
