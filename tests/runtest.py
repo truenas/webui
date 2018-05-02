37d6441d98b597ed7ca041bb6c6f21f1d7ce8ca2
ericbsd <ericturgeon.bsd@gmail.com>
2018-05-02 17:41:18 -0300

    change the import config code

--- tests/runtest.py
+++ tests/runtest.py
@@ -40,11 +40,14 @@ from theme import run_change_theme_test
 from logout import run_logout_test
 if path.exists("/usr/local/etc/ixautomation.conf"):
     copyfile("/usr/local/etc/ixautomation.conf", "config.py")
-from config import *
-if "Grid_ip" in locals():
-    grid_sever_ip = Grid_ip
+    from config import *
+    if "Grid_ip" in locals():
+        grid_sever_ip = Grid_ip
+    else:
+        grid_sever_ip = "127.0.0.1"
 else:
     grid_sever_ip = "127.0.0.1"
+
 sys.stdout.flush()
 
 argument = sys.argv
@@ -98,24 +101,23 @@ except NameError:
     print(UsageMSG)
     sys.exit(1)
 
+global runDriver
+
 try:
     driver_v
 except NameError:
     from driverG import webDriver
     print("Running Selenium Grid")
-    global runDriver
     runDriver = webDriver(grid_sever_ip)
 
 else:
     if (driver_v == "U"):
         from driverU import webDriver
         print ("Running Ubuntu driver")
-        global runDriver
         runDriver = webDriver()
     elif (driver_v == "G"):
         from driverG import webDriver
         print("Running Selenium Grid")
-        global runDriver
         runDriver = webDriver(grid_sever_ip)
 
 # turning on the autoflush to display result
