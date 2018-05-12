9fd5dfa0f0b2dd8da798a9fc0fadd87f34987467
Lola Yang <lola.ly27@gmail.com>
2018-05-11 17:36:56 -0700

    Enable to export certificate & priviate key

--- src/app/pages/system/certificates/certificate-list/certificate-list.component.ts
+++ src/app/pages/system/certificates/certificate-list/certificate-list.component.ts
@@ -1,10 +1,10 @@
-import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as _ from 'lodash';
-import { Subscription } from 'rxjs';
-import {Observable} from 'rxjs/Observable';
-import { RestService, WebSocketService } from '../../../../services/';
+import { WebSocketService } from '../../../../services/';
 import { T } from '../../../../translate-marker';
+import { MatSnackBar } from '@angular/material';
+import { EntityUtils } from '../../../common/entity/utils';
 
 @Component({
   selector: 'certificate-list',
@@ -21,12 +21,10 @@ export class CertificateListComponent {
   protected route_edit: string[] = ['system', 'certificates', 'edit'];
   protected route_success: string[] = [ 'system', 'certificates' ];
 
-  public busy: Subscription;
-  public sub: Subscription;
+  protected entityList: any;
 
   constructor(protected router: Router, protected aroute: ActivatedRoute,
-    protected rest: RestService, protected ws: WebSocketService,
-    protected _injector: Injector, protected _appRef: ApplicationRef) {
+    protected ws: WebSocketService, public snackBar: MatSnackBar) {
   }
 
   public columns: Array < any > = [
@@ -42,11 +40,84 @@ export class CertificateListComponent {
     sorting: { columns: this.columns },
   }
 
+  afterInit(entityList: any) {
+    this.entityList = entityList;
+  }
+
   dataHandler(entityList: any) {
     for (let i = 0; i < entityList.rows.length; i++) {
       if (_.isObject(entityList.rows[i].issuer)) {
         entityList.rows[i].issuer = entityList.rows[i].issuer.name;
       }
     }
+  }
+
+  isActionVisible(actionId: string, row: any) {
+    if (actionId === 'export_certificate' && row.CSR !== null) {
+      return false;
+    } else if (actionId === 'export_private_key' && row.CSR !== null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  getActions(row) {
+    return [{
+        id: "Edit",
+        label: T("Edit"),
+        onClick: (row) => {
+          // this.router.navigate(new Array('').concat(["system", "ca", "sign", row.id]))
+        }
+      },
+      {
+        id: "export_certificate",
+        label: T("Export Certificate"),
+        onClick: (row) => {
+          this.ws.call('certificate.query', [[["id", "=", row.id]]]).subscribe((res) => {
+            if (res[0]) {
+              this.ws.call('core.download', ['filesystem.get', [res[0].certificate_path], res[0].name + '.crt']).subscribe(
+                (res) => {
+                  this.snackBar.open(T("Redirecting to download. Make sure pop-ups are enabled in the browser."), T("Success"), {
+                    duration: 5000
+                  });
+                  window.open(res[1]);
+                },
+                (res) => {
+                  new EntityUtils().handleError(this, res);
+                }
+              );
+            }
+          })
+        }
+      },
+      {
+        id: "export_private_key",
+        label: T("Export Private Key"),
+        onClick: (row) => {
+          this.ws.call('certificate.query', [[["id", "=", row.id]]]).subscribe((res) => {
+            if (res[0]) {
+              this.ws.call('core.download', ['filesystem.get', [res[0].privatekey_path], res[0].name + '.key']).subscribe(
+                (res) => {
+                  this.snackBar.open(T("Redirecting to download. Make sure pop-ups are enabled in the browser."), T("Success"), {
+                    duration: 5000
+                  });
+                  window.open(res[1]);
+                },
+                (res) => {
+                  new EntityUtils().handleError(this, res);
+                }
+              );
+            }
+          })
+        }
+      },
+      {
+        id: "delete",
+        label: T("Delete"),
+        onClick: (row) => {
+          this.entityList.doDelete(row.id);
+        }
+      }];
   }
 }
