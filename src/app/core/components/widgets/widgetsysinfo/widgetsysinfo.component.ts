8190d98aced9c6fd13c6fca05830b2874105f687
Damian Szidiropulosz <damianos@knoxbug.org>
2018-05-14 18:01:45 -0400

    Removed all comments from stats service and adjusted the conditional in the widgetsysinfo template file concerning the manufacturer property.

--- src/app/core/components/widgets/widgetsysinfo/widgetsysinfo.component.ts
+++ src/app/core/components/widgets/widgetsysinfo/widgetsysinfo.component.ts
@@ -75,7 +75,7 @@ export class WidgetSysInfoComponent extends WidgetComponent implements OnInit, A
     //console.log(this.el.nativeElement.children);
     setTimeout(()=>{
       this.core.emit({name:"AnimateColorLoopStart", data:{element:'#widget-sysinfo-logo-bg', colors:this.themeAccentColors}});
-    }, 1000);
+    }, 3000);
   }
 
   getCardBg(){
