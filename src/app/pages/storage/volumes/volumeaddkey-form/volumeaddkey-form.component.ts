321ccdc6a10b67ceaca6116c8c1a86417eb40540
Erin Clark <erin@ixsystems.com>
2018-05-17 15:31:47 -0700

    Make the file downloaded be named geli_recovery like in the old ui
    Ticket: #33378

--- src/app/pages/storage/volumes/volumeaddkey-form/volumeaddkey-form.component.ts
+++ src/app/pages/storage/volumes/volumeaddkey-form/volumeaddkey-form.component.ts
@@ -90,7 +90,7 @@ export class VolumeAddkeyFormComponent implements Formconfiguration {
         this.rest.post(this.resource_name + "/" + value.name + "/recoverykey/", {}).subscribe((restPostResp) => {
           this.loader.close();
           this.snackBar.open(T("Successfully added recovery key to pool ") + value.name, 'close', { duration: 5000 });
-          this.storage.downloadFile('geli.key', restPostResp.data.content, 'application/octet-stream');
+          this.storage.downloadFile('geli_recovery.key', restPostResp.data.content, 'application/octet-stream');
           this.router.navigate(new Array('/').concat(
             this.route_success));
         }, (res) => {
