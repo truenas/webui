7531e8ce11f84346e9812d0328a83f3968a12c96
Damian Szidiropulosz <damianos@knoxbug.org>
2018-05-21 15:35:03 -0400

    WIP: updated scheduler popup preview and months/daysofweek checkboxes are functional

--- src/app/pages/common/entity/entity-form/components/form-scheduler/form-scheduler.component.ts
+++ src/app/pages/common/entity/entity-form/components/form-scheduler/form-scheduler.component.ts
@@ -27,6 +27,91 @@ export class FormSchedulerComponent implements Field,OnInit, AfterViewInit,OnCha
 
   @ViewChild('calendar') calendar: ElementRef;
 
+  private _minutes:string = "0";
+  private _hours:string = "*";
+  private _days:string = "*";
+
+  private _jan:boolean;
+  private _feb:boolean;
+  private _mar:boolean;
+  private _apr:boolean;
+  private _may:boolean;
+  private _jun:boolean;
+  private _jul:boolean;
+  private _aug:boolean;
+  private _sep:boolean;
+  private _oct:boolean;
+  private _nov:boolean;
+  private _dec:boolean;
+
+  private _sun:boolean = false;
+  private _mon:boolean = false;
+  private _tue:boolean = false;
+  private _wed:boolean = false;
+  private _thu:boolean = false;
+  private _fri:boolean = false;
+  private _sat:boolean = false;
+
+  //private _monthsValues: boolean[] = [];
+  private _months:string = "*";
+  //private _daysOfWeekValues: boolean[] = [];
+  private _daysOfWeek:string = "*";
+
+  get minutes(){ return this._minutes}
+  set minutes(val){ this._minutes = val; this.updateCronTab()}
+
+  get hours(){ return this._hours}
+  set hours(val){ this._hours = val; this.updateCronTab()}
+
+  get days(){ return this._days}
+  set days(val){ this._days = val; this.updateCronTab()}
+
+  get jan(){ return this._jan}
+  set jan(val){this._jan = val; this.formatMonths();}
+  get feb(){ return this._feb}
+  set feb(val){this._feb = val; this.formatMonths();}
+  get mar(){ return this._mar}
+  set mar(val){this._mar = val; this.formatMonths();}
+  get apr(){ return this._apr}
+  set apr(val){this._apr = val; this.formatMonths();}
+  get may(){ return this._may}
+  set may(val){this._may = val; this.formatMonths();}
+  get jun(){ return this._jun}
+  set jun(val){this._jun = val; this.formatMonths();}
+  get jul(){ return this._jul}
+  set jul(val){this._jul = val; this.formatMonths();}
+  get aug(){ return this._aug}
+  set aug(val){this._aug = val; this.formatMonths();}
+  get sep(){ return this._sep}
+  set sep(val){this._sep = val; this.formatMonths();}
+  get oct(){ return this._oct}
+  set oct(val){this._oct = val; this.formatMonths();}
+  get nov(){ return this._nov}
+  set nov(val){this._nov = val; this.formatMonths();}
+  get dec(){ return this._dec}
+  set dec(val){this._dec = val; this.formatMonths();}
+
+  get sun(){ return this._sun}
+  set sun(val){ this._sun = val; this.formatDaysOfWeek()}
+  get mon(){ return this._mon}
+  set mon(val){ this._mon = val; this.formatDaysOfWeek()}
+  get tue(){ return this._tue}
+  set tue(val){ this._tue = val; this.formatDaysOfWeek()}
+  get wed(){ return this._wed}
+  set wed(val){ this._wed = val; this.formatDaysOfWeek()}
+  get thu(){ return this._thu}
+  set thu(val){ this._thu = val; this.formatDaysOfWeek()}
+  get fri(){ return this._fri}
+  set fri(val){ this._fri = val; this.formatDaysOfWeek()}
+  get sat(){ return this._sat}
+  set sat(val){ this._sat = val; this.formatDaysOfWeek()}
+
+  /*get minutes(){ return minutes}
+  set minutes(val){ this._minutes = val; this.updateCronTab()}
+
+  get minutes(){ return minutes}
+  set minutes(val){ this._minutes = val; this.updateCronTab()}*/
+
   public minDate;
   public maxDate;
   public activeDate;
@@ -36,8 +121,8 @@ export class FormSchedulerComponent implements Field,OnInit, AfterViewInit,OnCha
   fieldShow: string;
   public picker:boolean = false;
   private _textInput:string = '';
-  public crontab:string = "* * * * * *";
-  public preset:CronPreset = { label:"Custom", value:"* * * * * *"};
+  public crontab:string = "0 0 * * * *";
+  private _preset:CronPreset;// = { label:"Custom", value:"* * * * * *"};
   private presets: CronPreset[] = [
     {
       label: "Hourly",
@@ -49,16 +134,16 @@ export class FormSchedulerComponent implements Field,OnInit, AfterViewInit,OnCha
     },
     {
       label: "Weekly",
-      value: "0 0 0 * * 1"
+      value: "0 0 0 * * sun"
     },
     {
       label: "Monthly",
       value: "0 0 0 1 * *"
-    },
+    }/*,
     {
       label:"Custom",
       value: "custom"
-    }
+    }*/
   ];
 
   get textInput(){
@@ -79,9 +164,34 @@ export class FormSchedulerComponent implements Field,OnInit, AfterViewInit,OnCha
     this.group.controls[this.config.name].setValue(val);;
   }
 
+  get preset(){
+    return this._preset;
+  }
+
+  set preset(p){
+    if(p.value == "custom"){
+      this.crontab = "0 0 0 * * *";
+      this.convertPreset("0 0 0 * * *");
+      this._preset = {label:"Custom", value:this.crontab};
+    } else {
+      this.crontab = p.value;
+      //this.updateCronTab(p.value);
+      this.convertPreset(p.value);
+      this._preset = p;
+    }
+    
+    if(this.minDate && this.maxDate){
+      this.generateSchedule(this.minDate, this.maxDate);
+    }
+    console.log("NEW SELECTION!")
+    console.log(this._preset);
+  }
+
   constructor(){
     //Set default value
-    //this.preset = this.presets[1];
+    this.preset = this.presets[1];
+    //for(let i = 0; i<12;i++){this._monthsValues.push(true)}
+    this._months = "*";
 
     let min = new Date('Tue 1 May 2018 00:00:00 UTC');
     //let min = Date.now();
@@ -104,6 +214,7 @@ export class FormSchedulerComponent implements Field,OnInit, AfterViewInit,OnCha
     this.generateSchedule(this.minDate, this.maxDate);
   }
 
+  // OLD COLORPICKER STUFF ////////////////////////////////////////////////////////
   cpListener(evt:string, data: any): void {
     this.group.value[this.config.name] = data;
   }
@@ -120,26 +231,29 @@ export class FormSchedulerComponent implements Field,OnInit, AfterViewInit,OnCha
   public togglePicker(){
     this.picker = !this.picker;
   }
+  // END OLD COLORPICKER STUFF ////////////////////////////////////////////////////////
 
   private generateSchedule(min, max){
+    let newSchedule = [];
     let options = {
       currentDate: min,
       endDate: max,
       iterator: true
     };
-    this.crontab = "0 5 4 */5 * *";
+    //this.crontab = "0 5 4 */5 * *";
     let interval = parser.parseExpression(this.crontab, options);
 
     while (true) {
       try {
         let obj:any = interval.next();
-        console.log(obj);
-        this.generatedSchedule.push(obj.value);
-        console.log('value:', obj.value.toString(), 'done:', obj.done);
+        //console.log(obj);
+        newSchedule.push(obj.value);
+        //console.log('value:', obj.value.toString(), 'done:', obj.done);
       } catch (e) {
         break;
       }
     }
+    this.generatedSchedule = newSchedule;
     this.updateCalendar();
   }
 
@@ -181,4 +295,181 @@ export class FormSchedulerComponent implements Field,OnInit, AfterViewInit,OnCha
     }
   }
 
+  formatMonths(){ 
+    console.log("****** formatMonths");
+    let months = [this._jan, this._feb, this._mar, this._apr, this._may, this._jun, this._jul, this._aug, this._sep, this._oct, this._nov, this._dec];
+    let months_str = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"];
+    let rule = "";
+    for(let i = 0; i < months.length; i++){
+      console.log(months[i]);
+      if(months[i]){
+        if(rule.length > 0 && i > 0){ rule += ","}
+        rule +=  months_str[i];
+      }
+    }
+    if(rule.length == 0){
+      rule = "*";
+    }
+    console.warn(rule);
+    this._months = rule;
+    this.updateCronTab();
+  }
+
+  formatDaysOfWeek(){
+    console.log("****** formatDaysOfWeek");
+    let dow = [this._sun, this._mon, this._tue, this._wed, this._thu, this._fri, this._sat];
+    let dow_str = ["sun","mon","tue","wed","thu","fri","sat"];
+    let rule = "";
+    for(let i = 0; i < dow.length; i++){
+      console.log(dow[i]);
+      if(dow[i]){
+        if(rule.length > 0 && i > 0){ rule += ","}
+        rule +=  dow_str[i];
+      }
+    }
+    if(rule.length == 0){
+      rule = "*";
+    }
+    console.warn(rule);
+    this._daysOfWeek = rule;
+    this.updateCronTab();
+  }
+
+  updateMonthsFields(rule){
+    // Wild card
+   if(rule == "*"){
+    this._jan = false;
+    this._feb = false;
+    this._mar = false;
+    this._apr = false;
+    this._may = false;
+    this._jun = false;
+    this._jul = false;
+    this._aug = false;
+    this._sep = false;
+    this._oct = false;
+    this._nov = false;
+    this._dec = false;
+    return;
+   }
+
+   // Assume a list and process it
+   let list = rule.split(",");
+   for(let i in list){
+      switch(list[i]){
+        case "jan":
+          this._jan = true;
+        break;
+        case "feb":
+          this._feb = true;
+        break;
+        case "mar":
+          this._mar = true
+        break;
+        case "apr":
+          this._apr = true;
+        break;
+        case "may":
+          this._may = true;
+        break;
+        case "jun":
+          this._jun = true;
+        break;
+        case "jul":
+          this._jul = true;
+        break;
+        case "aug":
+          this._aug = true
+        break;
+        case "sep":
+          this._sep = true;
+        break;
+        case "oct":
+          this._oct = true;
+        break;
+        case "nov":
+          this._nov = true;
+        break;
+        case "dec":
+          this._dec = true;
+        break;
+      }
+    }
+   }  
+
+  updateDaysOfWeekFields(rule){
+    // Wild card
+   if(rule == "*"){
+    this._sun = false;
+    this._mon = false;
+    this._tue = false;
+    this._wed = false;
+    this._thu = false;
+    this._fri = false;
+    this._sat = false;
+
+    return;
+   }
+
+   // Assume a list and process it
+   let list = rule.split(",");
+   for(let i in list){
+      switch(list[i]){
+        case "sun":
+          this._sun = true;
+        break;
+        case "mon":
+          this._mon = true;
+        break;
+        case "tue":
+          this._tue = true
+        break;
+        case "wed":
+          this._wed = true;
+        break;
+        case "thu":
+          this._thu = true;
+        break;
+        case "fri":
+          this._fri = true;
+        break;
+        case "sat":
+          this._sat = true;
+        break;
+      }
+    }
+   }  
+
+  updateCronTab(preset?){
+    this.crontab = "";
+    console.log("UPDATING CRONTAB ...");
+    if(!preset){
+      let result = "0" + " " + this.minutes + " " + this.hours + " " + this.days + " " + this._months + " " + this._daysOfWeek;
+      console.log(result);
+      this.crontab = result;
+    }
+    if(this.minDate && this.maxDate){
+      this.generateSchedule(this.minDate, this.maxDate);
+    }
+  }
+
+  convertPreset(value){
+    //this.days = "*";
+    console.log("CONVERTING PRESET");
+    console.log(value);
+
+    let arr = value.split(" ");
+    console.log(arr);
+    this._minutes = arr[1];
+    this._hours = arr[2];
+    this._days = arr[3];
+
+    // Months
+    this.updateMonthsFields(arr[4]);
+    this._months = arr[4];
+
+    // Days of Week
+    this.updateDaysOfWeekFields(arr[5]);
+    this._daysOfWeek = arr[5];
+  }
 }
