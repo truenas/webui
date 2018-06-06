cff0fe9e01ac52e1d535be9270d4b7129bc0ca0c
Dennis Mullen <dkmullen@gmail.com>
2018-06-06 09:37:13 -0400

    Update dialog config interface Ticket: #31707

--- src/app/pages/common/entity/entity-dialog/dialog-form-configuration.interface.ts
+++ src/app/pages/common/entity/entity-dialog/dialog-form-configuration.interface.ts
@@ -7,5 +7,7 @@ export interface DialogFormConfiguration {
   method_ws?: string,
   saveButtonText?: string,
   cancelButtonText?: string,
-  customSubmit?: any;
+  custActions?: any[],
+  customSubmit?: any,
+  isCustActionVisible?: any
 }
