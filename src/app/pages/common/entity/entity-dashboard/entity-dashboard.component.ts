08633e346ad93b9b613d6ffdae79eaa47807f60d
Lola Yang <lola.ly27@gmail.com>
2018-06-07 14:17:20 -0700

    added entity-dashboard component

---
+++ src/app/pages/common/entity/entity-dashboard/entity-dashboard.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as _ from 'lodash';
+
+@Component ({
+	selector: 'entity-dashboard',
+	templateUrl: './entity-dashboard.component.html',
+})
+export class EntityDashboardComponent implements OnInit {
+
+	public routeParts: any = [];
+	protected parent: string = "";
+	constructor(protected router: Router,
+				protected aroute: ActivatedRoute){
+
+	}
+
+	ngOnInit() {
+		this.parent = this.aroute.parent.parent.routeConfig.path;
+		let routeConfigs = this.aroute.parent.routeConfig.children;
+		for (let i in routeConfigs) {
+			if (routeConfigs[i].path !== "") {
+				this.routeParts.push(routeConfigs[i]);
+			}
+		}
+	}
+
+	goList(item) {
+		this.router.navigate(new Array('/').concat([this.parent, item.path]));
+	}
+
+	goAdd(item) {
+		this.router.navigate(new Array('/').concat([this.parent, item.path, "add"]));
+	}
+}
