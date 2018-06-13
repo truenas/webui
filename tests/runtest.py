a74cb2ad78195360309b2c27021c515095223d72
rishabh27892 <rishabh.chauhan27892@gmail.com>
2018-06-13 10:13:44 -0400

    funtion.py added to contain all the functions(not yet changes made in the whole script yet) lldp and ssh tests turned on

--- tests/runtest.py
+++ tests/runtest.py
@@ -136,8 +136,8 @@ except NameError:
     run_conf_ftp_test(runDriver)
     run_conf_iscsi_test(runDriver)
 # temporary shutdown
-#    run_conf_lldp_test(runDriver)
-#    run_conf_ssh_test(runDriver)
+    run_conf_lldp_test(runDriver)
+    run_conf_ssh_test(runDriver)
     run_conf_webdav_test(runDriver)
     run_view_guide_test(runDriver)
     run_edit_test(runDriver)
