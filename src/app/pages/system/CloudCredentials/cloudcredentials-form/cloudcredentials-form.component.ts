9cca50c9d4a6a8c5ef99dc1e76356028d27b116f
Lola Yang <lola.ly27@gmail.com>
2018-06-14 15:21:59 -0700

    added field config for all providers

--- src/app/pages/system/CloudCredentials/cloudcredentials-form/cloudcredentials-form.component.ts
+++ src/app/pages/system/CloudCredentials/cloudcredentials-form/cloudcredentials-form.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import * as _ from 'lodash';
-import { WebSocketService } from '../../../../services/';
+import { WebSocketService, CloudCredentialService } from '../../../../services/';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 import { T } from '../../../../translate-marker';
 
 @Component({
   selector: 'app-cloudcredentials-form',
-  template: `<entity-form [conf]="this"></entity-form>`
+  template: `<entity-form [conf]="this"></entity-form>`,
+  providers: [ CloudCredentialService ],
 })
 export class CloudCredentialsFormComponent {
 
@@ -34,142 +35,249 @@ export class CloudCredentialsFormComponent {
       type: 'select',
       name: 'provider',
       placeholder: T('Provider'),
-      options: [{
-        label: 'Amazon AWS',
-        value: 'AMAZON',
-      }, {
-        label: 'Microsoft Azure',
-        value: 'AZURE',
-      }, {
-        label: 'Backblaze B2',
-        value: 'BACKBLAZE',
-      }, {
-        label: 'Google Cloud',
-        value: 'GCLOUD',
-      }],
+      options: [],
       value: 'AMAZON',
       required: true,
       validation: [Validators.required],
     },
+    // Amazon_cloud_drive
     {
-      type: 'textarea',
-      name: 'access_key',
-      placeholder: T('Access Key'),
-      tooltip: T('Paste the Amazon account access key. This is found\
-                  on <a href="https://aws.amazon.com/"\
-                  target="_blank">Amazon AWS</a> by navigating <b>My\
-                  account -> Security Credentials -> Access Keys\
-                  (Access Key ID and Secret Access Key)</b>.'),
+      type: 'input',
+      name: 'client_id',
+      placeholder: T('Amazon Application Client ID'),
       required: true,
-      validation: [Validators.required],
-      isHidden: false,
     },
     {
-      type: 'textarea',
-      name: 'secret_key',
-      placeholder: T('Secret Key'),
-      tooltip: T('Enter or paste the saved <b>Secret Key</b>. Create a\
-                  new key pair on the same AWS screen where the\
-                  <b>Access Key</b> is generated if the Secret Key\
-                  cannot be found or remembered.'),
+      type: 'input',
+      name: 'client_secret',
+      placeholder: T('Application Key'),
       required: true,
-      validation: [Validators.required],
-      isHidden: false,
+    },
+    // Amazon_s3
+    {
+      type: 'input',
+      name: 'access_key_id',
+      placeholder: T('Access Key ID'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'secret_access_key',
+      placeholder: T('Secret Access Key'),
+      required: true,
     },
     {
       type: 'input',
       name: 'endpoint',
       placeholder: T('Endpoint URL'),
-      tooltip: T('Enter the entry point URL for the web service.'),
-      isHidden: false,
     },
+    // backblaze b2
     {
-      type : 'input',
-      name : 'account_name',
-      placeholder : T('Account Name'),
-      tooltip : T('Enter the <a\
-                   href="https://docs.microsoft.com/en-us/azure/storage/"\
-                   target="_blank">Azure Storage</a> account name.'),
+      type: 'input',
+      name: 'account',
+      placeholder: T('Account ID'),
       required: true,
-      validation : [ Validators.required ],
-      isHidden: true,
     },
     {
-      type : 'textarea',
-      name : 'account_key',
-      placeholder :  T('Account Key'),
-      tooltip : T('Enter or paste the <a\
-                   href="https://docs.microsoft.com/en-us/azure/storage/"\
-                   target="_blank">Azure Storage</a> account key.'),
-       required: true,
-       validation : [ Validators.required ],
-       isHidden: true,
-    },
-    {
-      type : 'textarea',
-      name : 'account_id',
-      placeholder :  T('Account ID'),
-      tooltip : T('Enter or paste the Accound ID. See the <a\
-                   href="https://www.backblaze.com/help.html"\
-                   target="_blank">BACKBLAZE help page</a> for more\
-                   information.'),
-       required: true,
-       validation : [ Validators.required ],
-       isHidden: true,
-    },
-    {
-      type : 'textarea',
-      name : 'app_key',
-      placeholder : T('Application Key'),
-      tooltip : T('Enter the application key for your <b>Account\
-                   ID</b>.'),
+      type: 'input',
+      name: 'key',
+      placeholder: T('Application Key'),
       required: true,
-      validation : [ Validators.required ],
-      isHidden: true,
+    },
+    // box
+    {
+      type: 'input',
+      name: 'token',
+      placeholder: T('Access Token'),
+      required: true,
+    },
+    // dropbox
+    {
+      type: 'input',
+      name: 'token',
+      placeholder: T('Access Token'),
+      required: true,
+    },
+    // ftp
+    {
+      type: 'input',
+      name: 'host',
+      placeholder: T('Host'),
+      required: true,
     },
     {
-      type : 'textarea',
-      name : 'preview',
-      placeholder : T('Preview JSON Service Account Key'),
-      tooltip: T('Read-only display of the selected Key file.'),
-      disabled: true,
-      isHidden: true,
-      readonly: true,
+      type: 'input',
+      name: 'port',
+      placeholder: T('Port'),
     },
     {
-      type : 'readfile',
-      name : 'keyfile',
-      placeholder : T('JSON Service Account Key'),
-      tooltip : T('Click <i>Browse</i> to select a saved <a\
-                   href="https://cloud.google.com/storage/docs/"\
-                   target="_blank">Google Cloud Storage</a> key.'),
-      validation : [ Validators.required ],
-      isHidden: true,
+      type: 'input',
+      name: 'user',
+      placeholder: T('Username'),
+      required: true,
     },
-  ];
-  protected amazonFields: Array<any> = [
-    'access_key',
-    'secret_key',
-    'endpoint',
-  ];
-  protected azureFields: Array<any> = [
-    'account_name',
-    'account_key',
-  ];
-  protected balckblazeFields: Array<any> = [
-    'account_id',
-    'app_key',
-  ];
-  protected gcloudFields: Array<any> = [
-    'preview',
-    'keyfile',
+    {
+      type: 'input',
+      name: 'pass',
+      placeholder: T('Password'),
+      required: true,
+    },
+    // google cloud storage
+    {
+      type: 'input',
+      name: 'service_account_credentials',
+      placeholder: T('Service Account'),
+      required: true,
+    },
+    // google drive
+    {
+      type: 'input',
+      name: 'token',
+      placeholder: T('Access Token'),
+      required: true,
+    },
+    // http
+    {
+      type: 'input',
+      name: 'url',
+      placeholder: T('URL'),
+      required: true,
+    },
+    // hubic
+    {
+      type: 'input',
+      name: 'token',
+      placeholder: T('Access Token'),
+      required: true,
+    },
+    // mega
+    {
+      type: 'input',
+      name: 'user',
+      placeholder: T('Username'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'pass',
+      placeholder: T('Password'),
+      required: true,
+    },
+    // microsoft azure
+    {
+      type: 'input',
+      name: 'account',
+      placeholder: T('Account Name'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'key',
+      placeholder: T('Account Key'),
+      required: true,
+    },
+    // microsoft onedrive
+    {
+      type: 'input',
+      name: 'token',
+      placeholder: T('Access Token'),
+      required: true,
+    },
+    // pcloud
+    {
+      type: 'input',
+      name: 'token',
+      placeholder: T('Access Token'),
+      required: true,
+    },
+    // sftp
+    {
+      type: 'input',
+      name: 'host',
+      placeholder: T('Host'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'port',
+      placeholder: T('Port'),
+    },
+    {
+      type: 'input',
+      name: 'user',
+      placeholder: T('Username'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'pass',
+      placeholder: T('Password'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'key_file',
+      placeholder: T('PEM-encoded private key file path'),
+      required: true,
+    },
+    // webdav
+    {
+      type: 'input',
+      name: 'url',
+      placeholder: T('URL'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'vendor',
+      placeholder: T('Name of the WebDAV site/service/software'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'user',
+      placeholder: T('Username'),
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'pass',
+      placeholder: T('Password'),
+      required: true,
+    },
+    // yandex
+    {
+      type: 'input',
+      name: 'token',
+      placeholder: T('Access Token'),
+      required: true,
+    },
   ];
 
   protected selectedProvider: string = 'AMAZON';
+  protected providers: Array<any>;
+  protected providerField: any;
 
   constructor(protected router: Router,
               protected aroute: ActivatedRoute,
-              protected ws: WebSocketService) {}
+              protected ws: WebSocketService,
+              protected cloudcredentialService: CloudCredentialService) {
+    this.providerField = _.find(this.fieldConfig, {'name': 'provider'});
+    this.cloudcredentialService.getProviders().subscribe(
+      (res) => {
+        this.providers = res;
+        console.log(this.providers);
+        for (let i in res) {
+          this.providerField.options.push(
+            {
+              label: res[i].title,
+              value: res[i].name,
+            }
+          );
+        }
+      }
+    );
+  }
 
   preInit() {
     this.aroute.params.subscribe(params => {
@@ -180,89 +288,89 @@ export class CloudCredentialsFormComponent {
     });
   }
 
-  afterInit(entityForm: any) {
-    entityForm.submitFunction = this.submitFunction;
+  // afterInit(entityForm: any) {
+  //   entityForm.submitFunction = this.submitFunction;
 
-    for (let i in this.azureFields) {
-      this.hideField(this.azureFields[i], true, entityForm);
-    }
-    for (let i in this.balckblazeFields) {
-      this.hideField(this.balckblazeFields[i], true, entityForm);
-    }
-    for (let i in this.gcloudFields) {
-      this.hideField(this.gcloudFields[i], true, entityForm);
-    }
-    for (let i in this.amazonFields) {
-      this.hideField(this.amazonFields[i], false, entityForm);
-    }
+  //   for (let i in this.azureFields) {
+  //     this.hideField(this.azureFields[i], true, entityForm);
+  //   }
+  //   for (let i in this.balckblazeFields) {
+  //     this.hideField(this.balckblazeFields[i], true, entityForm);
+  //   }
+  //   for (let i in this.gcloudFields) {
+  //     this.hideField(this.gcloudFields[i], true, entityForm);
+  //   }
+  //   for (let i in this.amazonFields) {
+  //     this.hideField(this.amazonFields[i], false, entityForm);
+  //   }
 
-    entityForm.formGroup.controls['provider'].valueChanges.subscribe((res) => {
-      this.selectedProvider = res;
-      if (res == 'AMAZON') {
-        for (let i in this.azureFields) {
-          this.hideField(this.azureFields[i], true, entityForm);
-        }
-        for (let i in this.balckblazeFields) {
-          this.hideField(this.balckblazeFields[i], true, entityForm);
-        }
-        for (let i in this.gcloudFields) {
-          this.hideField(this.gcloudFields[i], true, entityForm);
-        }
-        for (let i in this.amazonFields) {
-          this.hideField(this.amazonFields[i], false, entityForm);
-        }
-      } else if (res == 'AZURE') {
-        for (let i in this.amazonFields) {
-          this.hideField(this.amazonFields[i], true, entityForm);
-        }
-        for (let i in this.balckblazeFields) {
-          this.hideField(this.balckblazeFields[i], true, entityForm);
-        }
-        for (let i in this.gcloudFields) {
-          this.hideField(this.gcloudFields[i], true, entityForm);
-        }
-        for (let i in this.azureFields) {
-          this.hideField(this.azureFields[i], false, entityForm);
-        }
-      } else if (res == 'BACKBLAZE') {
-        for (let i in this.amazonFields) {
-          this.hideField(this.amazonFields[i], true, entityForm);
-        }
-        for (let i in this.gcloudFields) {
-          this.hideField(this.gcloudFields[i], true, entityForm);
-        }
-        for (let i in this.azureFields) {
-          this.hideField(this.azureFields[i], true, entityForm);
-        }
-        for (let i in this.balckblazeFields) {
-          this.hideField(this.balckblazeFields[i], false, entityForm);
-        }
-      } else if (res == 'GCLOUD') {
-        for (let i in this.amazonFields) {
-          this.hideField(this.amazonFields[i], true, entityForm);
-        }
-        for (let i in this.azureFields) {
-          this.hideField(this.azureFields[i], true, entityForm);
-        }
-        for (let i in this.balckblazeFields) {
-          this.hideField(this.balckblazeFields[i], true, entityForm);
-        }
-        for (let i in this.gcloudFields) {
-          this.hideField(this.gcloudFields[i], false, entityForm);
-        }
-      }
-    });
+  //   entityForm.formGroup.controls['provider'].valueChanges.subscribe((res) => {
+  //     this.selectedProvider = res;
+  //     if (res == 'AMAZON') {
+  //       for (let i in this.azureFields) {
+  //         this.hideField(this.azureFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.balckblazeFields) {
+  //         this.hideField(this.balckblazeFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.gcloudFields) {
+  //         this.hideField(this.gcloudFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.amazonFields) {
+  //         this.hideField(this.amazonFields[i], false, entityForm);
+  //       }
+  //     } else if (res == 'AZURE') {
+  //       for (let i in this.amazonFields) {
+  //         this.hideField(this.amazonFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.balckblazeFields) {
+  //         this.hideField(this.balckblazeFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.gcloudFields) {
+  //         this.hideField(this.gcloudFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.azureFields) {
+  //         this.hideField(this.azureFields[i], false, entityForm);
+  //       }
+  //     } else if (res == 'BACKBLAZE') {
+  //       for (let i in this.amazonFields) {
+  //         this.hideField(this.amazonFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.gcloudFields) {
+  //         this.hideField(this.gcloudFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.azureFields) {
+  //         this.hideField(this.azureFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.balckblazeFields) {
+  //         this.hideField(this.balckblazeFields[i], false, entityForm);
+  //       }
+  //     } else if (res == 'GCLOUD') {
+  //       for (let i in this.amazonFields) {
+  //         this.hideField(this.amazonFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.azureFields) {
+  //         this.hideField(this.azureFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.balckblazeFields) {
+  //         this.hideField(this.balckblazeFields[i], true, entityForm);
+  //       }
+  //       for (let i in this.gcloudFields) {
+  //         this.hideField(this.gcloudFields[i], false, entityForm);
+  //       }
+  //     }
+  //   });
 
-    entityForm.formGroup.controls['keyfile'].valueChanges.subscribe((value)=>{
-      entityForm.formGroup.controls['preview'].setValue(value);
-    });
-  }
+  //   entityForm.formGroup.controls['keyfile'].valueChanges.subscribe((value)=>{
+  //     entityForm.formGroup.controls['preview'].setValue(value);
+  //   });
+  // }
 
-  hideField(fieldName: any, show: boolean, entity: any) {
-    let target = _.find(this.fieldConfig, {'name' : fieldName});
-    target.isHidden = show;
-    entity.setDisabled(fieldName, show);
-  }
+  // hideField(fieldName: any, show: boolean, entity: any) {
+  //   let target = _.find(this.fieldConfig, {'name' : fieldName});
+  //   target.isHidden = show;
+  //   entity.setDisabled(fieldName, show);
+  // }
 
   submitFunction() {
     const attributes = {};
