2dbd4a6017de809e7f37edead6154c9855eb8daf
Erin Clark <erin@ixsystems.com>
2018-06-22 18:15:55 -0700

    Fix "detach" pools dialog closing on successful pool deletion
    Ticket: #35323

--- src/app/pages/storage/volumes/volumes-list/volumes-list.component.ts
+++ src/app/pages/storage/volumes/volumes-list/volumes-list.component.ts
@@ -272,7 +272,7 @@ export class VolumesListTableConfig implements InputTableConf {
             localDialogService = this.dialogService
 
           const conf: DialogFormConfiguration = { 
-            title: "Detatch pool: '" + row1.name + "'",
+            title: "Detach pool: '" + row1.name + "'",
             fieldConfig: [{
               type: 'paragraph',
               name: 'pool_detach_warning',
@@ -334,6 +334,7 @@ export class VolumesListTableConfig implements InputTableConf {
               } else {
                 return localRest.delete("storage/volume/" + row1.name, { body: JSON.stringify({}) 
                   }).subscribe((res) => {
+                    entityDialog.dialogRef.close(true);
                     localLoader.close();
                     localDialogService.Info(T("Detach Pool"), T("Successfully detached pool: '") + row1.name + 
                       T("'. All data on that pool was destroyed."));
