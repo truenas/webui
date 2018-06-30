adfded07d731b5aef34304dc289ce0aa6880715c
vaibhav-rbs <vaibhav.rbs@gmail.com>
2018-06-29 20:20:06 -0400

    implemted fix for #35398

--- src/app/pages/storage/volumes/zvol/zvol-form/zvol-form.component.ts
+++ src/app/pages/storage/volumes/zvol/zvol-form/zvol-form.component.ts
@@ -378,7 +378,22 @@ export class ZvolFormComponent {
     if (data.deduplication === 'INHERIT') {
       delete(data.deduplication);
     }
-    if (data.volblocksize === 'INHERIT') {
+    if (data.volblocksize !== 'INHERIT') {
+      const volblocksize_integer_value = data.volblocksize.match(/[a-zA-Z]+|[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)+/g)[0];
+      volblocksize_integer_value = parseInt(volblocksize_integer_value,10)
+      
+  
+      if (volblocksize_integer_value === 512){
+        volblocksize_integer_value = 512
+      } else {
+        volblocksize_integer_value = volblocksize_integer_value * 1024
+      }
+
+      data.volsize = Math.round(data.volsize);
+      data.volsize = data.volsize + (volblocksize_integer_value - data.volsize%volblocksize_integer_value)
+
+       
+    } else{
       delete(data.volblocksize);
     }
 
