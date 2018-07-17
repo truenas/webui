b54d09e2bef517e328ec520dacbdc7983c1f15e3
Lola Yang <lola.ly27@gmail.com>
2018-07-16 18:54:24 -0700

    added actions for row item

--- src/app/pages/storage/disks/disk-list/disk-list.component.ts
+++ src/app/pages/storage/disks/disk-list/disk-list.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { Observable, Subject, Subscription } from 'rxjs/Rx';
+import { Router } from '@angular/router';
 
-import { WebSocketService, StorageService } from '../../../../services';
+import { WebSocketService } from '../../../../services';
 import { T } from '../../../../translate-marker';
 import * as _ from 'lodash';
 
 @Component ({
 	selector: 'disk-list',
 	template: `<entity-table [title]="title" [conf]="this"></entity-table>`,
-	providers: [StorageService]
 })
 export class DiskListComponent {
 	public title = T("Disks");
@@ -34,31 +33,57 @@ export class DiskListComponent {
 	};
 
 	protected disk_pool: Map<string, string> = new Map<string, string>();
-	constructor(protected ws: WebSocketService, protected storageService: StorageService) {}
-
-	dataHandler(entityList: any) {
+	constructor(protected ws: WebSocketService, protected router: Router) {
 		this.ws.call('boot.get_disks', []).subscribe((boot_res)=>{
 			for (let boot in boot_res) {
 				this.disk_pool.set(boot_res[boot], T('Boot Pool'));
 			}
-			this.ws.call('disk.get_unused', []).subscribe((unused_res)=>{
-				for (let unused in unused_res) {
-					this.disk_pool.set(unused_res[unused].name, T('Unused'));
+		});
+	}
+
+	getActions(parentRow) {
+   	const actions = [{
+      label: T("Edit"),
+      onClick: (row) => {
+        this.router.navigate(new Array('/').concat([
+          "storage", "disks", "edit", row.identifier
+        ]));
+      }
+    }];
+    this.ws.call('disk.get_unused', []).subscribe((res)=>{
+			if (_.find(res, {"name": parentRow.name})) {
+	    	actions.push({
+	    		label: T("Wipe"),
+	        onClick: (row) => {
+	          this.router.navigate(new Array('/').concat([
+	            "storage", "disks", "wipe", row.name
+	          ]));
+	        }
+	    	})
+	    }
+		});
+    
+    return actions;
+  }
+
+	dataHandler(entityList: any) {
+		this.ws.call('disk.get_unused', []).subscribe((unused_res)=>{
+			for (let unused in unused_res) {
+				this.disk_pool.set(unused_res[unused].name, T('Unused'));
+			}
+			this.ws.call('pool.query', []).subscribe((pool_res)=>{
+				for (let pool in pool_res) {
+					this.ws.call('pool.get_disks', [pool_res[pool].id]).subscribe((res) => {
+						for (let k in res) {
+							this.disk_pool.set(res[k], pool_res[pool].name);
+						}
+						for (let i = 0; i < entityList.rows.length; i++) {
+				      entityList.rows[i].readable_size = (<any>window).filesize(entityList.rows[i].size, { standard: "iec" });
+				      entityList.rows[i].pool = this.disk_pool.get(entityList.rows[i].name);
+				    }
+					});
 				}
-				this.ws.call('pool.query', []).subscribe((pool_res)=>{
-					for (let pool in pool_res) {
-						this.ws.call('pool.get_disks', [pool_res[pool].id]).subscribe((res) => {
-							for (let k in res) {
-								this.disk_pool.set(res[k], pool_res[pool].name);
-							}
-							for (let i = 0; i < entityList.rows.length; i++) {
-					      entityList.rows[i].readable_size = (<any>window).filesize(entityList.rows[i].size, { standard: "iec" });
-					      entityList.rows[i].pool = this.disk_pool.get(entityList.rows[i].name);
-					    }
-						});
-					}
-				});
 			});
-		});  
+		});
   }
 }
