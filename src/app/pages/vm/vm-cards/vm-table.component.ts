bcf7e01a580a10d858d939c7b90d693e562ccebd
vaibhav-rbs <vaibhav.rbs@gmail.com>
2018-09-28 12:49:43 -0400

    vscode suggested linting fixes.

--- src/app/pages/vm/vm-cards/vm-table.component.ts
+++ src/app/pages/vm/vm-cards/vm-table.component.ts
@@ -1,23 +1,18 @@
-import {Component, OnChanges, ElementRef, OnInit, AfterViewInit, ViewChild, Input, Output, EventEmitter} from '@angular/core';
+import {Component, OnChanges, ViewChild, Input, Output, EventEmitter} from '@angular/core';
 import {Router} from '@angular/router';
-import {Subscription} from 'rxjs';
 import {Observable} from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import {RestService, WebSocketService} from '../../../services/';
-import { MaterialModule } from '../../../appMaterial.module';
-import { MatButtonToggleGroup } from '@angular/material/button-toggle';
 
 export class Page {
     //The number of elements in the page
-    size: number = 0;
+    size = 0;
     //The total number of elements
-    totalElements: number = 0;
+    totalElements = 0;
     //The total number of pages
-    totalPages: number = 0;
+    totalPages = 0;
     //The current page number
-    pageNumber: number = 0;
+    pageNumber = 0;
 }
 
 export class PagedData<T> {
@@ -26,19 +21,19 @@ export class PagedData<T> {
 }
 
 @Component({
-  selector : 'vm-table',
+  selector : 'app-vm-table',
   templateUrl: './vm-table.component.html' // Why does this throw a missing template error?
 })
 export class VmTableComponent implements OnChanges{
 
-  protected resource_name: string = 'vm/vm';
-  protected route_add: string[] = [ 'vm', 'add' ];
-  protected route_add_tooltip: string = "Add VM";
-  protected route_edit: string[] = [ 'vm', 'edit' ];
-  protected runnningState: string = "RUNNING";
-  protected toggleProp: string = "state";
-  protected toggleStart: string = "vm.start";
-  protected toggleStop: string = "vm.stop";
+  protected resource_name = 'vm/vm';
+  protected route_add = [ 'vm', 'add' ];
+  protected route_add_tooltip = "Add VM";
+  protected route_edit = [ 'vm', 'edit' ];
+  protected runnningState = "RUNNING";
+  protected toggleProp = "state";
+  protected toggleStart = "vm.start";
+  protected toggleStop = "vm.stop";
 
 
   public title = "Virtual Machines"
@@ -47,7 +42,6 @@ export class VmTableComponent implements OnChanges{
     {name : 'State', prop : 'state'},
     {name : 'Name', prop : 'name'} ,
     {name : 'Description', prop : 'description'},
-    //{name : 'Info', prop : 'info' },
     {name : 'vCPUs', prop : 'vcpus'},
     {name : 'Memory', prop : 'memory'},
     {name : 'Bootloader', prop : 'bootloader'},
@@ -73,31 +67,21 @@ export class VmTableComponent implements OnChanges{
 
   ngOnChanges(changes) {
     if(changes.data){
-      //console.log("VM Table: DATA CHANGED!");
-      let newData = Object.assign(this.data,{});
+      const newData = Object.assign(this.data,{});
       this.data = newData;
     }
-    //console.log("******** VM-CARDS ********");
     this.datatable.limit = this.pageSize; // items per page
-    this.datatable.pageSize = 8;//this.pageSize;
+    this.datatable.pageSize = 8;
     this.datatable.recalculate();
-    //console.log(this.datatable);
     this.setTableHeight(this.datatable);
   }
 
   setTableHeight(t){
-    //console.log("Row: " + t.rowHeight);
-    //console.log("Header: " + t.headerHeight);
-    //console.log("Footer: " + t.footerHeight);
-    let height = (50*this.pageSize) + 100;
-    //console.log("******** New tableHeight = " + height);
-    //return String(height) + "px";
+    const height = (50*this.pageSize) + 100;
     this.tableHeight = height;
   }
 
   setPage(pageInfo){
-    console.log("********  setPage ********");
-    console.log(pageInfo);
     this.page.pageNumber = pageInfo.offset;
     this.page.size = pageInfo.pageSize;
 
@@ -131,11 +115,11 @@ export class VmTableComponent implements OnChanges{
   }
 
   private getPagedData(page: Page): PagedData<any> {
-    let pagedData = new PagedData<any>();
+    const pagedData = new PagedData<any>();
     page.totalElements = this.data.length;
     page.totalPages = page.totalElements / page.size;
-    let start = page.pageNumber * page.size;
-    let end = Math.min((start + page.size), page.totalElements);
+    const start = page.pageNumber * page.size;
+    const end = Math.min((start + page.size), page.totalElements);
     for (let i = start; i < end; i++){
     /*
       let jsonObj = data[i];
@@ -148,33 +132,29 @@ export class VmTableComponent implements OnChanges{
   }
 
   editRow(row){
-    let index = this.data.indexOf(row);
-    console.log(index);
+    const index = this.data.indexOf(row);
     this.edit.emit(index);
   }
 
   goToDevices(row){
-    let index = this.data.indexOf(row);
     this.router.navigate(
       new Array('').concat([ "vm", row.id, "devices", row.name ])
     );
   }
 
   deleteRow(row){
-    let index = this.data.indexOf(row);
-    console.log(index);
+    const index = this.data.indexOf(row);
     this.delete.emit(index);
   }
 
   onPower(row){
-    let index = this.data.indexOf(row);
-    console.log(index);
+    const index = this.data.indexOf(row);
     this.power.emit(index);
   }
 
   onRestart(row){
-    let index = this.data.indexOf(row);
-    let machine = Object.assign({}, row);
+    const index = this.data.indexOf(row);
+    const machine = Object.assign({}, row);
     machine.machineId = row.id;
     this.target.next({name:"RestartVM", data:index, sender:machine})
   }
