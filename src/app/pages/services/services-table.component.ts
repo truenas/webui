a04c138d27240f0833e1d4c849df8961dd6743ef
Dennis Mullen <dkmullen@gmail.com>
2018-09-29 12:21:01 -0400

    Add inf scroll to services table, move entity table filter box

--- src/app/pages/services/services-table.component.ts
+++ src/app/pages/services/services-table.component.ts
@@ -7,16 +7,19 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { RestService, WebSocketService } from '../../services/';
 import { MaterialModule } from '../../appMaterial.module';
 import { MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { min } from 'rxjs/operator/min';
 
 @Component({
   selector: 'services-table',
   templateUrl: './services-table.component.html',
   styleUrls: ['./services-table.component.css']
 })
-export class ServicesTableComponent implements OnChanges {
+export class ServicesTableComponent implements OnChanges, OnInit {
 
   @Input() conf: any;
   @Input() data: any[];
+  @ViewChild('datatable') datatable;
+
 
   public columns: Array < any > = [
     { name: 'State', prop: 'state' },
@@ -25,10 +28,45 @@ export class ServicesTableComponent implements OnChanges {
     { name: 'Actions', prop: 'cardActions' }
   ];
 
+  public pageSize:number = 12;
+  public minPageSize: number = 3;
+  public baseWindowHeight: number = 910;
+  public tableHeight:number;
+
   constructor(protected router: Router, protected rest: RestService, protected ws: WebSocketService) {}
 
-  ngOnChanges() {
-
+  ngOnInit() {
+    this.findPageSize();
+    
+    window.onresize = () => {
+      this.findPageSize()
+    }
   }
 
+  findPageSize() {
+    let x = window.innerHeight - this.baseWindowHeight;
+    this.pageSize = 12 + (Math.floor(x/50));
+    if (this.pageSize < this.minPageSize) {
+      this.pageSize = this.minPageSize;
+    }
+    this.setTableHeight(this.datatable);
+  }
+
+  ngOnChanges(changes) {
+    if(changes.data){
+      console.log("VM Table: DATA CHANGED!");
+      let newData = Object.assign(this.data,{});
+      this.data = newData;
+    }
+    //console.log("******** VM-CARDS ********");
+    this.datatable.limit = this.pageSize; // items per page
+    // this.datatable.pageSize = 8;//this.pageSize;
+    this.datatable.recalculate();
+    //console.log(this.datatable);
+    this.setTableHeight(this.datatable);
+  }
+
+  setTableHeight(t){
+    this.tableHeight = (50*this.pageSize) + 100;
+  }
 }
