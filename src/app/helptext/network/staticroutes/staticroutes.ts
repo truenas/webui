2a60f9e9a029266532e7534635bce08c03f0a88a
Dennis Mullen <dkmullen@gmail.com>
2018-10-11 15:58:14 -0400

    Add all the network help strings

---
+++ src/app/helptext/network/staticroutes/staticroutes.ts
@@ -0,0 +1,17 @@
+import { T } from '../../../translate-marker';
+import {Validators} from '@angular/forms';
+import { matchOtherValidator } from '../../../pages/common/entity/entity-form/validators/password-validation';
+
+export default {
+    sr_destination_placeholder : T('Destination'),
+    sr_destination_tooltip : T('Use the format <i>A.B.C.D/E</i> where <i>E</i> is the\
+                       CIDR mask.'),
+    sr_destination_validation : [ Validators.required ],
+
+    sr_gateway_placeholder : T('Gateway'),
+    sr_gateway_tooltip : T('Enter the IP address of the gateway.'),
+    sr_gateway_validation : [ Validators.required ],
+
+    sr_description_placeholder : T('Description'),
+    sr_description_tooltip : T('Enter a description of the static route.'),
+}
