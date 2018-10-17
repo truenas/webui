7bdda57adf1c6fc950da8570c30ecbfe583f311e
Dennis Mullen <dkmullen@gmail.com>
2018-10-17 16:57:36 -0400

    Add lldp

---
+++ src/app/helptext/services/components/service-lldp.ts
@@ -0,0 +1,15 @@
+import { T } from '../../../translate-marker';
+
+export default {
+    lldp_intdesc_placeholder : T('Interface Description'),
+    lldp_intdesc_tooltip: T('Set to enable <i>receive</i> mode. Any received peer\
+                      information is saved in interface descriptions.'),
+
+    lldp_country_placeholder : T('Country Code'),
+    lldp_country_tooltip: T('Required for <a href="..//docs/services.html#lldp"\
+                      target="_blank">LLDP</a> location support. Enter a\
+                      two-letter ISO 3166 country code.'),
+
+    lldp_location_placeholder : T('Location'),
+    lldp_location_tooltip: T('Specify the physical location of the host.')
+}
