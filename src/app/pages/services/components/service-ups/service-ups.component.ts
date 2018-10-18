c00b30f5322bc78a9303d1840306c68e45de0e68
Dennis Mullen <dkmullen@gmail.com>
2018-10-18 12:59:36 -0400

    add ups

--- src/app/pages/services/components/service-ups/service-ups.component.ts
+++ src/app/pages/services/components/service-ups/service-ups.component.ts
@@ -1,27 +1,10 @@
-import {ApplicationRef, Component, Injector, OnDestroy} from '@angular/core';
-import {
-  AbstractControl,
-  FormArray,
-  FormGroup,
-  Validators
-} from '@angular/forms';
-import {ActivatedRoute, Router, RouterModule} from '@angular/router';
+import { ApplicationRef, Component, Injector, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import * as _ from 'lodash';
-import {Subscription} from 'rxjs';
 
-
-import {
-  RestService,
-  SystemGeneralService,
-  WebSocketService
-} from '../../../../services/';
-import {
-  FieldConfig
-} from '../../../common/entity/entity-form/models/field-config.interface';
-import {
-  matchOtherValidator
-} from '../../../common/entity/entity-form/validators/password-validation';
-import { T } from '../../../../translate-marker';
+import { RestService, WebSocketService } from '../../../../services/';
+import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import helptext from '../../../../helptext/services/components/service-ups';
 
 @Component({
   selector : 'ups-edit',
@@ -43,180 +26,143 @@ export class ServiceUPSComponent implements OnDestroy {
     {
       type : 'select',
       name : 'ups_mode',
-      placeholder : T('UPS Mode'),
-      tooltip : T('Choose <i>Master</i> if the UPS is plugged directly\
-                   into the system serial port. The UPS will remain the\
-                   last item to shut down. Choose <i>Slave</i> to have\
-                   this system shut down before <i>Master</i>. See the\
-                   <a href="http://networkupstools.org/docs/user-manual.chunked/ar01s02.html#_monitoring_client"\
-                   target="_blank">Network UPS Tools Overview</a>.'),
-      options : [
-        {label : 'Master', value : 'master'},
-        {label : 'Slave', value : 'slave'},
-      ]
+      placeholder : helptext.ups_mode_placeholder,
+      tooltip : helptext.ups_mode_tooltip,
+      options : helptext.ups_mode_options
     },
     {
       type : 'input',
       name : 'ups_identifier',
-      placeholder : T('Identifier'),
-      tooltip : T('Describe the UPS device. It can contain alphanumeric,\
-                   period, comma, hyphen, and underscore characters.'),
+      placeholder : helptext.ups_identifier_placeholder,
+      tooltip : helptext.ups_identifier_tooltip,
       required: true,
-    validation : [ Validators.required, Validators.pattern(/^[\w|,|\.|\-|_]+$/) ]
+    validation : helptext.ups_identifier_validation
     },
     {
       type : 'select',
       name : 'ups_driver',
-      placeholder : T('Driver'),
-      tooltip : T('See the <a\
-                   href="http://networkupstools.org/stable-hcl.html"\
-                   target="_blank">Network UPS Tools compatibility\
-                   list</a> for a list of supported UPS devices.'),
+      placeholder : helptext.ups_driver_placeholder,
+      tooltip : helptext.ups_driver_tooltip,
       required: true,
       options: [],
-      validation : [ Validators.required ]
+      validation : helptext.ups_driver_validation
     },
     {
       type : 'select',
       name : 'ups_port',
-      placeholder : T('Port'),
+      placeholder : helptext.ups_port_placeholder,
       options: [],
-      tooltip : T('Enter the serial or USB port the UPS is plugged into.'),
+      tooltip : helptext.ups_port_tooltip,
       required: true,
-      validation : [ Validators.required ]
+      validation : helptext.ups_port_validation
     },
     {
       type: 'input',
       name: 'ups_hostname',
-      placeholder: T('Hostname'),
-      tooltip: T('Enter the IP address or hostname for SNMP UPS.'),
+      placeholder: helptext.ups_hostname_placeholder,
+      tooltip: helptext.ups_hostname_tooltip,
       required: true,
     },
     {
       type : 'textarea',
       name : 'ups_options',
-      placeholder : T('Auxiliary Parameters (ups.conf)'),
-      tooltip : T('Enter any extra options from <a\
-                   href="http://networkupstools.org/docs/man/ups.conf.html"\
-                   target="_blank">UPS.CONF(5)</a>.'),
+      placeholder : helptext.ups_options_placeholder,
+      tooltip : helptext.ups_options_tooltip,
     },
     {
       type : 'textarea',
       name : 'ups_optionsupsd',
-      placeholder : T('Auxiliary Parameters (upsd.conf)'),
-      tooltip : T('Enter any extra options from <a\
-                   href="http://networkupstools.org/docs/man/upsd.conf.html"\
-                   target="_blank">UPSD.CONF(5)</a>.'),
+      placeholder : helptext.ups_optionsupsd_placeholder,
+      tooltip : helptext.ups_optionsupsd_tooltip,
     },
     {
       type : 'input',
       name : 'ups_description',
-      placeholder : T('Description'),
-      tooltip : T('Describe this service.'),
+      placeholder : helptext.ups_description_placeholder,
+      tooltip : helptext.ups_description_tooltip,
     },
     {
       type : 'select',
       name : 'ups_shutdown',
-      placeholder : T('Shutdown Mode'),
-      tooltip : T('Choose when the UPS initiates shutdown.'),
-      options : [
-        {label : 'UPS reaches low battery', value : 'lowbatt'},
-        {label : 'UPS goes on battery', value : 'batt'},
-      ]
+      placeholder : helptext.ups_shutdown_placeholder,
+      tooltip : helptext.ups_shutdown_tooltip,
+      options : helptext.ups_shutdown_options
     },
     {
       type : 'input',
       inputType: 'number',
       name : 'ups_shutdowntimer',
-      placeholder : T('Shutdown Timer'),
-      tooltip : T('Enter a value in seconds for the the UPS to wait\
-                   before initiating shutdown. Shutdown will not occur\
-                   if power is restored while the timer is counting\
-                   down. This value only applies when <b>Shutdown\
-                   mode</b> is set to <i>UPS goes on battery</i>.'),
+      placeholder : helptext.ups_shutdowntimer_placeholder,
+      tooltip : helptext.ups_shutdowntimer_tooltip,
     },
     {
       type : 'input',
       name : 'ups_shutdowncmd',
-      placeholder : T('Shutdown Command'),
-      tooltip : T('Enter a command to shut down the system when either\
-                   battery power is low or the shutdown timer ends.'),
+      placeholder : helptext.ups_shutdowncmd_placeholder,
+      tooltip : helptext.ups_shutdowncmd_tooltip,
       required: true,
-      validation : [ Validators.required ]
+      validation : helptext.ups_shutdowncmd_validation
     },
     {
       type: 'input',
       inputType: 'number',
       name: 'ups_nocommwarntime',
-      placeholder: T('No Communication Warning Time'),
-      tooltip: T('Enter a number of seconds to wait before alerting that\
-                  the service cannot reach any UPS. Warnings continue\
-                  until the situation is fixed.'),
+      placeholder: helptext.ups_nocommwarntime_placeholder,
+      tooltip: helptext.ups_nocommwarntime_tooltip,
       value: `300`,
     },
     {
       type : 'input',
       name : 'ups_monuser',
-      placeholder : T('Monitor User'),
-      tooltip : T('Enter a user to associate with this service. Keeping\
-                   the default is recommended.'),
+      placeholder : helptext.ups_monuser_placeholder,
+      tooltip : helptext.ups_monuser_tooltip,
       required: true,
-      validation : [ Validators.required ]
+      validation : helptext.ups_monuser_validation
     },
     {
       type : 'input',
       name : 'ups_monpwd',
       inputType: 'password',
       togglePw: true,
-      placeholder : T('Monitor Password'),
-      tooltip : T('Change the default password to improve system\
-                   security. The new password cannot contain a\
-                   space or <b>#</b> .'),
-      validation: [Validators.pattern(/^((?![\#|\s]).)*$/)]
+      placeholder : helptext.ups_monpwd_placeholder,
+      tooltip : helptext.ups_monpwd_tooltip,
+      validation: helptext.ups_monpwd_validation
     },
     {
       type : 'textarea',
       name : 'ups_extrausers',
-      placeholder : T('Extra Users(upsd.conf)'),
-      tooltip : T('Enter accounts that have administrative access.\
-                   See <a\
-                   href="http://networkupstools.org/docs/man/upsd.users.html"\
-                   target="_blank">upsd.users(5)</a> for examples.'),
+      placeholder : helptext.ups_extrausers_placeholder,
+      tooltip : helptext.ups_extrausers_tooltip,
     },
     {
       type : 'checkbox',
       name : 'ups_rmonitor',
-      placeholder : T('Remote Monitor'),
-      tooltip : T('Set for the default configuration to listen on all\
-                   interfaces using the known values of user:\
-                   <i>upsmon</i> and password: <i>fixmepass</i>.'),
+      placeholder : helptext.ups_rmonitor_placeholder,
+      tooltip : helptext.ups_rmonitor_tooltip,
     },
     {
       type : 'checkbox',
       name : 'ups_emailnotify',
-      placeholder : T('Send Email Status Updates'),
-      tooltip : T('Set enable sending messages to the address defined in\
-                   the <b>Email</b> field.'),
+      placeholder : helptext.ups_emailnotify_placeholder,
+      tooltip : helptext.ups_emailnotify_tooltip,
     },
     {
       type : 'input',
       name : 'ups_toemail',
-      placeholder : T('Email'),
-      tooltip : T('Enter any email addresses to receive status updates.\
-                   Separate multiple addresses with a <b>;</b> .'),
+      placeholder : helptext.ups_toemail_placeholder,
+      tooltip : helptext.ups_toemail_tooltip,
     },
     {
       type : 'input',
       name : 'ups_subject',
-      placeholder : T('Email Subject'),
-      tooltip : T('Enter the subject for status emails.'),
+      placeholder : helptext.ups_subject_placeholder,
+      tooltip : helptext.ups_subject_tooltip,
     },
     {
       type : 'checkbox',
       name : 'ups_powerdown',
-      placeholder : T('Power Off UPS'),
-      tooltip : T('Set for the UPS to power off after shutting down the\
-                   system.'),
+      placeholder : helptext.ups_powerdown_placeholder,
+      tooltip : helptext.ups_powerdown_tooltip
     },
   ];
 
