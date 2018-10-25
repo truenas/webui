c30c6e8b9fcc587ad5efb61e33c5bb357db8827f
Mrt134 <timmoore88@gmail.com>
2018-10-25 12:31:50 -0400

    Rename "Automatic check for updates" checkbox on System/Update and add help text. - Investigate the automatic update behavior and rename the checkbox to "Check for Updates Daily and Download if Available" - Add help text to the checkbox. - NPM build testing: no issues. - Clean up whitespace in files.

--- src/app/pages/system/update/update.component.ts
+++ src/app/pages/system/update/update.component.ts
@@ -39,19 +39,25 @@ export class UpdateComponent implements OnInit {
     "SDK": T("Changing SDK version is not a supported operation. Activate an existing boot environment that uses the desired train and boot into it to switch to that train."),
     "NIGHTLY_UPGRADE": T("Changing to a nightly train is one-way. Changing back to a stable train is not supported!")
   }
-  public release_train: boolean;  
-  public pre_release_train: boolean;  
-  public nightly_train: boolean;  
+  public release_train: boolean;
+  public pre_release_train: boolean;
+  public nightly_train: boolean;
   public updates_available: boolean = false;
   public currentTrainDescription: string;
   public fullTrainList: any[];
 
   public busy: Subscription;
   public busy2: Subscription;
+  public updatecheck_tooltip = T('Check the update server every morning \
+                                  for any updates on the chosen train. \
+                                  Automatically download an update if \
+                                  one is available. Click \
+                                  <i>APPLY PENDING UPDATE</i> to install \
+                                  the downloaded update.');
 
   protected dialogRef: any;
   constructor(protected router: Router, protected route: ActivatedRoute, protected snackBar: MatSnackBar,
-    protected rest: RestService, protected ws: WebSocketService, protected dialog: MatDialog, 
+    protected rest: RestService, protected ws: WebSocketService, protected dialog: MatDialog,
     protected loader: AppLoaderService, protected dialogService: DialogService, public translate: TranslateService) {
   }
   parseTrainName(name) {
