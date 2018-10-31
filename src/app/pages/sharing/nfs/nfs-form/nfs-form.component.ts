7d5b439f9130a5a11e18e1fdc0fa040f87e2241a
Sam Bonham <sam.a.bonham@gmail.com>
2018-10-30 17:52:08 -0700

    Ported text changes.

--- src/app/pages/sharing/nfs/nfs-form/nfs-form.component.ts
+++ src/app/pages/sharing/nfs/nfs-form/nfs-form.component.ts
@@ -92,9 +92,9 @@ export class NFSFormComponent {
       type: 'textarea',
       name: 'nfs_hosts',
       placeholder: T('Authorized Hosts and IP addresses'),
-      tooltip: T('Space-delimited list of allowed IP addresses or\
-                  hostnames. Example: <i>1.2.3.0</i>.\
-                  Leave empty to allow all.'),
+      tooltip: T('Space-delimited list of allowed IP addresses\
+                  <i>(192.168.1.10)</i> or hostnames\
+                  <i>(www.freenas.com)</i>. Leave empty to allow all.'),
       blurStatus : true,
       blurEvent: this.nfs_hosts_event,
       parent: this,
