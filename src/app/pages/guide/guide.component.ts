23b105f81aa243318c4ac0059de86a9cf5ffc707
Erin Clark <erin@ixsystems.com>
2018-11-08 13:53:07 -0800

    Undo changes to guide component, this isn't being used by the navigation service currently
    Ticket: #55944

--- src/app/pages/guide/guide.component.ts
+++ src/app/pages/guide/guide.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { DocsService } from '../../services/docs.service';
-
-import urls from '../../helptext/urls';
 
 @Component({
   selector: 'app-guide',
@@ -15,11 +12,9 @@ export class GuideComponent implements OnInit{
 
   public safeUrl: SafeUrl;
 
-  constructor(public sanitizer: DomSanitizer, public docsService: DocsService) {}
+  constructor(public sanitizer: DomSanitizer) {}
 
   ngOnInit() {
-  const url = this.docsService.docReplace("%%docurl%%");
-//  this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl("//" + environment.remote + "//docs/freenas.html");
-  this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+	this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl("//" + environment.remote + "//docs/freenas.html");
   }
 }
