1972b2baa7500b920557733805c4da9284620c16
rishabh27892 <rishabh.chauhan27892@gmail.com>
2018-11-14 12:20:07 -0500

    accidentally overwrote, but fixed QA-57690

--- tests/sys_support.py
+++ tests/sys_support.py
@@ -1,7 +1,7 @@
 # Author: Rishabh Chauhan
 # License: BSD
 # Location for tests  of FreeNAS new GUI
-# Test case count: 2
+# Test case count: 1
 
 import function
 from source import *
@@ -40,7 +40,6 @@ class conf_support_test(unittest.TestCase):
         driver.implicitly_wait(30)
         pass
 
-    # Test navigation Account>Users>Hover>New User and enter username,fullname,password,confirmation and wait till user is  visibile in the list
     def test_01_nav_system_support(self):
         try:
             driver.find_element_by_xpath(xpaths['submenuSupport']).click()
@@ -78,7 +77,6 @@ class conf_support_test(unittest.TestCase):
             for i in range(1,len(exc_info_p)):
                 print (exc_info_p[i].rstrip())
             self.assertEqual("Just for fail", str(Exception), msg="Test fail: Please check the traceback")
-
 
 
     # method to test if an element is present
