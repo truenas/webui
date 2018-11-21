e99eb13bf17e22a26f099afe101864920684e69d
Warren Block <wblock@wonkity.com>
2018-11-21 16:32:24 -0500

    Clarify meaning by replacing "redirect" with "hide"

--- src/app/helptext/task-calendar/cron/cron-form.ts
+++ src/app/helptext/task-calendar/cron/cron-form.ts
@@ -21,15 +21,15 @@ export default {
                 to open the advanced scheduler.'),
     cron_picker_validation: [ Validators.required ],
 
-    cron_stdout_placeholder: T('Redirect Standard Output'),
-    cron_stdout_tooltip: T('Redirect stdout to /dev/null. When unset, output\
-                from the command is mailed to the user running\
-                the cron job.'),
+    cron_stdout_placeholder: T('Hide Standard Output'),
+    cron_stdout_tooltip: T('Hide standard output (stdout) from the command.\
+                When unset, any standard output is mailed to the user\
+                account cron used to run the command.'),
 
-    cron_stderr_placeholder: T('Redirect Standard Error'),
-    cron_stderr_tooltip: T('Redirect stderr to /dev/null. When unset, error\
-                output from the command is mailed to the user\
-                running the cron job.'),
+    cron_stderr_placeholder: T('Hide Standard Error'),
+    cron_stderr_tooltip: T('Hide error output (stderr) from the command.\
+                When unset, any error output is mailed to the user\
+                account cron used to run the command.'),
 
     cron_enabled_placeholder: T('Enabled'),
     cron_enabled_tooltip: T('Enable this cron job. When unset, disable the\
