0ee765e569f9c54ab19269e22d2c9177e553ab65
Lola Yang <lola.ly27@gmail.com>
2018-11-27 18:11:09 -0800

    defined class EntityTreeTable and EntityTreeTableColumn

---
+++ src/app/pages/common/entity/entity-tree-table/entity-tree-table.model.ts
@@ -0,0 +1,12 @@
+import { TreeNode } from 'primeng/api';
+
+export class EntityTreeTableColumn {
+    name: string;
+    prop: string;
+    filesizePipe?: boolean;
+}
+
+export class EntityTreeTable {
+    tableData: TreeNode[];
+    columns: Array<EntityTreeTableColumn>;
+}
