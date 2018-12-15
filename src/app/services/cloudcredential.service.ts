88ab3f8b11f616b4132f55e791241e824341ae3f
Lola Yang <lola.ly27@gmail.com>
2018-12-14 18:38:09 -0800

    add getByte method to calculate byte value from string value (e.g. 10m)

--- src/app/services/cloudcredential.service.ts
+++ src/app/services/cloudcredential.service.ts
@@ -6,10 +6,22 @@ import { WebSocketService } from './ws.service';
 @Injectable()
 export class CloudCredentialService {
   protected credentialProviders: string = 'cloudsync.providers';
+  protected byteMap: Object= {
+    'T': 1099511627776,
+    'G': 1073741824,
+    'M': 1048576,
+    'K': 1024,
+  };
 
   constructor(protected ws: WebSocketService) {};
 
   getProviders() {
   	return this.ws.call(this.credentialProviders, []);
+  }
+
+  getByte(data: string): Number {
+    const unit = data.charAt(data.length - 1);
+    const value = Number(data.slice(0, data.length - 1));
+    return value * this.byteMap[unit.toUpperCase()];
   }
 }
