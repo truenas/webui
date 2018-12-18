15375b534c8883e538883e7dbd967d2e9bcc8867
Lola Yang <lola.ly27@gmail.com>
2018-12-18 13:02:18 -0800

    enable getByte method to valid string filesize value and return byte number value

--- src/app/services/cloudcredential.service.ts
+++ src/app/services/cloudcredential.service.ts
@@ -7,10 +7,11 @@ import { WebSocketService } from './ws.service';
 export class CloudCredentialService {
   protected credentialProviders: string = 'cloudsync.providers';
   protected byteMap: Object= {
-    'TB': 1099511627776,
-    'GB': 1073741824,
-    'MB': 1048576,
-    'KB': 1024,
+    'T': 1099511627776,
+    'G': 1073741824,
+    'M': 1048576,
+    'K': 1024,
+    'B':1,
   };
 
   constructor(protected ws: WebSocketService) {};
@@ -20,8 +21,30 @@ export class CloudCredentialService {
   }
 
   getByte(data: string): Number {
-    const unit = data.charAt(data.length - 1);
-    const value = Number(data.slice(0, data.length - 1));
-    return value * this.byteMap[unit.toUpperCase()];
-  }
+      let unit = 'K'; // default unit
+      let index = -1;
+
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].toUpperCase() == 'B' || data[i].toUpperCase() == 'K' ||
+            data[i].toUpperCase() == 'M' || data[i].toUpperCase() == 'G' ||
+            data[i].toUpperCase() == 'T') {
+          unit = data[i].toUpperCase();
+          index = i;
+          break;
+        }
+      }
+      const rest_unit = data.slice(index + 1, data.length).toUpperCase();
+      if (index == -1 && Number(data)) {
+        return Number(data) * this.byteMap[unit];
+      } else if (rest_unit == 'IB' || rest_unit == 'B' || rest_unit == '') {
+        if (unit == 'B' && rest_unit != '') {
+          return -1;
+        }
+        return Number(data.slice(0, index)) * this.byteMap[unit];
+      } else {
+        return -1;
+      }
+
+    }
+
 }
