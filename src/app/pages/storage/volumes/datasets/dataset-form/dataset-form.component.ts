2fe8ff6fb50faf1a69455f429732e78f5f8db4c6
vaibhav-rbs <vaibhav-rbs@users.noreply.github.com>
2018-12-18 11:33:50 -0500

    using "A smaller record size can reduce" warn msg.

--- src/app/pages/storage/volumes/datasets/dataset-form/dataset-form.component.ts
+++ src/app/pages/storage/volumes/datasets/dataset-form/dataset-form.component.ts
@@ -492,7 +492,7 @@ export class DatasetFormComponent implements Formconfiguration{
         if (res_number < recommended_size_number){
           _.find(this.fieldConfig, {name:'recordsize'}).warnings = `
           Recommended record size based on pool topology: ${this.recommended_size}.
-          Lower recordsize could reduce sequential I/O performance and space efficiency.`
+          A smaller record size can reduce sequential I/O performance and space efficiency.`
         } else {
           _.find(this.fieldConfig, {name:'recordsize'}).warnings = null;
         };
