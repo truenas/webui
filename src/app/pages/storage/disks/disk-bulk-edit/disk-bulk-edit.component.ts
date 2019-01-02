0e0a0d0b67edb641d80464daa85fa62fc3532820
Lola Yang <lola.ly27@gmail.com>
2019-01-02 14:11:03 -0800

    added error check when res.state is success

--- src/app/pages/storage/disks/disk-bulk-edit/disk-bulk-edit.component.ts
+++ src/app/pages/storage/disks/disk-bulk-edit/disk-bulk-edit.component.ts
@@ -153,8 +153,17 @@ export class DiskBulkEditComponent {
         (res) => { 
           if(res.state === 'SUCCESS') {
             this.loader.close();
-            this._router.navigate(new Array('/').concat([
-              "storage", "disks"]));          
+            let success_state = true;
+            for (let i = 0; i < res.result.length; i++) {
+              if (res.result[i].error != null) {
+                this.dialogService.errorReport(T("Error updating disks."), res.result[i].error);
+                success_state = false;
+                break;
+              }
+            }
+            if (success_state) {
+              this._router.navigate(new Array('/').concat(["storage", "disks"]));
+            }
           }
         },
         (err) => {
