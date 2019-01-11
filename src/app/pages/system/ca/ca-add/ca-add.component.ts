abd3566be017be3c5889e1e29de56771d67db955
Dennis Mullen <dkmullen@gmail.com>
2019-01-11 11:48:13 -0500

    modify cert and ca tooltips for private key

--- src/app/pages/system/ca/ca-add/ca-add.component.ts
+++ src/app/pages/system/ca/ca-add/ca-add.component.ts
@@ -189,7 +189,8 @@ export class CertificateAuthorityAddComponent {
       name : 'privatekey',
       placeholder : T('Private Key'),
       tooltip : T('Paste the private key associated with the\
-                   Certificate when available.'),
+                   Certificate when available. Please provide\
+                   a key with size greater than or equal to 1024 bits'),
       isHidden: true,
     },
     {
