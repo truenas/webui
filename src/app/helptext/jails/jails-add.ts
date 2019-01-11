4e306146b5d799f67c9697aa0c4295d8cf6382d1
a-stjohn <aaron.m.stjohn@gmail.com>
2019-01-11 09:33:13 -0500

    Updated with regard to feedback.

--- src/app/helptext/jails/jails-add.ts
+++ src/app/helptext/jails/jails-add.ts
@@ -303,7 +303,7 @@ manipulation of system file flags. \
 <b>securelevel</b> constraints are still enforced.'),
 
 allow_mlock_placeholder: T('allow.mlock'),
-allow_mlock_tooltip: T('Enables running services that require mlock() \
+allow_mlock_tooltip: T('Enable running services that require mlock() \
 in a jail.'),
 
 allow_mount_placeholder: T('allow.mount'),
@@ -364,7 +364,7 @@ for all protocal stacks.'),
 
 vnet_interfaces_placeholder: T('vnet.interfaces'),
 vnet_interfaces_tooltip: T('A space delimited list of network \
-interfaces for a VNET-enabled jail after it is created. \
+interfaces attached to a VNET-enabled jail after it is created. \
 Interfaces are automatically released when the jail is removed.'),
 
 //network field Config
@@ -480,8 +480,8 @@ hostid_tooltip: T('A new jail hostid, if desired. \
 <i>1a2bc345-678d-90e1-23fa-4b56c78901de</i>.'),
 
 hostid_strict_check_placeholder: T('hostid_strict_check'),
-hostid_strict_check_tooltip: T('Checks the hostid property of the jail \
-and if they are not the same as the hosts, it does not start the jail'),
+hostid_strict_check_tooltip: T('Check the <i>hostid</i> property of the \
+jail. If not the same as the hosts, do not start the jail'),
 
 comment_placeholder: T('comment'),
 comment_tooltip: T('Enter comments about the jail.'),
@@ -527,8 +527,9 @@ jail_zfs_mountpoint_tooltip: T('Enter the mountpoint for the \
 <b>Example:</b> <i>/data example-dataset-name</i>'),
 
 allow_tun_placeholder: T('allow_tun'),
-allow_tun_tooltip: T('Unhides tun devices for the jail with an \
-individual devfsruleset.  Allows the creation of tuns in the jail.'),
+allow_tun_tooltip: T('Reveal tun devices for the jail with an \
+individual devfs ruleset. Allow the creation of tun devices in the \
+jail.'),
 
 // rctl config
 memoryuse_placeholder: T('memoryuse'),
