a3dcc9c81808409560c9e7251114ee1aaa148bb0
Lola Yang <lola.ly27@gmail.com>
2019-01-18 10:53:34 -0800

    change secruelevel from input type to select type

--- src/app/pages/jails/jail-add/jail-add.component.ts
+++ src/app/pages/jails/jail-add/jail-add.component.ts
@@ -345,10 +345,26 @@ export class JailAddComponent implements OnInit {
       tooltip: helptext.login_flags_tooltip,
     },
     {
-      type: 'input',
+      type: 'select',
       name: 'securelevel',
       placeholder: helptext.securelevel_placeholder,
       tooltip: helptext.securelevel_tooltip,
+      options: [{
+        label: '3',
+        value: '3',
+      }, {
+        label: '2 (default)',
+        value: '2',
+      }, {
+        label: '1',
+        value: '1',
+      }, {
+        label: '0',
+        value: '0',
+      }, {
+        label: '-1',
+        value: '-1',
+      }],
     },
     {
       type: 'select',
