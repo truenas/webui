7cdfbe9d05d8aa9e66d679f251158efd410d23ec
Erin Clark <erin@ixsystems.com>
2019-02-12 15:17:04 -0800

    Another missing import
    Ticket: #70554

--- scripts/create_pr.js
+++ scripts/create_pr.js
@@ -2,6 +2,7 @@
 
 const exec = require('child_process').exec;
 const path = require('path');
+const fs = require('fs-extra');
 const octokit = require('@octokit/rest')();
 const CONFIG_FILE = path.join(process.cwd(), './github-pr.json');
 
