81208ff061b0610a8d2754991b07391b09658719
Lola Yang <lola.ly27@gmail.com>
2019-02-14 17:52:31 -0800

    add helptext for iscsi wizard

---
+++ src/app/helptext/sharing/iscsi/iscsi-wizard.ts
@@ -0,0 +1,27 @@
+import { T } from '../../../translate-marker';
+
+export default {
+    step1_label: T("Create or Choose Block Device"),
+
+    name_placeholder: T("Name"),
+    name_tooltip: T(""),
+
+    type_placeholder: T("Type"),
+    type_tooltip: T(""),
+
+    file_placeholder: T("File"),
+    file_tooltip: T(""),
+
+    filesize_placeholder: T("Filesize"),
+    filesize_tooltip: T(""),
+
+    disk_placeholder: T("Device"),
+    disk_tooltip: T(""),
+
+    blocksize_placeholder: T("Logical block size"),
+    blocksize_tooltip: T(""),
+
+
+    step2_label: T(""),
+
+}
