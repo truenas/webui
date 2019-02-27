8ab2fcd0d15afddb2bbc0d53f3ff8ec9e197485e
Lola Yang <lola.ly27@gmail.com>
2019-02-27 15:13:14 -0800

    enable entity-tree-table to construct treetable

--- src/app/pages/common/entity/entity-tree-table/entity-tree-table.component.ts
+++ src/app/pages/common/entity/entity-tree-table/entity-tree-table.component.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { EntityTreeTable, EntityTreeTableColumn } from './entity-tree-table.model';
 import { TreeNode } from 'primeng/api';
+
+import { EntityTreeTable, EntityTreeTableColumn } from './entity-tree-table.model';
+import { EntityTreeTableService } from './entity-tree-table.service';
+import { WebSocketService, DialogService } from '../../../../services';
+import { EntityUtils } from '../../entity/utils';
 
 @Component({
 	selector: 'entity-tree-table',
-	templateUrl: './entity-tree-table.component.html'
+	templateUrl: './entity-tree-table.component.html',
+	providers: [EntityTreeTableService]
 })
 export class EntityTreeTableComponent implements OnInit {
 	@Input('conf') conf: EntityTreeTable;
 
 	showActions = true;
+	treeTableData: Array<TreeNode> = [];
 	columns: Array<EntityTreeTableColumn>;
 
-	constructor() { }
+	constructor(private ws: WebSocketService,
+		private treeTableService: EntityTreeTableService,
+		private dialogService: DialogService) { }
 
 	ngOnInit() {
+		this.treeTableData = this.conf.tableData;
 		this.columns = this.conf.columns;
+
+		if (this.conf.queryCall) {
+			this.getData();
+		}
+	}
+
+	getData() {
+		this.ws.call(this.conf.queryCall).subscribe(
+			(res) => {
+				this.treeTableData = this.treeTableService.buildTree(res);
+			},
+			(err) => {
+				new EntityUtils().handleWSError(this, err, this.dialogService);
+			}
+		);
 	}
 }
