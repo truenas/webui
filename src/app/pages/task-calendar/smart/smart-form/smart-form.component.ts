7eb49d681d3c3975ebd88922052a6cd2d81dce5d
Lola Yang <lola.ly27@gmail.com>
2019-03-12 18:12:59 -0700

    converted smart test to ws call; added all disk field

--- src/app/pages/task-calendar/smart/smart-form/smart-form.component.ts
+++ src/app/pages/task-calendar/smart/smart-form/smart-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import * as _ from 'lodash';
 
@@ -16,12 +16,14 @@ import helptext from '../../../../helptext/task-calendar/smart/smart-form';
 })
 export class SmartFormComponent {
 
-  protected resource_name: string = 'tasks/smarttest';
+  public queryCall = "smart.test.query";
+  protected addCall = 'smart.test.create';
+  protected editCall = 'smart.test.update';
+  protected customFilter: Array<any> = [[["id", "="]]];
   protected route_success: string[] = ['tasks', 'smart'];
   protected entityForm: EntityFormComponent;
   protected isEntity: boolean = true;
 
-  protected preTaskName: string = 'smarttest';
   public fieldSets: FieldSet[] = [
     {
       name:'S.M.A.R.T. Test',
@@ -29,26 +31,56 @@ export class SmartFormComponent {
       label:true,
       width:'300px',
       config: [
+        {
+          type: 'checkbox',
+          name: 'all_disks',
+          placeholder: helptext.smarttest_all_disks_placeholder,
+          tooltip: helptext.smarttest_all_disks_tooltip,
+        },
       {
         type: 'select',
-        name: 'smarttest_disks',
+        name: 'disks',
         placeholder: helptext.smarttest_disks_placeholder,
         tooltip : helptext.smarttest_disks_tooltip,
         options: [],
         multiple: true,
         required: true,
-        validation : helptext.smarttest_disks_validation
+        validation : helptext.smarttest_disks_validation,
+        relation: [{
+          action: 'DISABLE',
+          when: [{
+            name: 'all_disks',
+            value: true,
+          }]
+        }],
       }, {
         type: 'select',
-        name: 'smarttest_type',
+        name: 'type',
         placeholder: helptext.smarttest_type_placeholder,
         tooltip : helptext.smarttest_type_tooltip,
-        options: [],
+        options: [
+          {
+            label: 'LONG',
+            value: 'LONG',
+          },
+          {
+            label: 'SHORT',
+            value: 'SHORT',
+          },
+          {
+            label: 'CONVEYANCE',
+            value: 'CONVEYANCE',
+          },
+          {
+            label: 'OFFLINE',
+            value: 'OFFLINE',
+          }
+        ],
         required: true,
         validation : helptext.smarttest_type_validation
       }, {
         type: 'input',
-        name: 'smarttest_desc',
+        name: 'desc',
         placeholder: helptext.smarttest_desc_placeholder,
         tooltip : helptext.smarttest_desc_tooltip
       },
@@ -66,47 +98,48 @@ export class SmartFormComponent {
   ];
 
   protected disk_field: any;
-  protected type_field: any;
-  protected month_field: any;
-  protected day_field: any;
-  protected hour_field: any;
-  protected daymonth_field: any;
+  protected pk: any;
 
-  constructor(protected router: Router, protected taskService: TaskService, protected storageService: StorageService, protected entityFormService: EntityFormService, ) {
-    this.disk_field = _.find(this.fieldSets[0].config, { 'name': 'smarttest_disks' });
+  constructor(protected router: Router, protected taskService: TaskService, protected storageService: StorageService, protected entityFormService: EntityFormService, protected aroute: ActivatedRoute) {
+    this.disk_field = _.find(this.fieldSets[0].config, { 'name': 'disks' });
     this.storageService.listDisks().subscribe((res) => {
-      for (let i in res) {
+      for (const i in res) {
         this.disk_field.options.push({ label: res[i].name, value: res[i].identifier })
       }
-    });
-
-    this.type_field = _.find(this.fieldSets[0].config, { 'name': 'smarttest_type' });
-    this.taskService.getSmarttestTypeChoices().subscribe((res) => {
-      res.forEach((item) => {
-        this.type_field.options.push({ label: item[1], value: item[0] });
-      });
     });
   }
 
   resourceTransformIncomingRestData(data) {
     data['smarttest_picker'] = "0" + " " + 
-                          data.smarttest_hour + " " + 
-                          data.smarttest_daymonth + " " + 
-                          data.smarttest_month + " " + 
-                          data.smarttest_dayweek;
+                          data.schedule.hour + " " +
+                          data.schedule.dom + " " +
+                          data.schedule.month + " " +
+                          data.schedule.dow;
     return data;
   }
 
-
-  afterInit(entityForm){ 
+  preInit() {
+    this.aroute.params.subscribe(params => {
+      if (params['pk']) {
+        this.pk = params['pk'];
+        this.customFilter[0][0].push(parseInt(params['pk']));
+      }
+    });
   }
 
   beforeSubmit(value){
     const spl = value.smarttest_picker.split(" ");
     delete value.smarttest_picker;
-    value['smarttest_hour'] = spl[1];
-    value['smarttest_daymonth'] = spl[2];
-    value['smarttest_month'] = spl[3];
-    value['smarttest_dayweek'] = spl[4];
+
+    value['schedule'] = {
+      hour: spl[1],
+      dom: spl[2],
+      month: spl[3],
+      dow: spl[4],
+    };
+
+    if (value.all_disks) {
+      value.disks = [];
+    }
   }
 }
