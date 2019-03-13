8f6b1458ab7e92ad9e07d5bd73f5752f706ea0f6
Dennis Mullen <dkmullen@gmail.com>
2019-03-13 07:25:23 -0400

    Hide unused fields on Cert Add

--- src/app/pages/common/entity/entity-form/entity-form.component.ts
+++ src/app/pages/common/entity/entity-form/entity-form.component.ts
@@ -558,14 +558,13 @@ export class EntityFormComponent implements OnInit, OnDestroy, OnChanges, AfterV
   }
 
   setDisabled(name: string, disable: boolean, hide?: boolean, status?:string) {
-    
     // if field will be hide, disabled it too
     if (hide) {
       disable = hide;
     } else {
       hide = false;
     }
-    console.log(name, hide)
+
 
     this.fieldConfig = this.fieldConfig.map((item) => {
       if (item.name === name) {
