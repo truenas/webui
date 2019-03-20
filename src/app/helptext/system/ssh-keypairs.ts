080c50aba378b15c61694f77bf431b06100cbed5
Lola Yang <lola.ly27@gmail.com>
2019-03-20 14:48:01 -0700

    added ssh keypairs form component and helptext

---
+++ src/app/helptext/system/ssh-keypairs.ts
@@ -0,0 +1,14 @@
+import { T } from '../../translate-marker';
+
+export default {
+    name_placeholder: T('Name'),
+    name_tooltip: T(''),
+
+    private_key_placeholder: T('Private Key'),
+    private_key_tooltip: T(''),
+
+    public_key_placeholder: T('Public Key'),
+    public_key_tooltip: T(''),
+
+    generate_key_button: T('Generate Keypair')
+}
