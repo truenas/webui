54ec46ae9114a915c27054c3becd74920ce3f8e4
ericbsd <ericturgeon.bsd@gmail.com>
2019-03-22 16:49:17 -0300

    change net_vlan.py to pytest

--- tests/selenium/test_016_net_vlan.py
+++ tests/selenium/test_016_net_vlan.py
@@ -1,11 +1,12 @@
 # Author: Rishabh Chauhan
 # License: BSD
 # Location for tests  of FreeNAS new GUI
-# Test case count: 1
+# Test case count: 3
 
 import sys
 import os
-from selenium.webdriver.common.by import By
+import time
+from selenium.webwb_driver.common.by import By
 cwd = str(os.getcwd())
 sys.path.append(cwd)
 from function import take_screenshot, is_element_present
@@ -16,8 +17,8 @@ script_name = os.path.basename(__file__).partition('.')[0]
 
 xpaths = {
     'navNetwork': '//*[@id="nav-4"]/div/a[1]',
-    # this should be the real ID 'submenuStatic' : '//*[@id="4-3"]',
-    'submenuStatic': '//*[@id="4-4"]'
+    # this should be the correct ID 'submenuVlan' : '//*[@id="4-4"]'
+    'submenuVlan': '//*[@id="4-5"]'
 }
 
 
@@ -25,18 +26,27 @@ def test_00_set_implicitly_wait(wb_driver):
     wb_driver.implicitly_wait(1)
 
 
-def test_01_nav_net_static(wb_driver):
-    # Click on the static submenu
-    wb_driver.find_element_by_xpath(xpaths['submenuStatic']).click()
+def test_01_nav_net_vlan(wb_driver):
+    # Click on the vlan submenu
+    wb_driver.find_element_by_xpath(xpaths['submenuVlan']).click()
     # cancelling the tour
-    if is_element_present(wb_driver, By.XPATH, "/html/body/div[6]/div[1]/button"):
+    if is_element_present(By.XPATH,"/html/body/div[6]/div[1]/button"):
         wb_driver.find_element_by_xpath("/html/body/div[6]/div[1]/button").click()
     # get the ui element
     ui_element = wb_driver.find_element_by_xpath("//*[@id='breadcrumb-bar']/ul/li[2]/a")
     # get the weather data
     page_data = ui_element.text
     # assert response
-    assert "Network Summay" in page_data, page_data
+    assert "VLANs" in page_data, page_data
+    # taking screenshot
+    test_name = sys._getframe().f_code.co_name
+    take_screenshot(wb_driver, script_name, test_name)
+
+
+def test_02_close_network_tab(wb_driver):
+    # Close the System Tab
+    wb_driver.find_element_by_xpath(xpaths['navNetwork']).click()
+    time.sleep(2)
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
