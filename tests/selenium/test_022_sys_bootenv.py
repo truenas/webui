38bc07990b40bea7c4b3bc251d8c5d69cfe3168e
ericbsd <ericturgeon.bsd@gmail.com>
2019-03-25 11:28:11 -0300

    converting sys_bootenv.py to pytest

--- tests/selenium/test_022_sys_bootenv.py
+++ tests/selenium/test_022_sys_bootenv.py
@@ -14,8 +14,8 @@ skip_mesages = "Skipping first run"
 script_name = os.path.basename(__file__).partition('.')[0]
 
 xpaths = {
-    'navSystem' : '//*[@id="nav-2"]/div/a[1]',
-    'submenuNtpserver' : '//*[@id="2-1"]',
+    'navSystem': '//*[@id="nav-2"]/div/a[1]',
+    'submenuBootenv': '//*[@id="2-2"]',
     'breadcrumbBar': "//*[@id='breadcrumb-bar']/ul/li[2]/a"
 }
 
@@ -24,14 +24,15 @@ def test_00_set_implicitly_wait(wb_driver):
     wb_driver.implicitly_wait(1)
 
 
-def test_01_nav_system_ntpserver(wb_driver):
-    wb_driver.find_element_by_xpath(xpaths['submenuNtpserver']).click()
+def test_01_nav_system_bootenv(wb_driver):
+    wb_driver.find_element_by_xpath(xpaths['submenuBootenv']).click()
+    # cancelling the tour
     # get the ui element
     ui_element = wb_driver.find_element_by_xpath(xpaths['breadcrumbBar'])
     # get the weather data
     page_data = ui_element.text
     # assert response
-    assert "NTP Servers" in page_data, page_data
+    assert "Boot Environments" in page_data, page_data
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
