25962ac58f5ac6023a91bfc12efa0479f65ace61
ericbsd <ericturgeon.bsd@gmail.com>
2019-03-25 15:10:31 -0300

    convert tasks_rsync.py tasks_SMART.py and tasks_periodic_snapshot.py to pytest

---
+++ tests/selenium/test_052_tasks_periodic_snapshot.py
@@ -0,0 +1,35 @@
+# Author: Rishabh Chauhan
+# License: BSD
+# Location for tests  of FreeNAS new GUI
+# Test case count: 1
+
+import sys
+import os
+cwd = str(os.getcwd())
+sys.path.append(cwd)
+from function import take_screenshot
+
+skip_mesages = "Skipping first run"
+script_name = os.path.basename(__file__).partition('.')[0]
+
+xpaths = {
+    'navTasks': '//*[@id="nav-3"]/div/a[1]',
+    'submenuPeriodicSS': '//*[@id="3-4"]'
+}
+
+
+def test_00_set_implicitly_wait(wb_driver):
+    wb_driver.implicitly_wait(1)
+
+
+def test_01_nav_tasks_periodicSS(wb_driver):
+    wb_driver.find_element_by_xpath(xpaths['submenuPeriodicSS']).click()
+    # get the ui element
+    ui_element = wb_driver.find_element_by_xpath(xpaths['breadcrumbBar'])
+    # get the weather data
+    page_data = ui_element.text
+    # assert response
+    assert "Periodic Snapshot Tasks" in page_data, page_data
+    # taking screenshot
+    test_name = sys._getframe().f_code.co_name
+    take_screenshot(wb_driver, script_name, test_name)
