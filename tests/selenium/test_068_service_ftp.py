d2b1b89084de0e9df45c0da422138ba16301e00e
ericbsd <ericturgeon.bsd@gmail.com>
2019-03-26 16:21:39 -0300

    convert service_ftp.py service_iscsi.py and service_lldp.py to pytest and fix status_change

--- tests/selenium/test_068_service_ftp.py
+++ tests/selenium/test_068_service_ftp.py
@@ -1,12 +1,11 @@
 # Author: Rishabh Chauhan
 # License: BSD
 # Location for tests  of FreeNAS new GUI
-# Test case count: 5
+# Test case count: 4
 
 import sys
 import os
 import time
-from selenium.webdriver.common.keys import Keys
 cwd = str(os.getcwd())
 sys.path.append(cwd)
 from function import take_screenshot, status_change, status_check
@@ -25,10 +24,10 @@ def test_00_set_implicitly_wait(wb_driver):
     wb_driver.implicitly_wait(1)
 
 
-def test_01_nav_services(wb_driver):
-    # Click  Service menu
+def test_01_turnon_ftp(wb_driver):
+    # Click Service Menu
     wb_driver.find_element_by_xpath(xpaths['navService']).click()
-    # allowing the button to load
+    # check if the Service page is opens
     time.sleep(1)
     # get the ui element
     ui_element = wb_driver.find_element_by_xpath(xpaths['breadcrumbBar'])
@@ -36,42 +35,35 @@ def test_01_nav_services(wb_driver):
     page_data = ui_element.text
     # assert response
     assert "Services" in page_data, page_data
+    status_change(wb_driver, "4", "start")
+    # ftp test takes almost 6 min to turn on and display
+    time.sleep(7)
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
 
 
-def test_02_turn_on_afp(wb_driver):
-    # scroll down
-    wb_driver.find_element_by_tag_name('body').send_keys(Keys.HOME)
-    time.sleep(2)
-    status_change(wb_driver, "1", "start")
-    # taking screenshot
-    test_name = sys._getframe().f_code.co_name
-    take_screenshot(wb_driver, script_name, test_name)
-
-
-def test_03_checkif_afp_on(wb_driver):
+def test_02_checkif_ftp_on(wb_driver):
     time.sleep(2)
     # status check
-    status_check(wb_driver, "1")
+    status_check(wb_driver, "4")
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
 
 
-def test_04_turnoff_afp(wb_driver):
+def test_03_turnoff_ftp(wb_driver):
     time.sleep(2)
-    status_change(wb_driver, "1", "stop")
+    status_change(wb_driver, "4", "stop")
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
 
 
-def test_05_checkif_afp_off(wb_driver):
+def test_04_checkif_ftp_off(wb_driver):
     time.sleep(2)
     # status check
-    status_check(wb_driver, "1")
+    status_check(wb_driver, "5")
     time.sleep(10)
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
--- tests/selenium/test_072_service_lldp.py
+++ tests/selenium/test_072_service_lldp.py
@@ -1,7 +1,7 @@
 # Author: Rishabh Chauhan
 # License: BSD
 # Location for tests  of FreeNAS new GUI
-# Test case count: 5
+# Test case count: 4
 
 import sys
 import os
@@ -25,53 +25,48 @@ def test_00_set_implicitly_wait(wb_driver):
     wb_driver.implicitly_wait(1)
 
 
-def test_01_nav_services(wb_driver):
-    # Click  Service menu
+def test_01_turnon_lldp(wb_driver):
+    # Click Service Menu
     wb_driver.find_element_by_xpath(xpaths['navService']).click()
-    # allowing the button to load
+    # check if the Service page is opens
     time.sleep(1)
     # get the ui element
     ui_element = wb_driver.find_element_by_xpath(xpaths['breadcrumbBar'])
     # get the weather data
-    page_data = ui_element.text
+    page_data = ui_element.tex
     # assert response
-    assert "Services" in page_data, page_data
-    # taking screenshot
-    test_name = sys._getframe().f_code.co_name
-    take_screenshot(wb_driver, script_name, test_name)
-
-
-def test_02_turn_on_afp(wb_driver):
+    assert"Services" in page_data, page_data
     # scroll down
-    wb_driver.find_element_by_tag_name('body').send_keys(Keys.HOME)
-    time.sleep(2)
-    status_change(wb_driver, "1", "start")
+    wb_driver.find_element_by_tag_name('html').send_keys(Keys.PAGE_DOWN)
+    status_change(wb_driver, "6", "start")
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
 
 
-def test_03_checkif_afp_on(wb_driver):
+def test_02_checkif_lldp_on(wb_driver):
     time.sleep(2)
     # status check
-    status_check(wb_driver, "1")
+    status_check(wb_driver, "6")
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
 
 
-def test_04_turnoff_afp(wb_driver):
+def test_03_turnoff_lldp(wb_driver):
     time.sleep(2)
-    status_change(wb_driver, "1", "stop")
+    status_change(wb_driver, "6", "stop")
+    # lldp takes almost 7 sec to turn off
+    time.sleep(7)
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
 
 
-def test_05_checkif_afp_off(wb_driver):
+def test_04_checkif_lldp_off(wb_driver):
     time.sleep(2)
     # status check
-    status_check(wb_driver, "1")
+    status_check(wb_driver, "6")
     time.sleep(10)
     # taking screenshot
     test_name = sys._getframe().f_code.co_name
