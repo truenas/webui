e9f0722c6ee1d30f71c9d587ab7b413442707943
Lola Yang <lola.ly27@gmail.com>
2019-03-26 16:27:45 -0700

    change as request

--- src/app/helptext/task-calendar/snapshot/snapshot-form.ts
+++ src/app/helptext/task-calendar/snapshot/snapshot-form.ts
@@ -6,21 +6,27 @@ export default {
     dataset_tooltip: T('Select a pool, dataset, or zvol.'),
 
     recursive_placeholder: T('Recursive'),
-    recursive_tooltip: T('Set this to take separate snapshots of the\
+    recursive_tooltip: T('Set to take separate snapshots of the\
  pool/dataset and each of its child datasets. Leave unset to take a\
  single snapshot of the specified pool/dataset with <b>no</b> child datasets.'),
 
     exclude_placeholder: T('Exclude'),
-    exclude_tooltip: T(''),
+    exclude_tooltip: T('Exclude specific child datasets from the snapshot.\
+ Use with recursive snapshots. List paths to any child datasets to exclude.\
+ Example: pool1/dataset1/child1. The pool1/dataset1 recursive snapshot will\
+ include all child datasets except child1.'),
 
     lifetime_value_placeholder: T('Snapshot Lifetime'),
-
     lifetime_unit_tooltip: T('Define a length of time to retain the snapshot on this\
  system. After the time expires, the snapshot is removed. Snapshots which have\
  been replicated to other systems are not affected.'),
 
     naming_schema_placeholer: T('Naming Schema'),
-    naming_schema_tooltip: T(''),
+    naming_schema_tooltip: T('Snapshot name format string. The default is snap-%Y-%m-%d-%H-%M.\
+ Must include the strings %Y, %m, %d, %H, and %M, which are replaced with the four-digit year,\
+ month, day of month, hour, and minute as defined in strftime(3). A string showing the snapshot\
+ lifetime is appended to the name. For example, snapshots of pool1 with a Naming Schema of\
+ "customsnap-%Y%m%d.%H%M" and lifetime of two weeks have names like pool1@customsnap-20190315.0527-2w.'),
 
     begin_placeholder: T('Begin'),
     begin_tooltip: T('Choose the hour and minute when the system can begin\
