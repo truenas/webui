8ad9144065ecc7ab270c913790a3428f6fe499e9
Erin Clark <erin@ixsystems.com>
2019-03-28 14:17:59 -0700

    Clean up comments from code
    Ticket: #79086
    (cherry picked from commit 7941b52dba5484bf98028778f34d5c2b569bb902)

--- src/app/pages/storage/disks/disk-list/disk-list.component.ts
+++ src/app/pages/storage/disks/disk-list/disk-list.component.ts
@@ -102,28 +102,6 @@ export class DiskListComponent {
 	protected unused: any;
 	protected disk_pool: Map<string, string> = new Map<string, string>();
 	constructor(protected ws: WebSocketService, protected router: Router,  public diskbucket: StorageService) {
-		/*this.ws.call('boot.get_disks', []).subscribe((boot_res)=>{
-			for (let boot in boot_res) {
-				this.disk_pool.set(boot_res[boot], T('Boot Pool'));
-			}
-			this.ws.call('disk.get_unused', []).subscribe((unused_res)=>{
-				this.unused = unused_res;
-				this.unusedDisk_ready.emit(true);
-				for (let unused in unused_res) {
-					this.disk_pool.set(unused_res[unused].name, T('Unused'));
-				}
-				this.ws.call('pool.query', []).subscribe((pool_res)=>{
-					for (let pool in pool_res) {
-						this.ws.call('pool.get_disks', [pool_res[pool].id]).subscribe((res) => {
-							for (let k in res) {
-								this.disk_pool.set(res[k], pool_res[pool].name);
-							}
-							this.disk_ready.emit(true);
-						});
-					}
-				});
-			});
-		});*/
 		this.ws.call('disk.get_unused', []).subscribe((unused_res)=>{
 			this.unused = unused_res;
 		});
@@ -138,18 +116,16 @@ export class DiskListComponent {
         ]));
       }
     }];
-    //this.unusedDisk_ready.subscribe((res)=>{
-			if (_.find(this.unused, {"name": parentRow.name})) {
-	    	actions.push({
-	    		label: T("Wipe"),
-	        onClick: (row) => {
-	          this.router.navigate(new Array('/').concat([
-	            "storage", "disks", "wipe", row.name
-	          ]));
-	        }
-	    	})
-	    }
-		//});
+		if (_.find(this.unused, {"name": parentRow.name})) {
+	   	actions.push({
+	   		label: T("Wipe"),
+	       onClick: (row) => {
+	        this.router.navigate(new Array('/').concat([
+	          "storage", "disks", "wipe", row.name
+	        ]));
+	      }
+	  	})
+	  }
     return actions;
   }
 
