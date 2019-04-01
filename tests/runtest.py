0706d5ea799202516f3094d1c194233213214a4d
ericbsd <ericturgeon.bsd@gmail.com>
2019-04-01 15:36:01 -0300

    remove list of imports in runtest.py

--- tests/runtest.py
+++ tests/runtest.py
@@ -68,81 +68,6 @@ except NameError:
     print(UsageMSG)
     sys.exit(1)
 
-
-# try:
-#     driver_v
-# except NameError:
-#     from driverU import webDriver
-#     print("Running DriverU")
-#     runDriver = webDriver()
-# else:
-#     if driver_v == "F":
-#         from driverU import webDriver
-#         print("Running Firefox driver")
-#         runDriver = webDriver()
-#     elif driver_v == "G":
-#         from driverG import webDriver
-#         print("Running Selenium Grid")
-#         runDriver = webDriver(grid_server_ip)
-#     else:
-#         print("Option '%s' not allowed" % driver_v)
-#         print(UsageMSG)
-#         sys.exit(1)
-
-# # running tests
-# run_login_test(runDriver, ip)
-#     run_create_user_test(runDriver)
-#     run_create_group_test(runDriver)
-#     run_create_pool_test(runDriver)
-
-#     run_conf_netglob_test(runDriver)
-#     run_conf_netinterface_test(runDriver)
-#     run_conf_netlinkagg_test(runDriver)
-#     run_conf_netstatic_test(runDriver)
-#     run_conf_netvlan_test(runDriver)
-
-#     run_conf_sysgeneral_test(runDriver)
-#     run_conf_ntpserver_test(runDriver)
-#     run_conf_bootenv_test(runDriver)
-#     run_conf_sysadvanced_test(runDriver)
-#     run_conf_email_test(runDriver)
-#     run_conf_sysdataset_test(runDriver)
-#     run_conf_alertservices_test(runDriver)
-#     run_conf_alertsettings_test(runDriver)
-#     run_conf_cloudcreds_test(runDriver)
-#     run_conf_tunables_test(runDriver)
-# #    run_check_update_test(runDriver)
-#     run_conf_ca_test(runDriver)
-#     run_conf_certificates_test(runDriver)
-#     run_conf_support_test(runDriver)
-
-#     run_conf_taskscron_test(runDriver)
-#     run_conf_tasksinitshutscript_test(runDriver)
-#     run_conf_tasksrsync_test(runDriver)
-#     run_conf_tasksSMART_test(runDriver)
-#     run_conf_tasksperiodicSS_test(runDriver)
-#     run_conf_tasksreplication_test(runDriver)
-#     run_conf_tasksresilver_test(runDriver)
-#     run_conf_tasksscrub_test(runDriver)
-#     run_conf_taskscloudsync_test(runDriver)
-
-# #    run_conf_afp_test(runDriver)
-# # special reason for dns other services turned off until status is figured out
-# #    run_conf_dc_test(runDriver)
-# #    run_conf_dns_test(runDriver)
-# #    run_conf_ftp_test(runDriver)
-# #    run_conf_iscsi_test(runDriver)
-# #    run_conf_lldp_test(runDriver)
-# #    run_conf_smb_test(runDriver)
-# #    run_conf_ssh_test(runDriver)
-# #    run_conf_webdav_test(runDriver)
-# #    run_view_guide_test(runDriver)
-#     run_edit_test(runDriver)
-#     run_delete_test(runDriver)
-#     run_delete_pool_test(runDriver)
-#     run_change_theme_test(runDriver)
-
-
 try:
     driver_v
 except NameError:
@@ -172,7 +97,7 @@ cfg_file.close()
 
 pytestcmd = [
     "pytest-3.6",
-    "-vs",
+    "-v",
     "selenium",
     "--junitxml=results/webui_test.xml"
 ]
