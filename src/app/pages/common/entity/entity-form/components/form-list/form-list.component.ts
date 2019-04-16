2d4ff9455741dd038a452f7e3ea1d05ee66ea015
Lola Yang <lola.ly27@gmail.com>
2019-04-16 10:53:32 -0700

    style add/delete button; format code

--- src/app/pages/common/entity/entity-form/components/form-list/form-list.component.ts
+++ src/app/pages/common/entity/entity-form/components/form-list/form-list.component.ts
@@ -1,25 +1,25 @@
-import {Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, FormArray, FormBuilder, AbstractControl} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, AbstractControl } from '@angular/forms';
 
-import {FieldConfig} from '../../models/field-config.interface';
-import {Field} from '../../models/field.interface';
-import {EntityFormService} from '../../services/entity-form.service';
-import { config } from 'rxjs';
+import { FieldConfig } from '../../models/field-config.interface';
+import { Field } from '../../models/field.interface';
+import { EntityFormService } from '../../services/entity-form.service';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 
 @Component({
-  selector : 'form-list',
-  templateUrl : './form-list.component.html',
+  selector: 'form-list',
+  templateUrl: './form-list.component.html',
+  styleUrls: ['./form-list.component.css'],
 })
-export class FormListComponent implements Field, OnInit{
+export class FormListComponent implements Field, OnInit {
   config: FieldConfig;
   group: FormGroup;
   fieldShow: string;
 
-  public listLength = 1;
   public listsFromArray: FormArray;
   public formlistControl: AbstractControl;
 
-  constructor(private entityFormService: EntityFormService, private formBuilder: FormBuilder,){}
+  constructor(private entityFormService: EntityFormService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.listsFromArray = this.formBuilder.array([]);
@@ -27,20 +27,20 @@ export class FormListComponent implements Field, OnInit{
 
     this.formlistControl = this.group.controls[this.config.name];
 
-    this.listsFromArray.valueChanges.subscribe((res)=> {
-        this.formlistControl.setValue(this.getListControlValue());
+    this.listsFromArray.valueChanges.subscribe((res) => {
+      this.formlistControl.setValue(this.getListControlValue());
     })
   }
 
   getListControlValue() {
     const listsValue = [];
     for (let i = 0; i < this.listsFromArray.controls.length; i++) {
-        const listValue = [];
-        const listFormGroup = this.listsFromArray.controls[i] as FormGroup;
-        for (const prop in listFormGroup.controls) {
-          listValue[prop] = listFormGroup.controls[prop].value;
-        }
-        listsValue.push(listValue);
+      const listValue = [];
+      const listFormGroup = this.listsFromArray.controls[i] as FormGroup;
+      for (const prop in listFormGroup.controls) {
+        listValue[prop] = listFormGroup.controls[prop].value;
+      }
+      listsValue.push(listValue);
     }
     return listsValue;
   }
