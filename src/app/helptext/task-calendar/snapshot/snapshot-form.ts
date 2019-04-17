439e6722f3835be27ca15046c05f1c8d9c10eb54
Warren Block <wblock@ixsystems.com>
2019-04-17 15:41:21 -0400

    Clarify and enhance

--- src/app/helptext/task-calendar/snapshot/snapshot-form.ts
+++ src/app/helptext/task-calendar/snapshot/snapshot-form.ts
@@ -2,19 +2,19 @@ import { T } from '../../../translate-marker';
 
 
 export default {
-    dataset_placeholder: T('Pool/Dataset'),
+    dataset_placeholder: T('Dataset'),
     dataset_tooltip: T('Select a pool, dataset, or zvol.'),
 
     recursive_placeholder: T('Recursive'),
     recursive_tooltip: T('Set to take separate snapshots of the\
- pool/dataset and each of its child datasets. Leave unset to take a\
- single snapshot of the specified pool/dataset with <b>no</b> child datasets.'),
+ dataset and each of its child datasets. Leave unset to take a\
+ single snapshot only of the specified dataset <i>without</i> child datasets.'),
 
     exclude_placeholder: T('Exclude'),
     exclude_tooltip: T('Exclude specific child datasets from the snapshot.\
  Use with recursive snapshots. List paths to any child datasets to exclude.\
- Example: pool1/dataset1/child1. The pool1/dataset1 recursive snapshot will\
- include all child datasets except child1.'),
+ Example: <i>pool1/dataset1/child1</i>. A recursive snapshot of
+ <i>pool1/dataset1</i> will include all child datasets except <i>child1</i>.'),
 
     lifetime_value_placeholder: T('Snapshot Lifetime'),
     lifetime_unit_tooltip: T('Define a length of time to retain the snapshot on this\
@@ -22,23 +22,23 @@ export default {
  been replicated to other systems are not affected.'),
 
     naming_schema_placeholder: T('Naming Schema'),
-    naming_schema_tooltip: T('Snapshot name format string. The default is snap-%Y-%m-%d-%H-%M.\
- Must include the strings %Y, %m, %d, %H, and %M, which are replaced with the four-digit year,\
- month, day of month, hour, and minute as defined in strftime(3). A string showing the snapshot\
- lifetime is appended to the name. For example, snapshots of pool1 with a Naming Schema of\
- "customsnap-%Y%m%d.%H%M" and lifetime of two weeks have names like pool1@customsnap-20190315.0527-2w.'),
+    naming_schema_tooltip: T('Snapshot name format string. The default is <i>snap-%Y-%m-%d-%H-%M</i>.\
+ Must include the strings <i>%Y</i>, <i>%m</i>, <i>%d</i>, <i>%H</i>, and <i>%M</i>, which are replaced with the four-digit year,\
+ month, day of month, hour, and minute as defined in <a href="https://www.freebsd.org/cgi/man.cgi?query=strftime" target="_blank">strftime(3)</a>. A string showing the snapshot\
+ lifetime is appended to the name. For example, snapshots of <i>pool1</i> with a Naming Schema of\
+ <i>customsnap-%Y%m%d.%H%M</i> and lifetime of two weeks have names like <i>pool1@customsnap-20190315.0527-2w</i>.'),
 
     begin_placeholder: T('Begin'),
-    begin_tooltip: T('Choose the hour and minute when the system can begin\
+    begin_tooltip: T('Hour and minute the system can begin\
  taking snapshots.'),
 
     end_placeholder: T('End'),
-    end_tooltip: T('Choose the hour and minute when the system must stop\
- taking snapshots.'),
+    end_tooltip: T('Hour and minute the system must stop\
+ creating snapshots. Snapshots already in progress will continue until complete.'),
 
     snapshot_picker_placeholder: T('Schedule the Periodic Snapshot Task'),
-    snapshot_picker_tooltip: T('Choose one of the convenient presets\
- or choose <b>Custom</b> to trigger the advanced scheduler UI'),
+    snapshot_picker_tooltip: T('Choose one of the presets\
+ or choose <i>Custom</i> to use the advanced scheduler.'),
 
     enabled_placeholder: T('Enabled'),
     enabled_tooltip: T('Unset to disable this task without deleting it.'),
