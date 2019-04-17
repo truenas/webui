19d615722b07d9244a8ccb872b35d838ca602a3c
Mrt134 <timmoore88@gmail.com>
2019-04-17 14:12:15 -0400

    NAS-100724: Update Services/SMB/NetBIOS Alias helptext

--- src/app/helptext/services/components/service-smb.ts
+++ src/app/helptext/services/components/service-smb.ts
@@ -10,7 +10,8 @@ cifs_srv_netbiosname_tooltip: T('Automatically populated with the original hostn
 cifs_srv_netbiosname_validation : [ Validators.required, Validators.maxLength(15) ],
 
 cifs_srv_netbiosalias_placeholder: T('NetBIOS Alias'),
-cifs_srv_netbiosalias_tooltip: T('Enter an alias. Limited to 15 characters.'),
+cifs_srv_netbiosalias_tooltip: T('Enter any aliases, separated by spaces.\
+ Each alias cannot be longer than 15 characters.'),
 cifs_srv_netbiosalias_validation: [ Validators.maxLength(15) ],
 
 cifs_srv_workgroup_placeholder: T('Workgroup'),
