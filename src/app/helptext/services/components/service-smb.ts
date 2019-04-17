0257b736225f4504746ddb29f602fa862f798e58
Tim Moore <Mrt134@users.noreply.github.com>
2019-04-17 15:04:46 -0400

    Update src/app/helptext/services/components/service-smb.ts
    Co-Authored-By: dkmullen <dkmullen@gmail.com>

--- src/app/helptext/services/components/service-smb.ts
+++ src/app/helptext/services/components/service-smb.ts
@@ -70,7 +70,7 @@ cifs_srv_guest_tooltip: T('Account to be used for guest access. Default is\
 
 cifs_srv_admin_group_placeholder: T('Administrators Group'),
 cifs_srv_admin_group_tooltip: T('Members of this group are local admins\
- and automatically have privileges to take ownership of any file in an SMB\
+ administrators that can take ownership of any file in an SMB\
  share, reset permissions, and administer the SMB server through the\
  Computer Management MMC snap-in.'),
 cifs_srv_admin_group_validation : [ Validators.maxLength(120) ],
