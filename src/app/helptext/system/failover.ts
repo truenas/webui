aba367933d68d9959f7b8e1a7719a6831439074b
Tim Moore <Mrt134@users.noreply.github.com>
2019-04-17 14:46:19 -0700

    Apply suggestions from code review
    Co-Authored-By: erinix <erincodepirate@gmail.com>

--- src/app/helptext/system/failover.ts
+++ src/app/helptext/system/failover.ts
@@ -2,18 +2,18 @@ import { Validators } from "@angular/forms";
 import { T } from "app/translate-marker";
 
 export const helptext_system_failover = {
-  dialog_sync_to_peer_title: T("Sync to peer"),
+  dialog_sync_to_peer_title: T("Sync to Peer"),
   dialog_sync_to_peer_message: T("Are you sure you want to sync to peer?"),
-  dialog_sync_to_peer_checkbox: T("Reboot standby node (advised on node replacement)"),
+  dialog_sync_to_peer_checkbox: T("Reboot Standby Storage Controller"),
   dialog_button_ok: T('Proceed'),
 
-  dialog_sync_from_peer_title: T("Sync from peer"),
+  dialog_sync_from_peer_title: T("Sync from Peer"),
   dialog_sync_from_peer_message: T("Are you sure you want to sync from peer?"),
 
-  snackbar_sync_from_peer_message_success: T("Sync from peer successful."),
+  snackbar_sync_from_peer_message_success: T("Sync from Peer: Success!"),
   snackbar_sync_from_peer_success_action: T("Ok"),
 
-  snackbar_sync_to_peer_message_success: T("Sync to peer successful."),
+  snackbar_sync_to_peer_message_success: T("Sync to Peer: Success!"),
   snackbar_sync_to_peer_success_action: T("Ok"),
 
   disabled_placeholder: T('Disabled'),
