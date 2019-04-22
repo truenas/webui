f80a5d55ca2407309ca050b338b032333778ed20
Erin Clark <erin@ixsystems.com>
2019-04-22 16:50:44 -0700

    Add remaining missing tooltips
    NAS-100822

--- src/app/helptext/system/reporting.ts
+++ src/app/helptext/system/reporting.ts
@@ -9,12 +9,14 @@ graphite_tooltip: T('Destination hostname or IP for collectd data sent\
  by the Graphite plugin.'),
     
 graph_age_placeholder: T('Graph Age'),
-graph_age_tooltip: T(''),
+graph_age_tooltip: T('Maximum age of graph stored, in months.'),
 
 graph_points_placeholder: T('Graph Points'),
-graph_points_tooltip: T(''),
+graph_points_tooltip: T('Number of points for each hourly, daily, weekly, monthly,\
+ or yearly graph. Set this to no less than the width of the graphs in pixels.'),
 
 confirm_rrd_destroy_placeholder: T('Confirm RRD Destroy'),
-confirm_rrd_destroy_tooltip: T(''),
+confirm_rrd_destroy_tooltip: T('Destroy the reporting database. Required for changes\
+ to <b>Graph Age</b> and <b>Graph Points</b> to take effect.'),
 
 }
