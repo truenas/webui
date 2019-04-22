55cf3dd0f8a06dc5deed44040f46722de8de86cd
Warren Block <wblock@wonkity.com>
2019-04-22 15:14:07 -0400

    Add period

--- src/app/helptext/task-calendar/initshutdown/initshutdown.ts
+++ src/app/helptext/task-calendar/initshutdown/initshutdown.ts
@@ -15,7 +15,7 @@ export default {
     ini_script_tooltip: T('Select the script from an existing pool.\
      The script is run using\
      <a href=https://www.freebsd.org/cgi/man.cgi?query=sh\
-     target="_blank">sh(1)</a>'),
+     target="_blank">sh(1)</a>.'),
 
     ini_when_placeholder: T('When'),
     ini_when_tooltip: T('Select when the command or script runs:<br>\
