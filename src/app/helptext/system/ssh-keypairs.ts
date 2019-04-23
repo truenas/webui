8dd8dccc55068c881d488a14ecd1a5e5b8156fec
Lola Yang <lola.ly27@gmail.com>
2019-04-23 10:46:42 -0700

    added tooltips

--- src/app/helptext/system/ssh-keypairs.ts
+++ src/app/helptext/system/ssh-keypairs.ts
@@ -2,13 +2,15 @@ import { T } from '../../translate-marker';
 
 export default {
     name_placeholder: T('Name'),
-    name_tooltip: T(''),
+    name_tooltip: T('A unique name to identify this keypair.'),
 
     private_key_placeholder: T('Private Key'),
-    private_key_tooltip: T(''),
+    private_key_tooltip: T('See <i>Public key authentication</i> in <a href="https://www.freebsd.org/cgi/man.cgi?query=ssh"\
+ target="_blank">SSH/Authentication</a>.'),
 
     public_key_placeholder: T('Public Key'),
-    public_key_tooltip: T(''),
+    public_key_tooltip: T('See <i>Public key authentication</i> in <a href="https://www.freebsd.org/cgi/man.cgi?query=ssh"\
+ target="_blank">SSH/Authentication</a>.'),
 
     generate_key_button: T('Generate Keypair')
 }
