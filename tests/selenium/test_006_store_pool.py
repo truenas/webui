f5bd41d5065147745640761dee316b2fa64208a0
ericbsd <ericturgeon.bsd@gmail.com>
2019-05-02 15:50:23 -0300

    adding foldTable for poll UI testing and rework error_check

--- tests/selenium/test_006_store_pool.py
+++ tests/selenium/test_006_store_pool.py
@@ -1,8 +1,5 @@
-# !/usr/bin/env python
 # Author: Rishabh Chauhan
 # License: BSD
-# Location for tests  of FreeNAS new GUI
-# Test case count: 6
 
 import sys
 import os
@@ -35,8 +32,8 @@ xpaths = {
     # or
     'createpoolButton': '//*[@id="confirm-dialog__action-button"]/span',
     'breadcrumbBar': "//*[@id='breadcrumb-bar']/ul/li[2]/a",
-    'pool1Table': f"//td[contains(.,'{pool1}')]",
-    'pool2Table': f"//td[contains(.,'{pool2}')]"
+    'pool1Table': f"//mat-panel-title[contains(.,'{pool1}')]",
+    'pool2Table': f"//mat-panel-title[contains(.,'{pool2}')]"
 }
 
 
@@ -92,7 +89,12 @@ def test_03_looking_if_the_new_pool_exist(wb_driver):
     test_name = sys._getframe().f_code.co_name
     xpath = xpaths['pool1Table']
     wait = wait_on_element(wb_driver, xpath, script_name, test_name)
-    assert wait, f'loading the new pool {pool2} timeout'
+    assert wait, 'Loading pool table timeout'
+    # get the ui element
+    ui_element = wb_driver.find_element_by_xpath(xpaths['pool1Table'])
+    element_text = ui_element.text
+    # assert response
+    assert pool1 in element_text, element_text
     # taking screenshot
     take_screenshot(wb_driver, script_name, test_name)
 
@@ -130,7 +132,14 @@ def test_05_looking_if_the_new_pool_exist(wb_driver):
     test_name = sys._getframe().f_code.co_name
     xpath = xpaths['pool2Table']
     wait = wait_on_element(wb_driver, xpath, script_name, test_name)
-    assert wait, f'loading the new pool {pool2} timeout'
+    assert wait, 'Loading pool table timeout'
+    # get the ui element
+    ui_element = wb_driver.find_element_by_xpath(xpaths['pool2Table'])
+    element_text = ui_element.text
+    # assert response
+    assert pool2 in element_text, element_text
+    # taking screenshot
+    take_screenshot(wb_driver, script_name, test_name)
     # taking screenshot
     take_screenshot(wb_driver, script_name, test_name)
 
@@ -145,15 +154,9 @@ def test_06_close_navStorage(wb_driver):
 
 
 def error_check(wb_driver):
-    if is_element_present(wb_driver, '//*[contains(text(), "Close")]'):
-        if is_element_present(wb_driver, '/html/body/div[5]/div[2]/div/mat-dialog-container/error-dialog/h1'):
-            ui_element = wb_driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/mat-dialog-container/error-dialog/h1')
-            assert false, error_element
-        wb_driver.find_element_by_xpath('//*[contains(text(), "Close")]').click()
-        print("Duplicate user cannot be created")
-    if is_element_present(wb_driver, '//*[contains(text(), "Close")]'):
-        if is_element_present(wb_driver,'/html/body/div[5]/div[2]/div/mat-dialog-container/error-dialog/h1'):
-            ui_element = wb_driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/mat-dialog-container/error-dialog/h1')
-            assert false, ui_element.text
-        wb_driver.find_element_by_xpath('//*[contains(text(), "Close")]').click()
-        print("Duplicate user cannot be created")
+    closeButton = '//*[contains(text(), "Close")]'
+    xpath = '/html/body/div[5]/div[2]/div/mat-dialog-container/error-dialog/h1'
+    if is_element_present(wb_driver, closeButton):
+        ui_element = wb_driver.find_element_by_xpath(xpath)
+        wb_driver.find_element_by_xpath(closeButton).click()
+        assert False, ui_element.text
--- tests/selenium/test_086_store_delete.py
+++ tests/selenium/test_086_store_delete.py
@@ -1,7 +1,5 @@
 # Author: Rishabh Chauhan
 # License: BSD
-# Location for tests  of FreeNAS new GUI
-# Test case count: 10
 
 import sys
 import os
@@ -27,8 +25,9 @@ xpaths = {
     'poolconfirmCheckbox': '//*[@id="confirm"]/mat-checkbox/label/div',
     'confirmButton': '//div[3]/button[2]/span',
     'closeButton': '//div[2]/button/span',
-    'pool1Table': f"//td[contains(.,'{pool1}')]",
-    'pool2Table': f"//td[contains(.,'{pool2}')]"
+    'foldPoolTable': "//mat-panel-title",
+    'topPoolTable': '//td',
+    'noPool': '//mat-card-content'
 }
 
 
@@ -43,18 +42,23 @@ def test_01_nav_store_pool(wb_driver):
     # get the ui element
     ui_element = wb_driver.find_element_by_xpath(xpaths['breadcrumbBar'])
     # get the weather data
-    page_data = ui_element.text
+    element_text = ui_element.text
     # assert response
-    assert "Pools" in page_data, page_data
+    assert "Pools" in element_text, element_text
     # taking screenshot
     take_screenshot(wb_driver, script_name, test_name)
 
 
 def test_02_looking_for_pool1(wb_driver):
     test_name = sys._getframe().f_code.co_name
-    xpath = xpaths['pool1Table']
+    xpath = xpaths['foldPoolTable']
     wait = wait_on_element(wb_driver, xpath, script_name, test_name)
-    assert wait, f'loading the new pool {pool1} timeout'
+    assert wait, 'Loading pool table timeout'
+    # get the ui element
+    ui_element = wb_driver.find_element_by_xpath(xpaths['foldPoolTable'])
+    element_text = ui_element.text
+    # assert response
+    assert pool1 in element_text, element_text
     # taking screenshot
     take_screenshot(wb_driver, script_name, test_name)
 
@@ -73,22 +77,40 @@ def test_03_delete_pool1(wb_driver):
 
 def test_04_looking_for_pool2(wb_driver):
     test_name = sys._getframe().f_code.co_name
-    xpath = xpaths['pool2Table']
+    xpath = xpaths['topPoolTable']
     wait = wait_on_element(wb_driver, xpath, script_name, test_name)
-    assert wait, f'loading the new pool {pool2} timeout'
+    assert wait, 'Loading pool table timeout'
+    # get the ui element
+    ui_element = wb_driver.find_element_by_xpath(xpaths['topPoolTable'])
+    element_text = ui_element.text
+    # assert response
+    assert pool2 in element_text, element_text
     # taking screenshot
     take_screenshot(wb_driver, script_name, test_name)
 
 
 def test_05_delete_pool2(wb_driver):
-    time.sleep(1)
     test_name = sys._getframe().f_code.co_name
     pool_detach(wb_driver, pool2, script_name, test_name)
     # taking screenshot
     take_screenshot(wb_driver, script_name, test_name)
 
 
-def test_06_close_navStorage(wb_driver):
+def test_06_looking_for_pool2(wb_driver):
+    test_name = sys._getframe().f_code.co_name
+    xpath = xpaths['noPool']
+    wait = wait_on_element(wb_driver, xpath, script_name, test_name)
+    assert wait, 'Loading pool table timeout'
+    # get the ui element
+    ui_element = wb_driver.find_element_by_xpath(xpaths['noPool'])
+    element_text = ui_element.text
+    # assert response
+    assert 'No pools' in element_text, element_text
+    # taking screenshot
+    take_screenshot(wb_driver, script_name, test_name)
+
+
+def test_07_close_navStorage(wb_driver):
     wb_driver.find_element_by_xpath(xpaths['navStorage']).click()
     test_name = sys._getframe().f_code.co_name
     take_screenshot(wb_driver, script_name, test_name)
