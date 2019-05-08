fa205d313fe59a8603b915cb16c2badbac7ca6f3
Tim Moore <Mrt134@users.noreply.github.com>
2019-05-08 14:14:13 -0700

    Update src/app/helptext/topbar.ts
    Co-Authored-By: erinix <erincodepirate@gmail.com>

--- src/app/helptext/topbar.ts
+++ src/app/helptext/topbar.ts
@@ -6,7 +6,7 @@ export default {
     ha_is_enabled : T('HA is enabled'),
     ha_status_text_disabled : T('HA Disabled'),
     ha_disabled_reasons : {
-        NO_VOLUME : T('No pools are configured'),
+        NO_VOLUME : T('No pools are configured.'),
         NO_VIP : T('No interfaces configured with Virtual IP'),
         NO_SYSTEM_READY : T('Other storage controller has not finished booting.'),
         NO_PONG : T('Other storage controller cannot be reached.'),
