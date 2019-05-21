f06d202e24923de79fe186ab9d026ee88e080c40
Dennis Mullen <dkmullen@gmail.com>
2019-05-21 10:43:47 -0400

    NAS-100177 finish dialog

--- src/app/components/common/dialog/directory-services-monitor/directory-services-monitor.component.ts
+++ src/app/components/common/dialog/directory-services-monitor/directory-services-monitor.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { MatTableDataSource, MatDialogTitle, MatTable } from '@angular/material';
-
 import { WebSocketService } from '../../../../services/';
 
 @Component({
@@ -20,49 +18,22 @@ import { WebSocketService } from '../../../../services/';
 export class DirectoryServicesMonitorComponent implements OnInit {
   displayedColumns: string[] = ['icon', 'name', 'state'];
   dataSource = [];
-  @ViewChild('dirServiceTable') dirServiceTable: MatTable<any>;
 
   constructor(private ws: WebSocketService, ) {}
 
   ngOnInit() {
     const tempArr = [];
     this.ws.call('activedirectory.get_state').subscribe((res) => {
-      const icon = this.getIcon(res);
-      tempArr.push({icon: icon, name: 'Active Directory', state: res});
+      tempArr.push({name: 'Active Directory', state: res});
 
       this.ws.call('ldap.get_state').subscribe((res) => {
-        const icon = this.getIcon(res);
-        tempArr.push({icon: icon, name: 'LDAP', state: res});
+        tempArr.push({name: 'LDAP', state: res});
 
         this.ws.call('nis.get_state').subscribe((res) => {
-          const icon = this.getIcon(res);
-          tempArr.push({icon: icon, name: 'NIS', state: res});
-          console.log(tempArr);
+          tempArr.push({name: 'NIS', state: res});
           this.dataSource = tempArr;
         });
       });
     });
-  }
-
-  getIcon(state) {
-    switch(state) {
-      case "DISABLED":
-        return 'remove_circle';
-        break;
-      case 'HEALTHY':
-        return 'check_circle'
-        break;
-      case 'FAULTED':
-        return 'highlight_off'
-        break;
-      case 'JOINING':
-        return 'arrow_forward'
-        break;
-      case 'LEAVING':
-        return 'arrow_back'
-        break;
-      default:
-        return 'remove_circle'
-    }
   }
 }
