edbd79a73f261e09cbe9fbb5108e32bfedcf9300
Dennis Mullen <dkmullen@gmail.com>
2019-05-22 12:51:16 -0400

    NAS-101524 title fix

--- src/app/pages/common/error-dialog/error-dialog.component.ts
+++ src/app/pages/common/error-dialog/error-dialog.component.ts
@@ -1,7 +1,6 @@
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { MatDialogRef } from '@angular/material';
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { DialogService } from 'app/services';
 
 @Component({
   selector: 'error-dialog',
@@ -17,9 +16,10 @@ export class ErrorDialog {
 
   constructor(public dialogRef: MatDialogRef < ErrorDialog >, public translate: TranslateService ) {}
 
-  public toggleOpen (data) {
+  public toggleOpen () {
     const messageWrapper = document.getElementById('err-message-wrapper');
     const dialog = document.getElementsByClassName('mat-dialog-container');
+    const title = document.getElementById('err-title');
     const content = document.getElementById('err-md-content');
     const btPanel = document.getElementById('err-bt-panel');
     const txtarea = document.getElementById('err-bt-text');
@@ -27,11 +27,12 @@ export class ErrorDialog {
     this.isCloseMoreInfo = !this.isCloseMoreInfo;
     if (!this.isCloseMoreInfo) {
       dialog[dialog.length-1].setAttribute('style','width : 800px; height: 600px');
-      let titleHeight = (document.getElementById('err-message-wrapper').offsetHeight)-21;
-      if (titleHeight > 63) {
-        titleHeight = 63;
+      let errMsgHeight = (document.getElementById('err-message-wrapper').offsetHeight)-21;
+      if (errMsgHeight > 63) {
+        errMsgHeight = 63;
       };
-      const tracebackHeight = (400-titleHeight).toString() + 'px';
+      const tracebackHeight = (400-errMsgHeight).toString() + 'px';
+      title.setAttribute('style', 'height: 40px; overflow: hidden');
       content.setAttribute('style', 'height: 450px');
       messageWrapper.setAttribute('style', 'max-height: 63px; overflow: auto');
       btPanel.setAttribute('style', 'width: 750px; max-height: 400px');
@@ -39,6 +40,7 @@ export class ErrorDialog {
       txtarea.style.height = tracebackHeight;
     } else {
       dialog[dialog.length-1].removeAttribute('style');
+      title.removeAttribute('style');
       content.removeAttribute('style');
       btPanel.removeAttribute('style');
       messageWrapper.removeAttribute('style');
