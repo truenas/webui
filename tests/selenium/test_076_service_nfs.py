83385e59d18a40b22c683eabac8053b534192670
ericbsd <ericturgeon.bsd@gmail.com>
2019-05-30 08:53:45 -0300

    adding tests to save nsf configuration and to reverse configuration

--- tests/selenium/test_076_service_nfs.py
+++ tests/selenium/test_076_service_nfs.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 cwd = str(os.getcwd())
 sys.path.append(cwd)
 from function import take_screenshot, status_change, status_check
-
+from function import is_element_present
 skip_mesages = "Skipping first run"
 script_name = os.path.basename(__file__).partition('.')[0]
 
@@ -16,7 +16,8 @@ xpaths = {
     'breadcrumbBar1': "//div[@id='breadcrumb-bar']/ul/li/a",
     'breadcrumbBar2': "//*[@id='breadcrumb-bar']/ul/li[2]/a",
     'nsfv4Checkbox': '//div[5]/form-checkbox/div/mat-checkbox/label/div',
-    'verifyNsfv4Check': '//div[5]/form-checkbox/div/mat-checkbox'
+    'verifyNsfv4Check': '//div[5]/form-checkbox/div/mat-checkbox',
+    'saveButton': "//button[@id='save_button']/span"
 }
 
 
@@ -58,3 +59,50 @@ def test_03_check_enable_nfsv4(wb_driver):
     root_checkbox = wb_driver.find_element_by_xpath(xpaths['verifyNsfv4Check'])
     class_value = root_checkbox.get_attribute('class')
     assert 'mat-checkbox-checked' in class_value, class_value
+
+
+def test_04_save_nfs_configuration(wb_driver):
+    # click on save button
+    wb_driver.find_element_by_xpath(xpaths['saveButton']).click()
+    time.sleep(5)
+    # taking screenshot
+    test_name = sys._getframe().f_code.co_name
+    take_screenshot(wb_driver, script_name, test_name)
+    assert is_element_present(wb_driver, xpaths['breadcrumbBar2']) is False
+
+
+def test_10_navigate_back_to_configure_nfs(wb_driver):
+    # click on configure button
+    wb_driver.find_element_by_xpath(xpaths['configButton']).click()
+    ui_element = wb_driver.find_element_by_xpath(xpaths['breadcrumbBar1'])
+    # get the weather data
+    page_data = ui_element.text
+    # assert response
+    assert "Services" in page_data, page_data
+    ui_element = wb_driver.find_element_by_xpath(xpaths['breadcrumbBar2'])
+    # get the weather data
+    page_data = ui_element.text
+    # assert response
+    assert "NFS" in page_data, page_data
+    test_name = sys._getframe().f_code.co_name
+    take_screenshot(wb_driver, script_name, test_name)
+
+
+def test_11_uncheck_enable_nfsv4(wb_driver):
+    # unchecked on Login as Root with Password
+    wb_driver.find_element_by_xpath(xpaths['nsfv4Checkbox']).click()
+    test_name = sys._getframe().f_code.co_name
+    take_screenshot(wb_driver, script_name, test_name)
+    root_checkbox = wb_driver.find_element_by_xpath(xpaths['verifyNsfv4Check'])
+    class_value = root_checkbox.get_attribute('class')
+    assert 'mat-checkbox-checked' in class_value, class_value
+
+
+def test_12_save_nfs_configuration(wb_driver):
+    # click on save button
+    wb_driver.find_element_by_xpath(xpaths['saveButton']).click()
+    time.sleep(5)
+    # taking screenshot
+    test_name = sys._getframe().f_code.co_name
+    take_screenshot(wb_driver, script_name, test_name)
+    assert is_element_present(wb_driver, xpaths['breadcrumbBar2']) is False
