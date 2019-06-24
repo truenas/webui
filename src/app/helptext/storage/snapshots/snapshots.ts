d2cee006599ee5debbcc4276d86f77114a4d1f6d
Aaron Ervin <aervin@ixsystems.com>
2019-06-24 09:35:09 -0500

    NAS-102156 -- translation markers in storage/snapshots helptext

--- src/app/helptext/storage/snapshots/snapshots.ts
+++ src/app/helptext/storage/snapshots/snapshots.ts
@@ -7,17 +7,17 @@ snapshot_add_dataset_placeholder: T('Pool/Dataset'),
 snapshot_add_dataset_tooltip: T('Select an existing ZFS pool, dataset, or zvol.'),
 snapshot_add_dataset_validation: [Validators.required],
 
-snapshot_add_name_placeholder: 'Name',
+snapshot_add_name_placeholder: T('Name'),
 snapshot_add_name_tooltip: T('Add a name for the new snapshot'),
 
-snapshot_add_naming_schema_placeholder: 'Naming Schema',
+snapshot_add_naming_schema_placeholder: T('Naming Schema'),
 snapshot_add_naming_schema_tooltip: T('Add a naming schema for the new snapshot'),
 
-snapshot_add_recursive_placeholder: 'Recursive',
+snapshot_add_recursive_placeholder: T('Recursive'),
 snapshot_add_recursive_tooltip: T('Set to include child datasets of the chosen dataset.'),
 
 // storage/snapshots/snapshot-clone
-snapshot_clone_name_placeholder: 'Name',
+snapshot_clone_name_placeholder: T('Name'),
 snapshot_clone_name_tooltip: T('Enter a name for the cloned snapshot.'),
 snapshot_clone_name_validation : [ Validators.required ],
 
