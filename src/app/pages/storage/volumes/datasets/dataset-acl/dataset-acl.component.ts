6ae32cb40b8111e3fc8ce66d96deaf055bd3eb0d
Mrt134 <timmoore88@gmail.com>
2019-07-10 13:16:44 -0400

    NAS-102047: Add help text to ACL Manager fields - Fix a couple help text links in dataset-acl.component.ts - Yarn build testing: links to 11.3 documentation are broke, but that is expected until that guide is published.

--- src/app/pages/storage/volumes/datasets/dataset-acl/dataset-acl.component.ts
+++ src/app/pages/storage/volumes/datasets/dataset-acl/dataset-acl.component.ts
@@ -140,7 +140,7 @@ export class DatasetAclComponent implements OnDestroy {
           type: 'select',
           name: 'perms_type',
           placeholder: helptext.dataset_acl_perms_type_placeholder,
-          tooltip: helptext.dataset_acl_perms_type_placeholder,
+          tooltip: helptext.dataset_acl_perms_type_tooltip,
           options: helptext.dataset_acl_perms_type_options,
         },
         {
@@ -163,7 +163,7 @@ export class DatasetAclComponent implements OnDestroy {
           type: 'select',
           name: 'flags_type',
           placeholder: helptext.dataset_acl_flags_type_placeholder,
-          tooltip: helptext.dataset_acl_flags_type_placeholder,
+          tooltip: helptext.dataset_acl_flags_type_tooltip,
           options: helptext.dataset_acl_flags_type_options,
         },
         {
