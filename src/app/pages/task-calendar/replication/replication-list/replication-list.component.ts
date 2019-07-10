9be61cfb8472a763e2adb881cf6e75b552eba2d0
Aaron Ervin <aervin@ixsystems.com>
2019-07-10 13:37:59 -0500

    NAS-102098 -- column order

--- src/app/pages/task-calendar/replication/replication-list/replication-list.component.ts
+++ src/app/pages/task-calendar/replication/replication-list/replication-list.component.ts
@@ -24,16 +24,16 @@ export class ReplicationListComponent {
 
     public columns: Array<any> = [
         { name: 'Name', prop: 'name' },
-        { name: 'Source Dataset', prop: 'source_datasets', hidden: true },
-        { name: 'Target Dataset', prop: 'target_dataset', hidden: true },
+        { name: 'Direction', prop: 'direction', hidden: true},
+        { name: 'Transport', prop: 'transport', hidden: true},
+        { name: 'SSH Connection', prop: 'ssh_connection', hidden: true},
+        { name: 'Source Dataset', prop: 'source_datasets', hidden: true},
+        { name: 'Target Dataset', prop: 'target_dataset', hidden: true},
+        { name: 'Recursive', prop: 'recursive', hidden: true},
+        { name: 'Auto', prop: 'auto', hidden: true},
         { name: 'Enabled', prop: 'enabled' },
         { name: 'State', prop: 'task_state', state: 'state' },
         { name: 'Last Snapshot', prop: 'task_last_snapshot' },
-        { name: 'Direction', prop: 'direction', hidden: true },
-        { name: 'Transport', prop: 'transport', hidden: true },
-        { name: 'SSH Connection', prop: 'ssh_connection', hidden: true },
-        { name: 'Recursive', prop: 'recursive', hidden: true },
-        { name: 'Auto', prop: 'auto', hidden: true }
     ];
     public detailsConf = {
       direction: 'horizontal',
