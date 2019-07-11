7d59bf091501db9a1867e5ffcb4c2bc3afd09a45
Lola Yang <lola.ly27@gmail.com>
2019-07-11 14:44:03 -0700

    NAS-100046 able to add input value to list; able to delete list item

--- src/app/pages/sharing/iscsi/initiator/initiator-form/dynamic-list/dynamic-list-component.ts
+++ src/app/pages/sharing/iscsi/initiator/initiator-form/dynamic-list/dynamic-list-component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { selector } from 'd3';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, AbstractControl } from '@angular/forms';
 
-@Component ({
+@Component({
     selector: 'app-dynamic-list',
     templateUrl: './dynamic-list.component.html',
     styleUrls: ['./dynamic-list.component.css']
@@ -10,21 +9,32 @@ import { FormGroup } from '@angular/forms';
 export class DynamciListComponent implements OnInit {
     @Input() config: any;
     @Input() group: FormGroup;
-    // @Input() lists: any;
     @Input() source: any;
 
     public lists: any;
-    constructor() {}
+    protected listControl: AbstractControl;
+    protected inputControl: AbstractControl;
+
+    constructor() { }
 
     ngOnInit() {
-
-        this.lists = this.group.controls[this.config.name.substring(0, this.config.name.length - 4)].value;
+        this.inputControl = this.group.controls[this.config.name];
+        this.listControl = this.group.controls[this.config.name.substring(0, this.config.name.length - 4)];
+        this.lists = this.listControl.value;
         console.log(this.config, this.group, this.lists);
         this.source.selectionChange.subscribe((res) => {
             console.log(res);
 
-                console.log(this.source.selectedOptions);
-                
+            console.log(this.source.selectedOptions);
+
         })
+    }
+
+    add() {
+        this.listControl.value.push(this.inputControl.value);
+        this.inputControl.setValue(undefined);
+    }
+    delete(index) {
+        this.listControl.value.splice(index, 1);
     }
 }
