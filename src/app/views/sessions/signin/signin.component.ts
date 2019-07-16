843802da480659a9a621ceee4530a0da2a6e95f1
Dennis Mullen <dkmullen@gmail.com>
2019-07-16 16:27:45 -0400

    NAS-100172 use global vars after all

--- src/app/views/sessions/signin/signin.component.ts
+++ src/app/views/sessions/signin/signin.component.ts
@@ -43,9 +43,9 @@ export class SigninComponent implements OnInit {
   public failover_status = '';
   public failover_statuses = {
     'SINGLE': "",
-    'MASTER': T("Active TrueNAS Controller."),
-    'BACKUP': T("Passive TrueNAS Controller."),
-    'ELECTING': T("Electing TrueNAS Controller."),
+    'MASTER': T(`Active ${globalHelptext.Ctrlr}.`),
+    'BACKUP': T(`Passive ${globalHelptext.Ctrlr}.`),
+    'ELECTING': T(`Electing ${globalHelptext.Ctrlr}.`),
     'IMPORTING': T("Importing pools."),
     'ERROR': T("Failover is in an error state.")
   }
