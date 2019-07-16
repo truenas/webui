0ba0a018860b2ad0932fe80e8a523d564606473c
Dennis Mullen <dkmullen@gmail.com>
2019-07-16 10:57:11 -0400

    NAS-102196 NAS-100172 change label to TrueNAS Controller

--- src/app/helptext/system/failover.ts
+++ src/app/helptext/system/failover.ts
@@ -8,7 +8,7 @@ export const helptext_system_failover = {
 
   dialog_sync_to_peer_title: T("Sync to Peer"),
   dialog_sync_to_peer_message: T("Are you sure you want to sync to peer?"),
-  dialog_sync_to_peer_checkbox: T("Reboot Standby Storage Controller"),
+  dialog_sync_to_peer_checkbox: T("Reboot Standby TrueNAS Controller"),
   dialog_button_ok: T('Proceed'),
 
   dialog_sync_from_peer_title: T("Sync from Peer"),
