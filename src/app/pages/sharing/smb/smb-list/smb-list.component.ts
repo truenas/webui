7b9972ddc57510c94671f43aca8e1d4adb2cd465
Aaron Ervin <aervin@ixsystems.com>
2019-07-17 08:35:47 -0500

    NAS-102087 -- smb-list actions

--- src/app/pages/sharing/smb/smb-list/smb-list.component.ts
+++ src/app/pages/sharing/smb/smb-list/smb-list.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { delete_share_message, helptext_sharing_smb } from 'app/helptext/sharing';
+import { EntityTableComponent } from 'app/pages/common/entity/entity-table';
+import { EntityUtils } from 'app/pages/common/entity/utils';
+import { DialogService, WebSocketService } from 'app/services';
 import { T } from 'app/translate-marker';
+import { map } from 'rxjs/operators';
  
 @Component({
   selector : 'app-smb-list',
@@ -15,6 +20,7 @@ export class SMBListComponent {
   protected route_add_tooltip: string = "Add Windows (SMB) Share";
   protected route_edit: string[] = [ 'sharing', 'smb', 'edit' ];
   protected route_delete: string[] = [ 'sharing', 'smb', 'delete' ];
+  private entityList: EntityTableComponent;
 
   public columns: any[] = [
     {name: helptext_sharing_smb.column_name, prop: 'cifs_name'},
@@ -33,5 +39,43 @@ export class SMBListComponent {
     title: T('Unshare'),
     button: T('Unshare'),
     buildMessage: share => delete_share_message(share.cifs_name)
+  }
+
+  constructor(private ws: WebSocketService, private router: Router, private dialogService: DialogService) {}
+
+  afterInit(entityList: any) {
+    this.entityList = entityList;
+  }
+
+  getActions(): any[] {
+    return [
+      {
+        id: "edit",
+        label: "Edit",
+        onClick: row => this.entityList.doEdit(row.id)
+      },
+      {
+        id: "edit_acl",
+        label: helptext_sharing_smb.action_edit_acl,
+        onClick: row => {
+          const datasetId = row.cifs_path.replace("/mnt/", "");
+          this.ws
+            .call("pool.dataset.query", [[["id", "=", datasetId]]])
+            .pipe(map(datasets => datasets[0]))
+            .subscribe(
+              dataset =>
+                this.router.navigate(
+                  ["/"].concat(["storage", "pools", "id", dataset.pool, "dataset", "acl", datasetId])
+                ),
+              error => new EntityUtils().handleWSError(this, error, this.dialogService)
+            );
+        }
+      },
+      {
+        id: "delete",
+        label: "Delete",
+        onClick: row => this.entityList.doDelete(row)
+      }
+    ];
   }
 }
