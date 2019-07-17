05f2a22d2a824a79efb91a1e0fcb2cffa6c95c77
Dennis Mullen <dkmullen@gmail.com>
2019-07-17 13:20:20 -0400

    NAS-101932 explicitly call redirect on log in

--- src/app/views/sessions/signin/signin.component.ts
+++ src/app/views/sessions/signin/signin.component.ts
@@ -235,6 +235,7 @@ export class SigninComponent implements OnInit {
     this.ws.call('auth.generate_token', [300]).subscribe((result) => {
       if (result) {
         this.ws.token = result;
+        this.redirect()
       }
     });
   }
