a3d8a35b53fdeb0702c712667ea272fe1626b6d0
Dennis Mullen <dkmullen@gmail.com>
2019-07-26 15:00:00 -0400

    NAS-102554 Docs changes

--- src/app/helptext/storage/volumes/volume-key.ts
+++ src/app/helptext/storage/volumes/volume-key.ts
@@ -1,12 +1,21 @@
 import {Validators} from '@angular/forms';
 import { T } from '../../../translate-marker';
 
+const passphrase_msg = T(' a passphrase creates a new \
+ encryption key file and invalidates any existing recovery key  or encryption key files. A dialog will open to save \
+ a backup of the new encryption key. A new recovery key can be added \
+ from the Encryption Operations menu.')
+
 export default {
 // Add key form
 add_key_name_validation: [Validators.required],
-add_key_instructions: T('Adding a recovery key invalidates any previous recovery key. The new recovery \
- key will download automatically. Back up this key. No copy of the key is stored by the system. This \
- key can be used instead of the password to unlock this pool.'),
+add_key_instructions: T('This is an additional key file that can be used to unlock the \
+ pool instead of the passphrase or encryption key file. <br /> <br /> \
+ Entering the administrator password and clicking <b>ADD RECOVERY KEY</b> generates a \
+ single recovery key file and presents it for download. <br /> \
+ This file is not stored on the system. <br/> \
+ Store the recovery key file in a secure location! <br /> \
+ Adding a new recovery key invalidates any previously downloaded recovery key file for this pool.'),
 add_key_password_placeholder: T('Root password'),
 add_key_password_tooltip: T('Enter the root password to authorize this operation.'),
 add_key_password_validation: [Validators.required],
@@ -16,18 +25,12 @@ changekey_adminpw_placeholder: T('Root Password'),
 changekey_adminpw_tooltip: T('Enter the root password.'),
 changekey_adminpw_validation: [Validators.required],
 
-changekey_instructions: T('Creating a passphrase requires the creation of a new \
- encryption key and invalidates any existing recovery key. A dialog will open to save \
- a backup of the new encryption key. A new recovery key, if desired, can be added \
- from the encryption menu.'),
+changekey_instructions: T('Creating') + passphrase_msg,
 changekey_passphrase_placeholder: T('Passphrase'),
 changekey_passphrase_tooltip: T('Enter the new passphrase for the encryption key.'),
 changekey_passphrase_validation: [Validators.required],
 
-changekey_instructions2: T('Changing a passphrase requires the creation of a new \
- encryption key and invalidates any existing recovery key. A dialog will open to save \
- a backup of the new encryption key. A new recovery key, if desired, can be added \
- from the encryption menu.'),
+changekey_instructions2: T('Changing') + passphrase_msg,
 changekey_passphrase2_placeholder: T('Verify passphrase'),
 changekey_passphrase2_tooltip: T('Confirm the encryption key passphrase.'),
 changekey_passphrase2_validation: [Validators.required],
@@ -37,7 +40,7 @@ changekey_remove_passphrase_tooltip: T('Select to remove the passphrase from thi
 
 // Create key form
 createkey_passphrase_placeholder: T('Passphrase'),
-createkey_passphrase_tooltip: T('Enter the new passphrase for the encryption key.'),
+createkey_passphrase_tooltip: T('Enter the new encryption key passphrase.'),
 createkey_passphrase_validation: [Validators.required],
 
 createkey_passphrase2_placeholder: T('Verify passphrase'),
@@ -45,10 +48,11 @@ createkey_passphrase2_tooltip: T('Confirm the encryption key passphrase.'),
 createkey_passphrase2_validation: [Validators.required],
 
 // Rekey form
-rekey_instructions: T('Rekeying generates a new encryption key, replacing the old one on the disks in the pool.\
- Passphrases will be removed by this operation, and any existing recovery key will be invalidated. \
- A dialog will open to save a backup of the new encryption key. A new recovery key, if desired, \
- can be added from the encryption menu.'),
+rekey_instructions: T('Re-keying the pool resets the encryption on the GELI master key of each encrypted disk. <br /> \
+ This invalidates all existing encryption and recovery key files.<br /> \
+ An encryption key passphrase is also removed.<br /> \
+ A new encryption key file is generated and an option to save a backup of this file will open.<br /> \
+ Adding a passphrase or recovery key file can be done from the Encryption Operations menu.'),
 rekey_password_label: T('Passphrase'),
 rekey_password_placeholder: T('Root password'),
 rekey_password_tooltip: T('Enter the root password to authorize this operation.'),
