71a203d591c0251b2c6dc1e95b40c2d5aeb099bc
Dennis Mullen <dkmullen@gmail.com>
2019-07-26 10:35:17 -0400

    NAS-102000 almost there

--- src/app/pages/network/interfaces/interfaces-form/interfaces-form.component.ts
+++ src/app/pages/network/interfaces/interfaces-form/interfaces-form.component.ts
@@ -180,8 +180,8 @@ export class InterfacesFormComponent implements OnDestroy {
             name: 'failover_address',
             placeholder: helptext.failover_alias_address_placeholder,
             tooltip: helptext.failover_alias_address_tooltip,
-            // disabled: true,
-            // isHidden: true,
+            disabled: true,
+            isHidden: true,
             type: 'ipwithnetmask',
             width: '55%',
             validation : [ regexValidator(this.networkService.ipv4_or_ipv6_cidr) ],
@@ -191,8 +191,8 @@ export class InterfacesFormComponent implements OnDestroy {
             name: 'failover_virtual_address',
             placeholder: helptext.failover_virtual_alias_address_placeholder,
             tooltip: helptext.failover_virtual_alias_address_tooltip,
-            // disabled: true,
-            // isHidden: true,
+            disabled: true,
+            isHidden: true,
             type: 'ipwithnetmask',
             width: '55%',
             netmaskPreset: 32,
@@ -356,7 +356,6 @@ export class InterfacesFormComponent implements OnDestroy {
   }
 
   resourceTransformIncomingRestData(data) {
-    console.log(data)
     const aliases = data['aliases'];
     const a = [];
     const failover_aliases = data['failover_aliases'];
@@ -400,33 +399,26 @@ export class InterfacesFormComponent implements OnDestroy {
     const propValues = ['address', 'failover_address', 'failover_virtual_address'];
     if (!entityForm.isNew) {
       const data = entityForm.queryResponse[0];
-      // console.log(data)
       for (const prop in data) {
         if (entityForm.formGroup.controls[prop] && !propNames.includes(prop)) {
           entityForm.formGroup.controls[prop].setValue(data[prop]);
         }
       }
-      
-      //   if (Object.keys(data).includes(propName)) {
-          const aliases_fg = entityForm.formGroup.controls['aliases'];
-          const aliasList = data['aliases'];
-          for (let i = 0; i < aliasList.length; i++) {
-            propNames.forEach((propName) => {
-              if (Object.keys(data).includes(propName)) {
-            if (aliases_fg.controls[i] === undefined) {
-              const templateListField = _.cloneDeep(_.find(this.fieldConfig, {'name': propName}).templateListField);
-              aliases_fg.push(entityForm.entityFormService.createFormGroup(templateListField));
-              this.aliases_fc.listFields.push(templateListField);
-            }
-            aliases_fg.controls[i].controls[propValues[propNames.indexOf(propName)]]
-              .setValue(aliasList[i][propValues[propNames.indexOf(propName)]]);
-            // console.log(aliasList[i][propValues[propNames.indexOf(propName)]])
-            console.log(aliases_fg.controls[i].controls[propValues[propNames.indexOf(propName)]])
-              }
-            })
+      const aliases_fg = entityForm.formGroup.controls['aliases'];
+      const aliasList = data['aliases'];
+      for (let i = 0; i < aliasList.length; i++) {
+        propNames.forEach((propName) => {
+          if (Object.keys(data).includes(propName)) {
+        if (aliases_fg.controls[i] === undefined) {
+          const templateListField = _.cloneDeep(_.find(this.fieldConfig, {'name': propName}).templateListField);
+          aliases_fg.push(entityForm.entityFormService.createFormGroup(templateListField));
+          this.aliases_fc.listFields.push(templateListField);
+        }
+        aliases_fg.controls[i].controls[propValues[propNames.indexOf(propName)]]
+          .setValue(aliasList[i][propValues[propNames.indexOf(propName)]]);
           }
-          // console.log(aliases_fg.controls)
-     
+        })
+      }
     }
   }
 
