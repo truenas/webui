4991076604438ac89ff7141ba5f52dfeb62bb677
Kris Moore <kris@ixsystems.com>
2019-07-31 12:19:45 -0400

    Auto-change the debian/changelog using dch helper

--- mk-deb.sh
+++ mk-deb.sh
@@ -9,4 +9,7 @@ yarnpkg install
 tar cvzf node_files.tgz node_modules/
 rm -rf node_modules
 
+DATESTAMP=$(date +%Y%m%d%H%M)
+dch -v ${DATESTAMP}~truenas+1 --force-distribution --distribution bullseye-truenas-unstable "Auto Update from Jenkins CI"
+
 dpkg-buildpackage --sign-key=${1}
