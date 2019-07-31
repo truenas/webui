30e22f468438ec668d7f327803e39bdc724e3cd7
Kris Moore <kris@ixsystems.com>
2019-07-30 20:12:27 -0400

    Add Jenkins pipeline

--- mk-deb.sh
+++ mk-deb.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ -z "$1" ] ; then
+	echo "Missing GPG signing key"
+	exit 1
+fi
+
 yarnpkg install
 tar cvzf node_files.tgz node_modules/
 rm -rf node_modules
+
+dpkg-buildpackage --sign-key=${1}
