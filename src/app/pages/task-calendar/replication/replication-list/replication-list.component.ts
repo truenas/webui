4ecd8b6c59d49691ce18e6f9c917c50c409d1321
Lola Yang <lola.ly27@gmail.com>
2019-08-12 15:05:28 -0700

    NAS-102330 show direction column as default

--- src/app/pages/task-calendar/replication/replication-list/replication-list.component.ts
+++ src/app/pages/task-calendar/replication/replication-list/replication-list.component.ts
@@ -23,14 +23,14 @@ export class ReplicationListComponent {
 
     public columns: Array<any> = [
         { name: 'Name', prop: 'name' },
-        { name: 'Direction', prop: 'direction', hidden: true},
+        { name: 'Direction', prop: 'direction'},
         { name: 'Transport', prop: 'transport', hidden: true},
         { name: 'SSH Connection', prop: 'ssh_connection', hidden: true},
         { name: 'Source Dataset', prop: 'source_datasets', hidden: true},
         { name: 'Target Dataset', prop: 'target_dataset', hidden: true},
         { name: 'Recursive', prop: 'recursive', hidden: true},
         { name: 'Auto', prop: 'auto', hidden: true},
-        { name: 'Enabled', prop: 'enabled' },
+        { name: 'Enabled', prop: 'enabled', hidden: true },
         { name: 'State', prop: 'task_state', state: 'state' },
         { name: 'Last Snapshot', prop: 'task_last_snapshot' }
     ];
