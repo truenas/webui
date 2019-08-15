4c7a5ec45e40eac2c524693ca2fbeda015eb2486
Lola Yang <lola.ly27@gmail.com>
2019-08-15 16:36:13 -0700

    NAS-102745 clean and format code

--- src/app/pages/storage/volumes/datasets/dataset-permissions/dataset-permissions.component.ts
+++ src/app/pages/storage/volumes/datasets/dataset-permissions/dataset-permissions.component.ts
@@ -1,39 +1,28 @@
 import { Component, OnDestroy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
 import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
-import * as _ from 'lodash';
-import { Subscription } from 'rxjs';
 import helptext from '../../../../../helptext/storage/volumes/datasets/dataset-permissions';
-import { DialogService, RestService, StorageService, WebSocketService } from '../../../../../services/';
-import { UserService } from '../../../../../services/user.service';
+import { DialogService, StorageService, WebSocketService, UserService } from '../../../../../services/';
 import { T } from '../../../../../translate-marker';
-import { FieldConfig } from '../../../../common/entity/entity-form/models/field-config.interface';
+import * as _ from 'lodash';
 
 @Component({
-  selector : 'app-dataset-permissions',
-  template : `<entity-form [conf]="this"></entity-form>`
+  selector: 'app-dataset-permissions',
+  template: `<entity-form [conf]="this"></entity-form>`
 })
 export class DatasetPermissionsComponent implements OnDestroy {
 
   protected editCall = 'pool.dataset.permission';
   protected datasetPath: string;
-  protected id: any;
-  protected user: any;
-  protected group: any;
-  protected mode: any;
   protected recursive: any;
   protected recursive_subscription: any;
-  public sub: Subscription;
   public formGroup: FormGroup;
-  public data: Object = {};
   public error: string;
-  public busy: Subscription;
-  protected fs: any = (<any>window).filesize;
-  protected route_success: string[] = [ 'storage', 'pools' ];
+  protected route_success: string[] = ['storage', 'pools'];
   protected isEntity = true;
 
-  public fieldConfig: FieldConfig[] = [];
   public fieldSets: FieldSet[] = [
     {
       name: helptext.heading_dataset_path,
@@ -41,16 +30,16 @@ export class DatasetPermissionsComponent implements OnDestroy {
       config: [
         {
           type: 'input',
-          name : 'id',
-          placeholder : helptext.dataset_permissions_id_placeholder,
+          name: 'id',
+          placeholder: helptext.dataset_permissions_id_placeholder,
           readonly: true
         }
       ],
       width: '100%'
     },
     {
-      name:'divider',
-      divider:true
+      name: 'divider',
+      divider: true
     },
     {
       name: helptext.heading_who,
@@ -94,8 +83,8 @@ export class DatasetPermissionsComponent implements OnDestroy {
       width: '50%'
     },
     {
-      name:'divider',
-      divider:true
+      name: 'divider',
+      divider: true
     },
     {
       name: helptext.heading_advanced,
@@ -119,49 +108,49 @@ export class DatasetPermissionsComponent implements OnDestroy {
       width: '100%'
     },
     {
-      name:'divider',
-      divider:true
+      name: 'divider',
+      divider: true
     }
   ];
-
   protected datasetMode: any;
 
-  constructor(protected router: Router, protected route: ActivatedRoute,
-              protected aroute: ActivatedRoute, protected rest: RestService,
-              protected ws: WebSocketService, protected userService: UserService,
-              protected storageService: StorageService, protected dialog: DialogService) {}
+  constructor(
+    protected aroute: ActivatedRoute,
+    protected ws: WebSocketService,
+    protected userService: UserService,
+    protected storageService: StorageService,
+    protected dialog: DialogService) { }
 
   preInit(entityEdit: any) {
     entityEdit.isNew = true; // remove me when we find a way to get the permissions
-    this.sub = this.aroute.params.subscribe(params => {
+    this.aroute.params.subscribe(params => {
       this.datasetPath = '/mnt/' + params['pk'];
-      this.id = _.find(this.fieldSets.find(set => set.name === helptext.heading_dataset_path).config, {name:'id'});
-      this.id.value = this.datasetPath;
+      const idField = _.find(this.fieldSets.find(set => set.name === helptext.heading_dataset_path).config, { name: 'id' });
+      idField.value = this.datasetPath;
     });
 
     this.userService.userQueryDSCache().subscribe(items => {
       const users = [];
       for (let i = 0; i < items.length; i++) {
-        users.push({label: items[i].username, value: items[i].username});
+        users.push({ label: items[i].username, value: items[i].username });
       }
-      this.user = _.find(this.fieldSets.find(set => set.name === helptext.heading_who).config, {'name' : 'user'});
-      this.user.options = users;
+      const userField = _.find(this.fieldSets.find(set => set.name === helptext.heading_who).config, { 'name': 'user' });
+      userField.options = users;
     });
 
     this.userService.groupQueryDSCache().subscribe(items => {
       const groups = [];
       for (let i = 0; i < items.length; i++) {
-        groups.push({label: items[i].group, value: items[i].group});
+        groups.push({ label: items[i].group, value: items[i].group });
       }
-      this.group = _.find(this.fieldSets.find(set => set.name === helptext.heading_who).config, {'name' : 'group'});
-        this.group.options = groups;
+      const groupField = _.find(this.fieldSets.find(set => set.name === helptext.heading_who).config, { 'name': 'group' });
+      groupField.options = groups;
     });
-    this.mode = _.find(this.fieldSets.find(set => set.name === helptext.heading_access).config, {'name' : "mode"});
   }
 
   afterInit(entityEdit: any) {
     this.storageService.filesystemStat(this.datasetPath).subscribe(res => {
-      this.datasetMode = res.mode.toString(8).substring(2,5);
+      this.datasetMode = res.mode.toString(8).substring(2, 5);
       entityEdit.formGroup.controls['mode'].setValue(this.datasetMode);
       entityEdit.formGroup.controls['user'].setValue(res.user);
       entityEdit.formGroup.controls['group'].setValue(res.group);
@@ -170,11 +159,11 @@ export class DatasetPermissionsComponent implements OnDestroy {
     this.recursive_subscription = this.recursive.valueChanges.subscribe((value) => {
       if (value === true) {
         this.dialog.confirm(T("Warning"), T("Setting permissions recursively will affect this directory and any others below it. This might make data inaccessible."))
-        .subscribe((res) => {
-          if (!res) {
-            this.recursive.setValue(false);
-          }
-        });
+          .subscribe((res) => {
+            if (!res) {
+              this.recursive.setValue(false);
+            }
+          });
       }
     });
   }
@@ -187,9 +176,9 @@ export class DatasetPermissionsComponent implements OnDestroy {
     parent.userService.groupQueryDSCache(value).subscribe(items => {
       const groups = [];
       for (let i = 0; i < items.length; i++) {
-        groups.push({label: items[i].group, value: items[i].group});
+        groups.push({ label: items[i].group, value: items[i].group });
       }
-        parent.group.searchOptions = groups;
+      parent.group.searchOptions = groups;
     });
   }
 
@@ -197,7 +186,7 @@ export class DatasetPermissionsComponent implements OnDestroy {
     parent.userService.userQueryDSCache(value).subscribe(items => {
       const users = [];
       for (let i = 0; i < items.length; i++) {
-        users.push({label: items[i].username, value: items[i].username});
+        users.push({ label: items[i].username, value: items[i].username });
       }
       parent.user.searchOptions = users;
     });
