7b60e12c61971b538e4e49258d6b7a13a2713c12
Lola Yang <lola.ly27@gmail.com>
2019-08-16 18:14:51 -0700

    NAS-102745 remove unused tooltips

--- src/app/helptext/storage/volumes/datasets/dataset-permissions.ts
+++ src/app/helptext/storage/volumes/datasets/dataset-permissions.ts
@@ -7,24 +7,15 @@ dataset_permissions_acl_placeholder: T('ACL Type'),
 dataset_permissions_acl_tooltip: T('Select the type that matches the type of client\
  accessing the dataset.'),
 
-dataset_permissions_user_en_placeholder: T('Apply User'),
-dataset_permissions_user_en_tooltip: T('Apply changes to the user.'),
-
 dataset_permissions_user_placeholder: T('User'),
 dataset_permissions_user_tooltip: T('Select the user to control the dataset. Users\
  created manually or imported from a directory service\
  appear in the drop-down menu.'),
 
-dataset_permissions_group_en_placeholder: T('Apply Group'),
-dataset_permissions_group_en_tooltip: T('Apply changes to the group.'),
-
 dataset_permissions_group_placeholder: T('Group'),
 dataset_permissions_group_tooltip: T('Select the group to control the dataset. Groups\
  created manually or imported from a directory service\
  appear in the drop-down menu.'),
-
-dataset_permissions_mode_en_placeholder: T('Apply Access Mode'),
-dataset_permissions_mode_en_tooltip: T('Apply changes to the mode.'),
 
 dataset_permissions_mode_placeholder: T('Access Mode'),
 dataset_permissions_mode_tooltip: T('Set the read, write, and execute permissions for the dataset.'),
