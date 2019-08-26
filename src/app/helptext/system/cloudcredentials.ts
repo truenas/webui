aef94ca99ff8330bc50cc87023829ae70afb8258
Mrt134 <timmoore88@gmail.com>
2019-08-26 09:21:00 -0400

    NAS-102240: Update Cloud Credential fields help text: - Unify with docs updates - Yarn build testing: no issues.

--- src/app/helptext/system/cloudcredentials.ts
+++ src/app/helptext/system/cloudcredentials.ts
@@ -12,6 +12,8 @@ export const helptext_system_cloudcredentials = {
 
   provider: {
     placeholder: T("Provider"),
+    tooltip: T("Third-party Cloud service providers. Choose an option to \
+ configure connection credentials for the selected provider."),
     validation: [Validators.required]
   },
 
@@ -24,7 +26,7 @@ export const helptext_system_cloudcredentials = {
     placeholder: T('OAuth Client Secret'),
     tooltip: T(''),
   },
-
+    // Amazon cloud drive is disabled
   client_id_amazon_cloud_drive: {
     placeholder: T("Amazon Application Client ID"),
     tooltip: T(
@@ -38,14 +40,14 @@ export const helptext_system_cloudcredentials = {
     placeholder: T("Application Client Secret"),
     tooltip: T("Client secret for the Amazon Drive account.")
   },
+    // Amazon cloud drive is disabled
 
   access_key_id_s3: {
     placeholder: T("Access Key ID"),
     tooltip: T(
-      'Amazon Web Services Key ID. This is found\
- on <a href="https://aws.amazon.com/"\
- target="_blank">Amazon AWS</a> by going through <i>My\
- account -> Security Credentials -> Access Keys\
+      'Amazon Web Services Key ID. This is found on \
+ <a href="https://aws.amazon.com/" target="_blank">Amazon AWS</a> by \
+ going through <i>My account -> Security Credentials -> Access Keys \
  (Access Key ID and Secret Access Key)</i>.'
     )
   },
@@ -53,20 +55,19 @@ export const helptext_system_cloudcredentials = {
   secret_access_key_s3: {
     placeholder: T("Secret Access Key"),
     tooltip: T(
-      "Amazon Web Services password. If the Secret Access Key\
- cannot be found or remembered, go to <i>My Account >\
- Security Credentials > Access Keys</i> and create a\
- new key pair."
+      "Amazon Web Services password. If the Secret Access Key cannot be \
+ found or remembered, go to <i>My Account > Security Credentials > \
+ Access Keys</i> and create a new key pair."
     )
   },
 
   endpoint_s3: {
     placeholder: T("Endpoint URL"),
-    tooltip: T('<a href="https://docs.aws.amazon.com/AmazonS3/latest/dev/WebsiteEndpoints.html"\
- target="_blank">S3 API endpoint URL</a>. When using AWS, the endpoint field can be \
- empty to use the default endpoint for the region, and available \
- buckets are automatically fetched. Refer to the AWS Documentation \
- for a list of \
+    tooltip: T('<a href="https://docs.aws.amazon.com/AmazonS3/latest/dev/WebsiteEndpoints.html" \
+ target="_blank">S3 API endpoint URL</a>. When using AWS, the endpoint \
+ field can be empty to use the default endpoint for the region, and \
+ available buckets are automatically fetched. Refer to the AWS \
+ Documentation for a list of \
  <a href="https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_website_region_endpoints \
  target="_blank">Simple Storage Service Website Endpoints</a>.'
     )
@@ -75,49 +76,44 @@ export const helptext_system_cloudcredentials = {
   skip_region_s3: {
     placeholder: T("Disable Endpoint Region"),
     tooltip: T(
-      "Skip automatic detection of the Endpoint URL\
- region. Set this when configuring a custom\
- Endpoint URL."
+      "Skip automatic detection of the Endpoint URL region. Set this \
+ when configuring a custom Endpoint URL."
     )
   },
 
   signatures_v2_s3: {
     placeholder: T("Use Signature Version 2"),
     tooltip: T(
-      'Force using <a href="https://docs.aws.amazon.com/general/latest/gr/signature-version-2.html"\
- target="_blank">Signature Version 2</a> to sign API\
- requests. Set this when configuring a custom\
- Endpoint URL.'
+      'Force using \
+ <a href="https://docs.aws.amazon.com/general/latest/gr/signature-version-2.html" \
+ target="_blank">Signature Version 2</a> to sign API requests. Set this \
+ when configuring a custom Endpoint URL.'
     )
   },
 
   account_b2: {
     placeholder: T("Account ID or Application Key ID"),
     tooltip: T(
-      'Alphanumeric <a\
- href="https://www.backblaze.com/b2/cloud-storage.html"\
- target="_blank">Backblaze B2</a> ID. Find an Account ID\
- or applicationKeyID by logging in to the account,\
- clicking <i>Buckets</i>, and clicking\
- <i>Show Account ID and Application Key</i>. Enter the\
- <i>Account ID</i> to associate the entire account or\
- generate a new <i>Application Key</i>. The <i>keyID</i>\
- replaces the Account ID and the key string is used in\
- place of the <i>Master Application Key</i>.'
+      'Alphanumeric \
+ <a href="https://www.backblaze.com/b2/cloud-storage.html" \
+ target="_blank">Backblaze B2</a> ID. Find an Account ID or \
+ applicationKeyID by logging in to the account, clicking <i>Buckets</i>, \
+ and clicking <i>Show Account ID and Application Key</i>. Enter the \
+ <i>Account ID</i> to associate the entire account or generate a new \
+ <i>Application Key</i>. The <i>keyID</i> replaces the Account ID and \
+ the key string is used in place of the <i>Master Application Key</i>.'
     )
   },
 
   key_b2: {
     placeholder: T("Master Application Key or Application Key"),
     tooltip: T(
-      'Backblaze B2 Application Key. Log in to\
- the B2 account and generate a key on the Buckets\
- page. <a\
- href="https://help.backblaze.com/hc/en-us/articles/224991568-Where-can-I-find-my-Account-ID-and-Application-Key-"\
- target="_blank">Generating a new Master Application Key</a>\
- will invalidate the existing Master key and require\
- updating this field. Using a limited permissions\
- Application Key also requires changing the\
+      'Backblaze B2 Application Key. Log in to the B2 account and \
+ generate a key on the Buckets page. \
+ <a href="https://help.backblaze.com/hc/en-us/articles/224991568-Where-can-I-find-my-Account-ID-and-Application-Key-" \
+ target="_blank">Generating a new Master Application Key</a> invalidates \
+ the existing Master key and requires updating this field. Using a \
+ limited permissions Application Key also requires changing the \
  <i>Account ID</i> to the new <i>keyID</i>.'
     )
   },
@@ -125,23 +121,23 @@ export const helptext_system_cloudcredentials = {
   token_box: {
     placeholder: T("Access Token"),
     tooltip: T(
-      'A User Access Token for <a\
- href="https://developer.box.com/"\
- target="_blank">Box</a>. An <a\
- href="https://developer.box.com/reference#token"\
- target="_blank">access token</a> enables Box to verify\
- a request belongs to an authorized session. Example\
- token: <i>T9cE5asGnuyYCCqIZFoWjFHvNbvVqHjl</i>.'
+      'A User Access Token for <a href="https://developer.box.com/" \
+ target="_blank">Box</a>. An \
+ <a href="https://developer.box.com/reference#token" \
+ target="_blank">access token</a> enables Box to verify a request \
+ belongs to an authorized session. Example token: \
+ <i>T9cE5asGnuyYCCqIZFoWjFHvNbvVqHjl</i>.'
     )
   },
 
   token_dropbox: {
     placeholder: T("Access Token"),
     tooltip: T(
-      'Access Token for a Dropbox account. A <a\
- href="https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/"\
- target="_blank">token must be generated</a> in the\
- account before adding it here.'
+      'Access Token for a Dropbox account. A \
+ <a href="https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/" \
+ target="_blank">token must be generated</a> by the \
+ <a href="https://www.dropbox.com/" target="_blank">Dropbox account</a> \
+ before adding it here.'
     )
   },
 
@@ -153,68 +149,67 @@ export const helptext_system_cloudcredentials = {
   port_ftp: {
     placeholder: T("Port"),
     tooltip: T(
-      "FTP Port number. Leave blank to use a default port\
- of <i>21</i>."
+      "FTP Port number. Leave blank to use the default port <i>21</i>."
     )
   },
 
   user_ftp: {
     placeholder: T("Username"),
     tooltip: T(
-      "A username on the FTP Host system. This user must\
- already exist on the FTP Host."
+      "A username on the FTP Host system. This user must already exist \
+ on the FTP Host."
     )
   },
 
   pass_ftp: {
     placeholder: T("Password"),
-    tooltip: T("Password for the username.")
+    tooltip: T("Password for the user account.")
   },
 
   preview_google_cloud_storage: {
     placeholder: T("Preview JSON Service Account Key"),
-    tooltip: T("View the contents of the Service Account JSON file.")
+    tooltip: T("Contents of the uploaded Service Account JSON file.")
   },
 
   service_account_credentials_google_cloud_storage: {
     placeholder: T("Service Account"),
     tooltip: T(
-      'Upload the Service Account JSON credential file. This <a\
- href="https://cloud.google.com/storage/docs/authentication#generating-a-private-key"\
- target="_blank">file must be generated</a> with the\
- Google Cloud Platform Console and uploaded from the\
- local system.'
+      'Upload a Google \
+ <a href="https://rclone.org/googlecloudstorage/#service-account-support" \
+ target="_blank">Service Account credential file</a>. The file is \
+ created with the \
+ <a href="https://console.cloud.google.com/apis/credentials" \
+ target="_blank">Google Cloud Platform Console</a>.'
     )
   },
 
   token_google_drive: {
     placeholder: T("Access Token"),
     tooltip: T(
-      'Token created with <a\
- href="https://developers.google.com/drive/api/v3/about-auth"\
- target="_blank">Google Drive</a>. Access Tokens expire\
- periodically and must be refreshed.'
+      'Token created with \
+ <a href="https://developers.google.com/drive/api/v3/about-auth"\
+ target="_blank">Google Drive</a>. Access Tokens expire periodically and \
+ must be refreshed.'
     )
   },
 
   team_drive_google_drive: {
     placeholder: T("Team Drive ID"),
     tooltip: T(
-      "Only needed when connecting to a Team Drive. The ID of\
- the top level folder of the Team Drive."
+      "Only needed when connecting to a Team Drive. The ID of the top \
+ level folder of the Team Drive."
     )
   },
 
   url_http: {
     placeholder: T("URL"),
-    tooltip: T("URL of the HTTP host to connect to.")
+    tooltip: T("HTTP host URL.")
   },
 
   token_hubic: {
     placeholder: T("Access Token"),
     tooltip: T(
-      'Access Token <a\
- href="https://api.hubic.com/sandbox/"\
+      'Access Token <a href="https://api.hubic.com/sandbox/" \
  target="_blank">generated by a Hubic account</a>.'
     )
   },
@@ -222,25 +217,24 @@ export const helptext_system_cloudcredentials = {
   user_mega: {
     placeholder: T("Username"),
     tooltip: T(
-      'Username for a <a href="https://mega.nz/"\
- target="_blank">MEGA</a> account.'
+      '<a href="https://mega.nz/" target="_blank">MEGA</a> account \
+ username.'
     )
   },
 
   pass_mega: {
     placeholder: T("Password"),
     tooltip: T(
-      'Password for the <a href="https://mega.nz/"\
- target="_blank">MEGA</a> account.'
+      '<a href="https://mega.nz/" target="_blank">MEGA</a> account \
+ password.'
     )
   },
 
   account_azureblob: {
     placeholder: T("Account Name"),
     tooltip: T(
-      'Name of a <a\
- href="https://docs.microsoft.com/en-us/azure/storage/common/storage-create-storage-account"\
- target="_blank">Microsoft Azure</a> account.'
+      '<a href="https://docs.microsoft.com/en-us/azure/storage/common/storage-create-storage-account" \
+ target="_blank">Microsoft Azure</a> account name.'
     )
   },
 
@@ -252,8 +246,8 @@ export const helptext_system_cloudcredentials = {
   token_onedrive: {
     placeholder: T("Access Token"),
     tooltip: T(
-      'Microsoft Onedrive <a\
- href="https://docs.microsoft.com/en-us/onedrive/developer/rest-api/getting-started/authentication"\
+      'Microsoft Onedrive \
+ <a href="https://docs.microsoft.com/en-us/onedrive/developer/rest-api/getting-started/authentication" \
  target="_blank">Access Token</a>.'
     )
   },
@@ -261,9 +255,8 @@ export const helptext_system_cloudcredentials = {
   drive_type_onedrive: {
     placeholder: T("Drive Account Type"),
     tooltip: T(
-      'Choose a <i>Drive Account Type</i>: <i>PERSONAL, BUSINESS,</i>\
- or <a\
- href="https://products.office.com/en-us/sharepoint/collaboration"\
+      'Choose the account type: <i>PERSONAL, BUSINESS,</i> or \
+ <a href="https://products.office.com/en-us/sharepoint/collaboration"\
  target="_blank">SharePoint</a> <i>DOCUMENT_LIBRARY</i>.'
     )
   },
@@ -271,23 +264,19 @@ export const helptext_system_cloudcredentials = {
   drive_id_onedrive: {
     placeholder: T("Drive ID"),
     tooltip: T(
-      'Choose a unique drive identifier. Open the\
- <i>Shell</i>, enter <i>rclone config</i>,\
- and follow the prompts to find the <i>Drive ID</i>.\
- The rclone <a\
- href="https://rclone.org/onedrive"\
- target="_blank">OneDrive documentation</a> walks through\
- the configuration process.'
+      'Unique drive identifier. Log in to the \
+ <a href="https://onedrive.live.com" target="_blank">OneDrive account</a> \
+ and copy the string that appears in the browser address bar after \
+ <i>cid=</i>. Example: https://onedrive.live.com/?id=root&cid=<i>12A34567B89C10D1</i>.'
     )
   },
 
   token_pcloud: {
     placeholder: T("Access Token"),
     tooltip: T(
-      '<a\
- href="https://docs.pcloud.com/methods/intro/authentication.html"\
- target="_blank">pCloud Access Token</a>. These tokens\
- can expire and require extension.'
+      '<a href="https://docs.pcloud.com/methods/intro/authentication.html" \
+ target="_blank">pCloud Access Token</a>. These tokens can expire and \
+ require extension.'
     )
   },
 
@@ -316,7 +305,7 @@ export const helptext_system_cloudcredentials = {
 
   private_key_sftp: {
     placeholder: T("Private Key ID"),
-    tooltip: T('Import the private key from an existing SSH keypair.\
+    tooltip: T('Import the private key from an existing SSH keypair. \
  Choose the name of the SSH keypair to use for this connection.')
   },
 
@@ -327,8 +316,7 @@ export const helptext_system_cloudcredentials = {
 
   vendor_webdav: {
     placeholder: T("WebDAV service"),
-    tooltip: T("Name of the WebDAV site, service, or software being\
- used.")
+    tooltip: T("Name of the WebDAV site, service, or software being used.")
   },
 
   user_webdav: {
@@ -344,8 +332,8 @@ export const helptext_system_cloudcredentials = {
   token_yandex: {
     placeholder: T("Access Token"),
     tooltip: T(
-      'Yandex <a\
- href="https://tech.yandex.com/direct/doc/dg-v4/concepts/auth-token-docpage/"\
+      'Yandex \
+ <a href="https://tech.yandex.com/direct/doc/dg-v4/concepts/auth-token-docpage/" \
  target="_blank">Access Token</a>.'
     )
   }
