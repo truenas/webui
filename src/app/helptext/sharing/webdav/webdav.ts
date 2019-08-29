25ecaf4d1fa601559102fdc8a82402b13ed9b870
Mrt134 <timmoore88@gmail.com>
2019-08-29 14:30:08 -0400

    NAS-100942: Update WebDAV sharing warning dialog and 'User & Group Ownership' help text - Rework with Warren and Andrew's input. - Yarn test: no issues.

--- src/app/helptext/sharing/webdav/webdav.ts
+++ src/app/helptext/sharing/webdav/webdav.ts
@@ -17,8 +17,10 @@ export const helptext_sharing_webdav = {
     tooltip_ro: T('Set to prohibit users from writing to this share.'),
 
     placeholder_perm: T('Change User & Group Ownership'),
-    tooltip_perm: T('Select to automatically set the contents of the\
- share to the <i>webdav</i> user and group.'),
+    tooltip_perm: T('Change existing ownership of ALL files in the share \
+ to user <samp>webdav</samp> and group <samp>webdav</samp>. If unset, \
+ ownership of files to be accessed through WebDAV must be manually set \
+ to the <samp>webdav</samp> or <samp>www</samp> user/group.'),
 
     column_name: T('Share Name'),
     column_comment: T('Comment'),
@@ -27,8 +29,11 @@ export const helptext_sharing_webdav = {
     column_perm: T('Change User and Group Ownership'),
 
     warning_dialog_title: T('WARNING'),
-    warning_dialog_message: T('The ownership of all files in the share will be \
- changed to group <samp>webdav</samp> and user <samp>webdav</samp>. Existing permissions \
- will not be changed, but the file ownership change might make files inaccessible to \
- their original owners.')
+    warning_dialog_message: T('Ownership of all files in the share will \
+ be changed to user <samp>webdav</samp> and group <samp>webdav</samp>. \
+ Existing permissions will not be changed, but the ownership change \
+ might make files inaccesible to their original owners. This operation \
+ cannot be undone! If unset, ownership of files to be accessed through \
+ WebDAV must be manually set to the <samp>webdav</samp> or \
+ <samp>www</samp> user/group.')
 };
