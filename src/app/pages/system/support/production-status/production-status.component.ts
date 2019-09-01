a03619e1d4562737d686df3b820919127f97006e
Dennis <dkmullen@gmail.com>
2019-09-01 19:57:58 -0400

    NAS-100746 started prod

--- src/app/pages/system/support/production-status/production-status.component.ts
+++ src/app/pages/system/support/production-status/production-status.component.ts
@@ -1,11 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FieldConfig } from 'app/pages/common/entity/entity-form/models/field-config.interface';
+import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
+import { helptext_system_support as helptext } from 'app/helptext/system/support';
 
 @Component({
   selector: 'app-production-status',
-  templateUrl: './production-status.component.html',
+  template : `<entity-form [conf]="this"></entity-form>`,
   styleUrls: ['./production-status.component.css']
 })
 export class ProductionStatusComponent implements OnInit {
+  public fieldConfig: FieldConfig[] = []
+  public fieldSets: FieldSet[] = [
+    {
+      name: 'Column 1',
+      label: false,
+      config:[
+        {
+          type: 'checkbox',
+          name: 'TN_is_production',
+          placeholder: helptext.is_production_checkbox.placeholder,
+          tooltip: helptext.is_production_checkbox.tooltip
+        },
+        {
+          type: 'checkbox',
+          name: 'TN_send_debug',
+          placeholder: 'Send initial debug',
+          tooltip: 'Send initial debug.',
+          value: false,
+          relation : [
+            {
+              action : 'SHOW',
+              when : [ {
+                name : 'TN_is_production',
+                value : true,
+              } ]
+            },
+          ]
+        },
+      ]
+    }
+  ]
 
   constructor() { }
 
