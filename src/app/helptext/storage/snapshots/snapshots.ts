45a14e6f4036dd2259b9c344576818d59abe8855
Mrt134 <timmoore88@gmail.com>
2019-09-16 10:58:30 -0400

    NAS-103033: Update help text for 'Name' and 'Naming Schema' fields Yarn build test: no issues.

--- src/app/helptext/storage/snapshots/snapshots.ts
+++ src/app/helptext/storage/snapshots/snapshots.ts
@@ -4,17 +4,24 @@ import { T } from '../../../translate-marker';
 export default {
 // storage/snapshots/snapshot-add
 snapshot_add_dataset_placeholder: T('Pool/Dataset'),
-snapshot_add_dataset_tooltip: T('Select an existing ZFS pool, dataset, or zvol.'),
+snapshot_add_dataset_tooltip: T('Select an existing ZFS pool, dataset, \
+ or zvol.'),
 snapshot_add_dataset_validation: [Validators.required],
 
 snapshot_add_name_placeholder: T('Name'),
-snapshot_add_name_tooltip: T('Add a name for the new snapshot'),
+snapshot_add_name_tooltip: T('Unique snapshot name. Cannot be used with \
+ a <i>Naming Schema</i>.'),
 
 snapshot_add_naming_schema_placeholder: T('Naming Schema'),
-snapshot_add_naming_schema_tooltip: T('Add a naming schema for the new snapshot'),
+snapshot_add_naming_schema_tooltip: T('Generate a name for the snapshot \
+ from a previously created \
+ <a href="--docurl--/tasks.html#periodic-snapshot-tasks" \
+ target="_blank">periodic snapshot task</a> naming schema. This allows \
+ the snapshot to be replicated. Cannot be used with a <i>Name</i>.'),
 
 snapshot_add_recursive_placeholder: T('Recursive'),
-snapshot_add_recursive_tooltip: T('Set to include child datasets of the chosen dataset.'),
+snapshot_add_recursive_tooltip: T('Set to include child datasets of the \
+ chosen dataset.'),
 
 // storage/snapshots/snapshot-clone
 snapshot_clone_name_placeholder: T('Name'),
