a671e13b41a634224c367b1adf017ddef61d87b9
Lola Yang <lola.ly27@gmail.com>
2019-09-17 14:12:21 -0700

    NAS-103078 convert iscsi extent to ws call

--- src/app/pages/sharing/iscsi/extent/extent-form/extent-form.component.ts
+++ src/app/pages/sharing/iscsi/extent/extent-form/extent-form.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
 
 import * as _ from 'lodash';
 
 import { EntityFormComponent } from '../../../../common/entity/entity-form';
 import { FieldConfig } from '../../../../common/entity/entity-form/models/field-config.interface';
-import { IscsiService, RestService } from '../../../../../services/';
+import { IscsiService, RestService, WebSocketService } from '../../../../../services/';
+import { EntityUtils } from '../../../../common/entity/utils';
+import { AppLoaderService } from '../../../../../services/app-loader/app-loader.service';
 import { helptext_sharing_iscsi } from 'app/helptext/sharing';
 
 @Component({
@@ -16,17 +18,20 @@ import { helptext_sharing_iscsi } from 'app/helptext/sharing';
 })
 export class ExtentFormComponent {
 
-  protected resource_name: string = 'services/iscsi/extent';
+  protected addCall = 'iscsi.extent.create';
+  protected queryCall = 'iscsi.extent.query';
+  protected editCall = 'iscsi.extent.update';
+  protected customFilter: Array<any> = [[["id", "="]]];
   protected route_success: string[] = [ 'sharing', 'iscsi', 'extent' ];
-  protected isEntity: boolean = true;
+  protected isEntity = true;
   protected entityForm: EntityFormComponent;
-  protected isNew: boolean = false;
+  protected isNew = false;
   public sub: Subscription;
 
   protected fieldConfig: FieldConfig[] = [
     {
       type : 'input',
-      name : 'iscsi_target_extent_name',
+      name : 'name',
       placeholder : helptext_sharing_iscsi.extent_placeholder_name,
       tooltip: helptext_sharing_iscsi.extent_tooltip_name,
       required: true,
@@ -34,23 +39,23 @@ export class ExtentFormComponent {
     },
     {
       type: 'select',
-      name: 'iscsi_target_extent_type',
+      name: 'type',
       placeholder: helptext_sharing_iscsi.extent_placeholder_type,
       tooltip: helptext_sharing_iscsi.extent_tooltip_type,
       options: [
         {
           label: 'Device',
-          value: 'Disk',
+          value: 'DISK',
         },
         {
           label: 'File',
-          value: 'File',
+          value: 'FILE',
         },
       ],
     },
     {
       type: 'select',
-      name: 'iscsi_target_extent_disk',
+      name: 'disk',
       placeholder: helptext_sharing_iscsi.extent_placeholder_disk,
       tooltip: helptext_sharing_iscsi.extent_tooltip_disk,
       options: [],
@@ -61,7 +66,7 @@ export class ExtentFormComponent {
     },
     {
       type : 'input',
-      name : 'iscsi_target_extent_serial',
+      name : 'serial',
       placeholder : helptext_sharing_iscsi.extent_placeholder_serial,
       tooltip: helptext_sharing_iscsi.extent_tooltip_serial,
     },
@@ -69,7 +74,7 @@ export class ExtentFormComponent {
       type : 'explorer',
       explorerType: 'file',
       initial: '/mnt',
-      name: 'iscsi_target_extent_path',
+      name: 'path',
       placeholder: helptext_sharing_iscsi.extent_placeholder_path,
       tooltip: helptext_sharing_iscsi.extent_tooltip_path,
       isHidden: false,
@@ -79,7 +84,7 @@ export class ExtentFormComponent {
     },
     {
       type: 'input',
-      name: 'iscsi_target_extent_filesize',
+      name: 'filesize',
       placeholder: helptext_sharing_iscsi.extent_placeholder_filesize,
       tooltip: helptext_sharing_iscsi.extent_tooltip_filesize,
       isHidden: false,
@@ -89,7 +94,7 @@ export class ExtentFormComponent {
     },
     {
       type: 'select',
-      name: 'iscsi_target_extent_blocksize',
+      name: 'blocksize',
       placeholder: helptext_sharing_iscsi.extent_placeholder_blocksize,
       tooltip: helptext_sharing_iscsi.extent_tooltip_blocksize,
       options: [
@@ -114,38 +119,39 @@ export class ExtentFormComponent {
     },
     {
       type: 'checkbox',
-      name: 'iscsi_target_extent_pblocksize',
+      name: 'pblocksize',
       placeholder: helptext_sharing_iscsi.extent_placeholder_pblocksize,
       tooltip: helptext_sharing_iscsi.extent_tooltip_pblocksize,
     },
     {
       type: 'input',
-      name: 'iscsi_target_extent_avail_threshold',
+      name: 'avail_threshold',
       placeholder: helptext_sharing_iscsi.extent_placeholder_avail_threshold,
       tooltip: helptext_sharing_iscsi.extent_tooltip_avail_threshold,
+      isHidden: false,
     },
     {
       type : 'input',
-      name : 'iscsi_target_extent_comment',
+      name : 'comment',
       placeholder : helptext_sharing_iscsi.extent_placeholder_comment,
       tooltip: helptext_sharing_iscsi.extent_tooltip_comment,
     },
     {
       type: 'checkbox',
-      name: 'iscsi_target_extent_insecure_tpc',
+      name: 'insecure_tpc',
       placeholder: helptext_sharing_iscsi.extent_placeholder_insecure_tpc,
       tooltip: helptext_sharing_iscsi.extent_tooltip_insecure_tpc,
       value: true,
     },
     {
       type: 'checkbox',
-      name: 'iscsi_target_extent_xen',
+      name: 'xen',
       placeholder: helptext_sharing_iscsi.extent_placeholder_xen,
       tooltip: helptext_sharing_iscsi.extent_tooltip_xen,
     },
     {
       type: 'select',
-      name: 'iscsi_target_extent_rpm',
+      name: 'rpm',
       placeholder: helptext_sharing_iscsi.extent_placeholder_rpm,
       tooltip: helptext_sharing_iscsi.extent_tooltip_rpm,
       options: [],
@@ -153,7 +159,7 @@ export class ExtentFormComponent {
     },
     {
       type: 'checkbox',
-      name: 'iscsi_target_extent_ro',
+      name: 'ro',
       placeholder: helptext_sharing_iscsi.extent_placeholder_ro,
       tooltip: helptext_sharing_iscsi.extent_tooltip_ro,
     },
@@ -161,20 +167,23 @@ export class ExtentFormComponent {
 
   protected rpm_control: any;
   protected deviceFieldGroup: any[] = [
-    'iscsi_target_extent_disk',
+    'disk',
   ];
   protected fileFieldGroup: any[] = [
-    'iscsi_target_extent_path',
-    'iscsi_target_extent_filesize',
+    'path',
+    'filesize',
   ];
   protected extent_type_control: any;
   protected extent_disk_control: any;
   protected pk: string;
+  protected avail_threshold_field: any;
 
   constructor(protected router: Router,
               protected aroute: ActivatedRoute,
               protected iscsiService: IscsiService,
-              protected rest: RestService) {}
+              protected rest: RestService,
+              protected ws: WebSocketService,
+              protected loader: AppLoaderService) {}
 
   preInit() {
     this.sub = this.aroute.params.subscribe(params => {
@@ -182,11 +191,12 @@ export class ExtentFormComponent {
       if (!params['pk']) {
         this.isNew = true;
         this.fieldConfig = _.filter(this.fieldConfig, function(item) {
-          return item.name != 'iscsi_target_extent_serial';
+          return item.name != 'serial';
         });
       } else {
         this.isNew = false;
         this.pk = params['pk'];
+        this.customFilter[0][0].push(parseInt(params['pk']));
       }
     });
   }
@@ -194,54 +204,51 @@ export class ExtentFormComponent {
   afterInit(entityForm: any) {
     this.entityForm = entityForm;
 
-    this.rpm_control = _.find(this.fieldConfig, {'name' : 'iscsi_target_extent_rpm'});
+    this.rpm_control = _.find(this.fieldConfig, {'name' : 'rpm'});
     this.iscsiService.getRPMChoices().subscribe((res) => {
       res.forEach((item) => {
         this.rpm_control.options.push({label : item[1], value : item[0]});
       });
     });
 
-    this.extent_disk_control = _.find(this.fieldConfig, {'name' : 'iscsi_target_extent_disk'});
+    let extent_disk_field = _.find(this.fieldConfig, {'name' : 'disk'});
     //get device options
     this.iscsiService.getExtentDevices().subscribe((res) => {
       for(let i in res) {
-        this.extent_disk_control.options.push({label: res[i], value: i});
+        extent_disk_field.options.push({label: res[i], value: i});
       }
     })
-    //show current value if isNew is false
-    if (!this.isNew) {
-      this.rest.get('/services/iscsi/extent/'+this.pk, {}).subscribe((res) =>{
-        if (res.data) {
-          this.entityForm.formGroup.controls['iscsi_target_extent_disk'].setValue(res.data.iscsi_target_extent_path.substring(5));
-        }
-      })
-    }
-    this.extent_type_control = entityForm.formGroup.controls['iscsi_target_extent_type'];
+
+    this.extent_type_control = entityForm.formGroup.controls['type'];
     this.extent_type_control.valueChanges.subscribe((value) => {
       this.formUpdate(value);
     });
 
+    this.avail_threshold_field = _.find(this.fieldConfig, {'name': 'avail_threshold'});
+    this.extent_disk_control = entityForm.formGroup.controls['disk'];
+    this.extent_disk_control.valueChanges.subscribe((value) => {
+      // zvol
+      if (_.startsWith(value, 'zvol')) {
+        this.avail_threshold_field.isHidden = false;
+      } else {
+        this.avail_threshold_field.isHidden = true;
+        if (this.pk && value != undefined && _.find(extent_disk_field.options, {value: value}) === undefined) {
+          extent_disk_field.options.push({label: value, value: value});
+        }
+      }
+    });
+
     if (this.isNew) {
-      this.extent_type_control.setValue('Disk');
+      this.extent_type_control.setValue('DISK');
     }
   }
 
   formUpdate (type) {
-    let isDevice = type == 'File' ? false : true;
-
-    //resetValue if editing zvol extent
-    if (type == 'ZVOL') {
-      this.extent_type_control.setValue('Disk');
-      let disk_path = this.entityForm.data['iscsi_target_extent_path'];
-      let disk_control = this.entityForm.formGroup.controls['iscsi_target_extent_disk'];
-      //remove '/dev/' from path
-      disk_path = disk_path.substring(5);
-      disk_control.setValue(disk_path);
-    }
+    const isDevice = type == 'FILE' ? false : true;
 
     this.fileFieldGroup.forEach(field => {
-      let control: any = _.find(this.fieldConfig, {'name': field});
-      control.isHidden = isDevice;
+      const control: any = _.find(this.fieldConfig, {'name': field});
+      control['isHidden'] = isDevice;
       control.disabled = isDevice;
       if (isDevice) {
         this.entityForm.formGroup.controls[field].disable();
@@ -251,8 +258,8 @@ export class ExtentFormComponent {
     });
 
     this.deviceFieldGroup.forEach(field => {
-      let control: any = _.find(this.fieldConfig, {'name': field});
-      control.isHidden = !isDevice;
+      const control: any = _.find(this.fieldConfig, {'name': field});
+      control['isHidden'] = !isDevice;
       control.disabled = !isDevice;
       if (!isDevice) {
         this.entityForm.formGroup.controls[field].disable();
@@ -260,5 +267,33 @@ export class ExtentFormComponent {
         this.entityForm.formGroup.controls[field].enable();
       }
     });
+  }
+
+  resourceTransformIncomingRestData(data) {
+    if (data.type == 'DISK') {
+      if (_.startsWith(data['path'], 'zvol')) {
+        data['disk'] = data['path'];
+      }
+      delete data['path'];
+    }
+    return data;
+  }
+
+  customEditCall(value) {
+    this.loader.open();
+    if (value['type'] === 'DISK') {
+      value['path'] = value['disk'];
+    }
+    this.ws.call(this.editCall, [this.pk, value]).subscribe(
+      (res) => {
+        this.loader.close();
+        this.router.navigate(new Array('/').concat(this.route_success));
+      },
+      (res) => {
+        this.loader.close();
+        new EntityUtils().handleWSError(this.entityForm, res);
+      }
+    );
+
   }
 }
