1a96c3bc91dd36418ff3a3135df4edf3e57883bc
Warren Block <wblock@wonkity.com>
2019-09-23 15:23:57 -0400

    Simplify Region helptext

--- src/app/helptext/system/cloudcredentials.ts
+++ src/app/helptext/system/cloudcredentials.ts
@@ -64,9 +64,10 @@ export const helptext_system_cloudcredentials = {
  target="_blank">AWS resources in a geographic area</a>. Leave empty to \
  automatically detect the correct public region for the bucket. Entering \
  a private region name allows interacting with Amazon buckets created in \
- that region. For example, discovering buckets created in the eastern \
+ that region. For example, enter <i>us-gov-east-1</i> to discover buckets \
+ created in the eastern \
  <a href="https://docs.aws.amazon.com/govcloud-us/latest/UserGuide/whatis.html" target="_blank">AWS GovCloud</a> \
- requires entering <i>us-gov-east-1</i> for the <i>Region</i>.'),
+ region.'),
   },
 
   skip_region_s3: {
