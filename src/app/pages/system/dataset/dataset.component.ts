e17b877f02578b12cfcb1907bb5e806bd22ada9e
Lola Yang <lola.ly27@gmail.com>
2019-09-25 17:30:55 -0700

    NAS-101403 able to pop warn dialog about smb service restart thing; fixed setValue issue

--- src/app/pages/system/dataset/dataset.component.ts
+++ src/app/pages/system/dataset/dataset.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import * as _ from 'lodash';
-import { RestService, WebSocketService, SnackbarService } from '../../../services/';
+import { RestService, WebSocketService, SnackbarService, DialogService } from '../../../services/';
 import { FieldConfig } from '../../common/entity/entity-form/models/field-config.interface';
 import { helptext_system_dataset } from 'app/helptext/system/dataset';
 import { EntityUtils } from '../../common/entity/utils';
@@ -41,9 +41,9 @@ export class DatasetComponent implements OnInit{
 
   private pool: any;
   private syslog: any;
-  private rrd: any;
   constructor(private rest: RestService, private ws: WebSocketService,
-              private loader: AppLoaderService, private snackbarService: SnackbarService) {}
+              private loader: AppLoaderService, private snackbarService: SnackbarService,
+              private dialogService: DialogService) {}
 
   ngOnInit() {
     this.rest.get(this.volume_name, {}).subscribe( res => {
@@ -60,30 +60,63 @@ export class DatasetComponent implements OnInit{
     this.ws.call('systemdataset.config').subscribe(res => {
       entityForm.formGroup.controls['pool'].setValue(res.pool);
       entityForm.formGroup.controls['syslog'].setValue(res.syslog);
-      entityForm.formGroup.controls['rrd'].setValue(res.rrd);
     });
   }
 
   customSubmit(value) {
+    this.ws.call("service.query").subscribe(
+      (services) => {
+        const smbShare = _.find(services, {'service': "cifs"});
+        if (smbShare.state === 'RUNNING') {
+          this.dialogService.confirm(
+            T('Restart SMB Service'),
+            T('The SMB service must be restarted after update systemdataset, causing a temporary outage of any active SMB connections. Do you want to continue?'),
+            false,
+            T('Continue')
+          ).subscribe((confirmed) => {
+            if (confirmed) {
+              this.doUpdate(value, true);
+            }
+          });
+        } else {
+          this.doUpdate(value, false);
+        }
+      }
+    );
+  }
+
+  doUpdate(value, restartSMB) {
     this.loader.open();
     this.ws.job('systemdataset.update', [value]).subscribe(
       (res) => {
-        this.loader.close();
         if (res.error) {
+          this.loader.close();
           if (res.exc_info && res.exc_info.extra) {
             res.extra = res.exc_info.extra;
           }
           new EntityUtils().handleWSError(this, res);
         }
         if (res.state === 'SUCCESS') {
-          this.snackbarService.open(T("Settings saved."), T('Close'), { duration: 5000 })
+          if (restartSMB) {
+            this.ws.call('service.restart', ['cifs']).subscribe(
+              (restart) => {
+                this.loader.close();
+                this.snackbarService.open(T("Settings saved and SMB service restarted."), T('Close'), { duration: 5000 });
+              },
+              (restartError) => {
+                this.loader.close();
+                new EntityUtils().handleWSError(this, restartError, this.dialogService);
+              }
+            )
+          } else {
+            this.loader.close();
+            this.snackbarService.open(T("Settings saved."), T('Close'), { duration: 5000 });
+          }
         }
       },
       (err) => {
-        this.loader.close();
         new EntityUtils().handleWSError(this, err);
       }
     );
-
   }
 }
