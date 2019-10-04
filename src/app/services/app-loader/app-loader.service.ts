f8e8f49d73b4610cd692860b91157d55e7add788
Lola Yang <lola.ly27@gmail.com>
2019-10-04 15:34:58 -0700

    NAS-103354 fixed undefined issue of loader service

--- src/app/services/app-loader/app-loader.service.ts
+++ src/app/services/app-loader/app-loader.service.ts
@@ -2,20 +2,18 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { Observable } from 'rxjs/Rx';
 import { AppLoaderComponent } from './app-loader.component';
-import { TranslateService } from '@ngx-translate/core';
 import { T } from '../../translate-marker';
 
 @Injectable()
 export class AppLoaderService {
   dialogRef: MatDialogRef<AppLoaderComponent>;
-  constructor(private dialog: MatDialog, private translate: TranslateService) { }
+
+  constructor(private dialog: MatDialog) { }
 
   public open(title: string = T('Please wait')): Observable<boolean> {
-    this.translate.get(title).subscribe(t => {
-      this.dialogRef = this.dialog.open(AppLoaderComponent, {disableClose: true});
-      this.dialogRef.updateSize('200px', '200px');
-      this.dialogRef.componentInstance.title = t;
-    });
+    this.dialogRef = this.dialog.open(AppLoaderComponent, {disableClose: true});
+    this.dialogRef.updateSize('200px', '200px');
+    this.dialogRef.componentInstance.title = title;
     return this.dialogRef.afterClosed();
   }
 
