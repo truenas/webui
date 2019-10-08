a103682a1433a2c225089d64f7f228d7b6cb8e87
Dennis Mullen <dkmullen@gmail.com>
2019-10-08 11:43:54 -0400

    NAS-103404 Validate mem value in ui, add tooltips to form etc

--- src/app/pages/vm/vm-form/vm-form.component.ts
+++ src/app/pages/vm/vm-form/vm-form.component.ts
@@ -24,22 +24,25 @@ export class VmFormComponent {
   protected save_button_enabled: boolean;
 
   public fieldConfig: FieldConfig[] = [
-    { type: 'input', name: 'name', placeholder: helptext.name_placeholder},
-    { type: 'input', name : 'description', placeholder : helptext.description_placeholder},
+    { type: 'input', name: 'name', placeholder: helptext.name_placeholder, tooltip: helptext.name_tooltip},
+    { type: 'input', name : 'description', placeholder : helptext.description_placeholder, tooltip: helptext.description_tooltip},
     {
       name: 'time',
       placeholder: helptext.time_placeholder,
+      tooltip: helptext.time_tooltip,
       type: 'select',
       options: [{ label: helptext.time_local_text, value: 'LOCAL'}, { label: helptext.time_utc_text, value: 'UTC' }]
     },
-    { type : 'input', name: 'vcpus'  ,placeholder : helptext.vcpus_placeholder},
-    { type: 'input', name : 'memory', placeholder : `${helptext.memory_placeholder} ${globalHelptext.human_readable.suggestion_label}`,
+    { type : 'input', name: 'vcpus'  ,placeholder : helptext.vcpus_placeholder, tooltip: helptext.vcpus_tooltip},
+    { type: 'input', name : 'memory', 
+      placeholder : `${helptext.memory_placeholder} ${globalHelptext.human_readable.suggestion_label}`,
+      tooltip: helptext.memory_tooltip,
       blurStatus : true,
       blurEvent : this.blurEvent,
       parent : this
     },
-    { type: 'select', name : 'bootloader', placeholder : helptext.bootloader_placeholder, options: []},
-    { type: 'checkbox', name : 'autostart', placeholder : helptext.autostart_placeholder}
+    { type: 'select', name : 'bootloader', placeholder : helptext.bootloader_placeholder, tooltip: helptext.bootloader_tooltip,options: []},
+    { type: 'checkbox', name : 'autostart', placeholder : helptext.autostart_placeholder, tooltip: helptext.autostart_tooltip}
 
   ];
   private bootloader: any;
@@ -77,17 +80,24 @@ export class VmFormComponent {
   blurEvent(parent){
     if (parent.entityForm) {
       parent.entityForm.formGroup.controls['memory'].setValue(parent.storageService.humanReadable)
+      let valString = (parent.entityForm.formGroup.controls['memory'].value);
+      let valBytes = Math.round(parent.storageService.convertHumanStringToNum(valString)/1048576);
+      if (valBytes < 256) {
+        const mem = _.find(parent.fieldConfig, {name: "memory"});
+        mem['hasErrors'] = true;
+        mem['errors'] = helptext.memory_size_err;
+      }
     }
   }
 
   resourceTransformIncomingRestData(wsResponse) {
-    wsResponse['memory'] = this.storageService.convertBytestoHumanReadable(wsResponse['memory']*1049000, 0);
+    wsResponse['memory'] = this.storageService.convertBytestoHumanReadable(wsResponse['memory']*1048576, 0);
     return wsResponse;
   }
 
   beforeSubmit(data) {
     if (data['memory'] !== undefined && data['memory'] !== null) {
-    data['memory'] = Math.round(this.storageService.convertHumanStringToNum(data['memory'])/1049000);
+    data['memory'] = Math.round(this.storageService.convertHumanStringToNum(data['memory'])/1048576);
     }
   }
 
