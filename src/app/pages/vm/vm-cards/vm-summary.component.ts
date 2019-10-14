c0dd7abecd00ff5e9a7f473db0f9933cd82ef9af
Dennis Mullen <dkmullen@gmail.com>
2019-10-14 14:34:41 -0400

    NAS-103484 Put VM avaiable space checking on an inverval

--- src/app/pages/vm/vm-cards/vm-summary.component.ts
+++ src/app/pages/vm/vm-cards/vm-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WebSocketService, StorageService } from 'app/services/';
 import helptext from '../../../helptext/vm/vm-wizard/vm-wizard';
 
@@ -7,14 +7,26 @@ import helptext from '../../../helptext/vm/vm-wizard/vm-wizard';
   templateUrl: './vm-summary.component.html',
   styleUrls: ['./vm-summary.component.css']
 })
-export class VmSummaryComponent {
+export class VmSummaryComponent implements OnInit, OnDestroy {
   public availMem: string;
   public memTitle = helptext.vm_mem_title;
   public memWarning = helptext.memory_warning;
-
-  constructor(private ws: WebSocketService, protected storageService: StorageService) {
+  public interval = setInterval(() => {
     this.ws.call('vm.get_available_memory').subscribe((res) => {
       this.availMem = this.storageService.convertBytestoHumanReadable(res);
-    })
+    })      
+  }, 10000)
+
+  constructor(private ws: WebSocketService, protected storageService: StorageService) {}
+
+  ngOnInit() {
+    this.ws.call('vm.get_available_memory').subscribe((res) => {
+      this.availMem = this.storageService.convertBytestoHumanReadable(res);
+    }) 
+    this.interval;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 }
