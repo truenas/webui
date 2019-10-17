868f47d5119613574d378892fa2e9ead4cbf5d38
Erin Clark <erin@ixsystems.com>
2019-10-17 13:47:32 -0700

    Remove acmedns (and several other things that should/shouldn't be shown) on entity-dashboard
    NAS-103479

--- src/app/pages/common/entity/entity-dashboard/entity-dashboard.component.ts
+++ src/app/pages/common/entity/entity-dashboard/entity-dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ɵALLOW_MULTIPLE_PLATFORMS } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as _ from 'lodash';
+import { WebSocketService } from '../../../../services/ws.service';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component ({
@@ -12,7 +13,12 @@ export class EntityDashboardComponent implements OnInit {
 
 	public routeParts: any = [];
 	protected parent: string = "";
-	constructor(protected router: Router,
+
+	protected freenas_exclude = ['failover', 'proactivesupport', 'viewenclosure'];
+	protected truenas_exclude = ['acmedns'];
+	constructor(
+				protected ws: WebSocketService,
+				protected router: Router,
 				protected aroute: ActivatedRoute,
 				public translate: TranslateService){
 
@@ -31,6 +37,43 @@ export class EntityDashboardComponent implements OnInit {
 				this.routeParts.push(routeConfigs[i]);
 			}
 		}
+		
+		let exclude = [];
+		if (window.localStorage.getItem('is_freenas') === 'false') {
+			exclude = exclude.concat(this.truenas_exclude);
+			this.ws.call('failover.licensed').subscribe((is_ha) => {
+				if (!is_ha) { // allow failover
+					this.remove('failover');
+				}
+			});
+
+			this.ws.call('support.is_available').subscribe((res) => {
+				if (res !== true) { // allow proactive support
+					this.remove('proactivesupport');
+				}
+			});
+		} else { // if freenas
+			exclude = exclude.concat(this.freenas_exclude);
+		}
+		this.ws.call('ipmi.is_loaded').subscribe((res)=>{
+			if(res !== true){ 
+				this.remove('ipmi');
+			}
+		});
+		this.ws.call('multipath.query').subscribe((res)=>{
+			if (!res || res.length === 0) {
+				this.remove('multipaths');
+			}
+		});
+		for (let i = 0; i < exclude.length; i++) {
+			this.remove(exclude[i]);
+		}
+	}
+
+	remove(element) {
+		this.routeParts = _.remove(this.routeParts, function(r) {
+			return r['path'] !== element;
+		});
 	}
 
 	goList(item) {
