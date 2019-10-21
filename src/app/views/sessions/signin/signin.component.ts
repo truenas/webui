e1000b6f7ae118e4738193159c5967eacbd86d40
Erin Clark <erin@ixsystems.com>
2019-10-21 14:35:15 -0700

    Fix interval logic
    NAS-103573

--- src/app/views/sessions/signin/signin.component.ts
+++ src/app/views/sessions/signin/signin.component.ts
@@ -32,6 +32,7 @@ export class SigninComponent implements OnInit {
   public checking_status = false;
   public copyrightYear = globalHelptext.copyright_year;
   private interval: any;
+  private token_interval: any;
   public exposeLegacyUI = false;
 
   signinData = {
@@ -103,9 +104,10 @@ export class SigninComponent implements OnInit {
     if (this.canLogin()) {
         this.loginToken();
     } else {
-      this.interval = setInterval(() => {
+      this.token_interval = setInterval(() => {
         if (this.canLogin()) {
           this.loginToken();
+          clearInterval(this.token_interval);
         }
       }, 5000);
     }
