dfe7a7e0b1cbf42e8e17a4f53625745be9bacc43
Lola Yang <lola.ly27@gmail.com>
2019-10-24 15:22:03 -0700

    NAS-103596 add name validator

--- src/app/pages/system/ssh-connections/ssh-connections-form/ssh-connections-form.component.ts
+++ src/app/pages/system/ssh-connections/ssh-connections-form/ssh-connections-form.component.ts
@@ -8,6 +8,7 @@ import { KeychainCredentialService, WebSocketService, DialogService, Replication
 import * as _ from 'lodash';
 import { AppLoaderService } from '../../../../services/app-loader/app-loader.service';
 import { EntityUtils } from '../../../common/entity/utils';
+import { forbiddenValues } from '../../../common/entity/entity-form/validators/forbidden-values-validation';
 
 @Component({
     selector: 'app-ssh-connections-form',
@@ -26,6 +27,8 @@ export class SshConnectionsFormComponent {
     protected editCall = 'keychaincredential.update';
     protected route_success: string[] = ['system', 'sshconnections'];
     protected isEntity = true;
+    protected namesInUseConnection = [];
+    protected namesInUse = [];
 
     protected fieldConfig: FieldConfig[] = [
         {
@@ -34,7 +37,7 @@ export class SshConnectionsFormComponent {
             placeholder: helptext.name_placeholder,
             tooltip: helptext.name_tooltip,
             required: true,
-            validation: [Validators.required]
+            validation: [Validators.required, forbiddenValues(this.namesInUseConnection)]
         }, {
             type: 'select',
             name: 'setup_method',
@@ -211,9 +214,18 @@ export class SshConnectionsFormComponent {
         private loader: AppLoaderService,
         private dialogService: DialogService,
         private replicationService: ReplicationService) {
+        this.keychainCredentialService.getSSHConnections().subscribe(
+            (res) => {
+                const sshConnections = res.map(sshConnection => sshConnection.name);
+                this.namesInUse.push(...sshConnections);
+                this.namesInUseConnection.push(...sshConnections);
+            }
+        )
         const privateKeyField = _.find(this.fieldConfig, { name: 'private_key' });
         this.keychainCredentialService.getSSHKeys().subscribe(
             (res) => {
+                this.namesInUse.push(...res.filter(sshKey => sshKey.name.endsWith(' Key')).map(sshKey =>
+                    sshKey.name.substring(0, sshKey.name.length - 4)));
                 for (const i in res) {
                     privateKeyField.options.push({ label: res[i].name, value: res[i].id });
                 }
@@ -252,6 +264,19 @@ export class SshConnectionsFormComponent {
         } else {
             this.entityForm.formGroup.controls['setup_method'].setValue('manual');
         }
+
+        const nameCtrl = this.entityForm.formGroup.controls['name'];
+        let preValue =  this.entityForm.formGroup.controls['private_key'].value;
+        this.entityForm.formGroup.controls['private_key'].valueChanges.subscribe((res) => {
+            if (res === 'NEW') {
+                nameCtrl.setValidators([Validators.required, forbiddenValues(this.namesInUse)]);
+                nameCtrl.updateValueAndValidity();
+            } else if (preValue === 'NEW') {
+                nameCtrl.setValidators([Validators.required, forbiddenValues(this.namesInUseConnection)]);
+                nameCtrl.updateValueAndValidity();
+            }
+            preValue = res;
+        });
     }
 
     resourceTransformIncomingRestData(wsResponse) {
