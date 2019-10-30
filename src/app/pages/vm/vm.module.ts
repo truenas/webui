f9dab18293b754a1b2f45a4f6a99e29ac6e908b9
Lola Yang <lola.ly27@gmail.com>
2019-10-30 16:56:16 -0700

    NAS-103631 removed unused components

--- src/app/pages/vm/vm.module.ts
+++ src/app/pages/vm/vm.module.ts
@@ -1,54 +1,43 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../../appMaterial.module';
-import {EntityFormService} from '../../pages/common/entity/entity-form/services/entity-form.service';
-import {MessageService} from '../../pages/common/entity/entity-form/services/message.service';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   VmService, NetworkService, SystemGeneralService
 } from '../../services'
-import {EntityModule} from '../common/entity/entity.module';
+import { EntityModule } from '../common/entity/entity.module';
 
-import {DeviceEditComponent} from './devices/device-edit/';
-import {DeviceListComponent} from './devices/device-list';
-import {VmFormComponent} from './vm-form/';
+import { EntityFormService } from '../../pages/common/entity/entity-form/services/entity-form.service';
+import { MessageService } from '../../pages/common/entity/entity-form/services/message.service';
+
+import { DeviceEditComponent } from './devices/device-edit/';
+import { DeviceListComponent } from './devices/device-list';
+import { VmFormComponent } from './vm-form/';
 import { VMListComponent } from './vm-list/vm-list.component';
-import {VmCardsComponent} from './vm-cards/vm-cards.component';
-import {VmCardEditComponent} from './vm-cards/vm-card-edit.component';
-import {VmTableComponent} from './vm-cards/vm-table.component';
-import {routing} from './vm.routing';
-import { VmSummaryComponent } from './vm-cards/vm-summary.component';
+import { routing } from './vm.routing';
 import { CoreComponents } from 'app/core/components/corecomponents.module';
 import { VMWizardComponent } from './vm-wizard/';
-import {VMSerialShellComponent} from './vm-cards/vm-serial-shell';
+import { VMSerialShellComponent } from './vm-serial-shell';
 import { DeviceAddComponent } from './devices/device-add2';
-import { VmDetailsComponent } from './vm-cards/vm-details.component';
 
 @NgModule({
-  imports : [
+  imports: [
     CoreComponents,
     EntityModule, CommonModule, FormsModule, TranslateModule,
-	  ReactiveFormsModule, routing, MaterialModule, NgxDatatableModule, FlexLayoutModule//, BrowserModule
+    ReactiveFormsModule, routing, MaterialModule, FlexLayoutModule//, BrowserModule
   ],
-  declarations : [
+  declarations: [
     VMListComponent,
-    VmCardsComponent,
-    VmCardEditComponent,
-    VmTableComponent,
     VmFormComponent,
     DeviceListComponent,
     DeviceEditComponent,
-    VmSummaryComponent,
     VMWizardComponent,
     VMSerialShellComponent,
-    VmDetailsComponent,
     DeviceAddComponent
   ],
-  entryComponents: [VmDetailsComponent],
-  providers : [ VmService, EntityFormService, NetworkService, SystemGeneralService,MessageService ]
+  providers: [VmService, EntityFormService, NetworkService, SystemGeneralService, MessageService]
 })
-export class VmModule {};
+export class VmModule { };
