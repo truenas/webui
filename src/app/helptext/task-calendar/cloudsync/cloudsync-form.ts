fbc1e2ca12834ca6a3606d0da84d3c7e10b96930
Lola Yang <lola.ly27@gmail.com>
2019-10-31 14:50:07 -0700

    NAS-103715 update tooltip of snapshot

--- src/app/helptext/task-calendar/cloudsync/cloudsync-form.ts
+++ src/app/helptext/task-calendar/cloudsync/cloudsync-form.ts
@@ -70,7 +70,7 @@ transfer_mode_warning_move: T('<b>MOVE</b>: After files are <i><u>copied</u></i>
 
 snapshot_placeholder: T('Take Snapshot'),
 snapshot_tooltip: T('Set to take a snapshot of the dataset before a\
- <i>PUSH</i> or <i>PULL</i>.'),
+ <i>PUSH</i>.'),
 
 pre_script_placeholder: T('Pre-script'),
 pre_script_tooltip: T('Script to execute before running sync.'),
