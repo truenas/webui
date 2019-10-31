58459a73cb76b976fbe7d67a66bf90bd977daca8
Dennis Mullen <dkmullen@gmail.com>
2019-10-31 10:49:02 -0400

    Make directive more generic, add to entity tree table

--- src/app/pages/common/entity/entity-table/data-table-cell.directive.ts
+++ src/app/pages/common/entity/entity-table/data-table-cell.directive.ts
@@ -2,12 +2,12 @@ import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line: directive-selector
-  selector: '[ix-auto-colname]'
+  selector: '[ix-auto]'
 })
 export class DataTableCellDirective implements OnChanges {
   public static readonly ATTRIBUTE = 'ix-auto';
 
-  @Input('ix-auto-colname') public column: string;
+  @Input('ix-auto') public tag: string;
   @Input('ix-auto-type') public type: string;
 
   // tslint:disable-next-line: no-input-rename
@@ -16,13 +16,14 @@ export class DataTableCellDirective implements OnChanges {
   public constructor(private el: ElementRef) {}
 
   public ngOnChanges(): void {
-    let elType, elColumn;
+    console.log(this.tag, this.identifier)
+    let elType, elTag;
     this.type ? elType = this.type : elType = 'NEEDS TYPE!';
-    this.column ? elColumn = `_${this.column}` : elColumn = '';
+    this.tag ? elTag = `_${this.tag}` : elTag = '';
     try {
       (this.el.nativeElement as HTMLElement).setAttribute(
         DataTableCellDirective.ATTRIBUTE,
-        this.identifier ? `${elType}__${this.identifier}${elColumn}` : `${elType}__${elColumn}`
+        this.identifier ? `${elType}__${this.identifier}${elTag}` : `${elType}__${elTag}`
       );
     } catch (error) {
       console.error(`Error in ${DataTableCellDirective.name}:`, error);
