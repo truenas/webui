d1579cf7ea381be1e18cc9aeb0d3c7d480769bba
Erin Clark <erin@ixsystems.com>
2019-11-05 11:18:16 -0800

    Fix reversed logic preventing train from showing
    (cherry picked from commit 80fdc45ad200308753574b29501c0615a9b9930d)

--- src/app/pages/system/update/update.component.ts
+++ src/app/pages/system/update/update.component.ts
@@ -158,7 +158,7 @@ export class UpdateComponent implements OnInit, OnDestroy {
               return "ALLOWED";
             }
             //comparing '.1' with '.2'
-            return version1[1] > version2[1] ? "MINOR_UPGRADE":"MINOR_DOWNGRADE";
+            return version1[1] < version2[1] ? "MINOR_UPGRADE":"MINOR_DOWNGRADE";
           }
           if(version1[1]){
             //handling a case where '.1' is compared with 'undefined'
