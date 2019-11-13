c153f733476608d97e4772b924d4bdae382352f6
Mrt134 <timmoore8@gmail.com>
2019-11-13 09:51:10 -0500

    NAS-103801: review update 1

--- src/app/helptext/system/ssh-connections.ts
+++ src/app/helptext/system/ssh-connections.ts
@@ -2,34 +2,32 @@ import { T } from '../../translate-marker';
 
 export default {
     name_placeholder: T('Name'),
-    name_tooltip: T('Descriptive name of this SSH connection. SSH \
- connection names must be unique.'),
+    name_tooltip: T('Name of this SSH connection. SSH connection names \
+ must be unique.'),
 
     setup_method_placeholder: T('Setup Method'),
     setup_method_tooltip: T('How to configure the connection\:<br><br> \
  <i>Manual</i> requires configuring authentication on the remote system. \
- This can require copying SSH keys and modifying the <i>root</i> user \
+ This can include copying SSH keys and modifying the <i>root</i> user \
  account on that system.<br><br> \
- <i>Semi-automatic</i> is only functional when configuring an SSH \
- connection between FreeNAS systems. After authenticating the \
- connection, all remaining connection options are automatically \
- configured.'),
+ <i>Semi-automatic</i> only works when configuring an SSH connection \
+ with a remote FreeNAS or TrueNAS system. This method uses the URL and
+ login credentials of the remote system to connect and exchange SSH \
+ keys.'),
 
     host_placeholder: T('Host'),
-    host_tooltip: T('Enter the hostname or IP address of the remote \
- system.'),
+    host_tooltip: T('Hostname or IP address of the remote system.'),
 
     port_placeholder: T('Port'),
     port_tooltip: T('Port number on the remote system to use for the SSH \
  connection.'),
 
     username_placeholder: T('Username'),
-    username_tooltip: T('User account name to use for logging in to the \
- remote system.'),
+    username_tooltip: T('Username for logging in to the remote system.'),
 
     private_key_placeholder: T('Private Key'),
     private_key_tooltip: T('Choose a saved SSH Keypair or select \
- <i>Generate New</i> to create a new keypair and apply it to this \
+ <i>Generate New</i> to create a new keypair and use it for this \
  connection.'),
 
     remote_host_key_placeholder: T('Remote Host Key'),
@@ -52,14 +50,14 @@ export default {
     connect_timeout_tooltip: T('Time \(in seconds\) before the system \
  stops attempting to establish a connection with the remote system.'),
 
-    url_placeholder: T('FreeNAS URL'),
-    url_tooltip: T('Hostname or IP address of the remote FreeNAS \
- system. A valid URL scheme is required. Example: \
+    url_placeholder: T('FreeNAS/TrueNAS URL'),
+    url_tooltip: T('Hostname or IP address of the remote system. A \
+ valid URL scheme is required. Example: \
  <b>https://<i>10.231.3.76</i></b>'),
 
     password_placeholder: T('Password'),
-    password_tooltip: T('User account password used to log in to the \
- FreeNAS system.'),
+    password_tooltip: T('User account password for logging into the \
+ remote system.'),
 
     discover_remote_host_key_button: T('Discover Remote Host Key')
 }
