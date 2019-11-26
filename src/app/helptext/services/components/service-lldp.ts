5953f66c5076a4fc6f1aca65efafa25979b65f07
Dennis Mullen <dkmullen@gmail.com>
2019-11-26 10:44:06 -0500

    Set up error checking for lldp form

--- src/app/helptext/services/components/service-lldp.ts
+++ src/app/helptext/services/components/service-lldp.ts
@@ -1,4 +1,5 @@
 import { T } from '../../../translate-marker';
+import { Validators } from '@angular/forms';
 
 export default {
 lldp_intdesc_placeholder : T('Interface Description'),
@@ -9,6 +10,7 @@ lldp_country_placeholder : T('Country Code'),
 lldp_country_tooltip: T('Required for <a href="--docurl--/services.html#lldp"\
  target="_blank">LLDP</a> location support. Enter a\
  two-letter ISO 3166 country code.'),
+lldp_country_validation_error: T('Leave this field blank or enter exactly two letters.'),
 
 lldp_location_placeholder : T('Location'),
 lldp_location_tooltip: T('Specify the physical location of the host.')
