ee7ef9e7c80f1405425c128f922cadea8defb233
Aaron Ervin <aervin@ixsystems.com>
2019-12-03 12:39:29 -0600

    NAS-103694 #in-progress -- nfs service rest -> ws

--- src/app/pages/services/components/service-nfs/service-nfs.component.ts
+++ src/app/pages/services/components/service-nfs/service-nfs.component.ts
@@ -4,6 +4,8 @@ import * as _ from 'lodash';
 import { RestService, WebSocketService } from '../../../../services/';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 import helptext from '../../../../helptext/services/components/service-nfs';
+import { EntityFormComponent } from 'app/pages/common/entity/entity-form';
+import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
 
 @Component({
   selector: 'nfs-edit',
@@ -11,93 +13,134 @@ import helptext from '../../../../helptext/services/components/service-nfs';
 })
 
 export class ServiceNFSComponent {
-  protected resource_name: string = 'services/nfs';
+  protected queryCall = 'nfs.config';
   protected route_success: string[] = ['services'];
-  public fieldConfig: FieldConfig[] = [{
-      type: 'input',
-      name: 'nfs_srv_servers',
-      placeholder: helptext.nfs_srv_servers_placeholder,
-      tooltip: helptext.nfs_srv_servers_tooltip,
-      required: true,
-      validation : helptext.nfs_srv_servers_validation
+  public fieldConfig: FieldConfig[] = [];
+  public fieldSets: FieldSet[] = [
+    {
+      name: 'General Options',
+      label: true,
+      config: [
+        {
+          type: 'input',
+          name: 'servers',
+          placeholder: helptext.nfs_srv_servers_placeholder,
+          tooltip: helptext.nfs_srv_servers_tooltip,
+          required: true,
+          validation : helptext.nfs_srv_servers_validation
+        },
+        {
+          type: 'select',
+          name: 'bindip',
+          placeholder: helptext.nfs_srv_bindip_placeholder,
+          tooltip: helptext.nfs_srv_bindip_tooltip,
+          options: [],
+          multiple: true
+        }
+      ]
     },
     {
-      type: 'checkbox',
-      name: 'nfs_srv_udp',
-      placeholder: helptext.nfs_srv_udp_placeholder,
-      tooltip: helptext.nfs_srv_udp_tooltip,
+      name: 'divider',
+      divider: true
     },
     {
-      type: 'select',
-      name: 'nfs_srv_bindip',
-      placeholder: helptext.nfs_srv_bindip_placeholder,
-      tooltip: helptext.nfs_srv_bindip_tooltip,
-      options: [],
-      multiple: true
+      name: 'NFSv4',
+      label: true,
+      width: '50%',
+      config: [
+        {
+          type: 'checkbox',
+          name: 'v4',
+          placeholder: helptext.nfs_srv_v4_placeholder,
+          tooltip: helptext.nfs_srv_v4_tooltip,
+          value: false,
+        },
+        {
+          type: 'checkbox',
+          name: 'v4_v3owner',
+          placeholder: helptext.nfs_srv_v4_v3owner_placeholder,
+          tooltip: helptext.nfs_srv_v4_v3owner_tooltip,
+          relation: helptext.nfs_srv_v4_v3owner_relation,
+        },
+        {
+          type: 'checkbox',
+          name: 'v4_krb',
+          placeholder: helptext.nfs_srv_v4_krb_placeholder,
+          tooltip: helptext.nfs_srv_v4_krb_tooltip,
+        }
+      ]
     },
     {
-      type: 'checkbox',
-      name: 'nfs_srv_allow_nonroot',
-      placeholder: helptext.nfs_srv_allow_nonroot_placeholder,
-      tooltip: helptext.nfs_srv_allow_nonroot_tooltip,
+      name: 'Ports',
+      label: true,
+      width: '50%',
+      config: [
+        {
+          type: 'input',
+          name: 'mountd_port',
+          placeholder: helptext.nfs_srv_mountd_port_placeholder,
+          tooltip: helptext.nfs_srv_mountd_port_tooltip,
+        },
+        {
+          type: 'input',
+          name: 'rpcstatd_port',
+          placeholder: helptext.nfs_srv_rpcstatd_port_placeholder,
+          tooltip: helptext.nfs_srv_rpcstatd_port_tooltip,
+        },
+        {
+          type: 'input',
+          name: 'rpclockd_port',
+          placeholder: helptext.nfs_srv_rpclockd_port_placeholder,
+          tooltip: helptext.nfs_srv_rpclockd_port_tooltip,
+        }
+      ]
     },
     {
-      type: 'checkbox',
-      name: 'nfs_srv_v4',
-      placeholder: helptext.nfs_srv_v4_placeholder,
-      tooltip: helptext.nfs_srv_v4_tooltip,
-      value: false,
+      name: 'divider',
+      divider: true
     },
     {
-      type: 'checkbox',
-      name: 'nfs_srv_v4_v3owner',
-      placeholder: helptext.nfs_srv_v4_v3owner_placeholder,
-      tooltip: helptext.nfs_srv_v4_v3owner_tooltip,
-      relation: helptext.nfs_srv_v4_v3owner_relation,
+      name: 'Other Options',
+      label: true,
+      config: [
+        {
+          type: 'checkbox',
+          name: 'udp',
+          placeholder: helptext.nfs_srv_udp_placeholder,
+          tooltip: helptext.nfs_srv_udp_tooltip,
+        },
+        {
+          type: 'checkbox',
+          name: 'allow_nonroot',
+          placeholder: helptext.nfs_srv_allow_nonroot_placeholder,
+          tooltip: helptext.nfs_srv_allow_nonroot_tooltip,
+        },
+        
+        {
+          type: 'checkbox',
+          name: 'userd_manage_gids',
+          placeholder: helptext.nfs_srv_16_placeholder,
+          tooltip: helptext.nfs_srv_16_tooltip,
+          relation: helptext.nfs_srv_16_relation,
+        },
+        {
+          type: 'checkbox',
+          name: 'mountd_log',
+          placeholder: helptext.nfs_srv_mountd_log_placeholder,
+          tooltip: helptext.nfs_srv_mountd_log_tooltip,
+        },
+        {
+          type: 'checkbox',
+          name: 'statd_lockd_log',
+          placeholder: helptext.nfs_srv_statd_lockd_log_placeholder,
+          tooltip: helptext.nfs_srv_statd_lockd_log_tooltip,
+        }
+      ]
     },
     {
-      type: 'checkbox',
-      name: 'nfs_srv_v4_krb',
-      placeholder: helptext.nfs_srv_v4_krb_placeholder,
-      tooltip: helptext.nfs_srv_v4_krb_tooltip,
-    },
-    {
-      type: 'input',
-      name: 'nfs_srv_mountd_port',
-      placeholder: helptext.nfs_srv_mountd_port_placeholder,
-      tooltip: helptext.nfs_srv_mountd_port_tooltip,
-    },
-    {
-      type: 'input',
-      name: 'nfs_srv_rpcstatd_port',
-      placeholder: helptext.nfs_srv_rpcstatd_port_placeholder,
-      tooltip: helptext.nfs_srv_rpcstatd_port_tooltip,
-    },
-    {
-      type: 'input',
-      name: 'nfs_srv_rpclockd_port',
-      placeholder: helptext.nfs_srv_rpclockd_port_placeholder,
-      tooltip: helptext.nfs_srv_rpclockd_port_tooltip,
-    },
-    {
-      type: 'checkbox',
-      name: 'nfs_srv_16',
-      placeholder: helptext.nfs_srv_16_placeholder,
-      tooltip: helptext.nfs_srv_16_tooltip,
-      relation: helptext.nfs_srv_16_relation,
-    },
-    {
-      type: 'checkbox',
-      name: 'nfs_srv_mountd_log',
-      placeholder: helptext.nfs_srv_mountd_log_placeholder,
-      tooltip: helptext.nfs_srv_mountd_log_tooltip,
-    },
-    {
-      type: 'checkbox',
-      name: 'nfs_srv_statd_lockd_log',
-      placeholder: helptext.nfs_srv_statd_lockd_log_placeholder,
-      tooltip: helptext.nfs_srv_statd_lockd_log_tooltip,
-    },
+      name: 'divider',
+      divider: true
+    }
   ];
 
   private nfs_srv_bindip: any;
@@ -105,23 +148,25 @@ export class ServiceNFSComponent {
     protected rest: RestService, protected ws: WebSocketService,
   ) {}
 
-  afterInit(entityForm: any) {
+  afterInit(entityForm: EntityFormComponent) {
+    entityForm.submitFunction = body => this.ws.call('nfs.update', [body]);
+
     this.ws.call('notifier.choices', ['IPChoices']).subscribe((res) => {
-      this.nfs_srv_bindip = _.find(this.fieldConfig, { name: 'nfs_srv_bindip' });
-      for (let item of res) {
+      this.nfs_srv_bindip = _.find(this.fieldConfig, { name: 'bindip' });
+      for (const item of res) {
         this.nfs_srv_bindip.options.push({ label: item[0], value: item[1] });
       }
     });
 
-    entityForm.formGroup.controls['nfs_srv_16'].valueChanges.subscribe((res)=> {
-      if (entityForm.formGroup.controls['nfs_srv_v4'].value) {
+    entityForm.formGroup.controls['userd_manage_gids'].valueChanges.subscribe((res)=> {
+      if (entityForm.formGroup.controls['v4'].value) {
         if (res) {
-          if (entityForm.formGroup.controls['nfs_srv_v4_v3owner'].enabled) {
-            entityForm.formGroup.controls['nfs_srv_v4_v3owner'].disable();
+          if (entityForm.formGroup.controls['v4_v3owner'].enabled) {
+            entityForm.formGroup.controls['v4_v3owner'].disable();
           }
         } else {
           if (entityForm.formGroup.controls['nfs_srv_v4_v3owner'].disabled) {
-            entityForm.formGroup.controls['nfs_srv_v4_v3owner'].enable();
+            entityForm.formGroup.controls['v4_v3owner'].enable();
           }
         }
       }
