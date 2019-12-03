523a790a9b06da41ed60ecb48bc027e31f9c3aa9
Aaron Ervin <aervin@ixsystems.com>
2019-12-03 11:20:01 -0600

    NAS-103693 #in-progress -- lldp service rest -> ws

--- src/app/pages/services/components/service-lldp/service-lldp.component.ts
+++ src/app/pages/services/components/service-lldp/service-lldp.component.ts
@@ -1,10 +1,10 @@
 import { ApplicationRef, Component, Injector } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import * as _ from 'lodash';
-
+import { EntityFormComponent } from 'app/pages/common/entity/entity-form';
+import helptext from '../../../../helptext/services/components/service-lldp';
 import { RestService, WebSocketService } from '../../../../services/';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
-import helptext from '../../../../helptext/services/components/service-lldp';
+
 
 @Component({
   selector : 'lldp-edit',
@@ -12,25 +12,25 @@ import helptext from '../../../../helptext/services/components/service-lldp';
 })
 
 export class ServiceLLDPComponent {
-  protected resource_name: string = 'services/lldp';
+  protected queryCall = 'lldp.config';
   protected route_success: string[] = [ 'services' ];
 
   public fieldConfig: FieldConfig[] = [
     {
       type : 'checkbox',
-      name : 'lldp_intdesc',
+      name : 'intdesc',
       placeholder : helptext.lldp_intdesc_placeholder,
       tooltip: helptext.lldp_intdesc_tooltip,
     },
     {
       type : 'input',
-      name : 'lldp_country',
+      name : 'country',
       placeholder : helptext.lldp_country_placeholder,
       tooltip: helptext.lldp_country_tooltip,
     },
     {
       type : 'input',
-      name : 'lldp_location',
+      name : 'location',
       placeholder : helptext.lldp_location_placeholder,
       tooltip: helptext.lldp_location_tooltip
     },
@@ -41,5 +41,7 @@ export class ServiceLLDPComponent {
               protected _injector: Injector, protected _appRef: ApplicationRef,
               ) {}
 
-  afterInit(entityEdit: any) { }
+  afterInit(entityEdit: EntityFormComponent) {
+    entityEdit.submitFunction = body => this.ws.call('lldp.update', [body]) 
+  }
 }
