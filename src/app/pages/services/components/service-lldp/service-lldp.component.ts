90eea7063e0f378d976b604adde62bf766b79b77
Aaron Ervin <aervin@ixsystems.com>
2019-12-04 16:53:28 -0600

    NAS-103693 -- add fieldset to lldp config form

--- src/app/pages/services/components/service-lldp/service-lldp.component.ts
+++ src/app/pages/services/components/service-lldp/service-lldp.component.ts
@@ -1,39 +1,43 @@
 import { ApplicationRef, Component, Injector } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EntityFormComponent } from 'app/pages/common/entity/entity-form';
+import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
 import helptext from '../../../../helptext/services/components/service-lldp';
 import { RestService, WebSocketService } from '../../../../services/';
-import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
-
 
 @Component({
   selector : 'lldp-edit',
   template : `<entity-form [conf]="this"></entity-form>`
 })
-
 export class ServiceLLDPComponent {
   protected queryCall = 'lldp.config';
   protected route_success: string[] = [ 'services' ];
 
-  public fieldConfig: FieldConfig[] = [
+  public fieldSets: FieldSet[] = [
     {
-      type : 'checkbox',
-      name : 'intdesc',
-      placeholder : helptext.lldp_intdesc_placeholder,
-      tooltip: helptext.lldp_intdesc_tooltip,
-    },
-    {
-      type : 'input',
-      name : 'country',
-      placeholder : helptext.lldp_country_placeholder,
-      tooltip: helptext.lldp_country_tooltip,
-    },
-    {
-      type : 'input',
-      name : 'location',
-      placeholder : helptext.lldp_location_placeholder,
-      tooltip: helptext.lldp_location_tooltip
-    },
+      name: helptext.lldp_fieldset_general,
+      label: true,
+      config: [
+        {
+          type : 'checkbox',
+          name : 'intdesc',
+          placeholder : helptext.lldp_intdesc_placeholder,
+          tooltip: helptext.lldp_intdesc_tooltip,
+        },
+        {
+          type : 'input',
+          name : 'country',
+          placeholder : helptext.lldp_country_placeholder,
+          tooltip: helptext.lldp_country_tooltip,
+        },
+        {
+          type : 'input',
+          name : 'location',
+          placeholder : helptext.lldp_location_placeholder,
+          tooltip: helptext.lldp_location_tooltip
+        }
+      ]
+    }
   ];
 
   constructor(protected router: Router, protected route: ActivatedRoute,
