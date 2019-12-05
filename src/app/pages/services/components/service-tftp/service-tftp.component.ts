587a80d49a62561c1b4e648993d76ae0c23f7633
Aaron Ervin <aervin@ixsystems.com>
2019-12-05 09:12:52 -0600

    NAS-103700 #in-progress -- tftp rest -> ws

--- src/app/pages/services/components/service-tftp/service-tftp.component.ts
+++ src/app/pages/services/components/service-tftp/service-tftp.component.ts
@@ -1,71 +1,94 @@
 import { ApplicationRef, Component, Injector } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import * as _ from 'lodash';
-
-import { RestService, WebSocketService, UserService } from '../../../../services/';
-import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import { EntityFormComponent } from 'app/pages/common/entity/entity-form';
+import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
 import helptext from '../../../../helptext/services/components/service-tftp';
+import { RestService, UserService, WebSocketService } from '../../../../services/';
 
 @Component({
   selector : 'tftp-edit',
   template : `<entity-form [conf]="this"></entity-form>`,
 })
-
 export class ServiceTFTPComponent {
-
-  protected resource_name: string = 'services/tftp';
+  protected queryCall = 'tftp.config';
   protected route_success: string[] = [ 'services' ];
 
-  public fieldConfig: FieldConfig[] = [
+  protected fieldSets: FieldSet[] = [
     {
-      type : 'explorer',
-      initial: '/mnt',
-      explorerType: 'directory',
-      name : 'tftp_directory',
-      placeholder : helptext.tftp_directory_placeholder,
-      tooltip : helptext.tftp_directory_tooltip,
+      name: helptext.tftp_fieldset_path,
+      label: true,
+      width: '50%',
+      config: [{
+        type : 'explorer',
+        initial: '/mnt',
+        explorerType: 'directory',
+        name : 'directory',
+        placeholder : helptext.tftp_directory_placeholder,
+        tooltip : helptext.tftp_directory_tooltip,
+      }]
     },
     {
-      type : 'checkbox',
-      name : 'tftp_newfiles',
-      placeholder : helptext.tftp_newfiles_placeholder,
-      tooltip : helptext.tftp_newfiles_tooltip,
+      name: helptext.tftp_fieldset_conn,
+      label: true,
+      width: '50%',
+      config: [
+        {
+          type : 'input',
+          name : 'host',
+          placeholder : helptext.tftp_host_placeholder,
+          tooltip : helptext.tftp_host_tooltip,
+        },
+        {
+          type : 'input',
+          name : 'port',
+          placeholder : helptext.tftp_port_placeholder,
+          tooltip : helptext.tftp_port_tooltip,
+        },
+        {
+          type : 'combobox',
+          name : 'username',
+          placeholder : helptext.tftp_username_placeholder,
+          tooltip : helptext.tftp_username_tooltip,
+          options : [],
+          searchOptions: [],
+          parent: this,
+          updater: this.updateUserSearchOptions,
+        }
+      ]
+    },
+    { name: 'divider', divider: true },
+    {
+      name: helptext.tftp_fieldset_access,
+      label: true,
+      width: '50%',
+      config: [
+        {
+          type : 'permissions',
+          name : 'umask',
+          noexec: true,
+          placeholder : helptext.tftp_umask_placeholder,
+          tooltip : helptext.tftp_umask_tooltip,
+        },
+        {
+          type : 'checkbox',
+          name : 'newfiles',
+          placeholder : helptext.tftp_newfiles_placeholder,
+          tooltip : helptext.tftp_newfiles_tooltip,
+        }
+      ]
     },
     {
-      type : 'input',
-      name : 'tftp_host',
-      placeholder : helptext.tftp_host_placeholder,
-      tooltip : helptext.tftp_host_tooltip,
+      name: helptext.tftp_fieldset_other,
+      label: true,
+      width: '50%',
+      config: [{
+        type : 'textarea',
+        name : 'options',
+        placeholder : helptext.tftp_options_placeholder,
+        tooltip : helptext.tftp_options_tooltip
+      }]
     },
-    {
-      type : 'input',
-      name : 'tftp_port',
-      placeholder : helptext.tftp_port_placeholder,
-      tooltip : helptext.tftp_port_tooltip,
-    },
-    {
-      type : 'combobox',
-      name : 'tftp_username',
-      placeholder : helptext.tftp_username_placeholder,
-      tooltip : helptext.tftp_username_tooltip,
-      options : [],
-      searchOptions: [],
-      parent: this,
-      updater: this.updateUserSearchOptions,
-    },
-    {
-      type : 'permissions',
-      name : 'tftp_umask',
-      noexec: true,
-      placeholder : helptext.tftp_umask_placeholder,
-      tooltip : helptext.tftp_umask_tooltip,
-    },
-    {
-      type : 'textarea',
-      name : 'tftp_options',
-      placeholder : helptext.tftp_options_placeholder,
-      tooltip : helptext.tftp_options_tooltip
-    },
+    { name: 'divider', divider: true }
   ];
 
   protected tftp_username: any;
@@ -75,29 +98,22 @@ export class ServiceTFTPComponent {
               protected _injector: Injector, protected _appRef: ApplicationRef,
               protected userService: UserService) {}
 
-  resourceTransformIncomingRestData(data: any) {
-    let perm = parseInt(data['tftp_umask'], 8);
-    let mask = (~perm & 0o666).toString(8);
-    while (mask.length < 3) {
-      mask = '0' + mask;
-    }
-    data['tftp_umask'] = mask;
-
-    return data;
-  }
-
   preInit(entityEdit: any) {
     this.userService.userQueryDSCache().subscribe(items => {
       const users = [];
       for (let i = 0; i < items.length; i++) {
         users.push({label: items[i].username, value: items[i].username});
       }
-      this.tftp_username = _.find(this.fieldConfig, {'name' : 'tftp_username'});
+      this.tftp_username =  this.fieldSets
+        .find(set => set.name === helptext.tftp_fieldset_conn)
+        .config.find(config => config.name === 'username');
       this.tftp_username.options = users;
     });
   }
 
-  afterInit(entityEdit: any) { }
+  afterInit(entityEdit: EntityFormComponent) {
+    entityEdit.submitFunction = body => this.ws.call('tftp.update', [body]);
+  }
 
   updateUserSearchOptions(value = "", parent) {
     parent.userService.userQueryDSCache(value).subscribe(items => {
