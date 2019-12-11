51936124e7fd1aa11d57f75aac7efe98cd76b6ba
dszidi <damianos@knoxbug.org>
2019-12-11 14:04:22 -0500

    Tunables page converted to web socket api.

--- src/app/pages/system/tunable/tunable-form/tunable-form.component.ts
+++ src/app/pages/system/tunable/tunable-form/tunable-form.component.ts
@@ -1,67 +1,88 @@
 import { ApplicationRef, Component, Injector } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { helptext_system_tunable as helptext } from 'app/helptext/system/tunable';
-import { RestService, WebSocketService } from '../../../../services/';
+import { WebSocketService } from '../../../../services/';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
+import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
 
 @Component({
   selector: 'system-tunable-edit',
   template: `<entity-form [conf]="this"></entity-form>`
 })
 export class TunableFormComponent {
-
-  protected resource_name: string = 'system/tunable';
+  protected queryCall = 'tunable.query';
+  protected queryKey = 'id';
+  protected editCall = 'tunable.update';
+  protected addCall = 'tunable.create';
+  protected pk: any;
+            
   protected route_success: string[] = ['system', 'tunable'];
   protected isEntity: boolean = true;
 
-  protected fieldConfig: FieldConfig[] = [{
-      type: 'input',
-      name: 'tun_var',
-      placeholder: helptext.var.placeholder,
-      tooltip: helptext.var.tooltip,
-      required: true,
-      validation : helptext.var.validation
-    },
+  protected fieldConfig: FieldConfig[] = [];
+  protected fieldSets: FieldSet[] = [
     {
-      type: 'textarea',
-      name: 'tun_value',
-      placeholder: helptext.value.placeholder,
-      tooltip: helptext.value.tooltip,
-      required: true,
-      validation : helptext.value.validation
-    },
-    {
-      type: 'select',
-      name: 'tun_type',
-      placeholder: helptext.type.placeholder,
-      tooltip: helptext.type.tooltip,
-      options: [
-        { label: 'loader', value: 'loader' },
-        { label: 'rc.conf', value: 'rc' },
-        { label: 'sysctl', value: 'sysctl' },
+      name: 'Tunable',
+      class:'add-cron',
+      label:true,
+      width:'300px',
+      config:[
+        {
+          type: 'input',
+          name: 'var',
+          placeholder: helptext.var.placeholder,
+          tooltip: helptext.var.tooltip,
+          required: true,
+          validation : helptext.var.validation
+        },
+        {
+          type: 'textarea',
+          name: 'value',
+          placeholder: helptext.value.placeholder,
+          tooltip: helptext.value.tooltip,
+          required: true,
+          validation : helptext.value.validation
+        },
+        {
+          type: 'select',
+          name: 'type',
+          placeholder: helptext.type.placeholder,
+          tooltip: helptext.type.tooltip,
+          options: [
+            { label: 'loader', value: 'LOADER' },
+            { label: 'rc.conf', value: 'RC' },
+            { label: 'sysctl', value: 'SYSCTL' },
+          ]
+        },
+        {
+          type: 'input',
+          name: 'comment',
+          placeholder: helptext.description.placeholder,
+          tooltip: helptext.description.tooltip,
+        },
+        {
+          type: 'checkbox',
+          name: 'enabled',
+          placeholder: helptext.enabled.placeholder,
+          tooltip: helptext.enabled.tooltip,
+        },
       ]
     },
     {
-      type: 'input',
-      name: 'tun_comment',
-      placeholder: helptext.description.placeholder,
-      tooltip: helptext.description.tooltip,
+      name:'divider',
+      divider:true
     },
-    {
-      type: 'checkbox',
-      name: 'tun_enabled',
-      placeholder: helptext.enabled.placeholder,
-      tooltip: helptext.enabled.tooltip,
-    },
-  ];
+  ]
 
 
-  constructor(protected router: Router, protected route: ActivatedRoute,
-    protected rest: RestService, protected ws: WebSocketService,
-    protected _injector: Injector, protected _appRef: ApplicationRef) {}
+  constructor(protected router: Router, 
+    protected route: ActivatedRoute,
+    protected ws: WebSocketService,
+    protected _injector: Injector, 
+    protected _appRef: ApplicationRef) {}
 
   afterInit(entityForm: any) {
-    entityForm.formGroup.controls['tun_enabled'].setValue(true);
-    entityForm.formGroup.controls['tun_type'].setValue('loader');
+    entityForm.formGroup.controls['enabled'].setValue(true);
+    entityForm.formGroup.controls['type'].setValue('loader');
   }
 }
--- src/app/pages/system/tunable/tunable-list/tunable-list.component.ts
+++ src/app/pages/system/tunable/tunable-list/tunable-list.component.ts
@@ -2,7 +2,7 @@ import { ApplicationRef, Component, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as _ from 'lodash';
 import { Subscription } from 'rxjs';
-import { RestService, WebSocketService } from '../../../../services/';
+import { WebSocketService } from '../../../../services/';
 
 @Component({
   selector: 'system-tunables-list',
@@ -12,7 +12,9 @@ import { RestService, WebSocketService } from '../../../../services/';
 export class TunableListComponent {
 
   public title = "Tunables";
-  protected resource_name: string = 'system/tunable';
+  protected wsDelete = "tunable.delete";
+  public queryCall:string = 'tunable.query';
+
   protected route_delete: string[] = ['system', 'tunable', 'delete'];
   protected route_edit: string[] = ['system', 'tunable', 'edit'];
   protected route_success: string[] = [ 'system', 'tunable' ];
@@ -22,25 +24,27 @@ export class TunableListComponent {
   public busy: Subscription;
   public sub: Subscription;
 
-  constructor(protected router: Router, protected aroute: ActivatedRoute,
-    protected rest: RestService, protected ws: WebSocketService,
-    protected _injector: Injector, protected _appRef: ApplicationRef) {}
+  constructor(protected router: Router, 
+    protected aroute: ActivatedRoute,
+    protected ws: WebSocketService,
+    protected _injector: Injector, 
+    protected _appRef: ApplicationRef) {}
 
   public columns: Array < any > = [
-    { name: 'Variable', prop: 'tun_var', always_display: true },
-    { name: 'Value', prop: 'tun_value' },
-    { name: 'Type', prop: 'tun_type' },
-    { name: 'Description', prop: 'tun_comment' },
-    { name: 'Enabled', prop: 'tun_enabled' },
+    { name: 'Variable', prop: 'var', always_display: true },
+    { name: 'Value', prop: 'value' },
+    { name: 'Type', prop: 'type' },
+    { name: 'Description', prop: 'comment' },
+    { name: 'Enabled', prop: 'enabled' },
   ];
-  public rowIdentifier = 'tun_var';
+  public rowIdentifier = 'var';
 
   public config: any = {
     paging: true,
     sorting: { columns: this.columns },
     deleteMsg: {
       title: 'Tunable',
-      key_props: ['tun_var']
+      key_props: ['var']
     },
   }
 
