799831762a64b4321f8af6a3d91f01fe81622b72
Mrt134 <timmoore88@gmail.com>
2019-12-12 10:23:43 -0500

    NAS-100815: Rewrite Rollback Dialog - Conduct behavior testing for the rollback action and review the zfs.py middleware code. - Rewrite the rollback dialog to reflect the behavior. - Expand the warning about what is deleted by the action. - Yarn build testing: no issues.

--- src/app/helptext/storage/snapshots/snapshots.ts
+++ src/app/helptext/storage/snapshots/snapshots.ts
@@ -24,7 +24,8 @@ snapshot_add_recursive_tooltip: T('Set to include child datasets of the \
 
 // storage/snapshots/snapshot-clone
 snapshot_clone_name_placeholder: T('Name'),
-snapshot_clone_name_tooltip: T('Name of the new dataset created from the cloned snapshot.'),
+snapshot_clone_name_tooltip: T('Name of the new dataset created from the \
+ cloned snapshot.'),
 snapshot_clone_name_validation : [ Validators.required ],
 
 label_clone: T('Clone to New Dataset'),
@@ -32,16 +33,33 @@ label_delete: T('Delete'),
 label_rollback: T('Rollback'),
 
 rollback_snapshot_placeholder: T('Snapshot'),
-rollback_recursive_radio_placeholder: T('Rollback options'),
-rollback_recursive_radio_tooltip: T('Select an option for rolling back.'),
-rollback_dataset_placeholder: T('This dataset'),
-rollback_dataset_tooltiP: T('Setting this will rollback only the current dataset, leaving any child datasets or clones unchanged.'),
-rollback_recursive_placeholder: T('This dataset and child datasets'),
-rollback_recursive_tooltip: T('Setting this will rollback child datasets as well as the current dataset, destroying any newer snapshots on the children as well as this one.'),
-rollback_recursive_clones_placeholder: T('This dataset, child datasets and clones'),
-rollback_recursive_clones_tooltip: T('Setting this will rollback child datasets and clones as well as the current dataset, destroying any newer snapshots on the children as well as this one.'),
-rollback_warning: T("<b>WARNING:</b> Rolling back to this snapshot will permanently delete later snapshots of this dataset.\
- Do not roll back until all desired snapshots have been backed up!"),
-rollback_title: ('Rollback Snapshot'),
+
+rollback_recursive_radio_placeholder: T('Stop Rollback if Snapshots Exist:'),
+rollback_recursive_radio_tooltip: T('Choose a safety level for the \
+ rollback action. The rollback is canceled when the safety check finds \
+ additional snapshots that are directly related to the dataset being rolled back.'),
+
+rollback_dataset_placeholder: T('Newer Intermediate, Child, and Clone'),
+rollback_dataset_tooltip: T('Stops the rollback when the safety check \
+ finds any related intermediate, child dataset, or clone snapshots that \
+ are newer than the rollback snapshot.'),
+
+rollback_recursive_placeholder: T('Newer Clone'),
+rollback_recursive_tooltip: T('Stops the rollback when the safety \
+ check finds any related clone snapshots that are newer than the \
+ rollback snapshot.'),
+
+rollback_recursive_clones_placeholder: T('No Safety Check (WARNING)'),
+rollback_recursive_clones_tooltip: T('The rollback will destroy any \
+ related intermediate, child dataset, and cloned snapshots that are \
+ newer than the rollback snapshot.'),
+
+rollback_warning: T("<b>WARNING:</b> Rolling the dataset back \
+ destroys data on the dataset <i>and</i> can destroy additional snapshots \
+ that are related to the dataset. <b>This can result in permanent data \
+ loss!</b> Do not roll back until all desired data and snapshots are \
+ backed up."),
+
+rollback_title: ('Dataset Rollback From Snapshot'),
 rollback_confirm: T('Confirm')
 }
