fcae9c37f36c42d2e7f3df883fd0c5053efe0e71
Erin Clark <erin@ixsystems.com>
2019-12-13 17:51:13 -0800

    Make the right call when resetting configuration
    NAS-104345

--- src/app/pages/system/general/config-reset/config-reset.component.ts
+++ src/app/pages/system/general/config-reset/config-reset.component.ts
@@ -3,7 +3,7 @@ import { Location } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { Observable } from 'rxjs/Observable';
 import { MatSnackBar } from '@angular/material';
-import { RestService, WebSocketService } from '../../../../services/';
+import { RestService, WebSocketService, DialogService } from '../../../../services/';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -13,16 +13,20 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class ConfigResetComponent {
 
   public route_success: string[] = ['system', 'general'];
-  constructor(protected router: Router, protected ws: WebSocketService, private _location: Location, public snackBar: MatSnackBar) {}
+  constructor(protected router: Router, protected ws: WebSocketService, protected rest:RestService, private _location: Location, public snackBar: MatSnackBar, protected dialogService: DialogService) {}
 
   _displayTime() {
 
   }
   doSubmit() {
-    this.ws.call('system.reboot', [{ delay: 5 }]).subscribe((res) => {
-      // this.countDown = setInterval( () => {if(this.count>0){this.count -= 1}}, 1000);
-      this.openSnackBar("System will reboot in 5 seconds", "Rebooting");
-    });;
+    this.rest.post('system/config/factory_restore', {}).subscribe(restore => {
+      this.ws.call('system.reboot', [{ delay: 5 }]).subscribe((res) => {
+        // this.countDown = setInterval( () => {if(this.count>0){this.count -= 1}}, 1000);
+        this.openSnackBar("System will reboot in 5 seconds", "Rebooting");
+      });;
+    }, (err) => {
+      this.dialogService.errorReport("Error Resetting Configuration", err.error, err.trace.formatted);
+    });
   }
 
   openSnackBar(message: string, action: string) {
