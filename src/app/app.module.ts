b654179262b52b09208cf0f83cbc64544f121f8f
Dennis Mullen <dkmullen@gmail.com>
2019-12-31 14:35:03 -0500

    NAS-103711-v12-pt3 Dialogs, tree-table, topbar, sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { NgxWebstorageModule } from 'ngx-webstorage';
+import { CommonDirectivesModule } from 'app/directives/common/common-directives.module';
 
 import { MaterialModule} from './appMaterial.module'; //customized MaterialModule
 import { rootRouterConfig } from './app.routes';
@@ -79,6 +80,7 @@ export function createTranslateLoader(http: HttpClient) {
     FormsModule,
     ReactiveFormsModule,
     EntityModule,
+    CommonDirectivesModule,
     NgxWebstorageModule.forRoot(),
   ],
   declarations: [AppComponent, ConfirmDialog, ErrorDialog, InfoDialog, AboutModalDialog, TaskManagerComponent, DirectoryServicesMonitorComponent, ConsolePanelModalDialog, DownloadKeyModalDialog, SelectDialogComponent ],
