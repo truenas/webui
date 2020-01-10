3b19f8cfcde3023683261f69434907eef30bcc72
Erin Clark <erin@ixsystems.com>
2020-01-10 15:50:15 -0800

    Apply fieldsets to S3 configuration
    NAS-104456

--- src/app/pages/services/components/service-s3/service-s3.component.ts
+++ src/app/pages/services/components/service-s3/service-s3.component.ts
@@ -7,6 +7,7 @@ import { RestService, SystemGeneralService, WebSocketService } from '../../../..
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 import { T } from '../../../../translate-marker';
 import helptext from '../../../../helptext/services/components/service-s3';
+import { FieldSet } from '../../../common/entity/entity-form/models/fieldset.interface';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -18,87 +19,85 @@ import { map } from 'rxjs/operators';
 export class ServiceS3Component implements OnDestroy {
   //protected resource_name: string = 'services/s3';
   protected queryCall: string = 's3.config';
-  protected addCall: string = 's3.update';
+  protected updateCall: string = 's3.update';
   protected route_success: string[] = [ 'services' ];
   private certificate: any;
   private ip_address: any;
 
 
-  public fieldConfig: FieldConfig[] = [
+  public fieldConfig: FieldConfig[] = [];
+  public fieldSets: FieldSet[] = [
     {
-      type : 'select',
-      name : 'bindip',
-      placeholder : helptext.bindip_placeholder,
-      tooltip: helptext.bindip_tooltip,
-      options : helptext.bindip_options
-    },
-    {
-      type : 'input',
-      name : 'bindport',
-      placeholder : helptext.bindport_placeholder,
-      tooltip: helptext.bindport_tooltip,
-      value: '9000',
-      required: true,
-      validation: helptext.bindport_validation
-    },
-    {
-      type : 'input',
-      name : 'access_key',
-      placeholder : helptext.access_key_placeholder,
-      tooltip: helptext.access_key_tooltip,
-      required: true,
-      validation: helptext.access_key_validation
-    },
-    {
-      type : 'input',
-      name : 'secret_key',
-      placeholder : helptext.secret_key_placeholder,
-      togglePw: true,
-      tooltip: helptext.secret_key_tooltip,
-      inputType : 'password',
-      required : true,
-      validation: helptext.secret_key_validation
-    },
-    {
-      type : 'input',
-      name : 'secret_key2',
-      placeholder : helptext.secret_key2_placeholder,
-      inputType : 'password',
-      required: true,
-      validation : helptext.secret_key2_validation
-    },
-    {
-      type : 'explorer',
-      initial: '/mnt',
-      explorerType: 'directory',
-      name : 'storage_path',
-      placeholder : helptext.storage_path_placeholder,
-      tooltip: helptext.storage_path_tooltip,
-      required: true,
-      validation: helptext.storage_path_validation
-    },
-    {
-      type : 'checkbox',
-      name : 'browser',
-      placeholder : helptext.browser_placeholder,
-      tooltip: helptext.browser_tooltip,
-    },
-/*  This is to be enabled when the mode feature is finished and fully implemented for S3
-    {
-      type : 'select',
-      name : 'mode',
-      placeholder : helptext.mode_placeholder,
-      options : helptext.mode_options
-    },
-*/
-    {
-      type : 'select',
-      name : 'certificate',
-      placeholder : helptext.certificate_placeholder,
-      tooltip : helptext.certificate_tooltip,
-      options : []
-    },
-  ];
+      name: helptext.fieldset_title,
+      class: 'group-configuration-form',
+      label:true,
+      config: [
+      {
+        type : 'select',
+        name : 'bindip',
+        placeholder : helptext.bindip_placeholder,
+        tooltip: helptext.bindip_tooltip,
+        options : helptext.bindip_options
+      },
+      {
+        type : 'input',
+        name : 'bindport',
+        placeholder : helptext.bindport_placeholder,
+        tooltip: helptext.bindport_tooltip,
+        value: '9000',
+        required: true,
+        validation: helptext.bindport_validation
+      },
+      {
+        type : 'input',
+        name : 'access_key',
+        placeholder : helptext.access_key_placeholder,
+        tooltip: helptext.access_key_tooltip,
+        required: true,
+        validation: helptext.access_key_validation
+      },
+      {
+        type : 'input',
+        name : 'secret_key',
+        placeholder : helptext.secret_key_placeholder,
+        togglePw: true,
+        tooltip: helptext.secret_key_tooltip,
+        inputType : 'password',
+        validation: helptext.secret_key_validation
+      },
+      {
+        type : 'explorer',
+        initial: '/mnt',
+        explorerType: 'directory',
+        name : 'storage_path',
+        placeholder : helptext.storage_path_placeholder,
+        tooltip: helptext.storage_path_tooltip,
+        required: true,
+        validation: helptext.storage_path_validation
+      },
+      {
+        type : 'checkbox',
+        name : 'browser',
+        placeholder : helptext.browser_placeholder,
+        tooltip: helptext.browser_tooltip,
+      },
+  /*  This is to be enabled when the mode feature is finished and fully implemented for S3
+      {
+        type : 'select',
+        name : 'mode',
+        placeholder : helptext.mode_placeholder,
+        options : helptext.mode_options
+      },
+  */
+      {
+        type : 'select',
+        name : 'certificate',
+        placeholder : helptext.certificate_placeholder,
+        tooltip : helptext.certificate_tooltip,
+        options : [{label: '---', value: null}]
+      },
+    ]
+  }];
   protected storage_path: any;
   protected storage_path_subscription: any;
 
@@ -143,31 +142,15 @@ export class ServiceS3Component implements OnDestroy {
       .subscribe(choices => {
         _.find(this.fieldConfig, { name: "bindip" }).options = choices;
       });
-    entityForm.ws.call('s3.config').subscribe((res)=>{
-      entityForm.formGroup.controls['bindip'].setValue(res.bindip);
-      entityForm.formGroup.controls['bindport'].setValue(res.bindport);
-      entityForm.formGroup.controls['access_key'].setValue(res.access_key);
-      entityForm.formGroup.controls['secret_key'].setValue(res.secret_key);
-      entityForm.formGroup.controls['secret_key2'].setValue(res.secret_key);
-      entityForm.formGroup.controls['storage_path'].setValue(res.storage_path);
-      entityForm.formGroup.controls['browser'].setValue(res.browser);
-      //entityForm.formGroup.controls['mode'].setValue(res.mode);
-      if (res.certificate && res.certificate.id) {
-        entityForm.formGroup.controls['certificate'].setValue(res.certificate.id);
-      }
-    })
     entityForm.submitFunction = this.submitFunction;
-
-  }
-
-  clean(value) {
-    delete value['secret_key2'];
-
-    return value;
   }
 
   resourceTransformIncomingRestData(data) {
+    if (data.certificate && data.certificate.id) {
+      data['certificate'] = data.certificate.id;
+    }
     delete data['secret_key'];
+    return data;
   }
 
   submitFunction(this: any, entityForm: any,){
