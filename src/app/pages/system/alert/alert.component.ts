0a667497b751279a666f4865d96d21ee30bfa44a
Erin Clark <erin@ixsystems.com>
2020-01-17 17:18:06 -0800

    Add warning level field to alert settings
    NAS-104702
    
    (cherry picked from commit c0a02491953cf09cabea6b4be62f70205c9c42f6)

--- src/app/pages/system/alert/alert.component.ts
+++ src/app/pages/system/alert/alert.component.ts
@@ -30,12 +30,21 @@ interface AlertCategory {
 })
 export class AlertConfigComponent implements OnInit {
   protected route_success = ['system', 'alertsettings'];
-  protected queryCall = 'alertdefaultsettings.config';
+  protected queryCall = 'alertclasses.config';
   protected editCall = 'alertclasses.update';
   protected isEntity = true;
   public fieldSets: FieldSets;
   public fieldConfig: FieldConfig[] = [];
   protected settingOptions: any = [];
+  protected warningOptions: any = [
+    {label: "INFO", value: "INFO"},
+    {label: "NOTICE", value: "NOTICE"},
+    {label: "WARNING", value: "WARNING"},
+    {label: "ERROR", value: "ERROR"},
+    {label: "CRITICAL", value: "CRITICAL"},
+    {label: "ALERT", value: "ALERT"},
+    {label: "EMERGENCY", value: "EMERGENCY"},
+  ];
   public formGroup: any;
   public settingFormGroup: any;
   public isReady = false;
@@ -49,6 +58,7 @@ export class AlertConfigComponent implements OnInit {
   ) {}
 
   async ngOnInit() {
+    this.loader.open();
     this.ws.call('alert.list_policies', []).subscribe((res) => {
       for (let i = 0; i < res.length; i++) {
         this.settingOptions.push({ label: res[i], value: res[i] });
@@ -67,19 +77,34 @@ export class AlertConfigComponent implements OnInit {
     categories.forEach((category, index) => {
       const modulo = index % 2;
 
+      let config = [];
+      for (let i = 0; i < category.classes.length; i++) {
+        const c = category.classes[i];
+        config.push({
+          type: "select",
+          name: c.id + '_level',
+          inlineLabel: c.title,
+          placeholder: T("Set Warning Level"),
+          options: this.warningOptions,
+          value: "WARNING"
+        },
+        {
+          type: "select",
+          name: c.id + '_policy',
+          inlineLabel: " ",
+          placeholder: T("Set frequency"),
+          options: this.settingOptions,
+          value: "IMMEDIATELY"
+        })
+      }
+
       let fieldSet = {
         name: category.title,
         label: true,
         width: "40%",
-        config: category.classes.map(c => ({
-          type: "select",
-          name: c.id,
-          inlineLabel: c.title,
-          placeholder: T("Set frequency"),
-          options: this.settingOptions,
-          value: "IMMEDIATELY"
-        }))
+        config: config
       }
+      console.log(fieldSet);
     
       sets.push(fieldSet);
 
@@ -98,9 +123,17 @@ export class AlertConfigComponent implements OnInit {
     this.formGroup = this.entityFormService.createFormGroup(this.fieldConfig);
   
     this.ws.call(this.queryCall).subscribe((res) => {
-      for (const k in res.settings) {
-        this.formGroup.controls[k].setValue(res.settings[k]);
+      this.loader.close();
+      for (const k in res.classes) {
+        for (const j in res.classes[k]) {
+          const prop = k + '_' + j;
+          this.formGroup.controls[prop].setValue(res.classes[k][j]);
+        }
       }
+    },
+    (err) => {
+      this.loader.close();
+      new EntityUtils().handleWSError(this, err, this.dialog)
     });
   }
 
@@ -108,7 +141,13 @@ export class AlertConfigComponent implements OnInit {
     const payload = { classes: {} };
 
     for (const key in this.formGroup.value) {
-      payload.classes[key] = { policy: this.formGroup.value[key] };
+      const key_values = key.split('_');
+      const alert_class = key_values[0];
+      const class_key = key_values[1];
+      if(!payload.classes[alert_class]) {
+        payload.classes[alert_class] = {};
+      }
+      payload.classes[alert_class][class_key] = this.formGroup.value[key];
     }
 
     this.loader.open();
