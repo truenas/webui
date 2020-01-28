fb3cea561b8387ce500b60206f2656a8f25b8320
Mrt134 <timmoore88@gmail.com>
2020-01-28 07:17:06 -0500

    NAS-104473: fix italic on one more element

--- src/app/helptext/network/interfaces/interfaces-form.ts
+++ src/app/helptext/network/interfaces/interfaces-form.ts
@@ -157,7 +157,7 @@ mtu_tooltip: T('Maximum Transmission Unit, the largest protocol data \
  unit that can be communicated. The largest workable MTU size varies \
  with network interfaces and equipment. <i>1500</i> and <i>9000</i> \
  are standard Ethernet MTU sizes. Leaving blank restores the field to \
- the default value of *1500*.'),
+ the default value of <i>1500</i>.'),
 mtu_validation: [rangeValidator(68, 9000)],
 
 alias_list_placeholder: T('Aliases'),
