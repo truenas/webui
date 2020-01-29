b1aef2da8de2d8a22773adaf64edc845170afa64
Lola Yang <lola.ly27@gmail.com>
2020-01-29 15:02:01 -0800

    NAS-100150 fixed disk sector size config of device add/edit

--- src/app/helptext/vm/devices/device-add-edit.ts
+++ src/app/helptext/vm/devices/device-add-edit.ts
@@ -34,7 +34,8 @@ mode_options : [
 
 sectorsize_placeholder : T('Disk sector size'),
 sectorsize_tooltip : T('Select a sector size in bytes. <i>Default</i> leaves the\
- leaves the sector size unset.'),
+ sector size unset, using value reported from ZFS volume. Setting a sector size\
+ will force the logical and physical sector size to that value.'),
 sectorsize_options: [
   { label: 'Default', value:0 },
   { label: '512', value:512 },
