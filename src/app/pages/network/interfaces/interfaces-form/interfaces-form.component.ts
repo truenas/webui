ccb899f3e3fb279ba00ca39d763578ddfb00c7c5
Erin Clark <erincodepirate@gmail.com>
2020-02-04 18:30:44 -0800

    Don't append a controller label if one is not available
    NAS-104835
    
    (cherry picked from commit 8dc7d6f6031acd82b3df41ea5af655bc3e5d97e6)

--- src/app/pages/network/interfaces/interfaces-form/interfaces-form.component.ts
+++ src/app/pages/network/interfaces/interfaces-form/interfaces-form.component.ts
@@ -376,6 +376,7 @@ export class InterfacesFormComponent extends ViewControllerComponent implements 
 
     if (window.localStorage.getItem('is_freenas') === 'false') {
       this.ws.call('failover.node').subscribe((node) => {
+        console.log(node);
         if (node === 'A') {
           this.ipPlaceholder = ` (${globalHelptext.thisCtlr})`;
           this.failoverPlaceholder = ` (${globalHelptext.Ctrlr} 2)`;
@@ -383,8 +384,7 @@ export class InterfacesFormComponent extends ViewControllerComponent implements 
           this.ipPlaceholder = ` (${globalHelptext.Ctrlr} 1)`;
           this.failoverPlaceholder = ` (${globalHelptext.thisCtlr})`;
         } else {
-          this.ipPlaceholder = ` ${globalHelptext.unknownCtrl}`;
-          this.failoverPlaceholder = ''
+          return;
         }
         _.find(this.ipListControl.templateListField, {'name': 'address'}).placeholder += this.ipPlaceholder;
         _.find(this.ipListControl.templateListField, {'name': 'failover_address'}).placeholder += this.failoverPlaceholder;
