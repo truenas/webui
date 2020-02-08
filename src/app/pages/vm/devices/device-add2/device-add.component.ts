d379bf1ef8984b1dbc81c386e08b10afc38e6e1f
Magnus Stattin <stattin42@gmail.com>
2020-02-09 00:55:21 +0100

    NAS-100045 PCI-E Passthrough Support for bhyve webui support for vm device PCI passthru

--- src/app/pages/vm/devices/device-add2/device-add.component.ts
+++ src/app/pages/vm/devices/device-add2/device-add.component.ts
@@ -31,6 +31,7 @@ export class DeviceAddComponent implements OnInit {
   public diskFormGroup: any;
   public nicFormGroup: any;
   public rawfileFormGroup: any;
+  public pciFormGroup: any;
   public vncFormGroup: any;
   public rootpwd: any;
   public vminfo: any;
@@ -56,6 +57,9 @@ export class DeviceAddComponent implements OnInit {
         }, {
         label: 'Raw File',
         value: 'RAW',
+        }, {
+	label: 'PCI Passthru Device',
+	value: 'PCI',
         }, {
         label: 'VNC',
         value: 'VNC',
@@ -221,6 +225,28 @@ export class DeviceAddComponent implements OnInit {
     },
   ];
 
+  //pci
+  public pciFieldConfig: FieldConfig[] = [
+    {
+      name: 'pptdev',
+      placeholder: helptext.pptdev_placeholder,
+      tooltip: helptext.pptdev_tooltip,
+      type: 'select',
+      options: [],
+      validation: helptext.pptdev_validation,
+      required: true
+    },
+    {
+      name : 'order',
+      placeholder : helptext.order_placeholder,
+      tooltip : helptext.order_tooltip,
+      type: 'input',
+      value: null,
+      inputType: 'number'
+    },
+  ];
+  protected pptdev: any;
+
   //vnc
   public vncFieldConfig: FieldConfig[]  = [
     {
@@ -318,7 +344,17 @@ export class DeviceAddComponent implements OnInit {
         });
       }
     );
+
+    // pci
+    this.ws.call('vm.device.pptdev_choices').subscribe((res) => {
+      this.pptdev = _.find(this.pciFieldConfig, { 'name': 'pptdev' });
+      this.pptdev.options = Object.keys(res || {}).map(pptdevId => ({
+        label: pptdevId,
+	value: pptdevId
+      }));
+    });
   }
+
   ngOnInit() {
     this.preInit();
 
@@ -333,6 +369,7 @@ export class DeviceAddComponent implements OnInit {
         diskFieldConfig: this.diskFieldConfig,
         nicFieldConfig: this.nicFieldConfig,
         rawfileFieldConfig: this.rawfileFieldConfig,
+        pciFieldConfig: this.pciFieldConfig,
         vncFieldConfig: this.vncFieldConfig,
       },
       {
@@ -347,6 +384,7 @@ export class DeviceAddComponent implements OnInit {
     this.diskFormGroup = this.entityFormService.createFormGroup(this.diskFieldConfig);
     this.nicFormGroup = this.entityFormService.createFormGroup(this.nicFieldConfig);
     this.rawfileFormGroup = this.entityFormService.createFormGroup(this.rawfileFieldConfig);
+    this.pciFormGroup = this.entityFormService.createFormGroup(this.pciFieldConfig);
     this.vncFormGroup = this.entityFormService.createFormGroup(this.vncFieldConfig);
 
     this.activeFormGroup = this.cdromFormGroup;
@@ -363,6 +401,9 @@ export class DeviceAddComponent implements OnInit {
         this.isCustActionVisible = false;
       } else if (res === 'RAW') {
         this.activeFormGroup = this.rawfileFormGroup;
+        this.isCustActionVisible = false;
+      } else if (res === 'PCI') {
+        this.activeFormGroup = this.pciFormGroup;
         this.isCustActionVisible = false;
       } else if (res === 'VNC') {
         this.activeFormGroup = this.vncFormGroup;
