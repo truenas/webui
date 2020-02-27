10c11c8f0bd5ed6344d54d8a0d3511557ba43b74
Erin Clark <erincodepirate@gmail.com>
2020-02-27 11:47:19 -0800

    Use FreeBSD man page link for kerberos settings
    NAS-105209

--- src/app/helptext/directoryservice/kerberossettings.ts
+++ src/app/helptext/directoryservice/kerberossettings.ts
@@ -6,11 +6,11 @@ ks_appdefaults_name: 'appdefaults_aux',
 ks_appdefaults_placeholder: T('Appdefaults Auxiliary Parameters'),
 ks_appdefaults_tooltip: T('Define any additional settings for use by some Kerberos\
  applications. The available settings and syntax is listed in the \
- <a href="https://web.mit.edu/kerberos/krb5-1.12/doc/admin/conf_files/krb5_conf.html#appdefaults" target="_blank">appdefaults section of krb.conf(5)</a>.'),
+ <a href="https://www.freebsd.org/cgi/man.cgi?query=krb5.conf&apropos=0&sektion=5&manpath=FreeBSD+12.0-RELEASE&arch=default&format=html" target="_blank">appdefaults section of krb.conf(5)</a>.'),
 
 ks_libdefaults_name: 'libdefaults_aux',
 ks_libdefaults_placeholder: T('Libdefaults Auxiliary Parameters'),
 ks_libdefaults_tooltip: T('Define any settings used by the Kerberos library. The\
  available settings and their syntax are listed in the\
- <a href="https://web.mit.edu/kerberos/krb5-1.12/doc/admin/conf_files/krb5_conf.html#libdefaults" target="_blank">libdefaults section of krb.conf(5)</a>.')
+ <a href="https://www.freebsd.org/cgi/man.cgi?query=krb5.conf&apropos=0&sektion=5&manpath=FreeBSD+12.0-RELEASE&arch=default&format=html" target="_blank">libdefaults section of krb.conf(5)</a>.')
 }
