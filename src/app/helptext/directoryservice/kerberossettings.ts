c17f6867e3124708c8d9f33c541003ee59a2c4fa
Erin Clark <erincodepirate@gmail.com>
2020-02-28 16:33:48 -0800

    Update tooltip text
    NAS-105209

--- src/app/helptext/directoryservice/kerberossettings.ts
+++ src/app/helptext/directoryservice/kerberossettings.ts
@@ -3,13 +3,15 @@ import { T } from '../../translate-marker';
 export default {
 ks_appdefaults_name: 'ks_appdefaults_aux',
 ks_appdefaults_placeholder: T('Appdefaults Auxiliary Parameters'),
-ks_appdefaults_tooltip: T('Define any additional settings for use by some Kerberos\
- applications. The available settings and syntax is listed in the \
- <a href="https://www.freebsd.org/cgi/man.cgi?query=krb5.conf&apropos=0&sektion=5&manpath=FreeBSD+11.3-RELEASE&arch=default&format=html" target="_blank">appdefaults section of krb.conf(5).</a>.'),
+ks_appdefaults_tooltip: T('Additional Kerberos application settings.\
+See the "appdefaults" section of\
+<a href="https://www.freebsd.org/cgi/man.cgi?query=krb5.conf&apropos=0&sektion=5&manpath=FreeBSD+11.3-RELEASE&arch=default&format=html" target="_blank">[krb.conf(5)]</a>.\
+for available settings and usage syntax.'),
 
 ks_libdefaults_name: 'ks_libdefaults_aux',
 ks_libdefaults_placeholder: T('Libdefaults Auxiliary Parameters'),
-ks_libdefaults_tooltip: T('Define any settings used by the Kerberos library. The\
- available settings and their syntax are listed in the\
- <a href="https://www.freebsd.org/cgi/man.cgi?query=krb5.conf&apropos=0&sektion=5&manpath=FreeBSD+11.3-RELEASE&arch=default&format=html" target="_blank">libdefaults section of krb.conf(5).</a>.')
+ks_libdefaults_tooltip: T('Additional Kerberos library settings.\
+See the "libdefaults" section of\
+<a href="https://www.freebsd.org/cgi/man.cgi?query=krb5.conf&apropos=0&sektion=5&manpath=FreeBSD+11.3-RELEASE&arch=default&format=html" target="_blank">[krb.conf(5)]</a>.\
+for available settings and usage syntax.')
 }
