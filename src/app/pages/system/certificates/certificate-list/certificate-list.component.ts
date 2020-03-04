94627ab5ebb33e90a2f3aac6cb5aa246bcfbf514
Erin Clark <erincodepirate@gmail.com>
2020-03-03 17:52:05 -0800

    Pass in dialog service to fix error handling for certificate delete
    NAS-105125

--- src/app/pages/system/certificates/certificate-list/certificate-list.component.ts
+++ src/app/pages/system/certificates/certificate-list/certificate-list.component.ts
@@ -152,7 +152,7 @@ export class CertificateListComponent {
             (progress) => {
             },
             (err) => {
-              new EntityUtils().handleWSError(this.entityList, err);
+              new EntityUtils().handleWSError(this.entityList, err, this.dialog);
             },
             () => {
               this.entityList.getData();
