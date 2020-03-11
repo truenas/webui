ea1e56803e12be5d0a9eeea4b02453a872665985
Lola Yang <lola.ly27@gmail.com>
2020-03-10 19:00:18 -0700

    NAS-101536 added form-chip component

---
+++ src/app/pages/common/entity/entity-form/components/form-chip/form-chip.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { FormGroup } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+
+import { FieldConfig } from '../../models/field-config.interface';
+import { EntityFormService } from '../../services/entity-form.service';
+import { Field } from '../../models/field.interface';
+import globalHelptext from '../../../../../../helptext/global-helptext';
+
+@Component({
+    selector: 'form-chip',
+    templateUrl: './form-chip.component.html',
+})
+export class FormChipComponent implements Field, OnInit{
+    config: FieldConfig;
+    group: FormGroup;
+    fieldShow: string;
+    chipLists: any[];
+
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+    constructor(
+        public translate: TranslateService,
+        private formService: EntityFormService) {
+    }
+
+    ngOnInit() {
+        console.log(this.config, this.group);
+        this.chipLists = this.group.controls[this.config.name].value || [];
+    }
+
+    add(event: MatChipInputEvent): void {
+        const input = event.input;
+        const value = event.value;
+
+        if ((value || '').trim()) {
+            this.chipLists.push(value.trim());
+        }
+
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    remove(item): void {
+        const index = this.chipLists.indexOf(item);
+
+        if (index >= 0) {
+            this.chipLists.splice(index, 1);
+        }
+    }
+}
