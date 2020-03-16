b18f5746b79f875b1b99b0883916e0c895ad4136
Lola Yang <lola.ly27@gmail.com>
2020-03-16 11:19:03 -0700

    NAS-102177 change tooltip as request

--- src/app/helptext/storage/disks/disks.ts
+++ src/app/helptext/storage/disks/disks.ts
@@ -2,21 +2,18 @@ import { T } from '../../../translate-marker';
 
 export default {
 disk_form_name_placeholder: T('Name'),
-disk_form_name_tooltip : T('This is the FreeBSD device name for the disk.'),
+disk_form_name_tooltip : T('FreeBSD disk device name.'),
 
 disk_form_serial_placeholder: T('Serial'),
-disk_form_serial_tooltip : T('This is the serial number of the disk.'),
+disk_form_serial_tooltip : T('Serial number for this disk.'),
 
 disk_form_description_placeholder: T('Description'),
-disk_form_description_tooltip : T('Enter any notes about this disk.'),
+disk_form_description_tooltip : T('Notes about this disk.'),
 
 disk_form_hddstandby_placeholder: T('HDD Standby'),
-disk_form_hddstandby_tooltip : T('Indicates the time of inactivity in minutes before\
- the drive enters standby mode. This <a\
- href="https://forums.freenas.org/index.php?threads/how-to-find-out-if-a-drive-is-spinning-down-properly.2068/"\
- target="_blank">forum post</a> demonstrates how to\
- determine if a drive has spun down. Temperature monitoring is \
- disabled if the disk is set to enter standby.'),
+disk_form_hddstandby_tooltip : T('Minutes of inactivity before the drive enters standby mode.\
+ This <a href="https://forums.freenas.org/index.php?threads/how-to-find-out-if-a-drive-is-spinning-down-properly.2068/"\
+ target="_blank">forum post</a> describes identifying spun down drives. Temperature monitoring is disabled for standby disks.'),
 
 disk_form_hddstandby_options: [
  {label:T('Always On'), value:'ALWAYS ON'},
@@ -56,10 +53,8 @@ disk_form_acousticlevel_options: [
 ],
 
 disk_form_togglesmart_placeholder : T('Enable S.M.A.R.T.'),
-disk_form_togglesmart_tooltip : T('Set by default if the disk supports S.M.A.R.T.\
- Unset to disable any configured <a\
- href="--docurl--/tasks.html#s-m-a-r-t-tests"\
- target="_blank">S.M.A.R.T. tests</a>.'),
+disk_form_togglesmart_tooltip : T('Enabling allows the system to conduct periodic\
+ <a href="--docurl--/tasks.html#s-m-a-r-t-tests" target="_blank">S.M.A.R.T. tests</a>.'),
 
 disk_form_smartoptions_placeholder: T('S.M.A.R.T. extra options'),
 disk_form_smartoptions_tooltip : T('Additional <a\
