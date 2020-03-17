81529b819df9977bbf7cfd84464bbbb27261caa1
Mrt134 <timmoore88@gmail.com>
2020-03-17 13:14:34 -0400

    NAS-105366: Update help text for Allow Taking Empty Snapshots

--- src/app/helptext/task-calendar/snapshot/snapshot-form.ts
+++ src/app/helptext/task-calendar/snapshot/snapshot-form.ts
@@ -50,9 +50,14 @@ export default {
  or choose <i>Custom</i> to use the advanced scheduler.'),
 
     allow_empty_placeholder: T('Allow Taking Empty Snapshots'),
-    allow_empty_tooltip: T('Creates dataset snapshots when there are no \
- changes. Set to support periodic snapshot schedules and replications \
- created in version 11.2 and earlier.'),
+    allow_empty_tooltip: T('Creates dataset snapshots even when there \
+ have been no changes to the dataset from the last snapshot. Recommended \
+ for creating long-term restore points, multiple snapshot tasks pointed \
+ at the same datasets, or to be compatible with snapshot schedules or \
+ replications created in %brand% 11.2 and earlier.<br><br> For example, \
+ allowing empty snapshots for a monthly snapshot schedule allows that \
+ monthly snapshot to be taken, even when a daily snapshot task has \
+ already taken a snapshot of any changes to the dataset.'),
 
     enabled_placeholder: T('Enabled'),
     enabled_tooltip: T('To activate this periodic snapshot schedule, set \
