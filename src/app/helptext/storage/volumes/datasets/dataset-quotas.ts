0cde39b5f7ab294cb56938683df28cefabee6b88
Dennis Mullen <dkmullen@gmail.com>
2020-03-18 11:16:03 -0400

    Update src/app/helptext/storage/volumes/datasets/dataset-quotas.ts
    Co-Authored-By: Tim Moore <Mrt134@users.noreply.github.com>

--- src/app/helptext/storage/volumes/datasets/dataset-quotas.ts
+++ src/app/helptext/storage/volumes/datasets/dataset-quotas.ts
@@ -38,8 +38,8 @@ will apply to all groups in this list.')
         },
         data_quota: {
           placeholder: T('Group Data Quota ') + globalHelptext.human_readable.suggestion_label,
-          tooltip: T('Enter the amount of disk space that may be consumed by the selected groups. \
-Enter 0 (zero) to remove the quota.') + globalHelptext.human_readable.suggestion_tooltip
+          tooltip: T('Amount of disk space that can be consumed by the selected groups. \
+Entering <code>0</code> (zero) removes the quota.') + globalHelptext.human_readable.suggestion_tooltip
         },
         obj_quota: {
           placeholder: T('Group Object Quota'),
@@ -56,4 +56,3 @@ Entering <code>0</code> (zero) removes the quota.'),
         cancel: T('Cancel')
     }
   }
-
