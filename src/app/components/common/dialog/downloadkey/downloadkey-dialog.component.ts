e23ed598948ce9602bafb3a0d0137bf2bfecc213
Erin Clark <erin@ixsystems.com>
2020-03-20 16:46:30 -0700

    Attempt to make downloading key after pool creation work properly
    NAS-104613

--- src/app/components/common/dialog/downloadkey/downloadkey-dialog.component.ts
+++ src/app/components/common/dialog/downloadkey/downloadkey-dialog.component.ts
@@ -1,4 +1,4 @@
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { Component } from '@angular/core';
 import {
   WebSocketService,
@@ -7,6 +7,9 @@ import {
 import { AppLoaderService } from '../../../../services/app-loader/app-loader.service';
 import { TranslateService } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
+import { EntityJobComponent } from '../../../../../app/pages/common/entity/entity-job';
+import { EntityUtils } from '../../../../../app/pages/common/entity/utils';
+import { T } from '../../../../translate-marker';
 
 @Component({
   selector: 'downloadkey-dialog',
@@ -15,7 +18,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DownloadKeyModalDialog {
 
+  public new = false;
   public volumeId: any;
+  public volumeName: any;
   public fileName: any;
   public isDownloaded: Boolean = false;
 
@@ -25,27 +30,50 @@ export class DownloadKeyModalDialog {
     private ws: WebSocketService,
     private storage: StorageService,
     private http: HttpClient,
+    public dialog: MatDialog,
     private loader:AppLoaderService) { }
 
   downloadKey() {
-    this.loader.open();
     const payload = [this.volumeId];
     if (this.fileName !== undefined) {
       payload.push(this.fileName);
     }
     const mimetype = 'application/octet-stream';
-    this.ws.call("pool.download_encryption_key", payload).subscribe((res) => {
-      this.loader.close();
-      this.storage.streamDownloadFile(this.http, res, this.fileName, mimetype).subscribe(file => {
-        if(res !== null && res !== "") {
-          this.storage.downloadBlob(file, this.fileName);
-          this.isDownloaded = true;
-        }
+    if (this.new) { // new is ZoL encryption
+      const dialogRef = this.dialog.open(EntityJobComponent, {
+        data: { title: T('Export Keys'), disableClose: true }
       });
-    }, (resError)=>{
-      this.isDownloaded = true;
-      this.loader.close();
+      dialogRef.componentInstance.setCall('pool.dataset.export_keys', [this.volumeId]);
+      dialogRef.componentInstance.success.subscribe(res => {
+        console.log(res);
+        this.storage.streamDownloadFile(this.http, res, this.fileName, mimetype).subscribe(file => {
+          if(res !== null && res !== "") {
+            this.storage.downloadBlob(file, this.fileName);
+            this.isDownloaded = true;
+          }
+        });
+      });
+      dialogRef.componentInstance.failure.subscribe(e => { 
+        new EntityUtils().handleWSError(this, e, this.dialog),
+        dialogRef.close(false);
+      });
+      dialogRef.componentInstance.submit();
 
-    });
+    } else {
+      this.loader.open();
+      this.ws.call("pool.download_encryption_key", payload).subscribe((res) => {
+        this.loader.close();
+        this.storage.streamDownloadFile(this.http, res, this.fileName, mimetype).subscribe(file => {
+          if(res !== null && res !== "") {
+            this.storage.downloadBlob(file, this.fileName);
+            this.isDownloaded = true;
+          }
+        });
+      }, (resError)=>{
+        this.isDownloaded = true;
+        this.loader.close();
+
+      });
+    }
   }
 }
