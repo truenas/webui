dcf4286f988fa0dc5933274ab546db909cfedf12
Dennis <dkmullen@gmail.com>
2020-04-07 16:04:31 -0400

    NAS-105622 Subscribe to vm events

--- src/app/pages/vm/vm-list/vm-list.component.ts
+++ src/app/pages/vm/vm-list/vm-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { WebSocketService, StorageService, AppLoaderService, DialogService } from '../../../services/';
@@ -12,6 +12,7 @@ import wizardHelptext from '../../../helptext/vm/vm-wizard/vm-wizard';
 import { EntityUtils } from '../../common/entity/utils';
 import { DialogFormConfiguration } from 'app/pages/common/entity/entity-dialog/dialog-form-configuration.interface';
 import * as _ from 'lodash';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'vm-list',
@@ -22,7 +23,7 @@ import * as _ from 'lodash';
     <entity-table [title]='title' [conf]='this'></entity-table>`,
     styleUrls: ['./vm-list.component.css']
 })
-export class VMListComponent {
+export class VMListComponent implements OnDestroy {
 
     public title = "Virtual Machines";
     protected queryCall = 'vm.query';
@@ -30,6 +31,7 @@ export class VMListComponent {
     protected route_add: string[] = ['vm', 'wizard'];
     protected route_edit: string[] = ['vm', 'edit'];
     protected dialogRef: any;
+    private eventSubscription: Subscription;
 
     public entityList: any;
     public columns: Array<any> = [
@@ -80,6 +82,10 @@ export class VMListComponent {
     afterInit(entityList) {
         this.checkMemory();
         this.entityList = entityList;
+
+        this.eventSubscription = this.ws.subscribe('vm.query').subscribe(res => {
+            console.log('Event', res)
+        })
     }
 
     resourceTransformIncomingRestData(vms) {
@@ -437,4 +443,8 @@ export class VMListComponent {
             this.availMem = this.storageService.convertBytestoHumanReadable(res);
         });
     }
-}
+
+    ngOnDestroy() {
+        this.eventSubscription.unsubscribe();
+    }
+ }
