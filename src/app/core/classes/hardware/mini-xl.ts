895667f3687e67292bdd0fd0899f6781d06b4e01
Damian Szidiropulosz <damianos@knoxbug.org>
2020-04-10 16:22:43 -0400

    WIP: chassis and 3.4 inch drives placements

--- src/app/core/classes/hardware/mini-xl.ts
+++ src/app/core/classes/hardware/mini-xl.ts
@@ -12,17 +12,13 @@ export class MINIXL extends Chassis {
 
     this.front = new ChassisView();
     this.front.container = new PIXI.Container();
-    this.front.chassisPath = "assets/images/hardware/mini/mini_673w.png";
-    this.front.driveTrayBackgroundPath = "assets/images/hardware/mini/mini_hdd_drivetray_bg.png" 
-    this.front.driveTrayHandlePath = "assets/images/hardware/mini/mini_hdd_drivetray_handle.png" 
-    this.front.driveTraysOffsetX = -200;
-    this.front.driveTraysOffsetY = -138;
+    this.front.chassisPath = "assets/images/hardware/mini_xl/mini_xl_768w.png";
+    this.front.driveTrayBackgroundPath = "assets/images/hardware/mini_xl/mini_hdd_drivetray_bg.png" 
+    this.front.driveTrayHandlePath = "assets/images/hardware/mini_xl/mini_hdd_drivetray_handle.png" 
     this.front.vertical = false;
     this.front.disabledOpacity = 0.5;
     this.front.chassisOpacity = 0.65;
     this.front.totalDriveTrays = 9;
-    //this.front.columns = 1;
-    //this.front.rows = 8;
     this.front.autoPosition = false;
 
     this.front.events.subscribe((evt: CoreEvent) => {
@@ -33,13 +29,27 @@ export class MINIXL extends Chassis {
   }
 
   onLoaded(){
+    // Scale the Chassis
+    let chassis = this.front.container.getChildAt(0);
+   
+    // We must hardcode the coordinates because 
+    // the parent container's dimensions are not constant
+    chassis.setTransform( -20, 170, 1.3, 1.3); // x, y, scaleX, scaleY 
 
-    console.warn(this.front.driveTrayObjects);
-    this.front.driveTrayObjects.forEach((dt, index) => {     
-      const gap = index < 4 ? 0 : 48;
+    // Place the drives
+    this.front.driveTrayObjects.forEach((dt, i) => {     
+      // HDD
+      if(i > 0){
+        const index:number = i - 1;
+        const gap: number = index < 4 ? 0 : 48;
+        const offsetY: number = 193;
 
-      dt.container.x = dt.container.width + this.front.driveTraysOffsetX;
-      dt.container.y = dt.container.y + (index * dt.container.height) + gap;
+        // x, y, scaleX, scaleY
+        dt.container.setTransform(68, offsetY + dt.container.y + (index * dt.container.height) + gap);
+      } else if(i == 0) {
+        // SSD
+        //dt.container.setTransform(95,193);       
+      }
     });
   }
 
