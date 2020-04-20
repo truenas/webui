5527c356dbede88df31a467035bc9e9e3ba0e9c0
Damian Szidiropulosz <damianos@knoxbug.org>
2020-04-20 13:35:17 -0400

    Code cleanup and setup default theme object as an exported constant

--- src/app/components/common/layouts/auth-layout/auth-layout.component.ts
+++ src/app/components/common/layouts/auth-layout/auth-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageService } from '../../../../services/language.service';
-import { Theme } from 'app/services/theme/theme.service';
+import { Theme, DefaultTheme } from 'app/services/theme/theme.service';
 
 @Component({
   selector: 'app-auth-layout',
@@ -9,64 +9,7 @@ import { Theme } from 'app/services/theme/theme.service';
 
 })
 export class AuthLayoutComponent implements OnInit {
-
-  /*private theme:Theme = {
-    name:'ix-blue',
-    label: "iX Blue",
-    description:'iX System Colors',
-    hasDarkLogo:false,
-    favorite:false,
-    accentColors:['blue', 'orange','green', 'violet','cyan', 'magenta', 'yellow','red'],
-    primary:"var(--blue)",
-    accent:"var(--yellow)",
-    bg1:'#dddddd',
-    bg2:'#ffffff',
-    fg1:'#222222',
-    fg2:'#333333',
-    'alt-bg1':'#f8f8f2',
-    'alt-bg2':'#fafaf5',
-    'alt-fg1':'#181a26',
-    'alt-fg2':'#282a36',
-    yellow:'#f0cb00',
-    orange:'#eec302',
-    red:'#ff0013',
-    magenta:'#d238ff',
-    violet:'#c17ecc',
-    blue:'#00a2ff',
-    cyan:'#00d0d6',
-    green:'#59d600'
-  }*/i
-
-  private theme:Theme = {
-      name:'ix-dark',
-      label: "iX Dark",
-      labelSwatch:"blue",
-      description:'iX System Colors on Dark',
-      hasDarkLogo:false,
-      logoPath:'assets/images/light-logo.svg',
-      logoTextPath:'light-logo-text.svg',
-      favorite:false,
-      accentColors:['green', 'violet', 'orange', 'cyan', 'magenta', 'red', 'yellow', 'blue'],
-      primary:"var(--blue)",
-      //secondary:"var(--bg1)",
-      accent:"var(--yellow)",
-      bg1:'#171E26',
-      bg2:'#232d35',//'#1D262D',
-      fg1:'#aaaaaa',
-      fg2:'#cccccc',
-      'alt-bg1':'rgba(122,122,122,0.25)',
-      'alt-bg2':'#6F6E6C',
-      'alt-fg1':'#c1c1c1',
-      'alt-fg2':'#e1e1e1',
-      yellow:'#f0cb00',
-      orange:'#ee9302',
-      red:'#ff0013',
-      magenta:'#d238ff',
-      violet:'#c17ecc',
-      blue:'#0D5788',
-      cyan:'#00d0d6',
-      green:'#1F9642'
-    }
+  private theme:Theme = DefaultTheme;
 
 
   constructor(public language: LanguageService) {
