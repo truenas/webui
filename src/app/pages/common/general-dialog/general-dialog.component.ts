2333d21b08a1042130afb0fdc6f0ba6413e5ac56
Lola Yang <lola.ly27@gmail.com>
2020-04-23 14:21:38 -0700

    NAS-105805 added/declared general dialog component

--- src/app/pages/common/general-dialog/general-dialog.component.ts
+++ src/app/pages/common/general-dialog/general-dialog.component.ts
@@ -1,33 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog'
 import { TranslateService } from '@ngx-translate/core';
 
 export interface GeneralDialogConfig {
-    title: string,
+    title?: string,
     message: string,
     icon?: string,
     is_html?: boolean,
+    confirmCheckbox?: boolean,
+    confirmCheckboxMsg?: string,
+    hideCancel?: boolean,
+    cancelBtnMsg?: string,
+    confirmBtnMsg?: string,
 }
 
 @Component({
     selector: 'app-general-dialog',
     templateUrl: './general-dialog.component.html',
-    styleUrls: ['./general-dialog.component.scss']
 })
-export class GeneralDialogComponent implements OnInit {
+export class GeneralDialogComponent {
     @Input() conf: GeneralDialogConfig;
 
-    public title: string;
-    public message: string;
-    public icon: string;
-    public is_html: boolean;
-    constructor(public dialogRef: MatDialogRef<GeneralDialogComponent>, protected translate: TranslateService) {
-    }
+    constructor(
+        public dialogRef: MatDialogRef<GeneralDialogComponent>,
+        protected translate: TranslateService) { }
 
-    ngOnInit() {
-        this.title = this.conf.title;
-        this.message = this.conf.message;
-        this.icon = this.conf.icon ? this.conf.icon : 'report_problem';
-        this.is_html = this.conf.is_html !== undefined ? this.conf.is_html : false;
+    isDisabled() {
+        return false;
     }
 }
