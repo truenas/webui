f0db7cdd36b91d24030d1b39c2098fdaff118ec0
Erin Clark <erincodepirate@gmail.com>
2020-05-06 16:16:39 -0700

    Only show "disable failover" if the form is actually disabling failover
    NAS-104582
    
    (cherry picked from commit 5d314a675f5565df24f290d2b02a2df293e52ac2)

--- src/app/pages/system/failover/failover.component.ts
+++ src/app/pages/system/failover/failover.component.ts
@@ -23,6 +23,7 @@ export class FailoverComponent implements OnDestroy {
   protected updateCall = 'failover.update';
   public entityForm: any;
   protected failoverDisableSubscription: any;
+  public alreadyDisabled = false;
   public confirmSubmit = false;
   public confirmSubmitDialog = {
     title: T("Disable Failover"),
@@ -133,7 +134,9 @@ export class FailoverComponent implements OnDestroy {
     this.entityForm = entityEdit;
     this.failoverDisableSubscription = 
       this.entityForm.formGroup.controls['disabled'].valueChanges.subscribe(res => {
-        this.confirmSubmit = res;
+        if (!this.alreadyDisabled) {
+          this.confirmSubmit = res;
+        }
       });
     this.master_fg = this.entityForm.formGroup.controls['master']
     this.masterSubscription = 
@@ -153,6 +156,7 @@ export class FailoverComponent implements OnDestroy {
   public customSubmit(body) {
     this.load.open();
     return this.ws.call('failover.update', [body]).subscribe((res) => {
+      this.alreadyDisabled = body['disabled'];
       this.load.close();
       this.dialog.Info(T("Settings saved."), '', '300px', 'info', true).subscribe(saved => {
         if (body.disabled && !body.master) {
@@ -166,6 +170,7 @@ export class FailoverComponent implements OnDestroy {
   }
 
   resourceTransformIncomingRestData(value) {
+    this.alreadyDisabled = value['disabled'];
     value['master'] = true;
     return value;
   }
