0fbbab08118063a67d8b4c38077f8d2f831b9e58
Dennis Mullen <dkmullen@gmail.com>
2020-05-19 12:08:29 -0400

    NAS-105824 Workaround for scrolling on win for 11.3

--- src/app/components/common/layouts/admin-layout/admin-layout.component.ts
+++ src/app/components/common/layouts/admin-layout/admin-layout.component.ts
@@ -113,6 +113,16 @@ export class AdminLayoutComponent implements OnInit, AfterViewChecked {
       });
     }, 500);
 
+    // Allows for one-page-at-a-time scrolling in sidenav on Windows
+    if (window.navigator.platform.toLowerCase() === 'win32') {
+      navigationHold.addEventListener('wheel', (e) => {
+        // deltaY is 1 for page scrolling and 33.3 per line for regular scrolling; default is 100, or 3 lines at a time
+        if (e.deltaY === 1 || e.deltaY === -1) {
+          navigationHold.scrollBy(0, e.deltaY * window.innerHeight);
+        }
+      })
+    }
+
     if (this.media.isActive('xs') || this.media.isActive('sm')) {
       this.isSidenavOpen = false;
     }
