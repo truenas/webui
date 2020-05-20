48fb39cfc888e648f5f36ab7a015ad945d15b328
Lola Yang <lola.ly27@gmail.com>
2020-05-20 10:50:13 -0700

    NAS-106156 enable to set expandAvailablePlugins to user preference

--- src/app/core/services/preferences.service.ts
+++ src/app/core/services/preferences.service.ts
@@ -34,6 +34,7 @@ export interface UserPreferences {
   showWelcomeDialog: boolean;
   showUserListMessage: boolean;
   showGroupListMessage: boolean;
+  expandAvailablePlguins: boolean;
 }
 
 @Injectable()
@@ -62,7 +63,8 @@ export class PreferencesService {
     "nicAttach": null,
     "showWelcomeDialog": true,
     "showUserListMessage": true,
-    "showGroupListMessage": true
+    "showGroupListMessage": true,
+    "expandAvailablePlguins": false,
   }
 
   public preferences: UserPreferences = this.defaultPreferences;
