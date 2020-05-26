510c985d9dd0df3854b89a9b015b34fac306ce9d
Lola Yang <lola.ly27@gmail.com>
2020-05-26 13:54:31 -0700

    NAS-106249 added helptext for alert settings

---
+++ src/app/helptext/system/alert-settings.ts
@@ -0,0 +1,276 @@
+import { T } from '../../translate-marker';
+
+export default {
+    CertificateExpired: {
+        level: T(''),
+        policy: T('')
+    },
+    CertificateIsExpiring: {
+        level: T(''),
+        policy: T('')
+    },
+    CertificateIsExpiringSoon: {
+        level: T(''),
+        policy: T('')
+    },
+    CertificateParsingFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    CertificateRevoked: {
+        level: T(''),
+        policy: T('')
+    },
+    WebUiCertificateSetupFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    ActiveDirectoryDomainBind: {
+        level: T(''),
+        policy: T('')
+    },
+    ActiveDirectoryDomainHealth: {
+        level: T(''),
+        policy: T('')
+    },
+    ActiveDirectoryDomainOffline: {
+        level: T(''),
+        policy: T('')
+    },
+    LDAPBind: {
+        level: T(''),
+        policy: T('')
+    },
+    NISBind: {
+        level: T(''),
+        policy: T('')
+    },
+    FreeNASBMC: {
+        level: T(''),
+        policy: T('')
+    },
+    IPMISELSpaceLeft: {
+        level: T(''),
+        policy: T('')
+    },
+    IPMISEL: {
+        level: T(''),
+        policy: T('')
+    },
+    MultipathIsNotOptimal: {
+        level: T(''),
+        policy: T('')
+    },
+    SMART: {
+        level: T(''),
+        policy: T('')
+    },
+    Smartd: {
+        level: T(''),
+        policy: T('')
+    },
+    KMIPConnectionFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    KMIPSEDGlobalPasswordSyncFailure: {
+        level: T(''),
+        policy: T('')
+    },
+    KMIPSEDDisksSyncFailure: {
+        level: T(''),
+        policy: T('')
+    },
+    KMIPZFSDatasetsSyncFailure: {
+        level: T(''),
+        policy: T('')
+    },
+    PluginUpdate: {
+        level: T(''),
+        policy: T('')
+    },
+    LAGGInactivePorts: {
+        level: T(''),
+        policy: T('')
+    },
+    LAGGNoActivePorts: {
+        level: T(''),
+        policy: T('')
+    },
+    CollectdCritical: {
+        level: T(''),
+        policy: T('')
+    },
+    CollectdWarning: {
+        level: T(''),
+        policy: T('')
+    },
+    ReportingDb: {
+        level: T(''),
+        policy: T('')
+    },
+    SyslogNg: {
+        level: T(''),
+        policy: T('')
+    },
+    ISCSIPortalIP: {
+        level: T(''),
+        policy: T('')
+    },
+    NFSBind: {
+        level: T(''),
+        policy: T('')
+    },
+    QuotaCritical: {
+        level: T(''),
+        policy: T('')
+    },
+    VolumeRekey: {
+        level: T(''),
+        policy: T('')
+    },
+    VolumeVersion: {
+        level: T(''),
+        policy: T('')
+    },
+    ZpoolCapacityWarning: {
+        level: T(''),
+        policy: T('')
+    },
+    ZpoolCapacityCritical: {
+        level: T(''),
+        policy: T('')
+    },
+    VolumeStatus: {
+        level: T(''),
+        policy: T('')
+    },
+    QuotaWarning: {
+        level: T(''),
+        policy: T('')
+    },
+    ScrubPaused: {
+        level: T(''),
+        policy: T('')
+    },
+    ZfsVersionOutOfDate: {
+        level: T(''),
+        policy: T('')
+    },
+    BootPoolStatus: {
+        level: T(''),
+        policy: T('')
+    },
+    ZfsDeadman: {
+        level: T(''),
+        policy: T('')
+    },
+    TruecommandConnectionPending: {
+        level: T(''),
+        policy: T('')
+    },
+    SSHLoginFailures: {
+        level: T(''),
+        policy: T('')
+    },
+    WebUiBindAddressV2: {
+        level: T(''),
+        policy: T('')
+    },
+    TruecommandConnectionDisabled: {
+        level: T(''),
+        policy: T('')
+    },
+    TruecommandConnectionHealth: {
+        level: T(''),
+        policy: T('')
+    },
+    UnscheduledReboot: {
+        level: T(''),
+        policy: T('')
+    },
+    HasUpdate: {
+        level: T(''),
+        policy: T('')
+    },
+    UpdateFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    UpdateNotApplied: {
+        level: T(''),
+        policy: T('')
+    },
+    CloudSyncTaskFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    VMWareSnapshotCreateFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    ReplicationFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    ReplicationSuccess: {
+        level: T(''),
+        policy: T('')
+    },
+    RsyncFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    RsyncSuccess: {
+        level: T(''),
+        policy: T('')
+    },
+    ScrubNotStarted: {
+        level: T(''),
+        policy: T('')
+    },
+    ScrubFinished: {
+        level: T(''),
+        policy: T('')
+    },
+    ScrubStarted: {
+        level: T(''),
+        policy: T('')
+    },
+    SnapshotFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    VMWareLoginFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    VMWareSnapshotDeleteFailed: {
+        level: T(''),
+        policy: T('')
+    },
+    UPSBatteryLow: {
+        level: T(''),
+        policy: T('')
+    },
+    UPSReplbatt: {
+        level: T(''),
+        policy: T('')
+    },
+    UPSCommok: {
+        level: T(''),
+        policy: T('')
+    },
+    UPSCommbad: {
+        level: T(''),
+        policy: T('')
+    },
+    UPSOnBattery: {
+        level: T(''),
+        policy: T('')
+    },
+    UPSOnline: {
+        level: T(''),
+        policy: T('')
+    }
+}
