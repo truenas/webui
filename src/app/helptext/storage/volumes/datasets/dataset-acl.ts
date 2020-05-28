5a4ff9f895f4b433cfebcbc74e6753c62106bcbd
Dennis Mullen <dkmullen@gmail.com>
2020-05-28 13:21:22 -0400

    Update src/app/helptext/storage/volumes/datasets/dataset-acl.ts
    Co-authored-by: Tim Moore <Mrt134@users.noreply.github.com>

--- src/app/helptext/storage/volumes/datasets/dataset-acl.ts
+++ src/app/helptext/storage/volumes/datasets/dataset-acl.ts
@@ -69,8 +69,8 @@ dataset_acl_gid_tooltip: T('The group which controls the dataset. This\
 
  apply_group: {
   placeholder: T('Apply Group'),
-  tooltip: T('Set to confirm changes to <i>Group</i>. To prevent errors, changes to <i>Group</i> \
- will be submitted only if this box is selected.')
+  tooltip: T('Confirm changes to <i>Group</i>. To prevent errors, changes to the <i>Group</i> \
+ are only submitted when this box is set.')
 },
 
 dataset_acl_perms_placeholder: T('Permissions'),
