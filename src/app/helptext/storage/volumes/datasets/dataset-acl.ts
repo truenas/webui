3601369d7ec398c3bc23cf7f3170f1c9ae25e340
Dennis <dkmullen@gmail.com>
2020-05-28 16:55:23 -0400

    NAS-106268 Text fix

--- src/app/helptext/storage/volumes/datasets/dataset-acl.ts
+++ src/app/helptext/storage/volumes/datasets/dataset-acl.ts
@@ -58,7 +58,7 @@ dataset_acl_uid_tooltip: T('User who controls the dataset. This user\
  apply_user: {
    placeholder: T('Apply User'),
    tooltip: T('Confirm changes to <i>User</i>. To prevent errors, changes to the <i>User</i> \
- are only submitted when this box is set.')
+ are submitted only when this box is set.')
  },
 
 dataset_acl_gid_placeholder: T('Group'),
@@ -70,7 +70,7 @@ dataset_acl_gid_tooltip: T('The group which controls the dataset. This\
  apply_group: {
   placeholder: T('Apply Group'),
  tooltip: T('Confirm changes to <i>Group</i>. To prevent errors, changes to the <i>Group</i> \
- are only submitted when this box is set.')
+ are submitted only when this box is set.')
 },
 
 dataset_acl_perms_placeholder: T('Permissions'),
