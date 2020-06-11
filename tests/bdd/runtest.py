980675fe459ed8b63fecf7b857fe282722ad19dd
ericbsd <ericturgeon.bsd@gmail.com>
2020-06-11 18:35:10 -0300

    added test/bdd folder for jira automation
    moved old ui test files in old-ui-test for future reference if needed
    added conftest.py with code to cofigure and  autostart firefox with selenium
    In runtest.py I:
    added --convert-feature option to convert jira feature files for pytest-bdd
    added --ip and --root-password to source it in confing.cfg
    added  code to setup confing.cfg for conftest.py
    added code to run pytest for bdd testing and to store the results for jenkins and Jira
    added code to convert jira feature files before starting the test
    added ha-test folder with features folder inside
    
    (cherry picked from commit 822434f0aaa860060e59c93af0e61f3fd764fa29)

---
+++ tests/bdd/runtest.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import getopt
+from subprocess import call
+from platform import system
+major_v = sys.version_info.major
+minor_v = sys.version_info.minor
+version = f"{major_v}" if system() == "Linux" else f"{major_v}.{minor_v}"
+cwd = str(os.getcwd())
+screenshot_path = f"{cwd}/screenshot"
+argument = sys.argv
+UsageMSG = """
+
+Usage for %s:
+Options:
+
+--ip <0.0.0.0>                             - IP of the targeted TrueNAS server
+--root-password <password>                 - need root password for login
+--convert-feature                          - This convert Jira feature files
+                                             for pytest-bdd
+""" % argument[0]
+
+# if have no argument stop
+if len(argument) == 1:
+    print(UsageMSG)
+    exit()
+
+# list of argument that should be use.
+option_list = ["ip=", 'root-password=', 'convert-feature']
+
+
+def convert_jira_feature_file(directory):
+    # convert Jira feature file for pytest-bdd cucumber results.
+    feature_files = os.listdir(f'{directory}/features')
+    if '.keepme' in feature_files:
+        feature_files.remove('.keepme')
+
+    for file in feature_files:
+        feature = open(f'{directory}/features/{file}', 'r')
+        old_feature = feature.readlines()
+        new_feature = open(f'{directory}/features/{file}', 'w')
+        for line in old_feature:
+            if 'Feature:' in line:
+                feature_list = line.split(':')
+                if len(feature_list) == 3:
+                    new_line = f'{feature_list[0]}:{feature_list[1]}\n'
+                    new_feature.writelines(new_line)
+                else:
+                    new_feature.writelines(line)
+            elif 'Scenario' in line:
+                scenario_list = line.split(':')
+                if len(scenario_list) == 3:
+                    new_line = f'{scenario_list[0]}:{scenario_list[2]}'
+                    new_feature.writelines(new_line)
+                else:
+                    new_feature.writelines(line)
+            else:
+                new_feature.writelines(line)
+        new_feature.close()
+
+
+# look if all the argument are there.
+try:
+    myopts, args = getopt.getopt(argument[1:], None, option_list)
+except getopt.GetoptError as e:
+    print(str(e))
+    print(UsageMSG)
+    sys.exit(1)
+
+global ip, password
+
+for output, arg in myopts:
+    if output == '--ip':
+        ip = arg
+    if output == '--root-password':
+        password = arg
+    if output == "--convert-feature":
+        run_convert = True
+    else:
+        run_convert = False
+
+
+def run_testing():
+    if 'ip' not in globals():
+        print("Option '--ip' is missing")
+        print(UsageMSG)
+        sys.exit(1)
+    if 'password' not in globals():
+        print("Option '--root-password' is missing")
+        print(UsageMSG)
+        sys.exit(1)
+
+    cfg = "[NAS_CONFIG]\n"
+    cfg += f"ip = {ip}\n"
+    cfg += f"password = {password}\n"
+    cfg_file = open("config.cfg", 'w')
+    cfg_file.write(cfg)
+    cfg_file.close()
+
+    convert_jira_feature_file('ha-test')
+    pytestcmd = [
+        f"pytest-{version}",
+        "-vs",
+        "ha-test",
+        "--junitxml=results/junit/webui_test.xml",
+        "--cucumber-json=results/cucumber/webui_test.json"
+    ]
+
+    call(pytestcmd)
+
+
+if run_convert is True:
+    convert_jira_feature_file('ha-test')
+else:
+    run_testing()
