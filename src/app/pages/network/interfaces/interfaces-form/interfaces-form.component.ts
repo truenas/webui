16a7b469aa33b7ca981894f9c64e5a818b2034dd
Lola Yang <lola.ly27@gmail.com>
2020-06-30 09:42:09 -0700

    NAS-106625 make options use regular input instead of chip

--- src/app/pages/network/interfaces/interfaces-form/interfaces-form.component.ts
+++ src/app/pages/network/interfaces/interfaces-form/interfaces-form.component.ts
@@ -225,7 +225,7 @@ export class InterfacesFormComponent extends ViewControllerComponent implements 
         value: 1500
       },
       {
-        type : 'chip',
+        type : 'input',
         name : 'options',
         placeholder : helptext.int_options_placeholder,
         tooltip : helptext.int_options_tooltip,
