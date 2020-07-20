ca4d2f59e31dcb2d06ca369f5da86229b2ec06f9
ericbsd <ericturgeon.bsd@gmail.com>
2020-07-20 16:52:27 -0300

    adding NAS-T912 test case test definition with code
    (cherry picked from commit 407718a86855860f919d463fcc5b5c899232ffc1)
    (cherry picked from commit e69937ae67b8269de7cbeb3aa8edf96b1098a28b)

---
+++ tests/bdd/ha-test-bhyve02/test_NAS_T0912_edit_user_auxiliary_group.py
@@ -0,0 +1,141 @@
+# coding=utf-8
+"""High Availability feature tests."""
+
+from function import wait_on_element, is_element_present, wait_on_element_disappear
+from selenium.webdriver.common.keys import Keys
+import time
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
+    parsers
+)
+
+
+@scenario('features/NAS-T912.feature', 'Edit user auxiliary group')
+def test_edit_user_auxiliary_group(driver):
+    """Edit user auxiliary group."""
+
+
+@given(parsers.parse('The browser is open navigate to "{nas_url}"'))
+def the_browser_is_open_navigate_to_tnbhyve02tnixsystemsnet(driver, nas_url):
+    """The browser is open navigate to "{nas_user}"."""
+    if nas_url not in driver.current_url:
+        driver.get(f"http://{nas_url}/ui/sessions/signin")
+        time.sleep(3)
+
+
+@when(parsers.parse('If login page appear enter "{user}" and "{password}"'))
+def if_login_page_appear_enter_root_and_testing(driver, user, password):
+    """If login page appear enter "{user}" and "{password}"."""
+    wait_on_element(driver, 0.5, 5, 'xpath', '//input[@placeholder="Username"]')
+    if is_element_present(driver, 'xpath', '//input[@placeholder="Username"]'):
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys(user)
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(password)
+        wait_on_element(driver, 0.5, 30, 'xpath', '//button[@name="signin_button"]')
+        driver.find_element_by_xpath('//button[@name="signin_button"]').click()
+    else:
+        element = driver.find_element_by_xpath('//span[contains(.,"root")]')
+        driver.execute_script("arguments[0].scrollIntoView();", element)
+        time.sleep(0.5)
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
+
+
+@then('You should see the dashboard')
+def you_should_see_the_dashboard(driver):
+    """You should see the dashboard."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//span[contains(.,"System Information")]')
+    driver.find_element_by_xpath('//span[contains(.,"System Information")]')
+
+
+@then('Click on the Accounts item in the left side menu')
+def click_on_the_accounts_item_in_the_left_side_menu(driver):
+    """Click on the Accounts item in the left side menu."""
+    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
+    class_attribute = element.get_attribute('class')
+    if 'open' not in class_attribute:
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]').click()
+
+
+@then('The Accounts menu should expand down')
+def the_accounts_menu_should_expand_down(driver):
+    """The Accounts menu should expand down."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//mat-list-item[@ix-auto="option__Users"]')
+    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
+    class_attribute = element.get_attribute('class')
+    assert 'open' in class_attribute, class_attribute
+
+
+@then('Click on Users')
+def click_on_users(driver):
+    """Click on Users."""
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Users"]').click()
+
+
+@then('The Users page should open')
+def the_users_page_should_open(driver):
+    """The Users page should open."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//div[contains(.,"Users")]')
+    driver.find_element_by_xpath('//div[contains(.,"Users")]')
+
+
+@then('On the right side of the table, click the Greater-Than-Sign for one of the users')
+def on_the_right_side_of_the_table_click_the_greaterthansign_for_one_of_the_users(driver):
+    """On the right side of the table, click the Greater-Than-Sign for one of the users."""
+    driver.find_element_by_xpath('//a[@ix-auto="expander__ericbsd"]').click()
+
+
+@then('The User Field should expand down to list further details')
+def the_user_field_should_expand_down_to_list_further_details(driver):
+    """The User Field should expand down to list further details."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//button[@ix-auto="button__EDIT_ericbsd"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]')
+
+
+@then('Click the Edit button that appears')
+def click_the_edit_button_that_appears(driver):
+    """Click the Edit button that appears."""
+    driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]').click()
+
+
+@then('The User Edit Page should open')
+def the_user_edit_page_should_open(driver):
+    """The User Edit Page should open."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//h4[contains(.,"Identification")]')
+    driver.find_element_by_xpath('//h4[contains(.,"Identification")]')
+
+
+@then('Add user to additional groups, like wheel and save change')
+def add_user_to_additional_groups_like_wheel_and_save_change(driver):
+    """Add user to additional groups, like wheel and save change."""
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Auxiliary Groups"]').click()
+    wait_on_element(driver, 0.5, 30, 'xpath', '//mat-option[@ix-auto="option__Auxiliary Groups_wheel"]')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Auxiliary Groups_wheel"]').click()
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Auxiliary Groups_wheel"]').send_keys(Keys.TAB)
+    wait_on_element(driver, 0.5, 30, 'xpath', '//button[@ix-auto="button__SAVE"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+
+
+@then('Change should be saved')
+def change_should_be_saved(driver):
+    """Change should be saved."""
+    wait_on_element_disappear(driver, 1, 30, 'xpath', '//h6[contains(.,"Please wait")]')
+    wait_on_element(driver, 0.5, 30, 'xpath', '//div[contains(.,"Users")]')
+
+
+@then('reopen the user edit page and ensure that the additional group was saved')
+def reopen_the_user_edit_page_and_ensure_that_the_additional_group_was_saved(driver):
+    """reopen the user edit page and ensure that the additional group was saved."""
+    driver.find_element_by_xpath('//a[@ix-auto="expander__ericbsd"]').click()
+    wait_on_element(driver, 0.5, 30, 'xpath', '//button[@ix-auto="button__EDIT_ericbsd"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]').click()
+    wait_on_element(driver, 0.5, 30, 'xpath', '//h4[contains(.,"Identification")]')
+
+
+@then('Aux Group added should be visible')
+def aux_group_added_should_be_visible(driver):
+    """Aux Group added should be visible."""
+    driver.find_element_by_xpath('//span[contains(.,"wheel,")]')
