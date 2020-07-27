ce83f25c437e27667643f59f1ad81f710185cf98
ericbsd <ericturgeon.bsd@gmail.com>
2020-07-27 15:31:10 -0300

    Adding NAS-T915 test cases steps definition with code
    (cherry picked from commit f6f1adbdaf24945322353ec94ac1ac266b5af947)
    (cherry picked from commit 005d7d44040d66fafb71fa17b761d546654dc219)

---
+++ tests/bdd/ha-bhyve02/test_NAS_T0915_edit_user_enable_password.py
@@ -0,0 +1,175 @@
+# coding=utf-8
+"""High Availability (tn-bhyve02) feature tests."""
+
+import time
+from function import wait_on_element, is_element_present, wait_on_element_disappear
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
+    parsers
+)
+
+
+@scenario('features/NAS-T915.feature', 'Edit User enable Password')
+def test_edit_user_enable_password(driver):
+    """Edit User enable Password."""
+
+
+@given(parsers.parse('The browser is open navigate to "{nas_url}"'))
+def the_browser_is_open_navigate_to_tnbhyve02tnixsystemsnet(driver, nas_url):
+    """The browser is open navigate to "{nas_user}"."""
+    if nas_url not in driver.current_url:
+        driver.get(f"http://{nas_url}/ui/sessions/signin")
+        time.sleep(3)
+
+
+@when(parsers.parse('If login page appear enter "{user}" and "{password}"'))
+def if_login_page_appear_enter_root_and_testing(driver, user, password):
+    """If login page appear enter "{user}" and "{password}"."""
+    if not is_element_present(driver, 'xpath', '//mat-list-item[@ix-auto="option__Dashboard"]'):
+        wait_on_element(driver, 0.5, 5, 'xpath', '//input[@placeholder="Username"]')
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys(user)
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(password)
+        wait_on_element(driver, 0.5, 30, 'xpath', '//button[@name="signin_button"]')
+        driver.find_element_by_xpath('//button[@name="signin_button"]').click()
+    else:
+        element = driver.find_element_by_xpath('//span[contains(.,"root")]')
+        driver.execute_script("arguments[0].scrollIntoView();", element)
+        time.sleep(0.5)
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
+
+
+@then('You should see the dashboard')
+def you_should_see_the_dashboard(driver):
+    """You should see the dashboard."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//span[contains(.,"System Information")]')
+    driver.find_element_by_xpath('//span[contains(.,"System Information")]')
+
+
+@then('Click on the Accounts item in the left side menu')
+def click_on_the_accounts_item_in_the_left_side_menu(driver):
+    """Click on the Accounts item in the left side menu."""
+    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
+    class_attribute = element.get_attribute('class')
+    if 'open' not in class_attribute:
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]').click()
+
+
+@then('The Accounts menu should expand down')
+def the_accounts_menu_should_expand_down(driver):
+    """The Accounts menu should expand down."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//mat-list-item[@ix-auto="option__Users"]')
+    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
+    class_attribute = element.get_attribute('class')
+    assert 'open' in class_attribute, class_attribute
+
+
+@then('Click on Users')
+def click_on_users(driver):
+    """Click on Users."""
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Users"]').click()
+
+
+@then('The Users page should open')
+def the_users_page_should_open(driver):
+    """The Users page should open."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//div[contains(.,"Users")]')
+    driver.find_element_by_xpath('//div[contains(.,"Users")]')
+
+
+@then('On the right side of the table, click the Greater-Than-Sign for one of the users')
+def on_the_right_side_of_the_table_click_the_greaterthansign_for_one_of_the_users(driver):
+    """On the right side of the table, click the Greater-Than-Sign for one of the users."""
+    driver.find_element_by_xpath('//a[@ix-auto="expander__ericbsd"]').click()
+
+
+@then('The User Field should expand down to list further details')
+def the_user_field_should_expand_down_to_list_further_details(driver):
+    """The User Field should expand down to list further details."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//button[@ix-auto="button__EDIT_ericbsd"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]')
+
+
+@then('Click the Edit button that appears')
+def click_the_edit_button_that_appears(driver):
+    """Click the Edit button that appears."""
+    driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]').click()
+
+
+@then('The User Edit Page should open')
+def the_user_edit_page_should_open(driver):
+    """The User Edit Page should open."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//h4[contains(.,"Identification")]')
+    driver.find_element_by_xpath('//h4[contains(.,"Identification")]')
+
+
+@then('Change "Disable Password" to No and click save')
+def change_disable_password_to_no_and_click_save(driver):
+    """Change "Disable Password" to No and click save."""
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Disable Password"]').click()
+    wait_on_element(driver, 0.5, 30, 'xpath', '//mat-option[@ix-auto="option__Disable Password_No"]')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Disable Password_No"]').click()
+
+
+@then('Change should be saved')
+def change_should_be_saved(driver):
+    """Change should be saved."""
+    wait_on_element(driver, 0.5, 30, 'xpath', '//button[@ix-auto="button__SAVE"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+    wait_on_element_disappear(driver, 1, 30, 'xpath', '//h6[contains(.,"Please wait")]')
+    wait_on_element(driver, 0.5, 30, 'xpath', '//div[contains(.,"Users")]')
+
+
+@then('Open the user drop down to verify the user Disable Password is false')
+def open_the_user_drop_down_to_verify_the_user_disable_password_is_false(driver):
+    """Open the user drop down to verify the user Disable Password is false."""
+    driver.find_element_by_xpath('//a[@ix-auto="expander__ericbsd"]').click()
+    wait_on_element(driver, 0.5, 30, 'xpath', '//button[@ix-auto="button__EDIT_ericbsd"]')
+    driver.find_element_by_xpath('//h4[contains(.,"Password Disabled:")]')
+
+
+@then('Updated value should be visible')
+def updated_value_should_be_visible(driver):
+    """Updated value should be visible."""
+    element_text = driver.find_element_by_xpath('//h4[contains(.,"Password Disabled:")]/../div/p').text
+    assert element_text == 'false'
+
+
+@then('Try login with ssh')
+def try_login_with_ssh(driver):
+    """Try login with ssh."""
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Services"]').click()
+    wait_on_element(driver, 0.5, 10, 'xpath', '//span[contains(.,"Name")]')
+    element = driver.find_element_by_xpath('//mat-slide-toggle[@ix-auto="slider__SSH_Running"]')
+    class_attribute = element.get_attribute('class')
+    if 'mat-checked' not in class_attribute:
+        driver.find_element_by_xpath('//div[@ix-auto="overlay__SSH_Running"]').click()
+        time.sleep(4)
+    wait_on_element(driver, 0.5, 30, 'xpath', '//mat-list-item[@ix-auto="option__Shell"]')
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Shell"]').click()
+    wait_on_element(driver, 4, 30, 'xpath', '//span[@class="reverse-video terminal-cursor"]')
+    actions = ActionChains(driver)
+    actions.send_keys('ssh ericbsd@127.0.0.1', Keys.ENTER)
+    actions.perform()
+    wait_on_element(driver, .5, 4, 'xpath', '//span[contains(.,"(yes/no)?")]')
+    if is_element_present(driver, 'xpath', '//span[contains(.,"(yes/no)?")]'):
+        actions = ActionChains(driver)
+        actions.send_keys('yes', Keys.ENTER)
+        actions.perform()
+    wait_on_element(driver, .5, 4, 'xpath', '//span[contains(.,"password:")]')
+    actions = ActionChains(driver)
+    actions.send_keys('testing', Keys.ENTER)
+    actions.perform()
+
+
+@then('User should be able to login')
+def user_should_be_able_to_login(driver):
+    """User should be able to login."""
+    wait_on_element(driver, 1, 5, 'xpath', '//span[contains(.,"Permission") and contains(.,"denied,")]')
+    assert not is_element_present(driver, 'xpath', '//span[contains(.,"Permission") and contains(.,"denied,")]')
