c002383da98e5018b0d716c3710472c7aef67ea1
Dennis <dkmullen@gmail.com>
2020-07-29 08:44:31 -0400

    NAS-106920 Display paragraph only on add
    (cherry picked from commit 646038c1e539d448a3aa86eb043dca50be6fe8fa)

--- src/app/pages/sharing/afp/afp-form/afp-form.component.ts
+++ src/app/pages/sharing/afp/afp-form/afp-form.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import globalHelptext from 'app/helptext/global-helptext';
 import { helptext_sharing_afp, shared } from 'app/helptext/sharing';
 import { FieldSets } from 'app/pages/common/entity/entity-form/classes/field-sets';
+import { FieldConfig } from 'app/pages/common/entity/entity-form/models/field-config.interface';
 import { EntityFormComponent } from 'app/pages/common/entity/entity-form/entity-form.component';
 import { forbiddenValues } from 'app/pages/common/entity/entity-form/validators/forbidden-values-validation';
 import { T } from "app/translate-marker";
@@ -31,7 +32,7 @@ export class AFPFormComponent implements OnDestroy {
   public afp_timemachine_subscription: any;
   public title = helptext_sharing_afp.formTitle;
   private namesInUse: string[] = [];
-
+  public fieldConfig: FieldConfig[] = []
   private fieldSets = new FieldSets([
     {
       name: 'recommendation',
@@ -41,7 +42,8 @@ export class AFPFormComponent implements OnDestroy {
         {
           name: 'recommendation',
           type: 'paragraph',
-          paraText: helptext_sharing_afp.smb_dialog.message
+          paraText: helptext_sharing_afp.smb_dialog.message,
+          isHidden: true
         }
       ]
     },
@@ -373,6 +375,7 @@ export class AFPFormComponent implements OnDestroy {
   afterInit(entityForm: any) {
     if (!this.pk) {
       this.openInfoDialog();
+      _.find(this.fieldConfig, {name: 'recommendation'}).isHidden = false;
     }
     this.entityForm = entityForm;
     if (entityForm.isNew) {
