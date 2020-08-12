3b86a8a9c619d6ee19ee97e8b192999d535cf582
Lola Yang <lola.ly27@gmail.com>
2020-08-12 15:36:02 -0700

    NAS-107062 update table component

--- src/app/pages/common/entity/table/table.component.ts
+++ src/app/pages/common/entity/table/table.component.ts
@@ -1,36 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { WebSocketService } from 'app/services';
 
+import * as _ from 'lodash';
+
 export interface InputTableConf {
-    columns:any[];
-    queryCall?: string;
-    queryCallOption?: any;
+  title?: string;
+  columns: any[];
+  queryCall: string;
+  queryCallOption?: any;
+
+  dataSourceHelper?(any); // customise handle/modify dataSource 
+  getActions?(); // actions for each row
 }
 
 @Component({
-    selector: 'app-table',
-    templateUrl: './table.component.html',
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
 })
-export class TableComponent implements OnInit{
-    public dataSource;
-    public columns: Array<any> = [
-        {name : 'Name', prop : 'name'},
-        {name : 'Link State', prop : 'link_state'},
-        {name : 'IP Addresses', prop : 'addresses'},
-      ];
-    public displayedColumns = this.columns.map(col => col.prop);
-   
+export class TableComponent implements OnInit {
+  @Input('conf') tableConf: InputTableConf;
 
-      queryCall = 'interface.query';
+  public title = '';
+  public dataSource;
+  public displayedColumns;
 
 
-      constructor(private ws: WebSocketService) {}
-      ngOnInit() {
-          console.log(this.displayedColumns);
-          
-          this.ws.call(this.queryCall).subscribe(res => {
-              console.log(res);
-              this.dataSource = res;
-          })
-      }
+  constructor(private ws: WebSocketService) { }
+  ngOnInit() {
+    this.title = this.tableConf.title || '';
+    this.displayedColumns = this.tableConf.columns.map(col => col.name);
+
+    if (this.tableConf.getActions) {
+      this.displayedColumns.push('action'); // 
+    }
+    this.ws.call(this.tableConf.queryCall).subscribe(res => {
+      if (this.tableConf.dataSourceHelper) {
+         this.tableConf.dataSourceHelper(res);
+      } 
+        this.dataSource = res;
+    })
+  }
+
+  // getProp(data, prop) {
+  //   return _.get(data, prop);
+  // }
+
+  editRow(row) {
+    console.log(row);
+  }
+
+  deleteRow(row) {
+    console.log('delete', row);
+    event.stopPropagation();
+  }
+
+  unifyState(state) {
+    const stateClass = {
+      UP: 'STATE_UP',
+      DOWN: 'STATE_DOWN',
+    }
+    switch(state.toUpperCase()) {
+      case 'UP':
+        return stateClass.UP;
+        break;
+      case 'DOWN':
+        return stateClass.DOWN;
+    }
+  }
 }
