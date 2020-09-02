8050388c97747c225570325a1ed5ad730050abe8
Dennis <dkmullen@gmail.com>
2020-09-02 14:10:28 -0400

    NAS107393 new component

---
+++ src/app/pages/system/general-settings/gui-form/gui-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gui-form',
+  templateUrl: './gui-form.component.html',
+  styleUrls: ['./gui-form.component.css']
+})
+export class GuiFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
