9bfda7ca60959598d16fd5f30be7e39e7639f209
ericbsd <ericturgeon.bsd@gmail.com>
2020-09-04 16:38:17 -0300

    adding ha-bhyve02/test_NAS_T0927.py with steps definition and code
    (cherry picked from commit 44a3088acc1978395a48124e5cba4c8585d04a82)

---
+++ tests/bdd/ha-bhyve02/test_NAS_T0927.py
@@ -0,0 +1,123 @@
+# coding=utf-8
+"""High Availability (tn-bhyve02) feature tests."""
+
+import time
+from function import wait_on_element, is_element_present
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
+    parsers
+)
+
+
+@scenario('features/NAS-T927.feature', 'Verify invalid email cannot enter be in the User Edit Page')
+def test_verify_invalid_email_cannot_enter_be_in_the_user_edit_page(driver):
+    """Verify invalid email cannot enter be in the User Edit Page."""
+
+
+@given(parsers.parse('The browser is open navigate to "{nas_url}"'))
+def the_browser_is_open_navigate_to_nas_url(driver, nas_url):
+    """The browser is open navigate to "{nas_url}"."""
+    if nas_url not in driver.current_url:
+        driver.get(f"http://{nas_url}/ui/sessions/signin")
+        time.sleep(3)
+
+
+@when(parsers.parse('If login page appear enter "{user}" and "{password}"'))
+def if_login_page_appear_enter_root_and_testing(driver, user, password):
+    """If login page appear enter "{user}" and "{password}"."""
+    if not is_element_present(driver, '//mat-list-item[@ix-auto="option__Dashboard"]'):
+        assert wait_on_element(driver, 0.5, 5, '//input[@placeholder="Username"]')
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys(user)
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(password)
+        assert wait_on_element(driver, 0.5, 30, '//button[@name="signin_button"]')
+        driver.find_element_by_xpath('//button[@name="signin_button"]').click()
+    else:
+        element = driver.find_element_by_xpath('//span[contains(.,"root")]')
+        driver.execute_script("arguments[0].scrollIntoView();", element)
+        time.sleep(0.5)
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
+
+
+@then('You should see the dashboard')
+def you_should_see_the_dashboard(driver):
+    """You should see the dashboard."""
+    assert wait_on_element(driver, 0.5, 30, '//span[contains(.,"System Information")]')
+
+
+@then('Click on the Accounts, Click on Users')
+def click_on_the_accounts_click_on_users(driver):
+    """Click on the Accounts, Click on Users."""
+    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
+    class_attribute = element.get_attribute('class')
+    if 'open' not in class_attribute:
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]').click()
+    assert wait_on_element(driver, 0.5, 30, '//mat-list-item[@ix-auto="option__Users"]')
+    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
+    class_attribute = element.get_attribute('class')
+    assert 'open' in class_attribute, class_attribute
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Users"]').click()
+
+
+@then('The Users page should open')
+def the_users_page_should_open(driver):
+    """The Users page should open."""
+    assert wait_on_element(driver, 0.5, 30, '//div[contains(.,"Users")]')
+    driver.find_element_by_xpath('//div[contains(.,"Users")]')
+
+
+@then('On the right side of the table, click the Greater-Than-Sign for one of the users')
+def on_the_right_side_of_the_table_click_the_greaterthansign_for_one_of_the_users(driver):
+    """On the right side of the table, click the Greater-Than-Sign for one of the users."""
+    driver.find_element_by_xpath('//a[@ix-auto="expander__ericbsd"]').click()
+
+
+@then('The User Field should expand down to list further details')
+def the_user_field_should_expand_down_to_list_further_details(driver):
+    """The User Field should expand down to list further details."""
+    assert wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__EDIT_ericbsd"]')
+
+
+@then('Click the Edit button that appears')
+def click_the_edit_button_that_appears(driver):
+    """Click the Edit button that appears."""
+    driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]').click()
+
+
+@then('The User Edit Page should open')
+def the_user_edit_page_should_open(driver):
+    """The User Edit Page should open."""
+    assert wait_on_element(driver, 0.5, 30, '//h4[contains(.,"Identification")]')
+
+
+@then(parsers.parse('Change the users email to an invalid email i.e. "{email}" and click Save'))
+def change_the_users_email_to_an_invalid_email_ie_email_and_click_save(driver, email):
+    """Change the users email to an invalid email i.e. "{email}" and click Save."""
+    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').send_keys(email)
+    assert wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__SAVE"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+
+
+@then('You should not be allowed to save the invalid email')
+def you_should_not_be_allowed_to_save_the_invalid_email(driver):
+    """You should not be allowed to save the invalid email."""
+    assert wait_on_element(driver, 0.5, 30, '//div[contains(.,"Not a valid E-Mail address")]')
+
+
+@then('Try saving a blank email')
+def try_saving_a_blank_email(driver):
+    """Try saving a blank email."""
+    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').clear()
+    assert wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__SAVE"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+
+
+@then('You should not be allowed to save a blank email')
+def You_should_not_be_allowed_to_save_a_blank_email(driver):
+    """You should not be allowed to save a blank email."""
+    assert wait_on_element(driver, 0.5, 30, '//div[contains(.,"Not a valid E-Mail address")]')
