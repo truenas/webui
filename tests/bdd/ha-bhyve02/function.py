f0d9b7fd2facce13355bd4e30a16ed429e12b25b
ericbsd <ericturgeon.bsd@gmail.com>
2020-09-09 20:03:21 -0300

    adding functions to start ssh-add and setup sshkey for ssh
    (cherry picked from commit 58bb7f250309795312171190b73828144297d1a0)

--- tests/bdd/ha-bhyve02/function.py
+++ tests/bdd/ha-bhyve02/function.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import time
+import os
+import re
 from selenium.common.exceptions import NoSuchElementException
 from subprocess import run, PIPE
 
@@ -68,3 +70,52 @@ def ssh_cmd(command, username, password, host):
         return {'result': False, 'output': output}
     else:
         return {'result': True, 'output': output}
+
+
+def start_ssh_agent():
+    process = run(['ssh-agent', '-s'], stdout=PIPE, universal_newlines=True)
+    torecompil = 'SSH_AUTH_SOCK=(?P<socket>[^;]+).*SSH_AGENT_PID=(?P<pid>\d+)'
+    OUTPUT_PATTERN = re.compile(torecompil, re.MULTILINE | re.DOTALL)
+    match = OUTPUT_PATTERN.search(process.stdout)
+    if match is None:
+        return False
+    else:
+        agentData = match.groupdict()
+        os.environ['SSH_AUTH_SOCK'] = agentData['socket']
+        os.environ['SSH_AGENT_PID'] = agentData['pid']
+        return True
+
+
+def is_agent_setup():
+    return os.environ.get('SSH_AUTH_SOCK') is not None
+
+
+def setup_ssh_agent():
+    if is_agent_setup():
+        return True
+    else:
+        return start_ssh_agent()
+
+
+def create_key(keyPath):
+    process = run('ssh-keygen -t rsa -f %s -q -N ""' % keyPath, shell=True)
+    if process.returncode != 0:
+        return False
+    else:
+        return True
+
+
+def if_key_listed():
+    process = run('ssh-add -L', shell=True)
+    if process.returncode != 0:
+        return False
+    else:
+        return True
+
+
+def add_ssh_key(keyPath):
+    process = run(['ssh-add', keyPath])
+    if process.returncode != 0:
+        return False
+    else:
+        return True
