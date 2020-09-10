03f846856b992029895aa3cdfa358337cde02ecd
Erin Clark <erincodepirate@gmail.com>
2020-09-10 16:40:25 -0700

    Only show dedup if license is present
    NAS-107528

--- src/app/pages/storage/volumes/datasets/dataset-form/dataset-form.component.ts
+++ src/app/pages/storage/volumes/datasets/dataset-form/dataset-form.component.ts
@@ -571,6 +571,8 @@ export class DatasetFormComponent implements Formconfiguration{
           { label: 'verify', value: 'VERIFY' },
           { label: 'off', value: 'OFF' }
         ],
+        disabled: true,
+        isHidden: true,
       },
       {
         type: 'select',
@@ -885,6 +887,11 @@ export class DatasetFormComponent implements Formconfiguration{
     }
     ///
     this.entityForm = entityForm;
+    this.ws.call('system.info').subscribe((res) => {
+        if (res.license && res.license.features.indexOf('DEDUP') > -1) {
+          this.entityForm.setDisabled('deduplication',false, false);
+        }
+      });
     if (!this.parent){
       _.find(this.fieldConfig, {name:'quota_warning_inherit'}).placeholder = helptext.dataset_form_default;
       _.find(this.fieldConfig, {name:'quota_critical_inherit'}).placeholder = helptext.dataset_form_default;
