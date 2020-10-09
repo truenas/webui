69bd5d70f1520dc94caa76c227743e32ded5d537
Dennis <dkmullen@gmail.com>
2020-10-09 11:55:31 -0400

    NAS-107899 Prevent null being sent for CC

--- src/app/pages/system/support/tn-support/tn-support.component.ts
+++ src/app/pages/system/support/tn-support/tn-support.component.ts
@@ -233,7 +233,9 @@ export class TnSupportComponent implements OnInit {
     let payload = {};
     payload['name'] = entityEdit.name;
     payload['email'] = entityEdit.email;
-    payload['cc'] = entityEdit.cc;
+    if (entityEdit.cc) {
+      payload['cc'] = entityEdit.cc;
+    }
     payload['phone'] = entityEdit.phone;
     payload['category'] = entityEdit.TNCategory;
     payload['environment'] = entityEdit.environment;
