65c5347223c8f36f4e758592576eb64a61ddce31
Dennis <dkmullen@gmail.com>
2020-10-12 17:16:31 -0400

    NAS-107419

--- src/app/pages/storage/volumes/volumes-list/volumes-list.component.ts
+++ src/app/pages/storage/volumes/volumes-list/volumes-list.component.ts
@@ -1554,6 +1554,31 @@ export class VolumesListTableConfig implements InputTableConf {
             }
           });
         }
+        if (rowData.encrypted && rowData.key_loaded && rowData.encryption_root === rowData.name) {
+          const fileName = "dataset_" + rowData.name + "_keys.json";
+          const mimetype = 'text/plain';
+          encryption_actions.push({
+            id: rowData.name,
+            name: T('Export Key'),
+            label: T('Export Key'),
+            onClick: (row) => {
+              this.loader.open();
+              this.ws.call('core.download', ['pool.dataset.export_keys', [row.name], fileName]).subscribe(res => {
+                this.loader.close();
+                const url = res[1];
+                this.storageService.streamDownloadFile(this.http, url, fileName, mimetype).subscribe(file => {
+                  if(res !== null && res !== "") {
+                    this.storageService.downloadBlob(file, fileName);
+                  }
+                });
+              }, (e) => {
+                this.loader.close();
+                new EntityUtils().handleWSError(this, e, this.dialogService);
+              });
+            }
+          });
+
+        }
       }
     }
     return encryption_actions;
