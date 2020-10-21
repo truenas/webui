0b4950f5eeb2debe34eb947a6836755bfc05ee4f
ericbsd <ericturgeon.bsd@gmail.com>
2020-10-21 12:07:42 -0300

    added wait_on_element before changing the password in NAS-T916
    added wait_on_element before changing the email in NAS-T927

--- tests/bdd/ha-bhyve02/test_NAS_T0916.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0916.py
@@ -36,7 +36,7 @@ def if_login_page_appear_enter_root_and_testing(driver, user, password):
         driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys(user)
         driver.find_element_by_xpath('//input[@placeholder="Password"]').clear()
         driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(password)
-        wait_on_element(driver, 0.5, 30, '//button[@name="signin_button"]')
+        wait_on_element(driver, 0.5, 7, '//button[@name="signin_button"]')
         driver.find_element_by_xpath('//button[@name="signin_button"]').click()
     else:
         element = driver.find_element_by_xpath('//span[contains(.,"root")]')
@@ -48,7 +48,7 @@ def if_login_page_appear_enter_root_and_testing(driver, user, password):
 @then('You should see the dashboard')
 def you_should_see_the_dashboard(driver):
     """You should see the dashboard."""
-    wait_on_element(driver, 0.5, 30, '//span[contains(.,"System Information")]')
+    wait_on_element(driver, 0.5, 7, '//span[contains(.,"System Information")]')
     driver.find_element_by_xpath('//span[contains(.,"System Information")]')
 
 
@@ -64,7 +64,7 @@ def click_on_the_accounts_item_in_the_left_side_menu(driver):
 @then('The Accounts menu should expand down')
 def the_accounts_menu_should_expand_down(driver):
     """The Accounts menu should expand down."""
-    wait_on_element(driver, 0.5, 30, '//mat-list-item[@ix-auto="option__Users"]')
+    wait_on_element(driver, 0.5, 7, '//mat-list-item[@ix-auto="option__Users"]')
     element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
     class_attribute = element.get_attribute('class')
     assert 'open' in class_attribute, class_attribute
@@ -79,7 +79,7 @@ def click_on_users(driver):
 @then('The Users page should open')
 def the_users_page_should_open(driver):
     """The Users page should open."""
-    wait_on_element(driver, 0.5, 30, '//div[contains(.,"Users")]')
+    wait_on_element(driver, 0.5, 7, '//div[contains(.,"Users")]')
     driver.find_element_by_xpath('//div[contains(.,"Users")]')
 
 
@@ -92,7 +92,7 @@ def on_the_right_side_of_the_table_click_the_greaterthansign_for_one_of_the_user
 @then('The User Field should expand down to list further details')
 def the_user_field_should_expand_down_to_list_further_details(driver):
     """The User Field should expand down to list further details."""
-    wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__EDIT_ericbsd"]')
+    wait_on_element(driver, 0.5, 7, '//button[@ix-auto="button__EDIT_ericbsd"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]')
 
 
@@ -105,13 +105,14 @@ def click_the_edit_button_that_appears(driver):
 @then('The User Edit Page should open')
 def the_user_edit_page_should_open(driver):
     """The User Edit Page should open."""
-    wait_on_element(driver, 0.5, 30, '//h4[contains(.,"Identification")]')
+    wait_on_element(driver, 0.5, 7, '//h4[contains(.,"Identification")]')
     driver.find_element_by_xpath('//h4[contains(.,"Identification")]')
 
 
 @then('Change the password in both fields and click save')
 def change_the_password_in_both_fields_and_click_save(driver):
     """Change the password in both fields and click save."""
+    assert wait_on_element(driver, 1, 7, '//input[@ix-auto="input__Password"]')
     driver.find_element_by_xpath('//input[@ix-auto="input__Password"]').send_keys('testing1')
     driver.find_element_by_xpath('//input[@ix-auto="input__Confirm Password"]').clear()
     driver.find_element_by_xpath('//input[@ix-auto="input__Confirm Password"]').send_keys('testing1')
@@ -120,18 +121,18 @@ def change_the_password_in_both_fields_and_click_save(driver):
 @then('Change should be saved')
 def change_should_be_saved(driver):
     """Change should be saved."""
-    wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__SAVE"]')
+    wait_on_element(driver, 1, 7, '//button[@ix-auto="button__SAVE"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
-    wait_on_element_disappear(driver, 1, 30, '//h6[contains(.,"Please wait")]')
-    wait_on_element(driver, 0.5, 30, '//div[contains(.,"Users")]')
+    wait_on_element_disappear(driver, 1, 7, '//h6[contains(.,"Please wait")]')
+    wait_on_element(driver, 1, 7, '//div[contains(.,"Users")]')
 
 
 @then('Log out and try to log back in with the old password for that user')
 def log_out_and_try_to_log_back_in_with_the_old_password_for_that_user(driver):
     """Log out and try to log back in with the old password for that user."""
-    wait_on_element(driver, 0.5, 30, '//mat-list-item[@ix-auto="option__Shell"]')
+    wait_on_element(driver, 0.5, 7, '//mat-list-item[@ix-auto="option__Shell"]')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Shell"]').click()
-    wait_on_element(driver, 4, 30, '//span[@class="reverse-video terminal-cursor"]')
+    wait_on_element(driver, 4, 7, '//span[@class="reverse-video terminal-cursor"]')
     actions = ActionChains(driver)
     actions.send_keys('ssh ericbsd@127.0.0.1', Keys.ENTER)
     actions.perform()
@@ -152,13 +153,13 @@ def user_should_not_be_able_to_log_in_ssh_with_the_old_password(driver):
 def try_to_log_back_in_ssh_with_the_new_password_for_that_user(driver):
     """Try to log back in ssh with the new password for that user."""
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
-    wait_on_element(driver, 0.5, 30, '//mat-list-item[@ix-auto="option__Shell"]')
+    wait_on_element(driver, 0.5, 7, '//mat-list-item[@ix-auto="option__Shell"]')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Shell"]').click()
-    wait_on_element(driver, 4, 30, '//span[@class="reverse-video terminal-cursor"]')
+    wait_on_element(driver, 4, 7, '//span[@class="reverse-video terminal-cursor"]')
     actions = ActionChains(driver)
     actions.send_keys('ssh ericbsd@127.0.0.1', Keys.ENTER)
     actions.perform()
-    wait_on_element(driver, .5, 4, '//span[contains(.,"password:")]')
+    wait_on_element(driver, 0.5, 4, '//span[contains(.,"password:")]')
     actions = ActionChains(driver)
     actions.send_keys('testing1', Keys.ENTER)
     actions.perform()
