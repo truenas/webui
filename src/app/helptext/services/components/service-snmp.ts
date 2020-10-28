64bc74978aa6498a152cfc447d11a6fcfaf7ed36
Dennis Mullen <dkmullen@gmail.com>
2020-10-28 11:55:56 -0400

    Update src/app/helptext/services/components/service-snmp.ts
    Co-authored-by: Tim Moore <Mrt134@users.noreply.github.com>
    (cherry picked from commit 63a4e0ec0d7eee54ea43657325eaef1f64b90c68)

--- src/app/helptext/services/components/service-snmp.ts
+++ src/app/helptext/services/components/service-snmp.ts
@@ -117,6 +117,6 @@ formTitle: T('SNMP'),
 
 iftop: {
   placeholder: T('Enable iftop'),
-  tooltip: T('Include network performance statistics via iftop in SNMP messages.')
+  tooltip: T('Include iftop network performance statistics in SNMP messages.')
 }
 }
