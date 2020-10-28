8f855278e49f7ba432fb753c4ddf5c42eb13e757
Lola Yang <lola.ly27@gmail.com>
2020-10-28 12:33:43 -0700

    NAS-107881 make entity-wizard able to handle fieldsets

--- src/app/pages/common/entity/entity-form/models/wizard.interface.ts
+++ src/app/pages/common/entity/entity-form/models/wizard.interface.ts
@@ -1,7 +1,9 @@
 import { FieldConfig} from './field-config.interface';
+import { FieldSet} from './fieldset.interface';
 
 export interface Wizard {
   label:string;
   fieldConfig:FieldConfig[];
+  fieldSets?: FieldSet[];
   skip?: boolean;
 }
