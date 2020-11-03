f479dd1a7c79bd70d665356570d941ca2c06ff3b
ericbsd <ericturgeon.bsd@gmail.com>
2020-11-03 13:44:58 -0400

    added step with code to send a file to the ACL smb share NAS-T939
    added step with code to verify that the file is on the dataset using REST API call filesystem/stat in test_NAS_T0939.py

--- tests/bdd/ha-bhyve02/test_NAS_T0939.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0939.py
@@ -5,7 +5,9 @@ import time
 from function import (
     wait_on_element,
     is_element_present,
-    attribute_value_exist
+    attribute_value_exist,
+    run_cmd,
+    post
 )
 from pytest_bdd import (
     given,
@@ -81,6 +83,8 @@ def click_add(driver):
 
 @then(parsers.parse('Set Path to the ACL dataset "{path}"'))
 def set_path_to_the_acl_dataset_mntdozermyacldataset(driver, path):
+    global smb_path
+    smb_path = path
     """Set Path to the ACL dataset "/mnt/dozer/my_acl_dataset"."""
     assert wait_on_element(driver, 0.5, 5, '//input[@ix-auto="input__path"]')
     driver.find_element_by_xpath('//input[@ix-auto="input__path"]').clear()
@@ -117,6 +121,8 @@ def click_summit(driver):
 @then(parsers.parse('The "{smbname}" should be added'))
 def the_mysmbshare_should_be_added(driver, smbname):
     """The "mysmbshare" should be added."""
+    assert wait_on_element(driver, 1, 5, '//div[contains(.,"Samba")]')
+    assert wait_on_element(driver, 1, 5, f'//div[contains(.,"{smbname}")]')
 
 
 @then('Click on service')
@@ -153,3 +159,20 @@ def click_on_smb_start_automatically_checkbox(driver):
     value_exist = attribute_value_exist(driver, '//mat-checkbox[@ix-auto="checkbox__SMB_Start Automatically"]', 'class', 'mat-checkbox-checked')
     if not value_exist:
         driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__SMB_Start Automatically"]').click()
+
+
+@then(parsers.parse('Send a file to the share with "{nas_url}"/"{smbname}" and "{user}"%"{password}"'))
+def send_a_file_to_the_share_with_nas_url_smbname_and_user_password(driver, nas_url, smbname, user, password):
+    """Send a file the share with "nas_url"/"smbname" and "user"%"password"."""
+    run_cmd('touch testfile.txt')
+    results = run_cmd(f'smbclient //{nas_url}/{smbname} -W AD01 -U {user}%{password} -c "put testfile.txt testfile.txt"')
+    time.sleep(1)
+    run_cmd('rm testfile.txt')
+    assert results['result'], results['output']
+
+
+@then(parsers.parse('Verify that the is on "{nas_url}" with "{user}" and "{password}"'))
+def Verify_that_the_is_on_url_with_root_and_password(driver, nas_url, user, password):
+    """Verify that the is on "{nas_url}" with "{user}" and "{password}"."""
+    results = post(nas_url, 'filesystem/stat/', (user, password), f'{smb_path}/testfile.txt')
+    assert results.status_code == 200, results.text
