2d6176ca81c9947896e7e548bf59bd7f0484e0a2
ericbsd <ericturgeon.bsd@gmail.com>
2020-11-17 16:58:57 -0400

    adding code to press AGREE in ha-bhyve02/test_NAS_T0945.py

--- tests/bdd/ha-bhyve02/test_NAS_T0945.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0945.py
@@ -54,6 +54,8 @@ def you_should_see_the_dashboard(driver):
     """you should see the dashboard."""
     assert wait_on_element(driver, 1, 10, '//button[@ix-auto="button__CLOSE"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
+    assert wait_on_element(driver, 1, 5, '//button[@ix-auto="button__I AGREE"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__I AGREE"]').click()
     assert wait_on_element(driver, 0.5, 5, '//span[contains(.,"System Information")]')
     driver.find_element_by_xpath('//span[contains(.,"System Information")]')
 
