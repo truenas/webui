22683e103904feb612325ea3e7fa1647bba5c273
ericbsd <ericturgeon.bsd@gmail.com>
2020-11-24 14:53:03 -0400

    added code to wait for slider__SSH_Running in a-tn09/test_NAS_T0918.py

--- tests/bdd/ha-tn09/test_NAS_T0918.py
+++ tests/bdd/ha-tn09/test_NAS_T0918.py
@@ -124,6 +124,7 @@ def click_start_automatically_ssh_checkbox_and_enable_the_ssh_service(driver):
 @then('the service should be enabled with no errors')
 def the_service_should_be_enabled_with_no_errors(driver):
     """the service should be enabled with no errors."""
+    assert wait_on_element(driver, 1, 7, '//mat-slide-toggle[@ix-auto="slider__SSH_Running"]')
     wait_for_value = wait_for_attribute_value(driver, 1, 5, '//mat-slide-toggle[@ix-auto="slider__SSH_Running"]', 'class', 'mat-checked')
     assert wait_for_value
     # make sure to scroll back up the mat-list-item
