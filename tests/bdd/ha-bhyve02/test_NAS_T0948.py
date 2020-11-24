156e638a01914c52ac6e39c820f9ed03a98bdf14
ericbsd <ericturgeon.bsd@gmail.com>
2020-11-24 15:25:42 -0400

    Update test_NAS_T0948.py step definition and code to work with scale

--- tests/bdd/ha-bhyve02/test_NAS_T0948.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0948.py
@@ -29,53 +29,46 @@ def the_browser_is_open_navigate_to_nas_url(driver, nas_url):
 def if_login_page_appear_enter_root_and_password(driver, user, password):
     """If login page appear enter "root" and "testing"."""
     if not is_element_present(driver, '//mat-list-item[@ix-auto="option__Dashboard"]'):
-        wait_on_element(driver, 0.5, 5, '//input[@data-placeholder="Username"]')
+        assert wait_on_element(driver, 0.5, 5, '//input[@data-placeholder="Username"]')
         driver.find_element_by_xpath('//input[@data-placeholder="Username"]').clear()
         driver.find_element_by_xpath('//input[@data-placeholder="Username"]').send_keys(user)
         driver.find_element_by_xpath('//input[@data-placeholder="Password"]').clear()
         driver.find_element_by_xpath('//input[@data-placeholder="Password"]').send_keys(password)
-        wait_on_element(driver, 0.5, 30, '//button[@name="signin_button"]')
+        assert wait_on_element(driver, 0.5, 30, '//button[@name="signin_button"]')
         driver.find_element_by_xpath('//button[@name="signin_button"]').click()
     else:
-
         driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
 
 
 @then('You should see the dashboard')
 def you_should_see_the_dashboard(driver):
     """You should see the dashboard."""
-    wait_on_element(driver, 0.5, 30, '//span[contains(.,"System Information")]')
-    driver.find_element_by_xpath('//span[contains(.,"System Information")]')
+    assert wait_on_element(driver, 0.5, 30, '//h1[contains(.,"Dashboard")]')
+    assert wait_on_element(driver, 0.5, 30, '//span[contains(.,"System Information")]')
 
 
-@then('Click on the Accounts item in the left side menu')
-def click_on_the_accounts_item_in_the_left_side_menu(driver):
-    """Click on the Accounts item in the left side menu."""
-    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
-    class_attribute = element.get_attribute('class')
-    if 'open' not in class_attribute:
-        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]').click()
+@then('Click on the Credentials item in the left side menu')
+def click_on_the_credentials_item_in_the_left_side_menu(driver):
+    """Click on the Credentials item in the left side menu."""
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Credentials"]').click()
 
 
-@then('The Accounts menu should expand down')
-def the_accounts_menu_should_expand_down(driver):
-    """The Accounts menu should expand down."""
-    wait_on_element(driver, 0.5, 30, '//mat-list-item[@ix-auto="option__Users"]')
-    element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Accounts"]')
-    class_attribute = element.get_attribute('class')
-    assert 'open' in class_attribute, class_attribute
+@then('The Credentials menu should expand to the right')
+def the_credentials_menu_should_expand_to_the_right(driver):
+    """The Credentials menu should expand to the right."""
+    assert wait_on_element(driver, 1, 30, '//mat-list-item[@ix-auto="option__Local Users"]')
 
 
-@then('Click on Users')
-def click_on_users(driver):
-    """Click on Users."""
-    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Users"]').click()
+@then('Click on Local Users')
+def click_on_localusers(driver):
+    """Click on Local Users."""
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Local Users"]').click()
 
 
 @then('The Users page should open')
 def the_users_page_should_open(driver):
     """The Users page should open."""
-    wait_on_element(driver, 0.5, 30, '//div[contains(.,"Users")]')
+    assert wait_on_element(driver, 0.5, 30, '//div[contains(.,"Users")]')
     driver.find_element_by_xpath('//div[contains(.,"Users")]')
 
 
@@ -88,7 +81,7 @@ def on_the_right_side_of_the_table_click_the_greaterthansign_for_one_of_the_user
 @then('The User Field should expand down to list further details')
 def the_user_field_should_expand_down_to_list_further_details(driver):
     """The User Field should expand down to list further details."""
-    wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__EDIT_ericbsd"]')
+    assert wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__EDIT_ericbsd"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_ericbsd"]')
 
 
@@ -101,8 +94,8 @@ def click_the_edit_button_that_appears(driver):
 @then('The User Edit Page should open')
 def the_user_edit_page_should_open(driver):
     """The User Edit Page should open."""
-    wait_on_element(driver, 0.5, 30, '//h4[contains(.,"Identification")]')
-    driver.find_element_by_xpath('//h4[contains(.,"Identification")]')
+    assert wait_on_element(driver, 0.5, 30, '//h3[contains(.,"Edit User")]')
+    driver.find_element_by_xpath('//h3[contains(.,"Edit User")]')
 
 
 @then('Change the users shell and click save')
@@ -112,28 +105,28 @@ def change_the_users_shell_and_click_save(driver):
     driver.execute_script("arguments[0].scrollIntoView();", element)
     time.sleep(0.5)
     driver.find_element_by_xpath('//mat-select[@ix-auto="select__Shell"]').click()
-    wait_on_element(driver, 0.5, 30, '//mat-option[@ix-auto="option__Shell_tcsh"]')
-    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Shell_tcsh"]').click()
-    wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__SAVE"]')
+    assert wait_on_element(driver, 0.5, 30, '//mat-option[@ix-auto="option__Shell_zsh"]')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Shell_zsh"]').click()
+    assert wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__SAVE"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
 
 
 @then('Change should be saved')
 def change_should_be_saved(driver):
     """Change should be saved."""
-    wait_on_element_disappear(driver, 1, 30, '//h6[contains(.,"Please wait")]')
-    wait_on_element(driver, 0.5, 30, '//div[contains(.,"Users")]')
+    assert wait_on_element_disappear(driver, 1, 30, '//h6[contains(.,"Please wait")]')
+    assert wait_on_element(driver, 0.5, 30, '//div[contains(.,"Users")]')
 
 
 @then('Open the user drop down to verify the shell was changed')
 def open_the_user_drop_down_to_verify_the_shell_was_changed(driver):
     """Open the user drop down to verify the shell was changed."""
     driver.find_element_by_xpath('//a[@ix-auto="expander__ericbsd"]').click()
-    wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__EDIT_ericbsd"]')
+    assert wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__EDIT_ericbsd"]')
     driver.find_element_by_xpath('//h4[contains(.,"Shell:")]')
 
 
 @then('Updated value should be visible')
 def updated_value_should_be_visible(driver):
     """Updated value should be visible."""
-    driver.find_element_by_xpath('//p[contains(.,"/bin/tcsh")]')
+    driver.find_element_by_xpath('//p[contains(.,"/bin/zsh")]')
