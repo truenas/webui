9d1ea4ba2c6fc5466445681143ead4c4c5496b33
ryousaf <ryousaf@ixsystems.com>
2020-11-28 02:41:30 +0500

    Fixed password_conf validation bug after first successful password update
    Removed hardcoded text messages
    NAS-108417

--- src/app/helptext/topbar.ts
+++ src/app/helptext/topbar.ts
@@ -96,19 +96,14 @@ Login or signup is required.'),
         message: T('A message with verification instructions has been sent to the new email \
 address. Please verify the email address before continuing.')
     },
-
     changePasswordDialog: {
         pw_form_title_name: T('Change Administrator Password'),
-        pw_form_title_class:'change-password',
-        pw_username_placeholder : T('Username'),
         pw_current_pw_placeholder : T('Current Password'),
         pw_new_pw_placeholder : T('New Password'),
         pw_new_pw_tooltip : T('Passwords cannot contain a <b>?</b>. Passwords should\
          be at least eight characters and contain a mix of lower and\
          upper case, numbers, and special characters.'),
-         pw_new_pw_validation: [ Validators.pattern('^[^?]*$') ],
         pw_confirm_pw_placeholder : T('Confirm Password'),
-        pw_confirm_pw_validation : [ matchOtherValidator('password') ],
         pw_invalid_title: T('Incorrect Password'),
         pw_invalid_msg: T('The administrator password is incorrect.'),
         pw_updated: T("Password updated.")
