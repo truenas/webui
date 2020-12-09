2efc8cb753722797a07f153f16f4e6ce1ea667ef
akarpov <karpov4you@gmail.com>
2020-12-09 23:37:11 +0700

    fix other shells

--- src/app/pages/vm/vm-serial-shell/vmserial-shell.component.ts
+++ src/app/pages/vm/vm-serial-shell/vmserial-shell.component.ts
@@ -1,16 +1,20 @@
-import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChange, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChange, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { CopyPasteMessageComponent } from 'app/pages/shell/copy-paste-message.component';
 import helptext from '../../../helptext/vm/vm-cards/vm-cards';
 import { ShellService, WebSocketService } from '../../../services';
+import { Terminal } from 'xterm';
+import { AttachAddon } from 'xterm-addon-attach';
+import { FitAddon } from 'xterm-addon-fit';
 
 @Component({
   selector: 'app-vmserial-shell',
   templateUrl: './vmserial-shell.component.html',
   styleUrls: ['./vmserial-shell.component.css'],
   providers: [ShellService],
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class VMSerialShellComponent implements OnInit, OnChanges, OnDestroy {
@@ -19,11 +23,13 @@ export class VMSerialShellComponent implements OnInit, OnChanges, OnDestroy {
   cols: string;
   rows: string;
   font_size: number;
+  font_name = 'Courier New';
+  public connectionId: string;
   public token: any;
   public xterm: any;
   private shellSubscription: any;
   public shell_tooltip = helptext.serial_shell_tooltip;
-
+  private fitAddon: any;
   clearLine = "\u001b[2K\r"
   protected pk: string;
 
@@ -43,7 +49,7 @@ export class VMSerialShellComponent implements OnInit, OnChanges, OnDestroy {
         this.initializeWebShell(res);
         this.shellSubscription = this.ss.shellOutput.subscribe((value) => {
           if (value !== undefined) {
-            this.xterm.write(value);
+            // this.xterm.write(value);
           }
         });
         this.initializeTerminal();
@@ -74,31 +80,79 @@ export class VMSerialShellComponent implements OnInit, OnChanges, OnDestroy {
     }
   }
 
-  initializeTerminal() {
-    const domHeight = document.body.offsetHeight;
-    let rowNum = (domHeight * 0.75 - 104) / 21;
-    if (rowNum < 10) {
-      rowNum = 10;
+  getSize() {
+    const domWidth = this.container.nativeElement.offsetWidth;
+    const domHeight = this.container.nativeElement.offsetHeight;
+    var span = document.createElement('span');
+    this.container.nativeElement.appendChild(span);
+    span.style.whiteSpace = 'nowrap';
+    span.style.fontFamily = this.font_name;
+    span.style.fontSize = this.font_size + 'px';
+    span.innerHTML = 'a';
+
+    let cols = 0;
+    while(span.offsetWidth < domWidth) {      
+      span.innerHTML += 'a';
+      cols++;
     }
 
-    this.xterm = new (<any>window).Terminal({
-      'cursorBlink': true,
-      'tabStopWidth': 8,
-      'cols': 80,
-      'rows': parseInt(rowNum.toFixed(),10),
-      'focus': true
-    });
+    let rows = Math.ceil(domHeight / span.offsetHeight);
+    span.remove();
+    if (cols < 80) {
+      cols = 80;
+    }
+    
+    if (rows < 10) {
+      rows = 10;
+    }
 
-    this.xterm.open(this.container.nativeElement);
-    this.xterm.attach(this.ss);
+    return {
+      rows: rows,
+      cols: cols
+    }
+  }
+
+  initializeTerminal() {
+    const size = this.getSize();
+
+    const setting = {
+      cursorBlink: false,
+      tabStopWidth: 8,
+      cols: size.cols,
+      rows: size.rows,
+      focus: true,
+      fontSize: this.font_size,
+      fontFamily: this.font_name,
+    };
+
+    this.xterm = new Terminal(setting);
+    const attachAddon = new AttachAddon(this.ss.socket);
+    this.xterm.loadAddon(attachAddon);
+    this.fitAddon = new FitAddon();
+    this.xterm.loadAddon(this.fitAddon);
+    this.xterm.open(this.container.nativeElement, true);
+    this.fitAddon.fit();
     this.xterm._initialized = true;
-    // this.xterm.send('attachconsole.py /dev/nmdm'+this.pk+'B\n')
+  }
+
+  resizeTerm(){
+    const size = this.getSize();
+    this.xterm.setOption('fontSize', this.font_size);
+    this.fitAddon.fit();
+    this.ws.call('core.resize_shell', [this.connectionId, size.cols, size.rows]).subscribe((res)=> {
+    });
+    return true;
   }
 
   initializeWebShell(res: string) {
     this.ss.vmId = Number(this.pk);
     this.ss.token = res;
     this.ss.connect();
+
+    this.ss.shellConnected.subscribe((res)=> {
+      this.connectionId = res.id;
+      this.resizeTerm();
+    })
   }
 
   getAuthToken() {
