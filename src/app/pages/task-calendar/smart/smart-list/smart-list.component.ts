ca3550913e39819fe5393dbdd4c58d2f9c91a7d3
Denys Butenko <ua13dark@gmail.com>
2020-12-14 15:34:14 +0200

    NAS-100207 Unsubscribe on destroy

--- src/app/pages/task-calendar/smart/smart-list/smart-list.component.ts
+++ src/app/pages/task-calendar/smart/smart-list/smart-list.component.ts
@@ -1,19 +1,21 @@
-import { StorageService } from './../../../../services/storage.service';
-import { Component } from '@angular/core';
-import helptext from './../../../../helptext/task-calendar/smart/smart';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StorageService } from 'app/services/storage.service';
+import helptext from 'app/helptext/task-calendar/smart/smart';
 import { T } from 'app/translate-marker';
 
 @Component({
   selector: 'app-smart-list',
   template: `<entity-table [title]="title" [conf]="this"></entity-table>`,
 })
-export class SmartListComponent {
+export class SmartListComponent implements OnDestroy {
   public title = "S.M.A.R.T. Tests";
   public queryCall = "smart.test.query";
   protected route_add: string[] = ['tasks', 'smart', 'add'];
   protected route_add_tooltip = "Add S.M.A.R.T. Test";
   protected route_edit: string[] = ['tasks', 'smart', 'edit'];
   protected wsDelete = "smart.test.delete";
+  private disksSubscription: Subscription;
 
   public columns: Array<any> = [
     { name: helptext.smartlist_column_disks, prop: 'disks', always_display: true },
@@ -33,8 +35,8 @@ export class SmartListComponent {
   public listDisks = [];
 
   constructor(protected storageService: StorageService) {
-    this.storageService.listDisks().subscribe((res) => {
-      this.listDisks = res;
+    this.disksSubscription = this.storageService.listDisks().subscribe(listDisks => {
+      this.listDisks = listDisks;
     });
   }
 
@@ -44,13 +46,18 @@ export class SmartListComponent {
       if (test.all_disks) {
         test.disks = [T('All Disks')]
       } else if (test.disks.length) {
-        const disksReadableName = [];
-        test.disks.map(disk => {
-          disksReadableName.push(`${this.listDisks.find(item => item.identifier === disk).devname}`)
-        })
-        test.disks = disksReadableName;
+        const readableDisks = test.disks.map(disk => {
+          return this.listDisks.find(item => item.identifier === disk).devname;
+        });
+        test.disks = readableDisks;
       }
       return test;
     });
+  }
+
+  ngOnDestroy() {
+    if (this.disksSubscription) {
+      this.disksSubscription.unsubscribe();
+    }
   }
 }
