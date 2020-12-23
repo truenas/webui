47d19fdce0f56a8f9de948542031a55cb0e42915
ericbsd <ericturgeon.bsd@gmail.com>
2020-12-23 11:47:36 -0400

    added test_NAS_T0989.py with test case steps and selenium code

--- tests/bdd/core/test_NAS_T0989.py
+++ tests/bdd/core/test_NAS_T0989.py
@@ -2,8 +2,6 @@
 """Core UI feature tests."""
 
 import time
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from function import (
     wait_on_element,
     is_element_present,
@@ -14,12 +12,13 @@ from pytest_bdd import (
     scenario,
     then,
     when,
+    parsers
 )
 
 
-@scenario('features/NAS-T988.feature', 'Enable user Permit Sudo')
-def test_enable_user_permit_sudo(driver):
-    """Enable user Permit Sudo."""
+@scenario('features/NAS-T989.feature', 'Add an email to the user')
+def test_add_an_email_to_the_user(driver):
+    """Add an email to the user."""
 
 
 @given('the browser is open, the FreeNAS URL and logged in')
@@ -87,59 +86,34 @@ def the_user_edit_page_should_open(driver):
     assert wait_on_element(driver, 1, 7, '//h4[contains(.,"Identification")]')
 
 
-@then('click Enable Permit Sudo checkbox and click save')
-def click_enable_permit_sudo_checkbox_and_click_save(driver):
-    """click Enable Permit Sudo checkbox and click save."""
-    element = driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]')
-    driver.execute_script("arguments[0].scrollIntoView();", element)
-    time.sleep(0.5)
-    driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__Permit Sudo"]').click()
-    wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__SAVE"]')
+@then(parsers.parse('change the user email for "{email}" and click save'))
+def change_the_users_email_for_user_email_and_click_save(driver, email):
+    """change the user email for "email" and click save."""
+    global user_email
+    user_email = email
+    wait_on_element(driver, 1, 7, '//input[@ix-auto="input__Email"]')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').send_keys(email)
+    wait_on_element(driver, 0.5, 7, '//button[@ix-auto="button__SAVE"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
 
 
-@then('the changes should be saved')
-def the_changes_should_be_saved(driver):
-    """the changes should be saved."""
-    wait_on_element_disappear(driver, 1, 30, '//h6[contains(.,"Please wait")]')
+@then('change should be saved')
+def change_should_be_saved(driver):
+    """change should be saved."""
+    wait_on_element_disappear(driver, 1, 7, '//h6[contains(.,"Please wait")]')
     wait_on_element(driver, 0.5, 7, '//div[contains(.,"Users")]')
 
 
 @then('open the user dropdown')
-def open_the_user_dropdown(driver):
+def open_the_user_drop_down(driver):
     """open the user dropdown."""
     driver.find_element_by_xpath('//a[@ix-auto="expander__ericbsd"]').click()
-    wait_on_element(driver, 0.5, 30, '//button[@ix-auto="button__EDIT_ericbsd"]')
-    driver.find_element_by_xpath('//h4[contains(.,"Permit Sudo:")]')
+    wait_on_element(driver, 0.5, 7, '//button[@ix-auto="button__EDIT_ericbsd"]')
+    driver.find_element_by_xpath('//h4[contains(.,"Email:")]')
 
 
-@then('updated value should be visible')
-def updated_value_should_be_visible(driver):
-    """updated value should be visible."""
-    assert wait_on_element(driver, 1, 5, '//h4[contains(.,"Permit Sudo:")]/../div/p')
-    element_text = driver.find_element_by_xpath('//h4[contains(.,"Permit Sudo:")]/../div/p').text
-    assert element_text == 'true'
-
-
-@then('open a shell and run su user to become that user')
-def open_a_shell_and_run_su_user_to_become_that_user(driver):
-    """open a shell and run su user to become that user."""
-    wait_on_element(driver, 0.5, 30, '//mat-list-item[@ix-auto="option__Shell"]')
-    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Shell"]').click()
-    wait_on_element(driver, 4, 30, '//span[@class="reverse-video terminal-cursor"]')
-    actions = ActionChains(driver)
-    actions.send_keys('su ericbsd', Keys.ENTER)
-    actions.perform()
-
-
-@then('the user should be able to use Sudo')
-def the_user_should_be_able_to_use_sudo(driver):
-    """the user should be able to use Sudo."""
-    actions = ActionChains(driver)
-    actions.send_keys('sudo ls /var/db/sudo', Keys.ENTER)
-    actions.perform()
-    wait_on_element(driver, 1, 30, '//span[contains(.,"Password:")]')
-    actions.send_keys('testing', Keys.ENTER)
-    actions.perform()
-    wait_on_element(driver, 1, 30, '//span[contains(.,"lectured")]')
-    driver.find_element_by_xpath('//span[contains(.,"lectured")]')
+@then('email value should be visible')
+def email_value_should_be_visible(driver):
+    """email value should be visible."""
+    driver.find_element_by_xpath(f'//p[contains(.,"{user_email}")]')
