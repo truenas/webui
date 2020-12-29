c1c51f8e4612826d12070d85690318a115039436
Damian Szidiropulosz <damianos.bouzouki@gmail.com>
2020-12-29 12:11:20 -0500

    Revert "NAS-108681 / 21.02 / NAS-108681"

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -86,6 +86,13 @@ export class ApiService {
         args: [],
         responseEvent: "VolumeData"
       },
+      preProcessor(def:ApiCall){
+        const queryFilters = [
+          ["name","~", "^[^\/]+$" ], // Root datasets only
+        ];
+
+        return { args: [queryFilters], ...def };
+      },
     },
     DisksRequest:{
       apiCall:{
