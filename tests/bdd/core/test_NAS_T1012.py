564f7d65993e46e37073b659d8715e678253c42e
ericbsd <ericturgeon.bsd@gmail.com>
2021-01-26 15:48:35 -0400

    fix parsers modual syntax and add NAS-T1011 and NAS-T1012

--- tests/bdd/core/test_NAS_T1012.py
+++ tests/bdd/core/test_NAS_T1012.py
@@ -6,17 +6,17 @@ from pytest_bdd import (
     scenario,
     then,
     when,
-    parses
+    parsers
 )
 
 
 @scenario('features/NAS-T1012.feature', 'Create a new dataset with the LDAP user and group permissions')
-def test_create_a_new_dataset_with_the_ldap_user_and_group_permissions(driver, nas_ip, root_password):
+def test_create_a_new_dataset_with_the_ldap_user_and_group_permissions(driver):
     """Create a new dataset with the LDAP user and group permissions."""
 
 
 @given('the browser is open on the TrueNAS URL and logged in')
-def the_browser_is_open_on_the_truenas_url_and_logged_in(driver):
+def the_browser_is_open_on_the_truenas_url_and_logged_in(driver, nas_ip, root_password):
     """the browser is open on the TrueNAS URL and logged in."""
 
 
@@ -45,17 +45,17 @@ def the_add_dataset_name_and_options_page_should_open(driver):
     """the Add Dataset Name and Options page should open."""
 
 
-@then(parses.parse('input dataset name {dataset_name} and click save'))
+@then(parsers.parse('input dataset name {dataset_name} and click save'))
 def input_dataset_name_my_ldap_dataset_and_click_save(driver, dataset_name):
     """input dataset name my_ldap_dataset and click save."""
 
 
-@then(parses.parse('{dataset_name}should be created'))
+@then(parsers.parse('{dataset_name}should be created'))
 def my_ldap_dataset_should_be_created(driver, dataset_name):
     """my_ldap_dataset should be created."""
 
 
-@then(parses.parse('click on the {dataset_name} three dots button, select Edit Permissions'))
+@then(parsers.parse('click on the {dataset_name} three dots button, select Edit Permissions'))
 def click_on_the_my_ldap_dataset_three_dots_button_select_edit_permissions(driver, dataset_name):
     """click on the my_ldap_dataset three dots button, select Edit Permissions."""
 
@@ -65,12 +65,12 @@ def the_edit_permissions_page_should_open(driver):
     """the Edit Permissions page should open."""
 
 
-@then(parses.parse('select {ldap_user} for User, click on the Apply User checkbox'))
+@then(parsers.parse('select {ldap_user} for User, click on the Apply User checkbox'))
 def select_ldap_user_for_user_click_on_the_apply_user_checkbox(driver, ldap_user):
     """select ldap_user for User, click on the Apply User checkbox."""
 
 
-@then(parses.parse('select {ldap_user} for Group name, click on the Apply Group checkbox'))
+@then(parsers.parse('select {ldap_user} for Group name, click on the Apply Group checkbox'))
 def select_ldap_user_for_group_name_click_on_the_apply_group_checkbox(driver, ldap_user):
     """select ldap_user for Group name, click on the Apply Group checkbox."""
 
@@ -80,6 +80,6 @@ def click_the_save_button_should_be_returned_to_the_pool_page(driver):
     """click the Save button, should be returned to the pool page."""
 
 
-@then(parses.parse('verify that user and group name is {ldap_user}'))
+@then(parsers.parse('verify that user and group name is {ldap_user}'))
 def verify_that_user_and_group_name_is_ldap_user(driver, ldap_user):
     """verify that user and group name is ldap_user."""
