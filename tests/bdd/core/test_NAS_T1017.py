102abe892859d24f9719e894ea6c9d045798ef2f
ericbsd <ericturgeon.bsd@gmail.com>
2021-01-27 16:28:33 -0400

    added test_NAS_T1017 with test case step definition and selenium code

---
+++ tests/bdd/core/test_NAS_T1017.py
@@ -0,0 +1,159 @@
+# coding=utf-8
+"""Core UI feature tests."""
+
+import time
+from function import (
+    wait_on_element,
+    is_element_present,
+    wait_on_element_disappear,
+    attribute_value_exist
+)
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
+    parsers
+)
+
+
+@scenario('features/NAS-T1017.feature', 'Create a new dataset with access permission to only ericbsd')
+def test_create_a_new_dataset_with_access_permission_to_only_ericbsd():
+    """Create a new dataset with access permission to only ericbsd."""
+
+
+@given('the browser is open on the TrueNAS URL and logged in')
+def the_browser_is_open_on_the_truenas_url_and_logged_in(driver, nas_ip, root_password):
+    """the browser is open on the TrueNAS URL and logged in."""
+    if nas_ip not in driver.current_url:
+        driver.get(f"http://{nas_ip}")
+        assert wait_on_element(driver, 0.5, 10, '//input[@placeholder="Username"]')
+        time.sleep(1)
+    if not is_element_present(driver, '//mat-list-item[@ix-auto="option__Dashboard"]'):
+        assert wait_on_element(driver, 1, 10, '//input[@placeholder="Username"]')
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys('root')
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(root_password)
+        assert wait_on_element(driver, 0.5, 4, '//button[@name="signin_button"]')
+        driver.find_element_by_xpath('//button[@name="signin_button"]').click()
+    else:
+        element = driver.find_element_by_xpath('//span[contains(.,"root")]')
+        driver.execute_script("arguments[0].scrollIntoView();", element)
+        time.sleep(0.5)
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
+
+
+@when('you should be on the dashboard')
+def you_should_be_on_the_dashboard(driver):
+    """you should be on the dashboard."""
+    assert wait_on_element(driver, 1, 7, '//a[contains(.,"Dashboard")]')
+    assert wait_on_element(driver, 0.5, 7, '//span[contains(.,"System Information")]')
+
+
+@then('click on the Storage on the side menu, click on Pools')
+def click_on_the_storage_on_the_side_menu_click_on_pools(driver):
+    """click on the Storage on the side menu, click on Pools."""
+    assert wait_on_element(driver, 0.5, 7, '//mat-list-item[@ix-auto="option__Storage"]')
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Storage"]').click()
+    assert wait_on_element(driver, 0.5, 7, '//mat-list-item[@ix-auto="option__Pools"]')
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Pools"]').click()
+
+
+@then('the Pools page should open')
+def the_pools_page_should_open(driver):
+    """the Pools page should open."""
+    assert wait_on_element(driver, 0.5, 7, '//div[contains(.,"Pools")]')
+
+
+@then('click on the tank three dots button, select Add Dataset')
+def click_on_the_tank_three_dots_button_select_add_dataset(driver):
+    """click on the tank three dots button, select Add Dataset."""
+    assert wait_on_element(driver, 1, 7, '//mat-icon[@id="actions_menu_button__tank"]')
+    driver.find_element_by_xpath('//mat-icon[@id="actions_menu_button__tank"]').click()
+    assert wait_on_element(driver, 1, 7, '//button[@ix-auto="action__tank_Add Dataset"]')
+    driver.find_element_by_xpath('//button[@ix-auto="action__tank_Add Dataset"]').click()
+
+
+@then('the Add Dataset Name and Options page should open')
+def the_add_dataset_name_and_options_page_should_open(driver):
+    """the Add Dataset Name and Options page should open."""
+    assert wait_on_element(driver, 0.5, 7, '//h4[contains(.,"Name and Options")]')
+
+
+@then(parsers.parse('input dataset name {dataset_name} and click save'))
+def input_dataset_name_wheel_dataset_and_click_save(driver, dataset_name):
+    """input dataset name wheel_dataset and click save."""
+    assert wait_on_element(driver, 0.5, 7, '//input[@ix-auto="input__Name"]')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').send_keys(dataset_name)
+    driver.find_element_by_xpath('//button[@ix-auto="button__SUBMIT"]').click()
+
+
+@then(parsers.parse('the {dataset_name} should be created'))
+def the_wheel_dataset_should_be_created(driver, dataset_name):
+    """the wheel_dataset should be created."""
+    assert wait_on_element_disappear(driver, 1, 20, '//h6[contains(.,"Please wait")]')
+    assert wait_on_element(driver, 0.5, 10, f'//span[contains(.,"{dataset_name}")]')
+
+
+@then(parsers.parse('click on the {dataset_name} three dots button, select Edit Permissions'))
+def click_on_the_wheel_dataset_three_dots_button_select_edit_permissions(driver, dataset_name):
+    """click on the wheel_dataset three dots button, select Edit Permissions."""
+    assert wait_on_element(driver, 1, 7, f'//mat-icon[@ix-auto="options__{dataset_name}"]')
+    driver.find_element_by_xpath(f'//mat-icon[@id="actions_menu_button__{dataset_name}"]').click()
+    assert wait_on_element(driver, 1, 7, f'//button[@ix-auto="action__{dataset_name}_Edit Permissions"]')
+    driver.find_element_by_xpath(f'//button[@ix-auto="action__{dataset_name}_Edit Permissions"]').click()
+
+
+@then('the Edit Permissions page should open')
+def the_edit_permissions_page_should_open(driver):
+    """the Edit Permissions page should open."""
+    assert wait_on_element(driver, 1, 7, '//h4[contains(.,"Dataset Path")]')
+
+
+@then(parsers.parse('select {user} for User, click on the Apply User checkbox'))
+def select_root_for_user_click_on_the_apply_user_checkbox(driver, user):
+    """select root for User, click on the Apply User checkbox."""
+    assert wait_on_element(driver, 0.5, 7, '//input[@placeholder="User"]')
+    driver.find_element_by_xpath('//input[@placeholder="User"]').clear()
+    driver.find_element_by_xpath('//input[@placeholder="User"]').send_keys(user)
+    driver.find_element_by_xpath(f'//mat-option[@ix-auto="option__{user}"]').click()
+    assert wait_on_element(driver, 0.5, 7, '//mat-checkbox[@ix-auto="checkbox__Apply User"]/label/div')
+    driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__Apply User"]/label/div').click()
+
+
+@then(parsers.parse('select {group} for Group name, click on the Apply Group checkbox'))
+def select_wheel_for_group_name_click_on_the_apply_group_checkbox(driver, group):
+    """select wheel for Group name, click on the Apply Group checkbox."""
+    assert wait_on_element(driver, 0.5, 7, '//input[@placeholder="Group"]')
+    driver.find_element_by_xpath('//input[@placeholder="Group"]').clear()
+    driver.find_element_by_xpath('//input[@placeholder="Group"]').send_keys(group)
+    driver.find_element_by_xpath(f'//mat-option[@ix-auto="option__{group}"]').click()
+    assert wait_on_element(driver, 0.5, 7, '//mat-checkbox[@ix-auto="checkbox__Apply Group"]/label/div')
+    driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__Apply Group"]/label/div').click()
+
+
+@then('uncheck on Other Read Access and uncheck Other Execute')
+def uncheck_on_other_read_access_and_uncheck_other_execute(driver):
+    """uncheck on Other Read Access and uncheck Other Execute."""
+    driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__mode_otherRead"]').click()
+    driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__mode_otherExec"]').click()
+
+
+@then('click the Save button, should be returned to the pool list page')
+def click_the_save_button_should_be_returned_to_the_pool_list_page(driver):
+    """click the Save button, should be returned to the pool list page."""
+    wait_on_element(driver, 0.5, 7, '//button[@ix-auto="button__SAVE"]')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+    assert wait_on_element(driver, 1, 7, '//mat-panel-title[contains(.,"tank")]')
+    driver.find_element_by_xpath('//td[@ix-auto="value__tank_name"]')
+
+
+@then(parsers.parse('verify that the user and group are set to {user}'))
+def verify_that_the_user_and_group_are_set_to_ericbsd(driver, user):
+    """verify that the user and group are set to ericbsd."""
+    """verify that the user is root and the group is wheel."""
+    assert wait_on_element(driver, 1, 7, '//input[@placeholder="User"]')
+    assert attribute_value_exist(driver, '//input[@placeholder="User"]', 'value', user)
+    assert attribute_value_exist(driver, '//input[@placeholder="Group"]', 'value', user)
