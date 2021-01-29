11ae839ed17c90b1a4acf86c390704e01c52534b
ericbsd <ericturgeon.bsd@gmail.com>
2021-01-29 17:16:42 -0400

    added test_NAS_T1022 with test case step definition and selenium code

---
+++ tests/bdd/core/test_NAS_T1022.py
@@ -0,0 +1,96 @@
+# coding=utf-8
+"""Core UI feature tests."""
+
+import time
+from function import (
+    wait_on_element,
+    is_element_present,
+    wait_on_element_disappear
+)
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
+    parsers
+)
+
+
+@scenario('features/NAS-T1022.feature', 'Create dataset name iscsi for isqsi file test cases')
+def test_create_dataset_name_iscsi_for_isqsi_file_test_cases():
+    """Create dataset name iscsi for isqsi file test cases."""
+
+
+@given('the browser is open on the TrueNAS URL and logged in')
+def the_browser_is_open_on_the_truenas_url_and_logged_in(driver, nas_ip, root_password):
+    """the browser is open on the TrueNAS URL and logged in."""
+    if nas_ip not in driver.current_url:
+        driver.get(f"http://{nas_ip}")
+        assert wait_on_element(driver, 0.5, 10, '//input[@placeholder="Username"]')
+        time.sleep(1)
+    if not is_element_present(driver, '//mat-list-item[@ix-auto="option__Dashboard"]'):
+        assert wait_on_element(driver, 1, 10, '//input[@placeholder="Username"]')
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys('root')
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').clear()
+        driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(root_password)
+        assert wait_on_element(driver, 0.5, 4, '//button[@name="signin_button"]')
+        driver.find_element_by_xpath('//button[@name="signin_button"]').click()
+    else:
+        element = driver.find_element_by_xpath('//span[contains(.,"root")]')
+        driver.execute_script("arguments[0].scrollIntoView();", element)
+        time.sleep(0.5)
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
+
+
+@when('you should be on the dashboard')
+def you_should_be_on_the_dashboard(driver):
+    """you should be on the dashboard."""
+    assert wait_on_element(driver, 1, 7, '//a[contains(.,"Dashboard")]')
+    assert wait_on_element(driver, 0.5, 7, '//span[contains(.,"System Information")]')
+
+
+@then('click on the Storage on the side menu, click on Pools')
+def click_on_the_storage_on_the_side_menu_click_on_pools(driver):
+    """click on the Storage on the side menu, click on Pools."""
+    assert wait_on_element(driver, 0.5, 7, '//mat-list-item[@ix-auto="option__Storage"]')
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Storage"]').click()
+    assert wait_on_element(driver, 0.5, 7, '//mat-list-item[@ix-auto="option__Pools"]')
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Pools"]').click()
+
+
+@then('the Pools page should open')
+def the_pools_page_should_open(driver):
+    """the Pools page should open."""
+    assert wait_on_element(driver, 0.5, 7, '//div[contains(.,"Pools")]')
+
+
+@then('click on the tank three dots button, select Add Dataset')
+def click_on_the_tank_three_dots_button_select_add_dataset(driver):
+    """click on the tank three dots button, select Add Dataset."""
+    assert wait_on_element(driver, 1, 7, '//mat-icon[@id="actions_menu_button__tank"]')
+    driver.find_element_by_xpath('//mat-icon[@id="actions_menu_button__tank"]').click()
+    assert wait_on_element(driver, 1, 7, '//button[@ix-auto="action__tank_Add Dataset"]')
+    driver.find_element_by_xpath('//button[@ix-auto="action__tank_Add Dataset"]').click()
+
+
+@then('the Add Dataset Name and Options page should open')
+def the_add_dataset_name_and_options_page_should_open(driver):
+    """the Add Dataset Name and Options page should open."""
+    assert wait_on_element(driver, 0.5, 7, '//h4[contains(.,"Name and Options")]')
+
+
+@then(parsers.parse('input dataset name {dataset_name} and click save'))
+def input_dataset_name_wheel_dataset_and_click_save(driver, dataset_name):
+    """input dataset name wheel_dataset and click save."""
+    assert wait_on_element(driver, 0.5, 7, '//input[@ix-auto="input__Name"]')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').send_keys(dataset_name)
+    driver.find_element_by_xpath('//button[@ix-auto="button__SUBMIT"]').click()
+
+
+@then(parsers.parse('the {dataset_name} should be created'))
+def the_wheel_dataset_should_be_created(driver, dataset_name):
+    """the wheel_dataset should be created."""
+    assert wait_on_element_disappear(driver, 1, 20, '//h6[contains(.,"Please wait")]')
+    assert wait_on_element(driver, 0.5, 10, f'//span[contains(.,"{dataset_name}")]')
