a031269dad177c5ff5861c446ca1690d3eaf3d1d
ericbsd <ericturgeon.bsd@gmail.com>
2021-02-02 17:32:32 -0400

    Fixed ssh service test in ha-bhyve02/test_NAS_T0945.py

--- tests/bdd/ha-bhyve02/test_NAS_T0945.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0945.py
@@ -74,12 +74,10 @@ def the_service_page_should_open(driver):
 @then('press on configure(pencil) SSH')
 def press_on_configure_ssh(driver):
     """press on configure(pencil) SSH."""
-    assert wait_on_element(driver, 0.5, 5, '//button[@ix-auto="button__S3_Actions"]')
-    # Scroll to SSH service
-    element = driver.find_element_by_xpath('//button[@ix-auto="button__S3_Actions"]')
-    driver.execute_script("arguments[0].scrollIntoView();", element)
-    time.sleep(1)
-    driver.find_element_by_xpath('//button[@ix-auto="button__SSH_Actions"]').click()
+    assert wait_on_element(driver, 1, 5, '//button[@aria-label="Last page"]')
+    driver.find_element_by_xpath('//button[@aria-label="Last page"]').click()
+    assert wait_on_element(driver, 1, 5, '//button[@ix-auto="action__Configure_SSH"]')
+    driver.find_element_by_xpath('//button[@ix-auto="action__Configure_SSH"]').click()
 
 
 @then('the SSH General Options page should open')
@@ -110,25 +108,22 @@ def click_save(driver):
 def click_start_automatically_ssh_checkbox_and_enable_the_ssh_service(driver):
     """click Start Automatically SSH checkbox and enable the SSH service."""
     assert wait_on_element(driver, 1, 5, '//services')
-    assert wait_on_element(driver, 1, 5, '//button[@ix-auto="button__S3_Actions"]')
-    # Scroll to SSH service
-    element = driver.find_element_by_xpath('//button[@ix-auto="button__S3_Actions"]')
-    driver.execute_script("arguments[0].scrollIntoView();", element)
-    time.sleep(1)
-    driver.find_element_by_xpath('//div[@ix-auto="value__SSH"]')
-    value_exist = attribute_value_exist(driver, '//mat-checkbox[@ix-auto="checkbox__SSH_Start Automatically"]', 'class', 'mat-checkbox-checked')
+    assert wait_on_element(driver, 1, 5, '//button[@aria-label="Last page"]')
+    driver.find_element_by_xpath('//button[@aria-label="Last page"]').click()
+    assert wait_on_element(driver, 1, 5, '//mat-checkbox[@ix-auto="checkbox__enable__SSH"]')
+    value_exist = attribute_value_exist(driver, '//mat-checkbox[@ix-auto="checkbox__enable__SSH"]', 'class', 'mat-checkbox-checked')
     if not value_exist:
-        driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__SSH_Start Automatically"]').click()
-    value_exist = attribute_value_exist(driver, '//mat-slide-toggle[@ix-auto="slider__SSH_Running"]', 'class', 'mat-checked')
+        driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__enable__SSH"]').click()
+    value_exist = attribute_value_exist(driver, '//mat-slide-toggle[@ix-auto="slider__state__SSH"]', 'class', 'mat-checked')
     if not value_exist:
-        driver.find_element_by_xpath('//div[@ix-auto="overlay__SSH_Running"]').click()
+        driver.find_element_by_xpath('//div[@ix-auto="overlay__stateSSH"]').click()
     time.sleep(1)
 
 
 @then('the service should be enabled with no errors')
 def the_service_should_be_enabled_with_no_errors(driver):
     """the service should be enabled with no errors."""
-    wait_for_value = wait_for_attribute_value(driver, 1, 7, '//mat-slide-toggle[@ix-auto="slider__SSH_Running"]', 'class', 'mat-checked')
+    wait_for_value = wait_for_attribute_value(driver, 1, 7, '//mat-slide-toggle[@ix-auto="slider__state__SSH"]', 'class', 'mat-checked')
     assert wait_for_value
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
     time.sleep(3)
