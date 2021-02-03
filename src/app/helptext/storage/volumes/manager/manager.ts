70ca9fb88085cc5d72c8b1e5b4909eadf3c3c699
themylogin <themylogin@gmail.com>
2021-02-03 14:47:15 +0100

    Fix lack of whitespace in dialog box

--- src/app/helptext/storage/volumes/manager/manager.ts
+++ src/app/helptext/storage/volumes/manager/manager.ts
@@ -52,7 +52,7 @@ manager_duplicate_button: T("Repeat Vdev"),
 
 force_title: T("Warning"),
 force_warning: T("The current pool layout is not recommended.\
-Override the following errors?"),
+ Override the following errors?"),
 force_warnings:{
     'diskSizeWarning': T("One or more data vdevs has disks of different sizes."),
 },
