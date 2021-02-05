8b528ad0d8f225fc7fbb409d6e96eb531aa96306
akarpov <karpov4you@gmail.com>
2021-02-06 00:38:37 +0700

    After upgrading chart release UI does not show updated version of app

--- src/app/pages/applications/chart-releases/chart-releases.component.ts
+++ src/app/pages/applications/chart-releases/chart-releases.component.ts
@@ -181,6 +181,7 @@ export class ChartReleasesComponent implements OnInit {
       
       if (app && evt && evt.fields) {
         app.status = evt.fields.status;
+        app.version = evt.fields.chart_metadata.version;
       }
     });
   }
