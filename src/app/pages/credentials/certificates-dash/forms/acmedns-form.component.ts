a5fb7e4ba6ece96fd61619b2dcc9b274869b5967
ryousaf <ryousaf@ixsystems.com>
2021-02-23 01:15:14 +0500

    Dynamically created fields based on schemas returned from middleware for ACME DNS Authenticators NAS-109443

--- src/app/pages/credentials/certificates-dash/forms/acmedns-form.component.ts
+++ src/app/pages/credentials/certificates-dash/forms/acmedns-form.component.ts
@@ -5,6 +5,8 @@ import { ModalService } from 'app/services/modal.service';
 import { FieldConfig } from '../../../common/entity/entity-form/models/field-config.interface';
 import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
 import { helptext_system_acme as helptext, helptext_system_acme } from 'app/helptext/system/acme';
+import _ from 'lodash';
+import { EntityFormComponent } from 'app/pages/common/entity/entity-form';
 
 @Component({
   selector: 'app-acmedns-form',
@@ -20,69 +22,7 @@ export class AcmednsFormComponent {
   public title: string;
 
   protected fieldConfig: FieldConfig[];
-    public fieldSets: FieldSet[] = [
-      {
-        name: 'Add DNS Authenticator',
-        label: true,
-        config:[
-          {
-            type : 'input',
-            name : 'name',
-            placeholder : helptext.authenticator_name_placeholder,
-            tooltip : helptext.authenticator_name_tooltip,
-            required: true,
-            validation : helptext.authenticator_name_validation,
-            parent: this
-          },
-          {
-            type : 'select',
-            name : 'authenticator',
-            placeholder : helptext.authenticator_provider_placeholder,
-            tooltip : helptext.authenticator_provider_tooltip,
-              options : [
-                {label: 'Route53', value: 'route53'},
-              ],
-              value: 'route53',
-            parent: this
-          },
-          {
-            type : 'input',
-            name : 'access_key_id',
-            placeholder : helptext.auth_credentials_1_placeholder,
-            tooltip : helptext.auth_credentials_1_tooltip,
-            required: true,
-            validation : helptext.auth_credentials_1_validation,
-            parent: this,
-            relation: [
-              {
-                action: 'SHOW',
-                when: [{
-                  name: 'authenticator',
-                  value: 'route53',
-                 }]
-              }
-            ]
-          },
-          {
-            type : 'input',
-            name : 'secret_access_key',
-            placeholder : helptext.auth_credentials_2_placeholder,
-            tooltip : helptext.auth_credentials_2_tooltip,
-            required: true,
-            validation : helptext.auth_credentials_2_validation,
-            parent: this,
-            relation: [
-              {
-                action: 'SHOW',
-                when: [{
-                  name: 'authenticator',
-                  value: 'route53',
-                 }]
-              }
-            ]
-          }
-        ]
-      }]
+  public fieldSets: FieldSet[] = []
 
   protected entityForm: any;
   private rowNum: any;
@@ -96,6 +36,57 @@ export class AcmednsFormComponent {
         this.queryCallOption = [['id', '=', rowId]]
         this.getRow.unsubscribe();
       })
+      this.ws.call('acme.dns.authenticator.authenticator_schemas', []).subscribe((schemas) => {
+        const fieldSet: any = [
+          {
+            name: 'Add DNS Authenticator',
+            label: true,
+            config:[
+              {
+                type : 'input',
+                name : 'name',
+                placeholder : helptext.authenticator_name_placeholder,
+                tooltip : helptext.authenticator_name_tooltip,
+                required: true,
+                validation : helptext.authenticator_name_validation,
+                parent: this
+              },
+              {
+                type : 'select',
+                name : 'authenticator',
+                placeholder : helptext.authenticator_provider_placeholder,
+                tooltip : helptext.authenticator_provider_tooltip,
+                options : [
+                ],
+                parent: this
+              }
+            ]
+          }]
+        for(let schema of schemas) {
+          fieldSet[0].config[1].options.push({label: schema.key, value: schema.key});
+          for(let input of schema.schema) {
+            const conf = {
+              name: input['_name_'],
+              type: 'input',
+              required: input['_required_'],
+              placeholder: input['title'],
+              parent: this,
+              relation: [
+                {
+                  action: 'SHOW',
+                  when: [{
+                    name: 'authenticator',
+                    value: schema.key,
+                   }]
+                }
+              ]
+            };
+            fieldSet[0].config.push(conf);
+          }
+        }
+        fieldSet[0].config[1].value = schemas[0].key;
+        this.fieldSets = fieldSet;
+      });
   }
 
   resourceTransformIncomingRestData(data) {
