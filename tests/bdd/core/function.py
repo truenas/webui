baee4d75a944a9e58be7ca9ada08ee7c3d1e0a0b
ericbsd <ericturgeon.bsd@gmail.com>
2021-03-31 10:57:09 -0300

    added the new implimantation of wait_on_element with WebDriverWait
    also changed wait_on_element_disappear wait_for_attribute_value to only have wait

--- tests/bdd/core/function.py
+++ tests/bdd/core/function.py
@@ -7,7 +7,13 @@ import re
 import requests
 import sys
 import time
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException
+)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from subprocess import run, PIPE
 
 header = {'Content-Type': 'application/json', 'Vary': 'accept'}
@@ -21,18 +27,30 @@ def is_element_present(driver, xpath):
     return True
 
 
-def wait_on_element(driver, wait, loop, xpath):
-    for _ in range(loop):
-        time.sleep(wait)
-        if is_element_present(driver, xpath):
+def wait_on_element(driver, wait, xpath, condition=None):
+    if condition == 'clickable':
+        try:
+            WebDriverWait(driver, wait).until(ec.element_to_be_clickable((By.XPATH, xpath)))
             return True
+        except TimeoutException:
+            return False
+    if condition == 'presence':
+        try:
+            WebDriverWait(driver, wait).until(ec.presence_of_element_located((By.XPATH, xpath)))
+            return True
+        except TimeoutException:
+            return False
     else:
-        return False
+        try:
+            WebDriverWait(driver, wait).until(ec.visibility_of_element_located((By.XPATH, xpath)))
+            return True
+        except TimeoutException:
+            return False
 
 
-def wait_on_element_disappear(driver, wait, loop, xpath):
-    for _ in range(loop):
-        time.sleep(wait)
+def wait_on_element_disappear(driver, wait, xpath):
+    for _ in range(wait):
+        time.sleep(1)
         if not is_element_present(driver, xpath):
             return True
     else:
@@ -48,9 +66,9 @@ def attribute_value_exist(driver, xpath, attribute, value):
         return False
 
 
-def wait_for_attribute_value(driver, wait, loop, xpath, attribute, value):
-    for _ in range(loop):
-        time.sleep(wait)
+def wait_for_attribute_value(driver, wait, xpath, attribute, value):
+    for _ in range(wait):
+        time.sleep(1)
         if attribute_value_exist(driver, xpath, attribute, value):
             return True
     else:
