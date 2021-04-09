54a4bc17fe11f1dbf28d3fa03a74b138633ea412
ericbsd <ericturgeon.bsd@gmail.com>
2021-04-09 13:39:32 -0300

    change the firefox window size to 1920x1080

--- tests/bdd/conftest.py
+++ tests/bdd/conftest.py
@@ -60,6 +60,7 @@ def browser():
     firefox_capabilities['firefox_profile'] = profile.encoded
     firefox_capabilities['binary'] = binary
     web_driver = webdriver.Firefox(capabilities=firefox_capabilities)
+    web_driver.set_window_size(1920, 1080)
     web_driver.implicitly_wait(2)
     return web_driver
 
