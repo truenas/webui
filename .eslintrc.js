34089ea2895efa56b9604596538b8fa7387eb655
Evgeny Stepanovych <undsoft@gmail.com>
2021-04-14 14:25:29 +0300

    NAS-110054 - Setting up Husky and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "root": true,
+  "ignorePatterns": [
+    "debian/**/*",
+    "docker/**/*",
+    "ports/**/*",
+    "scripts/**/*",
+    "tests/**/*",
+    "node_modules/**/*",
+  ],
+  "overrides": [
+    {
+      "files": ["*.ts"],
+      "parser": "@typescript-eslint/parser",
+      "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module",
+        "createDefaultProgram": true
+      },
+      "plugins": ["@typescript-eslint", "@angular-eslint"],
+      "rules": {
+        "no-new-wrappers": "error",
+      }
+    }
+  ]
+}
