97cfe9976b56ad8be1829ab15e5fcd6ab98eb5c8
ryousaf <ryousaf@ixsystems.com>
2021-04-23 14:36:13 +0500

    Added Permission enum

--- src/app/interfaces/permissions.ts
+++ src/app/interfaces/permissions.ts
@@ -1,17 +1,19 @@
-export interface Permissions {
-    READ_DATA: boolean;
-    WRITE_DATA: boolean;
-    APPEND_DATA: boolean;
-    READ_NAMED_ATTRS: boolean;
-    WRITE_NAMED_ATTRS: boolean;
-    EXECUTE: boolean;
-    DELETE_CHILD: boolean;
-    READ_ATTRIBUTES: boolean;
-    WRITE_ATTRIBUTES: boolean;
-    DELETE: boolean;
-    READ_ACL: boolean;
-    WRITE_ACL: boolean;
-    WRITE_OWNER: boolean;
-    SYNCHRONIZE: boolean;
-    BASIC: boolean;
-  }
+export enum Permission {
+  ReadData = 'READ_DATA',
+  WriteData = 'WRITE_DATA',
+  AppendData = 'APPEND_DATA',
+  ReadNamedAttrs = 'READ_NAMED_ATTRS',
+  WriteNamedAttrs = 'WRITE_NAMED_ATTRS',
+  Execute = 'EXECUTE',
+  DeleteChild = 'DELETE_CHILD',
+  ReadAttributes = 'READ_ATTRIBUTES',
+  WriteAttributes = 'WRITE_ATTRIBUTES',
+  Delete = 'DELETE',
+  ReadAcl = 'READ_ACL',
+  WriteAcl = 'WRITE_ACL',
+  WriteOwner = 'WRITE_OWNER',
+  Synchronize = 'SYNCHRONIZE',
+  Basic = 'Basic'
+}
+
+export type Permissions = { [K in Permission]: boolean };
