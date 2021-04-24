92043b6382d421129c81b96115fbb036f05585ee
ryousaf <ryousaf@ixsystems.com>
2021-04-25 03:05:00 +0500

    Added field removed by PR# 3619 NAS-110247
    (cherry picked from commit e2bf891de93ebec655f9cac5e3cb152cdeb0edae)

--- src/app/pages/services/components/service-smart/service-smart.component.ts
+++ src/app/pages/services/components/service-smart/service-smart.component.ts
@@ -31,9 +31,18 @@ export class ServiceSMARTComponent {
           validation : [ Validators.required ]
         },
         {
-          type : 'input',
-          name : 'difference',
-          placeholder : helptext.smart_difference_placeholder,
+          type: 'select',
+          name: 'powermode',
+          placeholder: helptext.smart_powermode_placeholder,
+          tooltip: helptext.smart_powermode_tooltip,
+          options: helptext.smart_powermode_options,
+          required: true,
+          validation: helptext.smart_powermode_validation,
+        },
+        {
+          type: 'input',
+          name: 'difference',
+          placeholder: helptext.smart_difference_placeholder,
           tooltip: helptext.smart_difference_tooltip,
           required: true,
           validation : helptext.smart_difference_validation
