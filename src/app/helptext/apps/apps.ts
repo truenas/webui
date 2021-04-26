536ed5a3783789b508d64a2f161bf387dd3c240f
Aurélien Sallé <android.aurelien.salle@gmail.com>
2021-04-26 09:29:22 +0200

    Update apps.ts
    Fix TYPO "Container"

--- src/app/helptext/apps/apps.ts
+++ src/app/helptext/apps/apps.ts
@@ -513,7 +513,7 @@ export default {
     statusUpdateAvailable: T('Update available'),
     containerImageStatusUpdateAvailableTo: T('Following container images are available to update:\n'),
     statusUpdateAvailableTo: T('Available version:\n'),
-    tooltipHeader: T('Conainter Images'),
+    tooltipHeader: T('Container Images'),
   },
 
   manageCatalogs: {
