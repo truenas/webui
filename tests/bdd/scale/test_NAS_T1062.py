7cccff62a4fb8db0ca8ec8534b82f5dbfc0fb2ec
q5sys (JT) <jt@obs-sec.com>
2021-05-06 12:48:22 -0400

    Update test_NAS_T1062.py

--- tests/bdd/scale/test_NAS_T1062.py
+++ tests/bdd/scale/test_NAS_T1062.py
@@ -39,7 +39,8 @@ def the_browser_is_open_navigate_to_the_scale_url(driver, nas_ip, root_password)
     else:
         driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
 
-@then('on the dashboard, verify the Welcome box is loaded, click Close')
+
+@when('on the dashboard, verify the Welcome box is loaded, click Close')
 def on_the_dashboard_verify_the_welcome_box_is_loaded_click_close(driver):
     """on the dashboard, verify the Welcome box is loaded, click Close."""
     assert wait_on_element(driver, 5, '//h1[contains(.,"Dashboard")]')
@@ -50,7 +51,7 @@ def on_the_dashboard_verify_the_welcome_box_is_loaded_click_close(driver):
     driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
 
 
-@when('on the dashboard click on the System Settings side menu, then click services')
+@then('on the dashboard click on the System Settings side menu, then click services')
 def on_the_dashboard_click_on_the_system_settings_side_menu_then_click_services(driver):
     """on the dashboard click on the System Settings side menu, then click services."""
     assert wait_on_element(driver, 10, '//li[contains(.,"Dashboard")]')
@@ -70,10 +71,12 @@ def on_the_service_page_press_on_configurepencil_ssh(driver):
     time.sleep(1)
     driver.find_element_by_xpath('//button[@ix-auto="action__Configure_SSH"]').click()
 
+
 @then('the SSH General Options page should open')
 def the_ssh_general_options_page_should_open(driver):
     """the SSH General Options page should open."""
     assert wait_on_element(driver, 5, '//h4[contains(text(),"General Options")]')
+
 
 @then('click the checkbox "Log in as root with password"')
 def click_the_checkbox_log_in_as_root_with_password(driver):
@@ -81,6 +84,7 @@ def click_the_checkbox_log_in_as_root_with_password(driver):
     value_exist = attribute_value_exist(driver, '//mat-checkbox[@ix-auto="checkbox__Log in as Root with Password"]', 'class', 'mat-checkbox-checked')
     if not value_exist:
         driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__Log in as Root with Password"]').click()
+
 
 @then('verify the checkbox works and click Save')
 def verify_the_checkbox_works_and_click_save(driver):
@@ -90,6 +94,7 @@ def verify_the_checkbox_works_and_click_save(driver):
     assert wait_on_element(driver, 5, '//button[@ix-auto="button__SAVE"]', 'clickable')
     driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
     wait_on_element_disappear(driver, 10, '//h6[contains(.,"Please wait")]')
+
 
 @then('click the Start Automatically SSH checkbox and enable the SSH service')
 def click_the_start_automatically_ssh_checkbox_and_enable_the_ssh_service(driver):
@@ -109,16 +114,13 @@ def click_the_start_automatically_ssh_checkbox_and_enable_the_ssh_service(driver
         driver.find_element_by_xpath('//div[@ix-auto="slider__state__SSH"]').click()
     time.sleep(1)
 
+
 @then('the service should be enabled with no errors')
 def the_service_should_be_enabled_with_no_errors(driver):
     """the service should be enabled with no errors."""
     wait_for_value = wait_for_attribute_value(driver, 7, '//mat-slide-toggle[@ix-auto="overlay__SSH_Running"]', 'class', 'mat-checked')
     assert wait_for_value
-    # scroll back up the mat-list-item
-    element = driver.find_element_by_xpath('//span[contains(.,"root")]')
-    driver.execute_script("arguments[0].scrollIntoView();", element)
-    time.sleep(0.5)
-    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
+
 
 @then('ssh to a NAS with root and the root password should work')
 def ssh_to_a_nas_with_root_and_the_root_password_should_work(driver):
