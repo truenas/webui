98231fccd85a73c7f9d76f7cea9ab860d77ad46e
q5sys <jt@obs-sec.com>
2021-05-25 14:30:02 -0400

    fix test

--- tests/bdd/scale/test_NAS_T1067.py
+++ tests/bdd/scale/test_NAS_T1067.py
@@ -2,13 +2,13 @@
 """SCALE UI feature tests."""
 
 import time
-from function (
+from function import (
     wait_on_element,
     is_element_present,
     attribute_value_exist,
     wait_for_attribute_value,
+    wait_on_element_disappear,
 )
-
 from pytest_bdd import (
     given,
     scenario,
@@ -23,7 +23,7 @@ def test_create_a_new_user_call_ericbsd():
 
 
 @given('the browser is open, the FreeNAS URL and logged in')
-def the_browser_is_open_the_freenas_url_and_logged_in():
+def the_browser_is_open_the_freenas_url_and_logged_in(driver, nas_ip, root_password):
     """the browser is open, the FreeNAS URL and logged in."""
     if nas_ip not in driver.current_url:
         driver.get(f"http://{nas_ip}")
@@ -41,7 +41,7 @@ def the_browser_is_open_the_freenas_url_and_logged_in():
 
 
 @when('you should be on the dashboard, click on the Accounts on the side menu and click on Users')
-def you_should_be_on_the_dashboard_click_on_the_accounts_on_the_side_menu_and_click_on_users():
+def you_should_be_on_the_dashboard_click_on_the_accounts_on_the_side_menu_and_click_on_users(driver):
     """you should be on the dashboard, click on the Accounts on the side menu and click on Users."""
     assert wait_on_element(driver, 10, '//span[contains(.,"Dashboard")]')
     """click on the Credentials on the side menu, click on Local Users."""
@@ -52,20 +52,19 @@ def you_should_be_on_the_dashboard_click_on_the_accounts_on_the_side_menu_and_cl
 
     
 @when('the Users page should open, click on the "Add" Button')
-def the_users_page_should_open_click_on_the_add_button():
+def the_users_page_should_open_click_on_the_add_button(driver):
     """the Users page should open, click on the "Add" Button."""
-    assert wait_on_element(driver, 1, 7, '//div[contains(.,"Users")]')
-    assert wait_on_element(driver, 0.5, 7, '//button[@ix-auto="button__Users_ADD"]', 'clickable')
+    assert wait_on_element(driver, 10, '//div[contains(.,"Users")]')
+    assert wait_on_element(driver, 10, '//button[@ix-auto="button__Users_ADD"]', 'clickable')
     driver.find_element_by_xpath('//button[@ix-auto="button__Users_ADD"]').click()
 
 
 @then('the Users Add Page should open, input the fields Full Name, Username, Password and click Save')
-def the_users_add_page_should_open_input_the_fields_full_name_username_password_and_click_save():
+def the_users_add_page_should_open_input_the_fields_full_name_username_password_and_click_save(driver):
     """the Users Add Page should open."""
-    assert wait_on_element(driver, 1, 7, '//h3[contains(.,"Add User")]')
-    assert wait_on_element(driver, 1, 7, '//div[contains(.,"full_name")]')
+    assert wait_on_element(driver, 7, '//h3[contains(.,"Add User")]')
     """input in the following fields Full Name, Username, and password."""
-    assert wait_on_element(driver, 1, 7, '//input[@ix-auto="input__Full Name"]')
+    assert wait_on_element(driver, 7, '//input[@ix-auto="input__Full Name"]')
     driver.find_element_by_xpath('//input[@ix-auto="input__Full Name"]').clear()
     driver.find_element_by_xpath('//input[@ix-auto="input__Full Name"]').send_keys('Eric Turgeon')
     driver.find_element_by_xpath('//input[@ix-auto="input__Username"]').clear()
@@ -74,14 +73,13 @@ def the_users_add_page_should_open_input_the_fields_full_name_username_password_
     driver.find_element_by_xpath('//input[@ix-auto="input__Password"]').send_keys('testing')
     driver.find_element_by_xpath('//input[@ix-auto="input__Confirm Password"]').clear()
     driver.find_element_by_xpath('//input[@ix-auto="input__Confirm Password"]').send_keys('testing')
-    assert wait_on_element(driver, 0.5, 7, '//button[@ix-auto="button__SAVE"]')
+    assert wait_on_element(driver, 7, '//button[@ix-auto="button__SAVE"]', 'clickable')
     driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
 
 
 @then('the new User should be created and added to the user list')
-def the_new_user_should_be_created_and_added_to_the_user_list():
+def the_new_user_should_be_created_and_added_to_the_user_list(driver):
     """the new User should be created and added to the user list."""
-    assert wait_on_element_disappear(driver, 1, 7, '//h6[contains(.,"Please wait")]')
-    assert wait_on_element(driver, 0.5, 7, '//div[contains(.,"Users")]')
-    driver.find_element_by_xpath('//div[@ix-auto="value__ericbsd_Username"]')
-
+    assert wait_on_element_disappear(driver, 20, '//h6[contains(.,"Please wait")]')
+    assert wait_on_element(driver, 10, '//div[contains(.,"Users")]')
+    assert wait_on_element(driver, 10, '//div[contains(.,"ericbsd")]')
