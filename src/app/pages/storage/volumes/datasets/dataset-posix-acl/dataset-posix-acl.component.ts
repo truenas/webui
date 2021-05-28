d61ec2cdabf9baba129b96564941aaca8cc3214d
Evgeny Stepanovych <undsoft@gmail.com>
2021-05-28 18:09:42 +0300

    Fixing ACL Manager edits

--- src/app/pages/storage/volumes/datasets/dataset-posix-acl/dataset-posix-acl.component.ts
+++ src/app/pages/storage/volumes/datasets/dataset-posix-acl/dataset-posix-acl.component.ts
@@ -508,7 +508,7 @@ export class DatasetPosixAclComponent implements FormConfiguration, OnDestroy {
     body.uid = body.apply_user ? body.uid : null;
     body.gid = body.apply_group ? body.gid : null;
 
-    const doesNotWantToEditDataset = this.storageService.isDatasetTopLevel(body.path.replace('mnt/', ''))
+    const doesNotWantToEditDataset = this.storageService.isDatasetTopLevel(this.path.replace('mnt/', ''))
       && !(await this.dialogService
         .confirm(helptext.dataset_acl_dialog_warning, helptext.dataset_acl_toplevel_dialog_message)
         .toPromise());
@@ -559,7 +559,7 @@ export class DatasetPosixAclComponent implements FormConfiguration, OnDestroy {
 
     this.dialogRef.componentInstance.setCall(this.updateCall,
       [{
-        path: body.path,
+        path: this.path,
         dacl,
         uid: body.uid,
         gid: body.gid,
