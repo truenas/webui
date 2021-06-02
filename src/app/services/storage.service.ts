13e16e1375c14cc43d7045e208d6f6fd41f2577d
Rehan Yousaf <rehanyousaf147@gmail.com>
2021-06-03 03:28:26 +0500

    Cleaned TaskService and StorageService

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -5,7 +5,7 @@ import { Disk } from 'app/interfaces/storage.interface';
 import { WebSocketService } from './ws.service';
 import { RestService } from './rest.service';
 
-import * as moment from 'moment';
+import { format } from 'date-fns-tz';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
@@ -183,7 +183,7 @@ export class StorageService {
 
       sorter = [];
       for (const elem of timeArr) {
-        sorter.push(moment(elem).format('YYYY-MM-DD HH:mm:ss')); // formate should matched locale service
+        sorter.push(format(elem, 'yyyy-MM-dd HH:mm:ss')); // formate should matched locale service
       }
     } else {
       sorter = tempArr.sort(myCollator.compare);
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import * as cronParser from 'cron-parser';
-import { Moment } from 'moment';
+import { formatDistanceToNow } from 'date-fns';
 import { Observable } from 'rxjs';
 import cronstrue from 'cronstrue/i18n';
 import { Options as CronOptions } from 'cronstrue/dist/options';
@@ -134,19 +134,19 @@ export class TaskService {
    * @param scheduleExpression A cron expression such as `0 0 * * mon`
    * @param count The desired number of future runs
    */
-  getTaskNextRuns(scheduleExpression: string, count = 10): Moment[] {
+  getTaskNextRuns(scheduleExpression: string, count = 10): Date[] {
     const schedule = cronParser.parseExpression(scheduleExpression, { iterator: true });
 
     /* Nasty type assertions due to type definition error in cron-parser lib */
     return new Array(count)
       .fill(null)
-      .map(() => ((schedule.next() as unknown) as { value: { _date: Moment } }).value._date);
+      .map(() => ((schedule.next() as unknown) as { value: { _date: any } }).value._date.toDate());
   }
 
   getTaskNextRun(scheduleExpression: string): string {
     const schedule = cronParser.parseExpression(scheduleExpression, { iterator: true });
 
-    return ((schedule.next() as unknown) as { value: { _date: Moment } }).value._date.fromNow();
+    return formatDistanceToNow(((schedule.next() as unknown) as { value: { _date: any } }).value._date.toDate(), { addSuffix: true });
   }
 
   getTaskCronDescription(scheduleExpression: string, options: CronOptions = this.cronOptions): string {
