0656d4cd25e1b71b1022e7feba9b132d73dad179
Rehan Yousaf <rehanyousaf147@gmail.com>
2021-06-05 15:20:06 +0500

    Fixed duration and units

--- src/app/pages/reportsdashboard/components/report/report.component.ts
+++ src/app/pages/reportsdashboard/components/report/report.component.ts
@@ -309,7 +309,7 @@ export class ReportComponent extends WidgetComponent implements AfterViewInit, O
 
   // Convert timespan to start/end options for RRDTool
   convertTimespan(timespan: any, direction = 'backward', currentDate?: number): TimeData {
-    let units: string;
+    let durationUnit: keyof Duration;
     let value: number;
 
     const now = this.getServerTime();
@@ -328,42 +328,42 @@ export class ReportComponent extends WidgetComponent implements AfterViewInit, O
 
     switch (timespan) {
       case '5M':
-        units = 'months';
+        durationUnit = 'months';
         value = 5;
         break;
       case '1M':
-        units = 'months';
+        durationUnit = 'months';
         value = 1;
         break;
       case '7d':
-        units = 'days';
+        durationUnit = 'days';
         value = 7;
         break;
       case '24h':
-        units = 'hours';
+        durationUnit = 'hours';
         value = 24;
         break;
       case '60m':
-        units = 'minutes';
+        durationUnit = 'minutes';
         value = 60;
         break;
     }
 
     if (direction == 'backward') {
-      const subOptions: any = {};
-      subOptions[units.toLowerCase()] = value;
+      const subOptions: Duration = {};
+      subOptions[durationUnit] = value;
       startDate = sub(endDate, subOptions);
     } else if (direction == 'forward') {
-      const subOptions: any = {};
-      subOptions[units.toLowerCase()] = value;
+      const subOptions: Duration = {};
+      subOptions[durationUnit] = value;
       endDate = add(startDate, subOptions);
     }
 
     // if endDate is in the future, reset with endDate to now
     if (endDate.getTime() >= now.getTime()) {
       endDate = new Date();
-      const subOptions: any = {};
-      subOptions[units.toLowerCase()] = value;
+      const subOptions: Duration = {};
+      subOptions[durationUnit] = value;
       startDate = sub(endDate, subOptions);
       this.stepForwardDisabled = true;
     } else {
