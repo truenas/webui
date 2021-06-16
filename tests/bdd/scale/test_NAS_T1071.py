e2d490725a91444059a6edfd8040f50ea3f976e3
q5sys <jt@obs-sec.com>
2021-06-16 11:22:52 -0400

    fix test

--- tests/bdd/scale/test_NAS_T1071.py
+++ tests/bdd/scale/test_NAS_T1071.py
@@ -46,13 +46,15 @@ def you_should_be_on_the_dashboard_click_storage_on_the_side_menu(driver):
     assert wait_on_element(driver, 10, '//h1[contains(.,"Dashboard")]')
     assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Storage"]', 'clickable')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Storage"]').click()
-    
-@then('the pools page appears click create pool')
+
+
+@when('the pools page appears click create pool')
 def the_pools_page_appears_click_create_pool(driver):
     """the pools page appears click create pool."""
     assert wait_on_element(driver, 10, '//h1[contains(.,"Storage")]')
-    assert wait_on_element(driver, 10, '//button[@ix-auto="button___POOL_CREATE"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button___POOL_CREATE"]').click()
+    assert wait_on_element(driver, 10, '//a[@ix-auto="button___POOL_CREATE"]', 'clickable')
+    driver.find_element_by_xpath('//a[@ix-auto="button___POOL_CREATE"]').click()
+
 
 @then('the Pool Manager appears, enter the tank for pool name')
 def the_pool_manager_appears_enter_the_tank_for_pool_name(driver):
@@ -65,14 +67,14 @@ def the_pool_manager_appears_enter_the_tank_for_pool_name(driver):
 @then('click sdb1 and sdb22 checkbox, press the right arrow under Data VDevs')
 def click_sdb1_and_sdb22_checkbox_press_the_right_arrow_under_data_vdevs(driver):
     """click sdb1 and sdb22 checkbox, press the right arrow under Data VDevs."""
-    assert wait_on_element(driver, 7, '//mat-checkbox[@name="disk_0_checkbox"]', 'clickable')
-    driver.find_element_by_xpath('//mat-checkbox[@name="disk_0_checkbox"]').click()
-    assert wait_on_element(driver, 7, '//mat-checkbox[@name="disk_1_checkbox"]', 'clickable')
-    driver.find_element_by_xpath('//mat-checkbox[@name="disk_1_checkbox"]').click()
+    assert wait_on_element(driver, 7, '//mat-checkbox[@id="pool-manager__disks-sdb"]', 'clickable')
+    driver.find_element_by_xpath('//mat-checkbox[@id="pool-manager__disks-sdb"]').click()
     assert wait_on_element(driver, 5, '//button[@id="vdev__add-button"]', 'clickable')
     driver.find_element_by_xpath('//button[@id="vdev__add-button"]').click()
-    assert wait_on_element(driver, 5, '//button[@id="pool-manager__create-button"]', 'clickable')
-    driver.find_element_by_xpath('//button[@id="pool-manager__create-button"]').click()
+    assert wait_on_element(driver, 7, '//mat-checkbox[@id="pool-manager__force-submit-checkbox"]', 'clickable')
+    driver.find_element_by_xpath('//mat-checkbox[@id="pool-manager__force-submit-checkbox"]').click()
+
+
 
 @then('click create, On the Warning widget, click confirm checkbox, click CREATE POOL')
 def click_create_on_the_warning_widget_click_confirm_checkbox_click_create_pool(driver):
@@ -80,8 +82,16 @@ def click_create_on_the_warning_widget_click_confirm_checkbox_click_create_pool(
     assert wait_on_element(driver, 10, '//h1[contains(.,"Warning")]')
     assert wait_on_element(driver, 7, '//mat-checkbox[@name="confirm_checkbox"]', 'clickable')
     driver.find_element_by_xpath('//mat-checkbox[@name="confirm_checkbox"]').click()
+    assert wait_on_element(driver, 7, '//button[@ix-auto="button__CONTINUE"]', 'clickable')
+    driver.find_element_by_xpath('//button[@ix-auto="button__CONTINUE"]').click()
+    assert wait_on_element(driver, 5, '//button[@id="pool-manager__create-button"]', 'clickable')
+    driver.find_element_by_xpath('//button[@id="pool-manager__create-button"]').click()
+    assert wait_on_element(driver, 10, '//h1[contains(.,"Warning")]')
+    assert wait_on_element(driver, 7, '//mat-checkbox[@name="confirm_checkbox"]', 'clickable')
+    driver.find_element_by_xpath('//mat-checkbox[@name="confirm_checkbox"]').click()
     assert wait_on_element(driver, 7, '//button[@ix-auto="button__CREATE POOL"]', 'clickable')
     driver.find_element_by_xpath('//button[@ix-auto="button__CREATE POOL"]').click()
+
 
 @then('Create pool should appear while pool is being created')
 def create_pool_should_appear_while_pool_is_being_created(driver):
