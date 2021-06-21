e29a7e03762ae4e54eff12d93dfa79384cba439c
Rehan Yousaf <rehanyousaf147@gmail.com>
2021-06-21 18:38:07 +0500

    Added line breaks for better readability

--- src/app/helptext/storage/volumes/datasets/dataset-form.ts
+++ src/app/helptext/storage/volumes/datasets/dataset-form.ts
@@ -41,14 +41,14 @@ export default {
  space for this dataset.'),
 
   acl_type_change_warning: T('Changes to ACL type affect how on-disk ZFS ACL is \
- written and read. When the ACL type is changed from POSIX to NFSv4, \
+ written and read.\n\nWhen the ACL type is changed from POSIX to NFSv4, \
  no migration is performed for default and access ACLs encoded in the \
- posix1e acl extended attributes to native ZFS ACLs. When ACL type is \
+ posix1e acl extended attributes to native ZFS ACLs.\n\nWhen ACL type is \
  changed from NFSv4 to POSIX, native ZFS ACLs are not converted to \
  posix1e extended attributes, but the native ACL will be used internally \
- by ZFS for access checks. This means that the user must manually set \
+ by ZFS for access checks.\n\nThis means that the user must manually set \
  new ACLs recursively on the dataset after ACL type changes in order to \
- avoid unexpected permissions behavior. This action will be destructive, \
+ avoid unexpected permissions behavior.\n\nThis action will be destructive, \
  and so it is advised to take a ZFS snapshot of the dataset prior to ACL \
  type changes and permissions modifications.'),
 
