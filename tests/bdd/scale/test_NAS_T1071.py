8a7ab5577200577594d3e419c425f4066511454d
q5sys (JT) <jt@obs-sec.com>
2021-06-23 08:25:33 -0400

    add more wait times since test fails randomly

--- tests/bdd/scale/test_NAS_T1071.py
+++ tests/bdd/scale/test_NAS_T1071.py
@@ -72,10 +72,13 @@ def the_pool_manager_appears_enter_the_tank_for_pool_name(driver):
 @then('click sdb checkbox, press the right arrow under Data VDevs')
 def click_sdb_checkbox_press_the_right_arrow_under_data_vdevs(driver):
     """click sdb checkbox, press the right arrow under Data VDevs."""
+    time.sleep(2)
     assert wait_on_element(driver, 7, '//mat-checkbox[@id="pool-manager__disks-sdb"]', 'clickable')
     driver.find_element_by_xpath('//mat-checkbox[@id="pool-manager__disks-sdb"]').click()
+    time.sleep(2)
     assert wait_on_element(driver, 5, '//button[@id="vdev__add-button"]', 'clickable')
     driver.find_element_by_xpath('//button[@id="vdev__add-button"]').click()
+    time.sleep(2)
     assert wait_on_element(driver, 7, '//mat-checkbox[@id="pool-manager__force-submit-checkbox"]', 'clickable')
     driver.find_element_by_xpath('//mat-checkbox[@id="pool-manager__force-submit-checkbox"]').click()
 
