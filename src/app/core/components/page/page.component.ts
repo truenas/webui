5be512afa52b2ba3067dd204cedccad51d975bcb
Evgeny Stepanovych <undsoft@gmail.com>
2021-06-23 10:43:54 +0300

    NAS-111136 - Finnish notation

--- src/app/core/components/page/page.component.ts
+++ src/app/core/components/page/page.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 import { CoreService } from 'app/core/services/core.service';
 import { CoreServiceInjector } from 'app/core/services/coreserviceinjector';
 
-export const PageComponentMetadata = {
-  selector: 'page',
-  template: '',
-};
-
 @Component({
   selector: 'page',
   template: '',
