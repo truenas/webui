6d260f032c1cedd190ec0ce2978a0aadf50b25e8
ericbsd <ericturgeon.bsd@gmail.com>
2021-06-28 16:06:24 -0300

    adding the leftover of the code of test_NAS_T0962.py

--- tests/bdd/ha-bhyve02/test_NAS_T0962.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0962.py
@@ -248,46 +248,77 @@ def on_the_storage_page_click_on_the_dozer_3_dots_button_select_add_dataset(driv
     driver.find_element_by_xpath('//button[normalize-space(text())="Add Dataset"]').click()
 
 
-@then(parsers.parse('on the Add Dataset page, input the dataset name "my_acl_dataset"'))
-def on_the_add_dataset_page_input_the_dataset_name_my_acl_dataset(driver):
+@then(parsers.parse('on the Add Dataset page, input the dataset name "{dataset_name}"'))
+def on_the_add_dataset_page_input_the_dataset_name_my_acl_dataset(driver, dataset_name):
     """on the Add Dataset page, input the dataset name "my_acl_dataset"."""
+    assert wait_on_element(driver, 5, '//h3[text()="Add Dataset"]')
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__Name"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').send_keys(dataset_name)
+    assert wait_on_element(driver, 5, '//mat-select[@ix-auto="select__Share Type"]')
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Share Type"]').click()
+    assert wait_on_element(driver, 5, '//mat-option[@ix-auto="option__Share Type_SMB"]', 'clickable')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Share Type_SMB"]').click()
 
 
-@then(parsers.parse('click Summit the "my_acl_dataset" data should be created'))
-def click_summit_the_my_acl_dataset_data_should_be_created(driver):
+@then(parsers.parse('click Summit the "{dataset_name}" data should be created'))
+def click_summit_the_my_acl_dataset_data_should_be_created(driver, dataset_name):
     """click Summit the "my_acl_dataset" data should be created."""
+    assert wait_on_element(driver, 5, '//button[@ix-auto="button__SAVE"]', 'clickable')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+    assert wait_on_element_disappear(driver, 20, '//h6[contains(.,"Please wait")]')
+    assert wait_on_element(driver, 10, f'//div[contains(text(),"{dataset_name}")]')
 
 
-@then(parsers.parse('click on the "my_acl_dataset" 3 dots button, select Edit Permissions'))
-def click_on_the_my_acl_dataset_3_dots_button_select_edit_permissions(driver):
+@then(parsers.parse('click on the "{dataset_name}" 3 dots button, select Edit Permissions'))
+def click_on_the_my_acl_dataset_3_dots_button_select_edit_permissions(driver, dataset_name):
     """click on the "my_acl_dataset" 3 dots button, select Edit Permissions."""
-
-
-@then('on the Edit Permissions page should open click on Use ACL Manager')
-def on_the_edit_permissions_page_should_open_click_on_use_acl_manager(driver):
-    """on the Edit Permissions page should open click on Use ACL Manager."""
+    assert wait_on_element(driver, 5, f'//tr[contains(.,"{dataset_name}")]//mat-icon[text()="more_vert"]')
+    driver.find_element_by_xpath(f'//tr[contains(.,"{dataset_name}")]//mat-icon[text()="more_vert"]').click()
+    assert wait_on_element(driver, 5, '//button[normalize-space(text())="Edit Permissions"]')
+    driver.find_element_by_xpath('//button[normalize-space(text())="Edit Permissions"]').click()
 
 
 @then('the Edit ACL page should open')
 def the_edit_acl_page_should_open(driver):
     """The Edit ACL page should open."""
+    assert wait_on_element(driver, 5, '//h1[text()="Edit ACL"]')
+    assert wait_on_element(driver, 5, '//h4[text()="File Information"]')
 
 
 @then('select OPEN for Default ACL Option')
 def select_open_for_default_acl_option(driver):
     """Select OPEN for Default ACL Option."""
+    assert wait_on_element(driver, 5, '//button[@id="cust_button_Select an ACL Preset"]', 'clickable')
+    driver.find_element_by_xpath('//button[@id="cust_button_Select an ACL Preset"]').click()
+    assert wait_on_element(driver, 5, '//mat-select[@ix-auto="select__Default ACL Options"]', 'clickable')
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Default ACL Options"]').click()
+    assert wait_on_element(driver, 5, '//mat-option[@ix-auto="option__Default ACL Options_NFS4_OPEN"]', 'clickable')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Default ACL Options_NFS4_OPEN"]').click()
+    assert wait_on_element(driver, 5, '//button[@ix-auto="button__CONTINUE"]', 'clickable')
+    driver.find_element_by_xpath('//button[@ix-auto="button__CONTINUE"]').click()
 
 
 @then(parsers.parse('select "{group_name}" for Group name, check the Apply Group'))
 def select_group_name_for_group_name_check_the_apply_group(driver, group_name):
     """select "{group_name}" for Group name, check the Apply Group."""
+    assert wait_on_element(driver, 5, '//mat-checkbox[@ix-auto="checkbox__Apply Group"]/label/div', 'clickable')
+    driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__Apply Group"]/label/div').click()
+    assert wait_on_element(driver, 5, '//input[@data-placeholder="Group"]')
+    driver.find_element_by_xpath('//input[@data-placeholder="Group"]').clear()
+    driver.find_element_by_xpath('//input[@data-placeholder="Group"]').send_keys('AD01\\administrator')
 
 
 @then('click the Save button, which should be returned to the storage page')
 def click_the_save_button_which_should_be_returned_to_the_storage_page(driver):
     """click the Save button, which should be returned to the storage page."""
+    assert wait_on_element(driver, 5, '//button[@ix-auto="button__SAVE"]', 'clickable')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+    assert wait_on_element(driver, 10, '//div[contains(text(),"my_acl_dataset")]')
 
 
 @then(parsers.parse('on the Edit ACL page, verify that the group name is "{group_name}"'))
 def on_the_edit_acl_page_verify_that_the_group_name_is_group_name(driver, group_name):
     """on the Edit ACL page, verify that the group name is "group_name"."""
+    assert wait_on_element(driver, 5, '//input[@data-placeholder="Group"]')
+    assert attribute_value_exist(driver, '//input[@data-placeholder="Group"]', 'value', group_name)
