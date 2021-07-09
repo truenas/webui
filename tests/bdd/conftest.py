35a3a4d3d95733f88746f78456fb3835d241e54f
ericbsd <ericturgeon.bsd@gmail.com>
2021-07-09 13:59:56 -0300

    adding code to escape if click CLOSE failes in conftest.py

--- tests/bdd/conftest.py
+++ tests/bdd/conftest.py
@@ -10,6 +10,7 @@ from platform import system
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import (
+    ElementClickInterceptedException,
     NoSuchElementException,
     TimeoutException
 )
@@ -121,8 +122,13 @@ def pytest_runtest_makereport(item):
                 traceback_name = f'screenshot/{report.nodeid.replace("::", "_")}.txt'
                 save_traceback(traceback_name)
                 save_screenshot(screenshot_error_name)
-                ActionChains(web_driver).send_keys(Keys.ESCAPE).perform()
-                # web_driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
+                # Press CLOSE if exist only if there is an error box.
+                time.sleep(1)
+                try:
+                    web_driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
+                except ElementClickInterceptedException:
+                    # if can't click Close ESCAPE
+                    ActionChains(web_driver).send_keys(Keys.ESCAPE).perform()
             save_screenshot(screenshot_name)
             # To make sure we are not stuck on a combobox to stop other test to fail
             if element_exist('//mat-option'):
