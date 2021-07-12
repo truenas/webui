13959127bf3a82a9351e2b4c0ecbcc88d64cd140
Rehan Yousaf <rehanyousaf147@gmail.com>
2021-07-12 22:45:34 +0500

    Removed done and fixed error catching

--- src/app/services/validation.service.spec.ts
+++ src/app/services/validation.service.spec.ts
@@ -16,59 +16,48 @@ describe('ValidationService', () => {
     });
   });
 
-  it('should have greatherThan error when value1 is smaller', (done) => {
+  it('should have greatherThan error when value1 is smaller', () => {
     formGroup.get(formControlName).setValue(0);
     formGroup.get(otherFormControlName).setValue(1);
     formGroup.updateValueAndValidity();
     expect(formGroup.valid).toBeFalsy();
     expect(formGroup.get(formControlName).errors.greaterThan).toBeTruthy();
     expect(formGroup.get(formControlName).errors.fields[0]).toBe(formControlPlaceholder);
-    done();
   });
 
-  it('should have greatherThan error when value1 is equal', (done) => {
+  it('should have greatherThan error when value1 is equal', () => {
     formGroup.get(formControlName).setValue(0);
     formGroup.get(otherFormControlName).setValue(0);
     formGroup.updateValueAndValidity();
     expect(formGroup.valid).toBeFalsy();
     expect(formGroup.get(formControlName).errors.greaterThan).toBeTruthy();
     expect(formGroup.get(formControlName).errors.fields[0]).toBe(formControlPlaceholder);
-    done();
   });
 
-  it('should not have greatherThan error when value1 is greater', (done) => {
+  it('should not have greatherThan error when value1 is greater', () => {
     formGroup.get(formControlName).setValue(1);
     formGroup.get(otherFormControlName).setValue(0);
     formGroup.updateValueAndValidity();
     expect(formGroup.valid).toBeTruthy();
     expect(formGroup.get(formControlName).errors).toBeFalsy();
-    done();
   });
 
-  it('should not have greaterThan error when control has no parent formGroup', (done) => {
+  it('should not have greaterThan error when control has no parent formGroup', () => {
     const value1FormControl = new FormControl(0, [
       service.greaterThan(otherFormControlName, [formControlPlaceholder]),
     ]);
     expect(value1FormControl.errors).toBeFalsy();
-    done();
   });
 
-  it('should throw error if value2 doesnt exist in formGroup', (done) => {
+  it('should throw error if value2 doesnt exist in formGroup', () => {
     formGroup = new FormGroup({
       value1: new FormControl(0, [
         service.greaterThan(otherFormControlName, [formControlPlaceholder]),
       ]),
     });
 
-    let customError: Error = null;
-    try {
-      formGroup.get(formControlName).updateValueAndValidity();
-    } catch (error) {
-      customError = error;
-    }
     expect(() => {
-      throw customError;
+      formGroup.get(formControlName).updateValueAndValidity();
     }).toThrow('greaterThanValidator(): other control is not found in parent group');
-    done();
   });
 });
