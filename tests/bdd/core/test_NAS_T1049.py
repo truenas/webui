4d5a6a5900f0d490b70923f2927e24daf19f0317
ericbsd <ericturgeon.bsd@gmail.com>
2021-07-14 15:34:37 -0300

    change the verification the the ixsystems url in test_NAS_T1049.py

--- tests/bdd/core/test_NAS_T1049.py
+++ tests/bdd/core/test_NAS_T1049.py
@@ -76,7 +76,7 @@ def verify_the_company_name_go_to_wwwixsystemscom(driver):
     time.sleep(1)
     driver.switch_to.window(driver.window_handles[1])
     assert wait_on_element(driver, 7, '//img[contains(@title,"ix_logo")]')
-    assert driver.current_url == 'https://www.ixsystems.com/', driver.current_url
+    assert 'www.ixsystems.com' in driver.current_url, driver.current_url
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
