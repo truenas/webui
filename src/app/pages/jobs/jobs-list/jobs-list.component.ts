028520cab33c7dbc6719f45057bd045debeb5e56
Denys Butenko <ua13dark@gmail.com>
2021-07-27 13:03:57 +0300

    Add filters and pagination

--- src/app/pages/jobs/jobs-list/jobs-list.component.ts
+++ src/app/pages/jobs/jobs-list/jobs-list.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import _ from 'lodash';
 import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { JobsManagerStore } from 'app/components/common/dialog/jobs-manager/jobs-manager.store';
 import { JobState } from 'app/enums/job-state.enum';
+import { ApiEvent } from 'app/interfaces/api-event.interface';
 import { Job } from 'app/interfaces/job.interface';
 import { QueryParams } from 'app/interfaces/query-api.interface';
+import { EntityUtils } from 'app/pages/common/entity/utils';
 import { JobRow } from 'app/pages/jobs/jobs-list/job-row.interface';
 import { DialogService } from 'app/services';
 import { LocaleService } from 'app/services/locale.service';
+import { WebSocketService } from 'app/services/ws.service';
 import { T } from 'app/translate-marker';
-import { WebSocketService } from '../../../services/ws.service';
+import { EmptyConfig, EmptyType } from '../../common/entity/entity-empty/entity-empty.component';
+
+export enum JobFilterState {
+  All = 'All',
+  Running = 'Running',
+  Failed = 'Failed',
+}
 
 @UntilDestroy()
 @Component({
@@ -23,15 +34,38 @@ import { WebSocketService } from '../../../services/ws.service';
 })
 export class JobsListComponent implements OnInit {
   title = this.translate.instant(T('Job Log'));
+  paginationPageIndex = 0;
+  paginationPageSize = 10;
+  paginationPageSizeOptions: number[] = [10, 50, 100];
+  paginationShowFirstLastButtons = true;
   queryCall: 'core.get_jobs' = 'core.get_jobs';
   queryCallOption: QueryParams<Job> = [[], { order_by: ['-id'] }];
-  @ViewChild('taskTable', { static: true }) taskTable: MatTable<any>;
+  @ViewChild('taskTable', { static: false }) taskTable: MatTable<JobRow[]>;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
-  dataSource: MatTableDataSource<Job>;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  dataSource: MatTableDataSource<JobRow>;
   displayedColumns = ['name', 'state', 'id', 'date_started', 'date_finished', 'logs_excerpt'];
   expandedElement: any | null;
   viewingLogsForJob: Job;
+  filterValue = '';
+  emptyTableConf: EmptyConfig = {
+    type: EmptyType.no_search_results,
+    large: true,
+    title: T('No jobs are available'),
+    message: T('Try reset filters to see all data'),
+    button: {
+      label: T('Show All Jobs'),
+      action: () => {
+        console.info('Reset filters');
+      },
+    },
+  };
   readonly JobState = JobState;
+  readonly JobFilterState = JobFilterState;
+
+  get isTableEmpty(): boolean {
+    return Boolean(this.dataSource.filter ? this.dataSource?.filteredData?.length : this.dataSource?.data?.length);
+  }
 
   constructor(
     private ws: WebSocketService,
@@ -68,45 +102,45 @@ export class JobsListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.ws.call(this.queryCall, this.queryCallOption)
+    this.getData()
       .pipe(untilDestroyed(this))
       .subscribe(
         (jobs) => {
-          for (let i = 0; i < jobs.length; i++) {
-            this.dataSource.data.push(this.transformJob(jobs[i]));
-          }
-
+          this.dataSource.data = jobs;
           this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
         },
-        () => {
-
+        (error) => {
+          new EntityUtils().handleWSError(this, error);
         },
       );
 
-    this.getData().pipe(untilDestroyed(this)).subscribe((job) => {
-      // only update exist jobs or add latest jobs
-      const lastJob = this.dataSource.data[0];
-      if (job.id >= lastJob?.id) {
-        const targetRow = _.findIndex(this.dataSource.data, { id: job.id });
-        if (targetRow === -1) {
-          this.dataSource.data.unshift(this.transformJob(job));
-        } else {
-          for (const key in this.dataSource.data[targetRow]) {
-            (this.dataSource.data[targetRow][key as keyof Job] as any) = job[key];
+    this.getUpdates()
+      .pipe(untilDestroyed(this))
+      .subscribe((job) => {
+        // only update exist jobs or add latest jobs
+        const lastJob = this.dataSource.data[0];
+        if (job.id >= lastJob?.id) {
+          const targetRow = _.findIndex(this.dataSource.data, { id: job.id });
+          const data = this.dataSource.data;
+          if (targetRow === -1) {
+            data.push(job);
+          } else {
+            data[targetRow] = job;
           }
+          this.dataSource.data = data;
         }
-        this.taskTable.renderRows();
-      }
-    });
+      });
   }
 
-  getData(): Observable<any> {
-    const source = Observable.create((observer: any) => {
-      this.ws.subscribe(this.queryCall).pipe(untilDestroyed(this)).subscribe((event) => {
-        observer.next(event.fields);
-      });
-    });
-    return source;
+  getData(): Observable<JobRow[]> {
+    return this.ws
+      .call(this.queryCall, this.queryCallOption)
+      .pipe(map((jobs: Job[]) => jobs.map((job) => this.transformJob(job))));
+  }
+
+  getUpdates(): Observable<JobRow> {
+    return this.ws.subscribe(this.queryCall).pipe(map((event: ApiEvent<Job>) => this.transformJob(event.fields)));
   }
 
   viewLogs(job: JobRow): void {
@@ -115,5 +149,27 @@ export class JobsListComponent implements OnInit {
 
   onLogsSidebarClosed(): void {
     this.viewingLogsForJob = null;
+  }
+
+  searchQuery(query: string): void {
+    if (query?.trim().length > 0) {
+      this.dataSource.filter = query;
+    } else {
+      this.dataSource.filter = '';
+    }
+  }
+
+  onFilterChange(state: MatButtonToggleChange): void {
+    switch (state.value) {
+      case JobFilterState.All:
+        this.dataSource.filter = '';
+        break;
+      case JobFilterState.Running:
+      case JobFilterState.Failed:
+        this.dataSource.filter = state.value;
+        break;
+      default:
+        break;
+    }
   }
 }
