ce173c7838e28f6f8c5d5d8d4695609455fb345d
Evgeny Stepanovych <undsoft@gmail.com>
2021-08-02 15:23:43 +0300

    NAS-111392 - Fix SMART tests page

--- src/app/pages/storage/disks/smart-results/smart-results.component.ts
+++ src/app/pages/storage/disks/smart-results/smart-results.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
+import { QueryParams } from 'app/interfaces/query-api.interface';
+import { SmartTestResults } from 'app/interfaces/smart-test.interface';
 import { EntityTableConfig } from 'app/pages/common/entity/entity-table/entity-table.interface';
 import { T } from 'app/translate-marker';
 
@@ -13,7 +15,14 @@ import { T } from 'app/translate-marker';
 export class SmartResultsComponent implements EntityTableConfig {
   title: string;
   queryCall: 'smart.test.results' = 'smart.test.results';
-  queryCallOption: any = [];
+  queryCallOption: QueryParams<SmartTestResults> = [];
+
+  emptyTableConfigMessages = {
+    no_page_data: {
+      title: this.translate.instant('No SMART test results'),
+      message: this.translate.instant('No SMART tests have been performed on this disk yet.'),
+    },
+  };
 
   columns = [
     { name: T('ID'), prop: 'num', always_display: true },
@@ -28,24 +37,23 @@ export class SmartResultsComponent implements EntityTableConfig {
     sorting: { columns: this.columns },
   };
   noActions = true;
+  noAdd = true;
 
   protected disk: string;
 
-  constructor(private aroute: ActivatedRoute, protected translate: TranslateService) { }
+  constructor(private aroute: ActivatedRoute, private translate: TranslateService) { }
 
   preInit(): void {
     this.aroute.params.pipe(untilDestroyed(this)).subscribe((params) => {
       this.disk = params['pk'];
-      this.translate.get(T('S.M.A.R.T Test Results of ')).pipe(untilDestroyed(this)).subscribe(
-        (res) => {
-          this.title = res + this.disk;
-        },
-      );
+      this.title = this.translate.instant('S.M.A.R.T Test Results of {disk}', {
+        disk: this.disk,
+      });
       this.queryCallOption = [[['disk', '=', this.disk]]];
     });
   }
 
-  resourceTransformIncomingRestData(data: any): any {
-    return data.tests || [];
+  resourceTransformIncomingRestData(data: SmartTestResults[]): SmartTestResults['tests'] {
+    return data[0]?.tests || [];
   }
 }
