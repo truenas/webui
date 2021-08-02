8eb0b41df4c5b299820c5fa941c9a8e060b2d5cd
Damian Szidiropulosz <damianos@knoxbug.org>
2021-08-02 11:51:13 -0400

    Fix app-bootenv-rename

--- src/app/pages/system/bootenv/bootenv-rename/bootenv-rename.component.ts
+++ src/app/pages/system/bootenv/bootenv-rename/bootenv-rename.component.ts
@@ -5,14 +5,14 @@ import { Observable } from 'rxjs';
 import { helptext_system_bootenv } from 'app/helptext/system/boot-env';
 import { FormConfiguration } from 'app/interfaces/entity-form.interface';
 import { EntityFormComponent } from 'app/pages/common/entity/entity-form';
-import { FieldConfig } from 'app/pages/common/entity/entity-form/models/field-config.interface';
+import { FieldSet } from 'app/pages/common/entity/entity-form/models/fieldset.interface';
 import { regexValidator } from 'app/pages/common/entity/entity-form/validators/regex-validation';
 import { BootEnvService, WebSocketService } from 'app/services';
 
 @UntilDestroy()
 @Component({
   selector: 'app-bootenv-rename',
-  template: '<entity-form [conf]="this"></entity-form>',
+  template: "<entity-form [conf]='this'></entity-form>",
   providers: [BootEnvService],
 })
 export class BootEnvironmentRenameComponent implements FormConfiguration {
@@ -23,19 +23,12 @@ export class BootEnvironmentRenameComponent implements FormConfiguration {
   isEntity = true;
   protected entityForm: EntityFormComponent;
 
-  fieldConfig: FieldConfig[];
-
-  constructor(
-    protected router: Router,
-    protected route: ActivatedRoute,
-    protected ws: WebSocketService,
-    protected bootEnvService: BootEnvService,
-  ) {}
-
-  preInit(entityForm: EntityFormComponent): void {
-    this.route.params.pipe(untilDestroyed(this)).subscribe((params) => {
-      this.pk = params['pk'];
-      this.fieldConfig = [
+  fieldSets: FieldSet[] = [
+    {
+      name: helptext_system_bootenv.rename_fieldset_name,
+      class: 'bootenv-rename',
+      label: false,
+      config: [
         {
           type: 'input',
           name: 'name',
@@ -44,12 +37,24 @@ export class BootEnvironmentRenameComponent implements FormConfiguration {
           validation: [regexValidator(this.bootEnvService.bootenv_name_regex)],
           required: true,
         },
-      ];
-    });
-    this.entityForm = entityForm;
-  }
+      ],
+    },
+  ];
+
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
+    protected ws: WebSocketService,
+    protected bootEnvService: BootEnvService,
+  ) {}
 
   afterInit(entityForm: EntityFormComponent): void {
+    this.route.params.pipe(untilDestroyed(this)).subscribe((params) => {
+      this.pk = params['pk'];
+      entityForm.formGroup.get('name').setValue(this.pk);
+    });
+    this.entityForm = entityForm;
+
     entityForm.submitFunction = this.submitFunction;
   }
 
