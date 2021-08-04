53b6c890193eb42c7c50d6363e834985c8477fe4
akarpov <karpov4you@gmail.com>
2021-08-05 02:41:39 +0700

    remove unused text

--- src/app/helptext/apps/apps.ts
+++ src/app/helptext/apps/apps.ts
@@ -168,13 +168,7 @@ export default {
 
     upgrade_dialog: {
       title: T('Upgrade'),
-      currentVersion: T('Current Version'),
-      versionToUpgrade: T('Version to be upgarded'),
-      images: T('Images(to be updated)'),
-      noImageUpdate: T('No image available for upgrading to this version'),
-      noChangeLogMsg: T('No changelog available for upgrading to this version'),
       job: T('Upgrading...'),
-      changeLog: T('Changelog'),
     },
 
     rollback_dialog: {
