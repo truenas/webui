93b9a8eda24a6a9718039c249b85fc65e19c6b7d
ericbsd <ericturgeon.bsd@gmail.com>
2021-08-27 17:10:01 -0300

    Gave more time for the UI to load in test_last_NAS_T1048.py

--- tests/bdd/core/test_last_NAS_T1048.py
+++ tests/bdd/core/test_last_NAS_T1048.py
@@ -126,8 +126,9 @@ def reboot_the_system_wait_for_login_and_login(driver):
 def on_the_dashboard_click_on_system_on_the_side_menu_click_on_boot(driver):
     """on the Dashboard, click on System on the side menu, click on Boot."""
     assert wait_on_element(driver, 10, '//li[contains(.,"Dashboard")]')
-    assert wait_on_element(driver, 10, '//span[contains(.,"System Information")]')
-    assert wait_on_element(driver, 7, '//mat-list-item[@ix-auto="option__System"]', 'clickable')
+    # Gave more time for the UI to load.
+    assert wait_on_element(driver, 30, '//span[contains(.,"System Information")]')
+    assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__System"]', 'clickable')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__System"]').click()
     assert wait_on_element(driver, 7, '//mat-list-item[@ix-auto="option__Boot"]')
     element = driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__System"]')
