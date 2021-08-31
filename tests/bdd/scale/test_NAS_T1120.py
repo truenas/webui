7a84d7d5d85d9cb20496a519596c1315f513a770
q5sys <jt@obs-sec.com>
2021-08-31 09:03:01 -0400

    update test

--- tests/bdd/scale/test_NAS_T1120.py
+++ tests/bdd/scale/test_NAS_T1120.py
@@ -14,6 +14,7 @@ from pytest_bdd import (
     scenario,
     then,
     when,
+    parsers
 )
 
 
@@ -41,75 +42,81 @@ def the_browser_is_open_the_freenas_url_and_logged_in(driver, nas_ip, root_passw
 
 
 @when('you should be on the dashboard')
-def you_should_be_on_the_dashboard():
+def you_should_be_on_the_dashboard(driver):
     """you should be on the dashboard."""
     assert wait_on_element(driver, 10, '//span[contains(.,"Dashboard")]')
     assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
 
 
-@then('click Storage on the side menu and click on the "{dataset_name}" 3 dots button, select Edit Permissions')
-def click_storage_on_the_side_menu_and_click_on_the_dataset_napme_3_dots_button_select_edit_permission(driver, dataset_name):
+@then('click Storage on the side menu and click on the "tank_acl_dataset" 3 dots button, select Edit Permissions')
+def click_storage_on_the_side_menu_and_click_on_the_tank_acl_dataset_3_dots_button_select_edit_permissions(driver):
+    """click Storage on the side menu and click on the "tank_acl_dataset" 3 dots button, select Edit Permissions."""
     time.sleep(1)
     assert wait_on_element(driver, 10, '//h1[contains(.,"Dashboard")]')
     assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Storage"]', 'clickable')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Storage"]').click()
     time.sleep(1)
     assert wait_on_element(driver, 10, '//h1[contains(.,"Storage")]')
-    assert wait_on_element(driver, 5, f'//tr[contains(.,"{dataset_name}")]//mat-icon[text()="more_vert"]')
-    driver.find_element_by_xpath(f'//tr[contains(.,"{dataset_name}")]//mat-icon[text()="more_vert"]').click()
-    assert wait_on_element(driver, 5, '//button[normalize-space(text())="Edit Permissions"]')
-    driver.find_element_by_xpath('//button[normalize-space(text())="Edit Permissions"]').click()
+    driver.find_element_by_xpath('//tr[contains(.,"tank_acl_dataset")]//mat-icon[text()="more_vert"]').click()
+    assert wait_on_element(driver, 5, '//button[normalize-space(text())="View Permissions"]')
+    driver.find_element_by_xpath('//button[normalize-space(text())="View Permissions"]').click()
+    time.sleep(2)
+    assert wait_on_element(driver, 5, '//mat-icon[normalize-space(text())="edit"]')
+    driver.find_element_by_xpath('//mat-icon[normalize-space(text())="edit"]').click()
 
 
 @then('the Edit ACL page should open')
 def the_edit_acl_page_should_open():
     """the Edit ACL page should open."""
-    if wait_on_element(driver, 2, '//button[@ix-auto="button__USE ACL MANAGER"]'):
-        driver.find_element_by_xpath('//button[@ix-auto="button__USE ACL MANAGER"]').click()
-    assert wait_on_element(driver, 5, '//h4[contains(.,"File Information")]')
+    time.sleep(2)
+    assert wait_on_element(driver, 5, '//h1[text()="Edit ACL"]')
 
 
 @then('click on Add ACL Item, click on select User, User input should appear, enter "{input}" and select "{user}"')
 def click_on_add_acl_item_click_on_select_user_user_input_should_appear_enter_eric_and_select_ericbsd(driver, input, user):
     """click on Add ACL Item, click on select User, User input should appear, enter "{input}" and select "{user}"."""
     time.sleep(1)
-    assert wait_on_element(driver, 5, '//button[@ix-auto="button__ADD ACL ITEM"]')
-    driver.find_element_by_xpath('//button[@ix-auto="button__ADD ACL ITEM"]').click()
+    assert wait_on_element(driver, 5, '//button[normalize-space(text())="Add Item"]')
+    driver.find_element_by_xpath('//button[normalize-space(text())="Add Item"]').click()
     time.sleep(1)
-    assert wait_on_element(driver, 5, '//mat-select[@ix-auto="select__Who"]/div/div/span[contains(.,"Who")]')
+
+    assert wait_on_element(driver, 5, '//mat-select[@ix-auto="select__Who"]/div/div/span[contains(.,"User")]')
     time.sleep(1)
-    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Who"]/div/div/span[contains(.,"Who")]').click()
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Who"]/div/div/span[contains(.,"User")]').click()
     assert wait_on_element(driver, 5, '//mat-option[@ix-auto="option__Who_User"]')
     driver.find_element_by_xpath('//mat-option[@ix-auto="option__Who_User"]').click()
     time.sleep(1)
     assert wait_on_element(driver, 5, '(//input[@placeholder="User"])[2]')
     time.sleep(1)
-    driver.find_element_by_xpath('(//input[@placeholder="User"])[2]').send_keys(input)
+    driver.find_element_by_xpath('(//div[@ix-auto="combobox__User"//input[@placeholder="User"])[2]').send_keys(input)
     assert wait_on_element(driver, 5, f'//mat-option[@ix-auto="option__{user}"]')
     driver.find_element_by_xpath(f'//mat-option[@ix-auto="option__{user}"]').click()
 
 
 @then('click the Save button, return to the Pools page, click on the "tank_acl_dataset" 3 dots button, select Edit Permissions')
-def click_the_save_button_return_to_the_pools_page_click_on_the_tank_acl_dataset_3_dots_button_select_edit_permissions():
+def click_the_save_button_return_to_the_pools_page_click_on_the_tank_acl_dataset_3_dots_button_select_edit_permissions(driver):
     """click the Save button, return to the Pools page, click on the "tank_acl_dataset" 3 dots button, select Edit Permissions."""
     time.sleep(1)
-    assert wait_on_element(driver, 5, '//button[@ix-auto="button__SAVE"]')
-    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
-    assert wait_on_element(driver, 5, '//mat-panel-title[contains(.,"system")]')
-    driver.find_element_by_xpath('//td[@ix-auto="system_name"]')
-    assert wait_on_element(driver, 5, '//mat-icon[@id="actions_menu_button__my_acl_dataset"]')
-    driver.find_element_by_xpath('//mat-icon[@id="actions_menu_button__my_acl_dataset"]').click()
+    assert wait_on_element(driver, 5, '//button//span[contains(text(),"Save Access Control List"])')
+    driver.find_element_by_xpath('//button//span[contains(text(),"Save Access Control List"])').click()
+    time.sleep(2)
+    assert wait_on_element(driver, 5, '//mat-panel-title[contains(.,"tank")]')
+    assert wait_on_element(driver, 5, '//mat-icon[@id="actions_menu_button__tank_acl_dataset"]')
+    driver.find_element_by_xpath('//mat-icon[@id="actions_menu_button__tank_acl_dataset"]').click()
     assert wait_on_element(driver, 5, '//button[@ix-auto="action__my_acl_dataset_Edit Permissions"]')
     driver.find_element_by_xpath('//button[@ix-auto="action__my_acl_dataset_Edit Permissions"]').click()
 
+    driver.find_element_by_xpath('//tr[contains(.,"tank_acl_dataset")]//mat-icon[text()="more_vert"]').click()
+    assert wait_on_element(driver, 5, '//button[normalize-space(text())="View Permissions"]')
+    driver.find_element_by_xpath('//button[normalize-space(text())="View Permissions"]').click()
 
-@then(parsers.parse('the Edit ACL page should open, verify the new ACL item for user "{user}" exists'))
-def the_edit_acl_page_should_open_verify_the_new_acl_item_for_user_ericbsd_exists(driver, user):
-    """the Edit ACL page should open, verify the new ACL item for user "{user}" exists."""
-    assert wait_on_element(driver, 5, '//h4[contains(.,"File Information")]')
-    assert wait_on_element(driver, 5, '(//input[@placeholder="User"])[2]')
-    assert attribute_value_exist(driver, '(//input[@placeholder="User"])[2]', 'value', user)
+
+@then('the Edit ACL page should open, verify the new ACL item for user ericbsd exists')
+def the_edit_acl_page_should_open_verify_the_new_acl_item_for_user_ericbsd_exists(driver):
+    """the Edit ACL page should open, verify the new ACL item for user ericbsd exists."""
+    time.sleep(2)
+    assert wait_on_element(driver, 5, '//div[contains(text(),"User - ericbsd")]')
     ## return to dashboard
     assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
