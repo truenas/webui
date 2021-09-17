d1bd1a904905be97a683cbc8ecfe0aadbe3e9bdc
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-09-17 11:12:31 +0200

    Review feedback

--- src/app/pages/applications/chart-releases/chart-releases.component.ts
+++ src/app/pages/applications/chart-releases/chart-releases.component.ts
@@ -282,9 +282,13 @@ export class ChartReleasesComponent implements OnInit {
     });
   }
 
+  portalName(name: string = "web_portal"): string {
+    const humanName = new EntityUtils().snakeToHuman(name);
+    return humanName
+  }
 
   portalLink(chart: ChartRelease, name: string = "web_portal"): void {
-  window.open(chart.portals[name][0]);
+    window.open(chart.portals[name][0]);
   }
 
   update(name: string): void {
