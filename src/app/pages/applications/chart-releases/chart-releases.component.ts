d07cc80ef9d774900acb088ec45818e7ad8494b7
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-09-17 11:22:51 +0200

    linter output

--- src/app/pages/applications/chart-releases/chart-releases.component.ts
+++ src/app/pages/applications/chart-releases/chart-releases.component.ts
@@ -282,12 +282,12 @@ export class ChartReleasesComponent implements OnInit {
     });
   }
 
-  portalName(name: string = "web_portal"): string {
+  portalName(name: string = 'web_portal'): string {
     const humanName = new EntityUtils().snakeToHuman(name);
-    return humanName
+    return humanName;
   }
 
-  portalLink(chart: ChartRelease, name: string = "web_portal"): void {
+  portalLink(chart: ChartRelease, name: string = 'web_portal'): void {
     window.open(chart.portals[name][0]);
   }
 
--- src/app/pages/common/entity/utils.ts
+++ src/app/pages/common/entity/utils.ts
@@ -514,7 +514,7 @@ export class EntityUtils {
   snakeToPascal(str: string): string {
     return str.split('_').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('');
   }
-  
+
   snakeToHuman(str: string): string {
     return str.split('_').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
   }
