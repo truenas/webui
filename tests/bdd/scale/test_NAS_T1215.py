356853e26becb4bda0b47c35c9e061cf7f4649e3
q5sys <jt@obs-sec.com>
2021-09-20 17:40:33 -0400

    change path

--- tests/bdd/scale/test_NAS_T1215.py
+++ tests/bdd/scale/test_NAS_T1215.py
@@ -93,7 +93,7 @@ def decode_the_tabfile_with_tabfile_string(driver, tabfile_string):
     datafile = open(os.path.expanduser(tabfile_path), 'rb').read()
     tab_result = word_xor(datafile, tabfile_string)
     time.sleep(2)
-    open('KEYTABNAME.KEYTAB','wb').write(tab_result)
+    open('/KEYTABNAME.KEYTAB','wb').write(tab_result)
 
 
 @then('name the keytab and upload the file and click save')
