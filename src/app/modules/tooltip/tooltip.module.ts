afb693ce4d370eedf14d604344d5dab3a3170ae2
Evgeny Stepanovych <undsoft@gmail.com>
2021-09-22 13:31:29 +0300

    NAS-111964: Enabling fullTemplateTypeCheck

--- src/app/modules/tooltip/tooltip.module.ts
+++ src/app/modules/tooltip/tooltip.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgxPopperjsModule } from 'ngx-popperjs';
+import { MaterialModule } from 'app/app-material.module';
 import { CommonDirectivesModule } from 'app/directives/common/common-directives.module';
 import { TooltipDocReplacePipe } from 'app/modules/tooltip/tooltip-docreplace';
 import { TooltipComponent } from 'app/modules/tooltip/tooltip.component';
@@ -12,6 +13,7 @@ import { TooltipComponent } from 'app/modules/tooltip/tooltip.component';
     NgxPopperjsModule,
     TranslateModule.forChild(),
     CommonDirectivesModule,
+    MaterialModule,
   ],
   declarations: [
     TooltipComponent,
