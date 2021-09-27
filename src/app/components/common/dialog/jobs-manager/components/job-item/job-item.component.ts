db36dc59858c5adf5768f5b5d1833c3149722611
Denys Butenko <ua13dark@gmail.com>
2021-09-27 22:20:46 +0300

    NAS-111467: Cleanup

--- src/app/components/common/dialog/jobs-manager/components/job-item/job-item.component.ts
+++ src/app/components/common/dialog/jobs-manager/components/job-item/job-item.component.ts
@@ -1,7 +1,6 @@
 import {
   Component, Input, Output, EventEmitter,
 } from '@angular/core';
-import { FormatDateTimePipe } from 'app/core/components/pipes/format-datetime.pipe';
 import { JobState } from 'app/enums/job-state.enum';
 import { Job } from 'app/interfaces/job.interface';
 
@@ -9,7 +8,6 @@ import { Job } from 'app/interfaces/job.interface';
   selector: 'app-job-item',
   templateUrl: './job-item.component.html',
   styleUrls: ['./job-item.component.scss'],
-  providers: [FormatDateTimePipe],
 })
 export class JobItemComponent {
   @Input() job: Job;
