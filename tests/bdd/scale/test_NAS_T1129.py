415a8967fec496e132194d9f495bdb1df61597bb
q5sys <jt@obs-sec.com>
2021-09-28 10:40:04 -0400

    get test passing locally

--- tests/bdd/scale/test_NAS_T1129.py
+++ tests/bdd/scale/test_NAS_T1129.py
@@ -8,7 +8,9 @@ from function import (
     wait_on_element,
     is_element_present,
     attribute_value_exist,
+    wait_on_element_disappear,
     run_cmd,
+    ssh_cmd,
     post
 )
 from pytest_bdd import (
@@ -16,6 +18,7 @@ from pytest_bdd import (
     scenario,
     then,
     when,
+    parsers
 )
 
 
@@ -39,54 +42,60 @@ def the_browser_is_open_the_freenas_url_and_logged_in(driver, nas_ip, root_passw
         assert wait_on_element(driver, 5, '//button[@name="signin_button"]')
         driver.find_element_by_xpath('//button[@name="signin_button"]').click()
     else:
+        assert wait_on_element(driver, 5, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
         driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
 
 
 @when('you should be on the dashboard, click on sharing and click add.')
-def you_should_be_on_the_dashboard_click_on_sharing_and_click_add():
+def you_should_be_on_the_dashboard_click_on_sharing_and_click_add(driver):
     """you should be on the dashboard, click on sharing and click add.."""
     assert wait_on_element(driver, 10, '//span[contains(.,"Dashboard")]')
-    assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
-    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').clickWindows (SMB) Shares()
-    time.sleep(2)
     assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Shares"]', 'clickable')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Shares"]').click()
-    time.sleep(2)
     assert wait_on_element(driver, 7, '//mat-card[contains(.,"Windows (SMB) Shares")]//button[contains(.,"Add")]', 'clickable')
     driver.find_element_by_xpath('//mat-card[contains(.,"Windows (SMB) Shares")]//button[contains(.,"Add")]').click()
 
 
-@then('set Path to the LDAP dataset at "{path}", input "{name}" as name, click enable, input "{description}" as the description, and click Summit')
-def set_path_to_the_ldap_dataset_at_mnttankmy_ldap_dataset_input_ldapsmbshare_as_name_click_enable_input_my_ldap_smb_test_share_as_the_description_and_click_summit(driver, name, path, description):
-    """set Path to the LDAP dataset at /mnt/tank/my_ldap_dataset, input ldapsmbshare as name, click enable, input "My LDAP smb test share" as the description, and click Summit."""
+@then(parsers.parse('set Path to the LDAP dataset at "{path}"'))
+def set_path_to_the_ldap_dataset_at_mnttankmy_ldap_dataset(driver, path):
+    """set Path to the LDAP dataset at "/mnt/tank/my_ldap_dataset"."""
     time.sleep(2)
     assert wait_on_element(driver, 5, '//h3[contains(.,"Add SMB")]')
     assert wait_on_element(driver, 5, '//input[@ix-auto="input__path"]')
     driver.find_element_by_xpath('//input[@ix-auto="input__path"]').clear()
     driver.find_element_by_xpath('//input[@ix-auto="input__path"]').send_keys(path)
+
+
+@then(parsers.parse('input "{name}" as name and click enable'))
+def input_ldapsmbshare_as_name_and_click_enable(driver, name):
+    """input "ldapsmbshare" as name and click enable."""
     assert wait_on_element(driver, 5, '//input[@ix-auto="input__Name"]')
     driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').clear()
     driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').send_keys(name)
+
+
+@then(parsers.parse('input "{description}" as the description and click Summit'))
+def input_my_ldap_smb_test_share_as_the_description_and_click_summit(driver, description):
+    """input "My LDAP smb test share" as the description and click Summit."""
     assert wait_on_element(driver, 5, '//input[@ix-auto="input__Description"]')
     driver.find_element_by_xpath('//input[@ix-auto="input__Description"]').clear()
     driver.find_element_by_xpath('//input[@ix-auto="input__Description"]').send_keys(description)
+    checkbox_checked = attribute_value_exist(driver, '//mat-checkbox[@ix-auto="checkbox__Enabled"]', 'class', 'mat-checkbox-checked')
     if not checkbox_checked:
         driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__Enabled"]').click()
-    assert attribute_value_exist(driver, '//mat-checkbox[@ix-auto="checkbox__Enabled"]', 'class', 'mat-checkbox-checked')
     time.sleep(1)
     assert wait_on_element(driver, 5, '//button[@ix-auto="button__SAVE"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
 
 
+@then('the ldapsmbshare should be added to the Shares list')
+def the_ldapsmbshare_should_be_added_to_the_shares_list(driver):
+    """the ldapsmbshare should be added to the Shares list."""
+    assert wait_on_element(driver, 5, f'//div[contains(.,"ldapsmbshare")]')
 
-@then('the ldapsmbshare should be added to the Shares list, click on systemsettings/services, if the SMB service is not started, start the service.')
-def the_ldapsmbshare_should_be_added_to_the_shares_list_click_on_systemsettingsservices_if_the_smb_service_is_not_started_start_the_service():
-    """the ldapsmbshare should be added to the Shares list, click on systemsettings/services, if the SMB service is not started, start the service.."""
-    raise NotImplementedError
 
-
-@then('send a file to the share with ip/{ldapsmbshare} and "{ldap_user}" and "{ldap_password}"')
-def send_a_file_to_the_share_with_ipldapsmbshare_and_eturgeon_and_need_4_testing(ldapsmbshare, ldap_user, ldap_password):
+@then(parsers.parse('send a file to the share with ip/"{ldapsmbshare}" and "{ldap_user}" and "{ldap_password}"'))
+def send_a_file_to_the_share_with_ipldapsmbshare_and_eturgeon_and_need_4_testing(ldapsmbshare, ldap_user, ldap_password, nas_ip):
     """send a file to the share with ip/ldapsmbshare and "eturgeon" and "Need_4_testing"."""
     run_cmd('touch testfile.txt')
     results = run_cmd(f'smbclient //{nas_ip}/{ldapsmbshare} -W AD01 -U {ldap_user}%{ldap_password} -c "put testfile.txt testfile.txt"')
@@ -95,30 +104,27 @@ def send_a_file_to_the_share_with_ipldapsmbshare_and_eturgeon_and_need_4_testing
 
 
 @then('verify that the file is on the NAS dataset')
-def verify_that_the_file_is_on_the_nas_dataset(driver, nas_ip, root_password,):
+def verify_that_the_file_is_on_the_nas_dataset(driver, nas_ip, root_password):
     """verify that the file is on the NAS dataset."""
-    results = post(nas_ip, 'filesystem/stat/', ('root', root_password), f'{smb_path}/testfile.txt')
-    assert results.status_code == 200, results.text
+    global results
+    cmd = 'ls -la /mnt/tank/my_ldap_dataset/'
+    results = ssh_cmd(cmd, 'root', root_password, nas_ip)
+    assert results['result'], results['output']
+    assert 'testfile' in results['output'], results['output']
 
 
 
 @then('click on Credentials/DirectoryServices, then LDAP Settings, then disable and click SAVE')
-def click_on_credentialsdirectoryservices_then_ldap_settings_then_disable_and_click_save():
+def click_on_credentialsdirectoryservices_then_ldap_settings_then_disable_and_click_save(driver):
     """click on Credentials/DirectoryServices, then LDAP Settings, then disable and click SAVE."""
     assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Credentials"]', 'clickable')
     driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Credentials"]').click()
-    time.sleep(2)
-    assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Directory Services"]', 'clickable')
-    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Directory Services"]').click()
-    time.sleep(1)
+    assert wait_on_element(driver, 10, '//*[contains(@class,"lidein-nav-md")]//mat-list-item[@ix-auto="option__Directory Services"]', 'clickable')
+    driver.find_element_by_xpath('//*[contains(@class,"lidein-nav-md")]//mat-list-item[@ix-auto="option__Directory Services"]').click()
     assert wait_on_element(driver, 7, '//mat-card[contains(.,"LDAP")]//button[contains(.,"Settings")]', 'clickable')
     driver.find_element_by_xpath('//mat-card[contains(.,"LDAP")]//button[contains(.,"Settings")]').click()
-    time.sleep(2)
+    assert wait_on_element(driver, 7, '//mat-checkbox[@ix-auto="checkbox__Enable"]', 'clickable')
     driver.find_element_by_xpath('//mat-checkbox[@ix-auto="checkbox__Enable"]').click()
-    time.sleep(1)
-    wait_on_element(driver, 10, '//button[@ix-auto="button"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button"]').click()
-    ## return to dashboard
-    assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
-    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
-    time.sleep(1)
+    wait_on_element(driver, 10, '//span[contains(text(),"Save")]', 'clickable')
+    driver.find_element_by_xpath('//span[contains(text(),"Save")]').click()
+    assert wait_on_element_disappear(driver, 60, '//h6[contains(.,"Please wait")]')
