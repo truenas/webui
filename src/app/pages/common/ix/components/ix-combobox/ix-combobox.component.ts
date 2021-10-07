6677bf1f5a559618f165a9ead81560c0a9f537d2
Rehan Yousaf <rehanyousaf147@gmail.com>
2021-10-07 13:44:02 +0500

    NAS-112309: Better IxCombobox NAS-112309

--- src/app/pages/common/ix/components/ix-combobox/ix-combobox.component.ts
+++ src/app/pages/common/ix/components/ix-combobox/ix-combobox.component.ts
@@ -1,9 +1,16 @@
 import {
-  Component, ElementRef, forwardRef, Input, OnChanges, SimpleChanges, ViewChild,
+  Component, ElementRef, EventEmitter, forwardRef, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild,
 } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MatAutocomplete, MatAutocompleteTrigger } from '@angular/material/autocomplete';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { Observable, of } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import {
+  fromEvent, Observable, of, Subject,
+} from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, map, takeUntil,
+} from 'rxjs/operators';
 import { Option } from 'app/interfaces/option.interface';
 
 @UntilDestroy()
@@ -19,13 +26,18 @@ import { Option } from 'app/interfaces/option.interface';
     },
   ],
 })
-export class IxComboboxComponent implements ControlValueAccessor, OnChanges {
+export class IxComboboxComponent implements ControlValueAccessor, OnChanges, OnInit {
   @Input() label: string;
   @Input() hint: string;
   @Input() required: boolean;
   @Input() tooltip: string;
   @Input() options: Observable<Option[]>;
   @ViewChild('ixInput') inputElementRef: ElementRef;
+  @ViewChild('auto') autoCompleteRef: MatAutocomplete;
+  @ViewChild(MatAutocompleteTrigger) autocompleteTrigger: MatAutocompleteTrigger;
+  @Output() scrollEnd: EventEmitter<string> = new EventEmitter<string>();
+  placeholder = this.translate.instant('Search');
+  getDisplayWith = this.displayWith.bind(this);
 
   @Input() filter: (options: Option[], filterValue: string) => Observable<Option[]> =
   (options: Option[], value: string): Observable<Option[]> => {
@@ -37,7 +49,7 @@ export class IxComboboxComponent implements ControlValueAccessor, OnChanges {
   };
 
   filteredOptions: Observable<Option[]>;
-
+  filterChanged$ = new Subject<string>();
   formControl = new FormControl(this);
   value: string | number = '';
   filterValue = '';
@@ -48,28 +60,67 @@ export class IxComboboxComponent implements ControlValueAccessor, OnChanges {
   onChange: (value: string | number) => void = (): void => {};
   onTouch: () => void = (): void => {};
 
+  constructor(private translate: TranslateService) {}
+
   writeValue(value: string | number): void {
     this.value = value;
     if (this.value && this.syncOptions) {
       this.selectedOption = { ...(this.syncOptions.find((option: Option) => option.value === this.value)) };
     }
     if (this.selectedOption) {
-      this.filterValue = this.selectedOption.label;
+      this.filterChanged$.next('');
     }
+  }
+
+  ngOnInit(): void {
+    this.filterChanged$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      untilDestroyed(this),
+    ).subscribe((changedValue: string) => {
+      this.filterValue = changedValue;
+      if (changedValue) {
+        this.filteredOptions = this.filter(this.syncOptions, changedValue);
+      } else {
+        this.filteredOptions = of(this.syncOptions);
+      }
+    });
+  }
+
+  onOpenDropdown(): void {
+    setTimeout(() => {
+      if (
+        this.autoCompleteRef
+        && this.autocompleteTrigger
+        && this.autoCompleteRef.panel
+      ) {
+        fromEvent(this.autoCompleteRef.panel.nativeElement, 'scroll')
+          .pipe(
+            debounceTime(300),
+            map(() => this.autoCompleteRef.panel.nativeElement.scrollTop),
+            takeUntil(this.autocompleteTrigger.panelClosingActions),
+            untilDestroyed(this),
+          )
+          .subscribe(() => {
+            const { scrollTop, scrollHeight, clientHeight: elementHeight } = this.autoCompleteRef.panel.nativeElement;
+            const atBottom = scrollHeight === scrollTop + elementHeight;
+            if (atBottom) {
+              this.scrollEnd.emit(this.filterValue);
+            }
+          });
+      }
+    });
   }
 
   onChanged(changedValue: string): void {
-    this.filterValue = changedValue;
-    if (changedValue) {
-      this.filteredOptions = this.filter(this.syncOptions, changedValue);
-    } else {
-      this.filteredOptions = of(this.syncOptions);
-    }
+    this.filterChanged$.next(changedValue);
   }
 
   resetInput(): void {
-    this.filterValue = '';
-    this.inputElementRef.nativeElement.value = '';
+    this.filterChanged$.next('');
+    if (this.inputElementRef && this.inputElementRef.nativeElement) {
+      this.inputElementRef.nativeElement.value = '';
+    }
     this.selectedOption = null;
     this.onChange('');
   }
@@ -84,7 +135,7 @@ export class IxComboboxComponent implements ControlValueAccessor, OnChanges {
 
   optionSelected(option: Option): void {
     this.selectedOption = { ...option };
-    this.filterValue = this.selectedOption.label;
+    this.filterChanged$.next('');
     this.onChange(this.selectedOption.value);
   }
 
@@ -98,9 +149,10 @@ export class IxComboboxComponent implements ControlValueAccessor, OnChanges {
         changes.options.currentValue.pipe(untilDestroyed(this)).subscribe((options: Option[]) => {
           this.syncOptions = options;
           this.filteredOptions = of(options);
-          this.selectedOption = { ...(this.syncOptions.find((option: Option) => option.value === this.value)) };
+          const setOption = this.syncOptions.find((option: Option) => option.value === this.value);
+          this.selectedOption = setOption ? { ...setOption } : null;
           if (this.selectedOption) {
-            this.filterValue = this.selectedOption.label;
+            this.filterChanged$.next('');
           }
         });
       }
@@ -112,6 +164,6 @@ export class IxComboboxComponent implements ControlValueAccessor, OnChanges {
   }
 
   hasValue(): boolean {
-    return this.filterValue && this.filterValue.length > 0;
+    return this.inputElementRef?.nativeElement?.value && this.inputElementRef.nativeElement.value.length > 0;
   }
 }
