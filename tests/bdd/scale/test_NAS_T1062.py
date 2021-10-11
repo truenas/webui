415ec11145b800b37971940dbec09095dadc7efe
q5sys <jt@obs-sec.com>
2021-10-11 09:47:27 -0400

    add wait_on_element for spinner

--- tests/bdd/scale/test_NAS_T1062.py
+++ tests/bdd/scale/test_NAS_T1062.py
@@ -120,6 +120,7 @@ def click_the_start_automatically_ssh_checkbox_and_enable_the_ssh_service(driver
 @then('the service should be enabled with no errors')
 def the_service_should_be_enabled_with_no_errors(driver):
     """the service should be enabled with no errors."""
+    assert wait_on_element(driver, 30, '//mat-spinner[@role="progressbar"]')
     wait_on_element_disappear(driver, 30, '//mat-spinner[@role="progressbar"]')
     assert wait_for_attribute_value(driver, 20, '//mat-slide-toggle[@ix-auto="slider__state__SSH"]', 'class', 'mat-checked')
 
