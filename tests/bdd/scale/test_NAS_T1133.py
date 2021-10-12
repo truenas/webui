d9c3e6280af2c38df12d5a86f80a28a069c213bd
q5sys <jt@obs-sec.com>
2021-10-12 15:01:59 -0400

    fix xpath

--- tests/bdd/scale/test_NAS_T1133.py
+++ tests/bdd/scale/test_NAS_T1133.py
@@ -146,11 +146,11 @@ def set_path_to_the_ldap_dataset_mnttankwheel_dataset_input_wheelsmbshare_as_nam
     assert wait_on_element_disappear(driver, 15, '//h6[contains(.,"Please wait")]')
     
 
-@then('parsers.parse(''smb should be added')
+@then('parsers.parse(''mb should be added')
 def test_wheel_smb_share_should_be_added(driver):
     """"{smbname}" should be added."""
-    assert wait_on_element(driver, 5, '//div[contains(.,"Windows (SMB) Shares")]')
-    assert wait_on_element(driver, 5, f'//div[contains(.,"test wheel SMB share")]')
+    assert wait_on_element(driver, 5, '//a[contains(.,"Windows (SMB) Shares")]')
+    assert wait_on_element(driver, 5, '//div[contains(.,"test wheel SMB share")]')
     
 
 @then(parsers.parse('Send a file to the share with nas_ip/"{wheelshare}" and "{user}" and "{password}"'))
