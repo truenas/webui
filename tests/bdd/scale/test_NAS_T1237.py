fca199ae8dc6170731712770e87f57044ef4003b
q5sys <jt@obs-sec.com>
2021-10-26 10:28:48 -0400

    add wait

--- tests/bdd/scale/test_NAS_T1237.py
+++ tests/bdd/scale/test_NAS_T1237.py
@@ -38,6 +38,7 @@ def the_browser_is_open_the_freenas_url_and_logged_in(driver, nas_ip, root_passw
         assert wait_on_element(driver, 5, '//button[@name="signin_button"]')
         driver.find_element_by_xpath('//button[@name="signin_button"]').click()
     else:
+        assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
         driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
 
 
