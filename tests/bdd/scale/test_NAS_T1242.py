a6d8bd71bff3d9b339ee77eef37ab8d367e345ef
q5sys <jt@obs-sec.com>
2021-11-02 09:12:07 -0400

    fix missing driver values

--- tests/bdd/scale/test_NAS_T1242.py
+++ tests/bdd/scale/test_NAS_T1242.py
@@ -23,7 +23,7 @@ def test_verify_an_encrypted_pool_can_be_created():
 
 
 @given('the browser is open, the FreeNAS URL and logged in')
-def the_browser_is_open_the_freenas_url_and_logged_in():
+def the_browser_is_open_the_freenas_url_and_logged_in(driver, nas_ip, root_password):
     """the browser is open, the FreeNAS URL and logged in."""
     if nas_ip not in driver.current_url:
         driver.get(f"http://{nas_ip}")
@@ -41,7 +41,7 @@ def the_browser_is_open_the_freenas_url_and_logged_in():
 
 
 @when('you are on the dashboard, click Storage on the side menu')
-def you_are_on_the_dashboard_click_storage_on_the_side_menu():
+def you_are_on_the_dashboard_click_storage_on_the_side_menu(driver):
     """you are on the dashboard, click Storage on the side menu."""
     assert wait_on_element(driver, 10, '//span[contains(.,"Dashboard")]')
     assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
@@ -52,21 +52,21 @@ def you_are_on_the_dashboard_click_storage_on_the_side_menu():
 
 
 @then('the pools page appears click create pool')
-def the_pools_page_appears_click_create_pool():
+def the_pools_page_appears_click_create_pool(driver):
     """the pools page appears click create pool."""
     assert wait_on_element(driver, 10, '//h1[contains(.,"Storage")]')
     assert wait_on_element(driver, 10, '//a[@ix-auto="button___POOL_CREATE"]', 'clickable')
     driver.find_element_by_xpath('//a[@ix-auto="button___POOL_CREATE"]').click()
 
 @then('the Pool Manager appears, enter encrypted_tank for pool name')
-def the_pool_manager_appears_enter_encrypted_tank_for_pool_name():
+def the_pool_manager_appears_enter_encrypted_tank_for_pool_name(driver):
     """the Pool Manager appears, enter encrypted_tank for pool name."""
     assert wait_on_element(driver, 7, '//div[contains(.,"Pool Manager")]')
     assert wait_on_element(driver, 10, '//input[@id="pool-manager__name-input-field"]', 'inputable')
     driver.find_element_by_xpath('//input[@id="pool-manager__name-input-field"]').send_keys('encrypted_tank')
 
 @then('click encryption and confirm popup')
-def click_encryption_and_confirm_popup():
+def click_encryption_and_confirm_popup(driver):
     """click encryption and confirm popup."""
     assert wait_on_element(driver, 10, '//mat-checkbox[@id="pool-manager__encryption-checkbox"]', 'clickable')
     driver.find_element_by_xpath('//mat-checkbox[@id="pool-manager__encryption-checkbox"]').click()
@@ -77,7 +77,7 @@ def click_encryption_and_confirm_popup():
 
 
 @then('click a drive checkbox and press the right arrow')
-def click_a_drive_checkbox_and_press_the_right_arrow():
+def click_a_drive_checkbox_and_press_the_right_arrow(driver):
     """click a drive checkbox and press the right arrow."""
     assert wait_on_element(driver, 10, '//datatable-body[contains(.,"sd")]//mat-checkbox[1]', 'clickable')
     driver.find_element_by_xpath('//datatable-body[contains(.,"sd")]//mat-checkbox[1]').click()
@@ -89,7 +89,7 @@ def click_a_drive_checkbox_and_press_the_right_arrow():
 
 
 @then('click create confirm the warning checkbox and click CREATE POOL')
-def click_create_confirm_the_warning_checkbox_and_click_create_pool():
+def click_create_confirm_the_warning_checkbox_and_click_create_pool(driver):
     """click create confirm the warning checkbox and click CREATE POOL."""
     assert wait_on_element(driver, 10, '//h1[contains(.,"Warning")]')
     assert wait_on_element(driver, 7, '//mat-checkbox[@name="confirm_checkbox"]', 'clickable')
@@ -109,6 +109,6 @@ def click_create_confirm_the_warning_checkbox_and_click_create_pool():
     assert wait_on_element(driver, 10, '//h1[contains(.,"Storage")]')
 
 @then('the pool should be listed on the storage page')
-def the_pool_should_be_listed_on_the_storage_page():
+def the_pool_should_be_listed_on_the_storage_page(driver):
     """the pool should be listed on the storage page."""
     assert wait_on_element(driver, 7, '//div[contains(.,"encrypted_tank")]')
