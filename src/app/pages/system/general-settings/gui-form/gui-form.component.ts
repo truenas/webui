d64eb66d2a2fea8da9cbca1ecd367d54b172eb6c
Rehan Yousaf <rehanyousaf147@gmail.com>
2021-11-02 16:28:17 +0500

    Revert "NAS-112556: Added temp field"
    This reverts commit 84f66978c3fa300a666dfb0d51b89aef7f963a4b.

--- src/app/pages/system/general-settings/gui-form/gui-form.component.ts
+++ src/app/pages/system/general-settings/gui-form/gui-form.component.ts
@@ -16,7 +16,6 @@ import { SystemGeneralConfig, SystemGeneralConfigUpdate } from 'app/interfaces/s
 import { WebsocketError } from 'app/interfaces/websocket-error.interface';
 import { ipValidator } from 'app/pages/common/entity/entity-form/validators/ip-validation';
 import { FormErrorHandlerService } from 'app/pages/common/ix-forms/services/form-error-handler.service';
-import { IxFormatterService } from 'app/pages/common/ix-forms/services/ix-formatter.service';
 import {
   DialogService, SystemGeneralService, WebSocketService,
 } from 'app/services';
@@ -44,7 +43,6 @@ export class GuiFormComponent {
     crash_reporting: [false, [Validators.required]],
     usage_collection: [false, [Validators.required]],
     ui_consolemsg: [false, [Validators.required]],
-    memory_size: ['2 mib'],
   });
 
   options = {
@@ -66,7 +64,6 @@ export class GuiFormComponent {
     private loader: AppLoaderService,
     private translate: TranslateService,
     private errorHandler: FormErrorHandlerService,
-    private ixFormatter: IxFormatterService,
   ) {
     this.sysGeneralService.getGeneralConfig$.pipe(
       untilDestroyed(this),
@@ -88,9 +85,6 @@ export class GuiFormComponent {
       this.cdr.markForCheck();
     });
   }
-
-  format = this.ixFormatter.memorySizeFormatting;
-  parse = this.ixFormatter.memorySizeParsing;
 
   reconnect(href: string): void {
     if (this.ws.connected) {
