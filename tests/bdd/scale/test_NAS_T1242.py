0c87015658560fc2cbfe8950a6861dc8da7129e5
q5sys <jt@obs-sec.com>
2021-11-11 11:04:39 -0500

    increase wait time

--- tests/bdd/scale/test_NAS_T1242.py
+++ tests/bdd/scale/test_NAS_T1242.py
@@ -104,7 +104,7 @@ def click_create_confirm_the_warning_checkbox_and_click_create_pool(driver):
     assert wait_on_element(driver, 7, '//button[@ix-auto="button__CREATE POOL"]', 'clickable')
     driver.find_element_by_xpath('//button[@ix-auto="button__CREATE POOL"]').click()
     assert wait_on_element_disappear(driver, 60, '//h6[contains(.,"Create Pool")]')
-    assert wait_on_element(driver, 7, '//button[contains(text(),"Done")]', 'clickable')
+    assert wait_on_element(driver, 30, '//button[contains(text(),"Done")]', 'clickable')
     driver.find_element_by_xpath('//button[contains(text(),"Done")]').click()
     assert wait_on_element(driver, 10, '//h1[contains(.,"Storage")]')
 
