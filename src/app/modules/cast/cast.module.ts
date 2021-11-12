d49064b966c1cceb84a8dfa40566f57bbbd2b136
Evgeny Stepanovych <undsoft@gmail.com>
2021-11-12 14:59:24 +0200

    NAS-113319: Enabling strict templates

--- src/app/modules/cast/cast.module.ts
+++ src/app/modules/cast/cast.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CastPipe } from 'app/modules/cast/cast.pipe';
 
 /**
- * TODO: This should really be in core or common module once
- * core module is cleaned up.
+ * TODO: This should really be in core or common module once core module is cleaned up.
  */
 @NgModule({
   declarations: [
--- src/app/modules/cast/cast.pipe.ts
+++ src/app/modules/cast/cast.pipe.ts
@@ -3,13 +3,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'cast' })
 export class CastPipe implements PipeTransform {
   /**
-   * Cast (S: SuperType) into (T: Type) using generics.
-   * @param value (S: SuperType) obtained from input type.
-   * @optional @param type (T CastingType)
-   * @returns (T: Type) obtained by optional @param type OR assignment type.
+   * @usage
+   * Use this to cast types in templates similarly to as.
+   * Picks up type automatically:
+   * [formControl]="abstractControl | cast"
    */
-  // eslint-disable-next-line unused-imports/no-unused-vars
-  transform<S, T extends S>(value: S, type?: { new (): T }): T {
+  transform<S, T extends S>(value: S): T {
     return value as T;
   }
 }
