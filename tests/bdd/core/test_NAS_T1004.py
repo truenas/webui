694fc4e212ffdb15f1a9957bff3ad200b1fca4c1
ericbsd <ericturgeon.bsd@gmail.com>
2021-11-17 11:04:15 -0400

    Fixed Group combobox input xpath in test_NAS_T1004.py

--- tests/bdd/core/test_NAS_T1004.py
+++ tests/bdd/core/test_NAS_T1004.py
@@ -139,9 +139,9 @@ def select_open_for_default_acl_option_select_group_name_for_group_name(driver, 
     assert wait_on_element(driver, 7, '//button[@ix-auto="button__CONTINUE"]')
     driver.find_element_by_xpath('//button[@ix-auto="button__CONTINUE"]').click()
     assert wait_on_element(driver, 7, '//input[@placeholder="Group"]')
-    driver.find_element_by_xpath('//input[@placeholder="Group"]').clear()
-    driver.find_element_by_xpath('//input[@placeholder="Group"]').send_keys(group_name)
-    assert wait_on_element(driver, 7, f'//mat-option[@ix-auto="option__{group_name}"]')
+    driver.find_element_by_xpath('//div[contains(.,"Group")]/input[contains(@class,"ng-touched")]').clear()
+    driver.find_element_by_xpath('//div[contains(.,"Group")]/input[contains(@class,"ng-touched")]').send_keys(group_name)
+    assert wait_on_element(driver, 7, f'//button[@ix-auto="option__{group_name}"]')
     ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
 
@@ -164,5 +164,5 @@ def you_should_return_to_the_pool_page(driver):
 @then(parsers.parse('Verify that the group name is "{group_name}"'))
 def verify_that_the_group_name_is_ad01administrator(driver, group_name):
     """Verify that the group name is "group_name"."""
-    assert wait_on_element(driver, 7, '//input[@placeholder="Group"]')
-    assert wait_for_attribute_value(driver, 5, '//input[@placeholder="Group"]', 'value', group_name)
+    assert wait_on_element(driver, 7, '//div[contains(.,"Group")]/input[contains(@class,"ng-touched")]')
+    assert wait_for_attribute_value(driver, 5, '//div[contains(.,"Group")]/input[contains(@class,"ng-touched")]', 'value', group_name)
