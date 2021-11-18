d716708f0315e0412b31de3aa2cf39271620dfc4
AlexKarpov <karpov4you@gmail.com>
2021-11-18 11:27:07 +0200

    NAS-113377: Refactor Rsync form to new ix forms

--- src/app/pages/services/components/service-rsync/rsync-configuration/configure-rsync/configure-rsync.component.ts
+++ src/app/pages/services/components/service-rsync/rsync-configuration/configure-rsync/configure-rsync.component.ts
@@ -1,45 +1,83 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef, Component, OnInit,
+} from '@angular/core';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@ngneat/reactive-forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import helptext from 'app/helptext/services/components/service-rsync';
-import { FormConfiguration } from 'app/interfaces/entity-form.interface';
 import { RsyncConfigUpdate } from 'app/interfaces/rsync-config.interface';
-import { EntityFormComponent } from 'app/pages/common/entity/entity-form/entity-form.component';
-import { FieldConfig } from 'app/pages/common/entity/entity-form/models/field-config.interface';
-import { WebSocketService } from 'app/services/ws.service';
+import { EntityUtils } from 'app/pages/common/entity/utils';
+import { FormErrorHandlerService } from 'app/pages/common/ix-forms/services/form-error-handler.service';
+import { WebSocketService, DialogService, AppLoaderService } from 'app/services';
 
+@UntilDestroy()
 @Component({
   selector: 'app-configure-rsync',
-  template: '<entity-form [conf]="this"></entity-form>',
+  templateUrl: './configure-rsync.component.html',
+  styleUrls: ['./configure-rsync.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CconfigureRYSNCComponent implements FormConfiguration {
-  queryCall = 'rsyncd.config' as const;
-  title = helptext.configureFormTitle;
+export class CconfigureRYSNCComponent implements OnInit {
+  form = this.fb.group({
+    port: [Number(helptext.rsyncd_port_value), Validators.pattern('^[0-9]*$')],
+    auxiliary: [''],
+  });
 
-  route_success: string[] = ['services'];
+  port = {
+    fcName: 'port',
+    label: helptext.rsyncd_port_placeholder,
+    tooltip: helptext.rsyncd_port_tooltip,
+  };
 
-  fieldConfig: FieldConfig[] = [
-    {
-      type: 'input',
-      name: 'port',
-      placeholder: helptext.rsyncd_port_placeholder,
-      tooltip: helptext.rsyncd_port_tooltip,
-      value: helptext.rsyncd_port_value,
-    },
-    {
-      type: 'textarea',
-      name: 'auxiliary',
-      placeholder: helptext.rsyncd_auxiliary_placeholder,
-      tooltip: helptext.rsyncd_auxiliary_tooltip,
-    },
-  ];
+  auxiliary = {
+    fcName: 'auxiliary',
+    label: helptext.rsyncd_auxiliary_placeholder,
+    tooltip: helptext.rsyncd_auxiliary_tooltip,
+  };
 
   constructor(
+    private fb: FormBuilder,
     protected router: Router,
-    protected route: ActivatedRoute,
+    private loader: AppLoaderService,
+    private dialogService: DialogService,
     protected ws: WebSocketService,
+    private cdr: ChangeDetectorRef,
+    private errorHandler: FormErrorHandlerService,
   ) {}
 
-  afterInit(entityEdit: EntityFormComponent): void {
-    entityEdit.submitFunction = (body: RsyncConfigUpdate) => this.ws.call('rsyncd.update', [body]);
+  ngOnInit(): void {
+    this.loader.open();
+
+    this.ws.call('rsyncd.config').pipe(untilDestroyed(this)).subscribe(
+      (config: RsyncConfigUpdate) => {
+        this.form.patchValue(config);
+        this.loader.close();
+        this.cdr.markForCheck();
+      },
+      (error) => {
+        this.loader.close();
+        new EntityUtils().handleWsError(null, error, this.dialogService);
+      },
+    );
+  }
+
+  onSubmit(): void {
+    this.loader.open();
+
+    const values = this.form.value;
+    this.ws.call('rsyncd.update', [values] as [RsyncConfigUpdate]).pipe(untilDestroyed(this)).subscribe(() => {
+      this.loader.close();
+      this.router.navigate(['services']);
+    }, (error) => {
+      this.loader.close();
+      this.errorHandler.handleWsFormError(error, this.form);
+      this.cdr.markForCheck();
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['services']);
   }
 }
