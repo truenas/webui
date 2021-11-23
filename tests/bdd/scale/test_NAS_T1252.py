17c7ca46df61a5daf23f725dce26ce0955749984
q5sys <jt@obs-sec.com>
2021-11-23 13:13:13 -0500

    add t1252

---
+++ tests/bdd/scale/test_NAS_T1252.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+"""SCALE UI: feature tests."""
+
+import time
+from function import (
+    wait_on_element,
+    is_element_present,
+    attribute_value_exist,
+    wait_for_attribute_value,
+    wait_on_element_disappear,
+)
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
+)
+
+
+@scenario('features/NAS-T1252.feature', 'Verify that you can create a new group')
+def test_verify_that_you_can_create_a_new_group():
+    """Verify that you can create a new group."""
+
+
+@given('the browser is open, navigate to the SCALE URL, and login')
+def the_browser_is_open_navigate_to_the_scale_url_and_login(driver, nas_ip, root_password):
+    """the browser is open, navigate to the SCALE URL, and login."""
+    if nas_ip not in driver.current_url:
+        driver.get(f"http://{nas_ip}")
+        assert wait_on_element(driver, 10, '//input[@data-placeholder="Username"]')
+    if not is_element_present(driver, '//mat-list-item[@ix-auto="option__Dashboard"]'):
+        assert wait_on_element(driver, 10, '//input[@data-placeholder="Username"]')
+        driver.find_element_by_xpath('//input[@data-placeholder="Username"]').clear()
+        driver.find_element_by_xpath('//input[@data-placeholder="Username"]').send_keys('root')
+        driver.find_element_by_xpath('//input[@data-placeholder="Password"]').clear()
+        driver.find_element_by_xpath('//input[@data-placeholder="Password"]').send_keys(root_password)
+        assert wait_on_element(driver, 5, '//button[@name="signin_button"]')
+        driver.find_element_by_xpath('//button[@name="signin_button"]').click()
+    else:
+        driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
+
+
+@when('on the dashboard click on Credentials and Local Groups')
+def on_the_dashboard_click_on_credentials_and_local_groups(driver):
+    """on the dashboard click on Credentials and Local Groups."""
+    assert wait_on_element(driver, 10, '//span[contains(.,"Dashboard")]')
+    assert wait_on_element(driver, 10, '//mat-list-item[@ix-auto="option__Credentials"]', 'clickable')
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Credentials"]').click()
+    time.sleep(1)
+    assert wait_on_element(driver, 10, '//*[contains(@class,"lidein-nav-md")]//mat-list-item[@ix-auto="option__Local Groups"]', 'clickable')
+    driver.find_element_by_xpath('//*[contains(@class,"lidein-nav-md")]//mat-list-item[@ix-auto="option__Local Groups"]').click()
+
+
+@then('on the Groups page expand QE group and click edit')
+def on_the_groups_page_expand_qe_group_and_click_edit(driver):
+    """on the Groups page expand QE group and click edit."""
+    assert wait_on_element(driver, 10, '//h1[contains(text(),"Groups")]')
+    assert wait_on_element(driver, 10, '//tr[@ix-auto="expander__qetest"]/td', 'clickable')
+    driver.find_element_by_xpath('//tr[@ix-auto="expander__qetest"]/td').click()
+    assert wait_on_element(driver, 7, '//button[@ix-auto="button__EDIT_qetest_qetest"]', 'clickable')
+    driver.find_element_by_xpath('//button[@ix-auto="button__EDIT_qetest_qetest"]').click()
+
+
+@then('change the group name from qetest to qatest and click save')
+def change_the_group_name_from_qetest_to_qatest_and_click_save(driver):
+    """change the group name from qetest to qatest and click save."""
+    assert wait_on_element(driver, 10, '//h3[contains(text(),"Edit Group")]')
+    assert wait_on_element(driver, 7, '//ix-input[@formcontrolname="name"]//input')
+    driver.find_element_by_xpath('//ix-input[@formcontrolname="name"]//input').clear()
+    driver.find_element_by_xpath('//ix-input[@formcontrolname="name"]//input').send_keys('qatest')
+    assert wait_on_element(driver, 7, '//span[contains(text(),"Save")]', 'clickable')
+    driver.find_element_by_xpath('//span[contains(text(),"Save")]').click()
+
+
+@then('verify that the group name shows as qatest')
+def verify_that_the_group_name_shows_as_qatest(driver):
+    """verify that the group name shows as qatest."""
+    assert wait_on_element_disappear(driver, 20, '//h6[contains(.,"Please wait")]')
+    assert wait_on_element(driver, 10, '//h1[contains(.,"Groups")]')
+    assert wait_on_element(driver, 10, '//div[contains(.,"qatest")]')
