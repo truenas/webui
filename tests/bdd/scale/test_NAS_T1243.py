8938a97bdaab1d1043c66bacfff47c95c3649725
q5sys <jt@obs-sec.com>
2021-12-01 11:42:53 -0500

    update button for t1243

--- tests/bdd/scale/test_NAS_T1243.py
+++ tests/bdd/scale/test_NAS_T1243.py
@@ -146,7 +146,7 @@ def unlock_the_pool(driver):
     driver.find_element_by_xpath('//button[@ix-auto="button__CONTINUE"]').click()
 
     assert wait_on_element(driver, 10, '//p[contains(.,"These datasets were successfully unlocked.")]')
-    assert wait_on_element(driver, 5, '//button[@ix-auto="button__CONTINUE"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__CONTINUE"]').click()
+    assert wait_on_element(driver, 5, '//button[@ix-auto="button__CLOSE"]', 'clickable')
+    driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
 
     assert wait_on_element(driver, 10, '//mat-icon[@fonticon="mdi-lock-open-variant"]')
