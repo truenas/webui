c94e88f0676736d88900b0982e0ec464a8d89bd3
AlexKarpov <karpov4you@gmail.com>
2021-12-08 11:42:52 +0200

    NAS-113651: Redirect + open slide-in form

--- src/app/pages/data-protection/cloudsync/cloudsync-form/cloudsync-form.component.ts
+++ src/app/pages/data-protection/cloudsync/cloudsync-form/cloudsync-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { TreeNode } from '@circlon/angular-tree-component';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
@@ -804,7 +804,8 @@ export class CloudsyncFormComponent implements FormConfiguration {
                   buttonMsg: this.translate.instant('Fix Credential'),
                 }).pipe(filter(Boolean), untilDestroyed(this)).subscribe(() => {
                   this.modalService.closeSlideIn();
-                  this.router.navigate(['/', 'credentials', 'backup-credentials']);
+                  const navigationExtras: NavigationExtras = { state: { editCredential: 'cloudcredentials', id: item.id } };
+                  this.router.navigate(['/', 'credentials', 'backup-credentials'], navigationExtras);
                 });
               });
             } else {
