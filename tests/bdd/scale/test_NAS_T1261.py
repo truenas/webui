d86ec8a1bd1e647e21ede3a663c8b1b75e2cee65
q5sys <jt@obs-sec.com>
2021-12-23 09:52:08 -0500

    add test

--- tests/bdd/scale/test_NAS_T1261.py
+++ tests/bdd/scale/test_NAS_T1261.py
@@ -53,43 +53,97 @@ def on_the_dashboard_click_on_credentials_and_certificates(driver):
 
 
 @then('click on Certificate add')
-def click_on_certificate_add():
+def click_on_certificate_add(driver):
     """click on Certificate add."""
-    raise NotImplementedError
+    assert wait_on_element(driver, 7, '//h1[contains(text(),"Certificates")]')
+    assert wait_on_element(driver, 5, '//mat-card[contains(.,"Certificates")]//button[contains(.,"Add")]', 'clickable')
+    driver.find_element_by_xpath('//mat-card[contains(.,"Certificates")]//button[contains(.,"Add")]').click()
 
 
 @then('set name and type and click next')
-def set_name_and_type_and_click_next():
+def set_name_and_type_and_click_next(driver):
     """set name and type and click next."""
-    raise NotImplementedError
+    assert wait_on_element(driver, 10, '//h3[contains(text(),"Add Certificate")]')       
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__Name"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Name"]').send_keys('cert1')
+    assert wait_on_element(driver, 5, '//mat-select[@ix-auto="select__Type"]', 'clickable')
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Type"]').click()
+    assert wait_on_element(driver, 10, '//span[contains(.," Internal Certificate ")]', 'clickable')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Type_Internal Certificate"]').click()
+    assert wait_on_element(driver, 10, '//div[contains(.,"Identifier and Type") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]', 'clickable')
+    driver.find_element_by_xpath('//div[contains(.,"Identifier and Type") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]').click()
 
 
 @then('set cert options and click next')
-def set_cert_options_and_click_next():
+def set_cert_options_and_click_next(driver):
     """set cert options and click next."""
-    raise NotImplementedError
+    assert wait_on_element(driver, 5, '//mat-select[@ix-auto="select__Signing Certificate Authority" and not(contains(@class,"mat-select-disabled"))]', 'clickable')
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Signing Certificate Authority" and not(contains(@class,"mat-select-disabled"))]').click()
+    assert wait_on_element(driver, 10, '//span[contains(.," ca1 ")]', 'clickable')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Signing Certificate Authority_ca1"]').click()
+    assert wait_on_element(driver, 5, '//mat-select[@ix-auto="select__Key Type"]', 'clickable')
+    driver.find_element_by_xpath('//mat-select[@ix-auto="select__Key Type"]').click()
+    assert wait_on_element(driver, 10, '//span[contains(.,"RSA")]', 'clickable')
+    driver.find_element_by_xpath('//mat-option[@ix-auto="option__Key Type_RSA"]').click()
+    assert wait_on_element(driver, 10, '//div[contains(.,"Certificate Options") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]', 'clickable')
+    driver.find_element_by_xpath('//div[contains(.,"Certificate Options") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]').click()  
+  
 
 
 @then('set cert subject and click next')
-def set_cert_subject_and_click_next():
+def set_cert_subject_and_click_next(driver):
     """set cert subject and click next."""
-    raise NotImplementedError
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__State"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__State"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__State"]').send_keys('TN')
+
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__Locality"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Locality"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Locality"]').send_keys('Maryville')
+
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__Organization"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Organization"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Organization"]').send_keys('iXsystems')
+
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__Organizational Unit"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Organizational Unit"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Organizational Unit"]').send_keys('QE')
+
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__Email"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').send_keys('qa@ixsystems.com')
+
+    assert wait_on_element(driver, 5, '//input[@ix-auto="input__Common Name"]', 'inputable')
+    driver.find_element_by_xpath('//input[@ix-auto="input__Common Name"]').clear()
+    driver.find_element_by_xpath('//input[@ix-auto="input__Common Name"]').send_keys('qe.ixsystems.com')
+
+    assert wait_on_element(driver, 5, '//mat-chip-list[@ix-auto="input__Subject Alternate Names"]', 'inputable')
+    driver.find_element_by_xpath('//input[@placeholder="Subject Alternate Names"]').clear()
+    driver.find_element_by_xpath('//input[@placeholder="Subject Alternate Names"]').send_keys('qa.ixsystems.com')
+
+    assert wait_on_element(driver, 10, '//div[contains(.,"Certificate Subject") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]', 'clickable')
+    driver.find_element_by_xpath('//div[contains(.,"Certificate Subject") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]').click()
 
 
 @then('set extra constraints and click next')
-def set_extra_constraints_and_click_next():
+def set_extra_constraints_and_click_next(driver):
     """set extra constraints and click next."""
-    raise NotImplementedError
+    # May add later for more indepth testing
+    assert wait_on_element(driver, 10, '//div[contains(.,"Extra Constraints") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]', 'clickable')
+    driver.find_element_by_xpath('//div[contains(.,"Extra Constraints") and contains(@class,"mat-step")]//button[@ix-auto="button__NEXT"]').click()
 
 
 @then('click save on the confirm options page')
-def click_save_on_the_confirm_options_page():
+def click_save_on_the_confirm_options_page(driver):
     """click save on the confirm options page."""
-    raise NotImplementedError
+    assert wait_on_element(driver, 10, '//button[@ix-auto="button__SAVE"]', 'clickable')
+    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+    assert wait_on_element(driver, 5, '/*[contains(.,"Creating Certificate")]')
+    assert wait_on_element_disappear(driver, 10, '//*[contains(.,"Creating Certificate")]')
 
 
 @then('verify that the Cert was added')
-def verify_that_the_cert_was_added():
+def verify_that_the_cert_was_added(driver):
     """verify that the Cert was added."""
-    raise NotImplementedError
-
+    assert wait_on_element(driver, 10, '//li[contains(.,"Name: cert1")]')
