7f8896a1709a637d2de5edb34f0ea88a4daaa093
ericbsd <ericturgeon.bsd@gmail.com>
2022-01-05 14:54:33 -0400

    Changed the wait for second node to 4 minutes in test_NAS_T0905.py

--- tests/bdd/ha-bhyve02/test_NAS_T0905.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0905.py
@@ -408,7 +408,7 @@ def navigate_to_dashboard_and_verify_that_both_controllers_show(driver):
     assert wait_on_element(driver, 7, '//span[contains(.,"System Information")]')
     # need to wait for all controller to be online.
     assert wait_on_element(driver, 60, '//div[contains(.,"tn-bhyve03-nodea")]')
-    assert wait_on_element(driver, 180, '//div[contains(.,"tn-bhyve03-nodeb")]')
+    assert wait_on_element(driver, 240, '//div[contains(.,"tn-bhyve03-nodeb")]')
     assert wait_on_element(driver, 60, '//mat-icon[@svgicon="ha_enabled"]')
 
 
