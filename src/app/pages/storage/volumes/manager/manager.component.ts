d0a6c043b524bb43ae0b275fb030de86ebd2b44d
Denys Butenko <ua13dark@gmail.com>
2022-01-13 12:23:16 +0200

    NAS-114146: Add support for "duplicate_serial" field

--- src/app/pages/storage/volumes/manager/manager.component.ts
+++ src/app/pages/storage/volumes/manager/manager.component.ts
@@ -142,7 +142,7 @@ export class ManagerComponent implements OnInit, AfterViewInit {
     }
 
     if (this.nonUniqueSerialDisks.every((disk) => disk.bus === DiskBus.Usb)) {
-      return this.translate.instant('Warning: There are {n} USB disks available that have non-unique serial numbers. USB controllers may report disk serial incorrectly, making such disks indistinguishable from each other.', { n: this.availableNonUniqueSerialDisksCount });
+      return this.translate.instant('Warning: There are {n} USB disks available that have non-unique serial numbers. USB controllers may report disk serial incorrectly, making such disks indistinguishable from each other. Adding such disks to a pool can result in lost data.', { n: this.availableNonUniqueSerialDisksCount });
     }
 
     return this.translate.instant('Warning: There are {n} disks available that have non-unique serial numbers. Non-unique serial numbers can be caused by a cabling issue and adding such disks to a pool can result in lost data.', { n: this.availableNonUniqueSerialDisksCount });
