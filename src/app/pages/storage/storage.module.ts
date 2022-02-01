2879543ad8f33f280f057e447bca455e3a2bbddf
Boris Vasilenko <dreaktor0@gmail.com>
2022-02-01 17:10:11 +0300

    Revert "NAS-114448 / 22.02 / NAS-114448: Refactor Export/Disconnect dialog into a separate component (#6349)" (#6366)
    This reverts commit 51b930ce9a51efbec231f37cfc8052122c6e33f8.
    
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/storage/storage.module.ts
+++ src/app/pages/storage/storage.module.ts
@@ -34,7 +34,6 @@ import { VolumeChangekeyFormComponent } from 'app/pages/storage/volumes/volume-c
 import { VolumeCreatekeyFormComponent } from 'app/pages/storage/volumes/volume-createkey-form/volume-createkey-form.component';
 import { VolumesListControlsComponent } from 'app/pages/storage/volumes/volume-list-controls/volumes-list-controls.component';
 import { VolumeRekeyFormComponent } from 'app/pages/storage/volumes/volume-rekey-form/volume-rekey-form.component';
-import { ExportDisconnectModalComponent } from 'app/pages/storage/volumes/volumes-list/components/export-disconnect-modal.component';
 import { JobService } from 'app/services';
 import { StorageService } from 'app/services/storage.service';
 import { UserService } from 'app/services/user.service';
@@ -115,7 +114,6 @@ import { ZvolWizardComponent } from './volumes/zvol/zvol-wizard/zvol-wizard.comp
     EditNfsAceComponent,
     EditPosixAceComponent,
     SelectPresetModalComponent,
-    ExportDisconnectModalComponent,
   ],
   exports: [VolumesListControlsComponent],
   entryComponents: [SnapshotDetailsComponent, UnlockDialogComponent],
