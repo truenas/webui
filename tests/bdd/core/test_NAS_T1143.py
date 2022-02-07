ce95d934c2131fe93bcdb118e82b14265178132c
ericbsd <ericturgeon.bsd@gmail.com>
2022-02-07 10:12:31 -0400

    Add back time.sleep(1) under Transfer in test_NAS_T1143.py

--- tests/bdd/core/test_NAS_T1143.py
+++ tests/bdd/core/test_NAS_T1143.py
@@ -264,6 +264,7 @@ def on_the_nas_cloud_sync_task_tab_click_edit(driver):
     assert wait_on_element(driver, 5, '//button[@ix-auto="button___edit"]', 'clickable')
     driver.find_element_by_xpath('//button[@ix-auto="button___edit"]').click()
     assert wait_on_element(driver, 5, '//h4[contains(.,"Transfer")]')
+    time.sleep(1)
 
 
 @then('select PUSH as the Direction then under Transfer Mode, select COPY')
