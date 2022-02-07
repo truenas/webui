b6e3a35747f5d7b551072c0ee90e429cfb4ac6c9
q5sys <jt@obs-sec.com>
2022-02-07 12:56:05 -0500

    give longer wait for the apps tab to change and reload

--- tests/bdd/scale/test_NAS_T1355.py
+++ tests/bdd/scale/test_NAS_T1355.py
@@ -60,7 +60,8 @@ def make_sure_the_installed_tab_is_open(driver):
 @then('click three dots icon for Chia and select delete')
 def click_three_dots_icon_for_chia_and_select_delete(driver):
     """click three dots icon for Chia and select delete."""
-    assert wait_on_element(driver, 20, '//mat-card[contains(.,"chia-test")]//mat-icon[contains(.,"more_vert")]', 'clickable')
+    time.sleep(2)  # we have to wait for the page to settle down and the card to fully load
+    assert wait_on_element(driver, 30, '//mat-card[contains(.,"chia-test")]//mat-icon[contains(.,"more_vert")]', 'clickable')
     driver.find_element_by_xpath('//mat-card[contains(.,"chia-test")]//mat-icon[contains(.,"more_vert")]').click()
     assert wait_on_element(driver, 10, '//span[contains(.,"Delete")]', 'clickable')
     driver.find_element_by_xpath('//span[contains(.,"Delete")]').click()
