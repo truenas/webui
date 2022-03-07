ace8c174a113c325abab4b62fe2cc1212afe21f8
Evgeny Stepanovych <undsoft@gmail.com>
2022-03-07 12:22:35 +0100

    NAS-115124: Refactoring SMART test task form

--- src/app/pages/data-protection/data-protection.module.ts
+++ src/app/pages/data-protection/data-protection.module.ts
@@ -6,6 +6,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { SchedulerModule } from 'app/modules/scheduler/scheduler.module';
+import { SmartTaskFormComponent } from 'app/pages/data-protection/smart-task/smart-task-form/smart-task-form.component';
 import { EntityModule } from '../../modules/entity/entity.module';
 import { IxFormsModule } from '../../modules/ix-forms/ix-forms.module';
 import { CloudsyncFormComponent } from './cloudsync/cloudsync-form/cloudsync-form.component';
@@ -20,6 +22,7 @@ import { RsyncListComponent } from './rsync/rsync-list/rsync-list.component';
 import { ResilverConfigComponent } from './scrub/resilver-config/resilver-config.component';
 import { ScrubFormComponent } from './scrub/scrub-form/scrub-form.component';
 import { ScrubListComponent } from './scrub/scrub-list/scrub-list.component';
+import { SmartTaskListComponent } from './smart-task/smart-task-list/smart-task-list.component';
 import { SnapshotFormComponent } from './snapshot/snapshot-form/snapshot-form.component';
 import { SnapshotListComponent } from './snapshot/snapshot-list/snapshot-list.component';
 
