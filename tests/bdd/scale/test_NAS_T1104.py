93d0d46b3c21a1d0390f3efa4256f8f0d57f0bd4
ericbsd <ericturgeon.bsd@gmail.com>
2022-03-10 13:41:57 -0400

    Fix wait_on_element Group - {group_name} in NAS-T1104 and NAS-T1119

--- tests/bdd/scale/test_NAS_T1104.py
+++ tests/bdd/scale/test_NAS_T1104.py
@@ -261,4 +261,4 @@ def click_the_save_button_which_should_be_returned_to_the_storage_page_on_the_ed
     driver.find_element_by_xpath('//tr[contains(.,"my_acl_dataset")]//mat-icon[text()="more_vert"]').click()
     assert wait_on_element(driver, 5, '//button[normalize-space(text())="View Permissions"]')
     driver.find_element_by_xpath('//button[normalize-space(text())="View Permissions"]').click()
-    assert wait_on_element(driver, 7, f'//div[text()="{group_name}" and @class="item-value"]')
+    assert wait_on_element(driver, 7, f'//div[text()="Group - {group_name}"]')
