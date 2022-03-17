aa8ca7e45f163be0a535f6176c46845defdc667d
AlexKarpov <karpov4you@gmail.com>
2022-03-17 08:59:54 +0200

    NAS-115243: Minor fix

--- src/app/pages/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/components/dashboard/dashboard.component.ts
@@ -561,7 +561,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
       case 'pool':
         if (spl) {
           const pools = this.pools.filter((pool) => pool[key as keyof Pool] == value);
-          if (pools) { data = pools[0]; }
+          if (pools.length) { data = pools[0]; }
         } else {
           console.warn('DashConfigItem has no identifier!');
         }
@@ -569,7 +569,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
       case 'interface':
         if (spl) {
           const nics = this.nics.filter((nic) => nic[key as keyof DashboardNetworkInterface] == value);
-          if (nics) { data = nics[0].state; }
+          if (nics.length) { data = nics[0].state; }
         } else {
           console.warn('DashConfigItem has no identifier!');
         }
