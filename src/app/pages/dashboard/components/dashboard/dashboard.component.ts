e4e9c88094c00a232b6aebff8d2985448448a549
AlexKarpov <Karpov4you@gmail.com>
2022-03-17 17:36:51 +0200

    NAS-115243 / 22.02.1 / Dashboard Pool Widget not seeing new pool (#6514)

--- src/app/pages/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/components/dashboard/dashboard.component.ts
@@ -561,7 +561,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
       case 'pool':
         if (spl) {
           const pools = this.pools.filter((pool) => pool[key as keyof Pool] == value);
-          if (pools) { data = pools[0]; }
+          if (pools.length) { data = pools[0]; }
         } else {
           console.warn('DashConfigItem has no identifier!');
         }
@@ -569,7 +569,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
       case 'interface':
         if (spl) {
           const nics = this.nics.filter((nic) => nic[key as keyof DashboardNetworkInterface] == value);
-          if (nics) { data = nics[0].state; }
+          if (nics.length) { data = nics[0].state; }
         } else {
           console.warn('DashConfigItem has no identifier!');
         }
@@ -620,7 +620,12 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     const hidden = this.dashState
-      .filter((w) => state.every((s) => !(w.identifier && w.identifier == s.identifier || w.name && w.name == s.name)))
+      .filter((w) => state.every((s) => {
+        if (w.identifier) {
+          return w.identifier !== s.identifier;
+        }
+        return w.name !== s.name;
+      }))
       .map((widget) => ({ ...widget, rendered: false }));
 
     this.setDashState([...state, ...hidden]);
