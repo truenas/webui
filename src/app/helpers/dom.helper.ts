d6c7803d91471ffe4aed702fbbaa5f3ddf5e801a
Evgeny Stepanovych <undsoft@gmail.com>
2022-03-18 19:42:27 +0100

    NAS-114658: Testing coverage reporting

--- src/app/helpers/dom.helper.ts
+++ src/app/helpers/dom.helper.ts
@@ -19,28 +19,3 @@ export function addClass(el: HTMLElement | HTMLCollectionOf<HTMLElement>, classN
     }
   }
 }
-
-export function findClosest(el: HTMLElement, className: string): HTMLElement {
-  if (!el) return;
-  while (el) {
-    const parent = el.parentElement;
-    if (parent && hasClass(parent, className)) {
-      return parent;
-    }
-    el = parent;
-  }
-}
-
-export function hasClass(el: HTMLElement, className: string): boolean {
-  if (!el) return;
-  return (` ${el.className} `).replace(/[\n\t]/g, ' ').includes(` ${className} `);
-}
-
-export function toggleClass(el: HTMLElement | HTMLCollectionOf<HTMLElement>, className: string): void {
-  if (!el) return;
-  if (hasClass((el as HTMLElement), className)) {
-    removeClass(el, className);
-  } else {
-    addClass(el, className);
-  }
-}
