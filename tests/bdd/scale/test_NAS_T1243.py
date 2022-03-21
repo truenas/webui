001f6097efd2f866c3cd5312c520611bf66ae62d
Upasana Bansal <39758747+upasanabnsl@users.noreply.github.com>
2022-03-21 10:44:42 -0700

    Update test_NAS_T1243.py

--- tests/bdd/scale/test_NAS_T1243.py
+++ tests/bdd/scale/test_NAS_T1243.py
@@ -94,6 +94,8 @@ def enter_abcd1234_for_both_fields_and_confirm_and_save(driver):
     driver.find_element_by_xpath('//ix-input[@formcontrolname="confirm_passphrase"]//input[@type="search"]').send_keys("abcd1234")
     #assert wait_on_element(driver, 10, '//mat-error[contains(.,"The passwords do not match.")]') is False
     
+    element = driver.find_element_by_xpath('//ix-checkbox[@formcontrolname="confirm"]')
+    driver.execute_script("arguments[0].scrollIntoView();", element)
     assert wait_on_element(driver, 10, '//ix-checkbox[@formcontrolname="confirm"]//input[@type="checkbox"]', 'clickable')
     driver.find_element_by_xpath('//ix-checkbox[@formcontrolname="confirm"]//input[@type="checkbox"]').click()   
     assert wait_on_element(driver, 5, '//button[contains(.,"Save")]', 'clickable')
