9b35c8f0e4fa66fe698c2ff1821298baf4512c56
Evgeny Stepanovych <undsoft@gmail.com>
2022-03-22 17:15:51 +0100

    NAS-115330 / 22.12 / NAS-115330: Refactoring POSIX and NFS permissions forms (#6529)

--- src/app/pages/storage/volumes/permissions/components/edit-posix-ace/edit-posix-ace-form-values.interface.ts
+++
@@ -1,9 +0,0 @@
-import { PosixAclTag, PosixPermission } from 'app/enums/posix-acl.enum';
-
-export interface EditPosixAceFormValues {
-  tag: PosixAclTag;
-  user?: string;
-  group?: string;
-  permissions: PosixPermission[];
-  default: boolean;
-}
