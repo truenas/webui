32ba2cb90348a6b391879bacf608a33b0c07d3f9
ericbsd <ericturgeon.bsd@gmail.com>
2022-03-23 10:46:48 -0300

    Added condition to skip greeting if missing

--- tests/bdd/ha-bhyve02/test_NAS_T0945.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0945.py
@@ -58,8 +58,8 @@ def you_should_see_the_dashboard(driver):
         try:
             assert wait_on_element(driver, 2, '//button[@ix-auto="button__I AGREE"]', 'clickable')
             driver.find_element_by_xpath('//button[@ix-auto="button__I AGREE"]').click()
-            assert wait_on_element(driver, 2, '//button[@ix-auto="button__CLOSE"]', 'clickable')
-            driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
+            if wait_on_element(driver, 2, '//button[@ix-auto="button__CLOSE"]', 'clickable'):
+                driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
         except ElementClickInterceptedException:
             assert wait_on_element(driver, 2, '//button[@ix-auto="button__CLOSE"]', 'clickable')
             driver.find_element_by_xpath('//button[@ix-auto="button__CLOSE"]').click()
