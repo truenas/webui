bf257b1cd654dc73c23f1ddc4f7c4be62bbe8e21
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2022-03-24 08:44:28 +0100

    NAS-115173: create ix-forms

--- src/app/pages/system/general-settings/support/proactive/proactive.component.ts
+++ src/app/pages/system/general-settings/support/proactive/proactive.component.ts
@@ -1,206 +1,108 @@
-import { Component } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@ngneat/reactive-forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { TranslateService } from '@ngx-translate/core';
 import { helptextSystemSupport as helptext } from 'app/helptext/system/support';
-import { FormConfiguration } from 'app/interfaces/entity-form.interface';
-import { SupportConfig, SupportConfigUpdate } from 'app/interfaces/support.interface';
+import { SupportConfigUpdate } from 'app/interfaces/support.interface';
 import { AppLoaderService } from 'app/modules/app-loader/app-loader.service';
-import { EntityFormComponent } from 'app/modules/entity/entity-form/entity-form.component';
-import { FieldConfig } from 'app/modules/entity/entity-form/models/field-config.interface';
-import { FieldSet } from 'app/modules/entity/entity-form/models/fieldset.interface';
-import { WebSocketService } from 'app/services/';
-import { DialogService } from 'app/services/dialog.service';
-import { ModalService } from 'app/services/modal.service';
+import { EntityUtils } from 'app/modules/entity/utils';
+import { FormErrorHandlerService } from 'app/modules/ix-forms/services/form-error-handler.service';
+import { DialogService, WebSocketService } from 'app/services';
+import { IxSlideInService } from 'app/services/ix-slide-in.service';
 
 @UntilDestroy()
 @Component({
-  selector: 'app-proactive',
-  template: '<entity-form [conf]="this"></entity-form>',
+  templateUrl: 'proactive.component.html',
+  styleUrls: ['./proactive.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProactiveComponent implements FormConfiguration {
-  entityEdit: EntityFormComponent;
-  queryCall = 'support.config' as const;
-  controls: { [key: string]: AbstractControl };
-  saveButtonEnabled: boolean;
+
+export class ProactiveComponent implements OnInit {
+  isLoading = false;
   title = helptext.proactive.title;
-  fieldConfig: FieldConfig[] = [];
-  fieldSets: FieldSet[] = [
-    {
-      name: 'col1',
-      label: false,
-      config: [
-        {
-          type: 'paragraph',
-          name: 'proactive_title',
-          paraText: helptext.proactive.primary_contact,
-          tooltip: helptext.proactive.instructions,
-        },
-        {
-          type: 'input',
-          name: 'name',
-          placeholder: helptext.proactive.pc_name_placeholder,
-          required: true,
-          validation: helptext.proactive.pc_validation,
-        },
-        {
-          type: 'input',
-          name: 'title',
-          placeholder: helptext.proactive.pc_title_placeholder,
-          required: true,
-          validation: helptext.proactive.pc_validation,
-        },
-        {
-          type: 'input',
-          name: 'email',
-          placeholder: helptext.proactive.pc_email_placeholder,
-          required: true,
-          validation: helptext.proactive.pc_email_validation,
-        },
-        {
-          type: 'input',
-          name: 'phone',
-          placeholder: helptext.proactive.pc_phone_placeholder,
-          required: true,
-          validation: helptext.proactive.pc_validation,
-        },
-      ],
-    },
-    {
-      name: 'col2',
-      label: false,
-      config: [
-        {
-          type: 'paragraph',
-          name: 'proactive_second_title',
-          paraText: helptext.proactive.secondary_contact,
-        },
-        {
-          type: 'input',
-          name: 'secondary_name',
-          placeholder: helptext.proactive.sec_name_placeholder,
-          required: true,
-          validation: helptext.proactive.pc_validation,
-        },
-        {
-          type: 'input',
-          name: 'secondary_title',
-          placeholder: helptext.proactive.sec_title_placeholder,
-          required: true,
-          validation: helptext.proactive.pc_validation,
-        },
-        {
-          type: 'input',
-          name: 'secondary_email',
-          placeholder: helptext.proactive.sec_email_placeholder,
-          validation: helptext.proactive.sec_email_validation,
-          required: true,
-        },
-        {
-          type: 'input',
-          name: 'secondary_phone',
-          placeholder: helptext.proactive.sec_phone_placeholder,
-          required: true,
-          validation: helptext.proactive.pc_validation,
-        },
-      ],
-    },
-    {
-      name: 'enabled',
-      label: false,
-      width: '100%',
-      config: [
-        {
-          type: 'checkbox',
-          name: 'enabled',
-          placeholder: helptext.proactive.enable_checkbox_placeholder,
-        },
-      ],
-    },
-  ];
 
-  constructor(public ws: WebSocketService, protected loader: AppLoaderService,
-    protected dialogService: DialogService, private translate: TranslateService,
-    private modalService: ModalService) { }
+  form = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    title: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required]],
+    enabled: [false],
+    secondary_name: ['', [Validators.required]],
+    secondary_title: ['', [Validators.required]],
+    secondary_email: ['', [Validators.required, Validators.email]],
+    secondary_phone: ['', [Validators.required]],
+  });
 
-  afterInit(entityEdit: EntityFormComponent): void {
-    this.entityEdit = entityEdit;
-    const proactiveFields = [
-      'enabled',
-      'name',
-      'title',
-      'email',
-      'phone',
-      'secondary_name',
-      'secondary_title',
-      'secondary_email',
-      'secondary_phone',
-    ];
+  readonly helptext = helptext;
 
-    const proactiveParatext = [
-      'proactive_title',
-      'proactive_second_title',
-    ];
+  readonly labels = {
+    pc_name: helptext.proactive.pc_name_placeholder,
+    pc_title: helptext.proactive.pc_title_placeholder,
+    pc_email: helptext.proactive.pc_email_placeholder,
+    pc_phone: helptext.proactive.pc_phone_placeholder,
+    enable_checkbox: helptext.proactive.enable_checkbox_placeholder,
+    sec_name: helptext.proactive.sec_name_placeholder,
+    sec_title: helptext.proactive.sec_title_placeholder,
+    sec_email: helptext.proactive.sec_email_placeholder,
+    sec_phone: helptext.proactive.sec_phone_placeholder,
+  };
 
-    this.ws.call('support.is_available').pipe(untilDestroyed(this)).subscribe((res) => {
-      if (!res) {
-        this.saveButtonEnabled = false;
-        proactiveFields.forEach((field) => {
-          this.entityEdit.setDisabled(field, true, false);
-        });
-        proactiveParatext.forEach((i) => {
-          document.getElementById(i).style.opacity = '0.38';
-        });
-        this.modalService.closeSlideIn();
-        this.dialogService.info(helptext.proactive.dialog_unavailable_title,
-          helptext.proactive.dialog_unavailable_warning, '500px', 'warning', true);
-      } else {
-        this.getContacts();
-        this.saveButtonEnabled = true;
-        this.ws.call('support.is_available_and_enabled').pipe(untilDestroyed(this)).subscribe((res) => {
-          if (res) {
-            this.entityEdit.formGroup.controls['enabled'].setValue(true);
-          } else {
-            this.entityEdit.formGroup.controls['enabled'].setValue(false);
-          }
-        });
-      }
-    });
+  constructor(
+    private formBuilder: FormBuilder,
+    private ws: WebSocketService,
+    private cdr: ChangeDetectorRef,
+    private dialogService: DialogService,
+    private slideInService: IxSlideInService,
+    private errorHandler: FormErrorHandlerService,
+    protected loader: AppLoaderService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadConfig();
   }
 
-  getContacts(): void {
-    this.controls = this.entityEdit.formGroup.controls;
-    this.ws.call(this.queryCall).pipe(untilDestroyed(this)).subscribe((res) => {
-      if (res && (res as any) !== {}) {
-        for (const i in res) {
-          if (i !== 'id') {
-            this.controls[i].setValue(res[i as keyof SupportConfig]);
-          }
-        }
-      }
-    });
+  onSubmit(): void {
+    const values = this.form.value as SupportConfigUpdate;
+
+    this.isLoading = true;
+    this.ws.call('support.update', [values])
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        () => {
+          this.isLoading = false;
+          this.slideInService.close();
+          this.cdr.markForCheck();
+          this.dialogService.info(helptext.proactive.dialog_title,
+            helptext.proactive.dialog_mesage, '350px', 'info', true);
+        }, (error) => {
+          this.isLoading = false;
+          this.errorHandler.handleWsFormError(error, this.form);
+          this.cdr.markForCheck();
+        },
+      );
   }
 
-  beforeSubmit(data: any): void {
-    delete data.proactive_second_title;
-    delete data.proactive_title;
-    if (!data.enabled) {
-      data.enabled = false;
-    }
-  }
-
-  customSubmit(data: SupportConfigUpdate): void {
-    this.loader.open();
-    this.ws.call('support.update', [data]).pipe(untilDestroyed(this)).subscribe(() => {
-      this.loader.close();
-      this.modalService.closeSlideIn();
-      this.dialogService.info(helptext.proactive.dialog_title,
-        helptext.proactive.dialog_mesage, '350px', 'info', true);
-    },
-    (err) => {
-      this.loader.close();
-      this.dialogService.errorReport(helptext.proactive.dialog_err,
-        err.error.message, err.error.traceback);
-    });
+  private loadConfig(): void {
+    this.isLoading = true;
+    this.ws.call('support.config')
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        (config) => {
+          this.form.patchValue(config);
+          this.isLoading = false;
+          this.cdr.markForCheck();
+        },
+        (error) => {
+          this.isLoading = false;
+          this.cdr.markForCheck();
+          new EntityUtils().handleWsError(null, error, this.dialogService);
+        },
+      );
   }
 }
+// this.ws.call('support.is_available') if available support
