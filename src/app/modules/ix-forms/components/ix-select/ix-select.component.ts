69115b5cbaa4100da21a5a588efd06f6a3ee5b6a
Rehan Yousaf <rehanyousaf147@gmail.com>
2022-03-24 19:37:58 +0500

    NAS-115262: Removed new property and added links

--- src/app/modules/ix-forms/components/ix-select/ix-select.component.ts
+++ src/app/modules/ix-forms/components/ix-select/ix-select.component.ts
@@ -1,6 +1,6 @@
 import {
   ChangeDetectionStrategy, ChangeDetectorRef,
-  Component, EventEmitter, Input, OnChanges, Output,
+  Component, Input, OnChanges,
 } from '@angular/core';
 import {
   ControlValueAccessor, FormControl, NgControl,
@@ -27,8 +27,6 @@ export class IxSelectComponent implements ControlValueAccessor, OnChanges {
   @Input() required: boolean;
   @Input() tooltip: string;
   @Input() multiple: boolean;
-  @Output() linkClicked: EventEmitter<void> = new EventEmitter();
-  @Input() linkText: string = null;
 
   constructor(
     public controlDirective: NgControl,
