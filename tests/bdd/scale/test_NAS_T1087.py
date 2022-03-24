c0e296d88ef5065c3e9652736482c490aa773367
Upasana Bansal <39758747+upasanabnsl@users.noreply.github.com>
2022-03-24 14:21:41 -0700

    Update test_NAS_T1087.py

--- tests/bdd/scale/test_NAS_T1087.py
+++ tests/bdd/scale/test_NAS_T1087.py
@@ -69,13 +69,13 @@ def the_user_edit_page_should_open_change_the_user_email_eturgeonixsystemscom_an
     """the User Edit Page should open, change the user email "eturgeon@ixsystems.com" and click save."""
     assert wait_on_element(driver, 10, '//h3[contains(.,"Edit User")]')
     assert wait_on_element_disappear(driver, 10, '//h6[contains(.,"Please wait")]')
-    assert wait_on_element(driver, 7, '//input[@ix-auto="input__Email"]', 'inputable')
-    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').clear()
-    driver.find_element_by_xpath('//input[@ix-auto="input__Email"]').send_keys("eturgeon@ixsystems.com")
-    assert wait_on_element(driver, 10, '//button[@ix-auto="button__SAVE"]', 'clickable')
-    element = driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]')
+    assert wait_on_element(driver, 7, '//ix-input[@formcontrolname="email"]//input', 'inputable')
+    driver.find_element_by_xpath('//ix-input[@formcontrolname="email"]//input').clear()
+    driver.find_element_by_xpath('//ix-input[@formcontrolname="email"]//input').send_keys("eturgeon@ixsystems.com")
+    assert wait_on_element(driver, 10, '//button[span[contains(.,"Save")]]', 'clickable')
+    element = driver.find_element_by_xpath('//button[span[contains(.,"Save")]]')
     driver.execute_script("arguments[0].scrollIntoView();", element)
-    driver.find_element_by_xpath('//button[@ix-auto="button__SAVE"]').click()
+    driver.find_element_by_xpath('//button[span[contains(.,"Save")]]').click()
 
 
 @then('change should be saved, open the user dropdown, the email value should be visible')
