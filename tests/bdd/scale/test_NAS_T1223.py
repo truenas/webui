0c2cf6016f990da7ed0029031a6443f2de65cb4c
ericbsd <ericturgeon.bsd@gmail.com>
2022-04-11 14:37:59 -0300

    make sure that group name input is ready in test_NAS_T1250.py

--- tests/bdd/scale/test_NAS_T1223.py
+++ tests/bdd/scale/test_NAS_T1223.py
@@ -117,5 +117,5 @@ def verify_that_the_core_file_alert_disappear(driver):
     assert wait_on_element(driver, 7, '//button[contains(.,"clear")]', 'clickable')
     driver.find_element_by_xpath('//button[contains(.,"clear")]').click()
     time.sleep(0.5)
-    #if wait_on_element(driver, 5, '//div[contains(@class,"mat-drawer-backdrop")]'):
-    #    assert wait_on_element_disappear (driver, 20, '//div[contains(@class,"mat-drawer-backdrop")]')
+    # if wait_on_element(driver, 5, '//div[contains(@class,"mat-drawer-backdrop")]'):
+    #    assert wait_on_element_disappear(driver, 20, '//div[contains(@class,"mat-drawer-backdrop")]')
