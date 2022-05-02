4d98c83b0c27b345e7edf6c091e6616ff1cf1e63
AlexKarpov <karpov4you@gmail.com>
2022-05-02 14:59:00 +0300

    NAS-112226: removed 'fresh browser'

--- src/app/services/redirect.service.ts
+++ src/app/services/redirect.service.ts
@@ -38,10 +38,9 @@ export class RedirectService {
 Because HTTP to HTTPS redirect is enabled in settings your browser will force HTTPS connection for this URL.<br>
 This may create issues if app does not support secure connections.<br>
 <br>
-You can try opening app url in a fresh browser or incognito mode.<br>
+You can try opening app url in an incognito mode.<br>
 Alternatively you can disable redirect in Settings, clear browser cache and try again.`, { url }),
           url,
-          target,
         } as RedirectDialogData,
       }).afterClosed().pipe(filter(Boolean), untilDestroyed(this)).subscribe(() => {
         return window.open(url, target);
