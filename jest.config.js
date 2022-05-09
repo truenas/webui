56fbd5cd562891c6a515e11ec04c013608d09af2
Evgeny Stepanovych <undsoft@gmail.com>
2022-05-09 13:01:57 +0200

    NAS-113293: Upgrading to Angular 13

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,6 @@ process.env.TZ = 'Europe/Kiev';
 
 module.exports = {
   preset: 'jest-preset-angular',
-  // roots: ['<rootDir>/src/'],
-  // testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   collectCoverage: false,
   collectCoverageFrom: ["**/*.ts"],
@@ -18,14 +16,7 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}),
   testPathIgnorePatterns: [
-    // "<rootDir>/node_modules/(?!date-fns-tz|date-fns)",
     "node_modules/(?!(date-fns-tz|date-fns))",
     "<rootDir>/dist/"
   ],
-  // globals: {
-  //   "ts-jest": {
-  //     "tsconfig": "<rootDir>/tsconfig.spec.json",
-  //     "stringifyContentPathRegex": "\\.html$"
-  //   }
-  // }
 };
