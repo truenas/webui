4fb2ff5db69193e1352a94fcb91e5a805a6cdbaa
Evgeny Stepanovych <undsoft@gmail.com>
2022-05-10 19:37:06 +0200

    NAS-116088 / 22.12 / NAS-116088: Refactoring quota forms (#6681)

--- src/app/pages/storage/storage.routing.ts
+++ src/app/pages/storage/storage.routing.ts
@@ -12,8 +12,6 @@ import { SmartResultsComponent } from './disks/smart-results/smart-results.compo
 import { ImportDiskComponent } from './import-disk/import-disk.component';
 import { VmwareSnapshotListComponent } from './vmware-snapshot/vmware-snapshot-list/vmware-snapshot-list.component';
 import { DatasetFormComponent } from './volumes/datasets/dataset-form/dataset-form.component';
-import { GroupQuotaFormComponent } from './volumes/datasets/dataset-quotas/dataset-quotas-grouplist/group-quota-form/group-quota-form.component';
-import { UserQuotaFormComponent } from './volumes/datasets/dataset-quotas/dataset-quotas-userlist/user-quota-form/user-quota-form.component';
 import { DatasetUnlockComponent } from './volumes/datasets/dataset-unlock/dataset-unlock.component';
 import { ManagerComponent } from './volumes/manager/manager.component';
 import { VolumeStatusComponent } from './volumes/volume-status/volume-status.component';
@@ -65,19 +63,9 @@ export const routes: Routes = [
             data: { title: 'User Quotas', breadcrumb: 'User Quotas' },
           },
           {
-            path: 'user-quotas-form/:pk',
-            component: UserQuotaFormComponent,
-            data: { title: 'Set User Quotas', breadcrumb: 'Set User Quotas' },
-          },
-          {
             path: 'group-quotas/:pk',
             component: DatasetQuotasGrouplistComponent,
-            data: { title: 'Edit Group Quotas', breadcrumb: 'Edit Group Quotas' },
-          },
-          {
-            path: 'group-quotas-form/:pk',
-            component: GroupQuotaFormComponent,
-            data: { title: 'Set Group Quotas', breadcrumb: 'Set Group Quotas' },
+            data: { title: 'Group Quotas', breadcrumb: 'Edit Group Quotas' },
           },
           {
             path: 'id/:pk/dataset/acl/:path',
