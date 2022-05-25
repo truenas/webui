a451b63d252ef15942edfafca3dd709dc4fdf4b9
Boris Vasilenko <bvasilenko@ixsystems.com>
2022-05-25 22:31:31 +0300

    NAS-115593: IxDynamicFormComponent tests

--- src/app/modules/ix-forms/components/ix-dynamic-form/ix-dynamic-form.component.spec.ts
+++ src/app/modules/ix-forms/components/ix-dynamic-form/ix-dynamic-form.component.spec.ts
@@ -1,17 +1,52 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  FormGroup, FormsModule, ReactiveFormsModule,
+} from '@angular/forms';
 import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { MockComponent } from 'ng-mocks';
+import { DynamicFormSchema } from 'app/interfaces/dynamic-form-schema.interface';
+import { IxDynamicFormItemComponent } from 'app/modules/ix-forms/components/ix-dynamic-form/ix-dynamic-form-item/ix-dynamic-form-item.component';
 import { IxDynamicFormComponent } from 'app/modules/ix-forms/components/ix-dynamic-form/ix-dynamic-form.component';
+import { IxFieldsetComponent } from 'app/modules/ix-forms/components/ix-fieldset/ix-fieldset.component';
+
+const dynamicForm = new FormGroup({});
+const dynamicSection = [
+  { name: 'section_1', description: '', schema: [{}, {}] },
+  { name: 'section_2', description: '', schema: [{}] },
+  { name: 'section_3', description: '', schema: [{}, {}, {}] },
+  { name: 'section_4', description: '', schema: [{}] },
+] as DynamicFormSchema[];
 
 describe('IxDynamicFormComponent', () => {
   let spectator: Spectator<IxDynamicFormComponent>;
   const createComponent = createComponentFactory({
     component: IxDynamicFormComponent,
+    imports: [
+      FormsModule,
+      ReactiveFormsModule,
+      FlexLayoutModule,
+    ],
+    declarations: [
+      MockComponent(IxDynamicFormItemComponent),
+      MockComponent(IxFieldsetComponent),
+    ],
   });
 
   beforeEach(() => {
     spectator = createComponent();
   });
 
-  describe('Control Emit', () => {
+  describe('UI tests', () => {
+    it('section', () => {
+      spectator.component.dynamicForm = dynamicForm;
+      spectator.component.dynamicSection = dynamicSection;
+      spectator.detectComponentChanges();
+
+      expect(spectator.queryAll('ix-dynamic-form-item').length).toEqual(7);
+    });
+  });
+
+  describe('Component methods', () => {
     it('addControlNext()', () => {
       jest.spyOn(spectator.component.addListItem, 'emit').mockImplementation();
       spectator.component.addControlNext(undefined);
