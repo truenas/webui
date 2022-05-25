61eec2958334209e3025474cbf8021c4bb6a5f46
AlexKarpov <Karpov4you@gmail.com>
2022-05-25 22:46:57 +0300

    NAS-116149 / 22.12 / Useless / broken breadcrumbs (#6699)
    * NAS-116149: Useless / broken breadcrumbs
    
    * NAS-116149: router breadcrumb config
    
    * NAS-116149: non-clickable tooltips
    
    * NAS-116149: different style
    
    * NAS-116149: muted color for disabled breadcrumbs

--- src/app/services/route-parts/route-parts.service.ts
+++ src/app/services/route-parts/route-parts.service.ts
@@ -1,59 +1,55 @@
 import { Injectable } from '@angular/core';
 import {
-  ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Params, Router,
+  ActivatedRoute, NavigationEnd, Router,
 } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 export interface RoutePart {
   title: string;
   breadcrumb: string;
-  params?: Params;
   url: string;
-  toplevel: boolean;
   disabled?: boolean;
 }
 
 @Injectable()
 export class RoutePartsService {
-  private lastSuccessfullrouteParts: RoutePart[];
+  private fullRouteParts: RoutePart[];
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {
     // only execute when routechange
-    this.lastSuccessfullrouteParts = this.generateRouteParts(this.activatedRoute.snapshot);
+    this.fullRouteParts = this.generateRouteParts(this.activatedRoute.root);
     this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd),
     ).subscribe(() => {
-      this.lastSuccessfullrouteParts = this.generateRouteParts(this.activatedRoute.snapshot);
+      this.fullRouteParts = this.generateRouteParts(this.activatedRoute.root);
     });
   }
 
-  get routeParts(): RoutePart[] {
-    return this.lastSuccessfullrouteParts;
-  }
+  private generateRouteParts(route: ActivatedRoute, url: string = '', routeParts: RoutePart[] = []): RoutePart[] {
+    const children: ActivatedRoute[] = route.children;
 
-  private generateRouteParts(snapshot: ActivatedRouteSnapshot): RoutePart[] {
-    let routeParts: RoutePart[] = [];
-    if (snapshot) {
-      if (snapshot.firstChild) {
-        routeParts = routeParts.concat(this.generateRouteParts(snapshot.firstChild));
+    if (children.length === 0) {
+      return routeParts;
+    }
+
+    for (const child of children) {
+      const routeUrl: string = child.snapshot.url.map((segment) => segment.path).join('/');
+      if (routeUrl) {
+        url += `/${routeUrl}`;
       }
-      if (snapshot.data['title'] && snapshot.url.length) {
-        let targetUrl: any = snapshot.url[0];
-        for (let i = 1; i < snapshot.url.length; i++) {
-          targetUrl = targetUrl + '/' + snapshot.url[i];
-        }
-        let toplevel = false;
-        if (snapshot.data['toplevel']) {
-          toplevel = snapshot.data['toplevel'];
-        }
+
+      const { title, breadcrumb, disabled } = child.snapshot.data;
+      if (title) {
         routeParts.push({
-          title: snapshot.data['title'],
-          breadcrumb: snapshot.data['breadcrumb'],
-          url: targetUrl,
-          toplevel,
+          title, breadcrumb, disabled, url,
         });
       }
+
+      return this.generateRouteParts(child, url, routeParts);
     }
-    return routeParts;
+  }
+
+  get routeParts(): RoutePart[] {
+    return this.fullRouteParts;
   }
 }
