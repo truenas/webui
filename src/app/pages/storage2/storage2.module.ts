5e5e7a643bf93961c2190ecca3b412f497de992e
AlexKarpov <karpov4you@gmail.com>
2022-06-15 04:42:59 +0300

    NAS-116417: Disk Info Card/Widget

--- src/app/pages/storage2/storage2.module.ts
+++ src/app/pages/storage2/storage2.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppCommonModule } from 'app/modules/common/app-common.module';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
 import { IxTableModule } from 'app/modules/ix-tables/ix-table.module';
 import { DeviceManagementComponent } from 'app/pages/storage2/components/device-management/device-management.component';
+import { DiskInfoCardComponent } from 'app/pages/storage2/components/disk-info-card/disk-info-card.component';
 import { PoolsDashboardComponent } from 'app/pages/storage2/components/pools-dashboard/pools-dashboard.component';
 import { routing } from 'app/pages/storage2/storage2.routing';
 
@@ -17,6 +19,7 @@ import { routing } from 'app/pages/storage2/storage2.routing';
     IxFormsModule,
     TranslateModule,
     MatCardModule,
+    MatListModule,
     AppCommonModule,
     MatButtonModule,
     RouterModule,
@@ -24,6 +27,7 @@ import { routing } from 'app/pages/storage2/storage2.routing';
   declarations: [
     DeviceManagementComponent,
     PoolsDashboardComponent,
+    DiskInfoCardComponent,
   ],
 })
 export class Storage2Module { }
