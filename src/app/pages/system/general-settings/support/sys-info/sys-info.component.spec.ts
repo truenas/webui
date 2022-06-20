de9a1346f20974adfe4d008a7f30b8acacf1cd8f
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2022-06-20 14:38:33 +0200

    NAS-116040: changed sys-info.component

--- src/app/pages/system/general-settings/support/sys-info/sys-info.component.spec.ts
+++ src/app/pages/system/general-settings/support/sys-info/sys-info.component.spec.ts
@@ -1,6 +1,7 @@
-import { createHostFactory, SpectatorHost } from '@ngneat/spectator/jest';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 import { LicenseInfoInSupport } from 'app/pages/system/general-settings/support/license-info-in-support.interface';
 import { SysInfoComponent } from 'app/pages/system/general-settings/support/sys-info/sys-info.component';
+import { SystemInfoInSupport } from 'app/pages/system/general-settings/support/system-info-in-support.interface';
 
 describe('SysInfoComponent', () => {
   const systemInfo = {
@@ -18,49 +19,59 @@ describe('SysInfoComponent', () => {
     daysLeftinContract: -4,
     add_hardware: 'NONE',
   };
-
-  let spectator: SpectatorHost<SysInfoComponent>;
-  const createHost = createHostFactory({
+  let spectator: Spectator<SysInfoComponent>;
+  const createComponent = createComponentFactory({
     component: SysInfoComponent,
   });
 
   beforeEach(() => {
-    spectator = createHost('<ix-sys-info [system_info]="systemInfo"></ix-sys-info>', {
-      hostProps: {
-        systemInfo,
-        has_license: false,
+    spectator = createComponent({
+      props: {
+        systemInfo: systemInfo as SystemInfoInSupport,
+        hasLicense: false,
       },
     });
   });
 
   it('it shows a block with system info', () => {
-    const sysInfoItems = spectator.queryAll('.sys-info-wrapper .value');
+    const sysInfoValues = spectator.queryAll('.sys-info-wrapper .value');
+    const sysInfoLabels = spectator.queryAll('.sys-info-wrapper .label');
+    const infoRows = sysInfoValues.reduce((acc, item, i) => {
+      return { ...acc, ...{ [sysInfoLabels[i].textContent]: item.textContent } };
+    }, {} as { [key: string]: string });
     const sysLicenseBlock = spectator.query('.sys-license-wrapper');
 
     expect(sysLicenseBlock).not.toBeTruthy();
-    expect(sysInfoItems[0]).toHaveText('TrueNAS-SCALE-22.12-MASTER-20220318-020017');
-    expect(sysInfoItems[1]).toHaveText('VirtualBox');
-    expect(sysInfoItems[2]).toHaveText('AMD Ryzen 3 3200G');
-    expect(sysInfoItems[3]).toHaveText('5 GiB');
-    expect(sysInfoItems[4]).toHaveText('ffbb355c');
+    expect(infoRows).toEqual({
+      'Memory:': systemInfo.memory,
+      'Model:': systemInfo.model,
+      'OS Version:': systemInfo.version,
+      'Product:': systemInfo.system_product,
+      'Serial Number:': systemInfo.serial,
+    });
   });
 
   it('it shows a block with license info', () => {
     spectator.setInput({
-      license_info: licenseInfo as LicenseInfoInSupport,
-      has_license: true,
+      licenseInfo: licenseInfo as LicenseInfoInSupport,
+      hasLicense: true,
     });
-    const sysLicenseItems = spectator.queryAll('.sys-license-wrapper .value');
+    const sysLicenseValues = spectator.queryAll('.sys-license-wrapper .value');
+    const sysLicenseLabels = spectator.queryAll('.sys-license-wrapper .label');
+    const infoRows = sysLicenseValues.reduce((acc, item, i) => {
+      return { ...acc, ...{ [sysLicenseLabels[i].textContent]: item.textContent } };
+    }, {} as { [key: string]: string });
     const sysInfoBlock = spectator.query('.sys-info-wrapper');
 
     expect(sysInfoBlock).not.toBeTruthy();
-    expect(sysLicenseItems[0]).toHaveText('iXsystems Inc.');
-    expect(sysLicenseItems[1]).toHaveText('DEDUP, FIBRECHANNEL, VM');
-    expect(sysLicenseItems[2]).toHaveText('GOLD');
-    expect(sysLicenseItems[3]).toHaveText('2022-06-10');
-    expect(sysLicenseItems[4]).toHaveText('( EXPIRED )');
-    expect(sysLicenseItems[5]).toHaveText('AMD Ryzen 3 3200G');
-    expect(sysLicenseItems[6]).toHaveText('ffbb355c');
-    expect(sysLicenseItems[7]).toHaveText('NONE');
+    expect(infoRows).toEqual({
+      'Customer Name:': licenseInfo.customer_name,
+      'Model:': systemInfo.model,
+      'System Serial:': systemInfo.serial,
+      'Features:': licenseInfo.features.join(', '),
+      'Contract Type:': licenseInfo.contract_type,
+      'Expiration Date:': licenseInfo.expiration_date + ' ( EXPIRED ) ',
+      'Additional Hardware:': licenseInfo.add_hardware,
+    });
   });
 });
--- src/app/pages/system/general-settings/support/sys-info/sys-info.component.ts
+++ src/app/pages/system/general-settings/support/sys-info/sys-info.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { LicenseInfoInSupport } from 'app/pages/system/general-settings/support/license-info-in-support.interface';
 import { SystemInfoInSupport } from 'app/pages/system/general-settings/support/system-info-in-support.interface';
 
 @Component({
   selector: 'ix-sys-info',
   templateUrl: './sys-info.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SysInfoComponent {
-  @Input() has_license: boolean;
-  @Input() license_info: LicenseInfoInSupport;
-  @Input() system_info: SystemInfoInSupport;
+  @Input() hasLicense: boolean;
+  @Input() licenseInfo: LicenseInfoInSupport;
+  @Input() systemInfo: SystemInfoInSupport;
 }
