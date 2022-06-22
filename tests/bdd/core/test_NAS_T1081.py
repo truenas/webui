aee8418265fd871f42c1336fccb37437f4b37f5c
ericbsd <ericturgeon.bsd@gmail.com>
2022-06-22 17:17:30 -0300

    Adding missing 0.5 second sleep after scroll to the top on some test

--- tests/bdd/core/test_NAS_T1081.py
+++ tests/bdd/core/test_NAS_T1081.py
@@ -41,6 +41,7 @@ def the_browser_is_open_the_freenas_url_and_logged_in(driver, nas_ip, root_passw
         assert wait_on_element(driver, 10, '//span[contains(.,"root")]')
         element = driver.find_element_by_xpath('//span[contains(.,"root")]')
         driver.execute_script("arguments[0].scrollIntoView();", element)
+        time.sleep(0.5)
         assert wait_on_element(driver, 5, '//mat-list-item[@ix-auto="option__Dashboard"]', 'clickable')
         driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Dashboard"]').click()
 
