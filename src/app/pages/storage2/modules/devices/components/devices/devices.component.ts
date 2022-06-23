979763412eeb2d8ac9effc4eb967a7c8d18338ed
Denys Butenko <ua13dark@gmail.com>
2022-06-23 22:54:40 +0530

    NAS-XXXXXX: Add ix-tree to devices page

--- src/app/pages/storage2/modules/devices/components/devices/devices.component.ts
+++ src/app/pages/storage2/modules/devices/components/devices/devices.component.ts
@@ -1,11 +1,13 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit,
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, TrackByFunction,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { VDevType } from 'app/enums/v-dev-type.enum';
 import { PoolTopology } from 'app/interfaces/pool.interface';
 import { VDev } from 'app/interfaces/storage.interface';
+import { IxNestedTreeDataSource } from 'app/modules/ix-tree/ix-tree-nested-datasource';
 import { AppLoaderService, WebSocketService } from 'app/services';
 
 @UntilDestroy()
@@ -17,6 +19,12 @@ import { AppLoaderService, WebSocketService } from 'app/services';
 export class DevicesComponent implements OnInit {
   topology: PoolTopology;
   selectedItem: VDev;
+  dataSource: IxNestedTreeDataSource<VDev>;
+  treeControl = new NestedTreeControl<VDev, string>((vdev) => vdev.children, {
+    trackBy: (vdev) => vdev.guid,
+  });
+  readonly trackByFn: TrackByFunction<VDev> = (_, vdev) => vdev.guid;
+  readonly hasNestedChild = (_: number, vdev: VDev): boolean => Boolean(vdev.children?.length);
 
   constructor(
     private ws: WebSocketService,
@@ -35,6 +43,7 @@ export class DevicesComponent implements OnInit {
     this.ws.call('pool.query', [[['id', '=', Number(poolId)]]]).pipe(untilDestroyed(this)).subscribe(
       (pools) => {
         this.topology = pools[0].topology;
+        this.dataSource = new IxNestedTreeDataSource(this.topology.data);
         this.loader.close();
         this.cdr.markForCheck();
       },
@@ -44,5 +53,9 @@ export class DevicesComponent implements OnInit {
   onRowSelected(vdev: VDev, event: MouseEvent): void {
     event.stopPropagation();
     this.selectedItem = vdev;
+  }
+
+  onSearch(query: string): void {
+    console.info('onSearch', query);
   }
 }
--- src/app/pages/storage2/modules/devices/devices.module.ts
+++ src/app/pages/storage2/modules/devices/devices.module.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { AppCommonModule } from 'app/modules/common/app-common.module';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
+import { IxTreeModule } from 'app/modules/ix-tree/ix-tree.module';
 import { DevicesComponent } from 'app/pages/storage2/modules/devices/components/devices/devices.component';
 import {
   DiskDetailsPanelComponent,
@@ -21,14 +25,18 @@ import { routes } from 'app/pages/storage2/modules/devices/devices.routing';
 
 @NgModule({
   imports: [
-    MatCardModule,
+    AppCommonModule,
     CommonModule,
-    TranslateModule,
     RouterModule.forChild(routes),
-    MatDialogModule,
-    ReactiveFormsModule,
     IxFormsModule,
+    IxTreeModule,
     MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    FlexLayoutModule,
   ],
   exports: [],
   declarations: [
