cc9abb8317b0cdf19712149904304a2d834b5d8b
Evgeny Stepanovych <undsoft@gmail.com>
2022-07-08 21:22:21 +0200

    NAS-116415: SMART info card improvements

--- src/app/pages/storage2/modules/devices/components/smart-info-card/smart-info-card.component.ts
+++ src/app/pages/storage2/modules/devices/components/smart-info-card/smart-info-card.component.ts
@@ -2,7 +2,9 @@ import {
   ChangeDetectionStrategy, Component, Input, OnChanges,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import _ from 'lodash';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { SmartTestResultStatus } from 'app/enums/smart-test-result-status.enum';
@@ -26,8 +28,12 @@ export class SmartInfoCardComponent implements OnChanges {
   @Input() disk: Disk;
 
   totalResults$: Observable<LoadingState<number>>;
+  lastResultsInCategory$: Observable<SmartTestResult[]>;
+  smartTasksCount$: Observable<LoadingState<number>>;
 
-  private results$: Observable<SmartTestResult[]>;
+  readonly tasksMessage = T('{n, plural, one {# Task} other {# Tasks}} Configured');
+
+  private readonly maxResultCategories = 4;
 
   constructor(
     private ws: WebSocketService,
@@ -36,6 +42,7 @@ export class SmartInfoCardComponent implements OnChanges {
 
   ngOnChanges(): void {
     this.loadTestResults();
+    this.loadSmartTasks();
   }
 
   onManualTest(): void {
@@ -54,12 +61,29 @@ export class SmartInfoCardComponent implements OnChanges {
   }
 
   private loadTestResults(): void {
-    this.results$ = this.ws.call('smart.test.results', [[['disk', '=', this.topologyItem.disk]]])
-      .pipe(map((results) => results[0]?.tests ?? []));
-    this.totalResults$ = this.results$.pipe(
-      map((results) => {
-        return results.filter((result) => result.status !== SmartTestResultStatus.Running).length;
+    const results$ = this.ws.call('smart.test.results', [[['disk', '=', this.topologyItem.disk]]]).pipe(
+      map((testResults) => {
+        const results = testResults[0]?.tests ?? [];
+        return results.filter((result) => result.status !== SmartTestResultStatus.Running);
       }),
+    );
+
+    this.totalResults$ = results$.pipe(
+      map((results) => results.length),
+      toLoadingState(),
+    );
+
+    this.lastResultsInCategory$ = results$.pipe(
+      map((results) => {
+        const lastResultsInCategories = _.uniqBy(results, (result) => result.description);
+        return lastResultsInCategories.slice(0, this.maxResultCategories);
+      }),
+    );
+  }
+
+  private loadSmartTasks(): void {
+    this.smartTasksCount$ = this.ws.call('smart.test.query_for_disk', [this.topologyItem.disk]).pipe(
+      map((tasks) => tasks.length),
       toLoadingState(),
     );
   }
