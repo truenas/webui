747b257210c24a2c544e1ef0af2717687e81bfc8
Boris Vasilenko <bvasilenko@ixsystems.com>
2022-07-13 10:15:09 +0300

    NAS-116916: tests for disk-node

---
+++ src/app/pages/storage2/modules/devices/components/disk-node/disk-node.component.spec.ts
@@ -0,0 +1,51 @@
+import { MatIcon } from '@angular/material/icon';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { DiskType } from 'app/enums/disk-type.enum';
+import { VDev } from 'app/interfaces/storage.interface';
+import { DiskNodeComponent } from 'app/pages/storage2/modules/devices/components/disk-node/disk-node.component';
+
+describe('DiskNodeComponent', () => {
+  let spectator: Spectator<DiskNodeComponent>;
+  const disk = {
+    type: 'DISK',
+    path: null,
+    guid: '123',
+    status: 'OFFLINE',
+    stats: {
+      read_errors: 1,
+      write_errors: 2,
+      checksum_errors: 3,
+    },
+    children: [],
+    disk: 'sdf',
+  } as VDev;
+  const type = DiskType.Hdd;
+  const size = 1024 * 1024 * 16;
+  const createComponent = createComponentFactory({
+    component: DiskNodeComponent,
+  });
+
+  beforeEach(() => {
+    spectator = createComponent({
+      props: { disk, type, size },
+    });
+  });
+
+  it('shows "Device Name"', () => {
+    expect(spectator.query('.name')).toHaveText('sdf');
+    expect(spectator.query(MatIcon).svgIcon).toBe('ix-hdd');
+  });
+
+  it('shows "Status"', () => {
+    expect(spectator.query('.cell-status span')).toHaveText('OFFLINE');
+    expect(spectator.component.statusColor).toEqual('var(--magenta)');
+  });
+
+  it('shows "Capacity"', () => {
+    expect(spectator.query('.cell-capacity')).toHaveText('16.00MiB');
+  });
+
+  it('shows "Errors"', () => {
+    expect(spectator.query('.cell-errors')).toHaveText('6 Errors');
+  });
+});
