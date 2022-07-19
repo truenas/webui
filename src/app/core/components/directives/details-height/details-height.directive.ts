b221e07348eed91e92ab35a2e00bdad6226c5725
Boris Vasilenko <bvasilenko@ixsystems.com>
2022-07-19 16:50:26 +0300

    NAS-117024: code improvements

--- src/app/core/components/directives/details-height/details-height.directive.ts
+++ src/app/core/components/directives/details-height/details-height.directive.ts
@@ -3,11 +3,16 @@ import {
 } from '@angular/core';
 import { WINDOW } from 'app/helpers/window.helper';
 
+///
+/// This directive is used to dynamically adjust height of the "details" block in a "master-details" layout
+/// to fill the bottom space, which becomes available when user scrolls the page down,
+/// so the page's heading is shifted off the screen
+///
 @Directive({
   selector: '[ixDetailsHeight]',
 })
 export class IxDetailsHeightDirective implements OnInit, OnDestroy {
-  @Input() ixDetailsHeightParantClass: string;
+  @Input() ixDetailsHeightParentClass: string;
   @Input() hiddenHeight = 230;
   @Input() maxShowHeight = 150;
 
@@ -29,13 +34,16 @@ export class IxDetailsHeightDirective implements OnInit, OnDestroy {
   }
 
   onScroll(event: Event): void {
-    if ((event.target as HTMLElement).className === this.ixDetailsHeightParantClass) {
-      if ((event.target as HTMLElement).scrollTop < this.maxShowHeight) {
-        this.heightDetails = `calc(100vh - ${this.hiddenHeight}px + ${(event.target as HTMLElement).scrollTop}px)`;
-      } else {
-        this.heightDetails = `calc(100vh - ${this.hiddenHeight}px + ${this.maxShowHeight}px)`;
-      }
-      this.el.nativeElement.style.height = this.heightDetails;
+    if ((event.target as HTMLElement).className !== this.ixDetailsHeightParentClass) {
+      return;
     }
+
+    if ((event.target as HTMLElement).scrollTop < this.maxShowHeight) {
+      this.heightDetails = `calc(100vh - ${this.hiddenHeight}px + ${(event.target as HTMLElement).scrollTop}px)`;
+    } else {
+      this.heightDetails = `calc(100vh - ${this.hiddenHeight}px + ${this.maxShowHeight}px)`;
+    }
+
+    this.el.nativeElement.style.height = this.heightDetails;
   }
 }
