9c3b9993509578a3017764c110e9d7203b52dda2
akarpov <karpov4you@gmail.com>
2022-07-25 08:37:46 +0300

    NAS-116397: Fix git branchUnit tests

--- src/app/pages/storage2/components/disk-health-card/disk-health-card.component.spec.ts
+++ src/app/pages/storage2/components/disk-health-card/disk-health-card.component.spec.ts
@@ -1,7 +1,9 @@
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatButtonHarness } from '@angular/material/button/testing';
-import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { createComponentFactory, Spectator, mockProvider } from '@ngneat/spectator/jest';
+import { mockCall, mockWebsocket } from 'app/core/testing/utils/mock-websocket.utils';
+import { SnackbarService } from 'app/modules/snackbar/services/snackbar.service';
 import { DiskHealthCardComponent } from 'app/pages/storage2/components/disk-health-card/disk-health-card.component';
 
 describe('DiskHealthCardComponent', () => {
@@ -9,6 +11,12 @@ describe('DiskHealthCardComponent', () => {
   let loader: HarnessLoader;
   const createComponent = createComponentFactory({
     component: DiskHealthCardComponent,
+    providers: [
+      mockProvider(SnackbarService),
+      mockWebsocket([
+        mockCall('disk.temperatures'),
+      ]),
+    ],
   });
 
   beforeEach(() => {
@@ -18,6 +26,7 @@ describe('DiskHealthCardComponent', () => {
 
   it('shows a button to manage all disks', async () => {
     const manageDisksButton = await loader.getHarness(MatButtonHarness.with({ text: 'Manage Disks' }));
+
     expect(manageDisksButton).toBeTruthy();
     expect(await (await manageDisksButton.host()).getAttribute('href')).toBe('/storage2/disks');
   });
