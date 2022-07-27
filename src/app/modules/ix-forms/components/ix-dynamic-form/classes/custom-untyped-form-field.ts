c03d158709a67ac18af99b80a26ae99b77df08b8
Rehan Yousaf <rehanyousaf147@gmail.com>
2022-07-27 13:49:50 +0500

    NAS-117108: Fixed placement

--- src/app/modules/ix-forms/components/ix-dynamic-form/classes/custom-untyped-form-field.ts
+++ src/app/modules/ix-forms/components/ix-dynamic-form/classes/custom-untyped-form-field.ts
@@ -2,7 +2,6 @@ import { CustomUntypedFormArray } from 'app/modules/ix-forms/components/ix-dynam
 import { CustomUntypedFormControl } from 'app/modules/ix-forms/components/ix-dynamic-form/classes/custom-untped-form-control';
 import { CustomUntypedFormGroup } from 'app/modules/ix-forms/components/ix-dynamic-form/classes/custom-untyped-form-group';
 
-export type CustomUntypedFormField =
-    CustomUntypedFormArray |
-    CustomUntypedFormControl |
-    CustomUntypedFormGroup;
+export type CustomUntypedFormField = CustomUntypedFormArray
+| CustomUntypedFormControl
+| CustomUntypedFormGroup;
