c5e65c0b4596de0851354e095da391b1eccf7b3d
Rehan Yousaf <rehanyousaf147@gmail.com>
2022-07-29 05:29:24 +0500

    NAS-116406: Added snackbar

--- src/app/pages/datasets/components/data-protection-card/data-protection-card.component.ts
+++ src/app/pages/datasets/components/data-protection-card/data-protection-card.component.ts
@@ -1,20 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { TranslateService } from '@ngx-translate/core';
+import { filter } from 'rxjs/operators';
+import { SnackbarService } from 'app/modules/snackbar/services/snackbar.service';
 import { SnapshotAddFormComponent } from 'app/pages/datasets/modules/snapshots/snapshot-add-form/snapshot-add-form.component';
 import { DatasetInTree } from 'app/pages/datasets/store/dataset-in-tree.interface';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 
+@UntilDestroy()
 @Component({
   selector: 'ix-data-protection-card',
   templateUrl: './data-protection-card.component.html',
   styleUrls: ['./data-protection-card.component.scss'],
 })
-export class DataProtectionCardComponent {
+export class DataProtectionCardComponent implements OnInit {
   readonly console = console;
   @Input() dataset: DatasetInTree;
 
   constructor(
     private slideIn: IxSlideInService,
+    private snackbarService: SnackbarService,
+    private translate: TranslateService,
   ) {}
+
+  ngOnInit(): void {
+    this.slideIn.onClose$.pipe(
+      filter((value) => value.modalType === SnapshotAddFormComponent),
+      untilDestroyed(this),
+    ).subscribe(() => {
+      this.snackbarService.success(this.translate.instant('Snapshot added successfully.'));
+    });
+  }
 
   addSnapshot(): void {
     const addForm = this.slideIn.open(SnapshotAddFormComponent);
