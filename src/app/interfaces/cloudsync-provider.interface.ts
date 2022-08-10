22ddfaafc01d69e30ae619ac399b1d30847291bc
Evgeny Stepanovych <undsoft@gmail.com>
2022-08-10 21:05:23 +0200

    NAS-114413: Fixing master

--- src/app/interfaces/cloudsync-provider.interface.ts
+++ src/app/interfaces/cloudsync-provider.interface.ts
@@ -8,7 +8,7 @@ export interface CloudsyncProvider {
   credentials_schema: any[];
   name: CloudsyncProviderName;
   task_schema: unknown[]; // Not really used
-  title: CloudsyncProviderName;
+  title: string;
 }
 
 export type CloudsyncRestoreParams = [
