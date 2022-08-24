b6a3c8f80d44a69eb0d9b97e2c7d569102e4d377
Evgeny Stepanovych <undsoft@gmail.com>
2022-08-24 12:28:57 +0200

    NAS-117769: Support for multiple in ix-explorer

--- src/app/pages/credentials/backup-credentials/backup-credentials.component.ts
+++ src/app/pages/credentials/backup-credentials/backup-credentials.component.ts
@@ -14,7 +14,6 @@ import { AppTableAction, AppTableConfig } from 'app/modules/entity/table/table.c
 import {
   CloudCredentialsFormComponent,
 } from 'app/pages/credentials/backup-credentials/cloud-credentials-form/cloud-credentials-form.component';
-import { DemoComponent } from 'app/pages/credentials/backup-credentials/demo/demo.component';
 import {
   SshConnectionFormComponent,
 } from 'app/pages/credentials/backup-credentials/ssh-connection-form/ssh-connection-form.component';
@@ -158,10 +157,6 @@ export class BackupCredentialsComponent implements OnInit {
       }
       return item;
     });
-  }
-
-  onTest(): void {
-    this.slideInService.open(DemoComponent);
   }
 
   sshConnectionsDataSourceHelper(res: KeychainCredential[]): KeychainSshCredentials[] {
