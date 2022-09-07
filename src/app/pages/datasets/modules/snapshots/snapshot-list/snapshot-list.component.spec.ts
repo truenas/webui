5de74a759b11ba5808f3e8d10f57b2500720eba0
Boris Vasilenko <dreaktor0@gmail.com>
2022-09-07 09:12:51 +0300

    NAS-117631 / 22.12 / NAS-117631: Rework to expansible/collapsible snapshots rows (#7037)
    * NAS-117631: Rework to expansible/collapsible snapshots rows
    
    * NAS-117631: Fix loader after deleting snapshot
    
    * NAS-117631: loading indication, restore "show extra columns"
    
    * NAS-117631: Update a filter string variable after changing it
    
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/datasets/modules/snapshots/snapshot-list/snapshot-list.component.spec.ts
+++ src/app/pages/datasets/modules/snapshots/snapshot-list/snapshot-list.component.spec.ts
@@ -135,8 +135,8 @@ describe('SnapshotListComponent', () => {
     const table = await loader.getHarness(IxTableHarness);
     const expectedRows = [
       ['', 'Dataset', 'Snapshot', ''],
-      ['', 'test-dataset', 'second-snapshot', 'more_vert'],
-      ['', 'test-dataset', 'first-snapshot', 'more_vert'],
+      ['', 'test-dataset', 'second-snapshot', 'expand_more'],
+      ['', 'test-dataset', 'first-snapshot', 'expand_more'],
     ];
     const tableData = await table.getCells(true);
     expect(tableData).toEqual(expectedRows);
@@ -155,8 +155,8 @@ describe('SnapshotListComponent', () => {
     const tableData = await table.getCells(true);
     const expectedRows = [
       ['', 'Dataset', 'Snapshot', 'Used', 'Date created', 'Referenced', ''],
-      ['', 'test-dataset', 'second-snapshot', '1.49 TiB', '2021-11-05 10:52:06', '1.49 TiB', 'more_vert'],
-      ['', 'test-dataset', 'first-snapshot', '1.49 TiB', '2021-11-05 10:52:06', '1.49 TiB', 'more_vert'],
+      ['', 'test-dataset', 'second-snapshot', '1.49 TiB', '2021-11-05 10:52:06', '1.49 TiB', 'expand_more'],
+      ['', 'test-dataset', 'first-snapshot', '1.49 TiB', '2021-11-05 10:52:06', '1.49 TiB', 'expand_more'],
     ];
     expect(tableData).toEqual(expectedRows);
   });
