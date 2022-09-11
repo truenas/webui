dad5da7789c87f73736dd743aaa9a6e05494163f
q5sys <jt@obs-sec.com>
2022-09-11 18:08:21 -0400

    enable fixed storage tests for cycle run

--- tests/bdd/bluefin/test_002_0_storage_tests.py
+++ tests/bdd/bluefin/test_002_0_storage_tests.py
@@ -47,22 +47,22 @@ def the_browser_is_open_navigate_to_the_scale_url(driver, nas_ip, root_password)
 @then('wipe one disk')
 def wipe_one_disk(driver):
     """wipe one disk"""
-#    import t_002_1_wipe_one_disk
-#    t_002_1_wipe_one_disk.test_wipe_one_disk(driver)
+    import t_002_1_wipe_one_disk
+    t_002_1_wipe_one_disk.test_wipe_one_disk(driver)
 
 
 @then('create pool for system dataset')
 def create_pool_for_system_dataset(driver):
     """create pool for system dataset"""
-#    import t_002_2_create_pool_for_system_dataset
-#    t_002_2_create_pool_for_system_dataset.test_create_pool_for_system_dataset(driver)
+    import t_002_2_create_pool_for_system_dataset
+    t_002_2_create_pool_for_system_dataset.test_create_pool_for_system_dataset(driver)
 
 
 @then(parsers.parse('setup ad with "{ad_ns}" "{ad_domain}" "{ad_user}" "{ad_password}" "{ca_ou}" "{cmd1}" "{ad_object1}" "{cmd2}" "{dataset_name}" "{group_name}"'))
 def setup_ad(driver, nas_ip, root_password, ad_ns, ad_domain, ad_user, ad_password, ca_ou, cmd1, ad_object1, cmd2, dataset_name, group_name):
     """setup ad"""
-#    import t_002_3_setup_ad
-#    t_002_3_setup_ad.test_setup_ad(driver, nas_ip, root_password, ad_ns, ad_domain, ad_user, ad_password, ca_ou, cmd1, ad_object1, cmd2, dataset_name, group_name)
+    import t_002_3_setup_ad
+    t_002_3_setup_ad.test_setup_ad(driver, nas_ip, root_password, ad_ns, ad_domain, ad_user, ad_password, ca_ou, cmd1, ad_object1, cmd2, dataset_name, group_name)
 
 
 @then(parsers.parse('create ad dataset with {dataset_name} {group_name}'))
