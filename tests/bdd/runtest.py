45d61a50d9e9a2dc236ce6b5759f9dc30aee7488
ericbsd <ericturgeon.bsd@gmail.com>
2022-09-15 10:30:16 -0300

    Fixed pytest version for FreeBSD

--- tests/bdd/runtest.py
+++ tests/bdd/runtest.py
@@ -8,7 +8,7 @@ from platform import system
 from subprocess import run
 major_v = sys.version_info.major
 minor_v = sys.version_info.minor
-version = f"{major_v}" if system() == "Linux" else f"{major_v}.{minor_v}"
+version = f"-{major_v}" if system() == "Linux" else f""
 cwd = str(os.getcwd())
 screenshot_path = f"{cwd}/screenshot"
 argument = sys.argv
@@ -159,7 +159,7 @@ def run_testing():
 
     convert_jira_feature_file(test_suite)
     pytest_cmd = [
-        f"pytest-{version}",
+        f"pytest{version}",
         "-v",
         test_suite,
         "--junitxml=results/junit/webui_test.xml",
