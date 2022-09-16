607b56ab4915a2ae591aeabaa9ec182dc1939072
Evgeny Stepanovych <undsoft@gmail.com>
2022-09-16 15:50:16 +0200

    NAS-118185: Reducing amount of any's in code

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         "plugin:@angular-eslint/recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:rxjs/recommended"
       ],
       "plugins": [
         "rxjs",
@@ -165,6 +166,8 @@ module.exports = {
         // Other temporary disables
         "@typescript-eslint/no-unsafe-argument": "off",
         "@typescript-eslint/dot-notation": ["off", { allowIndexSignaturePropertyAccess: true }],
+        "rxjs/no-implicit-any-catch": ["off"],
+        "rxjs/no-nested-subscribe": ["off"],
 
         // Other overwrites
         "@typescript-eslint/lines-between-class-members": "off",
