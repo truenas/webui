c53aeaec6f269b17c4ef6b1e1b1cdab79613bc47
Evgeny Stepanovych <undsoft@gmail.com>
2022-09-18 12:15:27 +0200

    NAS-117481: Refactoring sign in form

--- src/app/modules/common/dialog/about/about-dialog.component.ts
+++ src/app/modules/common/dialog/about/about-dialog.component.ts
@@ -3,14 +3,12 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { ProductType, productTypeLabels } from 'app/enums/product-type.enum';
 import helptext from 'app/helptext/about';
 import { SystemGeneralService } from 'app/services';
-import { LocaleService } from 'app/services/locale.service';
 
 @Component({
   templateUrl: './about-dialog.component.html',
   styleUrls: ['./about-dialog.component.scss'],
 })
 export class AboutDialogComponent {
-  copyrightYear = this.localeService.getCopyrightYearFromBuildTime();
   systemType = this.systemGeneralService.getProductType();
   helptext = helptext;
   readonly ProductType = ProductType;
@@ -18,7 +16,6 @@ export class AboutDialogComponent {
 
   constructor(
     public dialogRef: MatDialogRef<AboutDialogComponent>,
-    private localeService: LocaleService,
     private systemGeneralService: SystemGeneralService,
   ) {}
 }
