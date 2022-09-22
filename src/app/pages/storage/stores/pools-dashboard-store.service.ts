b51d38100ffc588a52cb0cf60595795be6047701
RehanY147 <rehanyousaf147@gmail.com>
2022-09-22 15:34:48 +0500

    NAS-117846: Clean up

--- src/app/pages/storage/stores/pools-dashboard-store.service.ts
+++ src/app/pages/storage/stores/pools-dashboard-store.service.ts
@@ -9,7 +9,8 @@ import { SmartTestResultStatus } from 'app/enums/smart-test-result-status.enum';
 import { Alert } from 'app/interfaces/alert.interface';
 import { Dataset } from 'app/interfaces/dataset.interface';
 import { Pool } from 'app/interfaces/pool.interface';
-import { StorageDashboardDisk } from 'app/interfaces/storage.interface';
+import { SmartTestResults } from 'app/interfaces/smart-test.interface';
+import { Disk, DiskTemperatureAgg, StorageDashboardDisk } from 'app/interfaces/storage.interface';
 import { WebsocketError } from 'app/interfaces/websocket-error.interface';
 import { EntityUtils } from 'app/modules/entity/utils';
 import { DialogService, StorageService, WebSocketService } from 'app/services';
@@ -55,78 +56,131 @@ export class PoolsDashboardStore extends ComponentStore<PoolsDashboardState> {
           areDisksLoading: true,
         });
       }),
-      switchMap(() => {
-        return forkJoin({
-          dashboardPools: combineLatest({
-            pools: this.ws.call('pool.query', [[], { extra: { is_upgraded: true } }]),
-            rootDatasets: this.ws.call('pool.dataset.query', [[], { extra: { retrieve_children: false } }]),
-          }).pipe(
-            tapResponse(({ pools, rootDatasets }) => {
-              this.patchState({
-                arePoolsLoading: false,
-                pools: this.sorter.tableSorter(pools, 'name', 'asc'),
-                rootDatasets: _.keyBy(rootDatasets, (dataset) => dataset.id),
-              });
-            },
-            (error: WebsocketError) => {
-              this.patchState({
-                arePoolsLoading: false,
-              });
-              new EntityUtils().handleWsError(this, error, this.dialogService);
-            }),
-          ),
-          dashboardDisks: this.ws.call('disk.query', [[], { extra: { pools: true } }]).pipe(
-            switchMap((disks: StorageDashboardDisk[]) => {
-              const diskNames = disks.map((disk) => disk.name);
-              return combineLatest({
-                disks: of(disks),
-                alerts: this.ws.call('disk.temperature_alerts', [diskNames]),
-                disksWithTestResults: this.ws.call('smart.test.results', [[['disk', 'in', diskNames]]]),
-                tempAgg: this.ws.call('disk.temperature_agg', [diskNames, 14]),
-              });
-            }),
-            switchMap(({
-              disks, alerts, disksWithTestResults, tempAgg,
-            }) => {
-              for (const disk of disks) {
-                disk.smartTests = 0;
-                disk.alerts = [];
-              }
-              for (const alert of alerts as Alert[]) {
-                const alertArgs = ((alert.args) as { device: string; message: string });
-                const alertDevice = alertArgs.device.split('/').reverse()[0];
-                const alertDisk = disks.find((disk) => disk.name === alertDevice);
-                alertDisk.alerts.push(alert);
-              }
-              (disksWithTestResults as unknown as StorageDashboardDisk[]).forEach((diskWithResults) => {
-                const testDisk = disks.find((disk) => disk.devname === diskWithResults.devname);
-                const tests = diskWithResults?.tests ?? [];
-                const testsStillRunning = tests.filter((test) => test.status !== SmartTestResultStatus.Running);
-                testDisk.smartTests = testsStillRunning.length;
-              });
-              const disksWithTempData = Object.keys(tempAgg);
-              for (const diskWithTempData of disksWithTempData) {
-                const disk = disks.find((disk) => disk.devname === diskWithTempData);
-                disk.tempAggregates = { ...tempAgg[diskWithTempData] };
-              }
-              return of(disks);
-            }),
-          ).pipe(
-            tapResponse((disks: StorageDashboardDisk[]) => {
-              this.patchState({
-                disks,
-                areDisksLoading: false,
-              });
-            },
-            (error: WebsocketError) => {
-              this.patchState({
-                areDisksLoading: false,
-              });
-              new EntityUtils().handleWsError(this, error, this.dialogService);
-            }),
-          ),
-        });
-      }),
+      switchMap(() => this.getPoolsAndDisks()),
     );
   });
+
+  getPoolsAndDisks(): Observable<{
+    dashboardPools: { pools: Pool[]; rootDatasets: Dataset[] };
+    dashboardDisks: StorageDashboardDisk[];
+  }> {
+    return forkJoin({
+      dashboardPools: this.getPoolsAndRootDatasets(),
+      dashboardDisks: this.getDisksWithDashboardData(),
+    });
+  }
+
+  getPoolsAndRootDatasets(): Observable<{ pools: Pool[]; rootDatasets: Dataset[] }> {
+    return combineLatest({
+      pools: this.getPools(),
+      rootDatasets: this.getRootDatasets(),
+    }).pipe(
+      tapResponse(({ pools, rootDatasets }) => {
+        this.patchState({
+          arePoolsLoading: false,
+          pools: this.sorter.tableSorter(pools, 'name', 'asc'),
+          rootDatasets: _.keyBy(rootDatasets, (dataset) => dataset.id),
+        });
+      },
+      (error: WebsocketError) => {
+        this.patchState({
+          arePoolsLoading: false,
+        });
+        new EntityUtils().handleWsError(this, error, this.dialogService);
+      }),
+    );
+  }
+
+  getPools(): Observable<Pool[]> {
+    return this.ws.call('pool.query', [[], { extra: { is_upgraded: true } }]);
+  }
+
+  getRootDatasets(): Observable<Dataset[]> {
+    return this.ws.call('pool.dataset.query', [[], { extra: { retrieve_children: false } }]);
+  }
+
+  getDisksWithDashboardData(): Observable<StorageDashboardDisk[]> {
+    return this.getDisks().pipe(
+      switchMap(this.getDashboardDataForDisks.bind(this)),
+      switchMap(this.getProcessedDisks.bind(this)),
+    ).pipe(
+      tapResponse((disks: StorageDashboardDisk[]) => {
+        this.patchState({
+          disks,
+          areDisksLoading: false,
+        });
+      },
+      (error: WebsocketError) => {
+        this.patchState({
+          areDisksLoading: false,
+        });
+        new EntityUtils().handleWsError(this, error, this.dialogService);
+      }),
+    );
+  }
+
+  getDisks(): Observable<Disk[]> {
+    return this.ws.call('disk.query', [[], { extra: { pools: true } }]);
+  }
+
+  getDashboardDataForDisks(disks: StorageDashboardDisk[]): Observable<{
+    disks: StorageDashboardDisk[];
+    alerts: Alert[];
+    disksWithTestResults: SmartTestResults[];
+    tempAgg: DiskTemperatureAgg;
+  }> {
+    const disksNames = disks.map((disk) => disk.name);
+    return combineLatest({
+      disks: of(disks),
+      alerts: this.getTemperatureAlerts(disksNames),
+      disksWithTestResults: this.getSmartResults(disksNames),
+      tempAgg: this.getDiskTempAggregates(disksNames),
+    });
+  }
+
+  getTemperatureAlerts(disksNames: string[]): Observable<Alert[]> {
+    return this.ws.call('disk.temperature_alerts', [disksNames]);
+  }
+
+  getSmartResults(disksNames: string[]): Observable<SmartTestResults[]> {
+    return this.ws.call('smart.test.results', [[['disk', 'in', disksNames]]]);
+  }
+
+  getDiskTempAggregates(disksNames: string[]): Observable<DiskTemperatureAgg> {
+    return this.ws.call('disk.temperature_agg', [disksNames, 14]);
+  }
+
+  getProcessedDisks(
+    {
+      disks, alerts, disksWithTestResults, tempAgg,
+    }: {
+      disks: StorageDashboardDisk[];
+      alerts: Alert[];
+      disksWithTestResults: SmartTestResults[];
+      tempAgg: DiskTemperatureAgg;
+    },
+  ): Observable<StorageDashboardDisk[]> {
+    for (const disk of disks) {
+      disk.smartTests = 0;
+      disk.alerts = [];
+    }
+    for (const alert of alerts) {
+      const alertArgs = ((alert.args) as { device: string; message: string });
+      const alertDevice = alertArgs.device.split('/').reverse()[0];
+      const alertDisk = disks.find((disk) => disk.name === alertDevice);
+      alertDisk.alerts.push(alert);
+    }
+    (disksWithTestResults as unknown as StorageDashboardDisk[]).forEach((diskWithResults) => {
+      const testDisk = disks.find((disk) => disk.devname === diskWithResults.devname);
+      const tests = diskWithResults?.tests ?? [];
+      const testsStillRunning = tests.filter((test) => test.status !== SmartTestResultStatus.Running);
+      testDisk.smartTests = testsStillRunning.length;
+    });
+    const disksWithTempData = Object.keys(tempAgg);
+    for (const diskWithTempData of disksWithTempData) {
+      const disk = disks.find((disk) => disk.devname === diskWithTempData);
+      disk.tempAggregates = { ...tempAgg[diskWithTempData] };
+    }
+    return of(disks);
+  }
 }
