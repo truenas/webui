a7e226d0f54f324c159d433638270e82303cec8a
ericbsd <ericturgeon.bsd@gmail.com>
2022-09-30 14:02:57 -0300

    Added pytest verbose

--- tests/bdd/runtest.py
+++ tests/bdd/runtest.py
@@ -158,6 +158,7 @@ def run_testing():
         sys.executable,
         '-m',
         'pytest',
+        '-v',
         test_suite,
         "--junitxml=results/junit/webui_test.xml",
         "--cucumber-json=results/cucumber/webui_test.json"
