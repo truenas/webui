8e02c03c2b3ea3af81156e56a0219ad2580502e4
ericbsd <ericturgeon.bsd@gmail.com>
2022-09-30 09:02:12 -0300

    Changed runtest.py to start pytest with sys.executable for virtualenv
    This add proper support to for virtualenv and will run fine without virtualenv.

--- tests/bdd/runtest.py
+++ tests/bdd/runtest.py
@@ -4,11 +4,7 @@ import sys
 import os
 import getopt
 from configparser import ConfigParser
-from platform import system
 from subprocess import run
-major_v = sys.version_info.major
-minor_v = sys.version_info.minor
-version = f"-{major_v}" if system() == "Linux" else f""
 cwd = str(os.getcwd())
 screenshot_path = f"{cwd}/screenshot"
 argument = sys.argv
@@ -159,8 +155,9 @@ def run_testing():
 
     convert_jira_feature_file(test_suite)
     pytest_cmd = [
-        f"pytest{version}",
-        "-v",
+        sys.executable,
+        '-m',
+        'pytest',
         test_suite,
         "--junitxml=results/junit/webui_test.xml",
         "--cucumber-json=results/cucumber/webui_test.json"
