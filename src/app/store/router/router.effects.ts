af0978083cb6a8c41776ed9a14bab2518c6ade68
Denys Butenko <ua13dark@gmail.com>
2022-10-07 23:10:14 +0700

    NAS-118490: Translate title in router effects

--- src/app/store/router/router.effects.ts
+++ src/app/store/router/router.effects.ts
@@ -2,20 +2,24 @@ import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { routerNavigationAction, RouterNavigationAction } from '@ngrx/router-store';
+import { TranslateService } from '@ngx-translate/core';
 import { tap } from 'rxjs/operators';
 import { CustomRouterState } from 'app/store/router/custom-router-serializer';
 
 @Injectable()
 export class RouterEffects {
-  constructor(private actions$: Actions, private titleService: Title) {}
+  constructor(
+    private actions$: Actions,
+    private titleService: Title,
+    private translate: TranslateService,
+  ) {}
 
   navigate$ = createEffect(
     () => this.actions$.pipe(
       ofType<RouterNavigationAction<CustomRouterState>>(routerNavigationAction),
       tap((data: RouterNavigationAction<CustomRouterState>) => {
-        this.titleService.setTitle(
-          data.payload.routerState.title + ' - ' + window.location.hostname,
-        );
+        const translatedTitle = this.translate.instant(data.payload.routerState.title);
+        this.titleService.setTitle(`${translatedTitle} - ${window.location.hostname}`);
       }),
     ),
     { dispatch: false },
