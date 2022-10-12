7d197a3c6c13cb2f8e913aa30c86415c0ce90fcf
Oleksandr Karpov <Karpov4you@gmail.com>
2022-10-12 13:03:34 +0300

    NAS-118454: error fixes

--- src/app/pages/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import {
-  Component, OnInit, AfterViewInit, OnDestroy, ElementRef, TemplateRef, ViewChild,
+  Component, OnInit, AfterViewInit, OnDestroy, ElementRef, TemplateRef, ViewChild, Inject,
 } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { select, Store } from '@ngrx/store';
@@ -9,6 +9,7 @@ import { Subject } from 'rxjs';
 import { filter, take } from 'rxjs/operators';
 import { NetworkInterfaceAliasType, NetworkInterfaceType } from 'app/enums/network-interface.enum';
 import { ScreenType } from 'app/enums/screen-type.enum';
+import { WINDOW } from 'app/helpers/window.helper';
 import { Dataset } from 'app/interfaces/dataset.interface';
 import { CoreEvent } from 'app/interfaces/events';
 import { MemoryStatsEventData } from 'app/interfaces/events/memory-stats-event.interface';
@@ -120,11 +121,12 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
     private slideInService: IxSlideInService,
     private layoutService: LayoutService,
     private store$: Store<AppState>,
+    @Inject(WINDOW) private window: Window,
   ) {
-    window.onresize = () => {
+    this.window.onresize = () => {
       this.checkScreenSize();
     };
-    window.onload = () => {
+    this.window.onload = () => {
       this.checkScreenSize();
     };
   }
