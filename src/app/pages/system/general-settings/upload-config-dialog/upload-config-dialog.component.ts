d4a10c3692853173dc1b242bb8ea184b4ba617ad
Rehan Yousaf <rehanyousaf147@gmail.com>
2022-10-19 05:17:13 +0500

    NAS-118465: Update upload-config-dialog.component.ts

--- src/app/pages/system/general-settings/upload-config-dialog/upload-config-dialog.component.ts
+++ src/app/pages/system/general-settings/upload-config-dialog/upload-config-dialog.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { FormBuilder } from '@ngneat/reactive-forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { helptextSystemGeneral as helptext } from 'app/helptext/system/general';
-import { EntityUtils } from 'app/modules/entity/utils';
+import { EntityJobComponent } from 'app/modules/entity/entity-job/entity-job.component';
 import { AppLoaderService, DialogService } from 'app/services';
 import { IxFileUploadService } from 'app/services/ix-file-upload.service';
 
@@ -29,33 +29,52 @@ export class UploadConfigDialogComponent {
     private fileUpload: IxFileUploadService,
     private dialog: DialogService,
     private dialogRef: MatDialogRef<UploadConfigDialogComponent>,
+    private mdDialog: MatDialog,
   ) {}
 
   onSubmit(): void {
     this.loader.open();
+    const formData: FormData = new FormData();
 
-    this.fileUpload.onUploaded$
-      .pipe(untilDestroyed(this))
-      .subscribe(
-        () => {
-          this.loader.close();
-          this.dialogRef.close();
-          this.router.navigate(['/others/reboot']);
-        },
-      );
+    const dialogRef = this.mdDialog.open(EntityJobComponent,
+      { data: { title: 'Uploading and Applying Config', closeOnClickOutside: false } });
+    dialogRef.componentInstance.setDescription('Uploading and Applying Config');
+    formData.append('data', JSON.stringify({
+      method: 'config.upload',
+      params: [],
+    }));
+    formData.append('file', this.form.value.config[0]);
+    dialogRef.componentInstance.wspost('config.upload', formData);
+    dialogRef.componentInstance.success.pipe(untilDestroyed(this)).subscribe(() => {
+      dialogRef.close();
+      this.router.navigate(['/others/reboot']);
+    });
+    dialogRef.componentInstance.failure.pipe(untilDestroyed(this)).subscribe((res) => {
+      dialogRef.componentInstance.setDescription(res.error);
+    });
 
-    this.fileUpload.onUploading$
-      .pipe(untilDestroyed(this))
-      .subscribe({
-        error: (error) => {
-          this.loader.close();
-          new EntityUtils().handleWsError(this, error, this.dialog);
-        },
-      });
+    // this.fileUpload.onUploaded$
+    //   .pipe(untilDestroyed(this))
+    //   .subscribe(
+    //     () => {
+    //       this.loader.close();
+    //       this.dialogRef.close();
+    //       this.router.navigate(['/others/reboot']);
+    //     },
+    //   );
 
-    this.fileUpload.upload(
-      this.form.value.config[0],
-      'config.upload',
-    );
+    // this.fileUpload.onUploading$
+    //   .pipe(untilDestroyed(this))
+    //   .subscribe({
+    //     error: (error) => {
+    //       this.loader.close();
+    //       new EntityUtils().handleWsError(this, error, this.dialog);
+    //     },
+    //   });
+
+    // this.fileUpload.upload(
+    //   this.form.value.config[0],
+    //   'config.upload',
+    // );
   }
 }
