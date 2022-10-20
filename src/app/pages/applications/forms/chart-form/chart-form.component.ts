bd1fa9ae6a78e63090bd8cb11740bfa3e6fc386c
Evgeny Stepanovych <undsoft@gmail.com>
2022-10-20 21:34:05 +0200

    NAS-118668: Removing usages of `any`

--- src/app/pages/applications/forms/chart-form/chart-form.component.ts
+++ src/app/pages/applications/forms/chart-form/chart-form.component.ts
@@ -16,7 +16,6 @@ import {
   ChartRelease, ChartReleaseCreate, ChartSchema, ChartSchemaNode,
 } from 'app/interfaces/chart-release.interface';
 import { AddListItemEvent, DeleteListItemEvent, DynamicFormSchema } from 'app/interfaces/dynamic-form-schema.interface';
-import { HierarchicalObjectMap } from 'app/interfaces/hierarhical-object-map.interface';
 import { Job } from 'app/interfaces/job.interface';
 import { EntityJobComponent } from 'app/modules/entity/entity-job/entity-job.component';
 import { EntityUtils } from 'app/modules/entity/utils';
@@ -34,7 +33,7 @@ import { IxSlideInService } from 'app/services/ix-slide-in.service';
 })
 export class ChartFormComponent implements OnDestroy {
   title: string;
-  config: HierarchicalObjectMap<ChartFormValue>;
+  config: { [key: string]: ChartFormValue };
   catalogApp: CatalogApp;
   selectedVersionKey: string;
 
@@ -229,12 +228,24 @@ export class ChartFormComponent implements OnDestroy {
     fieldTobeDeleted: string,
   ): void {
     const keys = fieldTobeDeleted.split('.');
-    const value = _.get(data, fieldTobeDeleted) as HierarchicalObjectMap<ChartFormValue>;
+    let value: any = data;
+    for (let i = 0; i < keys.length - 1; i++) {
+      value = value[keys[i]];
+      if (value === undefined || value === null) {
+        break;
+      }
+    }
     if (value !== undefined && value !== null) {
       if (this.isNew) {
         delete value[keys[keys.length - 1]];
       } else {
-        const configValue = _.get(this.config, fieldTobeDeleted) as HierarchicalObjectMap<ChartFormValue>;
+        let configValue: any = this.config;
+        for (let i = 0; i < keys.length - 1; i++) {
+          configValue = configValue[keys[i]];
+          if (configValue === undefined || configValue === null) {
+            break;
+          }
+        }
         if (!configValue || !configValue[keys[keys.length - 1]]) {
           delete value[keys[keys.length - 1]];
         }
