82776c00b8070a309319624958b43b74cca77698
Boris Vasilenko <bvasilenko@ixsystems.com>
2022-10-22 07:17:41 +0300

    NAS-118517: Hide error dialog on 'CalledProcessError' error

--- src/app/pages/storage/stores/pools-dashboard-store.service.ts
+++ src/app/pages/storage/stores/pools-dashboard-store.service.ts
@@ -99,7 +99,9 @@ export class PoolsDashboardStore extends ComponentStore<PoolsDashboardState> {
         this.patchState({
           arePoolsLoading: false,
         });
-        new EntityUtils().handleWsError(this, error, this.dialogService);
+        if (this.showError(error)) {
+          new EntityUtils().handleWsError(this, error, this.dialogService);
+        }
       },
     );
   }
@@ -131,7 +133,9 @@ export class PoolsDashboardStore extends ComponentStore<PoolsDashboardState> {
         this.patchState({
           areDisksLoading: false,
         });
-        new EntityUtils().handleWsError(this, error, this.dialogService);
+        if (this.showError(error)) {
+          new EntityUtils().handleWsError(this, error, this.dialogService);
+        }
       },
     );
   }
@@ -201,5 +205,12 @@ export class PoolsDashboardStore extends ComponentStore<PoolsDashboardState> {
       }
     }
     return of(disks);
+  }
+
+  showError(error: WebsocketError): boolean {
+    if (error.trace.class === 'CalledProcessError') {
+      return false;
+    }
+    return true;
   }
 }
