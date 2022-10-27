3e4e5492a0f0c96bbf5f06e04b10eadc3baecbba
Evgeny Stepanovych <undsoft@gmail.com>
2022-10-27 11:31:19 +0300

    NAS-118683 / 22.12 / Extracting dialog components (#7239)

---
+++ src/app/pages/system/view-enclosure/components/set-enclosure-label-dialog/set-enclosure-label-dialog.component.ts
@@ -0,0 +1,74 @@
+import {
+  ChangeDetectionStrategy, Component, Inject, OnInit,
+} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { EntityUtils } from 'app/modules/entity/utils';
+import { AppLoaderService, DialogService, WebSocketService } from 'app/services';
+
+export interface SetEnclosureLabelDialogData {
+  enclosureId: string;
+  currentLabel: string;
+  defaultLabel: string;
+}
+
+@UntilDestroy()
+@Component({
+  templateUrl: './set-enclosure-label-dialog.component.html',
+  styleUrls: ['./set-enclosure-label-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SetEnclosureLabelDialogComponent implements OnInit {
+  form = this.formBuilder.group({
+    label: ['', Validators.required],
+    resetToDefault: [false],
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private ws: WebSocketService,
+    private loader: AppLoaderService,
+    private dialogRef: MatDialogRef<SetEnclosureLabelDialogComponent, string>,
+    private dialogService: DialogService,
+    @Inject(MAT_DIALOG_DATA) private data: SetEnclosureLabelDialogData,
+  ) { }
+
+  ngOnInit(): void {
+    this.form.patchValue({
+      label: this.data.currentLabel,
+    });
+
+    this.setFormRelationship();
+  }
+
+  onSubmit(): void {
+    this.loader.open();
+    const formValues = this.form.value;
+    const newLabel = formValues.resetToDefault ? this.data.defaultLabel : formValues.label;
+
+    this.ws.call('enclosure.update', [this.data.enclosureId, { label: newLabel }])
+      .pipe(untilDestroyed(this))
+      .subscribe({
+        next: () => {
+          this.loader.close();
+          this.dialogRef.close(newLabel);
+        },
+        error: (error) => {
+          this.loader.close();
+          new EntityUtils().handleWsError(this, error, this.dialogService);
+          this.dialogRef.close();
+        },
+      });
+  }
+
+  private setFormRelationship(): void {
+    this.form.controls['resetToDefault'].valueChanges.pipe(untilDestroyed(this)).subscribe((resetToDefault) => {
+      if (resetToDefault) {
+        this.form.controls['label'].disable();
+      } else {
+        this.form.controls['label'].enable();
+      }
+    });
+  }
+}
