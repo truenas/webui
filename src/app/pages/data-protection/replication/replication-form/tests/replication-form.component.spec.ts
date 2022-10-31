563741586f73db5dc5087e5b754bf87edb4b56e6
Evgeny Stepanovych <undsoft@gmail.com>
2022-10-31 18:23:39 +0100

    NAS-117660: Refactoring replication form

--- src/app/pages/data-protection/replication/replication-form/tests/replication-form.component.spec.ts
+++ src/app/pages/data-protection/replication/replication-form/tests/replication-form.component.spec.ts
@@ -3,6 +3,9 @@ import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonHarness } from '@angular/material/button/testing';
 import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { ExplorerNodeData } from 'app/interfaces/tree-node.interface';
+import { TreeNodeProvider } from 'app/modules/ix-forms/components/ix-explorer/tree-node-provider.interface';
+import { DatasetService } from 'app/services/dataset-service/dataset.service';
 import { of } from 'rxjs';
 import { mockCall, mockWebsocket } from 'app/core/testing/utils/mock-websocket.utils';
 import { LifetimeUnit } from 'app/enums/lifetime-unit.enum';
@@ -13,8 +16,10 @@ import { SchedulerModule } from 'app/modules/scheduler/scheduler.module';
 import {
   ReplicationFormComponent,
 } from 'app/pages/data-protection/replication/replication-form/replication-form.component';
-import { existingReplication } from 'app/pages/data-protection/replication/replication-form/tests/replication-form-test-data';
-import { KeychainCredentialService, LanguageService, ModalService } from 'app/services';
+import {
+  expectedNewReplication
+} from 'app/pages/data-protection/replication/replication-form/tests/replication-form-test-data';
+import { KeychainCredentialService, LanguageService, ModalService, WebSocketService } from 'app/services';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 import { LocaleService } from 'app/services/locale.service';
 
@@ -67,6 +72,20 @@ describe('ReplicationFormComponent', () => {
       mockProvider(IxSlideInService),
       mockProvider(LanguageService),
       mockProvider(LocaleService),
+      mockProvider(DatasetService, {
+        getDatasetNodeProvider(): TreeNodeProvider {
+          return () => of([
+            {
+              name: 'local1',
+              path: '/local1',
+            },
+            {
+              name: 'local2',
+              path: '/local2',
+            },
+          ] as ExplorerNodeData[]);
+        },
+      }),
     ],
   });
 
@@ -79,7 +98,6 @@ describe('ReplicationFormComponent', () => {
 
   describe('general form functionality', () => {
     it('shows values for an existing replication task', async () => {
-      spectator.component.setExistingReplication(existingReplication);
 
       const formValues = await form.getValues();
       expect(formValues).toEqual(2);
@@ -87,10 +105,27 @@ describe('ReplicationFormComponent', () => {
 
     it('creates a new replication task when new form is saved', async () => {
       await form.fillForm({
-
+        Name: 'test',
+        Transport: 'LOCAL',
+        'Allow Blocks Larger than 128KB': false,
+        Source: ['/local1', '/local2'],
+        Destination: '/local2',
+        'Properties Override': ['override=true'],
+        'Properties Exclude': ['exclude'],
+        'Also Include Naming Schema': '%Y%m%d%H%M',
+        Schedule: true,
+      });
+      await form.fillForm({
+        Frequency: '0 * * * *',
+      });
+      await form.fillForm({
+        Begin: '11:00:00',
+        End: '22:00:00',
       });
 
       await saveButton.click();
+
+      expect(spectator.inject(WebSocketService).call).toHaveBeenCalledWith('replication.create', expectedNewReplication);
     });
 
     it('updates an existing replication task when update form is saved', async () => {
