c5031f7a386d203c936054441295c7f58e128d60
Boris Vasilenko <dreaktor0@gmail.com>
2022-11-08 17:21:55 +0300

    NAS-118823: Refactor Display device code in VmListComponent (#7278)
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/vm/vm.module.ts
+++ src/app/pages/vm/vm.module.ts
@@ -20,6 +20,7 @@ import { LayoutModule } from 'app/modules/layout/layout.module';
 import { TerminalModule } from 'app/modules/terminal/terminal.module';
 import { DeviceFormComponent } from 'app/pages/vm/devices/device-form/device-form.component';
 import { DeviceDeleteModalComponent } from 'app/pages/vm/devices/device-list/device-delete-modal/device-delete-modal.component';
+import { DisplayVmDialogComponent } from 'app/pages/vm/vm-list/display-vm-dialog/display-vm-dialog.component';
 import { VmSerialShellComponent } from 'app/pages/vm/vm-serial-shell/vm-serial-shell.component';
 import {
   VmService, NetworkService, SystemGeneralService,
@@ -64,6 +65,7 @@ import { routing } from './vm.routing';
     DeviceDeleteModalComponent,
     DeleteVmDialogComponent,
     StopVmDialogComponent,
+    DisplayVmDialogComponent,
     CloneVmDialogComponent,
   ],
   providers: [VmService, EntityFormService, NetworkService, SystemGeneralService, MessageService],
