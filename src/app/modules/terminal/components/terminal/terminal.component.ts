8a49ebe4c352b82a975099f3edea53c723ae667b
Oleksandr Karpov <Karpov4you@gmail.com>
2022-11-10 15:48:38 +0200

    NAS-117214: Courier added

--- src/app/modules/terminal/components/terminal/terminal.component.ts
+++ src/app/modules/terminal/components/terminal/terminal.component.ts
@@ -37,7 +37,7 @@ export class TerminalComponent implements OnInit, AfterViewInit, OnDestroy {
   waitParentChanges = 300;
   fontSize = 14;
   featuredFontName = 'Inconsolata';
-  defaultFontName = 'monospace';
+  defaultFontName = 'Courier';
   xterm: Terminal;
   shellConnected = false;
   connectionId: string;
