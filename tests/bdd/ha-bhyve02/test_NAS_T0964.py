33775bd33cf56604c1a35bd05325be54b0d7b7b1
ericbsd <ericturgeon.bsd@gmail.com>
2022-11-17 17:14:21 -0400

    Adding code in test_NAS_T0964.py disable AD.

--- tests/bdd/ha-bhyve02/test_NAS_T0964.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0964.py
@@ -25,6 +25,49 @@ from pytest_dependency import depends
 @scenario('features/NAS-T964.feature', 'Create an SMB share with the Active Directory dataset')
 def test_create_an_smb_share_with_the_active_directory_dataset(driver):
     """Create an SMB share with the Active Directory dataset."""
+    assert wait_on_element(driver, 7, '//mat-list-item[@ix-auto="option__Credentials"]', 'clickable')
+    driver.find_element_by_xpath('//mat-list-item[@ix-auto="option__Credentials"]').click()
+    assert wait_on_element(driver, 7, '//div[contains(@class,"lidein-nav-md")]//mat-list-item[@ix-auto="option__Directory Services"]')
+    driver.find_element_by_xpath('//div[contains(@class,"lidein-nav-md")]//mat-list-item[@ix-auto="option__Directory Services"]').click()
+    assert wait_on_element_disappear(driver, 20, '//h6[contains(.,"Please wait")]')
+    assert wait_on_element(driver, 7, '//h1[text()="Directory Services"]')
+
+    assert wait_on_element(driver, 5, '//button[contains(.,"Settings")]', 'clickable')
+    driver.find_element_by_xpath('//button[contains(.,"Settings")]').click()
+
+    assert wait_on_element(driver, 5, '//h3[@class="ix-formtitle" and text()="Active Directory"]')
+    assert wait_on_element(driver, 7, '//ix-checkbox[@formcontrolname="enable"]//mat-checkbox', 'clickable')
+    driver.find_element_by_xpath('//ix-checkbox[@formcontrolname="enable"]//mat-checkbox').click()
+    assert wait_on_element(driver, 7, '//button[contains(*/text(),"Save")]', 'clickable')
+    driver.find_element_by_xpath('//button[contains(*/text(),"Save")]').click()
+
+    assert wait_on_element_disappear(driver, 60, xpaths.progress.progressbar)
+    assert wait_on_element_disappear(driver, 60, '//h1[text()="Active Directory"]')
+
+    assert wait_on_element(driver, 7, '//button[contains(*/text(),"Show")]', 'clickable')
+    driver.find_element_by_xpath('//button[contains(*/text(),"Show")]').click()
+
+    assert wait_on_element(driver, 5, '//h1[text()="Warning"]')
+    assert wait_on_element(driver, 7, '//button[contains(*/text(),"Continue")]', 'clickable')
+    driver.find_element_by_xpath('//button[contains(*/text(),"Continue")]').click()
+
+    assert wait_on_element(driver, 7, '//tr[contains(.,"AD02.TN.IXSYSTEMS.NET")]//button', 'clickable')
+    driver.find_element_by_xpath('//tr[contains(.,"AD02.TN.IXSYSTEMS.NET")]//button').click()
+
+    assert wait_on_element(driver, 5, '//h1[text()="Delete"]')
+    assert wait_on_element(driver, 7, '//mat-checkbox[@name="confirm_checkbox"]', 'clickable')
+    driver.find_element_by_xpath('//mat-checkbox[@name="confirm_checkbox"]').click()
+    assert wait_on_element(driver, 7, '//button[@id="confirm-dialog__action-button"]', 'clickable')
+    driver.find_element_by_xpath('//button[@id="confirm-dialog__action-button"]').click()
+
+    assert wait_on_element(driver, 7, '//tr[contains(.,"AD_MACHINE_ACCOUNT")]//button', 'clickable')
+    driver.find_element_by_xpath('//tr[contains(.,"AD_MACHINE_ACCOUNT")]//button').click()
+
+    assert wait_on_element(driver, 5, '//h1[text()="Delete"]')
+    assert wait_on_element(driver, 7, '//mat-checkbox[@name="confirm_checkbox"]', 'clickable')
+    driver.find_element_by_xpath('//mat-checkbox[@name="confirm_checkbox"]').click()
+    assert wait_on_element(driver, 7, '//button[@id="confirm-dialog__action-button"]', 'clickable')
+    driver.find_element_by_xpath('//button[@id="confirm-dialog__action-button"]').click()
 
 
 @given(parsers.parse('the browser is open, navigate to "{host}"'))
