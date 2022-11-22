a96fd67e698f92fb9c1008b8bf873125c9483a09
RehanY147 <rehanyousaf147@gmail.com>
2022-11-23 04:23:20 +0500

    NAS-119071: Update user-form.component.spec.ts

--- src/app/pages/account/users/user-form/user-form.component.spec.ts
+++ src/app/pages/account/users/user-form/user-form.component.spec.ts
@@ -42,7 +42,7 @@ const mockUser = {
     id: 101,
   },
   groups: [101],
-  immutable: true,
+  immutable: false,
 } as User;
 
 describe('UserFormComponent', () => {
@@ -272,7 +272,7 @@ describe('UserFormComponent', () => {
     });
 
     it('check form inputs when user is builtin', async () => {
-      spectator.component.setupForm({ ...mockUser, builtin: true });
+      spectator.component.setupForm({ ...mockUser, builtin: true, immutable: true });
 
       const form = await loader.getHarness(IxFormHarness);
       const disabled = await form.getDisabledState();
