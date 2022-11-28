cf860f0b957f39ebe6b1c5be519c0ef0265f6e5f
Oleksandr Karpov <Karpov4you@gmail.com>
2022-11-28 13:24:38 +0200

    NAS-118660: Cloud sync task "Bandwith Limit" pop-up help text appears to be incorrect

--- src/app/helptext/data-protection/cloudsync/cloudsync-form.ts
+++ src/app/helptext/data-protection/cloudsync/cloudsync-form.ts
@@ -152,7 +152,7 @@ export default {
   bwlimit_tooltip: T('A single bandwidth limit or bandwidth limit schedule in rclone format.\
  Separate entries by pressing <code>Enter</code>. Example: \
  <samp>08:00,512 12:00,10MB 13:00,512 18:00,30MB 23:00,off</samp>.\
- Units can be specified with the beginning letter: <samp>b</samp>,\
+ Units can be specified with a suffix of <samp>b</samp>,\
  <samp>k</samp> (default), <samp>M</samp>, or <samp>G</samp>.\
  See <a href="https://rclone.org/docs/#bwlimit-bandwidth-spec"\
  target="_blank">rclone --bwlimit</a>.'),
