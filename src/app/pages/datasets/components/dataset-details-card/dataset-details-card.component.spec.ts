094cfedf81ac7c714733dccfa5d1193f50715951
Evgeny Stepanovych <undsoft@gmail.com>
2022-12-12 14:36:43 +0100

    NAS-119279 / 23.10 / Missing an option to promote dataset (#7459)

--- src/app/pages/datasets/components/dataset-details-card/dataset-details-card.component.spec.ts
+++ src/app/pages/datasets/components/dataset-details-card/dataset-details-card.component.spec.ts
@@ -2,11 +2,13 @@ import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatLegacyButtonHarness as MatButtonHarness } from '@angular/material/legacy-button/testing';
 import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
+import { Router } from '@angular/router';
 import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
 import { MockComponents } from 'ng-mocks';
 import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
 import { of } from 'rxjs';
 import { CopyButtonComponent } from 'app/core/components/copy-btn/copy-btn.component';
+import { mockCall, mockWebsocket } from 'app/core/testing/utils/mock-websocket.utils';
 import { DatasetType } from 'app/enums/dataset.enum';
 import { ZfsPropertySource } from 'app/enums/zfs-property-source.enum';
 import { DatasetDetails } from 'app/interfaces/dataset.interface';
@@ -15,7 +17,7 @@ import { DatasetFormComponent } from 'app/pages/datasets/components/dataset-form
 import { DeleteDatasetDialogComponent } from 'app/pages/datasets/components/delete-dataset-dialog/delete-dataset-dialog.component';
 import { ZvolFormComponent } from 'app/pages/datasets/components/zvol-form/zvol-form.component';
 import { DatasetTreeStore } from 'app/pages/datasets/store/dataset-store.service';
-import { ModalService } from 'app/services';
+import { ModalService, WebSocketService } from 'app/services';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 
 const dataset = {
@@ -29,6 +31,7 @@ const dataset = {
   deduplication: { value: 'OFF' },
   casesensitive: false,
   comments: { value: 'Test comment', source: ZfsPropertySource.Local },
+  origin: null,
 } as DatasetDetails;
 
 describe('DatasetDetailsCardComponent', () => {
@@ -54,6 +57,7 @@ describe('DatasetDetailsCardComponent', () => {
     providers: [
       mockProvider(DatasetTreeStore, {
         datasetUpdated: jest.fn(),
+        selectedParentDataset$: of({ id: 'pool' }),
       }),
       mockProvider(ModalService, {
         openInSlideIn: jest.fn(() => fakeModalRef),
@@ -68,6 +72,10 @@ describe('DatasetDetailsCardComponent', () => {
           afterClosed: () => of(true),
         })),
       }),
+      mockWebsocket([
+        mockCall('pool.dataset.promote'),
+      ]),
+      mockProvider(Router),
     ],
   });
 
@@ -146,5 +154,26 @@ describe('DatasetDetailsCardComponent', () => {
     await deleteButton.click();
 
     expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(DeleteDatasetDialogComponent, { data: dataset });
+  });
+
+  describe('promoting dataset', () => {
+    it('does not show a Promote Dataset button when dataset cannot be promoted', async () => {
+      const promoteButton = await loader.getHarnessOrNull(MatButtonHarness.with({ text: 'Promote' }));
+      expect(promoteButton).toBeNull();
+    });
+
+    it('promotes dataset when dataset can be promoted and Promote Dataset button is pressed', async () => {
+      spectator.setInput('dataset', {
+        ...dataset,
+        origin: {
+          parsed: 'pool/origin',
+        },
+      } as DatasetDetails);
+
+      const promoteButton = await loader.getHarnessOrNull(MatButtonHarness.with({ text: 'Promote' }));
+      await promoteButton.click();
+
+      expect(spectator.inject(WebSocketService).call).toHaveBeenCalledWith('pool.dataset.promote', ['pool/child']);
+    });
   });
 });
