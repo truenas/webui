9082f162dbe50f2006a784e0fde064cfe8fd91d0
ericbsd <ericturgeon.bsd@gmail.com>
2022-12-22 12:01:15 -0400

    Revert wheel change in T1089

--- tests/bdd/scale/test_NAS_T1089.py
+++ tests/bdd/scale/test_NAS_T1089.py
@@ -19,9 +19,9 @@ from pytest_bdd import (
 from pytest_dependency import depends
 
 
-@scenario('features/NAS-T1089.feature', 'Add wheel to auxiliary group of a user')
-def test_add_wheel_to_auxiliary_group_of_a_user():
-    """Add wheel to auxiliary group of a user."""
+@scenario('features/NAS-T1089.feature', 'Add root to auxiliary group of a user')
+def test_add_root_to_auxiliary_group_of_a_user():
+    """Add root to auxiliary group of a user."""
 
 
 @given('the browser is open, the TrueNAS URL and logged in')
@@ -70,29 +70,29 @@ def the_user_field_should_expand_down_click_the_edit_button(driver):
     driver.find_element_by_xpath(xpaths.users.eric_edit_button).click()
 
 
-@then('the User Edit Page should open, add the wheel group and click save')
+@then('the User Edit Page should open, add the root group and click save')
 def the_user_edit_page_should_open_add_the_root_group_and_click_save(driver):
-    """the User Edit Page should open, add the wheel group and click save."""
+    """the User Edit Page should open, add the root group and click save."""
     assert wait_on_element(driver, 10, xpaths.addUser.edit_title)
     assert wait_on_element_disappear(driver, 10, xpaths.popup.pleaseWait)
     assert wait_on_element(driver, 7, xpaths.addUser.auxiliaryGroups_select, 'clickable')
     element = driver.find_element_by_xpath(xpaths.addUser.auxiliaryGroups_select)
-    # Scroll to wheel
+    # Scroll to root
     driver.execute_script("arguments[0].scrollIntoView();", element)
     time.sleep(0.5)
     driver.find_element_by_xpath(xpaths.addUser.auxiliaryGroups_select).click()
-    assert wait_on_element(driver, 7, xpaths.addUser.wheelGroup_option, 'clickable')
-    driver.find_element_by_xpath(xpaths.addUser.wheelGroup_option).click()
-    driver.find_element_by_xpath(xpaths.addUser.wheelGroup_option).send_keys(Keys.TAB)
+    assert wait_on_element(driver, 7, xpaths.addUser.rootGroup_option, 'clickable')
+    driver.find_element_by_xpath(xpaths.addUser.rootGroup_option).click()
+    driver.find_element_by_xpath(xpaths.addUser.rootGroup_option).send_keys(Keys.TAB)
     wait_on_element(driver, 10, xpaths.button.save, 'clickable')
     element = driver.find_element_by_xpath(xpaths.button.save)
     driver.execute_script("arguments[0].scrollIntoView();", element)
     driver.find_element_by_xpath(xpaths.button.save).click()
 
 
-@then('change should be saved, reopen the edit page, wheel group value should be visible')
-def change_should_be_saved_reopen_the_edit_page_wheel_group_value_should_be_visible(driver):
-    """change should be saved, reopen the edit page, wheel group value should be visible."""
+@then('change should be saved, reopen the edit page, root group value should be visible')
+def change_should_be_saved_reopen_the_edit_page_root_group_value_should_be_visible(driver):
+    """change should be saved, reopen the edit page, root group value should be visible."""
     assert wait_on_element_disappear(driver, 15, xpaths.progress.progressbar)
     assert wait_on_element(driver, 7, xpaths.users.title)
     assert wait_on_element(driver, 10, xpaths.users.eric_user, 'clickable')
@@ -103,19 +103,19 @@ def change_should_be_saved_reopen_the_edit_page_wheel_group_value_should_be_visi
     assert wait_on_element_disappear(driver, 10, xpaths.popup.pleaseWait)
     assert wait_on_element(driver, 7, xpaths.addUser.auxiliaryGroups_select, 'clickable')
     element = driver.find_element_by_xpath(xpaths.addUser.auxiliaryGroups_select)
-    # Scroll to wheel
+    # Scroll to root
     driver.execute_script("arguments[0].scrollIntoView();", element)
     time.sleep(0.5)
     driver.find_element_by_xpath(xpaths.addUser.auxiliaryGroups_select).click()
-    assert wait_on_element(driver, 7, xpaths.addUser.wheelGroup_option, 'clickable')
-    element = driver.find_element_by_xpath('//span[contains(.,"wheel")]')
-    # Scroll to wheel
+    assert wait_on_element(driver, 7, xpaths.addUser.rootGroup_option, 'clickable')
+    element = driver.find_element_by_xpath('//span[contains(.,"root")]')
+    # Scroll to root
     driver.execute_script("arguments[0].scrollIntoView();", element)
     time.sleep(0.5)
-    wait_for_value = wait_for_attribute_value(driver, 5, xpaths.addUser.wheelGroup_option, 'class', 'mat-selected')
+    wait_for_value = wait_for_attribute_value(driver, 5, xpaths.addUser.rootGroup_option, 'class', 'mat-selected')
     assert wait_for_value
     # return to dashboard
-    driver.find_element_by_xpath(xpaths.addUser.wheelGroup_option).send_keys(Keys.TAB)
+    driver.find_element_by_xpath(xpaths.addUser.rootGroup_option).send_keys(Keys.TAB)
     time.sleep(0.5)
     assert wait_on_element(driver, 10, xpaths.button.close_icon, 'clickable')
     driver.find_element_by_xpath(xpaths.button.close_icon).click()
