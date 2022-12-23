dcdc32a090a5eeff374dafd417e84b405023bea4
ericbsd <ericturgeon.bsd@gmail.com>
2022-12-23 14:51:22 -0400

    Updated and fixed steps and xpath in T1215

--- tests/bdd/scale/test_NAS_T1138.py
+++ tests/bdd/scale/test_NAS_T1138.py
@@ -27,7 +27,7 @@ def test_create_a_1gb_zvol_call_nopeer1_for_the_no_peer_iscsi_test_case():
 @given('the browser is open, the TrueNAS URL and logged in')
 def the_browser_is_open_the_truenas_url_and_logged_in(driver, nas_ip, root_password, request):
     """the browser is open, the TrueNAS URL and logged in."""
-    #depends(request, ['tank_pool'], scope='session')
+    depends(request, ['tank_pool'], scope='session')
     if nas_ip not in driver.current_url:
         driver.get(f"http://{nas_ip}")
         assert wait_on_element(driver, 10, xpaths.login.user_input)
