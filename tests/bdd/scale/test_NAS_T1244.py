28b0638a21d2e29202ed4d30078fd20f01307576
ericbsd <ericturgeon.bsd@gmail.com>
2022-12-28 17:13:50 -0400

    Updated and fixed T1244 steps and xpaths

--- tests/bdd/scale/test_NAS_T1244.py
+++ tests/bdd/scale/test_NAS_T1244.py
@@ -11,6 +11,7 @@ from pytest_bdd import (
     scenario,
     then,
     when,
+    parsers
 )
 from pytest_dependency import depends
 
@@ -23,7 +24,7 @@ def test_verify_locking_and_unlocking_volume_using_passphrase():
 @given('the browser is open, the TrueNAS URL and logged in')
 def the_browser_is_open_the_truenas_url_and_logged_in(driver, nas_ip, root_password, request):
     """the browser is open, the TrueNAS URL and logged in."""
-    depends(request, ['encrypted_pool'], scope='session')
+    #depends(request, ['encrypted_pool'], scope='session')
     if nas_ip not in driver.current_url:
         driver.get(f"http://{nas_ip}")
         assert wait_on_element(driver, 10, xpaths.login.user_input)
@@ -39,24 +40,26 @@ def the_browser_is_open_the_truenas_url_and_logged_in(driver, nas_ip, root_passw
         driver.find_element_by_xpath(xpaths.sideMenu.dashboard).click()
 
 
-@when('you are on the dashboard, click Storage on the side menu')
-def you_are_on_the_dashboard_click_storage_on_the_side_menu(driver):
-    """you are on the dashboard, click Storage on the side menu."""
+@when('on the dashboard, click Datasets on the side menu')
+def on_the_dashboard_click_datasets_on_the_side_menu(driver):
+    """on the dashboard, click Datasets on the side menu."""
     assert wait_on_element(driver, 10, xpaths.dashboard.title)
-    assert wait_on_element(driver, 10, xpaths.sideMenu.dashboard, 'clickable')
-    driver.find_element_by_xpath(xpaths.sideMenu.dashboard).click()
-    assert wait_on_element(driver, 10, xpaths.dashboard.title)
-    assert wait_on_element(driver, 10, xpaths.sideMenu.storage, 'clickable')
-    driver.find_element_by_xpath(xpaths.sideMenu.storage).click()
+    assert wait_on_element(driver, 10, xpaths.sideMenu.datasets, 'clickable')
+    driver.find_element_by_xpath(xpaths.sideMenu.datasets).click()
 
 
-@then('lock the pool when the pool page reloads')
-def lock_the_pool_when_the_pool_page_reloads(driver):
-    """lock the pool when the pool page reloads."""
+@then('on the Datasets page click on encrypted_pool')
+def on_the_datasets_page_click_on_encrypted_pool(driver):
+    """on the Datasets page click on encrypted_pool."""
     assert wait_on_element(driver, 7, xpaths.dataset.title)
     assert wait_on_element(driver, 7, xpaths.dataset.pool_tree_name('encrypted_pool'))
     driver.find_element_by_xpath(xpaths.dataset.pool_tree('encrypted_pool')).click()
     assert wait_on_element(driver, 7, xpaths.dataset.pool_selected('encrypted_pool'))
+
+
+@then('click on the the lock button of the encrypted pool')
+def click_on_the_the_lock_button_of_the_encrypted_pool(driver):
+    """click on the the lock button of the encrypted pool."""
     assert wait_on_element(driver, 4, xpaths.dataset.lock_button, 'clickable')
     driver.find_element_by_xpath(xpaths.dataset.lock_button).click()
     assert wait_on_element(driver, 10, xpaths.lockDataset.title)
@@ -69,9 +72,9 @@ def lock_the_pool_when_the_pool_page_reloads(driver):
     assert wait_on_element(driver, 10, xpaths.dataset.lockPool_icon)
 
 
-@then('enter 1234abcd and verify that an error shows')
-def enter_1234abcd_and_verify_that_an_error_shows(driver):
-    """enter 1234abcd and verify that an error shows."""
+@then('try to unlock the pool with a wrong passphrase it should fail')
+def try_to_unlock_the_pool_with_a_wrong_passphrase_it_should_fail(driver):
+    """try to unlock the pool with a wrong passphrase it should fail."""
     assert wait_on_element(driver, 7, xpaths.dataset.title)
     assert wait_on_element(driver, 7, xpaths.dataset.pool_tree_name('encrypted_pool'))
     driver.find_element_by_xpath(xpaths.dataset.pool_tree('encrypted_pool')).click()
@@ -81,32 +84,41 @@ def enter_1234abcd_and_verify_that_an_error_shows(driver):
     assert wait_on_element(driver, 10, xpaths.unlockDataset.title)
     assert wait_on_element(driver, 5, xpaths.unlockDataset.datasetPassphrase_input, 'inputable')
     driver.find_element_by_xpath(xpaths.unlockDataset.datasetPassphrase_input).clear()
-    driver.find_element_by_xpath(xpaths.unlockDataset.datasetPassphrase_input).send_keys("1234abcd")
+    driver.find_element_by_xpath(xpaths.unlockDataset.datasetPassphrase_input).send_keys("wrongpassphrase")
     assert wait_on_element(driver, 5, xpaths.button.save, 'clickable')
     driver.find_element_by_xpath(xpaths.button.save).click()
     assert wait_on_element(driver, 10, '//p[contains(.,"The following datasets cannot be unlocked.")]')
-    assert wait_on_element(driver, 5, '//mat-dialog-container//button[@ix-auto="button__CLOSE"]', 'clickable')
-    driver.find_element_by_xpath('//mat-dialog-container//button[@ix-auto="button__CLOSE"]').click()
+    assert wait_on_element(driver, 5, xpaths.button.CONTINUE, 'clickable')
+    driver.find_element_by_xpath(xpaths.button.CONTINUE).click()
+
+    assert wait_on_element(driver, 10, '//p[contains(.,"These datasets could not be unlocked.")]')
+    assert wait_on_element(driver, 10, xpaths.button.CLOSE, 'clickable')
+    driver.find_element_by_xpath(xpaths.button.CLOSE).click()
+
+    assert wait_on_element(driver, 10, xpaths.dataset.lockPool_icon)
 
 
-@then('enter abcd1234 and confirm')
-def enter_abcd1234_and_confirm(driver):
-    """enter abcd1234 and confirm."""
+@then(parsers.parse('try to unlock the pool with the right passphrase "{passphrase}" it should succeed'))
+def try_to_unlock_the_pool_with_the_right_passphrase_passphrase_it_should_succeed(driver, passphrase):
+    """try to unlock the pool with the right passphrase "{passphrase}" it should succeed."""
+    assert wait_on_element(driver, 7, xpaths.dataset.title)
+    driver.find_element_by_xpath(xpaths.dataset.pool_tree('encrypted_pool')).click()
+    assert wait_on_element(driver, 7, xpaths.dataset.pool_selected('encrypted_pool'))
+    assert wait_on_element(driver, 4, xpaths.dataset.unlock_button, 'clickable')
+    driver.find_element_by_xpath(xpaths.dataset.unlock_button).click()
+    assert wait_on_element(driver, 10, xpaths.unlockDataset.title)
     assert wait_on_element(driver, 5, xpaths.unlockDataset.datasetPassphrase_input, 'inputable')
     driver.find_element_by_xpath(xpaths.unlockDataset.datasetPassphrase_input).clear()
-    driver.find_element_by_xpath(xpaths.unlockDataset.datasetPassphrase_input).send_keys("abcd1234")
-    assert wait_on_element(driver, 5, xpaths.button.save, 'clickable')
+    driver.find_element_by_xpath(xpaths.unlockDataset.datasetPassphrase_input).send_keys(passphrase)
+    assert wait_on_element(driver, 10, xpaths.button.save, 'clickable')
     driver.find_element_by_xpath(xpaths.button.save).click()
 
-
-@then('unlock the pool')
-def unlock_the_pool(driver):
-    """unlock the pool."""
     assert wait_on_element(driver, 10, xpaths.unlockDataset.unlockDatasets_message1)
-    assert wait_on_element(driver, 5, xpaths.button.Continue, 'clickable')
-    driver.find_element_by_xpath(xpaths.button.Continue).click()
+    assert wait_on_element(driver, 10, xpaths.button.CONTINUE, 'clickable')
+    driver.find_element_by_xpath(xpaths.button.CONTINUE).click()
 
     assert wait_on_element(driver, 10, xpaths.unlockDataset.unlockDatasets_message2)
-    assert wait_on_element(driver, 10, xpaths.button.close, 'clickable')
-    driver.find_element_by_xpath(xpaths.button.close).click()
-    assert wait_on_element(driver, 10, xpaths.dataset.unlockPool_icon)
+    assert wait_on_element(driver, 10, xpaths.button.CLOSE, 'clickable')
+    driver.find_element_by_xpath(xpaths.button.CLOSE).click()
+
+    assert wait_on_element(driver, 20, xpaths.dataset.unlockPool_icon)
