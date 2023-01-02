875441c6507e36017547f7cefad8cc5b88746427
RehanY147 <rehanyousaf147@gmail.com>
2023-01-02 19:30:35 +0500

    NAS-119616: Changing from method to subject

--- src/app/pages/credentials/backup-credentials/cloud-credentials-form/provider-forms/storj-provider-form/storj-provider-form.component.ts
+++ src/app/pages/credentials/backup-credentials/cloud-credentials-form/provider-forms/storj-provider-form/storj-provider-form.component.ts
@@ -1,19 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { BehaviorSubject } from 'rxjs';
+import { CloudCredential } from 'app/interfaces/cloud-sync-task.interface';
 import {
   BaseProviderFormComponent,
 } from 'app/pages/credentials/backup-credentials/cloud-credentials-form/provider-forms/base-provider-form';
 
+@UntilDestroy()
 @Component({
   templateUrl: './storj-provider-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StorjProviderFormComponent extends BaseProviderFormComponent {
+export class StorjProviderFormComponent extends BaseProviderFormComponent implements AfterViewInit {
   form = this.formBuilder.group({
     access_key_id: ['', Validators.required],
     secret_access_key: ['', Validators.required],
   });
+  private formPatcher$ = new BehaviorSubject<CloudCredential['attributes']>({});
 
+  getFormSetter$ = (): BehaviorSubject<CloudCredential['attributes']> => {
+    return this.formPatcher$;
+  };
+
+  ngAfterViewInit(): void {
+    this.formPatcher$.pipe(untilDestroyed(this)).subscribe((values) => {
+      this.form.patchValue(values);
+    });
+  }
   constructor(
     private formBuilder: FormBuilder,
   ) {
