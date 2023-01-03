c8519c4789d742d293d4039e964af80011e084c1
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2023-01-03 12:57:20 +0100

    Merge branch 'master' of https://github.com/truenas/webui into NAS-119412
    (cherry picked from commit 52c3d9dffffab5d18bbb3c7d00fa7ea36bfb10a4)

--- src/app/pages/applications/applications.module.ts
+++ src/app/pages/applications/applications.module.ts
@@ -9,6 +9,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTabsModule } from '@angular/material/tabs';
