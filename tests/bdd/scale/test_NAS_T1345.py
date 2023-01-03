370ed586305c7653e453ec5aaed5c8305709ea41
ericbsd <ericturgeon.bsd@gmail.com>
2023-01-03 15:56:41 -0400

    Fixed xpaths and updated steps in T1345

--- tests/bdd/scale/test_NAS_T1345.py
+++ tests/bdd/scale/test_NAS_T1345.py
@@ -44,96 +44,45 @@ def the_browser_is_open_navigate_to_the_scale_url_and_login(driver, nas_ip, root
         driver.find_element_by_xpath(xpaths.sideMenu.dashboard).click()
 
 
-@when('on the Dashboard, click on apps')
-def on_the_dashboard_click_on_apps(driver):
-    """on the Dashboard, click on apps."""
+@when('on the Dashboard, click Apps on the side menu')
+def on_the_dashboard_click_apps_on_the_side_menu(driver):
+    """on the Dashboard, click Apps on the side menu."""
     assert wait_on_element(driver, 10, xpaths.dashboard.title)
     assert wait_on_element(driver, 10, xpaths.dashboard.systemInfoCardTitle)
     assert wait_on_element(driver, 10, xpaths.sideMenu.apps, 'clickable')
     driver.find_element_by_xpath(xpaths.sideMenu.apps).click()
 
 
-@then('the Apps page load, open available applications')
-def the_apps_page_load_open_available_applications(driver):
-    """the Apps page load, open available applications."""
+@then('on Application page click on the Available Applications tab')
+def on_application_page_click_on_the_available_applications_tab(driver):
+    """on Application page click on the Available Applications tab."""
     assert wait_on_element(driver, 10, xpaths.applications.availableApplications_tab, 'clickable')
     driver.find_element_by_xpath(xpaths.applications.availableApplications_tab).click()
     assert wait_on_element_disappear(driver, 30, xpaths.progress.spinner)
 
 
-@then('click install')
-def click_install(driver):
-    """click install."""
-    time.sleep(2)  # we have to wait for the page to settle down and the card to fully load
-    assert wait_on_element(driver, 20, '//mat-card[contains(.,"plex")]//span[contains(.,"Install")]', 'clickable')
-    driver.find_element_by_xpath('//mat-card[contains(.,"plex")]//span[contains(.,"Install")]').click()
+@then('on the Plex card click the Install button')
+def on_the_plex_card_click_the_install_button(driver):
+    """on the Plex card click the Install button."""
+    assert wait_on_element(driver, 7, xpaths.applications.card('plex'))
+    assert wait_on_element(driver, 20, xpaths.applications.install_button('plex'), 'clickable')
+    driver.find_element_by_xpath(xpaths.applications.install_button('plex')).click()
     if is_element_present(driver, xpaths.popup.pleaseWait):
         assert wait_on_element_disappear(driver, 10, xpaths.popup.pleaseWait)
 
 
-@then('set application name')
-def set_application_name(driver):
-    """set application name."""
-    assert wait_on_element(driver, 7, '//h3[contains(.,"plex")]')
+@then('Enter an application name')
+def enter_an_application_name(driver):
+    """Enter an application name."""
+    assert wait_on_element(driver, 7, xpaths.appSetup.title('plex'))
     assert wait_on_element(driver, 7, xpaths.appSetup.appName_input)
     driver.find_element_by_xpath(xpaths.appSetup.appName_input).clear()
     driver.find_element_by_xpath(xpaths.appSetup.appName_input).send_keys('plex-test')
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Application Name"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Application Name"]').click()
 
 
-@then('set plex configuration')
-def set_plex_configuration(driver):
-    """set plex configuration."""
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Plex Configuration"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Plex Configuration"]').click()
-
-
-@then('set networking')
-def set_networking(driver):
-    """set networking."""
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Networking"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Networking"]').click()
-
-
-@then('set storage')
-def set_storage(driver):
-    """set storage."""
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Storage"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Storage"]').click()
-
-
-@then('set Scaling/Upgrade Policy')
-def set_scalingupgrade_policy(driver):
-    """set Scaling/Upgrade Policy."""
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Scaling/Upgrade Policy"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Scaling/Upgrade Policy"]').click()
-
-
-@then('set Resource Reservation')
-def set_resource_reservation(driver):
-    """set Resource Reservation."""
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Resource Reservation"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Resource Reservation"]').click()
-
-
-@then('set Advanced DNS Settings')
-def set_advanced_dns_settings(driver):
-    """set Advanced DNS Settings."""
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Advanced DNS Settings"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Advanced DNS Settings"]').click()
-
-
-@then('set Resource Limits')
-def set_resource_limits(driver):
-    """set Resource Limits."""
-    assert wait_on_element(driver, 7, '//button[@ix-auto="button__NEXT_Resource Limits"]', 'clickable')
-    driver.find_element_by_xpath('//button[@ix-auto="button__NEXT_Resource Limits"]').click()
-
-
-@then('confirm options')
-def confirm_options(driver):
-    """confirm options."""
+@then('click save, wait for the installation to finish')
+def click_save_wait_for_the_installation_to_finish(driver):
+    """click save, wait for the installation to finish."""
     assert wait_on_element(driver, 7, xpaths.button.save, 'clickable')
     driver.find_element_by_xpath(xpaths.button.save).click()
 
@@ -141,9 +90,9 @@ def confirm_options(driver):
     assert wait_on_element_disappear(driver, 45, xpaths.popup.installing)
 
 
-@then('confirm installation is successful')
-def confirm_installation_is_successful(driver):
-    """confirm installation is successful."""
+@then('confirm installation is successful and the App is active')
+def confirm_installation_is_successful_and_the_app_is_active(driver):
+    """confirm installation is successful and the App is active."""
     assert wait_on_element(driver, 10, '//div[contains(text(),"Installed Applications")]', 'clickable')
     driver.find_element_by_xpath('//div[contains(text(),"Installed Applications")]').click()
     assert wait_on_element_disappear(driver, 30, xpaths.progress.spinner)
