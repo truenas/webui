6a04c3e7bbc29ac46123ff2c179c89b65348a487
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2023-01-16 13:59:15 +0100

    NAS-119512: reset state preferences on adminUiInitialized
    (cherry picked from commit 4a15f3b08f6c79d287d106bd00663b4c2d51c21d)

--- src/app/store/preferences/preferences.effects.ts
+++ src/app/store/preferences/preferences.effects.ts
@@ -13,7 +13,7 @@ import {
   autoRefreshReportsToggled,
   builtinGroupsToggled,
   builtinUsersToggled, guiFormSubmitted, localizationFormSubmitted,
-  preferencesLoaded, preferredColumnsUpdated, clearPreferencesState,
+  preferencesLoaded, preferredColumnsUpdated,
   themeNotFound,
   updateRebootAfterManualUpdate,
 } from 'app/store/preferences/preferences.actions';
@@ -28,7 +28,6 @@ export class PreferencesEffects {
   loadPreferences$ = createEffect(() => this.actions$.pipe(
     ofType(adminUiInitialized),
     mergeMap(() => {
-      this.store$.dispatch(clearPreferencesState());
       return this.ws.call('user.query', [[['id', '=', rootUserId]]]).pipe(
         map(([user]) => {
           const preferences = user.attributes.preferences;
--- src/app/store/preferences/preferences.reducer.ts
+++ src/app/store/preferences/preferences.reducer.ts
@@ -2,13 +2,14 @@ import { createReducer, on } from '@ngrx/store';
 import { Preferences } from 'app/interfaces/preferences.interface';
 import { DashConfigItem } from 'app/pages/dashboard/components/widget-controller/widget-controller.component';
 import { defaultTheme } from 'app/services/theme/theme.constants';
+import { adminUiInitialized } from 'app/store/admin-panel/admin.actions';
 import { defaultPreferences } from 'app/store/preferences/default-preferences.constant';
 import {
   autoRefreshReportsToggled,
   builtinGroupsToggled,
   builtinUsersToggled, guiFormClosedWithoutSaving, guiFormSubmitted,
   localizationFormSubmitted, noPreferencesFound,
-  preferencesLoaded, preferredColumnsUpdated, clearPreferencesState, themeChangedInGuiForm,
+  preferencesLoaded, preferredColumnsUpdated, themeChangedInGuiForm,
   themeNotFound,
   updateRebootAfterManualUpdate,
 } from 'app/store/preferences/preferences.actions';
@@ -34,9 +35,9 @@ export const preferencesReducer = createReducer(
 
   on(dashboardStateLoaded, (state, { dashboardState }) => ({ ...state, dashboardState })),
   on(noDashboardStateFound, (state) => ({ ...state, dashboardState: null })),
+  on(adminUiInitialized, () => ({ ...initialState, areLoaded: false })),
   on(preferencesLoaded, (state, { preferences }) => ({ ...state, preferences, areLoaded: true })),
   on(noPreferencesFound, (state) => ({ ...state, preferences: defaultPreferences, areLoaded: true })),
-  on(clearPreferencesState, (state) => ({ ...state, ...initialState })),
   on(sidenavUpdated, (state, sidenavStatus) => updatePreferences(state, { sidenavStatus })),
   on(preferredColumnsUpdated, (state, { columns }) => updatePreferences(state, {
     tableDisplayedColumns: columns,
