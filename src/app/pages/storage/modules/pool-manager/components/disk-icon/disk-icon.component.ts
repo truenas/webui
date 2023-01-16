0a8c94d47e9b87848a83b3b2118dfeeda24a49e8
RehanY147 <rehanyousaf147@gmail.com>
2023-01-16 15:14:08 +0500

    NAS-119477: PR fixes

--- src/app/pages/storage/modules/pool-manager/components/disk-icon/disk-icon.component.ts
+++ src/app/pages/storage/modules/pool-manager/components/disk-icon/disk-icon.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ix-disk-icon',
   templateUrl: './disk-icon.component.svg',
   styleUrls: ['./disk-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DiskIconComponent {
-    @Input() identifier: string;
-    @Input() size: string;
+  @Input() identifier: string;
+  @Input() size: string;
 }
--- src/app/pages/storage/modules/pool-manager/components/disk-info/disk-info.component.ts
+++ src/app/pages/storage/modules/pool-manager/components/disk-info/disk-info.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ix-disk-info',
   templateUrl: './disk-info.component.html',
   styleUrls: ['./disk-info.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DiskInfoComponent {
-    @Input() slot: number;
-    @Input() type: string;
-    @Input() model: string;
-    @Input() serial: string;
+  @Input() slot: number;
+  @Input() type: string;
+  @Input() model: string;
+  @Input() serial: string;
 }
