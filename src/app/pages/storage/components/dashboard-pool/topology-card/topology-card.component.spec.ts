59e5051d8601fc70c4bcc21bdf85f8ce5a2133b2
Evgeny Stepanovych <undsoft@gmail.com>
2023-01-20 15:11:46 +0000

    NAS-119923: Replacing usages of Websocket service

--- src/app/pages/storage/components/dashboard-pool/topology-card/topology-card.component.spec.ts
+++ src/app/pages/storage/components/dashboard-pool/topology-card/topology-card.component.spec.ts
@@ -3,15 +3,19 @@ import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 import { MockComponent } from 'ng-mocks';
 import { MockStorageGenerator, MockStorageScenario } from 'app/core/testing/utils/mock-storage-generator.utils';
 import { PoolCardIconType } from 'app/enums/pool-card-icon-type.enum';
+import { PoolStatus } from 'app/enums/pool-status.enum';
 import { TopologyItemType } from 'app/enums/v-dev-type.enum';
 import { Pool } from 'app/interfaces/pool.interface';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
 import {
   PoolCardIconComponent,
 } from 'app/pages/storage/components/dashboard-pool/pool-card-icon/pool-card-icon.component';
-import { TopologyCardComponent } from 'app/pages/storage/components/dashboard-pool/topology-card/topology-card.component';
+import {
+  TopologyCardComponent,
+} from 'app/pages/storage/components/dashboard-pool/topology-card/topology-card.component';
 
-describe('TopologyCardComponent', () => {
+// eslint-disable-next-line jest/no-focused-tests
+describe.only('TopologyCardComponent', () => {
   let spectator: Spectator<TopologyCardComponent>;
 
   const createComponent = createComponentFactory({
@@ -87,19 +91,19 @@ describe('TopologyCardComponent', () => {
     expect(spectator.query(PoolCardIconComponent).type).toBe(PoolCardIconType.Safe);
     expect(spectator.query(PoolCardIconComponent).tooltip).toBe('Everything is fine');
 
-    spectator.setInput('poolState', { healthy: false, status: 'ONLINE' } as unknown as Pool);
+    spectator.setInput('poolState', { healthy: false, status: PoolStatus.Online } as Pool);
     expect(spectator.query(PoolCardIconComponent).type).toBe(PoolCardIconType.Warn);
     expect(spectator.query(PoolCardIconComponent).tooltip).toBe('Pool is not healthy');
 
-    spectator.setInput('poolState', { healthy: true, status: 'OFFLINE' } as unknown as Pool);
+    spectator.setInput('poolState', { healthy: true, status: PoolStatus.Offline } as Pool);
     expect(spectator.query(PoolCardIconComponent).type).toBe(PoolCardIconType.Warn);
     expect(spectator.query(PoolCardIconComponent).tooltip).toBe('Pool contains OFFLINE Data VDEVs');
 
-    spectator.setInput('poolState', { healthy: true, status: 'REMOVED' } as unknown as Pool);
+    spectator.setInput('poolState', { healthy: true, status: PoolStatus.Removed } as Pool);
     expect(spectator.query(PoolCardIconComponent).type).toBe(PoolCardIconType.Error);
     expect(spectator.query(PoolCardIconComponent).tooltip).toBe('Pool contains REMOVED Data VDEVs');
 
-    spectator.setInput('poolState', { healthy: true, status: 'FAULTED' } as unknown as Pool);
+    spectator.setInput('poolState', { healthy: true, status: PoolStatus.Faulted } as Pool);
     expect(spectator.query(PoolCardIconComponent).type).toBe(PoolCardIconType.Error);
     expect(spectator.query(PoolCardIconComponent).tooltip).toBe('Pool contains FAULTED Data VDEVs');
   });
