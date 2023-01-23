d58d8c558a0e2916b62b62e517083f9fa40a1e84
Damian Szidiropulosz <damianos@knoxbug.org>
2023-01-23 13:48:33 -0500

    NAS-119749: Code style

--- src/app/pages/system/support/support.component.ts
+++ src/app/pages/system/support/support.component.ts
@@ -45,7 +45,7 @@ export class SupportComponent implements OnInit {
       }
 
       // Always try to show product image for iX Systems hardware regardless of platform or license
-      if (res.system_manufacturer && res.system_manufacturer.toLowerCase() == 'ixsystems' && this.isRackmount(res.system_product) === false) {
+      if (res.system_manufacturer && res.system_manufacturer.toLowerCase() == 'ixsystems' && !this.isRackmount(res.system_product)) {
         this.getFreeNASImage(res.system_product);
       } else {
         this.getTrueNASImage(res.system_product);
