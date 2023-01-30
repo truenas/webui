4581a31f68d4eedf90adc406508f284da3416251
ericbsd <ericturgeon.bsd@gmail.com>
2023-01-30 16:55:07 -0400

    Adding new test cases to Verify NFS works after failover

--- tests/bdd/xpaths.py
+++ tests/bdd/xpaths.py
@@ -408,10 +408,11 @@ class progress:
 
 class services:
     title = '//h1[text()="Services"]'
+    nfs_Service_Checkbox = '//tr[contains(.,"NFS")]//mat-checkbox'
     nfs_Service_Toggle = '//tr[contains(.,"NFS")]//mat-slide-toggle'
     nfs_Service_Button = '//tr[contains(.,"NFS")]//button'
     nfs_Service_Name = '//td[contains(text(),"NFS")]'
-    smb_Toggle = '//tr[contains(.,"SMB")]//mat-slide-toggle'
+    smb_Service_Toggle = '//tr[contains(.,"SMB")]//mat-slide-toggle'
     smb_Service_Name = '//td[contains(text(),"SMB")]'
     smb_Service_Button = '//tr[contains(.,"SMB")]//button'
     ssh_Service_Toggle = '//tr[contains(.,"SSH")]//mat-slide-toggle'
