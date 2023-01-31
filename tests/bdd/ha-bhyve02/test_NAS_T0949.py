b225ad1c00f988b1b3ae4fec8bf2b183077a0b53
ericbsd <ericturgeon.bsd@gmail.com>
2023-01-31 15:22:30 -0400

    Fixed some UI issue related to sudo and auxiliary group

--- tests/bdd/ha-bhyve02/test_NAS_T0949.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0949.py
@@ -128,21 +128,21 @@ def enable_permit_sudo_and_click_save(driver):
 def change_should_be_saved(driver):
     """Change should be saved."""
     assert wait_on_element_disappear(driver, 15, xpaths.progress.progressbar)
-    assert wait_on_element(driver, 7, '//h1[text()="Users"]')
+    assert wait_on_element(driver, 7, xpaths.users.title)
 
 
 @then('Open the user drop down to verify the value has been changed')
 def open_the_user_drop_down_to_verify_the_value_has_been_changed(driver):
     """Open the user drop down to verify the value has been changed."""
-    assert wait_on_element(driver, 7, '//tr[contains(.,"ericbsd")]/td', 'clickable')
-    driver.find_element_by_xpath('//tr[contains(.,"ericbsd")]/td').click()
-    assert wait_on_element(driver, 7, '//tr[contains(.,"ericbsd")]/following-sibling::ix-user-details-row//dt[contains(.,"Allowed Sudo Commands:")]')
+    assert wait_on_element(driver, 7, xpaths.users.eric_User, 'clickable')
+    driver.find_element_by_xpath(xpaths.users.eric_User).click()
 
 
 @then('Updated value should be visible')
 def updated_value_should_be_visible(driver):
     """Updated value should be visible."""
-    element_text = driver.find_element_by_xpath('//tr[contains(.,"ericbsd")]/following-sibling::ix-user-details-row//dt[contains(.,"Allowed Sudo Commands:")]/../dd').text
+    assert wait_on_element(driver, 7, xpaths.users.eric_Allowed_Sudo_Commands)
+    element_text = driver.find_element_by_xpath(xpaths.users.eric_Allowed_Sudo_Commands).text
     assert element_text == 'ALL'
 
 
