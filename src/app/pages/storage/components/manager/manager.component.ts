1832d0ebcef64034713bf86b419b5cc10d20ddd4
Evgeny Stepanovych <undsoft@gmail.com>
2023-02-06 19:15:08 +0000

    NAS-119941: Drives disappear when "Suggest Layout" is used in Create Pool
    (cherry picked from commit 75eb7601f4322a8326942d87d76ec49ebf4744ec)

--- src/app/pages/storage/components/manager/manager.component.ts
+++ src/app/pages/storage/components/manager/manager.component.ts
@@ -350,6 +350,7 @@ export class ManagerComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.addVdev('data', new ManagerVdev(this.firstDataVdevDisktype, 'data'));
+    this.dirty = false;
 
     this.loader.open();
     this.loaderOpen = true;
@@ -1017,9 +1018,5 @@ export class ManagerComponent implements OnInit, AfterViewInit {
     }
 
     vdev.rawSize = estimate;
-  }
-
-  vdevsTracker(index: number, vdev: ManagerVdev): string {
-    return vdev.uuid;
   }
 }
