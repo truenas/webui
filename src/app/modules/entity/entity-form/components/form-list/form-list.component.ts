ccc10b18e9db6074cc7d614296bc5f3b56f00111
Oleksandr Karpov <Karpov4you@gmail.com>
2023-02-09 16:31:42 +0200

    NAS-119886: iSCSI Wizard - Add Listen should be a required field
    (cherry picked from commit af43ea67c62d3ce4aee681cb72442b272158a8a3)

--- src/app/modules/entity/entity-form/components/form-list/form-list.component.ts
+++ src/app/modules/entity/entity-form/components/form-list/form-list.component.ts
@@ -39,6 +39,10 @@ export class FormListComponent implements Field, OnInit {
   }
 
   delete(id: number): void {
+    if (this.config.required && this.listsFromArray.length === 1) {
+      return;
+    }
+
     this.listsFromArray.removeAt(id);
     this.config.listFields.splice(id, 1);
   }
