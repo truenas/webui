8591e5a80e6cefb20208f0bf3ed6e4b8778ef966
Denys Butenko <ua13dark@gmail.com>
2023-02-11 08:09:20 +0200

    NAS-120107: Similar Apps on App Detail View (#7694)

--- src/app/pages/apps/components/app-detail-view/app-detail-view.component.ts
+++ src/app/pages/apps/components/app-detail-view/app-detail-view.component.ts
@@ -5,12 +5,13 @@ import { ActivatedRoute } from '@angular/router';
 import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import {
-  map, filter, BehaviorSubject,
+  map, filter, BehaviorSubject, tap,
 } from 'rxjs';
 import { appImagePlaceholder, chartsTrain, officialCatalog } from 'app/constants/catalog.constants';
 import { CatalogApp } from 'app/interfaces/catalog.interface';
 import { SnackbarService } from 'app/modules/snackbar/services/snackbar.service';
 import { ApplicationsService } from 'app/pages/apps/services/applications.service';
+import { catalogToAppsTransform } from 'app/pages/apps/utils/catalog-to-apps-transform';
 import { LayoutService } from 'app/services/layout.service';
 
 @UntilDestroy()
@@ -27,13 +28,16 @@ export class AppDetailViewComponent implements OnInit, AfterViewInit {
   readonly imagePlaceholder = appImagePlaceholder;
   readonly officialCatalog = officialCatalog;
 
-  get pageTitle(): string {
-    if (this.app) {
-      return this.app.name;
-    }
+  similarApps: CatalogApp[] = [];
+  similarAppsLoading$ = new BehaviorSubject<boolean>(false);
 
+  get pageTitle(): string {
     if (this.appId) {
       return this.appId;
+    }
+
+    if (this.app) {
+      return this.app.name;
     }
 
     return this.translate.instant('Loading');
@@ -67,6 +71,10 @@ export class AppDetailViewComponent implements OnInit, AfterViewInit {
       .pipe(
         map((params) => params.appId as string),
         filter(Boolean),
+        tap(() => {
+          this.isLoading$.next(true);
+          this.similarAppsLoading$.next(true);
+        }),
         untilDestroyed(this),
       )
       .subscribe((appId) => {
@@ -98,7 +106,19 @@ export class AppDetailViewComponent implements OnInit, AfterViewInit {
   }
 
   private loadSimilarApps(): void {
-    console.warn('The Similar Apps section is under construction.');
+    this.similarAppsLoading$.next(true);
+    this.appService.getAllCatalogs().pipe(
+      catalogToAppsTransform(),
+      untilDestroyed(this),
+    ).subscribe({
+      next: (apps) => {
+        this.similarApps = apps.slice(0, 4);
+        this.similarAppsLoading$.next(false);
+      },
+      error: () => {
+        this.similarAppsLoading$.next(false);
+      },
+    });
   }
 
   private loadScreenshots(): void {
