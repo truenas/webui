5921ea1caa5f20a7ee9e319e9f3bf1d75a05a53b
Oleksandr Karpov <Karpov4you@gmail.com>
2023-02-13 23:06:02 +0200

    NAS-120296: PR update

--- src/app/pages/services/components/service-smb/service-smb.component.spec.ts
+++ src/app/pages/services/components/service-smb/service-smb.component.spec.ts
@@ -96,7 +96,6 @@ describe('ServiceSmbComponent', () => {
     expect(values).toEqual({
       'NetBIOS Name': 'truenas',
       'NetBIOS Alias': [],
-      'NetBIOS Name (TrueNAS Controller 2)': 'truenas-b',
       Workgroup: 'WORKGROUP',
       Description: 'TrueNAS Server',
       'Enable SMB1 support': false,
@@ -126,7 +125,6 @@ describe('ServiceSmbComponent', () => {
       'NTLMv1 Auth': false,
       'NetBIOS Alias': [],
       'NetBIOS Name': 'truenas',
-      'NetBIOS Name (TrueNAS Controller 2)': 'truenas-b',
       'UNIX Charset': 'UTF-8',
       'Use Syslog Only': false,
       Workgroup: 'WORKGROUP',
