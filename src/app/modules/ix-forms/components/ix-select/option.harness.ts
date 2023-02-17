3ed006d7ee019c8d928c5494e33836b63a557a4a
Oleksandr Karpov <Karpov4you@gmail.com>
2023-02-17 13:52:21 +0200

    NAS-120052: Tests fix

--- src/app/modules/ix-forms/components/ix-select/option.harness.ts
+++ src/app/modules/ix-forms/components/ix-select/option.harness.ts
@@ -1,8 +1,4 @@
-import { HarnessPredicate } from '@angular/cdk/testing';
-import {
-  OptionHarnessFilters,
-  MatOptionHarness,
-} from '@angular/material/core/testing';
+import { MatOptionHarness } from '@angular/material/core/testing';
 
 /**
  * Native MatOptionHarness adds tooltip text in option text.
@@ -10,19 +6,6 @@ import {
  */
 export class OptionHarness extends MatOptionHarness {
   private text = this.locatorFor('.mat-mdc-option-text');
-
-  // TODO: This shouldn't be necessary once we migrate away from the legacy harnesses.
-  static with(options: OptionHarnessFilters = {}): HarnessPredicate<OptionHarness> {
-    return new HarnessPredicate(OptionHarness, options)
-      .addOption('text', options.text, async (harness, title) => {
-        return HarnessPredicate.stringMatches(await harness.getText(), title);
-      })
-      .addOption(
-        'isSelected',
-        options.isSelected,
-        async (harness, isSelected) => (await harness.isSelected()) === isSelected,
-      );
-  }
 
   async getText(): Promise<string> {
     return (await this.text()).text({ exclude: 'ix-tooltip' });
