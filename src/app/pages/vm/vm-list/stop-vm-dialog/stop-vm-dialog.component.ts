08cf1114e486b5a3ccd2e000e3470ae7a24515d0
RehanY147 <rehanyousaf147@gmail.com>
2023-02-23 18:45:38 +0500

    NAS-120462 / 23.10 / Refactored how vmlistcomponent hands stopvm subscriptions (#7812)

--- src/app/pages/vm/vm-list/stop-vm-dialog/stop-vm-dialog.component.ts
+++ src/app/pages/vm/vm-list/stop-vm-dialog/stop-vm-dialog.component.ts
@@ -2,14 +2,10 @@ import {
   ChangeDetectionStrategy, Component, Inject,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { TranslateService } from '@ngx-translate/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UntilDestroy } from '@ngneat/until-destroy';
 import helptext from 'app/helptext/vm/vm-list';
 import { VirtualMachine } from 'app/interfaces/virtual-machine.interface';
-import { EntityJobComponent } from 'app/modules/entity/entity-job/entity-job.component';
-import { VmListComponent } from 'app/pages/vm/vm-list/vm-list.component';
-import { DialogService } from 'app/services';
 
 @UntilDestroy()
 @Component({
@@ -24,34 +20,10 @@ export class StopVmDialogComponent {
 
   constructor(
     private dialogRef: MatDialogRef<StopVmDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { vm: VirtualMachine; parent: VmListComponent },
-    private dialog: MatDialog,
-    private dialogService: DialogService,
-    private translate: TranslateService,
+    @Inject(MAT_DIALOG_DATA) public vm: VirtualMachine,
   ) { }
 
   onStop(): void {
-    const jobDialogRef = this.dialog.open(
-      EntityJobComponent,
-      {
-        data: {
-          title: this.translate.instant('Stopping {rowName}', { rowName: this.data.vm.name }),
-        },
-      },
-    );
-    jobDialogRef.componentInstance.setCall('vm.stop', [this.data.vm.id, {
-      force: false,
-      force_after_timeout: this.forceAfterTimeoutCheckbox.value,
-    }]);
-    jobDialogRef.componentInstance.submit();
-    jobDialogRef.componentInstance.success.pipe(untilDestroyed(this.data.parent)).subscribe(() => {
-      jobDialogRef.close(false);
-      this.dialogService.info(
-        this.translate.instant('Finished'),
-        this.translate.instant(helptext.stop_dialog.successMessage, { vmName: this.data.vm.name }),
-        true,
-      );
-    });
-    this.dialogRef.close(true);
+    this.dialogRef.close({ wasStopped: true, forceAfterTimeout: this.forceAfterTimeoutCheckbox.value });
   }
 }
