92c8606bf2b9a48faf06efb8aa70e15abd3c6531
Evgeny Stepanovych <undsoft@gmail.com>
2023-02-23 14:03:00 +0000

    NAS-120444: Passwords don't match error when they do

--- src/app/pages/common/entity/entity-form/validators/compare-validation.ts
+++ src/app/pages/common/entity/entity-form/validators/compare-validation.ts
@@ -1,27 +1,22 @@
 import { FormControl } from '@angular/forms';
 
 export function greaterThan(otherControlName: string, fieldPlaceholers: [string]) {
-  let thisControl: FormControl;
-  let otherControl: FormControl;
-
   return function greaterThanValidate(control: FormControl) {
     if (!control.parent) {
       return null;
     }
 
     // Initializing the validator.
-    if (!thisControl) {
-      thisControl = control;
-      otherControl = control.parent.get(otherControlName) as FormControl;
-      if (!otherControl) {
-        throw new Error(
-          'greaterThanValidator(): other control is not found in parent group',
-        );
-      }
-      otherControl.valueChanges.subscribe(
-        () => { thisControl.updateValueAndValidity(); },
+    const thisControl = control;
+    const otherControl = control.parent.get(otherControlName) as FormControl;
+    if (!otherControl) {
+      throw new Error(
+        'greaterThanValidator(): other control is not found in parent group',
       );
     }
+    otherControl.valueChanges.subscribe(
+      () => { thisControl.updateValueAndValidity(); },
+    );
 
     if (!otherControl) {
       return null;
