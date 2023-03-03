8a51791eb79f4e7b3d09775613c6951b9533a3a0
Damian Szidiropulosz <damianos@knoxbug.org>
2023-03-03 11:54:05 -0500

    NAS-119753: Updated asset including internal view

--- src/app/pages/system/view-enclosure/classes/hardware/r30.ts
+++ src/app/pages/system/view-enclosure/classes/hardware/r30.ts
@@ -10,7 +10,7 @@ export class R30 extends Chassis {
     this.front = new ChassisView();
     this.front.container = new PIXI.Container();
     this.front.chassisPath = 'assets/images/hardware/r30/r30_960w.png';
-    this.front.driveTrayBackgroundPath = 'assets/images/hardware/r30/r30_960w_drivetray_handle.png';
+    this.front.driveTrayBackgroundPath = 'assets/images/hardware/r30/r30_960w_drivetray_bg.png';
     this.front.driveTrayHandlePath = 'assets/images/hardware/r30/r30_960w_drivetray_handle.png';
 
     this.front.slotRange = { start: 1, end: 12 };
@@ -19,35 +19,39 @@ export class R30 extends Chassis {
     this.front.rows = 2;
 
     this.front.gapX = 1;
-    this.front.chassisOffsetX = 0; // -44;
-    this.front.driveTraysOffsetX = 45; // -22
-    this.front.driveTraysOffsetY = 128;
+    this.front.gapY = 3;
+    this.front.chassisOffsetX = 0;
+    this.front.driveTraysOffsetX = 45;
+    this.front.driveTraysOffsetY = 130;
 
     const fscale = 1.4;
     this.front.driveTrays.scale = { x: fscale, y: fscale } as Point;
     this.front.chassisScale = { x: fscale, y: fscale };
-    this.front.disabledOpacity = 0.3;
+    this.front.disabledOpacity = 0.2;
 
     // Rear (Actually internal)
-    /*
     this.rear = new ChassisView();
     this.rear.totalDriveTrays = 4;
 
-    this.rear.driveTraysOffsetX = 146;
-    this.rear.driveTraysOffsetY = -63;
+    this.rear.driveTraysOffsetX = 360;
+    this.rear.driveTraysOffsetY = 30;
     this.rear.gapX = 5;
+
+    const rscale = 1.2;
+    this.rear.driveTrays.scale = { x: rscale, y: rscale } as Point;
+    this.rear.chassisScale = { x: 1.4, y: 1.4 };
+    this.rear.disabledOpacity = 0.3;
 
     this.rear.container = new PIXI.Container();
 
-    this.rear.chassisPath = 'assets/images/hardware/r20a/r20a_rear_960w.png';
-    this.rear.driveTrayBackgroundPath = 'assets/images/hardware/r20a/r20a_rear_960w_drivetray_bg.png';
-    this.rear.driveTrayHandlePath = 'assets/images/hardware/r20a/r20a_rear_960w_drivetray_handle.png';
+    this.rear.chassisPath = 'assets/images/hardware/r30/r30_rear_960w.png';
+    this.rear.driveTrayBackgroundPath = 'assets/images/hardware/r30/r30_960w_drivetray_handle.png';
+    this.rear.driveTrayHandlePath = 'assets/images/hardware/r30/r30_960w_drivetray_handle.png';
 
     this.rear.columns = 2;
-    this.rear.rows = 1;
-    this.rear.slotRange = { start: 13, end: 14 };
+    this.rear.rows = 2;
+    this.rear.slotRange = { start: 13, end: 16 };
 
-    this.rear.totalDriveTrays = 2;
-    */
+    this.rear.totalDriveTrays = 4;
   }
 }
