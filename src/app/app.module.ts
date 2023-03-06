6b2729404be315f116118056db455d15a3544552
Evgeny Stepanovych <undsoft@gmail.com>
2023-03-06 11:06:19 +0100

    NAS-XXXXXX: Borked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -14,7 +14,6 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import {
   TranslateModule, TranslateLoader, TranslateCompiler, MissingTranslationHandler,
 } from '@ngx-translate/core';
-import * as Sentry from '@sentry/angular';
 import { environment } from 'environments/environment';
 import { NgxPopperjsModule } from 'ngx-popperjs';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
@@ -26,30 +25,17 @@ import { IcuMissingTranslationHandler } from 'app/core/classes/icu-missing-trans
 import { createTranslateLoader } from 'app/core/classes/icu-translations-loader';
 import { CoreComponents } from 'app/core/core-components.module';
 import { CommonDirectivesModule } from 'app/directives/common/common-directives.module';
-import { sentryCustomExtractor } from 'app/helpers/sentry-custom-extractor.helper';
-import { getWindow, WINDOW } from 'app/helpers/window.helper';
 import { DownloadKeyDialogComponent } from 'app/modules/common/dialog/download-key/download-key-dialog.component';
 import { SnackbarModule } from 'app/modules/snackbar/snackbar.module';
 import { TerminalModule } from 'app/modules/terminal/terminal.module';
 import { TestIdModule } from 'app/modules/test-id/test-id.module';
 import { TooltipModule } from 'app/modules/tooltip/tooltip.module';
-import { AuthService } from 'app/services/auth/auth.service';
-import { DisksUpdateService } from 'app/services/disks-update.service';
-import { IxFileUploadService } from 'app/services/ix-file-upload.service';
-import { IxSlideInService } from 'app/services/ix-slide-in.service';
-import { NavigationService } from 'app/services/navigation/navigation.service';
-import { ThemeService } from 'app/services/theme/theme.service';
-import { WebSocketService } from 'app/services/ws.service';
 import { rootEffects, rootReducers } from 'app/store';
 import { CustomRouterStateSerializer } from 'app/store/router/custom-router-serializer';
 import { AppComponent } from './app.component';
 import { rootRouterConfig } from './app.routes';
 import { AppCommonModule } from './modules/common/app-common.module';
 import { AppLoaderModule } from './modules/loader/app-loader.module';
-import { AppLoaderService } from './modules/loader/app-loader.service';
-import { AuthGuardService } from './services/auth/auth-guard.service';
-import { EntityTableService } from './services/entity-table.service';
-import { RoutePartsService } from './services/route-parts/route-parts.service';
 
 @NgModule({
   imports: [
@@ -121,30 +107,7 @@ import { RoutePartsService } from './services/route-parts/route-parts.service';
     DownloadKeyDialogComponent,
   ],
   providers: [
-    RoutePartsService,
-    AuthGuardService,
-    NavigationService,
-    AuthService,
-    WebSocketService,
-    AppLoaderService,
-    EntityTableService,
-    IxSlideInService,
-    IxFileUploadService,
-    DisksUpdateService,
-    ThemeService,
-    {
-      provide: ErrorHandler,
-      useValue: Sentry.createErrorHandler({
-        showDialog: false,
-        extractor(error, defaultExtractor) {
-          return sentryCustomExtractor(error, defaultExtractor);
-        },
-      }),
-    },
-    {
-      provide: WINDOW,
-      useFactory: getWindow,
-    },
+    BORKED,
   ],
   bootstrap: [
     AppComponent,
