a4af2af13667ec126461e4bdf29ce0ea0ac6f727
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2023-03-16 12:34:09 +0100

    NAS-120268: show description for PCI passthrough devices

--- src/app/pages/vm/devices/device-form/device-form.component.ts
+++ src/app/pages/vm/devices/device-form/device-form.component.ts
@@ -134,7 +134,6 @@ export class DeviceFormComponent implements OnInit {
           return {
             label: passthroughDevices[id].description || id,
             value: id,
-            tooltip: passthroughDevices[id]?.description,
           };
         });
       }),
