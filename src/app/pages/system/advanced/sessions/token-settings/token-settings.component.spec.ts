d005e72457bdc75dd966ac74f09f8caf6bf9e4f5
Evgeny Stepanovych <undsoft@gmail.com>
2023-03-24 17:16:39 +0100

    NAS-121145 / 23.10 / Refactoring Advanced Settings Page (#7988)

---
+++ src/app/pages/system/advanced/sessions/token-settings/token-settings.component.spec.ts
@@ -0,0 +1,73 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { Store } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import { mockWindow } from 'app/core/testing/utils/mock-window.utils';
+import { Preferences } from 'app/interfaces/preferences.interface';
+import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
+import { IxFormHarness } from 'app/modules/ix-forms/testing/ix-form.harness';
+import { TokenSettingsComponent } from 'app/pages/system/advanced/sessions/token-settings/token-settings.component';
+import { SystemGeneralService } from 'app/services';
+import { IxSlideInService } from 'app/services/ix-slide-in.service';
+import { lifetimeTokenUpdated } from 'app/store/preferences/preferences.actions';
+import { selectPreferences } from 'app/store/preferences/preferences.selectors';
+
+describe('TokenSettingsComponent', () => {
+  let spectator: Spectator<TokenSettingsComponent>;
+  let loader: HarnessLoader;
+  const createComponent = createComponentFactory({
+    component: TokenSettingsComponent,
+    imports: [
+      IxFormsModule,
+      ReactiveFormsModule,
+    ],
+    providers: [
+      mockWindow({
+        localStorage: {
+          setItem: jest.fn,
+        },
+      }),
+      mockProvider(IxSlideInService),
+      mockProvider(SystemGeneralService),
+      provideMockStore({
+        selectors: [{
+          selector: selectPreferences,
+          value: { lifetime: 300 } as Preferences,
+        }],
+      }),
+    ],
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
+    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
+  });
+
+  it('shows current lifetime values when form is being edited', async () => {
+    const form = await loader.getHarness(IxFormHarness);
+    const values = await form.getValues();
+
+    expect(values).toEqual({
+      'Token Lifetime': '300',
+    });
+  });
+
+  it('updates lifetime when save is pressed', async () => {
+    const store$ = spectator.inject(Store);
+    jest.spyOn(store$, 'dispatch');
+
+    const form = await loader.getHarness(IxFormHarness);
+    await form.fillForm({
+      'Token Lifetime': '60',
+    });
+
+    const saveButton = await loader.getHarness(MatButtonHarness.with({ text: 'Save' }));
+    await saveButton.click();
+
+    expect(store$.dispatch).toHaveBeenCalledWith(lifetimeTokenUpdated({ lifetime: 60 }));
+    expect(spectator.inject(IxSlideInService).close).toHaveBeenCalled();
+  });
+});
