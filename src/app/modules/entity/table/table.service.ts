a9b041e5db31164852310531911627781cdb61e2
Evgeny Stepanovych <undsoft@gmail.com>
2023-03-24 17:38:29 +0100

    NAS-121145: Hotfix for Network page

--- src/app/modules/entity/table/table.service.ts
+++ src/app/modules/entity/table/table.service.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Injectable } from '@angular/core';
+import { ChangeDetectorRef, Injectable, Optional } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
@@ -23,7 +23,7 @@ export class TableService {
     private loader: AppLoaderService,
     private translate: TranslateService,
     private matDialog: MatDialog,
-    private cdr: ChangeDetectorRef,
+    @Optional() private cdr: ChangeDetectorRef,
   ) { }
 
   // get table data source
@@ -69,7 +69,7 @@ export class TableService {
         }
 
         table.afterGetDataHook$.next();
-        this.cdr.markForCheck();
+        this.cdr?.markForCheck();
       });
   }
 
