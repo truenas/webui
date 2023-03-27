3ed2d760e543489ead595fc41deaafad3222848d
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2023-03-27 20:58:09 +0200

    NAS-120393: added shareReplay in forbiddenAsyncValues

--- src/app/modules/entity/entity-form/validators/forbidden-values-validation/forbidden-values-validation.ts
+++ src/app/modules/entity/entity-form/validators/forbidden-values-validation/forbidden-values-validation.ts
@@ -2,7 +2,7 @@ import {
   AsyncValidatorFn, FormControl, ValidationErrors, ValidatorFn,
 } from '@angular/forms';
 import {
-  EMPTY, map, Observable, of, take,
+  map, Observable, of, shareReplay,
 } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -20,41 +20,23 @@ export function forbiddenAsyncValues(
   arrayOfValues$: Observable<string[]>,
   caseInsensitive?: boolean,
 ): AsyncValidatorFn {
-  let storeValues$: Observable<string[]> = null;
-
-  arrayOfValues$.pipe(
+  const request$ = arrayOfValues$.pipe(
+    shareReplay({ refCount: false, bufferSize: 1 }),
     catchError((error) => {
       console.error(error);
-      return EMPTY;
+      return of(null);
     }),
-    take(1),
-  ).subscribe((values) => storeValues$ = of(values));
+  );
 
   return (control: FormControl): Observable<ValidationErrors> | null => {
     if (control.value === '' || control.value === undefined) {
       return null;
     }
 
-    if (storeValues$ !== null) {
-      return storeValues$.pipe(
-        map((arrayOfValues) => {
-          return forbiddenValuesError(arrayOfValues, caseInsensitive, control);
-        }),
-        // prevents FormControl.status:"PENDING"
-        // https://github.com/angular/angular/issues/41519
-        take(1),
-      );
-    }
-
-    return arrayOfValues$.pipe(
+    return request$.pipe(
       map((arrayOfValues) => {
         return forbiddenValuesError(arrayOfValues, caseInsensitive, control);
       }),
-      catchError((error) => {
-        console.error(error);
-        return EMPTY;
-      }),
-      take(1),
     );
   };
 }
