b51ef9108e9065c8cc3a617addfd9ac2afdd3184
Boris Vasilenko <dreaktor0@gmail.com>
2023-04-06 09:11:00 +0300

    NAS-121297: Add application search (#8031)
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/apps/components/installed-apps/installed-apps.component.ts
+++ src/app/pages/apps/components/installed-apps/installed-apps.component.ts
@@ -57,6 +57,11 @@ export class InstalledAppsComponent implements OnInit, AfterViewInit {
       });
   }
 
+  get filteredApps(): ChartRelease[] {
+    return this.dataSource
+      .filter((app) => app.name.toLocaleLowerCase().includes(this.filterString.toLocaleLowerCase()));
+  }
+
   get allAppsChecked(): boolean {
     return this.dataSource.every((app) => app.selected);
   }
