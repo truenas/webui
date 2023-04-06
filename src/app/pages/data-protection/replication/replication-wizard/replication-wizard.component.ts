893b61f5da31c524ab53e6754dc49e358dc7e6eb
Denys Butenko <ua13dark@gmail.com>
2023-04-06 13:43:49 +0700

    NAS-121266: Ignore EEXIST error while creating snapshots

--- src/app/pages/data-protection/replication/replication-wizard/replication-wizard.component.ts
+++ src/app/pages/data-protection/replication/replication-wizard/replication-wizard.component.ts
@@ -6,8 +6,10 @@ import { ITreeOptions, TreeNode } from '@circlon/angular-tree-component';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import * as _ from 'lodash';
-import { lastValueFrom, merge } from 'rxjs';
-import { take } from 'rxjs/operators';
+import {
+  EMPTY, lastValueFrom, merge, throwError,
+} from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
 import { truenasDbKeyLocation } from 'app/constants/truenas-db-key-location.constant';
 import { CipherType } from 'app/enums/cipher-type.enum';
 import { DatasetSource } from 'app/enums/dataset.enum';
@@ -1338,7 +1340,14 @@ export class ReplicationWizardComponent implements WizardConfiguration {
           recursive: data['recursive'] ? data['recursive'] : false,
         };
         snapshotPromises.push(
-          lastValueFrom(this.ws.call(this.createCalls[item], [payload])),
+          lastValueFrom(this.ws.call(this.createCalls[item], [payload]).pipe(
+            catchError((error) => {
+              if (error.error === 17) {
+                return EMPTY;
+              }
+              return throwError(() => error);
+            }),
+          )),
         );
       }
       return Promise.all(snapshotPromises);
