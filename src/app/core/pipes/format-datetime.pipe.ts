36d7087655e4dfe1ec0cb9f0415e445011e9be5b
Oleksandr Karpov <Karpov4you@gmail.com>
2023-04-10 12:18:23 +0300

    NAS-121247: TrueNAS SCALE - RangeError: Invalid time value on UI

--- src/app/core/pipes/format-datetime.pipe.ts
+++ src/app/core/pipes/format-datetime.pipe.ts
@@ -3,8 +3,8 @@ import {
 } from '@angular/core';
 import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
-import { format, utcToZonedTime } from 'date-fns-tz';
 import { WINDOW } from 'app/helpers/window.helper';
+import { LocaleService } from 'app/services/locale.service';
 import { AppState } from 'app/store';
 import { selectTimezone } from 'app/store/system-config/system-config.selectors';
 
@@ -21,6 +21,7 @@ export class FormatDateTimePipe implements PipeTransform {
   constructor(
     private store$: Store<AppState>,
     private cdr: ChangeDetectorRef,
+    private localeService: LocaleService,
     @Inject(WINDOW) private window: Window,
   ) {
     this.store$.select(selectTimezone).pipe(untilDestroyed(this)).subscribe((timezone) => {
@@ -43,29 +44,6 @@ export class FormatDateTimePipe implements PipeTransform {
       this.timeFormat = timeFormat;
     }
 
-    return this.formatDateTime(value, timezone);
-  }
-
-  formatDateTime(date: Date | number, tz?: string): string {
-    let localDate = date;
-    if (tz !== null) {
-      if (tz) {
-        localDate = utcToZonedTime(date.valueOf(), tz);
-      } else if (this.timezone) {
-        localDate = utcToZonedTime(date.valueOf(), this.timezone);
-      }
-    }
-
-    // Reason for below replacements: https://github.com/date-fns/date-fns/blob/master/docs/unicodeTokens.md
-    this.dateFormat = this.dateFormat
-      .replace('YYYY', 'yyyy')
-      .replace('YY', 'y')
-      .replace('DD', 'dd')
-      .replace('D', 'd')
-      .replace(' A', ' aa');
-    if (this.timeFormat) {
-      this.timeFormat = this.timeFormat.replace(' A', ' aa');
-    }
-    return format(localDate, `${this.dateFormat} ${this.timeFormat}`);
+    return this.localeService.formatDateTime(value, timezone);
   }
 }
