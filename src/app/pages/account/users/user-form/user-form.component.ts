d7253fc9c8e17f1db52f44a0337fabbe47e39ceb
Alex Karpov <akarpov@ixsystems.com>
2023-04-12 14:57:14 +0300

    NAS-121365: SSH configuration changes (#8043)

--- src/app/pages/account/users/user-form/user-form.component.ts
+++ src/app/pages/account/users/user-form/user-form.component.ts
@@ -93,6 +93,7 @@ export class UserFormComponent {
     sudo_commands_nopasswd: [[] as string[]],
     sudo_commands_nopasswd_all: [false],
     smb: [true],
+    ssh_password_enabled: [false],
   });
 
   readonly tooltips = {
@@ -246,6 +247,7 @@ export class UserFormComponent {
       password_disabled: values.password_disabled,
       shell: values.shell,
       smb: values.smb,
+      ssh_password_enabled: values.ssh_password_enabled,
       sshpubkey: values.sshpubkey,
       sudo_commands: values.sudo_commands_all ? [allCommands] : values.sudo_commands,
       sudo_commands_nopasswd: values.sudo_commands_nopasswd_all ? [allCommands] : values.sudo_commands_nopasswd,
