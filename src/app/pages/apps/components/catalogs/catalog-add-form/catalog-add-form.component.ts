f802b6e7d6a030644243ecf713227f822d12e872
Oleksandr Karpov <Karpov4you@gmail.com>
2023-04-13 18:05:40 +0300

    NAS-121429: Improve experience of adding a catalog

--- src/app/pages/apps/components/catalogs/catalog-add-form/catalog-add-form.component.ts
+++ src/app/pages/apps/components/catalogs/catalog-add-form/catalog-add-form.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import helptext from 'app/helptext/apps/apps';
 import { CatalogCreate } from 'app/interfaces/catalog.interface';
+import { EntityJobComponent } from 'app/modules/entity/entity-job/entity-job.component';
 import { FormErrorHandlerService } from 'app/modules/ix-forms/services/form-error-handler.service';
 import { SnackbarService } from 'app/modules/snackbar/services/snackbar.service';
+import { DialogService } from 'app/services';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 import { WebSocketService } from 'app/services/ws.service';
 
@@ -41,28 +44,36 @@ export class CatalogAddFormComponent {
     private fb: FormBuilder,
     private translate: TranslateService,
     private snackbar: SnackbarService,
+    private mdDialog: MatDialog,
+    private dialogService: DialogService,
   ) {}
 
   onSubmit(): void {
     const values = this.form.value;
 
     this.isFormLoading = true;
-    this.ws.call('catalog.create', [values as CatalogCreate])
-      .pipe(untilDestroyed(this))
-      .subscribe({
-        next: () => {
-          this.isFormLoading = false;
-          this.cdr.markForCheck();
-          this.snackbar.success(
-            this.translate.instant('Adding large catalogs can take minutes. Please check on the progress in Task Manager.'),
-          );
-          this.slideInService.close();
-        },
-        error: (error) => {
-          this.isFormLoading = false;
-          this.errorHandler.handleWsFormError(error, this.form);
-          this.cdr.markForCheck();
-        },
-      });
+
+    const dialogRef = this.mdDialog.open(EntityJobComponent, {
+      data: {
+        title: helptext.catalogForm.title,
+      },
+    });
+    dialogRef.componentInstance.setCall('catalog.create', [values as CatalogCreate]);
+    dialogRef.componentInstance.submit();
+    dialogRef.componentInstance.success.pipe(untilDestroyed(this)).subscribe(() => {
+      this.isFormLoading = false;
+      this.cdr.markForCheck();
+      this.dialogService.closeAllDialogs();
+      this.snackbar.success(
+        this.translate.instant('Catalog is being added. This may take some time. You can minimize this dialog and process will continue in background'),
+      );
+      this.slideInService.close();
+    });
+    dialogRef.componentInstance.failure.pipe(untilDestroyed(this)).subscribe((error) => {
+      this.isFormLoading = false;
+      this.cdr.markForCheck();
+      this.dialogService.closeAllDialogs();
+      this.errorHandler.handleWsFormError(error, this.form);
+    });
   }
 }
