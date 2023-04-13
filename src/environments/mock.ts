957703452e2955260467f43f7ffd750936d6d8d0
Damian Szidiropulosz <damianos@knoxbug.org>
2023-04-13 10:42:34 -0400

    NAS-120692: Make setting a configuration file a sub command

--- src/environments/mock.ts
+++ src/environments/mock.ts
@@ -11,59 +11,85 @@ export interface WebUiEnvironment {
   mockConfig: string;
 }
 
-const originalEnvironment = {...environment};
-const program: Command = new Command();
-program
-  .version('1.0.0')
-  .description('Mocking enclosure ui support')
-  .option('-d, --debug', 'debug output')
-  .option('-c, --config <path>', 'load mock config file')
-  .option('-r, --reset', 'Reset mock configuration option')
-  .parse(process.argv);
-
-const options = program.opts();
-const environmentTs = './src/environments/environment.ts';
-const defaultConfig = '$MOCKCONFIG$';
-
-if (!options.reset && !options.config) {
-  program.help();
-} else if (program.reset) {
-  setConfig(defaultConfig);
-} else {
-  for (let option in options) {
-    if (program.debug) {
-      console.log({
-        option: option,
-        value: program[option],
-      })
-    }
-
-    switch (option) {
-      case 'debug':
-      case 'reset':
-        break;
-      case 'version':
-        console.log(program.version);
-        break;
-      case 'config': {
-        if (!program.reset) {
-          setConfig(program.config);
-        }
-        break;
-      }
-    }
-  }
+interface CommandOptions {
+  [p: string]: any;
 }
 
+
+/*
+* Variables
+* */
+const environmentTs = './src/environments/environment.ts';
+const defaultConfig = '$MOCKCONFIG$';
+const originalEnvironment = {...environment};
+
+/*
+* Command Setup
+* */
+const program: Command = new Command();
+program
+  .version('0.0.1')
+  .description('WebUI setup utility')
+
+program
+  .command('mock')
+  .description('Mocking enclosure ui support')
+  .option('-r, --reset', 'Reset mock configuration option')
+  .option('-d, --debug', 'debug output')
+  .option('-c, --config <path>', 'load mock config file')
+  .action(() => {
+    const mockCommand = program.commands.find((command: Command) => command._name === 'mock');
+    const mockOptions = commandOpts(mockCommand);
+    mock(mockCommand, mockOptions);
+  });
+
+program
+  .command('*')
+  .description('Unmatched command')
+  .action(function() {
+    program.outputHelp();
+  });
+
+program.parse(process.argv);
+
+// Show help when no commands and args provided
+if (!process.argv.slice(2).length) {
+  // program.outputHelp();
+  program.help();
+}
+
+// How is this not built into command.js?
+function commandOpts(command: Command): CommandOptions {
+  const keys: string[] = command.options.map((option: any) => {
+    const key = option.long.replace(/^--/ ,'');
+    return key;
+  });
+
+  let options: CommandOptions = {};
+
+  keys.forEach((key: string) => {
+    if (command[key]) {
+      options[key] = command[key];
+    }
+  });
+
+  return options;
+}
+
+/*
+* General environment file editing functions
+* */
 function setConfig(file: string = defaultConfig): void {
   const prefix = 'export const environment = ';
   environment.mockConfig = file;
   const result = makePrintable(environment, prefix);
-  if (program.debug) verboseOutput(result);
+  if (program.debug) debugOutput(result);
   fs.writeFileSync(environmentTs, result, 'utf8');
+
+  console.log('WebUI environment updated with new settings');
 }
 
-function verboseOutput(newConfig: string): void {
+function debugOutput(newConfig: string): void {
   console.log('Old Environment :\n');
   console.log(originalEnvironment);
   console.log('****************************');
@@ -98,4 +124,74 @@ function wrap(key:string, value: any): string {
     default:
       console.log('WTF is this');
   }
+}
+
+/*
+* Mock Command
+* */
+function mockConfigReport(file: string): string {
+  const path = 'src/assets/mock/configs/';
+  let output = '';
+
+  console.log('Locating mock configuration file...');
+  const configStr = fs.readFileSync( path + file, 'utf8');
+  const config = JSON.parse(configStr);
+  console.log('Mock config file found √')
+  // console.log(config);
+  output += `
+  ******** MOCK CONFIGURATION DETAILS ********
+
+  * Config file: ${file}
+  * Controller: ${config.systemProduct}
+  * Shelves: ${config.enclosureOptions.expansionModels}
+  * Slot Asssignment: ${config.enclosureOptions.dispersal}
+
+  ********************************************
+
+  `;
+  return output;
+}
+
+function mockResetReport(): string {
+  const output = `
+  ******** MOCK CONFIGURATION UNSET ********
+  `;
+  return output;
+}
+
+function mock(command: Command, options: CommandOptions): void {
+  console.log('Running mock utility...');
+  let outputMessage = '';
+
+  if (!options.reset && !options.config) {
+    command.help();
+  } else if (command.reset) {
+    setConfig(defaultConfig);
+    outputMessage += mockResetReport();
+  } else {
+    for (let option in options) {
+      if (command.debug) {
+        console.log({
+          option: option,
+          value: command[option],
+        })
+      }
+
+      switch (option) {
+        case 'debug':
+        case 'reset':
+          break;
+        case 'config': {
+          if (!command.reset) {
+            let mockConfig = mockConfigReport(command.config);
+            console.log(mockConfig);
+            setConfig(command.config);
+          }
+          break;
+        }
+      }
+    }
+  }
+
+  console.log(outputMessage);
 }
