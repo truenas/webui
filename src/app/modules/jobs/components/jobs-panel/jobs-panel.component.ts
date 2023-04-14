2af85476467a5b658a0d3722503b2bd1a6c7bc4e
Oleksandr Karpov <Karpov4you@gmail.com>
2023-04-14 15:03:45 +0300

    NAS-121445: Trivial: Waiting to renew Kerberos ticket. Current ticket expires
    (cherry picked from commit f8679e0c2c4fc2211f7958fb1771cd8cf568e097)

--- src/app/modules/jobs/components/jobs-panel/jobs-panel.component.ts
+++ src/app/modules/jobs/components/jobs-panel/jobs-panel.component.ts
@@ -40,8 +40,7 @@ export class JobsPanelComponent {
     private translate: TranslateService,
     private dialog: DialogService,
     private matDialog: MatDialog,
-  ) {
-  }
+  ) {}
 
   onAbort(job: Job): void {
     this.dialog
