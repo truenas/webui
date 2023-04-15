5f0bd3b46899458ac459c04ef28073459eab6a5a
Evgeny Stepanovych <undsoft@gmail.com>
2023-04-15 18:54:29 +0200

    NAS-121504: Remove FibreChannel related components

--- src/app/modules/entity/entity-form/models/field-relation.interface.ts
+++ src/app/modules/entity/entity-form/models/field-relation.interface.ts
@@ -1,12 +1,6 @@
 import { RelationAction } from 'app/modules/entity/entity-form/models/relation-action.enum';
 import { RelationConnection } from 'app/modules/entity/entity-form/models/relation-connection.enum';
 
-export interface Relation {
-  fieldName: string;
-  operatorName: string;
-  operatorValue: unknown;
-}
-
 export interface FieldRelation {
   name: string;
   status?: string;
--- src/app/services/url-validation.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class UrlValidationService {
-  urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/;
-}
