a281d4cd92dc53c695026f6b0bedc635f5411dfe
Oleksandr Karpov <Karpov4you@gmail.com>
2023-04-18 16:43:25 +0300

    NAS-121470: PR update

--- src/app/services/schema/app-schema.service.ts
+++ src/app/services/schema/app-schema.service.ts
@@ -271,10 +271,7 @@ export class AppSchemaService {
     schema: ChartSchema['schema'],
   ): HierarchicalObjectMap<ChartFormValue>[] {
     return list.map((listItem: HierarchicalObjectMap<ChartFormValue>) => {
-      if (Object.keys(listItem).length > 1) {
-        return this.serializeFormValue(listItem, schema);
-      }
-      return this.serializeFormValue(listItem[Object.keys(listItem)[0]], schema);
+      return this.serializeFormValue(listItem, schema);
     }) as HierarchicalObjectMap<ChartFormValue>[];
   }
 
