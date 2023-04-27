71c784dbc3eabe083660175e4abd7babc0193172
Boris Vasilenko <bvasilenko@ixsystems.com>
2023-04-27 18:11:09 +0300

    NAS-121551: Add ordering property for multiple selection

--- src/app/modules/ix-forms/components/ix-select/ix-select.component.ts
+++ src/app/modules/ix-forms/components/ix-select/ix-select.component.ts
@@ -1,13 +1,15 @@
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef,
-  Component, Input, OnChanges,
+  AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, ViewChild,
 } from '@angular/core';
 import {
   ControlValueAccessor, NgControl,
 } from '@angular/forms';
+import { MatSelect } from '@angular/material/select';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { EMPTY, Observable } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import {
+  catchError, debounceTime, scan, tap,
+} from 'rxjs/operators';
 import { SelectOption, SelectOptionValueType } from 'app/interfaces/option.interface';
 
 type IxSelectValue = SelectOptionValueType;
@@ -19,16 +21,19 @@ type IxSelectValue = SelectOptionValueType;
   templateUrl: './ix-select.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IxSelectComponent implements ControlValueAccessor, OnChanges {
+export class IxSelectComponent implements ControlValueAccessor, OnChanges, AfterViewInit {
   @Input() label: string;
   @Input() value: IxSelectValue;
   @Input() hint: string;
   @Input() options: Observable<SelectOption[]>;
   @Input() required: boolean;
   @Input() tooltip: string;
+  @Input() ordered: boolean;
   @Input() multiple: boolean;
   @Input() emptyValue: string = null;
   @Input() hideEmpty = false;
+
+  @ViewChild('matSelect') matSelect: MatSelect;
 
   isDisabled = false;
   hasErrorInOptions = false;
@@ -38,11 +43,19 @@ export class IxSelectComponent implements ControlValueAccessor, OnChanges {
 
   get multipleLabels(): string[] {
     const selectedLabels: string[] = [];
-    this.opts.forEach((opt) => {
-      if (Array.isArray(this.value) && this.value.some((val) => val === opt.value)) {
-        selectedLabels.push(` ${opt.label}`);
-      }
-    });
+    if (this.multiple && this.ordered) {
+      (Array.isArray(this.value) ? this.value : []).forEach((val) => {
+        if (this.opts.some((opt) => opt.value === val)) {
+          selectedLabels.push(` ${this.opts.find((opt) => opt.value === val).label}`);
+        }
+      });
+    } else {
+      this.opts.forEach((opt) => {
+        if (Array.isArray(this.value) && this.value.some((val) => val === opt.value)) {
+          selectedLabels.push(` ${opt.label}`);
+        }
+      });
+    }
     return selectedLabels.length > 0 ? selectedLabels : null;
   }
 
@@ -51,6 +64,26 @@ export class IxSelectComponent implements ControlValueAccessor, OnChanges {
     private cdr: ChangeDetectorRef,
   ) {
     this.controlDirective.valueAccessor = this;
+  }
+
+  ngAfterViewInit(): void {
+    if (this.multiple) {
+      this.matSelect.optionSelectionChanges.pipe(
+        debounceTime(0),
+        scan((acc, change) => {
+          if (change.source.selected) {
+            return acc.includes(change.source.value) ? acc : [...acc, change.source.value];
+          }
+          return acc.filter((entry) => entry !== change.source.value);
+        }, this.value as (string | number)[]),
+        untilDestroyed(this),
+      ).subscribe((selectedValues) => {
+        if (this.ordered) {
+          this.writeValue(selectedValues);
+          this.onChange(selectedValues);
+        }
+      });
+    }
   }
 
   ngOnChanges(): void {
