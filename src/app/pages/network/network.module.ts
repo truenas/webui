0ea4d73ea960d9602c62b502927458683eb69580
Boris Vasilenko <bvasilenko@ixsystems.com>
2023-04-28 19:35:43 +0300

    NAS-121551: Implement `OrderedListboxComponent`

--- src/app/pages/network/network.module.ts
+++ src/app/pages/network/network.module.ts
@@ -13,6 +13,7 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { NgxUploaderModule } from 'ngx-uploader';
 import { CommonDirectivesModule } from 'app/directives/common/common-directives.module';
 import { CastModule } from 'app/modules/cast/cast.module';
+import { NgxOrderedListboxModule } from 'app/modules/common/ordered-list/ordered-list.module';
 import { EntityFormService } from 'app/modules/entity/entity-form/services/entity-form.service';
 import { EntityModule } from 'app/modules/entity/entity.module';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
@@ -59,6 +60,7 @@ import { routing } from './network.routing';
     CastModule,
     MatDialogModule,
     TestIdModule,
+    NgxOrderedListboxModule,
   ],
   declarations: [
     StaticRouteFormComponent,
