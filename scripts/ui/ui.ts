2df4c58e8e4420e26107bf8dc37a0a675b1df8bd
Damian Szidiropulosz <damianos@knoxbug.org>
2023-04-30 17:27:58 -0400

    NAS-121735: Add examples to help text

--- scripts/ui/ui.ts
+++ scripts/ui/ui.ts
@@ -131,7 +131,7 @@ program
 
   });
 
-const mockOptExamples = `
+const mockEnclosureExamples = `
 EXAMPLES:
 
   * Retrieve a list of mockable controller models
@@ -172,7 +172,7 @@ program
   .name('mock-enclosure')
   .alias('me')
   .description('Set specific mock enclosure options')
-  .addHelpText('after', mockOptExamples)
+  .addHelpText('after', mockEnclosureExamples)
   .option('-e, --enable', 'enable mock config')
   .option('-d, --disable', 'disable mock config')
   .option('-M, --showcontrollers ', 'show available controllers for mock')
@@ -189,7 +189,19 @@ program
     mock(mockCommand, mockOptions);
   });
 
-const mockDiskExamples = ``
+const mockDiskExamples = `
+The mock-disk command allows developers to mock unassigned disks
+
+EXAMPLES:
+
+  * Create 150 x 12TB disks that are not assigned to any pool
+
+  % ui mock-disk --disksize 12 --diskrepeats 150
+
+  * Create 150 x 12TB disks that are not assigned to any pool using abbreviated syntax
+
+  % ui md -s 12 -r 150
+`
 
 program
   .command('mock-disks')
@@ -207,7 +219,41 @@ program
     mockDisks(mockCommand, mockOptions);
   });
 
-const mockPoolExamples = ``
+const mockPoolExamples = `
+The mock-pool command allows developers to mock a single pool with data topology.
+This command offers some different options to update certain aspects of the pool
+The most useful option however is the --vdevscenario option. This removes the need
+to manually specify every aspect and instead state typical conditions that need to
+be tested for.
+
+Supported scenarios include:
+
+- Uniform (default)
+- MixedDiskCapacity
+- MixedVdevCapacity
+- MixedVdevLayout
+- MixedVdevWidth
+- NoRedundancy
+
+
+EXAMPLES:
+
+  * Enable mock pool
+
+  % ui mock-pool -e
+
+  * Create 3 x two way mirrors using all 4TB disks
+
+  % ui mock-pool --vdevscenario Uniform --vdevdisksize 4 --vdevrepeats 3
+
+  * Create vdevs with mixed layouts (first vdev is different and subsequent vdevs honor the mock configuration settings)
+
+  % ui mock-pool -v MixedVdevLayout -l Raidz3 -r 150
+
+  * Create vdevs with mixed widths (first vdev has one extra member disk. Subsequent vdevs honor the mock configuration settings)
+
+  % ui mock-pool -v MixedVdevWidth -r 150
+`
 
 program
   .command('mock-pool')
