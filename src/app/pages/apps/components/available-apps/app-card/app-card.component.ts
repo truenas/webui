77700d3c4093a652daf82512e95d132d6a8b8d99
Boris Vasilenko <bvasilenko@ixsystems.com>
2023-05-02 20:58:15 +0300

    NAS-121712: Redirect to first installed app

--- src/app/pages/apps/components/available-apps/app-card/app-card.component.ts
+++ src/app/pages/apps/components/available-apps/app-card/app-card.component.ts
@@ -1,9 +1,14 @@
 import {
   ChangeDetectionStrategy, Component, Input,
 } from '@angular/core';
+import { Router } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { map } from 'rxjs';
 import { officialCatalog } from 'app/constants/catalog.constants';
 import { AvailableApp } from 'app/interfaces/available-app.interfase';
+import { AvailableAppsStore } from 'app/pages/apps/store/available-apps-store.service';
 
+@UntilDestroy()
 @Component({
   selector: 'ix-app-card',
   templateUrl: './app-card.component.html',
@@ -19,5 +24,24 @@ export class AppCardComponent {
     // TODO: This temporarily uses incorrect field. This is supposed to be a short description with no html tags.
     const description = this.app.app_readme.replace(/<[^>]*>/g, '');
     return description.length > 150 ? `${description.substring(0, 150)}...` : description;
+  }
+
+  constructor(
+    private applicationsStore: AvailableAppsStore,
+    private router: Router,
+  ) {}
+
+  navigateToAllInstalledPage(): void {
+    this.applicationsStore.installedApps$.pipe(
+      map((apps) => apps.filter((app) => (app.chart_metadata.name === this.app.name
+        && app.catalog === this.app.catalog && app.catalog_train === this.app.train))),
+      untilDestroyed(this),
+    ).subscribe((apps) => {
+      if (apps.length) {
+        this.router.navigate(['/apps', 'installed', apps[0].name]);
+      } else {
+        this.router.navigate(['/apps', 'installed']);
+      }
+    });
   }
 }
