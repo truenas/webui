da762945975cf2c5f228f23fa058dec078da7458
Boris Vasilenko <bvasilenko@ixsystems.com>
2023-05-03 10:52:46 +0300

    NAS-121551: Add drag indicator

--- src/app/modules/common/ordered-list/ordered-list.module.ts
+++ src/app/modules/common/ordered-list/ordered-list.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
+import { IxIconModule } from 'app/modules/ix-icon/ix-icon.module';
 import { TestIdModule } from 'app/modules/test-id/test-id.module';
 import { OrderedListboxComponent } from './ordered-list.component';
 
@@ -14,6 +15,7 @@ import { OrderedListboxComponent } from './ordered-list.component';
     CommonModule,
     MatButtonModule,
     MatListModule,
+    IxIconModule,
     IxFormsModule,
     DragDropModule,
     TestIdModule,
