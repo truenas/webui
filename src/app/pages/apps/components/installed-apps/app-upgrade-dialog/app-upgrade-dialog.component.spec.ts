315dd250af51da132d52b6d33f7820b0a5f12862
Oleksandr Karpov <Karpov4you@gmail.com>
2023-05-04 17:54:10 +0300

    NAS-121615: Check

--- src/app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component.spec.ts
+++ src/app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component.spec.ts
@@ -1,5 +1,9 @@
+import { HarnessLoader, parallel } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { FormsModule } from '@angular/forms';
+import { MatButtonHarness } from '@angular/material/button/testing';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSelectHarness } from '@angular/material/select/testing';
 import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
 import { ImgFallbackModule } from 'ngx-img-fallback';
 import { AppUpgradeDialogComponent } from 'app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component';
@@ -33,6 +37,7 @@ const fakeUpgradeSummary = {
 
 describe('AppUpgradeDialogComponent', () => {
   let spectator: Spectator<AppUpgradeDialogComponent>;
+  let loader: HarnessLoader;
   const createComponent = createComponentFactory({
     component: AppUpgradeDialogComponent,
     imports: [FormsModule, ImgFallbackModule],
@@ -50,9 +55,52 @@ describe('AppUpgradeDialogComponent', () => {
 
   beforeEach(() => {
     spectator = createComponent();
+    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
   });
 
   it('shows title as application name', () => {
     expect(spectator.query('.chart-name').textContent).toBe('elastic-search');
+  });
+
+  it('shows current application version', () => {
+    expect(spectator.query('.version').textContent).toBe(' 8.7.0_1.0.1');
+  });
+
+  it('shows application image', () => {
+    expect(spectator.query('img').getAttribute('src')).toBe('https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt280217a63b82a734/6202d3378b1f312528798412/elastic-logo.svg');
+  });
+
+  it('shows 2 mat-panels with titles', () => {
+    const panelTitles = spectator.queryAll('mat-expansion-panel mat-panel-title');
+    expect(panelTitles[0].textContent).toBe(' Images ( to be updated ) ');
+    expect(panelTitles[1].textContent).toBe(' Changelog ');
+  });
+
+  it('shows 2 mat-panels detail rows with data', () => {
+    const panelContent = spectator.queryAll('mat-expansion-panel .detail-row');
+    expect(panelContent[0].textContent).toBe(' There are no images requiring upgrade ');
+    expect(panelContent[1].textContent).toBe('No Changelog');
+  });
+
+  it('shows a list of versions to be upgraded to', async () => {
+    const select = await loader.getHarness(MatSelectHarness);
+    await select.open();
+    const options = await select.getOptions();
+    const optionLabels = await parallel(() => options.map((option) => option.getText()));
+    expect(optionLabels).toEqual(['8.7.0_1.0.2']);
+  });
+
+  it('submits upgrade from 1.0.1 to 1.0.2 version', async () => {
+    const upgradeButton = await loader.getHarness(MatButtonHarness.with({ text: 'Upgrade' }));
+    await upgradeButton.click();
+
+    expect(spectator.inject(MatDialogRef).close).toHaveBeenCalledWith('1.0.2');
+  });
+
+  it('closes modal with no upgrades to start if Close button clicked', async () => {
+    const upgradeButton = await loader.getHarness(MatButtonHarness.with({ text: 'Close' }));
+    await upgradeButton.click();
+
+    expect(spectator.inject(MatDialogRef).close).toHaveBeenCalled();
   });
 });
