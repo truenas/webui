94168c91303185e3c838cbd7c45002d89f0b71fe
Damian Szidiropulosz <damianos@knoxbug.org>
2023-05-05 13:35:00 -0400

    NAS-121844: remove mockConfig property from prod environment file. Don't create mockutils unless mockConfig property exists

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 import { enableProdMode } from '@angular/core';
-import { MockEnclosureConfig } from 'app/core/testing/interfaces/mock-enclosure-utils.interface';
 import { WebUiEnvironment } from './environment.interface';
 import { environmentVersion } from './environment.version';
 
@@ -9,7 +8,6 @@ export const environment: WebUiEnvironment = {
   remote: document.location.host,
   production: true,
   sentryPublicDsn: 'https://7ac3e76fe2a94f77a58e1c38ea6b42d9@sentry.ixsystems.com/4',
-  mockConfig: {} as MockEnclosureConfig,
 };
 
 // Production
