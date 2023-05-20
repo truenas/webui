1b8d4a466ed2ad903bb1996e399ac1860d628756
Denys Butenko <ua13dark@gmail.com>
2023-05-20 15:08:41 +0700

    NAS-122057: Fix tests

--- src/app/pages/apps/components/app-detail-view/app-details-header/app-details-header.component.spec.ts
+++ src/app/pages/apps/components/app-detail-view/app-details-header/app-details-header.component.spec.ts
@@ -105,7 +105,7 @@ describe('AppDetailsHeaderComponent', () => {
 
   describe('other elements', () => {
     it('shows app catalog', () => {
-      expect(spectator.query('.catalog-header')).toHaveText('Official Catalog');
+      expect(spectator.query('.catalog-header')).toHaveText('Truenas Catalog');
     });
 
     it('shows app version', () => {
