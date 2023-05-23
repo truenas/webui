9da51481a5e3d8cab0048277d82e33b2cf32ead1
Denys Butenko <ua13dark@gmail.com>
2023-05-23 12:37:25 +0700

    NAS-122057: Rename official catalog (#8217)

--- src/app/pages/applications/forms/chart-form/chart-form.component.spec.ts
+++ src/app/pages/applications/forms/chart-form/chart-form.component.spec.ts
@@ -34,7 +34,7 @@ describe('ChartFormComponent', () => {
       },
     },
     catalog: {
-      id: 'OFFICIAL',
+      id: 'TRUENAS',
       train: 'charts',
     },
     schema: {
@@ -405,7 +405,7 @@ describe('ChartFormComponent', () => {
 
     expect(spectator.component.dialogRef.componentInstance.setCall).toHaveBeenCalledWith(
       'chart.release.create', [{
-        catalog: 'OFFICIAL',
+        catalog: 'TRUENAS',
         item: 'ipfs',
         release_name: 'app_name',
         train: 'charts',
