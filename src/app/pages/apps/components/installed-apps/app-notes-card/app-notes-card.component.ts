66c277bc1b5113115328efebcadd96ba4872b84d
Boris Vasilenko <dreaktor0@gmail.com>
2023-05-24 08:01:13 +0200

    NAS-121879 / 23.10 / Fix button visibility (#8213)
    * NAS-121879: Implement notes card with markdown
    
    * NAS-121879: Fix remarks
    
    * NAS-121879: Fix remarks
    
    * NAS-121879: Fix button visibility
    
    ---------
    
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/apps/components/installed-apps/app-notes-card/app-notes-card.component.ts
+++ src/app/pages/apps/components/installed-apps/app-notes-card/app-notes-card.component.ts
@@ -1,6 +1,7 @@
 import {
-  ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, ViewChild,
+  AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, ViewChild,
 } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { ChartRelease } from 'app/interfaces/chart-release.interface';
 
 @Component({
@@ -9,15 +10,16 @@ import { ChartRelease } from 'app/interfaces/chart-release.interface';
   styleUrls: ['./app-notes-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppNotesCardComponent implements OnChanges {
+export class AppNotesCardComponent implements AfterViewChecked, OnChanges {
   @Input() app: ChartRelease;
   @Input() maxHeight = 250;
   @ViewChild('notes', { static: true, read: ElementRef }) notes: ElementRef<HTMLElement>;
 
   isCollapsed = true;
+  height: Subject<number> = new BehaviorSubject(this.maxHeight);
 
-  get showMoreLess(): boolean {
-    return this.notes.nativeElement.offsetHeight >= this.maxHeight;
+  ngAfterViewChecked(): void {
+    this.setHeight();
   }
 
   ngOnChanges(): void {
@@ -25,7 +27,16 @@ export class AppNotesCardComponent implements OnChanges {
     this.notes.nativeElement.style.maxHeight = `${this.maxHeight}px`;
   }
 
+  setHeight(): void {
+    this.height.next(this.notes.nativeElement.offsetHeight || this.maxHeight);
+  }
+
+  onResize(): void {
+    this.setHeight();
+  }
+
   changeCollapsed(): void {
+    this.setHeight();
     this.isCollapsed = !this.isCollapsed;
     this.notes.nativeElement.style.maxHeight = this.isCollapsed ? `${this.maxHeight}px` : 'none';
   }
