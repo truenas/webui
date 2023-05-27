90eb71077ca3e2551ef3886981c0bf59d91659d7
Denys Butenko <ua13dark@gmail.com>
2023-05-27 12:18:40 +0700

    NAS-122151: Remove OpenVPN Server

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CertificateAuthority } from 'app/interfaces/certificate-authority.interface';
 import { Certificate } from 'app/interfaces/certificate.interface';
-import { Choices } from 'app/interfaces/choices.interface';
-import { OpenvpnServerConfig } from 'app/interfaces/openvpn-server-config.interface';
 import { WebSocketService } from 'app/services/ws.service';
 
 @Injectable({ providedIn: 'root' })
@@ -16,20 +14,5 @@ export class ServicesService {
 
   getCertificateAuthorities(): Observable<CertificateAuthority[]> {
     return this.ws.call('certificateauthority.query');
-  }
-
-  getOpenVpnServerAuthAlgorithmChoices(): Observable<Choices> {
-    return this.ws.call('openvpn.server.authentication_algorithm_choices');
-  }
-
-  getOpenServerCipherChoices(): Observable<Choices> {
-    return this.ws.call('openvpn.server.cipher_choices');
-  }
-
-  generateOpenServerClientConfig(id: number, address: string): Observable<string> {
-    return this.ws.call('openvpn.server.client_configuration_generation', [id, address]);
-  }
-  renewStaticKey(): Observable<OpenvpnServerConfig> {
-    return this.ws.call('openvpn.server.renew_static_key');
   }
 }
