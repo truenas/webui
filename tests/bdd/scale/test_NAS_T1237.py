d7e72c5c1b3d35c04ddab3f1f6c8a04f6500c45c
ericbsd <ericturgeon.bsd@gmail.com>
2023-06-06 12:14:21 -0300

    Add missing LDAP_SMB depends in test_NAS_T1237.py
    (cherry picked from commit 66ddef27b48f4c3fc2601a8331ac986ae03c2066)

--- tests/bdd/scale/test_NAS_T1237.py
+++ tests/bdd/scale/test_NAS_T1237.py
@@ -29,7 +29,7 @@ def test_verify_recursive_and_transverse_acl_options():
 @given('the browser is open, the TrueNAS URL and logged in')
 def the_browser_is_open_the_truenas_url_and_logged_in(driver, nas_ip, root_password, request):
     """the browser is open, the TrueNAS URL and logged in."""
-    depends(request, ['tank_pool'], scope='session')
+    depends(request, ['tank_pool', 'LDAP_SMB'], scope='session')
     if nas_ip not in driver.current_url:
         driver.get(f"http://{nas_ip}")
         assert wait_on_element(driver, 10, xpaths.login.user_Input)
