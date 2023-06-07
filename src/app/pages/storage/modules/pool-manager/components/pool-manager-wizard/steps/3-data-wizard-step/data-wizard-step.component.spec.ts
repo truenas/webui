c478629952a6052c9c9934fd1281da43ae8eb528
RehanY147 <rehanyousaf147@gmail.com>
2023-06-07 17:02:19 +0500

    NAS-121999: Added tests for new components

--- src/app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/3-data-wizard-step/data-wizard-step.component.spec.ts
+++ src/app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/3-data-wizard-step/data-wizard-step.component.spec.ts
@@ -1,9 +1,8 @@
-import { HarnessLoader } from '@angular/cdk/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { MatButtonHarness } from '@angular/material/button/testing';
 import { mockProvider, Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 import { MockComponent } from 'ng-mocks';
 import { of } from 'rxjs';
+import { CreateVdevLayout, VdevType } from 'app/enums/v-dev-type.enum';
+import helptext from 'app/helptext/storage/volumes/manager/manager';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
 import { LayoutStepComponent } from 'app/pages/storage/modules/pool-manager/components/pool-manager-wizard/components/layout-step/layout-step.component';
 import { DataWizardStepComponent } from 'app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/3-data-wizard-step/data-wizard-step.component';
@@ -11,7 +10,25 @@ import { PoolManagerStore } from 'app/pages/storage/modules/pool-manager/store/p
 
 describe('DataWizardStepComponent', () => {
   let spectator: Spectator<DataWizardStepComponent>;
-  let loader: HarnessLoader;
+
+  const fakeInventory = [
+    {
+      identifier: '{serial_lunid}8HG7MZJH_5000cca2700de678',
+      name: 'sdo',
+      number: 2272,
+      serial: '8HG7MZJH',
+      size: 12000138625024,
+      type: 'HDD',
+    },
+    {
+      identifier: '{serial_lunid}8DJ61EBH_5000cca2537bba6c',
+      name: 'sdv',
+      number: 16720,
+      serial: '8DJ61EBH',
+      size: 12000138625024,
+      type: 'HDD',
+    },
+  ];
 
   const createComponent = createComponentFactory({
     component: DataWizardStepComponent,
@@ -23,18 +40,21 @@ describe('DataWizardStepComponent', () => {
     ],
     providers: [
       mockProvider(PoolManagerStore, {
-        topology$: of({}),
+        getInventoryForStep: jest.fn(() => of(fakeInventory)),
       }),
     ],
   });
 
   beforeEach(() => {
     spectator = createComponent();
-    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
   });
 
-  it('has back button', async () => {
-    const backBtn = await loader.getHarness(MatButtonHarness.with({ text: 'Back' }));
-    expect(backBtn).toBeTruthy();
+  it('has the correct inputs', () => {
+    const layoutComponent = spectator.query(LayoutStepComponent);
+    expect(layoutComponent.description).toBe(helptext.data_vdev_description);
+    expect(layoutComponent.canChangeLayout).toBeTruthy();
+    expect(layoutComponent.inventory).toStrictEqual([...fakeInventory]);
+    expect(layoutComponent.limitLayouts).toStrictEqual(Object.values(CreateVdevLayout));
+    expect(layoutComponent.type).toStrictEqual(VdevType.Data);
   });
 });
