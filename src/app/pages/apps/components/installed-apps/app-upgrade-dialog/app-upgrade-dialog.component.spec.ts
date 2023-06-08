4b0ed62a60bee6c8b6631c5b0b4b75cc41f46752
Evgeny Stepanovych <undsoft@gmail.com>
2023-06-08 12:32:35 +0200

    NAS-121615: Testing

--- src/app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component.spec.ts
+++ src/app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component.spec.ts
@@ -1,14 +1,66 @@
-import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
 import { AppUpgradeDialogComponent } from 'app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component';
+import { ApplicationsService } from 'app/pages/apps/services/applications.service';
+import { AppLoaderService, DialogService } from 'app/services';
+import { ErrorHandlerService } from 'app/services/error-handler.service';
+
+const fakeAppInfo = {
+  name: 'elastic-search',
+  chart_metadata: {
+    icon: 'https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt280217a63b82a734/6202d3378b1f312528798412/elastic-logo.svg',
+  },
+  id: 'elastic-search',
+  human_version: '8.7.0_1.0.1',
+  human_latest_version: '8.7.0_1.0.2',
+};
+
+const fakeUpgradeSummary = {
+  container_images_to_update: {},
+  changelog: null as string,
+  available_versions_for_upgrade: [
+    {
+      version: '1.0.2',
+      human_version: '8.7.0_1.0.2',
+    },
+  ],
+  item_update_available: true,
+  image_update_available: false,
+  latest_version: '1.0.2',
+  upgrade_version: '1.0.2',
+  latest_human_version: '8.7.0_1.0.2',
+  upgrade_human_version: '8.7.0_1.0.2',
+};
 
 describe('AppUpgradeDialogComponent - test 1', () => {
   let spectator: Spectator<AppUpgradeDialogComponent>;
   const createComponent = createComponentFactory({
     component: AppUpgradeDialogComponent,
+    imports: [ReactiveFormsModule, FormsModule],
+    providers: [
+      mockProvider(MatDialogRef),
+      mockProvider(DialogService),
+      mockProvider(AppLoaderService),
+      mockProvider(ErrorHandlerService),
+      mockProvider(ApplicationsService),
+    ],
   });
 
   beforeEach(() => {
-    spectator = createComponent();
+    spectator = createComponent(
+      {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: {
+              appInfo: fakeAppInfo,
+              upgradeSummary: fakeUpgradeSummary,
+            },
+          },
+        ],
+      },
+    );
   });
 
   it('shows title as application name', () => {
--- src/app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component.ts
+++ src/app/pages/apps/components/installed-apps/app-upgrade-dialog/app-upgrade-dialog.component.ts
@@ -1,7 +1,18 @@
 import {
-  Component,
+  Component, Inject,
 } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import { appImagePlaceholder } from 'app/constants/catalog.constants';
+import helptext from 'app/helptext/apps/apps';
+import { UpgradeSummary } from 'app/interfaces/application.interface';
+import { AppLoaderService } from 'app/modules/loader/app-loader.service';
+import { ApplicationsService } from 'app/pages/apps-old/applications.service';
+import { ChartUpgradeDialogConfig } from 'app/pages/apps-old/interfaces/chart-upgrade-dialog-config.interface';
+import { DialogService } from 'app/services';
+import { ErrorHandlerService } from 'app/services/error-handler.service';
+
+type Version = Omit<UpgradeSummary, 'upgrade_version' | 'image_update_available' | 'upgrade_human_version'> & { fetched?: boolean };
 
 @UntilDestroy()
 @Component({
@@ -9,4 +20,19 @@ import { UntilDestroy } from '@ngneat/until-destroy';
   templateUrl: './app-upgrade-dialog.component.html',
 })
 export class AppUpgradeDialogComponent {
+  dialogConfig: ChartUpgradeDialogConfig;
+  imagePlaceholder = appImagePlaceholder;
+  helptext = helptext;
+  versionOptions = new Map<string, Version>();
+  selectedVersionKey: string;
+  selectedVersion: Version;
+
+  constructor(
+    public dialogRef: MatDialogRef<AppUpgradeDialogComponent>,
+    private appLoaderService: AppLoaderService,
+    private errorHandler: ErrorHandlerService,
+    private appService: ApplicationsService,
+    public dialogService: DialogService,
+    @Inject(MAT_DIALOG_DATA) public data: ChartUpgradeDialogConfig,
+  ) {}
 }
