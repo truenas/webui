f3d182a439341c312bca3549347a13ea4344d8ef
Oleksandr Karpov <Karpov4you@gmail.com>
2023-06-09 15:44:58 +0300

    NAS-119953: Make alert/email settings easier to find

--- src/app/pages/system/alert-settings/alert-settings.component.ts
+++ src/app/pages/system/alert-settings/alert-settings.component.ts
@@ -1,9 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'ix-alert-settings',
   templateUrl: './alert-settings.component.html',
-  styleUrls: ['./alert-settings.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlertSettingsComponent {
