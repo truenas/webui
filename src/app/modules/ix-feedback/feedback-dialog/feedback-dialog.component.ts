183c24cffbb52bfe4aa787a0620b5b5482a9823a
Denys Butenko <ua13dark@gmail.com>
2023-06-14 14:31:14 +0700

    NAS-122349: WIP

--- src/app/modules/ix-feedback/feedback-dialog/feedback-dialog.component.ts
+++ src/app/modules/ix-feedback/feedback-dialog/feedback-dialog.component.ts
@@ -9,7 +9,10 @@ import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
 import { UUID } from 'angular2-uuid';
 import { environment } from 'environments/environment';
-import { take } from 'rxjs';
+import {
+  filter, map, take,
+} from 'rxjs';
+import { ticketAcceptedFiles } from 'app/enums/file-ticket.enum';
 import { WINDOW } from 'app/helpers/window.helper';
 import { AddReview, FeedbackEnvironment } from 'app/modules/ix-feedback/interfaces/feedback.interface';
 import { IxFeedbackService } from 'app/modules/ix-feedback/ix-feedback.service';
@@ -18,6 +21,7 @@ import { SnackbarService } from 'app/modules/snackbar/services/snackbar.service'
 import { FileTicketFormComponent } from 'app/pages/system/file-ticket/file-ticket-form/file-ticket-form.component';
 import { DialogService } from 'app/services';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
+import { IxFileUploadService } from 'app/services/ix-file-upload.service';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 import { AppState } from 'app/store';
 import { waitForSystemInfo } from 'app/store/system-info/system-info.selectors';
@@ -34,8 +38,11 @@ export class FeedbackDialogComponent implements OnInit {
   protected form = this.formBuilder.group({
     rating: [undefined as number, [Validators.required, rangeValidator(1, maxRatingValue)]],
     message: [''],
+    image: [null as File[]],
   });
   private release: string;
+  private image: File;
+  readonly acceptedFiles = ticketAcceptedFiles;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -47,6 +54,7 @@ export class FeedbackDialogComponent implements OnInit {
     private translate: TranslateService,
     private errorHandler: ErrorHandlerService,
     private dialogService: DialogService,
+    private fileUpload: IxFileUploadService,
     @Inject(WINDOW) private window: Window,
   ) {}
 
@@ -58,6 +66,15 @@ export class FeedbackDialogComponent implements OnInit {
     ).subscribe(({ version }) => {
       this.release = version;
     });
+    this.form.controls.image.valueChanges.pipe(
+      filter((files) => !!files.length),
+      map((files) => files[0]),
+      untilDestroyed(this),
+    ).subscribe((image) => {
+      console.info('image', image);
+      this.image = image;
+      this.attachImageToReview(7, image);
+    });
   }
 
   openFileTicketForm(): void {
@@ -67,9 +84,10 @@ export class FeedbackDialogComponent implements OnInit {
 
   onSubmit(): void {
     const values: AddReview = {
-      ...this.form.getRawValue(),
       host_u_id: UUID.UUID(),
-      page: this.window.location.href,
+      rating: this.form.controls.rating.value,
+      message: this.form.controls.message.value,
+      page: this.window.location.pathname,
       user_agent: this.window.navigator.userAgent,
       environment: environment.production ? FeedbackEnvironment.Production : FeedbackEnvironment.Development,
       release: this.release,
@@ -79,15 +97,42 @@ export class FeedbackDialogComponent implements OnInit {
     this.feedbackService.addReview(values)
       .pipe(untilDestroyed(this))
       .subscribe({
-        next: () => {
-          this.snackbar.success(
-            this.translate.instant('Thank you for sharing your feedback with us! Your insights are valuable in helping us improve our product.'),
-          );
-          this.dialogRef.close();
+        next: (response) => {
+          if (this.form.controls.image.value && response.success) {
+            this.attachImageToReview(response.review_id, this.image);
+          } else {
+            this.onSuccess();
+          }
         },
         error: (error: HttpErrorResponse) => {
           this.dialogService.error(this.errorHandler.parseHttpError(error));
         },
       });
+  }
+
+  private attachImageToReview(reviewId: number, image: File): void {
+    this.feedbackService.addAttachment(reviewId, image)
+      .pipe(untilDestroyed(this))
+      .subscribe({
+        next: (attachmentResponse) => {
+          console.info('image uploaded', attachmentResponse);
+          this.onSuccess();
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error(error);
+          this.dialogService.error({
+            title: this.translate.instant('Uploading failed.'),
+            message: error.message,
+            backtrace: JSON.stringify(error, null, '  '),
+          });
+        },
+      });
+  }
+
+  private onSuccess(): void {
+    this.snackbar.success(
+      this.translate.instant('Thank you for sharing your feedback with us! Your insights are valuable in helping us improve our product.'),
+    );
+    this.dialogRef.close();
   }
 }
--- src/app/modules/ix-feedback/ix-feedback.service.ts
+++ src/app/modules/ix-feedback/ix-feedback.service.ts
@@ -2,26 +2,41 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {
-  AddReview,
+  AddReview, AttachmentAddedResponse,
 } from 'app/modules/ix-feedback/interfaces/feedback.interface';
 import { ReviewAddedResponse } from './interfaces/feedback.interface';
 
 @Injectable()
 export class IxFeedbackService {
   private readonly hostname = 'https://feedback.ui.truenas.com';
-  private readonly token = '946bde8d85fc86c6c48344adf1ef2139';
 
   constructor(private httpClient: HttpClient) {}
 
   addReview(body: AddReview): Observable<ReviewAddedResponse> {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer 946bde8d85fc86c6c48344adf1ef2139',
+    });
+
     return this.httpClient.post<ReviewAddedResponse>(
       `${this.hostname}/api/reviews/add/`,
       body,
-      {
-        headers: new HttpHeaders({
-          Authorization: `Bearer ${this.token}`,
-        }),
-      },
+      { headers },
+    );
+  }
+
+  addAttachment(reviewId: number, image: File): Observable<AttachmentAddedResponse> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'multipart/form-data',
+    });
+    const formData = new FormData();
+    formData.append('image', image);
+
+    console.info(formData.entries());
+
+    return this.httpClient.post<AttachmentAddedResponse>(
+      `${this.hostname}/api/reviews/${reviewId}/add-attachment/`,
+      formData,
+      { headers },
     );
   }
 }
