f7488a9defddb25a3336aa984ce8ead84be358c0
themylogin <themylogin@gmail.com>
2023-06-23 10:36:35 +0200

    Remove SSH credentials cipher

--- src/app/interfaces/ssh-credentials.interface.ts
+++ src/app/interfaces/ssh-credentials.interface.ts
@@ -1,5 +1,3 @@
-import { CipherType } from 'app/enums/cipher-type.enum';
-
 export interface SshCredentials {
   id?: string;
   host: string;
@@ -7,6 +5,5 @@ export interface SshCredentials {
   username: string;
   private_key: number;
   remote_host_key: string;
-  cipher: CipherType;
   connect_timeout: number;
 }
