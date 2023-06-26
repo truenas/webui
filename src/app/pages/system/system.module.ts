9930618c4f4588808b771f10f00e365c97f0a7ca
Boris Vasilenko <dreaktor0@gmail.com>
2023-06-26 10:44:17 +0300

    NAS-119265 / 23.10 / New ix-table2 component (with fix) (#8337)
    * NAS-119265: Implement `IxTable2`
    
    * NAS-119265: Integrate `IxTable2` into sessions card
    
    * NAS-119265: Improve `IxTable2`
    
    * NAS-119265: Integrate `IxTable2` into user list
    
    * NAS-119265: Minor adjustment
    
    * NAS-119265: Rename `ix-table-row` to `ix-table-cell`
    
    * NAS-119265: Update tests for `UserListComponent`
    
    * NAS-119265: Fix remarks
    
    * NAS-119265: Fix `SortDirection`
    
    ---------
    
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/system/system.module.ts
+++ src/app/pages/system/system.module.ts
@@ -26,6 +26,7 @@ import { AppCommonModule } from 'app/modules/common/app-common.module';
 import { EntityModule } from 'app/modules/entity/entity.module';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
 import { IxIconModule } from 'app/modules/ix-icon/ix-icon.module';
+import { IxTable2Module } from 'app/modules/ix-table2/ix-table2.module';
 import { IxTableModule } from 'app/modules/ix-tables/ix-table.module';
 import { TreeModule } from 'app/modules/ix-tree/tree.module';
 import { JobsModule } from 'app/modules/jobs/jobs.module';
@@ -172,6 +173,7 @@ import { routing } from './system.routing';
     MatSlideToggleModule,
     AppLoaderModule,
     TestIdModule,
+    IxTable2Module,
   ],
   declarations: [
     AdvancedSettingsComponent,
