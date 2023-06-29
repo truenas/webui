c566f5607ac2d576657a8c9ad0b10c4c0eb2eb68
Evgeny Stepanovych <undsoft@gmail.com>
2023-06-29 16:44:08 +0200

    NAS-121827: Updating to Angular 16

--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -105,27 +105,29 @@ defineGlobalsInjections({
   ],
 });
 
-// eslint-disable-next-line no-restricted-globals
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query: unknown) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
+beforeEach(() => {
+  // eslint-disable-next-line no-restricted-globals
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query: unknown) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
 
-// eslint-disable-next-line no-restricted-globals
-Object.defineProperty(window, 'ResizeObserver', {
-  writable: true,
-  value: jest.fn().mockImplementation(() => ({
-    disconnect: jest.fn(),
-    observe: jest.fn(),
-    unobserve: jest.fn(),
-  })),
+  // eslint-disable-next-line no-restricted-globals
+  Object.defineProperty(window, 'ResizeObserver', {
+    writable: true,
+    value: jest.fn().mockImplementation(() => ({
+      disconnect: jest.fn(),
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+    })),
+  });
 });
