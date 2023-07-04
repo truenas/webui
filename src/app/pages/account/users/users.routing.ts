241bc97d5a6181f7b3b33e8cf6b0342fa0cec293
Aurélien Sallé <android.aurelien.salle@gmail.com>
2023-07-04 21:57:49 +0200

    Extract strings from some files for translations
    Extract with yarn & yarn extract ;-)

--- src/app/pages/account/users/users.routing.ts
+++ src/app/pages/account/users/users.routing.ts
@@ -1,16 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { UserListComponent } from 'app/pages/account/users/user-list/user-list.component';
 
 export const routes: Routes = [
   {
     path: 'users',
-    data: { title: 'Users' },
+    data: { title: T('Users') },
     children: [
       {
         path: '',
         component: UserListComponent,
-        data: { title: 'Users', breadcrumb: 'Users' },
+        data: { title: T('Users'), breadcrumb: T('Users') },
       },
     ],
   },
