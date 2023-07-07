7c08e459ae780c1b7fd17985072aac0699996c6e
Boris Vasilenko <dreaktor0@gmail.com>
2023-07-07 07:46:17 +0300

    NAS-122528 / 23.10 / Add tests to ix-table2 (#8345)
    * NAS-119265: Implement `IxTable2`
    
    * NAS-119265: Integrate `IxTable2` into sessions card
    
    * NAS-119265: Improve `IxTable2`
    
    * NAS-119265: Integrate `IxTable2` into user list
    
    * NAS-119265: Minor adjustment
    
    * NAS-119265: Rename `ix-table-row` to `ix-table-cell`
    
    * NAS-119265: Update tests for `UserListComponent`
    
    * NAS-122527: Create `IxTable2Harness`
    
    * NAS-122527: Fix user list tests
    
    * NAS-119265: Fix remarks
    
    * NAS-122528: Add tests to ix-table2
    
    * NAS-122528: Fix `SortDirection` source
    
    * NAS-122528: Remove empty test
    
    * NAS-122528: Fix tests
    
    ---------
    
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

---
+++ src/app/modules/ix-table2/components/ix-table-body/ix-table-body.component.spec.ts
@@ -0,0 +1,69 @@
+import { Spectator } from '@ngneat/spectator';
+import { createComponentFactory } from '@ngneat/spectator/jest';
+import { ArrayDataProvider } from 'app/modules/ix-table2/array-data-provider';
+import { IxTableBodyComponent } from 'app/modules/ix-table2/components/ix-table-body/ix-table-body.component';
+import { TableColumn } from 'app/modules/ix-table2/interfaces/table-column.interface';
+
+interface TestTableData {
+  numberField: number;
+  stringField: string;
+  booleanField: boolean;
+}
+
+const testTableData: TestTableData[] = [
+  { numberField: 1, stringField: 'a', booleanField: true },
+  { numberField: 2, stringField: 'c', booleanField: false },
+  { numberField: 4, stringField: 'b', booleanField: false },
+  { numberField: 3, stringField: 'd', booleanField: true },
+];
+
+const columns: TableColumn<TestTableData>[] = [
+  {
+    title: 'Number Field',
+    propertyName: 'numberField',
+    sortable: true,
+  },
+  {
+    title: 'String Field',
+    propertyName: 'stringField',
+    sortable: true,
+  },
+  {
+    title: 'Boolean Field',
+    propertyName: 'booleanField',
+  },
+];
+
+describe('IxTableBodyComponent', () => {
+  let spectator: Spectator<IxTableBodyComponent<TestTableData>>;
+
+  const createComponent = createComponentFactory({
+    component: IxTableBodyComponent<TestTableData>,
+  });
+
+  beforeEach(() => {
+    const dataProvider = new ArrayDataProvider<TestTableData>();
+    spectator = createComponent({
+      props: { columns, dataProvider },
+    });
+    spectator.component.dataProvider.setRows(testTableData);
+    spectator.fixture.detectChanges();
+  });
+
+  it('shows rows', () => {
+    const rows = spectator.queryAll('.row');
+    expect(rows).toHaveLength(4);
+    expect(
+      spectator.queryAll('.row').map((row) => {
+        const values: string[] = [];
+        row.querySelectorAll('td').forEach((td) => values.push(td.textContent.trim()));
+        return values;
+      }),
+    ).toEqual([
+      ['1', 'a', 'true'],
+      ['2', 'c', 'false'],
+      ['4', 'b', 'false'],
+      ['3', 'd', 'true'],
+    ]);
+  });
+});
---
+++ src/app/modules/ix-table2/components/ix-table-head/ix-table-head.component.spec.ts
@@ -0,0 +1,87 @@
+import { Spectator } from '@ngneat/spectator';
+import { createComponentFactory } from '@ngneat/spectator/jest';
+import { ArrayDataProvider } from 'app/modules/ix-table2/array-data-provider';
+import { IxTableHeadComponent } from 'app/modules/ix-table2/components/ix-table-head/ix-table-head.component';
+import { SortDirection } from 'app/modules/ix-table2/enums/sort-direction.enum';
+import { TableColumn } from 'app/modules/ix-table2/interfaces/table-column.interface';
+
+interface TestTableData {
+  numberField: number;
+  stringField: string;
+  booleanField: boolean;
+}
+
+const columns: TableColumn<TestTableData>[] = [
+  {
+    title: 'Number Field',
+    propertyName: 'numberField',
+    sortable: true,
+  },
+  {
+    title: 'String Field',
+    propertyName: 'stringField',
+    sortable: true,
+  },
+  {
+    title: 'Boolean Field',
+    propertyName: 'booleanField',
+  },
+];
+
+let headers: HTMLDivElement[];
+
+describe('IxTableHeadComponent', () => {
+  let spectator: Spectator<IxTableHeadComponent<TestTableData>>;
+
+  const createComponent = createComponentFactory({
+    component: IxTableHeadComponent<TestTableData>,
+  });
+
+  beforeEach(() => {
+    const dataProvider = new ArrayDataProvider<TestTableData>();
+    spectator = createComponent({
+      props: { columns, dataProvider },
+    });
+    headers = spectator.queryAll('th');
+  });
+
+  it('shows titles', () => {
+    expect(headers.map((header) => header.querySelector('.title').textContent)).toEqual([
+      'Number Field',
+      'String Field',
+      'Boolean Field',
+    ]);
+  });
+
+  it('sets sorting when clicking on one heading', () => {
+    const dataProvider = spectator.component.dataProvider;
+    expect(headers).toHaveLength(3);
+    expect(dataProvider.sorting).toEqual({ active: null, direction: null, propertyName: null });
+    headers[0].click();
+    expect(dataProvider.sorting).toEqual({ active: 0, direction: SortDirection.Asc, propertyName: 'numberField' });
+    headers[0].click();
+    expect(dataProvider.sorting).toEqual({ active: 0, direction: SortDirection.Desc, propertyName: 'numberField' });
+    headers[0].click();
+    expect(dataProvider.sorting).toEqual({ active: 0, direction: null, propertyName: 'numberField' });
+  });
+
+  it('sets sorting when clicking on different headings', () => {
+    const dataProvider = spectator.component.dataProvider;
+    expect(headers).toHaveLength(3);
+
+    expect(dataProvider.sorting).toEqual({ active: null, direction: null, propertyName: null });
+    headers[0].click();
+    expect(dataProvider.sorting).toEqual({ active: 0, direction: SortDirection.Asc, propertyName: 'numberField' });
+    headers[1].click();
+    expect(dataProvider.sorting).toEqual({ active: 1, direction: SortDirection.Asc, propertyName: 'stringField' });
+  });
+
+  it('sets sorting when clicking on an unsortable heading', () => {
+    const dataProvider = spectator.component.dataProvider;
+    expect(headers).toHaveLength(3);
+
+    expect(dataProvider.sorting).toEqual({ active: null, direction: null, propertyName: null });
+    headers[2].click();
+    expect(dataProvider.sorting).toEqual({ active: null, direction: null, propertyName: null });
+  });
+});
