d231a5dc3a67380abf868ba60331e37870f40ea2
Denys Butenko <ua13dark@gmail.com>
2023-07-12 13:18:12 +0700

    NAS-122801: Fix build

--- src/app/pages/network/network.module.ts
+++ src/app/pages/network/network.module.ts
@@ -21,6 +21,7 @@ import { IxIconModule } from 'app/modules/ix-icon/ix-icon.module';
 import { IxTable2Module } from 'app/modules/ix-table2/ix-table2.module';
 import { AppLoaderModule } from 'app/modules/loader/app-loader.module';
 import { TestIdModule } from 'app/modules/test-id/test-id.module';
+import { NetworkConfigurationComponent } from 'app/pages/network/components/configuration/configuration.component';
 import {
   DefaultGatewayDialogComponent,
 } from 'app/pages/network/components/default-gateway-dialog/default-gateway-dialog.component';
@@ -72,6 +73,7 @@ import { routing } from './network.routing';
     IpmiEventsDialogComponent,
     IpmiFormComponent,
     NetworkComponent,
+    NetworkConfigurationComponent,
     NetworkConfigurationCardComponent,
     StaticRouteDeleteDialogComponent,
     StaticRouteFormComponent,
