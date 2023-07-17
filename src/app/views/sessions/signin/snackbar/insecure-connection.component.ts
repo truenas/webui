8d89784889f05b7a89d26ba57c056a7a0086494b
Oleksandr Karpov <Karpov4you@gmail.com>
2023-07-17 17:06:05 +0300

    NAS-122951: Adjust http warning to appear on login screen

--- src/app/views/sessions/signin/snackbar/insecure-connection.component.ts
+++ src/app/views/sessions/signin/snackbar/insecure-connection.component.ts
@@ -1,24 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatSnackBarRef } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
 
 @Component({
+  selector: 'ix-insecure-connection',
   templateUrl: './insecure-connection.component.html',
   styleUrls: ['./insecure-connection.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InsecureConnectionComponent {
-  constructor(
-    private snackRef: MatSnackBarRef<InsecureConnectionComponent>,
-    private router: Router,
-  ) {}
-
-  onConfigure(): void {
-    this.router.navigate(['/system', 'general'], { fragment: 'gui' });
-    this.snackRef.dismiss();
-  }
-
-  onClose(): void {
-    this.snackRef.dismiss();
-  }
 }
