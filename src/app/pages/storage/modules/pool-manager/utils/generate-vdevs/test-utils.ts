315f53f1107f03ee1fadf17cc69e859721a42a8b
Oleksandr Karpov <Karpov4you@gmail.com>
2023-07-17 23:26:34 +0300

    NAS-122959: PR update

--- src/app/pages/storage/modules/pool-manager/utils/generate-vdevs/test-utils.ts
+++ src/app/pages/storage/modules/pool-manager/utils/generate-vdevs/test-utils.ts
@@ -30,7 +30,7 @@ export const generateVdevDisks = _.shuffle([
   {
     ...makeDisk(2, 4),
     devname: 'enclosure2-never-used-too-small',
-    size: 0.1 * GiB,
+    size: 0.99 * GiB,
   },
 
   makeDisk(3, 1),
