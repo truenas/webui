7b831962352c25a59ccb20e6d89e2cc71ecd4d0b
Denys Butenko <ua13dark@gmail.com>
2023-07-20 16:38:25 +0700

    NAS-122854 / 23.10 / Reduce cognitive complexity to 40 (#8424)
    * NAS-122854: Reduce cognitive complexity to 40
    
    * NAS-122854: Address feedback

--- .eslintrc.js
+++ .eslintrc.js
@@ -182,7 +182,7 @@ module.exports = {
         "@typescript-eslint/no-unsafe-argument": "off",
         "rxjs/no-implicit-any-catch": ["off"],
         "rxjs/no-nested-subscribe": ["off"],
-        "sonarjs/cognitive-complexity": ["error", 45],
+        "sonarjs/cognitive-complexity": ["error", 40],
         "@typescript-eslint/consistent-indexed-object-style": ["off"], // Maybe enable later.
         "@typescript-eslint/no-unsafe-enum-comparison": ["off"],
         "@typescript-eslint/no-base-to-string": ["off"],
