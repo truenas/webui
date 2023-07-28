0af12de1dd9a35e3226613423b3a5a56f6150cfb
Boris Vasilenko <dreaktor0@gmail.com>
2023-07-28 12:56:36 +0300

    NAS-123110 / 23.10 / NFS 'Add SPN' option should be presented after 'Require Kerberos' is in DB (#8485)

--- src/app/pages/services/services.module.ts
+++ src/app/pages/services/services.module.ts
@@ -20,6 +20,7 @@ import { IxIconModule } from 'app/modules/ix-icon/ix-icon.module';
 import { IxTableModule } from 'app/modules/ix-tables/ix-table.module';
 import { LayoutModule } from 'app/modules/layout/layout.module';
 import { TestIdModule } from 'app/modules/test-id/test-id.module';
+import { TooltipModule } from 'app/modules/tooltip/tooltip.module';
 import { ServiceFtpComponent } from 'app/pages/services/components/service-ftp/service-ftp.component';
 import { ServiceLldpComponent } from 'app/pages/services/components/service-lldp/service-lldp.component';
 import { AddSpnDialogComponent } from 'app/pages/services/components/service-nfs/add-spn-dialog/add-spn-dialog.component';
@@ -56,6 +57,7 @@ import { routing } from './services.routing';
     AppCommonModule,
     LayoutModule,
     MatDialogModule,
+    TooltipModule,
     TestIdModule,
   ],
   declarations: [
