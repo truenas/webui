721e9caf96a574859f0a3d7d3764fe51e1aaf257
RehanY147 <rehanyousaf147@gmail.com>
2023-08-01 16:52:04 +0500

    NAS-122920: Don't refresh table until the job has concluded
    (cherry picked from commit 5eb1be8461a83e9cdbf983f87ef267323cc0736a)

--- src/app/pages/data-protection/data-protection-dashboard.component.ts
+++ src/app/pages/data-protection/data-protection-dashboard.component.ts
@@ -569,19 +569,20 @@ export class DataProtectionDashboardComponent implements OnInit {
               );
             }),
             switchMap((id: number) => this.store$.select(selectJob(id)).pipe(filter(Boolean))),
-            catchError((error: WebsocketError) => {
-              this.dialogService.error(this.errorHandler.parseWsError(error));
+            tap((job) => {
+              if (!([JobState.Running, JobState.Pending].includes(job.state))) {
+                this.refreshTable(TaskCardId.Replication); return;
+              }
+              row.state.state = job.state;
+              row.job = { ...job };
+              this.jobStates.set(job.id, job.state);
+            }),
+            catchError((error: Job) => {
+              this.dialogService.error(this.errorHandler.parseJobError(error));
               return EMPTY;
             }),
             untilDestroyed(this),
-          ).subscribe((job: Job) => {
-            row.state.state = job.state;
-            row.job = { ...job };
-            if (this.jobStates.get(job.id) !== job.state) {
-              this.refreshTable(TaskCardId.Replication);
-            }
-            this.jobStates.set(job.id, job.state);
-          });
+          ).subscribe();
         },
       },
       {
