ea0423aa958e4de9691ef803106c3295821edbdd
Oleksandr Karpov <Karpov4you@gmail.com>
2023-08-11 13:26:56 +0300

    NAS-123151: HA: Alert is duplicated

--- src/app/modules/alerts/store/alert.actions.ts
+++ src/app/modules/alerts/store/alert.actions.ts
@@ -6,8 +6,7 @@ export const alertPanelClosed = createAction('[Alert] Panel Closed');
 export const alertsLoaded = createAction('[Alerts API] Loaded', props<{ alerts: Alert[] }>());
 export const alertsNotLoaded = createAction('[Alerts API] Not Loaded', props<{ error: string }>());
 
-export const alertAdded = createAction('[Alerts API] Alert Added', props<{ alert: Alert }>());
-export const alertChanged = createAction('[Alerts API] Alert Changed', props<{ alert: Alert }>());
+export const alertReceived = createAction('[Alerts API] Alert Received');
 export const alertRemoved = createAction('[Alerts API] Alert Removed', props<{ id: string }>());
 
 export const dismissAlertPressed = createAction('[Alert Panel] Dismiss Pressed', props<{ id: string }>());
