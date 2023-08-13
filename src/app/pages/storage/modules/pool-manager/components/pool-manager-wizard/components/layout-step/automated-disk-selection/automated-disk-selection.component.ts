f9a9151ade0d17d93b462813929bce227e182ac3
Boris Vasilenko <dreaktor0@gmail.com>
2023-08-13 20:29:07 +0300

    NAS-123284 / 24.04 / Unable to add spares in new Pool Creation Wizard (#8563)
    * NAS-123284: Fix pool creation bug
    
    * NAS-123284: Revert "NAS-226: Fix pool creation bug"
    
    * NAS-123284: Fix pool update bug
    
    ---------
    
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/storage/modules/pool-manager/components/pool-manager-wizard/components/layout-step/automated-disk-selection/automated-disk-selection.component.ts
+++ src/app/pages/storage/modules/pool-manager/components/pool-manager-wizard/components/layout-step/automated-disk-selection/automated-disk-selection.component.ts
@@ -102,7 +102,7 @@ export class AutomatedDiskSelectionComponent implements OnInit, OnChanges {
 
   resetToDefaults(): void {
     this.form.reset({
-      layout: null,
+      layout: this.canChangeLayout ? null : this.limitLayouts[0],
       sizeAndType: [null, null],
       width: null,
       treatDiskSizeAsMinimum: false,
@@ -234,7 +234,7 @@ export class AutomatedDiskSelectionComponent implements OnInit, OnChanges {
 
     const isValueNull = this.form.controls.layout.value === null;
     if (!isValueNull && !layoutOptions.some((option) => option.value === this.form.controls.layout.value)) {
-      this.form.controls.layout.setValue(null, { emitEvent: false });
+      this.form.controls.layout.setValue(this.canChangeLayout ? null : this.limitLayouts[0], { emitEvent: false });
     }
     this.store.getLayoutsForVdevType(this.type)
       .pipe(
