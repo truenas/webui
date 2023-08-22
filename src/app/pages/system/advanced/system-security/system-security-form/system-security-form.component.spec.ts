72bfde5ab59ba56bedf422ed88b55f91f324aa11
Oleksandr Karpov <Karpov4you@gmail.com>
2023-08-22 18:00:50 +0300

    NAS-122568: PR update

--- src/app/pages/system/advanced/system-security/system-security-form/system-security-form.component.spec.ts
+++ src/app/pages/system/advanced/system-security/system-security-form/system-security-form.component.spec.ts
@@ -5,6 +5,7 @@ import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
 import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
 import { mockEntityJobComponentRef } from 'app/core/testing/utils/mock-entity-job-component-ref.utils';
 import { mockCall, mockWebsocket } from 'app/core/testing/utils/mock-websocket.utils';
 import { ProductType } from 'app/enums/product-type.enum';
@@ -45,7 +46,9 @@ describe('SystemSecurityFormComponent', () => {
       mockWebsocket([
         mockCall('system.security.update'),
       ]),
-      mockProvider(DialogService),
+      mockProvider(DialogService, {
+        confirm: jest.fn(() => of()),
+      }),
       mockProvider(SnackbarService),
       mockProvider(MatDialog, {
         open: jest.fn(() => mockEntityJobComponentRef),
