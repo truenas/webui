634e280af3dfae6e1bdba40a13b0770607fee2f9
Evgeny Stepanovych <undsoft@gmail.com>
2023-08-29 15:02:35 +0200

    NAS-123744: Removing display device password
    (cherry picked from commit 5dbcda0b7199ea0824e0001b8e764b40731d299a)

--- src/app/pages/vm/vm-list/display-vm-dialog/display-vm-dialog.component.ts
+++ src/app/pages/vm/vm-list/display-vm-dialog/display-vm-dialog.component.ts
@@ -13,6 +13,8 @@ import { DialogService } from 'app/services/dialog.service';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
 import { WebSocketService } from 'app/services/ws.service';
 
+// TODO: Dialog is useless now because only SPICE is supported.
+// TODO: Check with middleware if vm.get_display_web_uri can be replaced with a property in vm.get_display_devices.
 @UntilDestroy()
 @Component({
   templateUrl: './display-vm-dialog.component.html',
@@ -22,7 +24,6 @@ import { WebSocketService } from 'app/services/ws.service';
 export class DisplayVmDialogComponent {
   form = this.formBuilder.group({
     display_device: [null as number, Validators.required],
-    password: ['', Validators.required],
   });
 
   optionsDevice$ = of(
@@ -36,10 +37,6 @@ export class DisplayVmDialogComponent {
     return this.data.displayDevices.length === 1;
   }
 
-  get isPasswordConfigured(): boolean {
-    return this.form.controls.password.enabled;
-  }
-
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DisplayVmDialogData,
     @Inject(WINDOW) private window: Window,
@@ -51,46 +48,26 @@ export class DisplayVmDialogComponent {
     private translate: TranslateService,
     private loader: AppLoaderService,
   ) {
-    if (this.isSingleDevice && !this.data.displayDevices[0].attributes.password_configured) {
+    if (this.isSingleDevice) {
       this.dialogRef.close(true);
       this.openDisplayDevice(this.data.displayDevices[0].id);
       return;
     }
 
-    this.form.controls.display_device.valueChanges.pipe(untilDestroyed(this)).subscribe((selectDeviceId: number) => {
-      this.form.controls.password.patchValue('');
-      if (this.data.displayDevices.find((device) => device.id === selectDeviceId).attributes.password_configured) {
-        this.form.controls.password.enable();
-      } else {
-        this.form.controls.password.disable();
-      }
-    });
-
     if (this.data.displayDevices.length) {
       this.form.controls.display_device.patchValue(this.data.displayDevices[0].id);
-    } else {
-      this.form.controls.password.disable();
     }
   }
 
   onOpen(): void {
-    this.openDisplayDevice(this.form.value.display_device, this.form.value.password);
+    this.openDisplayDevice(this.form.value.display_device);
   }
 
-  private openDisplayDevice(displayDeviceId: number, password?: string): void {
+  private openDisplayDevice(displayDeviceId: number): void {
     this.loader.open();
-    let displayOptions = {
+    const displayOptions = {
       protocol: this.window.location.protocol.replace(':', '').toUpperCase(),
     } as VmDisplayWebUriParamsOptions;
-
-    if (password) {
-      displayOptions = {
-        ...displayOptions,
-        devices_passwords: [
-          { device_id: displayDeviceId, password },
-        ],
-      };
-    }
 
     const requestParams: VmDisplayWebUriParams = [
       this.data.vm.id,
