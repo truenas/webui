60401d91eebb257d4d66c42f7e3e6fabc4f65abf
Oleksandr Karpov <Karpov4you@gmail.com>
2023-08-30 18:24:19 +0300

    NAS-122963: PR update

--- src/app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/1-general-wizard-step/general-wizard-step.component.spec.ts
+++ src/app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/1-general-wizard-step/general-wizard-step.component.spec.ts
@@ -13,7 +13,7 @@ import { PoolWarningsComponent } from 'app/pages/storage/modules/pool-manager/co
 import {
   GeneralWizardStepComponent,
 } from 'app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/1-general-wizard-step/general-wizard-step.component';
-import { PoolWizardNameAsyncValidatorService } from 'app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/1-general-wizard-step/pool-wizard-name-validation.service';
+import { PoolWizardNameValidationService } from 'app/pages/storage/modules/pool-manager/components/pool-manager-wizard/steps/1-general-wizard-step/pool-wizard-name-validation.service';
 import { PoolManagerStore } from 'app/pages/storage/modules/pool-manager/store/pool-manager.store';
 import { DialogService } from 'app/services/dialog.service';
 
@@ -40,7 +40,7 @@ describe('GeneralWizardStepComponent', () => {
           'AES-128-GCM': 'AES-128-GCM',
         }),
       ]),
-      mockProvider(PoolWizardNameAsyncValidatorService, {
+      mockProvider(PoolWizardNameValidationService, {
         validatePoolName: () => of(null),
       }),
       mockProvider(DialogService, {
@@ -71,6 +71,7 @@ describe('GeneralWizardStepComponent', () => {
     expect(spectator.inject(PoolManagerStore).setGeneralOptions).toHaveBeenCalledWith({
       name: 'newpool',
       encryption: null,
+      nameErrors: null,
     });
   });
 
@@ -87,12 +88,16 @@ describe('GeneralWizardStepComponent', () => {
     await encryptionCheckbox.setValue(true);
     spectator.detectChanges();
 
+    const nameInput = await loader.getHarness(IxInputHarness.with({ label: 'Name' }));
+    await nameInput.setValue('test');
+
     const encryptionStandards = await loader.getHarness(IxSelectHarness.with({ label: 'Encryption Standard' }));
     await encryptionStandards.setValue('AES-128-GCM');
 
     expect(spectator.inject(PoolManagerStore).setGeneralOptions).toHaveBeenCalledWith({
-      name: '',
+      name: 'test',
       encryption: 'AES-128-GCM',
+      nameErrors: null,
     });
   });
 
