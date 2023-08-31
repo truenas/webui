bb7a7a3a4286cad7ec67246ce5483a359a2a5cae
ericbsd <ericturgeon.bsd@gmail.com>
2023-08-31 16:53:44 -0300

    Replace UI code to create pool with API calls
    (cherry picked from commit cd4b67ecf66bdf9f687ca0267218368da8611231)
    (cherry picked from commit 4a0ed1395b00c89537e3ca397ddefdef60590187)

--- tests/bdd/ha-bhyve02/test_NAS_T0961.py
+++ tests/bdd/ha-bhyve02/test_NAS_T0961.py
@@ -8,7 +8,8 @@ import xpaths
 from function import (
     wait_on_element,
     wait_on_element_disappear,
-    get
+    get,
+    create_Pool
 )
 from pytest_bdd import (
     given,
@@ -30,8 +31,8 @@ def test_creating_new_pool_and_set_it_as_system_dataset(driver):
 def the_browser_is_open_navigate_to_nas_url(driver, nas_url, request):
     """the browser is open, navigate to "{nas_url}"."""
     depends(request, ["Setup_HA"], scope='session')
-    global host
-    host = nas_url
+    global NAS_HOSTNAME
+    NAS_HOSTNAME = nas_url
     if nas_url not in driver.current_url:
         driver.get(f"http://{nas_url}/ui/sessions/signin")
         assert wait_on_element(driver, 5, xpaths.login.user_Input)
@@ -40,9 +41,9 @@ def the_browser_is_open_navigate_to_nas_url(driver, nas_url, request):
 @when(parsers.parse('the login page appears, enter "{admin_user}" and "{password}"'))
 def the_login_page_appear_enter_root_and_password(driver, admin_user, password):
     """the login page appears, enter "{admin_user}" and "{password}"."""
-    global root_Password, admin_User
-    admin_User = admin_user
-    root_Password = password
+    global ADMIN_PASSWORD, ADMIN_USER
+    ADMIN_USER = admin_user
+    ADMIN_PASSWORD = password
 
     rsc.Login_If_Not_On_Dashboard(driver, admin_user, password)
 
@@ -61,46 +62,51 @@ def navigate_to_storage(driver):
     driver.find_element_by_xpath(xpaths.side_Menu.storage).click()
 
 
+# TODO: when Bluefin is replaced by Cobia the steps below need to be refactor.
 @then('when the storage page appears, click Create')
 def when_the_storage_page_appears_click_create(driver):
     """when the storage page appears, click Create."""
     assert wait_on_element(driver, 7, xpaths.storage.title)
-    assert wait_on_element(driver, 7, '//a[contains(.,"Create Pool")]', 'clickable')
-    driver.find_element_by_xpath('//a[contains(.,"Create Pool")]').click()
+    # assert wait_on_element(driver, 7, '//a[contains(.,"Create Pool")]', 'clickable')
+    # driver.find_element_by_xpath('//a[contains(.,"Create Pool")]').click()
 
 
 @then('the Pools page should open')
 def the_pools_page_should_open(driver):
     """the Pools page should open."""
-    assert wait_on_element(driver, 5, '//div[contains(.,"Pools")]')
+    # assert wait_on_element(driver, 5, '//div[contains(.,"Pools")]')
+    pass
 
 
 @then('the Pool Manager page should open')
 def the_pool_manager_page_should_open(driver):
     """the Pool Manager page should open."""
-    assert wait_on_element(driver, 5, xpaths.pool_manager.title)
-    assert wait_on_element_disappear(driver, 120, xpaths.popup.please_Wait)
+    # assert wait_on_element(driver, 5, xpaths.pool_manager.title)
+    # assert wait_on_element_disappear(driver, 120, xpaths.popup.please_Wait)
+    pass
 
 
 @then(parsers.parse('enter {pool_name} for pool name, check the box next to {disk}'))
 def enter_dozer_for_pool_name_check_the_box_next_to_sdb(driver, pool_name, disk):
     """enter dozer for pool name, check the box next to sdb."""
-    assert wait_on_element(driver, 7, '//input[@id="pool-manager__name-input-field"]', 'inputable')
-    driver.find_element_by_xpath('//input[@id="pool-manager__name-input-field"]').clear()
-    driver.find_element_by_xpath('//input[@id="pool-manager__name-input-field"]').send_keys(pool_name)
-    driver.find_element_by_xpath(f'//mat-checkbox[@id="pool-manager__disks-{disk}"]').click()
-    if wait_on_element(driver, 3, '//mat-dialog-container[contains(.,"Warning: sd")]'):
-        assert wait_on_element(driver, 5, '//button[*/text()=" Close "]', 'clickable')
-        driver.find_element_by_xpath('//button[*/text()=" Close "]').click()
+    # assert wait_on_element(driver, 7, '//input[@id="pool-manager__name-input-field"]', 'inputable')
+    # driver.find_element_by_xpath('//input[@id="pool-manager__name-input-field"]').clear()
+    # driver.find_element_by_xpath('//input[@id="pool-manager__name-input-field"]').send_keys(pool_name)
+    # driver.find_element_by_xpath(f'//mat-checkbox[@id="pool-manager__disks-{disk}"]').click()
+    # if wait_on_element(driver, 3, '//mat-dialog-container[contains(.,"Warning: sd")]'):
+    #     assert wait_on_element(driver, 5, '//button[*/text()=" Close "]', 'clickable')
+    #     driver.find_element_by_xpath('//button[*/text()=" Close "]').click()
+    create_Pool(NAS_HOSTNAME, (ADMIN_USER, ADMIN_PASSWORD), pool_name)
 
 
 @then('press right arrow under data vdev, click on the Force checkbox')
 def press_right_arrow_under_data_vdev_click_on_the_force_checkbox(driver):
     """press right arrow under data vdev, click on the Force checkbox."""
-    assert wait_on_element(driver, 7, '//button[@id="vdev__add-button"]', 'clickable')
-    driver.find_element_by_xpath('//button[@id="vdev__add-button"]').click()
-    assert wait_on_element(driver, 7, '//mat-checkbox[@id="pool-manager__force-submit-checkbox"]')
-    driver.find_element_by_xpath('//mat-checkbox[@id="pool-manager__force-submit-checkbox"]').click()
+    # assert wait_on_element(driver, 7, '//button[@id="vdev__add-button"]', 'clickable')
+    # driver.find_element_by_xpath('//button[@id="vdev__add-button"]').click()
+    # assert wait_on_element(driver, 7, '//mat-checkbox[@id="pool-manager__force-submit-checkbox"]')
+    # driver.find_element_by_xpath('//mat-checkbox[@id="pool-manager__force-submit-checkbox"]').click()
+    pass
 
 
 # This is passed since the confirmation box is missing in Cobia
@@ -108,25 +114,29 @@ def press_right_arrow_under_data_vdev_click_on_the_force_checkbox(driver):
 def on_the_warning_box_click_confirm_checkbox_and_click_continue(driver):
     """on the warning box, click Confirm checkbox and click CONTINUE."""
     # rsc.Confirm_Single_Disk(driver)
+    pass
 
 
 @then('click Create, click on Confirm checkbox and click CREATE POOL')
 def click_create_click_on_confirm_checkbox_and_click_create_pool(driver):
     """click Create, click on Confirm checkbox and click CREATE POOL."""
-    assert wait_on_element(driver, 7, '//button[@name="create-button"]', 'clickable')
-    driver.find_element_by_xpath('//button[@name="create-button"]').click()
-    rsc.Confirm_Creating_Pool(driver)
+    # assert wait_on_element(driver, 7, '//button[@name="create-button"]', 'clickable')
+    # driver.find_element_by_xpath('//button[@name="create-button"]').click()
+    # rsc.Confirm_Creating_Pool(driver)
+    pass
 
 
 @then('Create Pool should appear while the pool is being created')
 def create_pool_should_appear_while_the_pool_is_being_created(driver):
     """Create Pool should appear while the pool is being created."""
-    assert wait_on_element_disappear(driver, 60, '//h1[contains(.,"Create Pool")]')
+    # assert wait_on_element_disappear(driver, 60, '//h1[contains(.,"Create Pool")]')
+    pass
 
 
 @then('you should be returned to the list of Pools')
 def you_should_be_returned_to_the_list_of_pools(driver):
     """you should be returned to the list of Pools."""
+    driver.refresh()
     assert wait_on_element(driver, 7, xpaths.storage.title)
 
 
@@ -209,7 +219,7 @@ def refresh_and_wait_for_the_second_node_to_be_up(driver):
 @then('verify the system dataset is dozer on the active node')
 def verify_the_system_dataset_is_dozer_on_the_active_node(driver):
     """verify the system dataset is dozer on the active node."""
-    results = get(host, '/systemdataset/', ('root', root_Password))
+    results = get(NAS_HOSTNAME, '/systemdataset/', (ADMIN_USER, ADMIN_PASSWORD))
     assert results.status_code == 200, results.text
     assert results.json()['pool'] == 'dozer', results.text
 
@@ -227,7 +237,7 @@ def wait_for_the_login_and_the_HA_enabled_status_and_login(driver):
     """wait for the login and the HA enabled status and login."""
     rsc.HA_Login_Status_Enable(driver)
 
-    rsc.Login(driver, admin_User, root_Password)
+    rsc.Login(driver, ADMIN_USER, ADMIN_PASSWORD)
 
     rsc.Verify_The_Dashboard(driver)
 
@@ -241,6 +251,6 @@ def wait_for_the_login_and_the_HA_enabled_status_and_login(driver):
 @then('verify the system dataset is dozer on the active node after failover')
 def verify_the_system_dataset_is_dozer_on_the_active_node_after_failover(driver):
     """verify the system dataset is dozer on the active node after failover."""
-    results = get(host, '/systemdataset/', ('root', root_Password))
+    results = get(NAS_HOSTNAME, '/systemdataset/', ('root', ADMIN_PASSWORD))
     assert results.status_code == 200, results.text
     assert results.json()['pool'] == 'dozer', results.text
