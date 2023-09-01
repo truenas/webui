724ea4c63db42177719e0f1cfee6f11d25f12710
ericbsd <ericturgeon.bsd@gmail.com>
2023-09-01 09:49:42 -0300

    Added create pool from API in single controller UI test
    Refactored create pool and created a method to create encrypted pool
    
    (cherry picked from commit 79e8cd762f3d1ebf928ecf7841954eb62ea7479c)

--- tests/bdd/scale/test_003_NAS_T1071.py
+++ tests/bdd/scale/test_003_NAS_T1071.py
@@ -7,12 +7,13 @@ from function import (
     wait_on_element,
     is_element_present,
     wait_on_element_disappear,
+    create_Pool
 )
 from pytest_bdd import (
     given,
     scenario,
     then,
-    when,
+    when
 )
 import pytest
 pytestmark = [pytest.mark.debug_test]
@@ -51,35 +52,39 @@ def you_should_be_on_the_dashboard_click_storage_on_the_side_menu(driver):
     driver.find_element_by_xpath(xpaths.side_Menu.storage).click()
 
 
+# TODO: when Bluefin is replaced by Cobia the steps below need to be refactor.
 @then('when the storage page appears, click Create')
 def when_the_storage_page_appears_click_create(driver):
     """when the storage page appears, click Create."""
     assert wait_on_element(driver, 10, xpaths.storage.title)
-    assert wait_on_element(driver, 10, xpaths.storage.create_Pool_Button, 'clickable')
-    driver.find_element_by_xpath(xpaths.storage.create_Pool_Button).click()
+    # assert wait_on_element(driver, 10, xpaths.storage.create_Pool_Button, 'clickable')
+    # driver.find_element_by_xpath(xpaths.storage.create_Pool_Button).click()
 
 
 @then('on the Pool Manager page enter tank for pool name,')
-def on_the_pool_manager_page_enter_tank_for_pool_name(driver):
+def on_the_pool_manager_page_enter_tank_for_pool_name(driver, nas_ip, root_password):
     """on the Pool Manager page enter tank for pool name,."""
-    assert wait_on_element(driver, 7, xpaths.pool_manager.title)
-    assert wait_on_element(driver, 10, xpaths.pool_manager.name_Input, 'inputable')
-    driver.find_element_by_xpath(xpaths.pool_manager.name_Input).send_keys('tank')
+    # assert wait_on_element(driver, 7, xpaths.pool_manager.title)
+    # assert wait_on_element(driver, 10, xpaths.pool_manager.name_Input, 'inputable')
+    # driver.find_element_by_xpath(xpaths.pool_manager.name_Input).send_keys('tank')
+    create_Pool(nas_ip, ('root', root_password), 'tank')
 
 
 @then('select the a disk and press right arrow under Data VDevs')
 def select_the_a_disk_and_press_right_arrow_under_data_vdev(driver):
     """select the a disk and press right arrow under data vdev."""
-    assert wait_on_element(driver, 5, xpaths.pool_manager.first_Disk_Checkbox, 'clickable')
-    driver.find_element_by_xpath(xpaths.pool_manager.first_Disk_Checkbox).click()
-    assert wait_on_element(driver, 5, xpaths.pool_manager.vdev_Add_Button, 'clickable')
-    driver.find_element_by_xpath(xpaths.pool_manager.vdev_Add_Button).click()
+    # assert wait_on_element(driver, 5, xpaths.pool_manager.first_Disk_Checkbox, 'clickable')
+    # driver.find_element_by_xpath(xpaths.pool_manager.first_Disk_Checkbox).click()
+    # assert wait_on_element(driver, 5, xpaths.pool_manager.vdev_Add_Button, 'clickable')
+    # driver.find_element_by_xpath(xpaths.pool_manager.vdev_Add_Button).click()
+    pass
 
 
 @then('click on the Force checkbox on the warning box')
 def click_on_the_force_checkbox_on_the_warning_box(driver):
-    assert wait_on_element(driver, 7, xpaths.pool_manager.force_Checkbox, 'clickable')
-    driver.find_element_by_xpath(xpaths.pool_manager.force_Checkbox).click()
+    # assert wait_on_element(driver, 7, xpaths.pool_manager.force_Checkbox, 'clickable')
+    # driver.find_element_by_xpath(xpaths.pool_manager.force_Checkbox).click()
+    pass
 
 
 # This is passed since the confirmation box is missing in Cobia
@@ -87,21 +92,24 @@ def click_on_the_force_checkbox_on_the_warning_box(driver):
 def click_confirm_checkbox_and_click_continue(driver):
     """click Confirm checkbox and click CONTINUE."""
     # rsc.Confirm_Single_Disk(driver)
+    pass
 
 
 @then('click Create, click on Confirm checkbox and click CREATE POOL')
 def click_create_click_on_confirm_checkbox_and_click_create_pool(driver):
     """click Create, click on Confirm checkbox and click CREATE POOL."""
-    assert wait_on_element(driver, 5, xpaths.pool_manager.create_Button, 'clickable')
-    driver.find_element_by_xpath(xpaths.pool_manager.create_Button).click()
-    rsc.Confirm_Creating_Pool(driver)
+    # assert wait_on_element(driver, 5, xpaths.pool_manager.create_Button, 'clickable')
+    # driver.find_element_by_xpath(xpaths.pool_manager.create_Button).click()
+    # rsc.Confirm_Creating_Pool(driver)
+    pass
 
 
 @then('Create Pool should appear while the pool is being created')
 def create_pool_should_appear_while_the_pool_is_being_created(driver):
     """Create Pool should appear while the pool is being created."""
-    assert wait_on_element(driver, 10, xpaths.pool_manager.create_Pool_Popup)
-    assert wait_on_element_disappear(driver, 120, xpaths.pool_manager.create_Pool_Popup)
+    # assert wait_on_element(driver, 10, xpaths.pool_manager.create_Pool_Popup)
+    # assert wait_on_element_disappear(driver, 120, xpaths.pool_manager.create_Pool_Popup)
+    driver.refresh()
     assert wait_on_element(driver, 10, xpaths.storage.title)
 
 
