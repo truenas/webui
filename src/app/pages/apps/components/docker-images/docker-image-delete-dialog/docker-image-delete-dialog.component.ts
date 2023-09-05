39a9aa3a8dc82bda84536144e4476ca86cf2711d
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-09-05 11:21:39 -0300

    NAS-123836 / 24.04 / Add Manage Container Images (by denysbutenko) (#8751)
    * Empty commit to create PR on github.
    
    You should reset it
    
    * NAS-123836: Add Manage Container Images
    
    * NAS-123836: Add Manage Container Images
    
    ---------
    
    Co-authored-by: Vladimir Vinogradenko <vladimirv@bugclerk.tys1.ixsystems.net>
    Co-authored-by: Evgeny Stepanovych <undsoft@gmail.com>

---
+++ src/app/pages/apps/components/docker-images/docker-image-delete-dialog/docker-image-delete-dialog.component.ts
@@ -0,0 +1,90 @@
+import { KeyValue } from '@angular/common';
+import {
+  Component, ChangeDetectionStrategy, Inject, ChangeDetectorRef, TrackByFunction,
+} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
+import { filter } from 'rxjs/operators';
+import { BulkListItem, BulkListItemState } from 'app/core/components/bulk-list-item/bulk-list-item.interface';
+import { ContainerImage, DeleteContainerImageParams } from 'app/interfaces/container-image.interface';
+import { CoreBulkResponse } from 'app/interfaces/core-bulk.interface';
+import { Job } from 'app/interfaces/job.interface';
+import { WebSocketService } from 'app/services/ws.service';
+
+@UntilDestroy()
+@Component({
+  templateUrl: './docker-image-delete-dialog.component.html',
+  styleUrls: ['./docker-image-delete-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DockerImageDeleteDialogComponent {
+  form = this.fb.group({
+    confirm: [false, [Validators.requiredTrue]],
+  });
+  isJobCompleted = false;
+  bulkItems = new Map<string, BulkListItem<ContainerImage>>();
+  hasErrors = false;
+
+  get successCount(): number {
+    return [...this.bulkItems.values()].filter((item) => item.state === BulkListItemState.Success).length;
+  }
+  get failedCount(): number {
+    return [...this.bulkItems.values()].filter((item) => item.state === BulkListItemState.Error).length;
+  }
+  readonly trackByKey: TrackByFunction<KeyValue<string, BulkListItem<ContainerImage>>> = (_, entry) => entry.key;
+
+  constructor(
+    private fb: FormBuilder,
+    private ws: WebSocketService,
+    private cdr: ChangeDetectorRef,
+    private dialogRef: MatDialogRef<DockerImageDeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public images: ContainerImage[],
+  ) {
+    this.images.forEach((image) => {
+      this.bulkItems.set(image.id, { state: BulkListItemState.Initial, item: image });
+    });
+  }
+
+  onSubmit(): void {
+    const deleteParams = this.images.map((image) => [image.id]);
+
+    this.images.forEach((image) => {
+      this.bulkItems.set(image.id, { state: BulkListItemState.Running, item: image });
+    });
+
+    this.ws.job('core.bulk', ['container.image.delete', deleteParams]).pipe(
+      filter((job: Job<CoreBulkResponse<void>[], DeleteContainerImageParams[]>) => !!job.result),
+      untilDestroyed(this),
+    ).subscribe((response) => {
+      response.arguments[1].forEach((params, index: number) => {
+        const [imageId] = params.toString().split(',');
+        const bulkItem = this.bulkItems.get(imageId);
+        if (bulkItem) {
+          const item = response.result[index];
+          if (item.error) {
+            this.bulkItems.set(imageId, {
+              ...bulkItem,
+              state: BulkListItemState.Error,
+              message: item.error
+                .replace('[EFAULT]', '')
+                .replace('DockerError(409, \'', '')
+                .replace('\')', ''),
+            });
+          } else {
+            this.bulkItems.set(imageId, {
+              ...bulkItem,
+              state: BulkListItemState.Success,
+              message: null,
+            });
+            if (this.bulkItems.size === 1) {
+              this.dialogRef.close(true);
+            }
+          }
+        }
+      });
+      this.isJobCompleted = true;
+      this.cdr.markForCheck();
+    });
+  }
+}
---
+++ src/app/pages/apps/components/docker-images/pull-image-form/pull-image-form.component.ts
@@ -0,0 +1,86 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { TranslateService } from '@ngx-translate/core';
+import { latestVersion } from 'app/constants/catalog.constants';
+import helptext from 'app/helptext/apps/apps';
+import { PullContainerImageParams } from 'app/interfaces/container-image.interface';
+import { EntityJobComponent } from 'app/modules/entity/entity-job/entity-job.component';
+import { IxSlideInRef } from 'app/modules/ix-forms/components/ix-slide-in/ix-slide-in-ref';
+import { DialogService } from 'app/services/dialog.service';
+import { ErrorHandlerService } from 'app/services/error-handler.service';
+
+@UntilDestroy()
+@Component({
+  templateUrl: './pull-image-form.component.html',
+  styleUrls: ['./pull-image-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PullImageFormComponent {
+  isFormLoading = false;
+
+  form = this.fb.group({
+    from_image: ['', Validators.required],
+    tag: [latestVersion],
+    username: [''],
+    password: [''],
+  });
+
+  readonly tooltips = {
+    from_image: helptext.pullImageForm.imageName.tooltip,
+    tag: helptext.pullImageForm.imageTags.tooltip,
+    username: helptext.pullImageForm.username.tooltip,
+    password: helptext.pullImageForm.password.tooltip,
+  };
+
+  constructor(
+    private slideInRef: IxSlideInRef<PullImageFormComponent>,
+    private cdr: ChangeDetectorRef,
+    private errorHandler: ErrorHandlerService,
+    private fb: FormBuilder,
+    private matDialog: MatDialog,
+    private translate: TranslateService,
+    private dialogService: DialogService,
+  ) {}
+
+  onSubmit(): void {
+    const values = this.form.value;
+
+    const params: PullContainerImageParams = {
+      from_image: values.from_image,
+    };
+
+    if (values.tag) {
+      params.tag = values.tag;
+    }
+    if (values.username || values.password) {
+      params.docker_authentication = {
+        username: values.username,
+        password: values.password,
+      };
+    }
+
+    this.isFormLoading = true;
+    const dialogRef = this.matDialog.open(EntityJobComponent, {
+      data: {
+        title: this.translate.instant('Pulling...'),
+      },
+      disableClose: true,
+    });
+    dialogRef.componentInstance.setCall('container.image.pull', [params]);
+    dialogRef.componentInstance.submit();
+    dialogRef.componentInstance.success.pipe(untilDestroyed(this)).subscribe(() => {
+      this.isFormLoading = false;
+      this.cdr.markForCheck();
+      dialogRef.close();
+      this.slideInRef.close();
+    });
+    dialogRef.componentInstance.failure.pipe(untilDestroyed(this)).subscribe((error) => {
+      this.isFormLoading = false;
+      dialogRef.close();
+      this.dialogService.error(this.errorHandler.parseJobError(error));
+      this.cdr.markForCheck();
+    });
+  }
+}
