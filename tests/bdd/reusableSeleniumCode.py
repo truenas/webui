0561ec9edec59a343b61af15e36f1bcb7a9b5afa
ericbsd <ericturgeon.bsd@gmail.com>
2023-09-05 15:48:55 -0300

    Rework HA_Login_Status_Enable

--- tests/bdd/reusableSeleniumCode.py
+++ tests/bdd/reusableSeleniumCode.py
@@ -86,9 +86,9 @@ def Go_To_Service(driver):
 
 
 def HA_Login_Status_Enable(driver):
-    assert wait_on_element(driver, 240, xpaths.login.ha_Status_Enable)
+    wait_on_element(driver, 180, xpaths.login.user_Input)
     driver.refresh()
-    assert wait_on_element(driver, 10, xpaths.login.user_Input)
+    assert wait_on_element(driver, 180, xpaths.login.ha_Status_Enable)
 
 
 def Input_Value(driver, xpath, value):
