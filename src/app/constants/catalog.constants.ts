3422aab22ee7a9ad3821eb4e209d93eebab8303f
Denys Butenko <ua13dark@gmail.com>
2023-09-05 15:39:09 +0700

    NAS-123836: Manage Container Images

--- src/app/constants/catalog.constants.ts
+++ src/app/constants/catalog.constants.ts
@@ -1,4 +1,5 @@
 export const officialCatalog = 'TRUENAS';
 export const ixChartApp = 'ix-chart';
 export const chartsTrain = 'charts';
+export const latestVersion = 'latest';
 export const appImagePlaceholder = 'assets/images/truenas_scale_ondark_favicon.png';
