76a5c0e2a1faa2382f3d068d94120854ced73d42
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-09-08 09:19:01 -0300

    NAS-123749 / 23.10-RC.1 /  "Spares" category from pool creation "Configuration Preview" inaccurate/unneeded (by AlexKarpov98) (#8800)
    * Empty commit to create PR on github.
    
    You should reset it
    
    * NAS-123749: PR Update
    
    ---------
    
    Co-authored-by: Vladimir Vinogradenko <vladimirv@bugclerk.tys1.ixsystems.net>
    Co-authored-by: Oleksandr Karpov <Karpov4you@gmail.com>

--- src/app/pages/storage/modules/pool-manager/components/configuration-preview/configuration-preview.component.ts
+++ src/app/pages/storage/modules/pool-manager/components/configuration-preview/configuration-preview.component.ts
@@ -3,6 +3,7 @@ import {
 } from '@angular/core';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
+import { map } from 'rxjs';
 import { vdevTypeLabels } from 'app/enums/v-dev-type.enum';
 import { isTopologyLimitedToOneLayout } from 'app/helpers/storage.helper';
 import {
@@ -21,7 +22,15 @@ export class ConfigurationPreviewComponent {
 
   protected name$ = this.store.name$;
   protected encryption$ = this.store.encryption$;
-  protected topology$ = this.store.topology$;
+
+  protected topology$ = this.store.topology$.pipe(
+    map((topology) => {
+      if (this.store.isUsingDraidLayout(topology)) {
+        delete topology.spare;
+      }
+      return topology;
+    }),
+  );
   protected totalCapacity$ = this.store.totalUsableCapacity$;
   protected isLimitedToOneLayout = isTopologyLimitedToOneLayout;
 
