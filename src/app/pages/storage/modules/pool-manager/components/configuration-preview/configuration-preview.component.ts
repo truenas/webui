ead9a5ce3ca724ebdc119e371e8d7e02dd665309
Oleksandr Karpov <Karpov4you@gmail.com>
2023-09-08 14:29:24 +0300

    NAS-123749: PR Update

--- src/app/pages/storage/modules/pool-manager/components/configuration-preview/configuration-preview.component.ts
+++ src/app/pages/storage/modules/pool-manager/components/configuration-preview/configuration-preview.component.ts
@@ -3,6 +3,7 @@ import {
 } from '@angular/core';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
+import { map } from 'rxjs';
 import { vdevTypeLabels } from 'app/enums/v-dev-type.enum';
 import {
   PoolManagerStore,
@@ -20,7 +21,15 @@ export class ConfigurationPreviewComponent {
 
   protected name$ = this.store.name$;
   protected encryption$ = this.store.encryption$;
-  protected topology$ = this.store.topology$;
+
+  protected topology$ = this.store.topology$.pipe(
+    map((topology) => {
+      if (this.store.isUsingDraidLayout(topology)) {
+        delete topology.spare;
+      }
+      return topology;
+    }),
+  );
   protected totalCapacity$ = this.store.totalUsableCapacity$;
 
   constructor(
