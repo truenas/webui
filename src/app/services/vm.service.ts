4b653d4c6b6b64e575dec807e38b53e90bfc065b
Alex Karpov <akarpov@ixsystems.com>
2023-09-13 10:18:48 -0400

    NAS-123885: Remove DisplayVmDialogComponent (#8769)

--- src/app/services/vm.service.ts
+++ src/app/services/vm.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, Inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable, take } from 'rxjs';
 import { VmNicType } from 'app/enums/vm.enum';
-import { VirtualizationDetails } from 'app/interfaces/virtual-machine.interface';
+import { WINDOW } from 'app/helpers/window.helper';
+import { VirtualizationDetails, VmDisplayWebUriParams, VmDisplayWebUriParamsOptions } from 'app/interfaces/virtual-machine.interface';
+import { WebsocketError } from 'app/interfaces/websocket-error.interface';
+import { AppLoaderService } from 'app/modules/loader/app-loader.service';
+import { DialogService } from 'app/services/dialog.service';
+import { ErrorHandlerService } from 'app/services/error-handler.service';
 import { WebSocketService } from 'app/services/ws.service';
 
 @Injectable({ providedIn: 'root' })
 export class VmService {
-  constructor(protected ws: WebSocketService) {}
+  constructor(
+    private ws: WebSocketService,
+    private loader: AppLoaderService,
+    private dialogService: DialogService,
+    private translate: TranslateService,
+    private errorHandler: ErrorHandlerService,
+    @Inject(WINDOW) private window: Window,
+  ) {}
 
   getNicTypes(): string[][] {
     return [
@@ -17,5 +30,37 @@ export class VmService {
 
   getVirtualizationDetails(): Observable<VirtualizationDetails> {
     return this.ws.call('vm.virtualization_details');
+  }
+
+  openDisplayWebUri(vmId: number): void {
+    this.loader.open();
+
+    const displayOptions = {
+      protocol: this.window.location.protocol.replace(':', '').toUpperCase(),
+    } as VmDisplayWebUriParamsOptions;
+
+    const requestParams: VmDisplayWebUriParams = [
+      vmId,
+      this.window.location.host,
+      displayOptions,
+    ];
+
+    this.ws.call('vm.get_display_web_uri', requestParams)
+      .pipe(take(1))
+      .subscribe({
+        next: (webUri) => {
+          this.loader.close();
+          if (webUri.error) {
+            this.dialogService.warn(this.translate.instant('Error'), webUri.error);
+            return;
+          }
+          this.window.open(webUri.uri, '_blank');
+          this.loader.close();
+        },
+        error: (error: WebsocketError) => {
+          this.loader.close();
+          this.dialogService.error(this.errorHandler.parseWsError(error));
+        },
+      });
   }
 }
