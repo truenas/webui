a0548d52eb88d12aa41f362775ca1324e2aa1ecf
Damian Szidiropulosz <damianos.bouzouki@gmail.com>
2023-09-14 10:35:57 -0400

    NAS-123778: Mock utility support for mini models E+ X+ X and R10/R30 (#8843)

--- src/app/core/testing/utils/enclosure-templates/mock-mini-3.0-x.ts
+++ src/app/core/testing/utils/enclosure-templates/mock-mini-3.0-x.ts
@@ -4,8 +4,8 @@ import {
 } from 'app/interfaces/enclosure.interface';
 import { MockEnclosure } from './mock-enclosure-template';
 
-export class MockMini30Xl extends MockEnclosure {
-  readonly totalSlotsFront: number = 9;
+export class MockMini30X extends MockEnclosure {
+  readonly totalSlotsFront: number = 7;
   readonly totalSlotsRear: number = 0;
   readonly totalSlotsInternal: number = 0;
 
@@ -49,8 +49,9 @@ export class MockMini30Xl extends MockEnclosure {
 
   data = {
     id: 'mapped_enclosure_0',
+    bsg: 'bsg/4:0:0:0',
     name: 'Drive Bays',
-    model: 'FREENAS-MINI-3.0-XL+',
+    model: 'FREENAS-MINI-3.0-X',
     controller: true,
     elements: [
       {
@@ -66,7 +67,7 @@ export class MockMini30Xl extends MockEnclosure {
         has_slot_status: false,
       },
     ],
-    number: this.enclosureNumber,
+    number: 0,
     label: 'Drive Bays',
   } as Enclosure;
 
@@ -79,14 +80,14 @@ export class MockMini30Xl extends MockEnclosure {
   processSlotTemplate(template: EnclosureElement): EnclosureElement {
     const updatedTemplate = { ...template };
     const original = { ...template.original };
-    if (template.slot === 1) {
-      original.slot = 6;
+    if (template.slot === 7) {
+      original.slot = 4;
       original.enclosure_id = '3000000000000002';
-    } else if (template.slot === 10) {
-      original.slot = 5;
+    } else if (template.slot > 4) {
+      original.slot = template.slot - 4;
       original.enclosure_id = '3000000000000002';
     } else {
-      original.slot = template.slot - 1;
+      original.slot = template.slot;
       original.enclosure_id = '3000000000000001';
     }
 
