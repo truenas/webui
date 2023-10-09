85a7003132068caab412d5d8a1b3f5117400f5ad
Oleksandr Karpov <Karpov4you@gmail.com>
2023-10-09 12:54:11 +0300

    NAS-123773: syslog option for system dataset is deprecated and should be removed
    (cherry picked from commit eed78493086a016f8b49b13289c3d910bf9f76a5)

--- src/app/pages/system/advanced/syslog/syslog-card/syslog-card.component.ts
+++ src/app/pages/system/advanced/syslog/syslog-card/syslog-card.component.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { map, startWith, switchMap } from 'rxjs/operators';
 import { syslogLevelLabels } from 'app/enums/syslog.enum';
 import { toLoadingState } from 'app/helpers/to-loading-state.helper';
 import { AdvancedSettingsService } from 'app/pages/system/advanced/advanced-settings.service';
 import { SyslogFormComponent } from 'app/pages/system/advanced/syslog/syslog-form/syslog-form.component';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
-import { WebSocketService } from 'app/services/ws.service';
 import { AppState } from 'app/store';
 import { waitForAdvancedConfig } from 'app/store/system-config/system-config.selectors';
 
@@ -22,18 +20,10 @@ export class SyslogCardComponent {
     toLoadingState(),
   );
 
-  syslog$ = this.slideInService.onClose$.pipe(
-    startWith(undefined),
-    switchMap(() => this.ws.call('systemdataset.config')),
-    map((config) => config.syslog),
-    toLoadingState(),
-  );
-
   readonly syslogLevelLabels = syslogLevelLabels;
 
   constructor(
     private store$: Store<AppState>,
-    private ws: WebSocketService,
     private slideInService: IxSlideInService,
     private advancedSettings: AdvancedSettingsService,
   ) {}
