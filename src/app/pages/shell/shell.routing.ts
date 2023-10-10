cdaa4d16dd0a409488da1a74a7f7af7f691ae4fa
Evgeny Stepanovych <undsoft@gmail.com>
2023-10-10 15:01:55 +0200

    Revert "NAS-124392: Remove web shell (#8967)"
    This reverts commit b8a4319eb017e2263f7b85dc180bd18a38db7d61.

---
+++ src/app/pages/shell/shell.routing.ts
@@ -0,0 +1,11 @@
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ShellComponent } from './shell.component';
+
+export const routes: Routes = [{
+  path: '',
+  component: ShellComponent,
+}];
+
+export const routing: ModuleWithProviders = RouterModule.forChild(routes);
