27c0d0b491104e54d0cd9891dcdf9ebdc7b673c7
RehanY147 <rehanyousaf147@gmail.com>
2023-10-14 04:39:40 +0500

    NAS-123925: Update app-common.module.ts

--- src/app/modules/common/app-common.module.ts
+++ src/app/modules/common/app-common.module.ts
@@ -32,7 +32,6 @@ import { ErrorDialogComponent } from 'app/modules/common/dialog/error-dialog/err
 import { FullScreenDialogComponent } from 'app/modules/common/dialog/full-screen-dialog/full-screen-dialog.component';
 import { GeneralDialogComponent } from 'app/modules/common/dialog/general-dialog/general-dialog.component';
 import { InfoDialogComponent } from 'app/modules/common/dialog/info-dialog/info-dialog.component';
-import { JobProgressDialogComponent } from 'app/modules/common/dialog/job-progress/job-progress-dialog.component';
 import { ErrorTemplateComponent } from 'app/modules/common/dialog/multi-error-dialog/error-template/error-template.component';
 import { MultiErrorDialogComponent } from 'app/modules/common/dialog/multi-error-dialog/multi-error-dialog.component';
 import { RedirectDialogComponent } from 'app/modules/common/dialog/redirect-dialog/redirect-dialog.component';
@@ -92,7 +91,6 @@ import { ShowLogsDialogComponent } from './dialog/show-logs-dialog/show-logs-dia
     FormsModule,
   ],
   declarations: [
-    JobProgressDialogComponent,
     AboutDialogComponent,
     ConfirmDialogComponent,
     ConsolePanelDialogComponent,
