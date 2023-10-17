d504629552865cd1c0e3521c6f159bd0541f2f69
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-10-17 04:43:01 -0300

    NAS-124569 / 13.0 / Rename ES102S to ES102G2 (UI) (by AlexKarpov98) (#9051)
    * Empty commit to create PR on github.
    
    You should reset it
    
    * NAS-124569: PR update
    
    ---------
    
    Co-authored-by: Vladimir Vinogradenko <vladimirv@bugclerk.tys1.ixsystems.net>
    Co-authored-by: Oleksandr Karpov <Karpov4you@gmail.com>

--- src/app/core/classes/hardware/es102g2.ts
+++ src/app/core/classes/hardware/es102g2.ts
@@ -9,16 +9,16 @@ interface LayoutColumn {
   iomIndex: number;
 }
 
-export class ES102S extends Chassis {
+export class ES102G2 extends Chassis {
   constructor() {
     super();
     this.model = 'es102';
 
     this.front = new ChassisView();
     this.front.container = new PIXI.Container();
-    this.front.chassisPath = 'assets/images/hardware/es102s/es102s_960w.png';
-    this.front.driveTrayBackgroundPath = 'assets/images/hardware/es102s/es102s_960w_drivetray_bg.png';
-    this.front.driveTrayHandlePath = 'assets/images/hardware/es102s/es102s_960w_drivetray_handle.png';
+    this.front.chassisPath = 'assets/images/hardware/es102g2/es102g2_960w.png';
+    this.front.driveTrayBackgroundPath = 'assets/images/hardware/es102g2/es102g2_960w_drivetray_bg.png';
+    this.front.driveTrayHandlePath = 'assets/images/hardware/es102g2/es102g2_960w_drivetray_handle.png';
 
     this.front.totalDriveTrays = 102;
     this.front.rows = 12;
