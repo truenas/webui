e552570038c742d5ba35d108d915747a0b9c3140
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-10-24 08:37:40 -0300

    NAS-124608 / 23.10.1 / Add a note explaining how to configure session timeout (by AlexKarpov98) (#9089)
    * Empty commit to create PR on github.
    
    You should reset it
    
    * NAS-124608: PR update
    
    ---------
    
    Co-authored-by: Vladimir Vinogradenko <vladimirv@bugclerk.tys1.ixsystems.net>
    Co-authored-by: Oleksandr Karpov <Karpov4you@gmail.com>

--- src/app/pages/system/advanced/advanced-settings.component.ts
+++ src/app/pages/system/advanced/advanced-settings.component.ts
@@ -1,9 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { take, timer } from 'rxjs';
 
+@UntilDestroy()
 @Component({
   templateUrl: './advanced-settings.component.html',
   styleUrls: ['./advanced-settings.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdvancedSettingsComponent {
+export class AdvancedSettingsComponent implements AfterViewInit {
+  constructor(private route: ActivatedRoute) {}
+
+  ngAfterViewInit(): void {
+    this.route.fragment.pipe(untilDestroyed(this)).subscribe((fragment: string) => {
+      const sessionsContainer = document.getElementById('sessions-card');
+
+      if (fragment === 'sessions' && sessionsContainer) {
+        this.handleHashScrollIntoView(sessionsContainer);
+      }
+    });
+  }
+
+  private handleHashScrollIntoView(container: HTMLElement): void {
+    const highlightedClass = 'highlighted-card';
+
+    container?.scrollIntoView({ block: 'center' });
+    container.classList.add(highlightedClass);
+    timer(2000).pipe(take(1), untilDestroyed(this)).subscribe(() => container.classList.remove(highlightedClass));
+  }
 }
