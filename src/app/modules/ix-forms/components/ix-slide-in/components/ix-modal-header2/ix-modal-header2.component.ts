a63dc8518aa4ba308ac4231a9a4aeb324212d7ec
RehanY147 <rehanyousaf147@gmail.com>
2023-10-26 10:05:51 +0500

    NAS-124305: Added back button

--- src/app/modules/ix-forms/components/ix-slide-in/components/ix-modal-header2/ix-modal-header2.component.ts
+++ src/app/modules/ix-forms/components/ix-slide-in/components/ix-modal-header2/ix-modal-header2.component.ts
@@ -1,20 +1,45 @@
-import { Component, Inject, Input } from '@angular/core';
+import { AfterViewInit, Component, Inject, Input } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { TranslateService } from '@ngx-translate/core';
 import { Subject } from 'rxjs';
 import { SLIDE_IN_CLOSER } from 'app/modules/ix-forms/components/ix-slide-in/ix-slide-in.token';
+import { IxChainedSlideInService } from 'app/services/ix-chained-slide-in.service';
 
+@UntilDestroy()
 @Component({
   selector: 'ix-modal-header2',
   templateUrl: './ix-modal-header2.component.html',
   styleUrls: ['./ix-modal-header2.component.scss'],
 })
-export class IxModalHeader2Component {
+export class IxModalHeader2Component implements AfterViewInit {
   @Input() title: string;
   @Input() loading: boolean;
   @Input() disableClose = false;
 
+
+  iconName = 'cancel';
+
+  tooltip = this.translate.instant('Close the form');
+
   constructor(
+    private translate: TranslateService,
+    private chainedSlideIn: IxChainedSlideInService,
     @Inject(SLIDE_IN_CLOSER) protected slideInCloser$: Subject<unknown>,
   ) {}
+
+  ngAfterViewInit(): void {
+    this.chainedSlideIn.components$.pipe(
+      untilDestroyed(this),
+    ).subscribe((components) => {
+      if (components.length > 1) {
+        this.iconName = 'mdi-chevron-right-circle';
+        this.tooltip = this.translate.instant('Go back to the previous form');
+      } else {
+        this.iconName = 'cancel';
+        this.tooltip = this.translate.instant('Close the form');
+      }
+    });
+  }
 
   close(): void {
     this.slideInCloser$.next(null);
