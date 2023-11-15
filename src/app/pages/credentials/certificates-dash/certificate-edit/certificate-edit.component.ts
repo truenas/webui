7e040979b79955e50a7b945e704a3c1ac477aa6c
Oleksandr Karpov <Karpov4you@gmail.com>
2023-11-15 14:48:49 +0200

    NAS-125191: PR update

--- src/app/pages/credentials/certificates-dash/certificate-edit/certificate-edit.component.ts
+++ src/app/pages/credentials/certificates-dash/certificate-edit/certificate-edit.component.ts
@@ -71,7 +71,7 @@ export class CertificateEditComponent implements OnInit {
   }
 
   setRenewDaysForEditIfAvailable(): void {
-    if (this.certificate) {
+    if (this.certificate?.acme) {
       this.form.addControl('renew_days', new FormControl(this.certificate?.renew_days || null));
     }
   }
