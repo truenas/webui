4d97077d1ec97eb9e867d9ce53dafa0f62693615
Oleksandr Karpov <Karpov4you@gmail.com>
2023-11-16 11:31:35 +0200

    NAS-125091: PR update

--- src/app/directives/common/if-user-has-roles/if-user-has-roles.directive.ts
+++ src/app/directives/common/if-user-has-roles/if-user-has-roles.directive.ts
@@ -6,43 +6,80 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Role } from 'app/enums/role.enum';
 import { AuthService } from 'app/services/auth/auth.service';
 
-const roleMapping = {
-  [Role.SharingIscsiExtentWrite]: [Role.SharingIscsiExtentWrite, Role.SharingIscsiExtentRead],
-  [Role.SharingNfsWrite]: [Role.SharingNfsWrite, Role.SharingNfsRead],
-  [Role.FullAdmin]: [Role.FullAdmin, Role.DatasetDelete],
-  [Role.SharingSmbWrite]: [Role.SharingSmbWrite, Role.SharingSmbRead],
-  [Role.SharingRead]: [Role.SharingRead, Role.SharingIscsiExtentRead, Role.SharingNfsRead, Role.SharingSmbRead],
-  [Role.SharingWrite]: [Role.SharingWrite, Role.SharingIscsiExtentWrite, Role.SharingNfsWrite, Role.SharingSmbWrite],
-  [Role.KeychainCredentialWrite]: [Role.KeychainCredentialWrite, Role.KeychainCredentialRead],
-  [Role.ReplicationTaskConfigWrite]: [Role.ReplicationTaskConfigWrite, Role.ReplicationTaskConfigRead],
-  [Role.ReplicationTaskWrite]: [Role.ReplicationTaskWrite, Role.ReplicationTaskRead],
-  [Role.ReplicationTaskWritePull]: [Role.ReplicationTaskWritePull, Role.ReplicationTaskWrite],
-  [Role.SnapshotTaskWrite]: [Role.SnapshotTaskWrite, Role.SnapshotTaskRead],
-  [Role.DatasetWrite]: [Role.DatasetWrite, Role.DatasetRead],
-  [Role.SnapshotWrite]: [Role.SnapshotWrite, Role.SnapshotRead],
+// Key in this Map will be auto included in the values array
+// example: [Role.SomeRole]: [Role.DatasetDelete] => [Role.SomeRole]: [Role.DatasetDelete, Role.SomeRole]
+const specificRoleMapping = {
+  [Role.SharingRead]: [Role.SharingIscsiExtentRead, Role.SharingNfsRead, Role.SharingSmbRead],
+  [Role.SharingWrite]: [Role.SharingIscsiExtentWrite, Role.SharingNfsWrite, Role.SharingSmbWrite],
   [Role.ReplicationManager]: [
-    Role.ReplicationManager,
     Role.KeychainCredentialWrite,
     Role.ReplicationTaskConfigWrite,
     Role.ReplicationTaskWrite,
     Role.SnapshotTaskWrite,
     Role.SnapshotWrite,
   ],
-  [Role.SharingManager]: [Role.SharingManager, Role.DatasetWrite, Role.SharingWrite],
-  [Role.FilesystemAttrsWrite]: [Role.FilesystemAttrsWrite, Role.FilesystemAttrsRead],
-  [Role.FilesystemDataWrite]: [Role.FilesystemDataWrite, Role.FilesystemDataRead],
-  [Role.FilesystemFullControl]: [
-    Role.FilesystemDataWrite, Role.FilesystemDataRead, Role.FilesystemAttrsWrite, Role.FilesystemAttrsRead,
-  ],
-  [Role.SharingIscsiAuthWrite]: [Role.SharingIscsiAuthWrite, Role.SharingIscsiAuthRead],
-  [Role.SharingIscsiGlobalWrite]: [Role.SharingIscsiGlobalWrite, Role.SharingIscsiGlobalRead],
-  [Role.SharingIscsiHostWrite]: [Role.SharingIscsiHostWrite, Role.SharingIscsiHostRead],
-  [Role.SharingIscsiInitiatorWrite]: [Role.SharingIscsiInitiatorWrite, Role.SharingIscsiInitiatorRead],
-  [Role.SharingIscsiPortalWrite]: [Role.SharingIscsiPortalWrite, Role.SharingIscsiPortalRead],
-  [Role.SharingIscsiTargetWrite]: [Role.SharingIscsiTargetWrite, Role.SharingIscsiTargetRead],
-  [Role.SharingIscsiTargetExtentWrite]: [Role.SharingIscsiTargetExtentWrite, Role.SharingIscsiTargetExtentRead],
-  [Role.SharingIscsiWrite]: [Role.SharingIscsiWrite, Role.SharingIscsiRead],
+  [Role.SharingManager]: [Role.DatasetWrite, Role.SharingWrite],
+  [Role.FilesystemFullControl]: [Role.FilesystemDataWrite, Role.FilesystemAttrsWrite],
 } as { [key in Role]: Role[] };
+
+// Function to check if a role is a 'Write' role
+function isWriteRole(role: Role): boolean {
+  return role.toString().endsWith('_WRITE');
+}
+
+// Function to get corresponding 'Read' role for a 'Write' role
+function getReadRoleForWriteRole(writeRole: Role): Role | undefined {
+  const readRoleString = writeRole.toString().replace('_WRITE', '_READ');
+
+  // Use Object.keys to iterate over the Role enum
+  const keys = Object.keys(Role) as (keyof typeof Role)[];
+  for (const key of keys) {
+    if (Role[key] === readRoleString) {
+      return Role[key];
+    }
+  }
+
+  return undefined;
+}
+
+// Recursive function to resolve roles
+function resolveRoles(inputRoles: Role[]): Role[] {
+  const resolvedRoles: Role[] = [];
+  const processedRoles = new Set<Role>();
+
+  function resolve(role: Role): void {
+    if (processedRoles.has(role)) {
+      return;
+    }
+
+    processedRoles.add(role);
+    resolvedRoles.push(role);
+
+    if (isWriteRole(role)) {
+      const readRole = getReadRoleForWriteRole(role);
+      if (readRole && !processedRoles.has(readRole)) {
+        resolve(readRole);
+      }
+    }
+
+    const includedRoles = specificRoleMapping[role];
+    if (includedRoles) {
+      includedRoles.forEach(includedRole => {
+        if (!processedRoles.has(includedRole)) {
+          resolve(includedRole);
+        }
+      });
+    }
+  }
+
+  inputRoles.forEach(role => {
+    if (!processedRoles.has(role)) {
+      resolve(role);
+    }
+  });
+
+  return resolvedRoles;
+}
 
 @UntilDestroy()
 @Directive({ selector: '[ixIfUserHasRoles]' })
@@ -64,7 +101,7 @@ export class IfUserHasRolesDirective {
     private authService: AuthService,
   ) {
     this.authService.user$.pipe(untilDestroyed(this)).subscribe(user => {
-      this.currentUserRoles = user.roles?.flatMap((role) => roleMapping[role] || role);
+      this.currentUserRoles = resolveRoles(user.roles);
       this.cdr.markForCheck();
     });
   }
