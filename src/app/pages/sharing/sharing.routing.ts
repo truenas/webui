f97e2f6148e1a4ae4fc4081b6490a55b1b4e8f94
Denys Butenko <ua13dark@gmail.com>
2023-11-18 21:50:54 +0700

    NAS-124969 / 24.04 / Add Smb Status page (#9168)
    * NAS-124969: Add Smb Status page
    
    * NAS-124969: Add Smb Status page
    
    * NAS-124969: Add Smb Status page

--- src/app/pages/sharing/sharing.routing.ts
+++ src/app/pages/sharing/sharing.routing.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { SharesDashboardComponent } from 'app/pages/sharing/components/shares-dashboard/shares-dashboard.component';
 import { NfsListComponent } from 'app/pages/sharing/nfs/nfs-list/nfs-list.component';
-import { SmbSessionListComponent } from 'app/pages/sharing/smb/smb-session-list/smb-session-list.component';
+import { SmbStatusComponent } from 'app/pages/sharing/smb/smb-status/smb-status.component';
 import { InitiatorFormComponent } from './iscsi/initiator/initiator-form/initiator-form.component';
 import { IscsiComponent } from './iscsi/iscsi.component';
-import { SmbFormComponent } from './smb/smb-form/smb-form.component';
 import { SmbListComponent } from './smb/smb-list/smb-list.component';
 
 export const routes: Routes = [
@@ -35,17 +34,20 @@ export const routes: Routes = [
           path: '',
           component: SmbListComponent,
         }, {
-          path: 'sessions',
-          component: SmbSessionListComponent,
-          data: { title: T('SMB Sessions'), breadcrumb: null },
-        }, {
-          path: 'add',
-          component: SmbFormComponent,
-          data: { title: T('Add'), breadcrumb: null },
-        }, {
-          path: 'edit/:pk',
-          component: SmbFormComponent,
-          data: { title: T('Edit'), breadcrumb: null },
+          path: 'status',
+          data: { title: T('Smb Status'), breadcrumb: T('Smb Status') },
+          children: [
+            {
+              path: '',
+              redirectTo: 'sessions',
+              pathMatch: 'full',
+            },
+            {
+              path: ':activeTab',
+              component: SmbStatusComponent,
+              data: { title: T('Smb Status') },
+            },
+          ],
         }],
       }, {
         path: 'iscsi',
