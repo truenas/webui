cf63c65413f9f1b6ea924dee9ce79da8e569f56d
Oleksandr Karpov <Karpov4you@gmail.com>
2023-11-21 18:12:39 +0200

    NAS-125191: PR update
    (cherry picked from commit 1999e33058c291829f3f4684ad4e5b0113b8fbad)

--- src/app/pages/credentials/certificates-dash/certificate-edit/certificate-edit.component.ts
+++ src/app/pages/credentials/certificates-dash/certificate-edit/certificate-edit.component.ts
@@ -76,11 +76,6 @@ export class CertificateEditComponent implements OnInit {
     this.cdr.markForCheck();
   }
 
-  findRenewDaysValue(data: unknown[]): number | null {
-    const itemWithRenewDays = data.find(item => item.hasOwnProperty('renew_days'));
-    return itemWithRenewDays ? (itemWithRenewDays as { renew_days: number }).renew_days : null;
-  }
-
   setRenewDaysForEditIfAvailable(): void {
     const initialValue = this.findRenewDaysValue(this.certificate?.signedby?.revoked_certs || []);
 
@@ -135,5 +130,10 @@ export class CertificateEditComponent implements OnInit {
           this.errorHandler.handleWsFormError(error, this.form);
         },
       });
+  }
+
+  private findRenewDaysValue(data: unknown[]): number | null {
+    const itemWithRenewDays = data.find(item => item.hasOwnProperty('renew_days'));
+    return itemWithRenewDays ? (itemWithRenewDays as { renew_days: number }).renew_days : null;
   }
 }
