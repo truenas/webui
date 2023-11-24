8b4d6653dca71a851d396d7326dde116c4c4ff05
Evgeny Stepanovych <undsoft@gmail.com>
2023-11-24 09:14:23 +0100

    NAS-125339: Fully enabling airbnb codestyle

--- src/app/pages/apps/components/installed-apps/installed-apps.component.spec.ts
+++ src/app/pages/apps/components/installed-apps/installed-apps.component.spec.ts
@@ -727,7 +727,8 @@ describe('Install app', () => {
     await saveButton.click();
 
     expect(mockEntityJobComponentRef.componentInstance.setCall).toHaveBeenCalledWith(
-      'chart.release.create', [{
+      'chart.release.create',
+      [{
         catalog: 'TRUENAS',
         item: 'webdav',
         release_name: 'appname',
