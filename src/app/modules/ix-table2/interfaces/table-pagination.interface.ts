cb9a8573ad485507df223000f3b0f2eb782a1e71
RehanY147 <rehanyousaf147@gmail.com>
2023-11-30 15:15:23 +0500

    NAS-124964 / 24.04 / Only server side pagination/sorting in ApiDataProvider (#9263)

--- src/app/modules/ix-table2/interfaces/table-pagination.interface.ts
+++ src/app/modules/ix-table2/interfaces/table-pagination.interface.ts
@@ -1,14 +1,4 @@
-import { ApiCallParams } from 'app/modules/ix-table2/classes/api-data-provider/api-data-provider';
-
 export interface TablePagination {
   pageNumber: number;
   pageSize: number;
-}
-
-export interface PaginationStrategy {
-  getParams(pagination: TablePagination): ApiCallParams;
-
-  paginate<T>(rows: T[], pagination: TablePagination): T[];
-
-  handleCurrentPage(loadRowsAndUpdatePage: () => void, updatePage: () => void): void;
 }
