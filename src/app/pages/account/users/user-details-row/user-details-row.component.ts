74ce255018c61acc33ddf699d8931b253f6e840c
Evgeny Stepanovych <undsoft@gmail.com>
2023-12-01 12:38:38 +0100

    NAS-125480: Add links to audit page (#9267)

--- src/app/pages/account/users/user-details-row/user-details-row.component.ts
+++ src/app/pages/account/users/user-details-row/user-details-row.component.ts
@@ -2,6 +2,7 @@ import {
   Component, ChangeDetectionStrategy, Input, EventEmitter, Output,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import { YesNoPipe } from 'app/core/pipes/yes-no.pipe';
@@ -12,6 +13,7 @@ import {
 } from 'app/pages/account/users/user-details-row/delete-user-dialog/delete-user-dialog.component';
 import { UserFormComponent } from 'app/pages/account/users/user-form/user-form.component';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
+import { UrlOptionsService } from 'app/services/url-options.service';
 
 @UntilDestroy()
 @Component({
@@ -28,6 +30,8 @@ export class UserDetailsRowComponent {
     private slideInService: IxSlideInService,
     private matDialog: MatDialog,
     private yesNoPipe: YesNoPipe,
+    private urlOptions: UrlOptionsService,
+    private router: Router,
   ) {}
 
   getDetails(user: User): Option[] {
@@ -88,6 +92,16 @@ export class UserDetailsRowComponent {
 
         this.delete.emit(user.id);
       });
+  }
+
+  viewLogs(user: User): void {
+    const url = this.urlOptions.buildUrl('/system/audit', {
+      searchQuery: {
+        isBasicQuery: false,
+        filters: [['username', '=', user.username]],
+      },
+    });
+    this.router.navigateByUrl(url);
   }
 
   private getSshStatus(user: User): string {
