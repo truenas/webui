c21a2abbdc33255056f0255d376371e63003e7de
Evgeny Stepanovych <undsoft@gmail.com>
2023-12-07 18:05:59 +0100

    NAS-125612: Simplify mocking of user roles

--- src/app/pages/sharing/components/shares-dashboard/shares-dashboard.component.spec.ts
+++ src/app/pages/sharing/components/shares-dashboard/shares-dashboard.component.spec.ts
@@ -1,18 +1,17 @@
-import { Spectator, createComponentFactory, mockProvider } from '@ngneat/spectator/jest';
+import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 import { MockComponents } from 'ng-mocks';
-import { of } from 'rxjs';
+import { MockAuthService } from 'app/core/testing/classes/mock-auth.service';
+import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
 import { mockCall, mockWebsocket } from 'app/core/testing/utils/mock-websocket.utils';
 import { Role } from 'app/enums/role.enum';
-import { LoggedInUser } from 'app/interfaces/ds-cache.interface';
 import { IscsiCardComponent } from 'app/pages/sharing/components/shares-dashboard/iscsi-card/iscsi-card.component';
 import { NfsCardComponent } from 'app/pages/sharing/components/shares-dashboard/nfs-card/nfs-card.component';
 import { SharesDashboardComponent } from 'app/pages/sharing/components/shares-dashboard/shares-dashboard.component';
 import { SmbCardComponent } from 'app/pages/sharing/components/shares-dashboard/smb-card/smb-card.component';
-import { AuthService } from 'app/services/auth/auth.service';
 
-// TODO: Rework how we check for user permissions.
-describe.skip('SharesDashboardComponent', () => {
+describe('SharesDashboardComponent', () => {
   let spectator: Spectator<SharesDashboardComponent>;
+  let auth: MockAuthService;
   const createComponent = createComponentFactory({
     component: SharesDashboardComponent,
     declarations: [
@@ -26,78 +25,34 @@ describe.skip('SharesDashboardComponent', () => {
       mockWebsocket([
         mockCall('cluster.utils.is_clustered', true),
       ]),
+      mockAuth(),
     ],
   });
 
-  describe('user with privileges to see all cards', () => {
-    beforeEach(() => {
-      spectator = createComponent({
-        providers: [
-          mockProvider(AuthService, {
-            user$: of({
-              privilege: {
-                roles: {
-                  $set: [Role.FullAdmin],
-                },
-              },
-            } as LoggedInUser),
-          }),
-        ],
-      });
-    });
-
-    it('renders Shares Dashboard Cards for privileged user', () => {
-      expect(spectator.query(SmbCardComponent)).toExist();
-      expect(spectator.query(NfsCardComponent)).toExist();
-      expect(spectator.query(IscsiCardComponent)).toExist();
-    });
+  beforeEach(() => {
+    spectator = createComponent();
+    auth = spectator.inject(MockAuthService);
   });
 
-  describe('user with no privileges to see all cards', () => {
-    beforeEach(() => {
-      spectator = createComponent({
-        providers: [
-          mockProvider(AuthService, {
-            user$: of({
-              privilege: {
-                roles: {
-                  $set: [Role.Readonly],
-                },
-              },
-            } as LoggedInUser),
-          }),
-        ],
-      });
-    });
-
-    it('renders Shares Dashboard Cards for privileged user', () => {
-      expect(spectator.query(SmbCardComponent)).not.toExist();
-      expect(spectator.query(NfsCardComponent)).not.toExist();
-      expect(spectator.query(IscsiCardComponent)).not.toExist();
-    });
+  it('user with privileges to see all cards', () => {
+    expect(spectator.query(SmbCardComponent)).toExist();
+    expect(spectator.query(NfsCardComponent)).toExist();
+    expect(spectator.query(IscsiCardComponent)).toExist();
   });
 
-  describe('user with only SMB Card privileges to see', () => {
-    beforeEach(() => {
-      spectator = createComponent({
-        providers: [
-          mockProvider(AuthService, {
-            user$: of({
-              privilege: {
-                roles: {
-                  $set: [Role.SharingSmbRead],
-                },
-              },
-            } as LoggedInUser),
-          }),
-        ],
-      });
-    });
+  it('user with no privileges to see all cards', () => {
+    auth.setRoles([Role.AlertListRead]);
 
-    it('renders Shares Dashboard Cards for privileged user', () => {
-      expect(spectator.query(SmbCardComponent)).toExist();
-      expect(spectator.query(NfsCardComponent)).not.toExist();
-      expect(spectator.query(IscsiCardComponent)).not.toExist();
-    });
+    expect(spectator.query(SmbCardComponent)).not.toExist();
+    expect(spectator.query(NfsCardComponent)).not.toExist();
+    expect(spectator.query(IscsiCardComponent)).not.toExist();
+  });
+
+  it('user with only SMB Card privileges to see', () => {
+    auth.setRoles([Role.SharingSmbRead]);
+
+    expect(spectator.query(SmbCardComponent)).toExist();
+    expect(spectator.query(NfsCardComponent)).not.toExist();
+    expect(spectator.query(IscsiCardComponent)).not.toExist();
   });
 });
