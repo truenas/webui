c3be6686f7c1846fefac8ae21b5b29abfdf2978d
RehanY147 <rehanyousaf147@gmail.com>
2023-12-09 12:31:17 +0500

    NAS-125307 / 24.04 / Added `hasRoleGuard` route guard method and role checks for shares (#9298)

---
+++ src/app/pages/unauthorized/unauthorized/unauthorized.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  templateUrl: './unauthorized.component.html',
+  styleUrls: ['./unauthorized.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UnauthorizedComponent { }
