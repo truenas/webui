d7377e056e4c13f87b943f2ff2309bf9b98668a3
Eric Turgeon <4249848+ericbsd@users.noreply.github.com>
2023-12-18 16:43:31 -0400

    TE-1331 / 23.10.2 / Fixed some UI test isues (#9343)
    * try to slow down going to Return_To_Pool_list
    
    * set a driver.refresh before looking for dirservices manager
    
    * try to see if it click dialog_Cancel
    
    * Added longer wait after setting system pool
    
    and added progressbar before looking for system dataset
    
    * change Set ACL for this dataset xpath with *

--- tests/bdd/scale/test_NAS_T1237.py
+++ tests/bdd/scale/test_NAS_T1237.py
@@ -97,6 +97,7 @@ def create_a_second_smb_dataset_rtacltest2_under_rtacltest1(driver, dataset2_nam
     driver.find_element_by_xpath(xpaths.add_Dataset.share_Type_SMB_Option).click()
     assert wait_on_element(driver, 5, xpaths.button.save, 'clickable')
     driver.find_element_by_xpath(xpaths.button.save).click()
+    assert wait_on_element(driver, 30, xpaths.progress.progressbar)
     rsc.Return_To_Pool_list(driver)
     assert wait_on_element_disappear(driver, 30, xpaths.progress.progressbar)
     assert wait_on_element(driver, 7, xpaths.dataset.dataset_Name(dataset2_name))
