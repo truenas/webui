5230e1642aa8adddedbac08b12999ac8a9d8eff9
RehanY147 <rehanyousaf147@gmail.com>
2023-12-19 22:14:48 +0500

    NAS-125354 / 24.04 / Added ixRequiresRoles directive (#9340)

--- src/app/directives/common/common-directives.module.ts
+++ src/app/directives/common/common-directives.module.ts
@@ -1,19 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslateModule } from '@ngx-translate/core';
 import { AutofocusDirective } from 'app/directives/common/autofocus/autofocus.directive';
 import { HasRoleDirective } from 'app/directives/common/has-role/has-role.directive';
 import { IfNightlyDirective } from 'app/directives/common/if-nightly/if-nightly.directive';
+import { RequiresRolesWrapperComponent } from 'app/directives/common/requires-roles/requires-roles-wrapper.component';
+import { RequiresRolesDirective } from 'app/directives/common/requires-roles/requires-roles.directive';
 import { StepActivationDirective } from 'app/directives/common/step-activation.directive';
 import { LetDirective } from './app-let.directive';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatTooltipModule,
+    TranslateModule,
   ],
   declarations: [
     LetDirective,
     IfNightlyDirective,
     HasRoleDirective,
+    RequiresRolesWrapperComponent,
+    RequiresRolesDirective,
     AutofocusDirective,
     StepActivationDirective,
   ],
@@ -21,6 +29,8 @@ import { LetDirective } from './app-let.directive';
     LetDirective,
     IfNightlyDirective,
     HasRoleDirective,
+    RequiresRolesWrapperComponent,
+    RequiresRolesDirective,
     AutofocusDirective,
     StepActivationDirective,
   ],
