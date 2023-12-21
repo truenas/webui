b51532e9660ed6c1fee6dff30f83165568da2b1f
Evgeny Stepanovych <undsoft@gmail.com>
2023-12-21 16:28:37 +0100

    NAS-125744: Remove the UDP config setting for NFS

--- src/app/pages/services/components/service-nfs/service-nfs.component.ts
+++ src/app/pages/services/components/service-nfs/service-nfs.component.ts
@@ -42,7 +42,6 @@ export class ServiceNfsComponent implements OnInit {
     mountd_port: [null as number, portRangeValidator()],
     rpcstatd_port: [null as number, portRangeValidator()],
     rpclockd_port: [null as number, portRangeValidator()],
-    udp: [false],
     userd_manage_gids: [false],
   });
 
@@ -55,7 +54,6 @@ export class ServiceNfsComponent implements OnInit {
     mountd_port: helptext.nfs_srv_mountd_port_tooltip,
     rpcstatd_port: helptext.nfs_srv_rpcstatd_port_tooltip,
     rpclockd_port: helptext.nfs_srv_rpclockd_port_tooltip,
-    udp: helptext.nfs_srv_udp_tooltip,
     userd_manage_gids: helptext.nfs_srv_16_tooltip,
   };
 
