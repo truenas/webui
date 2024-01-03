5038d571b496f6613bbb775730fd3f3ee775a09b
RehanY147 <rehanyousaf147@gmail.com>
2024-01-04 02:58:19 +0500

    NAS-125832: Reverting changes

--- src/app/pages/dashboard/components/widget-network/widget-network.component.ts
+++ src/app/pages/dashboard/components/widget-network/widget-network.component.ts
@@ -12,6 +12,7 @@ import { Subscription, timer } from 'rxjs';
 import {
   filter, map, take, throttleTime,
 } from 'rxjs/operators';
+import { KiB } from 'app/constants/bytes.constant';
 import { EmptyType } from 'app/enums/empty-type.enum';
 import { LinkState, NetworkInterfaceAliasType } from 'app/enums/network-interface.enum';
 import { deepCloneState } from 'app/helpers/state-select.helper';
@@ -363,7 +364,7 @@ export class WidgetNetworkComponent extends WidgetComponent implements OnInit, A
           }
           (updatedResponse.data as number[][]).forEach((row, index) => {
             // remove first column and convert kilobits/s to bytes
-            (updatedResponse.data as number[][])[index] = [...row.slice(1)];
+            (updatedResponse.data as number[][])[index] = row.slice(1).map((value) => value * KiB);
           });
           return updatedResponse;
         }),
