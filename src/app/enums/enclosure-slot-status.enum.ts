af33083d34742a44c24dc9797c7f8674b4aa5716
Damian Szidiropulosz <damianos@knoxbug.org>
2024-01-07 14:58:21 -0500

    NAS-125235: Update Enclosure related enum

--- src/app/enums/enclosure-slot-status.enum.ts
+++ src/app/enums/enclosure-slot-status.enum.ts
@@ -14,7 +14,16 @@ export enum EnclosureSlotDiskStatus {
 // TODO: Find out what all element name possibilities are to complete the enum
 export enum EnclosureElementName {
   ArrayDeviceSlot = 'Array Device Slot',
-  Expander = 'SAS Expander',
+  Enclosure = 'Enclosure',
+  PowerSupply = 'Power Supply',
+  Cooling = 'Cooling',
+  TemperatureSensors = 'Temperature Sensors',
+  EnclosureServicesControllerElectronics = 'Enclosure Services Controller Electronics',
+  SasExpander = 'SAS Expander',
+  SasConnector = 'SAS Connector',
+  VoltageSensor = 'Voltage Sensor',
+  CurrentSensor = 'Current Sensor',
+  DoorLock = 'Door Lock',
 }
 
 export type EnclosureSlotTopologyStatus = TopologyItemStatus | EnclosureSlotDiskStatus;
