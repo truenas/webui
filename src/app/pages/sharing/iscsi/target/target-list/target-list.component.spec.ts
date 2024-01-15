5b492d213800ec71632228c7ab535d8452821677
RehanY147 <rehanyousaf147@gmail.com>
2024-01-15 14:30:54 +0500

    NAS-126751 / 24.04 / Limit users list to smb users only in smb acl form (#9439)

--- src/app/pages/sharing/iscsi/target/target-list/target-list.component.spec.ts
+++ src/app/pages/sharing/iscsi/target/target-list/target-list.component.spec.ts
@@ -6,6 +6,7 @@ import { of, pipe } from 'rxjs';
 import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
 import { mockCall, mockWebsocket } from 'app/core/testing/utils/mock-websocket.utils';
 import { IscsiTarget } from 'app/interfaces/iscsi.interface';
+import { AppCommonModule } from 'app/modules/common/app-common.module';
 import { IxSlideInRef } from 'app/modules/ix-forms/components/ix-slide-in/ix-slide-in-ref';
 import { IxIconHarness } from 'app/modules/ix-icon/ix-icon.harness';
 import { IxTable2Harness } from 'app/modules/ix-table2/components/ix-table2/ix-table2.harness';
@@ -36,6 +37,7 @@ describe('TargetListComponent', () => {
     imports: [
       IxTable2Module,
       AppLoaderModule,
+      AppCommonModule,
     ],
     providers: [
       mockProvider(AppLoaderService),
--- src/app/pages/sharing/smb/smb-status/components/smb-lock-list/smb-lock-list.component.spec.ts
+++ src/app/pages/sharing/smb/smb-status/components/smb-lock-list/smb-lock-list.component.spec.ts
@@ -5,6 +5,7 @@ import { Spectator } from '@ngneat/spectator';
 import { createComponentFactory } from '@ngneat/spectator/jest';
 import { mockWebsocket, mockCall } from 'app/core/testing/utils/mock-websocket.utils';
 import { SmbLockInfo, SmbOpenInfo } from 'app/interfaces/smb-status.interface';
+import { AppCommonModule } from 'app/modules/common/app-common.module';
 import { EntityModule } from 'app/modules/entity/entity.module';
 import { IxTable2Harness } from 'app/modules/ix-table2/components/ix-table2/ix-table2.harness';
 import { IxTable2Module } from 'app/modules/ix-table2/ix-table2.module';
@@ -61,7 +62,12 @@ describe('SmbLockListComponent', () => {
 
   const createComponent = createComponentFactory({
     component: SmbLockListComponent,
-    imports: [AppLoaderModule, EntityModule, IxTable2Module],
+    imports: [
+      AppLoaderModule,
+      EntityModule,
+      IxTable2Module,
+      AppCommonModule,
+    ],
     providers: [
       mockWebsocket([
         mockCall('smb.status', locks),
