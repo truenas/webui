adf575b4129ec62b3101616b4636d0ba82c79771
Oleksandr Karpov <Karpov4you@gmail.com>
2024-01-17 00:53:47 +0200

    NAS-125407: Polish Add Reporting Exporter Screen

---
+++ src/app/helpers/get-dynamic-form-schema-node.ts
@@ -0,0 +1,30 @@
+import { DynamicFormSchemaType } from 'app/enums/dynamic-form-schema-type.enum';
+import { SchemaType } from 'app/enums/schema.enum';
+import { toHumanReadableKey } from 'app/helpers/object-keys-to-human-readable.helper';
+import { DynamicFormSchemaCheckbox, DynamicFormSchemaInput, DynamicFormSchemaNode } from 'app/interfaces/dynamic-form-schema.interface';
+import { Schema } from 'app/interfaces/schema.interface';
+
+export function getDynamicFormSchemaNode(schema: Schema): DynamicFormSchemaNode {
+  const baseSchema = {
+    controlName: schema._name_,
+    type: DynamicFormSchemaType.Input,
+    title: toHumanReadableKey(schema.title),
+    required: schema._required_,
+  } as DynamicFormSchemaInput;
+
+  if (schema.type === SchemaType.Boolean) {
+    return {
+      ...baseSchema,
+      type: DynamicFormSchemaType.Checkbox,
+    } as DynamicFormSchemaCheckbox;
+  }
+
+  if (schema.type === SchemaType.Integer) {
+    return {
+      ...baseSchema,
+      inputType: 'number',
+    };
+  }
+
+  return baseSchema;
+}
