b642d2df173368de39821cde4bb2b8529948e1ef
Damian Szidiropulosz <damianos@knoxbug.org>
2024-01-17 10:12:53 -0500

    NAS-123855: Use dedicated h10 asets. Renamed images
    (cherry picked from commit 68367ec62fccc30e151df9db033b206d6119f0ee)

--- src/app/pages/system/view-enclosure/classes/hardware/h10.ts
+++ src/app/pages/system/view-enclosure/classes/hardware/h10.ts
@@ -9,9 +9,9 @@ export class H10 extends Chassis {
 
     this.front = new ChassisView();
     this.front.container = new PIXI.Container();
-    this.front.chassisPath = 'assets/images/hardware/r20/r20_960w.png';
-    this.front.driveTrayBackgroundPath = 'assets/images/hardware/r20/r20_960w_drivetray_bg.png';
-    this.front.driveTrayHandlePath = 'assets/images/hardware/r20/r20_960w_drivetray_handle.png';
+    this.front.chassisPath = 'assets/images/hardware/h10/h10_960w.png';
+    this.front.driveTrayBackgroundPath = 'assets/images/hardware/h10/h10_960w_drivetray_bg.png';
+    this.front.driveTrayHandlePath = 'assets/images/hardware/h10/h10_960w_drivetray_handle.png';
 
     this.front.slotRange = { start: 1, end: 12 };
     this.front.totalDriveTrays = 12;
