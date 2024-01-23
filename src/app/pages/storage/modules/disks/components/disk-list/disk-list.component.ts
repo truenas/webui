04b19ba805e529e7ffe3e394e1ed1daac8b4acba
Boris Vasilenko <bvasilenko@ixsystems.com>
2024-01-23 18:28:51 +0300

    NAS-116358: Refactor Disks page to ix-tables

--- src/app/pages/storage/modules/disks/components/disk-list/disk-list.component.ts
+++ src/app/pages/storage/modules/disks/components/disk-list/disk-list.component.ts
@@ -1,270 +1,246 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { TooltipPosition } from '@angular/material/tooltip';
 import { Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
-import * as filesize from 'filesize';
-import {
-  forkJoin, lastValueFrom, of, Subject, switchMap,
-} from 'rxjs';
-import {
-  catchError, debounceTime, filter, map,
-} from 'rxjs/operators';
-import { YesNoPipe } from 'app/core/pipes/yes-no.pipe';
+import filesize from 'filesize';
+import { filter, forkJoin, map } from 'rxjs';
 import { SmartTestResultPageType } from 'app/enums/smart-test-results-page-type.enum';
-import { ApiEvent } from 'app/interfaces/api-message.interface';
 import { Choices } from 'app/interfaces/choices.interface';
-import { QueryParams } from 'app/interfaces/query-api.interface';
 import { Disk, UnusedDisk } from 'app/interfaces/storage.interface';
-import { EntityTableComponent } from 'app/modules/entity/entity-table/entity-table.component';
-import { EntityTableAction, EntityTableConfig } from 'app/modules/entity/entity-table/entity-table.interface';
-import {
-  DiskBulkEditComponent,
-} from 'app/pages/storage/modules/disks/components/disk-bulk-edit/disk-bulk-edit.component';
+import { IxSlideInRef } from 'app/modules/ix-forms/components/ix-slide-in/ix-slide-in-ref';
+import { AsyncDataProvider } from 'app/modules/ix-table2/classes/async-data-provider/async-data-provider';
+import { checkboxColumn } from 'app/modules/ix-table2/components/ix-table-body/cells/ix-cell-checkbox/ix-cell-checkbox.component';
+import { textColumn } from 'app/modules/ix-table2/components/ix-table-body/cells/ix-cell-text/ix-cell-text.component';
+import { Column, ColumnComponent } from 'app/modules/ix-table2/interfaces/table-column.interface';
+import { createTable } from 'app/modules/ix-table2/utils';
+import { EmptyService } from 'app/modules/ix-tables/services/empty.service';
+import { DiskBulkEditComponent } from 'app/pages/storage/modules/disks/components/disk-bulk-edit/disk-bulk-edit.component';
 import { DiskFormComponent } from 'app/pages/storage/modules/disks/components/disk-form/disk-form.component';
-import {
-  DiskWipeDialogComponent,
-} from 'app/pages/storage/modules/disks/components/disk-wipe-dialog/disk-wipe-dialog.component';
-import {
-  ManualTestDialogComponent, ManualTestDialogParams,
-} from 'app/pages/storage/modules/disks/components/manual-test-dialog/manual-test-dialog.component';
-import { DialogService } from 'app/services/dialog.service';
-import { DisksUpdateService } from 'app/services/disks-update.service';
-import { ErrorHandlerService } from 'app/services/error-handler.service';
+import { DiskWipeDialogComponent } from 'app/pages/storage/modules/disks/components/disk-wipe-dialog/disk-wipe-dialog.component';
+import { ManualTestDialogComponent, ManualTestDialogParams } from 'app/pages/storage/modules/disks/components/manual-test-dialog/manual-test-dialog.component';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 import { WebSocketService } from 'app/services/ws.service';
 
+interface DiskUi extends Disk {
+  selected: boolean;
+}
+
 @UntilDestroy()
 @Component({
-  template: '<ix-entity-table [title]="title" [conf]="this"></ix-entity-table>',
+  selector: 'ix-disk-list',
+  templateUrl: './disk-list.component.html',
+  styleUrls: ['./disk-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DiskListComponent implements EntityTableConfig<Disk>, OnDestroy {
-  title = this.translate.instant('Disks');
-  queryCall = 'disk.query' as const;
-  queryCallOption: QueryParams<Disk, { extra: { pools: boolean; passwords: boolean } }> = [[], {
-    extra: {
-      pools: true,
-      passwords: true,
-    },
-  }];
-  noAdd = true;
+export class DiskListComponent implements OnInit {
+  dataProvider: AsyncDataProvider<DiskUi>;
+  filterString = '';
 
-  columns = [
-    { name: this.translate.instant('Name'), prop: 'name', always_display: true },
-    { name: this.translate.instant('Serial'), prop: 'serial' },
-    { name: this.translate.instant('Disk Size'), prop: 'readable_size' },
-    { name: this.translate.instant('Pool'), prop: 'pool' },
-    { name: this.translate.instant('Disk Type'), prop: 'type', hidden: true },
-    { name: this.translate.instant('Description'), prop: 'description', hidden: true },
-    { name: this.translate.instant('Model'), prop: 'model', hidden: true },
-    { name: this.translate.instant('Transfer Mode'), prop: 'transfermode', hidden: true },
-    { name: this.translate.instant('Rotation Rate (RPM)'), prop: 'rotationrate', hidden: true },
-    { name: this.translate.instant('HDD Standby'), prop: 'hddstandby', hidden: true },
-    { name: this.translate.instant('Adv. Power Management'), prop: 'advpowermgmt', hidden: true },
-    { name: this.translate.instant('Enable S.M.A.R.T.'), prop: 'togglesmart', hidden: true },
-    { name: this.translate.instant('S.M.A.R.T. extra options'), prop: 'smartoptions', hidden: true },
-  ];
-  config = {
-    paging: true,
-    sorting: { columns: this.columns },
-    multiSelect: true,
-    deleteMsg: {
-      title: 'User',
-      key_props: ['name'],
-    },
-  };
+  columns = createTable<DiskUi>([
+    checkboxColumn({
+      propertyName: 'selected',
+      onRowCheck: (row, checked) => {
+        this.disks.find((disk) => row.name === disk.name).selected = checked;
+        this.dataProvider.setRows([]);
+        this.dataProvider.setRows(this.disks.filter(this.filterDisk));
+      },
+      onColumnCheck: (checked) => {
+        this.disks.forEach((disk) => disk.selected = checked);
+        this.dataProvider.setRows([]);
+        this.dataProvider.setRows(this.disks.filter(this.filterDisk));
+      },
+    }),
+    textColumn({
+      title: this.translate.instant('Name'),
+      propertyName: 'name',
+    }),
+    textColumn({
+      title: this.translate.instant('Serial'),
+      propertyName: 'serial',
+    }),
+    textColumn({
+      title: this.translate.instant('Disk Size'),
+      propertyName: 'size',
+      getValue: (row) => (filesize(row.size, { standard: 'iec' })),
+    }),
+    textColumn({
+      title: this.translate.instant('Pool'),
+      propertyName: 'pool',
+    }),
+    textColumn({
+      title: this.translate.instant('Disk Type'),
+      propertyName: 'type',
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('Description'),
+      propertyName: 'description',
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('Model'),
+      propertyName: 'model',
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('Transfer Mode'),
+      propertyName: 'transfermode',
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('Rotation Rate (RPM)'),
+      propertyName: 'rotationrate',
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('HDD Standby'),
+      propertyName: 'hddstandby',
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('Adv. Power Management'),
+      propertyName: 'advpowermgmt',
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('Enable S.M.A.R.T.'),
+      propertyName: 'togglesmart',
+      getValue: (row) => (row.togglesmart ? this.translate.instant('Yes') : this.translate.instant('No')),
+      hidden: true,
+    }),
+    textColumn({
+      title: this.translate.instant('S.M.A.R.T. extra options'),
+      propertyName: 'smartoptions',
+      hidden: true,
+    }),
+  ], {
+    rowTestId: (row) => `disk-${row.name}`,
+  });
+
+  get hiddenColumns(): Column<DiskUi, ColumnComponent<DiskUi>>[] {
+    return this.columns.filter((column) => column?.hidden);
+  }
+
+  get selectedDisks(): DiskUi[] {
+    return this.disks.filter(this.filterDisk).filter((disk) => disk.selected);
+  }
+
+  private disks: DiskUi[] = [];
+  private unusedDisks: UnusedDisk[] = [];
   private smartDiskChoices: Choices = {};
 
-  multiActions = [{
-    id: 'medit',
-    label: this.translate.instant('Edit Disk(s)'),
-    icon: 'edit',
-    enable: true,
-    ttpos: 'above' as TooltipPosition,
-    onClick: (selected: Disk[]) => {
-      if (selected.length > 1) {
-        const slideInRef = this.slideInService.open(DiskBulkEditComponent);
-        slideInRef.componentInstance.setFormDiskBulk(selected);
-        slideInRef.slideInClosed$.pipe(
-          filter((response) => Boolean(response)),
-          untilDestroyed(this),
-        ).subscribe(() => this.updateData());
-      } else {
-        const slideInRef = this.slideInService.open(DiskFormComponent, { wide: true });
-        slideInRef.componentInstance.setFormDisk(selected[0]);
-        slideInRef.slideInClosed$.pipe(
-          filter((response) => Boolean(response)),
-          untilDestroyed(this),
-        ).subscribe(() => this.updateData());
-      }
-    },
-  }, {
-    id: 'mmanualtest',
-    label: this.translate.instant('Manual Test'),
-    icon: 'play_arrow',
-    enable: true,
-    ttpos: 'above' as TooltipPosition,
-    onClick: (selected: Disk[]) => {
-      this.manualTest(selected);
-    },
-  }];
-
-  private diskUpdateSubscriptionId: string;
-  private entityList: EntityTableComponent<Disk>;
-
-  protected unusedDisks: UnusedDisk[] = [];
   constructor(
-    protected ws: WebSocketService,
-    private errorHandler: ErrorHandlerService,
-    protected router: Router,
+    private ws: WebSocketService,
+    private router: Router,
     private matDialog: MatDialog,
-    protected translate: TranslateService,
+    private translate: TranslateService,
     private slideInService: IxSlideInService,
-    private dialogService: DialogService,
-    private disksUpdate: DisksUpdateService,
-    private yesNoPipe: YesNoPipe,
+    protected emptyService: EmptyService,
+    private cdr: ChangeDetectorRef,
   ) {}
 
-  ngOnDestroy(): void {
-    this.disksUpdate.removeSubscriber(this.diskUpdateSubscriptionId);
-  }
-
-  getActions(parentRow: Disk): EntityTableAction[] {
-    const actions = [{
-      id: parentRow.name,
-      icon: 'edit',
-      name: 'edit',
-      label: this.translate.instant('Edit'),
-      onClick: (disk: Disk) => {
-        const slideInRef = this.slideInService.open(DiskFormComponent, { wide: true });
-        slideInRef.componentInstance.setFormDisk(disk);
-        slideInRef.slideInClosed$.pipe(
-          filter((response) => Boolean(response)),
-          untilDestroyed(this),
-        ).subscribe(() => this.updateData());
-      },
-    }];
-
-    const isSmartSupported = Object.keys(this.smartDiskChoices).includes(parentRow.identifier);
-    if (isSmartSupported) {
-      actions.push({
-        id: parentRow.name,
-        icon: 'format_list_bulleted',
-        name: 'manual_test',
-        label: this.translate.instant('Manual Test'),
-        onClick: (row: Disk) => {
-          this.manualTest(row);
-        },
-      });
-      actions.push({
-        id: parentRow.name,
-        icon: 'format_list_bulleted',
-        name: 'smartresults',
-        label: this.translate.instant('S.M.A.R.T. Test Results'),
-        onClick: (row) => {
-          this.router.navigate(['/storage', 'disks', 'smartresults', SmartTestResultPageType.Disk, row.name]);
-        },
-      });
-    }
-
-    const devMatch = this.unusedDisks.filter((dev) => dev.name === parentRow.name);
-    if (devMatch.length > 0) {
-      actions.push({
-        id: parentRow.name,
-        icon: 'delete_sweep',
-        name: 'wipe',
-        label: this.translate.instant('Wipe'),
-        onClick: (diskToWipe): void => {
-          const unusedDisk: Partial<UnusedDisk> = this.unusedDisks.find(
-            (disk) => disk.devname === diskToWipe.devname,
-          );
-          this.matDialog.open(DiskWipeDialogComponent, {
-            data: {
-              diskName: diskToWipe.name,
-              exportedPool: unusedDisk?.exported_zpool,
-            },
-          });
-        },
-      });
-    }
-
-    return actions as EntityTableAction[];
-  }
-
-  prerequisite(): Promise<boolean> {
-    return lastValueFrom(
-      forkJoin([
-        this.ws.call('disk.get_unused'),
-        this.ws.call('smart.test.disk_choices'),
-      ]).pipe(
-        map(([unusedDisks, disksThatSupportSmart]) => {
-          this.unusedDisks = unusedDisks;
-          this.smartDiskChoices = disksThatSupportSmart;
-          return true;
-        }),
-        catchError((error: unknown) => {
-          this.dialogService.error(this.errorHandler.parseError(error));
-          return of(false);
-        }),
-      ),
+  ngOnInit(): void {
+    const request$ = forkJoin([
+      this.ws.call('disk.get_unused'),
+      this.ws.call('smart.test.disk_choices'),
+      this.ws.call('disk.query', [[], { extra: { pools: true, passwords: true } }]),
+    ]).pipe(
+      map(([unusedDisks, disksThatSupportSmart, disks]) => {
+        this.unusedDisks = unusedDisks;
+        this.smartDiskChoices = disksThatSupportSmart;
+        this.disks = disks.map((disk) => ({
+          ...disk,
+          pool: this.getPoolColumn(disk),
+          selected: false,
+        }));
+        return this.disks.filter(this.filterDisk);
+      }),
     );
+    this.dataProvider = new AsyncDataProvider(request$);
+    this.dataProvider.load();
   }
 
-  afterInit(entityList: EntityTableComponent<Disk>): void {
-    const disksUpdateTrigger$ = new Subject<ApiEvent<Disk>>();
-    disksUpdateTrigger$.pipe(
-      debounceTime(50),
-      switchMap(() => this.ws.call('disk.get_unused')),
-      untilDestroyed(this),
-    ).subscribe((unusedDisks) => {
-      this.unusedDisks = unusedDisks;
-      entityList.getData();
-    });
-    this.diskUpdateSubscriptionId = this.disksUpdate.addSubscriber(disksUpdateTrigger$);
-    this.entityList = entityList;
-
-    this.entityList.dataSource$.pipe(untilDestroyed(this)).subscribe((data) => {
-      this.entityList.dataSource.data = data.map((item) => {
-        return {
-          ...item,
-          togglesmart: this.yesNoPipe.transform(item.togglesmart) as unknown as boolean,
-        };
-      });
-    });
-  }
-
-  resourceTransformIncomingRestData(disks: Disk[]): Disk[] {
-    return disks.map((disk) => ({
-      ...disk,
-      pool: this.getPoolColumn(disk),
-      readable_size: filesize(disk.size, { standard: 'iec' }),
-      togglesmart: this.yesNoPipe.transform(disk.togglesmart) as unknown as boolean,
-    }));
-  }
-
-  getPoolColumn(diskToCheck: Disk): string {
-    const unusedDisk = this.unusedDisks.find((disk) => disk.devname === diskToCheck.devname);
-    if (unusedDisk?.exported_zpool) {
-      return unusedDisk.exported_zpool + ' (' + this.translate.instant('Exported') + ')';
-    }
-    return diskToCheck.pool || this.translate.instant('N/A');
-  }
-
-  manualTest(selected: Disk | Disk[]): void {
-    const selectedDisks = Array.isArray(selected) ? selected : [selected];
-
+  manualTest(disks: DiskUi[]): void {
     this.matDialog.open(ManualTestDialogComponent, {
       data: {
-        selectedDisks,
+        selectedDisks: this.prepareDisks(disks),
         diskIdsWithSmart: Object.keys(this.smartDiskChoices),
       } as ManualTestDialogParams,
     });
   }
 
-  updateData(): void {
-    this.entityList.getData();
-    this.entityList.pageChanged();
+  goToTestResults(disk: Disk): void {
+    this.router.navigate(['/storage', 'disks', 'smartresults', SmartTestResultPageType.Disk, disk.name]);
+  }
+
+  edit(disks: DiskUi[]): void {
+    const preparedDisks = this.prepareDisks(disks);
+    let slideInRef: IxSlideInRef<DiskBulkEditComponent | DiskFormComponent>;
+
+    if (preparedDisks.length > 1) {
+      slideInRef = this.slideInService.open(DiskBulkEditComponent);
+      (slideInRef as IxSlideInRef<DiskBulkEditComponent>).componentInstance.setFormDiskBulk(preparedDisks);
+    } else {
+      slideInRef = this.slideInService.open(DiskFormComponent, { wide: true });
+      (slideInRef as IxSlideInRef<DiskFormComponent>).componentInstance.setFormDisk(preparedDisks[0]);
+    }
+
+    slideInRef.slideInClosed$.pipe(
+      filter((response) => Boolean(response)),
+      untilDestroyed(this),
+    ).subscribe(() => this.dataProvider.load());
+  }
+
+  wipe(disk: Disk): void {
+    const exportedPool = this.unusedDisks.find((dev) => dev.devname === disk.devname)?.exported_zpool;
+    this.matDialog.open(DiskWipeDialogComponent, {
+      data: {
+        diskName: disk.name,
+        exportedPool,
+      },
+    });
+  }
+
+  protected isSmartSupported(disk: Disk): boolean {
+    return Object.keys(this.smartDiskChoices).includes(disk.identifier);
+  }
+
+  protected isUnusedDisk(disk: Disk): boolean {
+    return !!this.unusedDisks.find((unusedDisk) => unusedDisk.name === disk.name);
+  }
+
+  protected filterUpdated(query: string): void {
+    this.filterString = query;
+    this.dataProvider.setRows(this.disks.filter(this.filterDisk));
+  }
+
+  protected columnsChange(columns: typeof this.columns): void {
+    this.columns = [...columns];
+    this.cdr.detectChanges();
+    this.cdr.markForCheck();
+  }
+
+  private filterDisk = (disk: Disk): boolean => {
+    return disk.name.includes(this.filterString)
+      || disk.pool.includes(this.filterString)
+      || disk.serial.includes(this.filterString);
+  };
+
+  private getPoolColumn(diskToCheck: Disk): string {
+    const unusedDisk = this.unusedDisks.find((disk) => disk.devname === diskToCheck.devname);
+    if (unusedDisk?.exported_zpool) {
+      return `${unusedDisk.exported_zpool} (${this.translate.instant('Exported')})`;
+    }
+    return diskToCheck.pool || this.translate.instant('N/A');
+  }
+
+  private prepareDisks(disks: DiskUi[]): Disk[] {
+    return disks.map((disk) => {
+      delete disk.selected;
+      return disk as Disk;
+    });
   }
 }
