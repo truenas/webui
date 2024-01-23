45ff3b0497b970fc7e2617e00f4072fbca240d6b
Oleksandr Karpov <Karpov4you@gmail.com>
2024-01-23 23:04:49 +0200

    NAS-126919: PR update
    (cherry picked from commit 0e2595327c528611dd9898b2b59385262ecbc60e)

--- src/app/helpers/operators/filter-async.operator.ts
+++ src/app/helpers/operators/filter-async.operator.ts
@@ -1,14 +1,17 @@
 import {
-  filter, map, Observable, OperatorFunction, switchMap,
+  filter, first, map, Observable, OperatorFunction, switchMap,
 } from 'rxjs';
 
 /**
  * Similar to filter(), but accepts an observable.
+ * The first() operator will take the first emitted value from asyncPredicate$
+ * and then complete, which means it won't emit multiple times for the same source item.
  */
 export function filterAsync<T>(asyncPredicate$: Observable<boolean>): OperatorFunction<T, T> {
   return (source$: Observable<T>) => source$.pipe(
     switchMap((item) => asyncPredicate$.pipe(
       filter((result) => result),
+      first(),
       map(() => item),
     )),
   );
