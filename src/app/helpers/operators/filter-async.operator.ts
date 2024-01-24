b683b29c97566db8dfd891d53cd4e4d5fdcfb516
Oleksandr Karpov <Karpov4you@gmail.com>
2024-01-24 13:56:41 +0200

    NAS-126919: PR update
    (cherry picked from commit f3a61764014c31d65329cabf852fc7e4f54f7960)

--- src/app/helpers/operators/filter-async.operator.ts
+++ src/app/helpers/operators/filter-async.operator.ts
@@ -4,8 +4,6 @@ import {
 
 /**
  * Similar to filter(), but accepts an observable.
- * The first() operator will take the first emitted value from asyncPredicate$
- * and then complete, which means it won't emit multiple times for the same source item.
  */
 export function filterAsync<T>(asyncPredicate$: Observable<boolean>): OperatorFunction<T, T> {
   return (source$: Observable<T>) => source$.pipe(
