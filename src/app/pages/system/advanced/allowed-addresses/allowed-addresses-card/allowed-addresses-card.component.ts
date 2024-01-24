9d74612c2b24ae8c6fc3f82aae1524db82eaae83
Oleksandr Karpov <Karpov4you@gmail.com>
2024-01-24 11:45:04 +0200

    NAS-125066: Missing explaination for "System Settings / Advanced / Allowed IP Addresses"
    (cherry picked from commit 79228b50347d074405b1df394c1d624c92c7e519)

--- src/app/pages/system/advanced/allowed-addresses/allowed-addresses-card/allowed-addresses-card.component.ts
+++ src/app/pages/system/advanced/allowed-addresses/allowed-addresses-card/allowed-addresses-card.component.ts
@@ -28,7 +28,7 @@ interface AllowedAddressRow {
 @UntilDestroy()
 @Component({
   selector: 'ix-allowed-addresses-card',
-  styleUrls: ['../../common-card.scss'],
+  styleUrls: ['../../common-card.scss', './allowed-addresses-card.component.scss'],
   templateUrl: './allowed-addresses-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
