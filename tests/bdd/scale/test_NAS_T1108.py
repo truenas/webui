d9ec2eddf5ab7c04fc59879c6c70e16fdebf22a3
ericbsd <ericturgeon.bsd@gmail.com>
2024-01-25 16:10:29 -0400

    added proper uncheck code
    (cherry picked from commit a182cdb40aec69a990dd1cfc178c1e200b11af49)

--- tests/bdd/scale/test_NAS_T1108.py
+++ tests/bdd/scale/test_NAS_T1108.py
@@ -80,9 +80,9 @@ def on_the_add_dataset_slide_input_name_my_ad_dataset_and_share_type_smb(driver,
     assert wait_on_element(driver, 5, xpaths.add_Dataset.share_Type_SMB_Option, 'clickable')
     driver.find_element_by_xpath(xpaths.add_Dataset.share_Type_SMB_Option).click()
     time.sleep(2)
-    save_screenshot(driver, 'screenshot/before_uncheck_smb.png')
-    rsc.Click_On_Element(driver, xpaths.add_Dataset.create_Smb_Checkbox)
-    save_screenshot(driver, 'screenshot/after_uncheck_smb.png')
+    save_screenshot(driver, 'screenshot/scale/before_uncheck_smb.png')
+    rsc.unset_checkbox(driver, xpaths.add_Dataset.create_Smb_Checkbox)
+    save_screenshot(driver, 'screenshot/scale/after_uncheck_smb.png')
 
 
 @then(parsers.parse('click Save the "{dataset_name}" data should be created'))
