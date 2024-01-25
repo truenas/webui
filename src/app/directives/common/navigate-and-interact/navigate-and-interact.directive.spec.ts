7052399bfb3d00316974690dcc5de0ada0d481eb
Alex Karpov <akarpov@ixsystems.com>
2024-01-25 16:57:17 +0200

    NAS-126904 / 24.10 / Directive for highlighting parts of UI (#9507)
    * NAS-126904: Directive for highlighting parts of UI
    
    * NAS-126904: PR update
    
    * NAS-126904: PR Update

---
+++ src/app/directives/common/navigate-and-interact/navigate-and-interact.directive.spec.ts
@@ -0,0 +1,49 @@
+import { Router } from '@angular/router';
+import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator/jest';
+import { NavigateAndInteractDirective } from './navigate-and-interact.directive';
+
+describe('NavigateAndInteractDirective', () => {
+  let spectator: SpectatorDirective<NavigateAndInteractDirective>;
+  let mockRouter: Router;
+  const createDirective = createDirectiveFactory({
+    directive: NavigateAndInteractDirective,
+    mocks: [Router],
+  });
+
+  beforeEach(() => {
+    spectator = createDirective('<div ixNavigateAndInteract [navigateRoute]="[\'/some-path\']" navigateHash="testHash"></div>');
+    mockRouter = spectator.inject(Router);
+  });
+
+  it('should create an instance', () => {
+    expect(spectator.directive).toBeTruthy();
+  });
+
+  it('should call router.navigate with correct parameters on click', () => {
+    spectator.dispatchMouseEvent(spectator.element, 'click');
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/some-path'], { fragment: 'testHash' });
+  });
+
+  it('should scroll to and highlight the element with the given ID', () => {
+    const scrollIntoViewMock = jest.fn();
+    HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
+
+    const mockElement = document.createElement('div');
+    mockElement.id = 'testHash';
+    document.body.appendChild(mockElement);
+
+    const clickSpy = jest.spyOn(HTMLElement.prototype, 'click');
+
+    spectator.dispatchMouseEvent(spectator.element, 'click');
+
+    setTimeout(() => {
+      expect(scrollIntoViewMock).toHaveBeenCalled();
+      expect(clickSpy).toHaveBeenCalled();
+
+      // Clean up
+      document.body.removeChild(mockElement);
+      // Restore original scrollIntoView
+      delete HTMLElement.prototype.scrollIntoView;
+    }, 0);
+  });
+});
---
+++ src/app/directives/common/navigate-and-interact/navigate-and-interact.directive.ts
@@ -0,0 +1,36 @@
+import {
+  Directive, HostListener, Input,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { UntilDestroy } from '@ngneat/until-destroy';
+
+@UntilDestroy()
+@Directive({
+  selector: '[ixNavigateAndInteract]',
+})
+export class NavigateAndInteractDirective {
+  @Input() navigateRoute: string[];
+  @Input() navigateHash: string;
+
+  constructor(private router: Router) {}
+
+  @HostListener('click')
+  onClick(): void {
+    this.router.navigate(this.navigateRoute, { fragment: this.navigateHash }).then(() => {
+      const htmlElement = document.getElementById(this.navigateHash);
+      if (htmlElement) {
+        this.handleHashScrollIntoView(htmlElement);
+      }
+    });
+  }
+
+  private handleHashScrollIntoView(htmlElement: HTMLElement): void {
+    const highlightedClass = 'highlighted-element';
+    setTimeout(() => {
+      htmlElement.scrollIntoView({ block: 'center' });
+      htmlElement.classList.add(highlightedClass);
+      htmlElement.click();
+    }, 150);
+    setTimeout(() => htmlElement.classList.remove(highlightedClass), 2150);
+  }
+}
