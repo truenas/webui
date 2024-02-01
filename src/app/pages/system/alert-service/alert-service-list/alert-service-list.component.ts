d367a96388cbfc258ad58f7beb7e92b684e0f262
Oleksandr Karpov <Karpov4you@gmail.com>
2024-02-01 15:20:23 +0200

    Revert "NAS-127002 / 24.04-BETA.1 / Refactor AlertServiceListComponent to ix-tables2 (by AlexKarpov98) (#9578)"
    This reverts commit 4bd84d3f1b140c678a0da57cf0122da95fd83032.

--- src/app/pages/system/alert-service/alert-service-list/alert-service-list.component.ts
+++ src/app/pages/system/alert-service/alert-service-list/alert-service-list.component.ts
@@ -1,20 +1,12 @@
-import {
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
-import {
-  filter, switchMap, tap,
-} from 'rxjs';
-import { alertLevelLabels } from 'app/enums/alert-level.enum';
-import { alertServiceNames } from 'app/enums/alert-service-type.enum';
-import { Role } from 'app/enums/role.enum';
+import { filter } from 'rxjs';
+import { AlertServiceType } from 'app/enums/alert-service-type.enum';
 import { AlertService } from 'app/interfaces/alert-service.interface';
-import { AsyncDataProvider } from 'app/modules/ix-table2/classes/async-data-provider/async-data-provider';
-import { actionsColumn } from 'app/modules/ix-table2/components/ix-table-body/cells/ix-cell-actions/ix-cell-actions.component';
-import { textColumn } from 'app/modules/ix-table2/components/ix-table-body/cells/ix-cell-text/ix-cell-text.component';
-import { createTable } from 'app/modules/ix-table2/utils';
-import { EmptyService } from 'app/modules/ix-tables/services/empty.service';
+import { EntityTableComponent } from 'app/modules/entity/entity-table/entity-table.component';
+import { EntityTableConfig } from 'app/modules/entity/entity-table/entity-table.interface';
 import { AlertServiceComponent } from 'app/pages/system/alert-service/alert-service/alert-service.component';
 import { DialogService } from 'app/services/dialog.service';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
@@ -24,121 +16,94 @@ import { WebSocketService } from 'app/services/ws.service';
 @UntilDestroy()
 @Component({
   selector: 'ix-alert-service-list',
-  templateUrl: './alert-service-list.component.html',
-  styleUrls: ['./alert-service-list.component.scss'],
+  template: '<ix-entity-table [title]="title" [conf]="this"></ix-entity-table>',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlertServiceListComponent implements OnInit {
-  readonly requiresRoles = [Role.AlertListWrite];
+export class AlertServiceListComponent implements EntityTableConfig<AlertService> {
+  title = 'Alert Services';
+  routeAddTooltip = this.translate.instant('Add Alert Service');
+  queryCall = 'alertservice.query' as const;
+  wsDelete = 'alertservice.delete' as const;
+  protected routeSuccess: string[] = ['system', 'alertservice'];
+  routeAdd: string[] = ['system', 'alertservice', 'add'];
+  routeEdit: string[] = ['system', 'alertservice', 'edit'];
+  entityTable: EntityTableComponent<AlertService>;
+  actionsOutOfHeader = true;
 
-  dataProvider: AsyncDataProvider<AlertService>;
-  filterString = '';
-
-  columns = createTable<AlertService>([
-    textColumn({
-      title: this.translate.instant('Service Name'),
-      propertyName: 'name',
-    }),
-    textColumn({
-      title: this.translate.instant('Type'),
-      propertyName: 'type',
-      getValue: (service) => this.translate.instant(
-        alertServiceNames.find((alertService) => alertService.value === service.type).label,
-      ),
-    }),
-    textColumn({
-      title: this.translate.instant('Level'),
-      propertyName: 'level',
-      getValue: (service) => this.translate.instant(alertLevelLabels.get(service.level)),
-    }),
-    textColumn({
-      title: this.translate.instant('Enabled'),
-      propertyName: 'enabled',
-      getValue: (service) => (service.enabled ? this.translate.instant('Yes') : this.translate.instant('No')),
-    }),
-    actionsColumn({
-      cssClass: 'wide-actions',
-      actions: [
-        {
-          iconName: 'edit',
-          tooltip: this.translate.instant('Edit'),
-          onClick: (row) => this.editAlertService(row),
-        },
-        {
-          iconName: 'delete',
-          tooltip: this.translate.instant('Delete'),
-          onClick: (row) => this.confirmDeleteAlertService(row),
-          requiresRoles: this.requiresRoles,
-        },
-      ],
-    }),
-  ], {
-    rowTestId: (row) => `disk-${row.name}`,
-  });
-
-  private alertServices: AlertService[] = [];
-
-  constructor(
-    protected emptyService: EmptyService,
-    private errorHandler: ErrorHandlerService,
-    private translate: TranslateService,
-    private ws: WebSocketService,
-    private slideInService: IxSlideInService,
-    private dialogService: DialogService,
-    private cdr: ChangeDetectorRef,
-  ) { }
-
-  ngOnInit(): void {
-    const alertServices$ = this.ws.call('alertservice.query').pipe(
-      tap((alertServices) => this.alertServices = alertServices),
-      untilDestroyed(this),
-    );
-    this.dataProvider = new AsyncDataProvider<AlertService>(alertServices$);
-    this.getAlertServices();
-  }
-
-  addAlertService(): void {
-    const slideInRef = this.slideInService.open(AlertServiceComponent);
-    slideInRef.slideInClosed$.pipe(filter(Boolean), untilDestroyed(this)).subscribe(() => this.getAlertServices());
-  }
-
-  protected filterUpdated(query: string): void {
-    this.filterString = query;
-    this.dataProvider.setRows(this.alertServices.filter(this.filterAlertService));
-  }
-
-  protected columnsChange(columns: typeof this.columns): void {
-    this.columns = [...columns];
-    this.cdr.detectChanges();
-    this.cdr.markForCheck();
-  }
-
-  private filterAlertService = (alertService: AlertService): boolean => {
-    return alertService.name.toLowerCase().includes(this.filterString.toLowerCase())
-      || alertService.type.toLowerCase().includes(this.filterString.toLowerCase())
-      || alertService.level.toLowerCase().includes(this.filterString.toLowerCase());
+  columns = [
+    { name: 'Service Name', prop: 'name', always_display: true },
+    { name: 'Type', prop: 'type' },
+    { name: 'Level', prop: 'level' },
+    { name: 'Enabled', prop: 'enabled', selectable: true },
+  ];
+  config = {
+    paging: true,
+    sorting: { columns: this.columns },
+    deleteMsg: {
+      title: 'Alert Service',
+      key_props: ['name'],
+    },
   };
 
-  private editAlertService(row: AlertService): void {
-    const slideInRef = this.slideInService.open(AlertServiceComponent, { data: row });
-    slideInRef.slideInClosed$.pipe(filter(Boolean), untilDestroyed(this)).subscribe(() => this.getAlertServices());
+  private providerList = [
+    AlertServiceType.AwsSns,
+    AlertServiceType.Mail,
+    AlertServiceType.InfluxDb,
+    AlertServiceType.Mattermost,
+    AlertServiceType.OpsGenie,
+    AlertServiceType.PagerDuty,
+    AlertServiceType.Slack,
+    AlertServiceType.SnmpTrap,
+    AlertServiceType.Telegram,
+    AlertServiceType.VictorOps,
+  ];
+
+  constructor(
+    protected router: Router,
+    protected aroute: ActivatedRoute,
+    protected ws: WebSocketService,
+    protected dialogService: DialogService,
+    private errorHandler: ErrorHandlerService,
+    private translate: TranslateService,
+    private slideInService: IxSlideInService,
+  ) { }
+
+  isActionVisible(actionId: string, row: AlertService): boolean {
+    if (actionId === 'edit' && !this.providerList.includes(row.type)) {
+      return false;
+    }
+    return true;
   }
 
-  private confirmDeleteAlertService(alertService: AlertService): void {
-    this.dialogService.confirm({
-      title: this.translate.instant('Confirmation'),
-      message: this.translate.instant('Delete Alert Service <b>"{name}"</b>?', {
-        name: alertService.name,
-      }),
-    }).pipe(
-      filter(Boolean),
-      switchMap(() => this.ws.call('alertservice.delete', [alertService.id])),
-      this.errorHandler.catchError(),
-      untilDestroyed(this),
-    ).subscribe(() => this.getAlertServices());
+  afterInit(entityTable: EntityTableComponent<AlertService>): void {
+    this.entityTable = entityTable;
   }
 
-  private getAlertServices(): void {
-    this.dataProvider.load(false);
+  doAdd(): void {
+    const slideInRef = this.slideInService.open(AlertServiceComponent);
+    slideInRef.slideInClosed$.pipe(filter(Boolean), untilDestroyed(this)).subscribe(() => this.entityTable.getData());
+  }
+
+  doEdit(id: number, entityTable: EntityTableComponent<AlertService>): void {
+    const alertService = entityTable.rows.find((row) => row.id === id);
+
+    const slideInRef = this.slideInService.open(AlertServiceComponent, { data: alertService });
+    slideInRef.slideInClosed$.pipe(filter(Boolean), untilDestroyed(this)).subscribe(() => this.entityTable.getData());
+  }
+
+  onCheckboxChange(row: AlertService): void {
+    row.enabled = !row.enabled;
+    this.ws.call('alertservice.update', [row.id, { enabled: row.enabled }])
+      .pipe(untilDestroyed(this)).subscribe({
+        next: (alertService) => {
+          if (!alertService) {
+            row.enabled = !row.enabled;
+          }
+        },
+        error: (error: unknown) => {
+          row.enabled = !row.enabled;
+          this.dialogService.error(this.errorHandler.parseError(error));
+        },
+      });
   }
 }
