ea2e5e370f8b6440cadbbfc2f05f21f43784ebba
Evgeny Stepanovych <undsoft@gmail.com>
2024-02-09 18:01:13 +0100

    NAS-127291 / 24.10 / Disallow warnings in tests (#9657)

--- src/app/modules/loader/directives/with-loading-state/with-loading-state-loader/with-loading-state-loader.component.ts
+++ src/app/modules/loader/directives/with-loading-state/with-loading-state-loader/with-loading-state-loader.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  selector: 'ix-with-loading-state-loader',
   templateUrl: './with-loading-state-loader.component.html',
   styleUrls: ['./with-loading-state-loader.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
