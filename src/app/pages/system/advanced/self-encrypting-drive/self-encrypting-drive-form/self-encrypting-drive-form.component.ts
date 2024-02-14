9a2fb5de100064944fe3ef2a98bfa83d281bac68
RehanY147 <rehanyousaf147@gmail.com>
2024-02-14 20:49:43 +0500

    NAS-126998: moved self-encrypting-drive-form to chained slide-ins

--- src/app/pages/system/advanced/self-encrypting-drive/self-encrypting-drive-form/self-encrypting-drive-form.component.ts
+++ src/app/pages/system/advanced/self-encrypting-drive/self-encrypting-drive-form/self-encrypting-drive-form.component.ts
@@ -1,25 +1,23 @@
 import {
-  Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit,
+  Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, Inject,
 } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
-import { forkJoin, of } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { Role } from 'app/enums/role.enum';
 import { SedUser } from 'app/enums/sed-user.enum';
 import { helptextSystemAdvanced } from 'app/helptext/system/advanced';
-import { IxSlideInRef } from 'app/modules/ix-forms/components/ix-slide-in/ix-slide-in-ref';
-import { IxValidatorsService } from 'app/modules/ix-forms/services/ix-validators.service';
+import { CHAINED_COMPONENT_REF, SLIDE_IN_DATA } from 'app/modules/ix-forms/components/ix-slide-in/ix-slide-in.token';
 import { matchOthersFgValidator } from 'app/modules/ix-forms/validators/password-validation/password-validation';
 import { SnackbarService } from 'app/modules/snackbar/services/snackbar.service';
 import { DialogService } from 'app/services/dialog.service';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
+import { ChainedComponentRef } from 'app/services/ix-chained-slide-in.service';
 import { WebSocketService } from 'app/services/ws.service';
 import { AppState } from 'app/store';
 import { advancedConfigUpdated } from 'app/store/system-config/system-config.actions';
-import { waitForAdvancedConfig } from 'app/store/system-config/system-config.selectors';
 
 @UntilDestroy()
 @Component({
@@ -64,13 +62,13 @@ export class SelfEncryptingDriveFormComponent implements OnInit {
     private fb: FormBuilder,
     private ws: WebSocketService,
     private translate: TranslateService,
-    private validatorsService: IxValidatorsService,
-    private slideInRef: IxSlideInRef<SelfEncryptingDriveFormComponent>,
     private errorHandler: ErrorHandlerService,
     private cdr: ChangeDetectorRef,
     private store$: Store<AppState>,
     private snackbar: SnackbarService,
     private dialogService: DialogService,
+    @Inject(SLIDE_IN_DATA) private sedConfig: { sedUser: SedUser; sedPassword: string },
+    @Inject(CHAINED_COMPONENT_REF) private chainedRef: ChainedComponentRef,
   ) {}
 
   ngOnInit(): void {
@@ -88,7 +86,7 @@ export class SelfEncryptingDriveFormComponent implements OnInit {
         this.isFormLoading = false;
         this.snackbar.success(this.translate.instant('Settings saved'));
         this.cdr.markForCheck();
-        this.slideInRef.close();
+        this.chainedRef.close({ response: true, error: null });
         this.store$.dispatch(advancedConfigUpdated());
       },
       error: (error: unknown) => {
@@ -100,31 +98,11 @@ export class SelfEncryptingDriveFormComponent implements OnInit {
   }
 
   private loadConfig(): void {
-    this.isFormLoading = true;
+    this.form.patchValue({
+      sed_user: this.sedConfig.sedUser,
+      sed_passwd: this.sedConfig.sedPassword,
+    });
+    this.isFormLoading = false;
     this.cdr.markForCheck();
-
-    forkJoin([
-      this.store$.pipe(
-        waitForAdvancedConfig,
-        take(1),
-      ),
-      this.ws.call('system.advanced.sed_global_password'),
-    ])
-      .pipe(untilDestroyed(this))
-      .subscribe({
-        next: ([config, sedPassword]) => {
-          this.form.patchValue({
-            sed_user: config.sed_user,
-            sed_passwd: sedPassword,
-          });
-          this.isFormLoading = false;
-          this.cdr.markForCheck();
-        },
-        error: (error: unknown) => {
-          this.dialogService.error(this.errorHandler.parseError(error));
-          this.isFormLoading = false;
-          this.cdr.markForCheck();
-        },
-      });
   }
 }
