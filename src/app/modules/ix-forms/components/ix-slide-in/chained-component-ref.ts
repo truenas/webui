b45078c536de3ef56a70f8e768668b6101e46c57
RehanY147 <rehanyousaf147@gmail.com>
2024-02-15 02:38:10 +0500

    NAS-126998: Fixed tests for access-card

---
+++ src/app/modules/ix-forms/components/ix-slide-in/chained-component-ref.ts
@@ -0,0 +1,14 @@
+import { Type } from '@angular/core';
+import { ChainedComponentResponse } from 'app/services/ix-chained-slide-in.service';
+
+export class ChainedComponentRef {
+  close: (response: ChainedComponentResponse) => void;
+  /**
+   * This method will destroy the caller slide-in component and replace it with the
+   * provided new component. The new component will also take on the same "on-close"
+   * observable that the caller had. Makes it easy to switch between components that
+   * have the same purpose and return the same response type e.g, form to wizard and
+   * wizard to form.
+   */
+  swap: (component: Type<unknown>, wide: boolean, data?: unknown) => void;
+}
