27a842c50d298005332af0906a64be944ebc0e4b
Evgeny Stepanovych <undsoft@gmail.com>
2024-02-15 13:40:59 +0100

    VARIANTS-2: NAS-XXXXXX: Experimenting with restricted icons
    (cherry picked from commit 172c27f7a89d365ac203581bf5fb7c006cad5849)

--- src/app/directives/common/requires-roles/requires-roles-wrapper.component.ts
+++ src/app/directives/common/requires-roles/requires-roles-wrapper.component.ts
@@ -7,6 +7,7 @@ import {
   template: `
 <span [class]="['role-missing', class]" [matTooltip]="'Missing permissions for this action' | translate" matTooltipPosition="above">
   <ng-container *ngTemplateOutlet="template"></ng-container>
+  <ix-icon name="lock" class="role-missing-icon"></ix-icon>
 </span>
 `,
   styleUrls: ['./requires-roles-wrapper.component.scss'],
