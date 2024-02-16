d68dd198faff6f05327fac4e7040c409d8d1915c
Oleksandr Karpov <Karpov4you@gmail.com>
2024-02-16 10:59:21 +0200

    NAS-127055: PR update

--- src/app/enums/role.enum.ts
+++ src/app/enums/role.enum.ts
@@ -1,6 +1,8 @@
 import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 
 export enum Role {
+  TrueCommandRead = 'TRUECOMMAND_READ',
+  TrueCommandWrite = 'TRUECOMMAND_WRITE',
   AccountRead = 'ACCOUNT_READ',
   AccountWrite = 'ACCOUNT_WRITE',
   AlertListRead = 'ALERT_LIST_READ',
@@ -102,6 +104,8 @@ export enum Role {
 }
 
 export const roleNames = new Map<Role, string>([
+  [Role.TrueCommandRead, T('TrueCommand Read')],
+  [Role.TrueCommandWrite, T('TrueCommand Write')],
   [Role.AlertListRead, T('Alert List Read')],
   [Role.AuthSessionsRead, T('Auth Sessions Read')],
   [Role.AuthSessionsWrite, T('Auth Sessions Write')],
