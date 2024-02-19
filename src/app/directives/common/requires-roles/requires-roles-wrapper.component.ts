6c93ef9ed03c928e24849f27b4ad053b5eb66726
Evgeny Stepanovych <undsoft@gmail.com>
2024-02-19 13:51:03 +0100

    NAS-127417: Replacing strike-through for restricted elements with a lock icon

--- src/app/directives/common/requires-roles/requires-roles-wrapper.component.ts
+++ src/app/directives/common/requires-roles/requires-roles-wrapper.component.ts
@@ -2,12 +2,7 @@ import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'ix-requires-roles-wrapper',
-  template: `
-<span [class]="['role-missing', class]" [matTooltip]="'Missing permissions for this action' | translate" matTooltipPosition="above">
-  <ng-container *ngTemplateOutlet="template"></ng-container>
-  <ix-icon name="lock" class="role-missing-icon"></ix-icon>
-</span>
-`,
+  templateUrl: './requires-roles-wrapper.component.html',
   styleUrls: ['./requires-roles-wrapper.component.scss'],
 })
 export class RequiresRolesWrapperComponent {
