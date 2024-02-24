9d564b4341f9d99a3d1b20648903976f5f80a2d5
RehanY147 <rehanyousaf147@gmail.com>
2024-02-25 03:26:52 +0500

    NAS-125235: Commented out mock files

--- src/app/core/testing/utils/enclosure-templates/mock-r20.ts
+++ src/app/core/testing/utils/enclosure-templates/mock-r20.ts
@@ -1,4 +1,3 @@
-import { EnclosureUi, EnclosureElementsGroup } from 'app/interfaces/enclosure.interface';
 import { MockEnclosure } from './mock-enclosure-template';
 
 export class MockR20 extends MockEnclosure {
@@ -6,28 +5,28 @@ export class MockR20 extends MockEnclosure {
   readonly totalSlotsRear: number = 2;
   readonly totalSlotsInternal: number = 0;
 
-  data = {
-    id: 'mapped_enclosure_0',
-    name: 'Drive Bays',
-    model: 'R20',
-    controller: true,
-    elements: [
-      {
-        name: 'Array Device Slot',
-        descriptor: 'Drive Slots',
-        header: [
-          'Descriptor',
-          'Status',
-          'Value',
-          'Device',
-        ],
-        elements: [],
-        has_slot_status: false,
-      },
-    ],
-    number: 0,
-    label: 'Drive Bays',
-  } as EnclosureUi;
+  // data = {
+  //   id: 'mapped_enclosure_0',
+  //   name: 'Drive Bays',
+  //   model: 'R20',
+  //   controller: true,
+  //   elements: [
+  //     {
+  //       name: 'Array Device Slot',
+  //       descriptor: 'Drive Slots',
+  //       header: [
+  //         'Descriptor',
+  //         'Status',
+  //         'Value',
+  //         'Device',
+  //       ],
+  //       elements: [],
+  //       has_slot_status: false,
+  //     },
+  //   ],
+  //   number: 0,
+  //   label: 'Drive Bays',
+  // } as EnclosureUi;
 
   constructor(number: number) {
     super(number);
@@ -35,8 +34,8 @@ export class MockR20 extends MockEnclosure {
     this.enclosureInit();
   }
 
-  resetSlotsToEmpty(): void {
-    const emptySlots = this.generateEmptySlots(this.totalSlotsFront + this.totalSlotsRear);
-    (this.data.elements[0] as EnclosureElementsGroup).elements = emptySlots;
-  }
+  // resetSlotsToEmpty(): void {
+  //   const emptySlots = this.generateEmptySlots(this.totalSlotsFront + this.totalSlotsRear);
+  //   (this.data.elements[0] as EnclosureElementsGroup).elements = emptySlots;
+  // }
 }
