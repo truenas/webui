819b9e2e1db79f33f622004308501c90b25c0ed0
Alex Karpov <akarpov@ixsystems.com>
2024-03-13 16:41:02 +0200

    NAS-127615 / 24.10 / Do not re-request count during shifts in pagination (do request recount if someone shifts to last offset) (#9787)
    * NAS-127615: Do not re-request count during shifts in pagination (do request recount if someone shifts to last offset)
    
    * NAS-127615: PR update
    
    * NAS-127615: PR update

--- src/app/modules/ix-table2/classes/api-data-provider/audit-api-data-provider.ts
+++ src/app/modules/ix-table2/classes/api-data-provider/audit-api-data-provider.ts
@@ -1,16 +1,33 @@
-import { Observable } from 'rxjs';
+import _ from 'lodash';
+import { Observable, of } from 'rxjs';
 import { ApiCallParams } from 'app/interfaces/api/api-call-directory.interface';
-import { AuditEntry } from 'app/interfaces/audit/audit.interface';
+import { AuditEntry, AuditQueryParams } from 'app/interfaces/audit/audit.interface';
 import { QueryFilters } from 'app/interfaces/query-api.interface';
 import { ApiDataProvider } from 'app/modules/ix-table2/classes/api-data-provider/api-data-provider';
 import { WebSocketService } from 'app/services/ws.service';
 
 export class AuditApiDataProvider extends ApiDataProvider<'audit.query'> {
+  lastParams: AuditQueryParams;
+
+  get isLastOffset(): boolean {
+    return Boolean((this.totalRows / this.pagination.pageNumber) < this.pagination.pageSize);
+  }
+
+  get avoidCountRowsRequest(): boolean {
+    return this.totalRows && !this.isLastOffset && _.isEqual(this.lastParams, this.params[0]);
+  }
+
   constructor(ws: WebSocketService) {
     super(ws, 'audit.query');
   }
 
   protected override countRows(): Observable<number> {
+    if (this.avoidCountRowsRequest) {
+      return of(this.totalRows);
+    }
+
+    this.lastParams = this.params[0];
+
     const params = [
       {
         'query-filters': this.params[0] || [],
