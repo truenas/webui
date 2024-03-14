a09dc4aacef6cdfaa8a7b65319b18535277dc5ae
Denys Butenko <ua13dark@gmail.com>
2024-03-14 14:41:49 +0800

    NAS-127701: Fix SysInfo widget on failover reconnect

--- src/app/pages/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/components/dashboard/dashboard.component.ts
@@ -13,7 +13,6 @@ import { ScreenType } from 'app/enums/screen-type.enum';
 import { WidgetName } from 'app/enums/widget-name.enum';
 import { WINDOW } from 'app/helpers/window.helper';
 import { EmptyConfig } from 'app/interfaces/empty-config.interface';
-import { SystemInfoWithFeatures } from 'app/interfaces/events/sys-info-event.interface';
 import {
   NetworkInterface, NetworkInterfaceAlias,
   NetworkInterfaceState,
@@ -92,12 +91,6 @@ export class DashboardComponent implements AfterViewInit, OnDestroy {
     },
   };
 
-  // For widgetsysinfo
-  isHaLicensed: boolean;
-
-  // For CPU widget
-  systemInformation: SystemInfoWithFeatures;
-
   // For widgetpool
   pools: Pool[];
   volumeData: VolumesData;
@@ -144,8 +137,6 @@ export class DashboardComponent implements AfterViewInit, OnDestroy {
         this.nics = state.nics;
         this.pools = state.pools;
         this.volumeData = state.volumesData;
-        this.systemInformation = state.sysInfoWithFeatures;
-        this.isHaLicensed = state.isHaLicensed;
         this.setDashState(state.dashboardState);
       }),
       untilDestroyed(this),
