04fd0e4ab26905b329f2ae29faa935f707ff425b
Oleksandr Karpov <Karpov4you@gmail.com>
2024-03-14 14:11:20 +0200

    NAS-127440: Refactor dataset-quotas-userlist.component List to ix-table2

--- src/app/pages/datasets/components/dataset-quotas/dataset-quotas-user-list/dataset-quotas-user-list.component.spec.ts
+++ src/app/pages/datasets/components/dataset-quotas/dataset-quotas-user-list/dataset-quotas-user-list.component.spec.ts
@@ -14,25 +14,27 @@ import { IxSlideInRef } from 'app/modules/ix-forms/components/ix-slide-in/ix-sli
 import { SLIDE_IN_DATA } from 'app/modules/ix-forms/components/ix-slide-in/ix-slide-in.token';
 import { FormErrorHandlerService } from 'app/modules/ix-forms/services/form-error-handler.service';
 import { IxFormatterService } from 'app/modules/ix-forms/services/ix-formatter.service';
-import { IxTableModule } from 'app/modules/ix-tables/ix-table.module';
-import { IxTableHarness } from 'app/modules/ix-tables/testing/ix-table.harness';
+import { IxIconHarness } from 'app/modules/ix-icon/ix-icon.harness';
+import { IxTable2Harness } from 'app/modules/ix-table2/components/ix-table2/ix-table2.harness';
+import { IxTable2Module } from 'app/modules/ix-table2/ix-table2.module';
 import { AppLoaderService } from 'app/modules/loader/app-loader.service';
 import { PageHeaderModule } from 'app/modules/page-header/page-header.module';
 import { SearchInput1Component } from 'app/modules/search-input1/search-input1.component';
+import { DatasetQuotaAddFormComponent } from 'app/pages/datasets/components/dataset-quotas/dataset-quota-add-form/dataset-quota-add-form.component';
 import { DatasetQuotaEditFormComponent } from 'app/pages/datasets/components/dataset-quotas/dataset-quota-edit-form/dataset-quota-edit-form.component';
+import { DatasetQuotasUserListComponent } from 'app/pages/datasets/components/dataset-quotas/dataset-quotas-user-list/dataset-quotas-user-list.component';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 import { WebSocketService } from 'app/services/ws.service';
-import { DatasetQuotasUserlistComponent } from './dataset-quotas-userlist.component';
 
 const fakeUserQuotas: DatasetQuota[] = [{
   id: 1,
   name: 'daemon',
-  obj_quota: 0,
-  obj_used: 0,
+  obj_quota: 1,
+  obj_used: 55,
   quota: 512000,
   quota_type: DatasetQuotaType.User,
-  used_bytes: 0,
-  used_percent: 0,
+  used_bytes: 1,
+  used_percent: 25,
 }, {
   id: 2,
   name: 'bin',
@@ -40,19 +42,20 @@ const fakeUserQuotas: DatasetQuota[] = [{
   obj_used: 0,
   quota: 512000,
   quota_type: DatasetQuotaType.User,
-  used_bytes: 0,
+  used_bytes: 5,
   used_percent: 0,
 }];
 
-describe('DatasetQuotasUserlistComponent', () => {
-  let spectator: Spectator<DatasetQuotasUserlistComponent>;
+describe('DatasetQuotasUserListComponent', () => {
+  let spectator: Spectator<DatasetQuotasUserListComponent>;
   let loader: HarnessLoader;
   let ws: WebSocketService;
+  let table: IxTable2Harness;
 
   const createComponent = createComponentFactory({
-    component: DatasetQuotasUserlistComponent,
+    component: DatasetQuotasUserListComponent,
     imports: [
-      IxTableModule,
+      IxTable2Module,
       MockModule(PageHeaderModule),
       SearchInput1Component,
     ],
@@ -85,10 +88,11 @@ describe('DatasetQuotasUserlistComponent', () => {
     ],
   });
 
-  beforeEach(() => {
+  beforeEach(async () => {
     spectator = createComponent();
     loader = TestbedHarnessEnvironment.loader(spectator.fixture);
     ws = spectator.inject(WebSocketService);
+    table = await loader.getHarness(IxTable2Harness);
   });
 
   it('should show table rows', async () => {
@@ -102,20 +106,20 @@ describe('DatasetQuotasUserlistComponent', () => {
       ['Test', DatasetQuotaType.User, [['name', '=', null]]],
     );
 
-    const table = await loader.getHarness(IxTableHarness);
-    const cells = await table.getCells(true);
+    const cells = await table.getCellTexts();
+
     const expectedRows = [
       ['Name', 'ID', 'Data Quota', 'DQ Used', 'DQ % Used', 'Object Quota', 'OQ Used', 'OQ % Used', ''],
-      ['daemon', '1', '500 KiB', '0', '0%', '—', '0', '—', ''],
-      ['bin', '2', '500 KiB', '0', '0%', '—', '0', '—', ''],
+      ['daemon', '1', '500 KiB', '500 KiB', '25%', '1', '55', '55%', ''],
+      ['bin', '2', '500 KiB', '500 KiB', '0%', '—', '—', '—', ''],
     ];
 
     expect(cells).toEqual(expectedRows);
   });
 
   it('should delete user quota when click delete button', async () => {
-    const [firstDeleteButton] = await loader.getAllHarnesses(MatButtonHarness.with({ selector: '[aria-label="Delete"]' }));
-    await firstDeleteButton.click();
+    const deleteIcon = await table.getHarnessInCell(IxIconHarness.with({ name: 'delete' }), 1, 8);
+    await deleteIcon.click();
 
     expect(spectator.inject(DialogService).confirm).toHaveBeenCalledWith(
       expect.objectContaining({
@@ -137,13 +141,22 @@ describe('DatasetQuotasUserlistComponent', () => {
   });
 
   it('should open slide to edit user quota when click a row', async () => {
-    const element = await spectator.fixture.nativeElement as HTMLElement;
-    const [, secondRow] = element.querySelectorAll('.mat-mdc-row');
-    (secondRow as HTMLElement).click();
+    const editIcon = await table.getHarnessInCell(IxIconHarness.with({ name: 'edit' }), 2, 8);
+    await editIcon.click();
 
     expect(spectator.inject(IxSlideInService).open).toHaveBeenCalledWith(
       DatasetQuotaEditFormComponent,
       { data: { datasetId: 'Test', id: 2, quotaType: 'USER' } },
+    );
+  });
+
+  it('opens form when "Add" button is pressed', async () => {
+    const addButton = await loader.getHarness(MatButtonHarness.with({ text: 'Add' }));
+    await addButton.click();
+
+    expect(spectator.inject(IxSlideInService).open).toHaveBeenCalledWith(
+      DatasetQuotaAddFormComponent,
+      { data: { datasetId: 'Test', quotaType: 'USER' } },
     );
   });
 });
