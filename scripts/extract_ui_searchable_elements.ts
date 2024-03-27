9fb74e98644d7e8fda180456f7a6de67fb1eeec1
Oleksandr Karpov <Karpov4you@gmail.com>
2024-03-27 10:12:45 +0200

    NAS-127965: PR update

--- scripts/extract_ui_searchable_elements.ts
+++ scripts/extract_ui_searchable_elements.ts
@@ -56,186 +56,187 @@
   In this case I set custom anchor `#configure-access`, so in the access-form.elements.ts -> we need to provide `triggerAnchor: 'configure-access',`
  */
 
-import * as cheerio from 'cheerio';
-import * as ts from 'typescript';
-import * as fs from 'fs';
-import { Role } from '../src/app/enums/role.enum';
-import { generateIdFromHierarchy } from '../src/app/modules/global-search/helpers/generate-id-from-hierarchy';
-import { GlobalSearchSection } from '../src/app/modules/global-search/enums/global-search-section';
-import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
-const glob = require('glob');
-const path = require('path');
+  import * as cheerio from 'cheerio';
+  import * as ts from 'typescript';
+  import * as fs from 'fs';
+  import { Role } from '../src/app/enums/role.enum';
+  import { generateIdFromHierarchy } from '../src/app/modules/global-search/helpers/generate-id-from-hierarchy';
+  import { GlobalSearchSection } from '../src/app/modules/global-search/enums/global-search-section';
+  import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
+  const glob = require('glob');
+  const path = require('path');
 
-(global as any).T = (input: string) => input;
-(global as any).Role = Role;
+  (global as any).T = (input: string) => input;
+  (global as any).Role = Role;
 
-interface ComponentProperties {
-  [propertyName: string]: string;
-}
+  interface ComponentProperties {
+    [propertyName: string]: string;
+  }
 
-enum TsExtraction {
-  ElementsConfig = 'elements',
-  ClassProperties = 'properties'
-}
+  enum TsExtraction {
+    ElementsConfig = 'elements',
+    ClassProperties = 'properties'
+  }
 
-let uiElements: UiSearchableElement[] = [];
+  let uiElements: UiSearchableElement[] = [];
 
-function parseRouterLink(inputText: string): string[] {
-  let trimmed = inputText?.trim()?.slice(1, -1);
-  return trimmed?.split(',')
-    ?.map((keyword) => keyword?.trim()?.replace(/^'(.*)'$/, '$1'))
-    ?.filter(Boolean);
-}
+  function parseRouterLink(inputText: string): string[] {
+    let trimmed = inputText?.trim()?.slice(1, -1);
+    return trimmed?.split(',')
+      ?.map((keyword) => keyword?.trim()?.replace(/^'(.*)'$/, '$1'))
+      ?.filter(Boolean);
+  }
 
-function extractTsFileContent(filePath: string, extractionType: TsExtraction): string | {} {
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const sourceFile = ts.createSourceFile(filePath, fileContents, ts.ScriptTarget.ES2015, true);
+  function extractTsFileContent(filePath: string, extractionType: TsExtraction): string | {} {
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const sourceFile = ts.createSourceFile(filePath, fileContents, ts.ScriptTarget.ES2015, true);
 
-  let extractedElements: string;
-  let properties: ComponentProperties = {};
+    let extractedElements: string;
+    let properties: ComponentProperties = {};
 
-  function visit(node: ts.Node) {
-    if (extractionType === TsExtraction.ElementsConfig && ts.isVariableStatement(node)) {
-      node.declarationList.declarations.forEach((declaration) => {
-        if (ts.isVariableDeclaration(declaration) && declaration.name.getText(sourceFile)) {
-          const initializer = declaration.initializer;
-          if (initializer) {
-            extractedElements = initializer.getText(sourceFile);
+    function visit(node: ts.Node) {
+      if (extractionType === TsExtraction.ElementsConfig && ts.isVariableStatement(node)) {
+        node.declarationList.declarations.forEach((declaration) => {
+          if (ts.isVariableDeclaration(declaration) && declaration.name.getText(sourceFile)) {
+            const initializer = declaration.initializer;
+            if (initializer) {
+              extractedElements = initializer.getText(sourceFile);
+            }
           }
-        }
-      });
-    }
-    if (extractionType === TsExtraction.ClassProperties && ts.isClassDeclaration(node)) {
-      node.members.forEach(member => {
-        if (ts.isPropertyDeclaration(member) && member.initializer) {
-          const propertyName = member.name.getText(sourceFile);
-          properties[propertyName] = member.initializer.getText(sourceFile);
-        }
-      });
-    }
-    ts.forEachChild(node, visit);
-  }
-
-  ts.forEachChild(sourceFile, visit);
-
-  return extractionType === TsExtraction.ClassProperties ? properties : extractedElements;
-}
-
-function findComponentFiles(pattern: string): Promise<string[]> {
-  return new Promise((resolve, reject) => {
-    glob(pattern, (error: Error, files: string[] | PromiseLike<string[]>) => {
-      error ? reject(error) : resolve(files)
-    });
-  });
-}
-
-function parseRoles(rolesValue: string): Role[] {
-  const roleNames = rolesValue.replace(/^\[|\]$/g, '')
-    ?.split(', ')
-    ?.map(role => role.trim());
-
-  return roleNames.map(roleName => Role[roleName.split('.')[1] as keyof typeof Role]).filter(Boolean);
-}
-
-// convert string config to an actual object
-function convertDataStringToDataObject(dataString: string): any {
-  return eval(`(${dataString})`);
-}
-
-function parseHtmlFile(
-  filePath: string,
-  elementConfig: string,
-  componentProperties: ComponentProperties
-): UiSearchableElement[] {
-  const htmlContent = fs.readFileSync(filePath, 'utf8');
-  const $ = cheerio.load(htmlContent);
-  const elements: UiSearchableElement[] = [];
-
-  $('[ixUiSearchableElement]').each((_, element) => {
-    const configKeysSplitted = $(element).attr('[ixsearchconfig]').split('.');
-    const childKey = configKeysSplitted[configKeysSplitted.length - 1] as string;
-    const parentKey = configKeysSplitted[configKeysSplitted.length - 3];
-    const configObject = convertDataStringToDataObject(elementConfig);
-
-    let mergedElement;
-
-    if (configKeysSplitted?.[configKeysSplitted.length - 2] === TsExtraction.ElementsConfig) {
-      mergedElement = mergeElementsData($, element, configObject, parentKey, childKey, componentProperties);
-    } else {
-      mergedElement = mergeElementsData($, element, configObject, childKey, null, componentProperties);
-    }
-
-    if (mergedElement) {
-      elements.push(mergedElement);
-    }
-  });
-
-  return elements;
-}
-
-function mergeElementsData(
-  $: cheerio.Root,
-  element: cheerio.Element,
-  elementConfig: any,
-  parentKey: string,
-  childKey: string,
-  componentProperties: ComponentProperties
-): UiSearchableElement {
-  try {
-    const parent = elementConfig?.[parentKey] || elementConfig;
-    const child = parent?.elements?.[childKey] || {};
-
-    const hierarchy = [...(parent?.hierarchy || []), ...(child?.hierarchy || [])];
-    const synonyms = [...(parent?.synonyms || []), ...(child?.synonyms || [])];
-    const anchorRouterLink = parent?.anchorRouterLink || child?.anchorRouterLink;
-    const triggerAnchor = parent?.triggerAnchor || child?.triggerAnchor || null;
-    const routerLink = parseRouterLink($(element).attr('[routerlink]')) ?? null;
-    let requiredRoles = parent.requiredRoles || child.requiredRoles || [];
-
-    const rolesAttrName = $(element).attr('*ixrequiresroles') || '';
-
-    if (rolesAttrName) {
-      requiredRoles = parseRoles(rolesAttrName);
-
-      if (componentProperties[rolesAttrName]) {
-        requiredRoles = parseRoles(componentProperties[rolesAttrName])
+        });
       }
+
+      if (extractionType === TsExtraction.ClassProperties && ts.isClassDeclaration(node)) {
+        node.members.forEach(member => {
+          if (ts.isPropertyDeclaration(member) && member.initializer) {
+            const propertyName = member.name.getText(sourceFile);
+            properties[propertyName] = member.initializer.getText(sourceFile);
+          }
+        });
+      }
+
+      ts.forEachChild(node, visit);
     }
 
-    return {
-      hierarchy,
-      synonyms,
-      requiredRoles,
-      anchorRouterLink,
-      routerLink,
-      anchor: child.anchor || parent.anchor || generateIdFromHierarchy(child.hierarchy || parent.hierarchy || []),
-      triggerAnchor,
-      section: GlobalSearchSection.Ui,
-    };
-  } catch (err) {
-    console.log(`Error extracting ${childKey}/${parentKey}`);
+    ts.forEachChild(sourceFile, visit);
+
+    return extractionType === TsExtraction.ClassProperties ? properties : extractedElements;
   }
-}
 
-async function extractUiSearchableElements(): Promise<void> {
-  try {
-    const tsFiles = await findComponentFiles("src/**/*.elements.ts");
-    tsFiles.forEach((elementsTsFilePath) => {
-      const htmlComponentFilePath = elementsTsFilePath.replace('.elements.ts', '.component.html');
-      const tsComponentFilePath = elementsTsFilePath.replace('.elements.ts', '.component.ts');
+  function findComponentFiles(pattern: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      glob(pattern, (error: Error, files: string[] | PromiseLike<string[]>) => {
+        error ? reject(error) : resolve(files)
+      });
+    });
+  }
 
-      if (fs.existsSync(htmlComponentFilePath)) {
-        const elementConfig = extractTsFileContent(elementsTsFilePath, TsExtraction.ElementsConfig) as string;
-        const componentProperties = extractTsFileContent(tsComponentFilePath, TsExtraction.ClassProperties) as {};
-        const elements = parseHtmlFile(htmlComponentFilePath, elementConfig, componentProperties);
-        uiElements = uiElements.concat(elements);
+  function parseRoles(rolesValue: string): Role[] {
+    const roleNames = rolesValue.replace(/^\[|\]$/g, '')
+      ?.split(', ')
+      ?.map(role => role.trim());
+
+    return roleNames.map(roleName => Role[roleName.split('.')[1] as keyof typeof Role]).filter(Boolean);
+  }
+
+  function convertDataStringToDataObject(dataString: string): any {
+    return eval(`(${dataString})`);
+  }
+
+  function parseHtmlFile(
+    filePath: string,
+    elementConfig: string,
+    componentProperties: ComponentProperties
+  ): UiSearchableElement[] {
+    const htmlContent = fs.readFileSync(filePath, 'utf8');
+    const $ = cheerio.load(htmlContent);
+    const elements: UiSearchableElement[] = [];
+
+    $('[ixUiSearchableElement]').each((_, element) => {
+      const configKeysSplitted = $(element).attr('[ixsearchconfig]').split('.');
+      const childKey = configKeysSplitted[configKeysSplitted.length - 1] as string;
+      const parentKey = configKeysSplitted[configKeysSplitted.length - 3];
+      const configObject = convertDataStringToDataObject(elementConfig);
+
+      let mergedElement;
+
+      if (configKeysSplitted?.[configKeysSplitted.length - 2] === TsExtraction.ElementsConfig) {
+        mergedElement = mergeElementsData($, element, configObject, parentKey, childKey, componentProperties);
+      } else {
+        mergedElement = mergeElementsData($, element, configObject, childKey, null, componentProperties);
+      }
+
+      if (mergedElement) {
+        elements.push(mergedElement);
       }
     });
 
-    fs.writeFileSync('src/assets/ui-searchable-elements.json', JSON.stringify(uiElements, null, 2));
-  } catch (err) {
-    console.error("An error occurred:", err);
+    return elements;
   }
-}
 
-extractUiSearchableElements();
+  function mergeElementsData(
+    $: cheerio.Root,
+    element: cheerio.Element,
+    elementConfig: any,
+    parentKey: string,
+    childKey: string,
+    componentProperties: ComponentProperties
+  ): UiSearchableElement {
+    try {
+      const parent = elementConfig?.[parentKey] || elementConfig;
+      const child = parent?.elements?.[childKey] || {};
+
+      const hierarchy = [...(parent?.hierarchy || []), ...(child?.hierarchy || [])];
+      const synonyms = [...(parent?.synonyms || []), ...(child?.synonyms || [])];
+      const anchorRouterLink = parent?.anchorRouterLink || child?.anchorRouterLink;
+      const triggerAnchor = parent?.triggerAnchor || child?.triggerAnchor || null;
+      const routerLink = parseRouterLink($(element).attr('[routerlink]')) ?? null;
+      let requiredRoles = parent.requiredRoles || child.requiredRoles || [];
+
+      const rolesAttrName = $(element).attr('*ixrequiresroles') || '';
+
+      if (rolesAttrName) {
+        requiredRoles = parseRoles(rolesAttrName);
+
+        if (componentProperties[rolesAttrName]) {
+          requiredRoles = parseRoles(componentProperties[rolesAttrName]);
+        }
+      }
+
+      return {
+        hierarchy,
+        synonyms,
+        requiredRoles,
+        anchorRouterLink,
+        routerLink,
+        anchor: child.anchor || parent.anchor || generateIdFromHierarchy(child.hierarchy || parent.hierarchy || []),
+        triggerAnchor,
+        section: GlobalSearchSection.Ui,
+      };
+    } catch (err) {
+      console.log(`Error extracting ${childKey}/${parentKey}`);
+    }
+  }
+
+  async function extractUiSearchableElements(): Promise<void> {
+    try {
+      const tsFiles = await findComponentFiles('src/**/*.elements.ts');
+      tsFiles.forEach((elementsTsFilePath) => {
+        const htmlComponentFilePath = elementsTsFilePath.replace('.elements.ts', '.component.html');
+        const tsComponentFilePath = elementsTsFilePath.replace('.elements.ts', '.component.ts');
+
+        if (fs.existsSync(htmlComponentFilePath)) {
+          const elementConfig = extractTsFileContent(elementsTsFilePath, TsExtraction.ElementsConfig) as string;
+          const componentProperties = extractTsFileContent(tsComponentFilePath, TsExtraction.ClassProperties) as {};
+          const elements = parseHtmlFile(htmlComponentFilePath, elementConfig, componentProperties);
+          uiElements = uiElements.concat(elements);
+        }
+      });
+
+      fs.writeFileSync('src/assets/ui-searchable-elements.json', JSON.stringify(uiElements, null, 2));
+    } catch (err) {
+      console.error('An error occurred:', err);
+    }
+  }
+
+  extractUiSearchableElements();
