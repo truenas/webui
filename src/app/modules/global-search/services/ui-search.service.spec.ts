471f911736ca063fab59196bc0812d291f9875c7
Oleksandr Karpov <Karpov4you@gmail.com>
2024-03-29 18:05:38 +0200

    NAS-128070: Show recently clicked searches when opening global search

--- src/app/modules/global-search/services/ui-search.service.spec.ts
+++ src/app/modules/global-search/services/ui-search.service.spec.ts
@@ -1,14 +1,12 @@
 import { createServiceFactory, SpectatorService, mockProvider } from '@ngneat/spectator/jest';
 import { of, lastValueFrom } from 'rxjs';
-import { Role } from 'app/enums/role.enum';
-import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
 import { UiSearchProvider } from 'app/modules/global-search/services/ui-search.service';
 import { AuthService } from 'app/services/auth/auth.service';
 
-const mockedUiElements = [
-  { hierarchy: ['Technology', 'Internet'], synonyms: ['Web'], requiredRoles: [Role.FullAdmin] },
+jest.mock('app/../assets/ui-searchable-elements.json', () => [
+  { hierarchy: ['Technology', 'Internet'], synonyms: ['Web'], requiredRoles: ['FullAdmin'] },
   { hierarchy: ['Technology', 'Programming'], synonyms: ['Coding'], requiredRoles: [] },
-] as UiSearchableElement[];
+]);
 
 describe('UiSearchProvider with mocked uiElements', () => {
   let spectator: SpectatorService<UiSearchProvider>;
@@ -24,12 +22,11 @@ describe('UiSearchProvider with mocked uiElements', () => {
           mockProvider(AuthService, { hasRole: () => of(true) }),
         ],
       });
-      spectator.service.uiElements = mockedUiElements;
     });
 
     it('should return expected search results on specific search', async () => {
       const searchTerm = 'Internet';
-      const results = await lastValueFrom(spectator.service.search(searchTerm));
+      const results = await lastValueFrom(spectator.service.search(searchTerm, 10));
 
       expect(results).toHaveLength(1);
       expect(results[0].hierarchy).toContain('Internet');
@@ -37,7 +34,7 @@ describe('UiSearchProvider with mocked uiElements', () => {
 
     it('should return expected search results without search', async () => {
       const searchTerm = '';
-      const results = await lastValueFrom(spectator.service.search(searchTerm));
+      const results = await lastValueFrom(spectator.service.search(searchTerm, 10));
 
       expect(results).toHaveLength(2);
       expect(results[0].hierarchy).toContain('Internet');
@@ -52,19 +49,18 @@ describe('UiSearchProvider with mocked uiElements', () => {
           mockProvider(AuthService, { hasRole: () => of(false) }),
         ],
       });
-      spectator.service.uiElements = mockedUiElements;
     });
 
     it('should not show search result where user has no required role', async () => {
       const searchTerm = 'Internet';
-      const results = await lastValueFrom(spectator.service.search(searchTerm));
+      const results = await lastValueFrom(spectator.service.search(searchTerm, 10));
 
       expect(results).toHaveLength(0);
     });
 
     it('should show search result where no role check required', async () => {
       const searchTerm = 'Programming';
-      const results = await lastValueFrom(spectator.service.search(searchTerm));
+      const results = await lastValueFrom(spectator.service.search(searchTerm, 10));
 
       expect(results).toHaveLength(1);
       expect(results[0].hierarchy).toContain('Programming');
@@ -72,7 +68,7 @@ describe('UiSearchProvider with mocked uiElements', () => {
 
     it('should return expected search results based on appropriate roles', async () => {
       const searchTerm = '';
-      const results = await lastValueFrom(spectator.service.search(searchTerm));
+      const results = await lastValueFrom(spectator.service.search(searchTerm, 10));
 
       expect(results).toHaveLength(1);
       expect(results[0].hierarchy).toContain('Programming');
@@ -86,19 +82,18 @@ describe('UiSearchProvider with mocked uiElements', () => {
           mockProvider(AuthService, { hasRole: () => of(true) }),
         ],
       });
-      spectator.service.uiElements = mockedUiElements;
     });
 
     it('should limit results to first 50 items & show results for empty string request', async () => {
       const searchTerm = '';
-      const results = await lastValueFrom(spectator.service.search(searchTerm));
+      const results = await lastValueFrom(spectator.service.search(searchTerm, 10));
 
       expect(results.length).toBeLessThanOrEqual(50);
     });
 
     it('should return empty array if no matches found', async () => {
       const searchTerm = 'Some Non Matching String';
-      const results = await lastValueFrom(spectator.service.search(searchTerm));
+      const results = await lastValueFrom(spectator.service.search(searchTerm, 10));
 
       expect(results).toHaveLength(0);
     });
