6e1f26a96f548edeb2b70e812681746e56bc044a
Evgeny Stepanovych <undsoft@gmail.com>
2024-04-02 18:43:11 +0200

    NAS-128005: Main dashboard architecture

--- src/app/interfaces/api/api-event-directory.interface.ts
+++ src/app/interfaces/api/api-event-directory.interface.ts
@@ -34,7 +34,6 @@ export interface ApiEventDirectory {
   'user.query': { response: User };
   'container.image.pull': { response: Job<PullContainerImageResponse, PullContainerImageParams> };
   'disk.query': { response: Disk };
-  'interface.query': { response: unknown };
   'pool.query': { response: Pool };
   'group.query': { response: Group };
   'container.image.query': { response: ContainerImage };
