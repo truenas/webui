70c50bce2532f9fe9cdda13fa19a1f33cc0c2c18
Oleksandr Karpov <Karpov4you@gmail.com>
2024-04-02 13:35:41 +0300

    NAS-127731: PR update

--- src/app/helptext/vm/vm-wizard/vm-wizard.ts
+++ src/app/helptext/vm/vm-wizard/vm-wizard.ts
@@ -16,9 +16,8 @@ export const helptextVmWizard = {
   autostart_tooltip: T('Set to start this VM when the system boots.'),
   enable_display_tooltip: T('Enable a Display (Virtual Network Computing) remote\
  connection. Requires <i>UEFI</i> booting.'),
-  display_bind_tooltip: T('Display network interface IP address. The primary\
- interface IP address is the default. A different interface IP\
- address can be chosen.'),
+  display_bind_tooltip: T('Select an IP address to use for remote VNC sessions.\
+ Note: this setting only applies if you are using a VNC client other than the TrueNAS WebUI.'),
   vcpus_warning: T('The product of vCPUs, cores and threads must not exceed {maxVcpus} on this system.'),
   vcpus_tooltip: T('Number of virtual CPUs to allocate to the virtual\
  machine. The VM operating system\
