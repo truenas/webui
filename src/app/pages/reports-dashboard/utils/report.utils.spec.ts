b658adc42021be70cd86d376ec28fc6daaf9f5ca
Boris Vasilenko <bvasilenko@ixsystems.com>
2024-04-02 16:36:38 +0300

    NAS-127963: Add tests

---
+++ src/app/pages/reports-dashboard/utils/report.utils.spec.ts
@@ -0,0 +1,31 @@
+import { formatValue } from 'app/pages/reports-dashboard/utils/report.utils';
+
+describe('formatValue', () => {
+  it('returns default value', () => {
+    expect(formatValue(500000000, 'test_unknown')).toBe('500m');
+  });
+
+  it('returns value for Seconds', () => {
+    expect(formatValue(60 * 60 * 24 * 3, 'Seconds')).toBe('3 days');
+  });
+
+  it('returns value for Mebibytes', () => {
+    expect(formatValue(500, 'Mebibytes')).toBe('500 MiB');
+  });
+
+  it('returns value for Kibibytes', () => {
+    expect(formatValue(500, 'Kibibytes')).toBe('500 KiB');
+  });
+
+  it('returns value for Kilobits', () => {
+    expect(formatValue(500, 'Kilobits')).toBe('500 Kb');
+  });
+
+  it('returns value for bits', () => {
+    expect(formatValue(500, 'bits')).toBe('500 B');
+  });
+
+  it('returns value for bytes', () => {
+    expect(formatValue(500, 'bytes')).toBe('500 B');
+  });
+});
