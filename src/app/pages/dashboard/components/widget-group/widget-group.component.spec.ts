26e9ee87c72b4718936bcffd7213c3a59b6eaa9d
Evgeny Stepanovych <undsoft@gmail.com>
2024-04-03 13:10:57 +0200

    NAS-128005: Main dashboard architecture

--- src/app/pages/dashboard/components/widget-group/widget-group.component.spec.ts
+++ src/app/pages/dashboard/components/widget-group/widget-group.component.spec.ts
@@ -1,8 +1,5 @@
 import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
-import { MockComponent } from 'ng-mocks';
 import { WidgetGroupComponent } from 'app/pages/dashboard/components/widget-group/widget-group.component';
-import { WidgetHostnameComponent } from 'app/pages/dashboard/widgets/network/widget-hostname/widget-hostname.component';
-import { WidgetInterfaceIpComponent } from 'app/pages/dashboard/widgets/network/widget-interface-ip/widget-interface-ip.component';
 
 describe('WidgetGroupComponent', () => {
   // TODO:
@@ -10,11 +7,7 @@ describe('WidgetGroupComponent', () => {
   let spectator: Spectator<WidgetGroupComponent>;
   const createComponent = createComponentFactory({
     component: WidgetGroupComponent,
-    declarations: [
-      // TODO: Temporary
-      MockComponent(WidgetHostnameComponent),
-      MockComponent(WidgetInterfaceIpComponent),
-    ],
+    shallow: true,
   });
 
   beforeEach(() => {
