112d2f21516688e64ff209872cdc10cb881690c9
Evgeny Stepanovych <undsoft@gmail.com>
2024-04-09 13:21:22 +0200

    NAS-128268: Simplify ixSearchableElement (#9937)

--- src/app/directives/common/ui-search.directive.ts
+++ src/app/directives/common/ui-search.directive.ts
@@ -5,19 +5,19 @@ import { generateIdFromHierarchy } from 'app/modules/global-search/helpers/gener
 import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
 
 @Directive({
-  selector: '[ixUiSearchableElement]',
+  selector: '[ixUiSearch]',
 })
-export class UiSearchableElementDirective implements OnInit {
-  @Input() ixSearchConfig!: UiSearchableElement;
+export class UiSearchDirective implements OnInit {
+  @Input({ required: true, alias: 'ixUiSearch' }) config: UiSearchableElement;
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit(): void {
-    if (this.ixSearchConfig?.hierarchy || this.ixSearchConfig?.anchor) {
+    if (this.config.hierarchy || this.config.anchor) {
       this.renderer.setAttribute(
         this.el.nativeElement,
         'id',
-        this.ixSearchConfig.anchor || generateIdFromHierarchy(this.ixSearchConfig?.hierarchy),
+        this.config.anchor || generateIdFromHierarchy(this.config.hierarchy),
       );
     }
   }
