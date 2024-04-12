b91a5b515fe559de9785d66fc866d2c19db3b58b
Alex Karpov <akarpov@ixsystems.com>
2024-04-12 10:30:27 +0300

    NAS-128208 / 24.10 / Implement tables for cloud backup (#9943)
    * NAS-128208: Implement tables for cloud backup
    
    * NAS-128208: Implement tables for cloud backup

---
+++ src/app/pages/data-protection/cloud-backup/cloud-backup-list/cloud-backup-list.elements.ts
@@ -0,0 +1,12 @@
+import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
+import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
+
+export const cloudBackupListElements: UiSearchableElement = {
+  hierarchy: [T('Data Protection'), T('TrueCloud Backup Tasks')],
+  anchorRouterLink: ['/data-protection', 'cloud-backup'],
+  elements: {
+    tasks: {
+      synonyms: [T('Tasks'), T('Cloud Backup')],
+    },
+  },
+};
