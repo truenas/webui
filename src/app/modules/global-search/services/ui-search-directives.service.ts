3c62e8fbc3c73809e15cd8978b426f003894fd65
Denys Butenko <ua13dark@gmail.com>
2024-04-15 12:55:10 +0300

    NAS-128071: Use ixUiSearchableElement to highlight a found element

--- src/app/modules/global-search/services/ui-search-directives.service.ts
+++ src/app/modules/global-search/services/ui-search-directives.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { UiSearchDirective } from 'app/directives/common/ui-search.directive';
 import { getSearchableElementId } from 'app/modules/global-search/helpers/get-searchable-element-id';
 import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
@@ -9,25 +9,35 @@ import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-sea
 })
 export class UiSearchDirectivesService {
   private directives = new Set<UiSearchDirective>();
-  highlightOnDirectiveAdded$ = new BehaviorSubject<UiSearchDirective>(null);
+  highlightOnDirectiveAdded$ = new Subject<void>();
 
   size(): number {
     return this.directives.size;
   }
 
   has(element: UiSearchableElement): boolean {
-    return [...this.directives].some((directive) => directive.id === getSearchableElementId(element));
+    const elementId = getSearchableElementId(element);
+    for (const directive of this.directives.values()) {
+      if (directive.id === elementId) {
+        return true;
+      }
+    }
+    return false;
   }
 
   get(element: UiSearchableElement): UiSearchDirective {
-    return [...this.directives.values()].find((directive) => {
-      return directive.id === getSearchableElementId(element);
-    });
+    const elementId = getSearchableElementId(element);
+    for (const directive of this.directives.values()) {
+      if (directive.id === elementId) {
+        return directive;
+      }
+    }
+    return null;
   }
 
   register(directive: UiSearchDirective): void {
     this.directives.add(directive);
-    this.highlightOnDirectiveAdded$.next(directive);
+    this.highlightOnDirectiveAdded$.next();
   }
 
   unregister(directive: UiSearchDirective): void {
