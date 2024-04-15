ff552b05d84f0b4981f5af0e793ace7aff01793a
Evgeny Stepanovych <undsoft@gmail.com>
2024-04-15 13:59:09 +0200

    NAS-128332: Porting memory widget

--- src/app/pages/dashboard/widgets/all-widgets.constant.ts
+++ src/app/pages/dashboard/widgets/all-widgets.constant.ts
@@ -1,5 +1,6 @@
 import { WidgetType } from 'app/pages/dashboard/types/widget.interface';
 import { helpWidget } from 'app/pages/dashboard/widgets/help/widget-help/widget-help.definition';
+import { memoryWidget } from 'app/pages/dashboard/widgets/memory/widget-memory/widget-memory.definition';
 import { hostnameWidget } from 'app/pages/dashboard/widgets/network/widget-hostname/widget-hostname.definition';
 import {
   interfaceIpWidget,
@@ -10,10 +11,12 @@ export const widgetComponents = [
   interfaceIpWidget.component,
   interfaceIpWidget.settingsComponent,
   helpWidget.component,
+  memoryWidget.component,
 ];
 
 export const widgetRegistry = {
   [WidgetType.Hostname]: hostnameWidget,
   [WidgetType.InterfaceIp]: interfaceIpWidget,
   [WidgetType.Help]: helpWidget,
+  [WidgetType.Memory]: memoryWidget,
 };
---
+++ src/app/pages/dashboard/widgets/memory/widget-memory/widget-memory.definition.ts
@@ -0,0 +1,13 @@
+import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
+import { WidgetCategory } from 'app/pages/dashboard/types/widget-category.enum';
+import { dashboardWidget } from 'app/pages/dashboard/types/widget-component.interface';
+import { SlotSize } from 'app/pages/dashboard/types/widget.interface';
+import { WidgetMemoryComponent } from 'app/pages/dashboard/widgets/memory/widget-memory/widget-memory.component';
+
+export const memoryWidget = dashboardWidget({
+  name: T('Memory'),
+  supportedSizes: [SlotSize.Full],
+  category: WidgetCategory.Memory,
+  component: WidgetMemoryComponent,
+  settingsComponent: null,
+});
