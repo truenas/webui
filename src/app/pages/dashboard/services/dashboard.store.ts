38d9635eeddada44a68b65e11f3940d327c021b0
Evgeny Stepanovych <undsoft@gmail.com>
2024-04-22 12:27:38 +0200

    NAS-128167 / 24.10 / Widget groups (#9966)

--- src/app/pages/dashboard/services/dashboard.store.ts
+++ src/app/pages/dashboard/services/dashboard.store.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { ComponentStore } from '@ngrx/component-store';
+import { Observable, of, tap } from 'rxjs';
+import { demoWidgets } from 'app/pages/dashboard/services/demo-widgets.constant';
 import { WidgetGroup } from 'app/pages/dashboard/types/widget-group.interface';
 
 export interface DashboardState {
@@ -22,17 +24,30 @@ export interface DashboardState {
 @Injectable()
 export class DashboardStore extends ComponentStore<DashboardState> {
   readonly isLoading$ = this.select((state) => state.isLoading);
+  readonly groups$ = this.select((state) => state.groups);
 
   readonly entered = this.effect((trigger$) => {
     // TODO: Load data from user attributes.
     // TODO: Set isLoading flag, but do not clear previous state.
     // TODO: If it's in old format, migrate to new format.
     // TODO: Handle global errors like something network errors or something completely unexpected in user attributes.
-    return trigger$;
+    return trigger$.pipe(
+      tap(() => this.setDemoData()),
+    );
   });
 
-  readonly save = this.effect<WidgetGroup[]>((groups$) => {
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  save(groups: WidgetGroup[]): Observable<void> {
     // TODO: Save to user settings.
-    return groups$;
-  });
+    return of(undefined);
+  }
+
+  // TODO: Demo code.
+  private setDemoData(): void {
+    this.setState({
+      isLoading: false,
+      globalError: '',
+      groups: demoWidgets,
+    });
+  }
 }
