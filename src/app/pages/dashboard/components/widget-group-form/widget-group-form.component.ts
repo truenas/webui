3be8a09f740bb2b8b630a06c3c51025da58cb70d
RehanY147 <rehanyousaf147@gmail.com>
2024-05-02 03:32:17 +0500

    NAS-128557: Update category options based on select layout and slot size

--- src/app/pages/dashboard/components/widget-group-form/widget-group-form.component.ts
+++ src/app/pages/dashboard/components/widget-group-form/widget-group-form.component.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef, Component,
+} from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { of } from 'rxjs';
+import {
+  Observable, of, tap,
+} from 'rxjs';
+import { Option } from 'app/interfaces/option.interface';
 import { ChainedRef } from 'app/modules/ix-forms/components/ix-slide-in/chained-component-ref';
-import { WidgetGroup, WidgetGroupLayout, widgetGroupIcons } from 'app/pages/dashboard/types/widget-group.interface';
+import { WidgetCategory } from 'app/pages/dashboard/types/widget-category.enum';
+import {
+  WidgetGroup, WidgetGroupLayout, layoutToSlotSizes, widgetGroupIcons,
+} from 'app/pages/dashboard/types/widget-group.interface';
+import { widgetRegistry } from 'app/pages/dashboard/widgets/all-widgets.constant';
 
 @UntilDestroy()
 @Component({
@@ -18,8 +27,11 @@ export class WidgetGroupFormComponent {
   protected form = this.formBuilder.group({
     template: [''],
     layout: [WidgetGroupLayout.Full],
+    category: [null as WidgetCategory],
   });
   readonly layoutsMap = widgetGroupIcons;
+
+  widgetCategoriesOptions: Observable<Option[]>;
 
   // TODO: Implement template options
   templateOptions$ = of([]);
@@ -29,14 +41,49 @@ export class WidgetGroupFormComponent {
     private chainedRef: ChainedRef<WidgetGroup>,
     private cdr: ChangeDetectorRef,
   ) {
-    this.group = chainedRef.getData();
+    this.setCategoryOptions();
+    this.setInitialFormValues();
+  }
+
+  setInitialFormValues(): void {
+    this.group = this.chainedRef.getData();
     this.form.controls.layout.setValue(this.group.layout);
-    this.form.controls.layout.valueChanges
-      .pipe(untilDestroyed(this))
-      .subscribe((layout) => {
+    this.form.controls.layout.valueChanges.pipe(
+      tap((layout) => {
         this.group = { ...this.group, layout };
+        this.setCategoryOptions();
         this.cdr.markForCheck();
-      });
+      }),
+      untilDestroyed(this),
+    ).subscribe();
+  }
+
+  selectedSlotChanged(slotIndex: number): void {
+    this.selectedSlot = slotIndex;
+    this.setCategoryOptions();
+  }
+
+  setCategoryOptions(): void {
+    const categoryLabels = (Object.keys(WidgetCategory) as (keyof typeof WidgetCategory)[]);
+    const sizeSuitedCategoryLabels = categoryLabels.filter(
+      (key: keyof typeof WidgetCategory) => {
+        const widgetNames = Object.keys(widgetRegistry) as (keyof typeof widgetRegistry)[];
+        return widgetNames.some((widgetName) => {
+          if (widgetRegistry[widgetName].category !== WidgetCategory[key]) {
+            return false;
+          }
+          const layout = this.form.controls.layout.value;
+          const slotIndex = this.selectedSlot;
+          const slotSize = layoutToSlotSizes[layout][slotIndex];
+          return widgetRegistry[widgetName].supportedSizes.includes(slotSize);
+        });
+      },
+    );
+    this.widgetCategoriesOptions = of(
+      sizeSuitedCategoryLabels.map(
+        (label) => ({ label, value: WidgetCategory[label] }),
+      ),
+    );
   }
 
   onSubmit(): void {
