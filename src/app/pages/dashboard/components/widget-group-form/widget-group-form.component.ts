8a5dfef611a3817973c00e2d3ed9150ae9e15f77
RehanY147 <rehanyousaf147@gmail.com>
2024-05-02 05:17:02 +0500

    NAS-128557: Added size suited type options

--- src/app/pages/dashboard/components/widget-group-form/widget-group-form.component.ts
+++ src/app/pages/dashboard/components/widget-group-form/widget-group-form.component.ts
@@ -12,6 +12,7 @@ import { WidgetCategory } from 'app/pages/dashboard/types/widget-category.enum';
 import {
   WidgetGroup, WidgetGroupLayout, layoutToSlotSizes, widgetGroupIcons,
 } from 'app/pages/dashboard/types/widget-group.interface';
+import { WidgetType } from 'app/pages/dashboard/types/widget.interface';
 import { widgetRegistry } from 'app/pages/dashboard/widgets/all-widgets.constant';
 
 @UntilDestroy()
@@ -28,10 +29,12 @@ export class WidgetGroupFormComponent {
     template: [''],
     layout: [WidgetGroupLayout.Full],
     category: [null as WidgetCategory],
+    type: [null as WidgetType],
   });
   readonly layoutsMap = widgetGroupIcons;
 
-  widgetCategoriesOptions: Observable<Option[]>;
+  widgetCategoriesOptions$: Observable<Option[]>;
+  widgetTypesOptions$: Observable<Option[]>;
 
   // TODO: Implement template options
   templateOptions$ = of([]);
@@ -43,16 +46,31 @@ export class WidgetGroupFormComponent {
   ) {
     this.setCategoryOptions();
     this.setInitialFormValues();
+    this.setupLayoutUpdates();
+    this.setupCategoryUpdate();
   }
 
   setInitialFormValues(): void {
     this.group = this.chainedRef.getData();
     this.form.controls.layout.setValue(this.group.layout);
+  }
+
+  setupLayoutUpdates(): void {
     this.form.controls.layout.valueChanges.pipe(
       tap((layout) => {
         this.group = { ...this.group, layout };
+        this.form.controls.category.setValue(null);
         this.setCategoryOptions();
         this.cdr.markForCheck();
+      }),
+      untilDestroyed(this),
+    ).subscribe();
+  }
+
+  setupCategoryUpdate(): void {
+    this.form.controls.category.valueChanges.pipe(
+      tap((category) => {
+        this.setWidgetTypeOptions(category);
       }),
       untilDestroyed(this),
     ).subscribe();
@@ -63,27 +81,52 @@ export class WidgetGroupFormComponent {
     this.setCategoryOptions();
   }
 
+  getEnumKeyByEnumValue<T extends Record<string, string>>(
+    myEnum: T,
+    enumValue: string,
+  ): keyof T | null {
+    const keys = Object.keys(myEnum).filter((x) => myEnum[x] === enumValue);
+    return keys.length > 0 ? keys[0] : null;
+  }
+
   setCategoryOptions(): void {
-    const categoryLabels = (Object.keys(WidgetCategory) as (keyof typeof WidgetCategory)[]);
-    const sizeSuitedCategoryLabels = categoryLabels.filter(
-      (key: keyof typeof WidgetCategory) => {
-        const widgetNames = Object.keys(widgetRegistry) as (keyof typeof widgetRegistry)[];
-        return widgetNames.some((widgetName) => {
-          if (widgetRegistry[widgetName].category !== WidgetCategory[key]) {
-            return false;
-          }
-          const layout = this.form.controls.layout.value;
-          const slotIndex = this.selectedSlot;
-          const slotSize = layoutToSlotSizes[layout][slotIndex];
-          return widgetRegistry[widgetName].supportedSizes.includes(slotSize);
-        });
-      },
-    );
-    this.widgetCategoriesOptions = of(
-      sizeSuitedCategoryLabels.map(
-        (label) => ({ label, value: WidgetCategory[label] }),
+    const widgets = Object.values(widgetRegistry);
+    const categories = new Map<keyof typeof WidgetCategory, WidgetCategory>();
+    const layout = this.form.controls.layout.value;
+    const slotIndex = this.selectedSlot;
+    const slotSize = layoutToSlotSizes[layout][slotIndex];
+    for (const widget of widgets) {
+      if (widget.supportedSizes.includes(slotSize)) {
+        const categoryName = this.getEnumKeyByEnumValue(WidgetCategory, widget.category);
+        categories.set(categoryName as keyof typeof WidgetCategory, widget.category);
+      }
+    }
+    this.widgetCategoriesOptions$ = of(
+      Array.from(categories.entries()).map(
+        ([key, value]) => {
+          return ({ label: key, value });
+        },
       ),
     );
+  }
+
+  setWidgetTypeOptions(category: WidgetCategory): void {
+    const layout = this.form.controls.layout.value;
+    const slotIndex = this.selectedSlot;
+    const slotSize = layoutToSlotSizes[layout][slotIndex];
+
+    const sizeSuitedTypes = Object.values(WidgetType).filter((type) => {
+      const widget = widgetRegistry[type];
+      if (widget.category === category && widget.supportedSizes.includes(slotSize)) {
+        return true;
+      }
+
+      return false;
+    });
+
+    this.widgetTypesOptions$ = of(sizeSuitedTypes.map((type) => {
+      return { label: this.getEnumKeyByEnumValue(WidgetType, type), value: type };
+    }));
   }
 
   onSubmit(): void {
