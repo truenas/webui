a74a23ce2e433cb3794ba1ff771a4641f7ad0eaa
Evgeny Stepanovych <undsoft@gmail.com>
2024-05-03 11:53:21 +0200

    NAS-128676: Fallback for failed Netdata login

--- src/app/pages/reports-dashboard/components/reports-global-controls/reports-global-controls.component.spec.ts
+++ src/app/pages/reports-dashboard/components/reports-global-controls/reports-global-controls.component.spec.ts
@@ -2,6 +2,8 @@ import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
 import { mockProvider, Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 import { provideMockStore } from '@ngrx/store/testing';
 import { of } from 'rxjs';
@@ -10,11 +12,12 @@ import { mockCall, mockWebSocket } from 'app/core/testing/utils/mock-websocket.u
 import { Preferences } from 'app/interfaces/preferences.interface';
 import { IxDynamicFormModule } from 'app/modules/ix-dynamic-form/ix-dynamic-form.module';
 import { IxFormsModule } from 'app/modules/ix-forms/ix-forms.module';
-import { FormErrorHandlerService } from 'app/modules/ix-forms/services/form-error-handler.service';
+import {
+  NetdataDialogComponent,
+} from 'app/pages/reports-dashboard/components/reports-global-controls/netdata-dialog/netdata-dialog.component';
 import { ReportsGlobalControlsComponent } from 'app/pages/reports-dashboard/components/reports-global-controls/reports-global-controls.component';
 import { ReportTab, ReportType } from 'app/pages/reports-dashboard/interfaces/report-tab.interface';
 import { ReportsService } from 'app/pages/reports-dashboard/reports.service';
-import { WebSocketService } from 'app/services/ws.service';
 import { selectPreferences } from 'app/store/preferences/preferences.selectors';
 
 describe('ReportsGlobalControlsComponent', () => {
@@ -32,11 +35,15 @@ describe('ReportsGlobalControlsComponent', () => {
       mockWebSocket([
         mockCall('disk.query', []),
         mockCall('disk.temperatures'),
-        mockCall('reporting.netdataweb_generate_password'),
         mockCall('reporting.netdata_graphs', []),
       ]),
+      mockProvider(MatDialog),
       mockAuth(),
-      mockProvider(FormErrorHandlerService),
+      mockProvider(ActivatedRoute, {
+        routeConfig: {
+          path: 'system',
+        },
+      }),
       mockProvider(ReportsService, {
         getReportTabs: () => ([({ value: ReportType.System, label: 'system' } as ReportTab)] as ReportTab[]),
         getReportGraphs: () => of([]),
@@ -59,11 +66,10 @@ describe('ReportsGlobalControlsComponent', () => {
     loader = TestbedHarnessEnvironment.loader(spectator.fixture);
   });
 
-  it('click netdata button', async () => {
+  it('opens NetdataDialog when Netdata button is pressed', async () => {
     const netdataButton = await loader.getHarness(MatButtonHarness.with({ text: 'Netdata' }));
-
     await netdataButton.click();
 
-    expect(spectator.inject(WebSocketService).call).toHaveBeenCalledWith('reporting.netdataweb_generate_password', []);
+    expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(NetdataDialogComponent);
   });
 });
