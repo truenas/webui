45ee7ea6f4c907c7fe8d89e8e85b1f9523f04d1e
Evgeny Stepanovych <undsoft@gmail.com>
2024-05-10 19:41:17 +0200

    NAS-128883: Porting jbof and set enclosure label dialog

--- src/app/pages/system/enclosure/components/enclosure-dashboard/enclosure-dashboard.component.spec.ts
+++ src/app/pages/system/enclosure/components/enclosure-dashboard/enclosure-dashboard.component.spec.ts
@@ -1,18 +1,23 @@
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { MatDialog } from '@angular/material/dialog';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
 import { of } from 'rxjs';
+import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
+import { mockCall, mockWebSocket } from 'app/core/testing/utils/mock-websocket.utils';
 import { DashboardEnclosure } from 'app/interfaces/enclosure.interface';
 import { PageHeaderModule } from 'app/modules/page-header/page-header.module';
 import {
   EnclosureDashboardComponent,
 } from 'app/pages/system/enclosure/components/enclosure-dashboard/enclosure-dashboard.component';
+import {
+  SetEnclosureLabelDialogComponent,
+} from 'app/pages/system/enclosure/components/enclosure-dashboard/set-enclosure-label-dialog/set-enclosure-label-dialog.component';
 import { EnclosureStore } from 'app/pages/system/enclosure/services/enclosure.store';
 
 describe('EnclosureDashboardComponent', () => {
   let spectator: Spectator<EnclosureDashboardComponent>;
-  // eslint-disable-next-line unused-imports/no-unused-vars
   let loader: HarnessLoader;
   const createComponent = createComponentFactory({
     component: EnclosureDashboardComponent,
@@ -21,6 +26,9 @@ describe('EnclosureDashboardComponent', () => {
       PageHeaderModule,
     ],
     providers: [
+      mockWebSocket([
+        mockCall('jbof.licensed', 5),
+      ]),
       mockProvider(MatDialog),
       mockProvider(EnclosureStore, {
         selectedEnclosure$: of({
@@ -28,33 +36,39 @@ describe('EnclosureDashboardComponent', () => {
           name: 'M50',
           label: 'Current label',
         } as DashboardEnclosure),
+        selectedSlot$: of(null),
         initiate: jest.fn(),
         renameSelectedEnclosure: jest.fn(),
       }),
+      mockAuth(),
     ],
   });
 
   beforeEach(() => {
+    spectator = createComponent();
+    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
   });
 
   it('initializes store when component is initialized', () => {
-    console.info('start of initializes store');
-    spectator = createComponent();
-    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
-    expect(2).toBe(2);
-    console.info('end of initializes store');
+    expect(spectator.inject(EnclosureStore).initiate).toHaveBeenCalled();
   });
 
-  it('opens edit dialog when Edit Label is pressed', () => {
-    try {
-      console.info('start of opens edit dialog');
-      spectator = createComponent();
-      loader = TestbedHarnessEnvironment.loader(spectator.fixture);
+  it('opens edit dialog when Edit Label is pressed', async () => {
+    jest.spyOn(spectator.inject(MatDialog), 'open').mockReturnValue({
+      afterClosed: () => of('new label'),
+    } as MatDialogRef<SetEnclosureLabelDialogComponent>);
 
-      console.info('a');
-      expect(2).toBe(2);
-    } catch (error) {
-      console.error(error);
-    }
+    const editLabel = await loader.getHarness(MatButtonHarness.with({ text: 'Edit Label' }));
+    await editLabel.click();
+
+    expect(spectator.inject(MatDialog).open).toHaveBeenCalledWith(SetEnclosureLabelDialogComponent, {
+      data: {
+        currentLabel: 'Current label',
+        defaultLabel: 'M50',
+        enclosureId: 'enclosure-id',
+      },
+    });
+
+    expect(spectator.inject(EnclosureStore).renameSelectedEnclosure).toHaveBeenCalledWith('new label');
   });
 });
