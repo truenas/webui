8cacafa55e5e2fc4aecedf9d7dc40a24aac425eb
Evgeny Stepanovych <undsoft@gmail.com>
2024-05-13 13:08:53 +0200

    NAS-128947 / 24.10 / Replacing more usages of EntityJobComponent (#10050)

--- src/app/views/others/config-reset/config-reset.component.ts
+++ src/app/views/others/config-reset/config-reset.component.ts
@@ -1,14 +1,17 @@
 import { Location } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy, Component, OnDestroy, OnInit,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
+import { Timeout } from 'app/interfaces/timeout.interface';
 import { DialogService } from 'app/modules/dialog/dialog.service';
-import { EntityJobComponent } from 'app/modules/entity/entity-job/entity-job.component';
 import { AppLoaderService } from 'app/modules/loader/app-loader.service';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
 import { WebSocketConnectionService } from 'app/services/websocket-connection.service';
+import { WebSocketService } from 'app/services/ws.service';
 
 @UntilDestroy()
 @Component({
@@ -17,7 +20,9 @@ import { WebSocketConnectionService } from 'app/services/websocket-connection.se
   styleUrls: ['./config-reset.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfigResetComponent implements OnInit {
+export class ConfigResetComponent implements OnInit, OnDestroy {
+  private connectedSubscription: Timeout;
+
   constructor(
     private wsManager: WebSocketConnectionService,
     protected router: Router,
@@ -27,16 +32,19 @@ export class ConfigResetComponent implements OnInit {
     protected dialogService: DialogService,
     protected matDialog: MatDialog,
     private location: Location,
+    private ws: WebSocketService,
   ) {}
 
   isWsConnected(): void {
+    // TODO: isConnected$ doesn't work correctly.
     this.wsManager.isConnected$.pipe(untilDestroyed(this)).subscribe({
       next: (isConnected) => {
         if (isConnected) {
           this.loader.close();
           this.router.navigate(['/sessions/signin']);
         } else {
-          setTimeout(() => {
+          // TODO: Why not just rely on isConnected$ emitting new value.
+          this.connectedSubscription = setTimeout(() => {
             this.isWsConnected();
           }, 1000);
         }
@@ -49,25 +57,34 @@ export class ConfigResetComponent implements OnInit {
     this.location.replaceState('/sessions/signin');
 
     this.matDialog.closeAll();
-    this.resetConfigSubmit();
+    this.resetConfig();
   }
 
-  resetConfigSubmit(): void {
-    const dialogRef = this.matDialog.open(EntityJobComponent, { data: { title: this.translate.instant('Resetting. Please wait...') }, disableClose: true });
-    dialogRef.componentInstance.setCall('config.reset', [{ reboot: true }]);
-    dialogRef.componentInstance.setDescription(this.translate.instant('Resetting system configuration to default settings. The system will restart.'));
-    dialogRef.componentInstance.submit();
-    dialogRef.componentInstance.success.pipe(untilDestroyed(this)).subscribe(() => {
-      dialogRef.close();
-      this.wsManager.prepareShutdown();
-      this.loader.open();
-      setTimeout(() => {
-        this.isWsConnected();
-      }, 15000);
-    });
-    dialogRef.componentInstance.failure.pipe(untilDestroyed(this)).subscribe((failedJob) => {
-      dialogRef.close();
-      this.dialogService.error(this.errorHandler.parseError(failedJob));
-    });
+  ngOnDestroy(): void {
+    if (this.connectedSubscription) {
+      clearTimeout(this.connectedSubscription);
+    }
+  }
+
+  resetConfig(): void {
+    this.dialogService.jobDialog(
+      this.ws.job('config.reset', [{ reboot: true }]),
+      {
+        title: this.translate.instant('Resetting. Please wait...'),
+        description: this.translate.instant('Resetting system configuration to default settings. The system will restart.'),
+      },
+    )
+      .afterClosed()
+      .pipe(
+        this.errorHandler.catchError(),
+        untilDestroyed(this),
+      )
+      .subscribe(() => {
+        this.wsManager.prepareShutdown();
+        this.loader.open();
+        setTimeout(() => {
+          this.isWsConnected();
+        }, 15000);
+      });
   }
 }
