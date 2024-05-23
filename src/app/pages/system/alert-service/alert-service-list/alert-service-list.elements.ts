3597e844e671c7a300e489e2ee935c33252207e5
Denys Butenko <ua13dark@gmail.com>
2024-05-23 17:51:15 +0700

    NAS-129098: Global search QA 3: Reporting, System, Topbar

--- src/app/pages/system/alert-service/alert-service-list/alert-service-list.elements.ts
+++ src/app/pages/system/alert-service/alert-service-list/alert-service-list.elements.ts
@@ -4,7 +4,19 @@ import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-sea
 export const alertServiceListElements = {
   hierarchy: [T('System'), T('Alert Settings'), T('Alert Services')],
   anchorRouterLink: ['/system', 'alert-settings', 'services'],
+  synonyms: [T('Alerts'), T('Configure Notifications'), T('Configure Alerts')],
   elements: {
     alertServiceList: {},
+    add: {
+      hierarchy: [T('Add')],
+      synonyms: [
+        T('Alerts'),
+        T('Configure Alerts'),
+        T('Create Alert'),
+        T('Add Alert'),
+        T('New Alert'),
+        T('Notifications'),
+      ],
+    },
   },
 } satisfies UiSearchableElement;
--- src/app/pages/system/general-settings/gui/gui-card/gui-card.elements.ts
+++ src/app/pages/system/general-settings/gui/gui-card/gui-card.elements.ts
@@ -11,5 +11,36 @@ export const guiCardElements = {
     theme: {
       hierarchy: [T('Theme')],
     },
+    sslCertificate: {
+      hierarchy: [T('SSL Certificate')],
+    },
+    ipv4Address: {
+      hierarchy: [T('IPv4 Address')],
+      synonyms: [T('Web Interface Address')],
+    },
+    ipv6Address: {
+      hierarchy: [T('IPv6 Address')],
+      synonyms: [T('Web Interface Address')],
+    },
+    uiHttpPort: {
+      hierarchy: [T('HTTP Port')],
+      synonyms: [T('Web Interface Port')],
+    },
+    uiHttpsPort: {
+      hierarchy: [T('HTTPS Port')],
+      synonyms: [T('Web Interface Port'), T('SSL Web Interface Port')],
+    },
+    sslProtocols: {
+      hierarchy: [T('SSL Protocols')],
+    },
+    httpsRedirect: {
+      hierarchy: [T('HTTPS Redirect')],
+    },
+    usageCollection: {
+      hierarchy: [T('Usage Collection')],
+    },
+    consoleMsg: {
+      hierarchy: [T('Show Console Messages')],
+    },
   },
 } satisfies UiSearchableElement;
