73b7331fe46ffeb88e93f52f161e7043ceda0b13
RehanY147 <rehanyousaf147@gmail.com>
2024-05-29 14:35:05 +0500

    NAS-128901: Update m50-enclosure.component.ts

--- src/app/pages/system/enclosure/components/views/enclosure-view/enclosures/m50-enclosure/m50-enclosure.component.ts
+++ src/app/pages/system/enclosure/components/views/enclosure-view/enclosures/m50-enclosure/m50-enclosure.component.ts
@@ -2,7 +2,7 @@ import {
   ChangeDetectionStrategy, Component, computed, input,
 } from '@angular/core';
 import { DashboardEnclosure, DashboardEnclosureSlot } from 'app/interfaces/enclosure.interface';
-import { OverviewInfo } from 'app/pages/system/enclosure/components/views/enclosure-view/disks-overview/disks-overview.component';
+import { EnclosureView } from 'app/pages/system/enclosure/components/views/enclosure-view/disks-overview/disks-overview.component';
 import { EnclosureComponent } from 'app/pages/system/enclosure/interfaces/enclosure-component.interface';
 
 @Component({
@@ -12,7 +12,7 @@ import { EnclosureComponent } from 'app/pages/system/enclosure/interfaces/enclos
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class M50EnclosureComponent implements EnclosureComponent {
-  readonly selectedView = input.required<OverviewInfo['name']>();
+  readonly selectedView = input.required<EnclosureView>();
   readonly enclosure = input.required<DashboardEnclosure>();
   readonly selectedSlot = input.required<DashboardEnclosureSlot>();
 
