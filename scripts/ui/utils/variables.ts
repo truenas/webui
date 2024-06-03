6bdfdb7ebdbd2022408c051debfcd578be6023d2
Evgeny Stepanovych <undsoft@gmail.com>
2024-06-03 13:46:52 +0200

    NAS-129241: Enclosure mocking rework

--- scripts/ui/utils/variables.ts
+++ scripts/ui/utils/variables.ts
@@ -1,5 +1,3 @@
-import { realpathSync } from 'fs';
-
 const root = `${__dirname}/../../../`;
-export const environmentTs = realpathSync(`${root}src/environments/environment.ts`);
-export const environmentTemplate = realpathSync(`${root}scripts/ui/environment.ts.template`);
+export const environmentTs = `${root}src/environments/environment.ts`;
+export const environmentTemplate = `${root}scripts/ui/environment.ts.template`;
