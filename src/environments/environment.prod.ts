feda8582379fcfcb0f1b767c53b8fe0194250f81
Evgeny Stepanovych <undsoft@gmail.com>
2024-06-06 13:17:36 +0200

    NAS-129432: Fix environment.ts for production builds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 import { enableProdMode } from '@angular/core';
+import { MockStorageScenario } from 'app/core/testing/mock-enclosure/enums/mock-storage.enum';
 import { WebUiEnvironment } from './environment.interface';
 
 // Sentry Error handling for production mode
@@ -7,6 +8,12 @@ export const environment: WebUiEnvironment = {
   remote: document.location.host,
   production: true,
   sentryPublicDsn: 'https://7ac3e76fe2a94f77a58e1c38ea6b42d9@sentry.ixsystems.com/4',
+  mockConfig: {
+    enabled: false,
+    controllerModel: 'M40',
+    expansionModels: [],
+    scenario: MockStorageScenario.FillSomeSlots,
+  },
 };
 
 // Production
