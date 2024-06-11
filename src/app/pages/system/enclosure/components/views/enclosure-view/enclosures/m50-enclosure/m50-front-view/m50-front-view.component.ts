3e84b7c526fc3ca425001a015a20734d767f13c3
RehanY147 <rehanyousaf147@gmail.com>
2024-06-11 21:13:39 +0500

    NAS-128453: Cleaned up selectors

--- src/app/pages/system/enclosure/components/views/enclosure-view/enclosures/m50-enclosure/m50-front-view/m50-front-view.component.ts
+++ src/app/pages/system/enclosure/components/views/enclosure-view/enclosures/m50-enclosure/m50-front-view/m50-front-view.component.ts
@@ -1,12 +1,18 @@
 import { KeyValue } from '@angular/common';
 import {
   AfterViewInit,
-  ChangeDetectionStrategy, Component, computed, ElementRef, input, Renderer2, TrackByFunction,
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, ElementRef, input, Renderer2, TrackByFunction,
   ViewChild,
 } from '@angular/core';
 import { EnclosureElementType } from 'app/enums/enclosure-slot-status.enum';
 import { DashboardEnclosure, DashboardEnclosureSlot } from 'app/interfaces/enclosure.interface';
 import { EnclosureStore } from 'app/pages/system/enclosure/services/enclosure.store';
+
+interface MouseEventsHandlers {
+  mouseoverHandler: () => void;
+  mouseoutHandler: () => void;
+  clickHandler: () => void;
+}
 
 @Component({
   selector: 'ix-m50-front-view',
@@ -16,6 +22,7 @@ import { EnclosureStore } from 'app/pages/system/enclosure/services/enclosure.st
 export class M50FrontViewComponent implements AfterViewInit {
   @ViewChild('mySvg') viewSvg: ElementRef<HTMLObjectElement>;
   readonly enclosure = input.required<DashboardEnclosure>();
+  private previousSelectRect: SVGRectElement;
   readonly selectedSlot = input.required<DashboardEnclosureSlot>();
   protected readonly selectedSlotIndex = computed(() => {
     const selectedSlot = this.selectedSlot();
@@ -41,6 +48,7 @@ export class M50FrontViewComponent implements AfterViewInit {
   constructor(
     private enclosureStore: EnclosureStore,
     private renderer: Renderer2,
+    private cdr: ChangeDetectorRef,
   ) { }
 
   ngAfterViewInit(): void {
@@ -58,35 +66,88 @@ export class M50FrontViewComponent implements AfterViewInit {
         if (enclosure.elements['Array Device Slot'][driveCageNumber].dev == null) {
           this.renderer.setStyle(group, 'opacity', '0.5');
         }
+        const {
+          mouseoverHandler,
+          mouseoutHandler,
+          clickHandler,
+        } = this.getMouseEventsHandlers(driveCageNumber, group);
 
-        const mouseoverEventHandler = ((gElement: SVGGElement): void => {
-          this.renderer.setStyle(gElement, 'opacity', '0.5');
-        }).bind(this, group);
+        group.addEventListener('mouseover', mouseoverHandler);
 
-        group.addEventListener('mouseover', mouseoverEventHandler);
+        group.addEventListener('mouseout', mouseoutHandler);
 
-        const mouseoutEventHandler = ((slotNumber: number, gElement: SVGGElement): void => {
-          if (enclosure.elements['Array Device Slot'][slotNumber].dev == null) {
-            return;
-          }
-          this.renderer.setStyle(gElement, 'opacity', '1');
-
-          const rect = this.renderer.createElement('rect', 'http://www.w3.org/2000/svg') as SVGRectElement;
-          const gRect = gElement.getBoundingClientRect();
-          this.renderer.setAttribute(rect, 'width', `${gRect.width}`);
-          this.renderer.setAttribute(rect, 'height', `${gRect.height}`);
-          this.renderer.setAttribute(rect, 'x', `${gRect.left}`);
-          this.renderer.setAttribute(rect, 'y', `${gRect.top}`);
-          this.renderer.setAttribute(rect, 'stroke', 'red');
-          this.renderer.setAttribute(rect, 'stroke-width', '3px');
-
-          this.renderer.insertBefore(gElement.parentNode, rect, gElement);
-        }).bind(this, driveCageNumber, group);
-        group.addEventListener('mouseout', mouseoutEventHandler);
+        group.addEventListener('click', clickHandler);
 
         driveCageNumber++;
       }
     };
+  }
+
+  getMouseEventsHandlers(
+    slotNumber: number,
+    gElement: SVGGElement,
+  ): MouseEventsHandlers {
+    const highlightRect = this.createHighlightRect(gElement, slotNumber);
+    const selectRect = this.createSelectRect(gElement);
+
+    const mouseoutHandler = ((slotNo: number, grpEl: SVGGElement, borderedRect: SVGRectElement): void => {
+      this.renderer.removeChild(grpEl.parentNode, borderedRect);
+      if (this.enclosure().elements['Array Device Slot'][slotNo].dev == null) {
+        return;
+      }
+      this.cdr.markForCheck();
+    }).bind(this, slotNumber, gElement, highlightRect);
+
+    const mouseoverHandler = ((grpEl: SVGGElement, borderedRect: SVGRectElement): void => {
+      this.renderer.insertBefore(grpEl.parentNode, borderedRect, grpEl);
+      this.cdr.markForCheck();
+    }).bind(this, gElement, highlightRect);
+
+    const clickHandler = ((slotNo: number) => {
+      if (this.previousSelectRect) {
+        this.renderer.removeChild(gElement.parentNode, this.previousSelectRect);
+      }
+
+      this.previousSelectRect = selectRect;
+      this.renderer.insertBefore(gElement.parentNode, selectRect, gElement);
+      this.onTraySelected(slotNo - 1);
+      this.cdr.markForCheck();
+    }).bind(this, slotNumber);
+
+    return {
+      mouseoutHandler,
+      mouseoverHandler,
+      clickHandler,
+    };
+  }
+
+  createHighlightRect(gElement: SVGGElement, slotNumber: number): SVGRectElement {
+    const highlightRect = this.renderer.createElement('rect', 'http://www.w3.org/2000/svg') as SVGRectElement;
+    const gRect = gElement.getBBox();
+
+    this.renderer.setAttribute(highlightRect, 'width', `${gRect.width}`);
+    this.renderer.setAttribute(highlightRect, 'height', `${gRect.height}`);
+    this.renderer.setAttribute(highlightRect, 'x', `${gRect.x}`);
+    this.renderer.setAttribute(highlightRect, 'y', `${gRect.y}`);
+    this.renderer.setAttribute(highlightRect, 'stroke', '#ff3');
+    this.renderer.setAttribute(highlightRect, 'stroke-width', '5px');
+    this.renderer.setAttribute(highlightRect, 'stroke-dasharray', '5 10');
+    this.renderer.setAttribute(highlightRect, 'id', `highlight_${slotNumber}`);
+    return highlightRect;
+  }
+
+  createSelectRect(gElement: SVGGElement): SVGRectElement {
+    const selectRect = this.renderer.createElement('rect', 'http://www.w3.org/2000/svg') as SVGRectElement;
+    const gRect = gElement.getBBox();
+
+    this.renderer.setAttribute(selectRect, 'width', `${gRect.width}`);
+    this.renderer.setAttribute(selectRect, 'height', `${gRect.height}`);
+    this.renderer.setAttribute(selectRect, 'x', `${gRect.x}`);
+    this.renderer.setAttribute(selectRect, 'y', `${gRect.y}`);
+    this.renderer.setAttribute(selectRect, 'stroke', '#ff3');
+    this.renderer.setAttribute(selectRect, 'stroke-width', '5px');
+    this.renderer.setAttribute(selectRect, 'id', 'select');
+    return selectRect;
   }
 
   protected onTraySelected(slotIndex: number): void {
