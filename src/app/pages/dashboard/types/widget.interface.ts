172ab486ec8b1cdbc67ba16f548da711db8a175e
Boris Vasilenko <bvasilenko@ixsystems.com>
2024-06-12 23:40:21 +0300

    NAS-129491: Add pool widget (Step 1)

--- src/app/pages/dashboard/types/widget.interface.ts
+++ src/app/pages/dashboard/types/widget.interface.ts
@@ -23,7 +23,7 @@ export enum WidgetType {
   OsVersion = 'os-version',
   SystemUptime = 'system-uptime',
   SystemImage = 'system-image',
-  PoolName = 'pool-name',
+  Pool = 'pool',
   ArbitraryText = 'arbitrary-text',
 }
 
---
+++ src/app/pages/dashboard/widgets/storage/widget-pool/widget-pool.component.spec.ts
@@ -0,0 +1,71 @@
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { MockComponent } from 'ng-mocks';
+import { of } from 'rxjs';
+import { FakeFormatDateTimePipe } from 'app/core/testing/classes/fake-format-datetime.pipe';
+import { Pool } from 'app/interfaces/pool.interface';
+import { WidgetResourcesService } from 'app/pages/dashboard/services/widget-resources.service';
+import { SlotSize } from 'app/pages/dashboard/types/widget.interface';
+import { GaugeChartComponent } from 'app/pages/dashboard/widgets/storage/widget-pool/gauge-chart/gauge-chart.component';
+import { WidgetPoolComponent } from 'app/pages/dashboard/widgets/storage/widget-pool/widget-pool.component';
+import { ThemeService } from 'app/services/theme/theme.service';
+
+describe('WidgetPoolComponent', () => {
+  let spectator: Spectator<WidgetPoolComponent>;
+  const createComponent = createComponentFactory({
+    component: WidgetPoolComponent,
+    declarations: [
+      MockComponent(GaugeChartComponent),
+      FakeFormatDateTimePipe,
+    ],
+    providers: [
+      mockProvider(ThemeService, {
+        currentTheme: jest.fn(() => ({
+          bg1: 'bg1',
+          primary: 'primary',
+          red: 'red',
+        })),
+      }),
+    ],
+  });
+
+  beforeEach(() => {
+    spectator = createComponent({
+      props: {
+        settings: {
+          poolId: '1',
+        },
+        size: SlotSize.Full,
+      },
+      providers: [
+        mockProvider(WidgetResourcesService, {
+          getPoolById: jest.fn(() => of({
+            name: 'Pool 1',
+            id: 1,
+            scan: {
+              errors: 2,
+              end_time: {
+                $date: 1717916420000,
+              },
+            },
+          } as Pool)),
+          getDatasetById: jest.fn(() => of({
+            id: '1',
+            available: {
+              parsed: 557187072,
+            },
+            used: {
+              parsed: 2261385216,
+            },
+          })),
+          getDisksByPoolId: jest.fn(() => of([])),
+        }),
+      ],
+    });
+    spectator.detectChanges();
+  });
+
+  it('shows pool name', () => {
+    const poolName = spectator.query('.pool-name');
+    expect(poolName.textContent).toBe('Pool 1');
+  });
+});
