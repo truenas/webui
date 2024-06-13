fd2cf37400e7a9a3fce43acead013de61dd7db5b
RehanY147 <rehanyousaf147@gmail.com>
2024-06-13 05:42:58 +0500

    NAS-128922: Use username instead of parent data

--- src/app/helpers/get-credentials-creation-source.utils.ts
+++ src/app/helpers/get-credentials-creation-source.utils.ts
@@ -1,14 +1,11 @@
 import { CredentialType, Credentials } from 'app/interfaces/credential-type.interface';
 
 export function getCredentialsCreationSource(credentials: Credentials): string {
-  if (credentials.type === CredentialType.UnixSocket || credentials.type === CredentialType.LoginPassword) {
+  if (credentials.type in [CredentialType.UnixSocket, CredentialType.LoginPassword, CredentialType.Token]) {
     return credentials.data.username;
   }
   if (credentials.type === CredentialType.ApiKey) {
     return credentials.data.api_key;
-  }
-  if (credentials.type === CredentialType.Token) {
-    return getCredentialsCreationSource(credentials.data.parent);
   }
   return '';
 }
