8bd62edb557e21367c30c8f8622ced45f8473f56
RehanY147 <rehanyousaf147@gmail.com>
2024-06-17 14:37:47 +0500

    NAS-129104: Add remaining seconds back (#10191)

--- src/app/pages/apps/components/dockerhub-rate-limit-info-dialog/dockerhub-rate-limit-info-dialog.component.ts
+++ src/app/pages/apps/components/dockerhub-rate-limit-info-dialog/dockerhub-rate-limit-info-dialog.component.ts
@@ -1,7 +1,13 @@
 import {
   ChangeDetectionStrategy, Component,
+  Inject,
 } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import { TranslateService } from '@ngx-translate/core';
+import { formatDistance } from 'date-fns';
+import { helptextApps } from 'app/helptext/apps/apps';
+import { DockerHubRateLimit } from 'app/interfaces/dockerhub-rate-limit.interface';
 
 @UntilDestroy()
 @Component({
@@ -9,4 +15,20 @@ import { UntilDestroy } from '@ngneat/until-destroy';
   templateUrl: './dockerhub-rate-limit-info-dialog.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DockerHubRateInfoDialogComponent {}
+export class DockerHubRateInfoDialogComponent {
+  helpText = helptextApps;
+
+  get warningText(): string {
+    return this.translate.instant(
+      this.helpText.dockerHubRateLimit.message,
+      {
+        seconds: formatDistance(0, this.data.remaining_time_limit_in_secs * 1000, { includeSeconds: true }),
+      },
+    );
+  }
+
+  constructor(
+    private translate: TranslateService,
+    @Inject(MAT_DIALOG_DATA) public data: DockerHubRateLimit,
+  ) {}
+}
