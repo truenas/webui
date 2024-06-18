73ee50f7344c503ab3c1ba6b175f6514ac3ab3fa
Alex Karpov <akarpov@ixsystems.com>
2024-06-18 19:32:14 +0300

    NAS-129630: Make Name a required field

--- src/app/pages/data-protection/cloud-backup/cloud-backup-form/cloud-backup-form.component.ts
+++ src/app/pages/data-protection/cloud-backup/cloud-backup-form/cloud-backup-form.component.ts
@@ -61,7 +61,7 @@ export class CloudBackupFormComponent implements OnInit {
     exclude: [[] as string[]],
     pre_script: [''],
     post_script: [''],
-    description: [''],
+    description: ['', [Validators.required]],
     snapshot: [false],
     bwlimit: [[] as string[]],
     transfers: [null as number],
