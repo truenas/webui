00395d32fb3436778aef9c25d7a9daf08b3c8865
Alex Karpov <akarpov@ixsystems.com>
2024-06-18 11:33:56 +0300

    NAS-125931: Prevent User From Focusing/Interacting with restricted controls (requiresRoles)

--- src/app/directives/common/has-access/missing-access-wrapper.component.ts
+++ src/app/directives/common/has-access/missing-access-wrapper.component.ts
@@ -1,5 +1,5 @@
 import {
-  ChangeDetectionStrategy, Component, Input, TemplateRef,
+  ChangeDetectionStrategy, Component, Input, TemplateRef, ElementRef, Renderer2, AfterViewInit, ViewChild,
 } from '@angular/core';
 
 @Component({
@@ -8,7 +8,21 @@ import {
   styleUrls: ['./missing-access-wrapper.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MissingAccessWrapperComponent {
-  @Input() template: TemplateRef<unknown>;
+export class MissingAccessWrapperComponent implements AfterViewInit {
+  @Input() template: TemplateRef<HTMLElement>;
   @Input() class: string;
+  @ViewChild('wrapper', { static: true }) wrapper: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.disableFocusableElements();
+  }
+
+  private disableFocusableElements(): void {
+    const focusableElements = this.wrapper.nativeElement.querySelectorAll(
+      'a, button, input, textarea, select, [tabindex]',
+    );
+    focusableElements.forEach((element: HTMLElement) => this.renderer.setAttribute(element, 'tabindex', '-1'));
+  }
 }
