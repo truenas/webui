409c38c691855cf2493941a0af8e0f14591bc2dc
RehanY147 <rehanyousaf147@gmail.com>
2024-06-19 05:00:00 +0500

    NAS-129642: Add more models

--- src/app/pages/system/enclosure/utils/supported-enclosures.ts
+++ src/app/pages/system/enclosure/utils/supported-enclosures.ts
@@ -1,4 +1,6 @@
-import { ES24Model, MSeriesModel } from 'app/constants/server-series.constant';
+import {
+  ES102Model, ES24Model, ES60Model, MSeriesModel,
+} from 'app/constants/server-series.constant';
 
 export enum EnclosureSide {
   Front = 'front',
@@ -16,12 +18,20 @@ const mSeries = {
   [EnclosureSide.Rear]: 'assets/images/new-hardware/mseries/mseries-rear.svg',
 };
 
-const es24f = {
+const eS24F = {
   [EnclosureSide.Front]: 'assets/images/new-hardware/es24f/es24f-front.svg',
 };
 
-const es24n = {
+const eS24N = {
   [EnclosureSide.Front]: 'assets/images/new-hardware/es24n/es24n-front.svg',
+};
+
+const eS60G2 = {
+  [EnclosureSide.Top]: 'assets/images/new-hardware/es60g2/es60g2-top.svg',
+};
+
+const eS102G2 = {
+  [EnclosureSide.Top]: 'assets/images/new-hardware/es102g2/es102g2-top.svg',
 };
 
 export const supportedEnclosures: Record<string, EnclosureViews> = {
@@ -29,7 +39,8 @@ export const supportedEnclosures: Record<string, EnclosureViews> = {
   [MSeriesModel.M40]: mSeries,
   [MSeriesModel.M50]: mSeries,
   [MSeriesModel.M60]: mSeries,
-  [ES24Model.ES24F]: es24f,
-  [ES24Model.ES24N]: es24n,
-
+  [ES24Model.Es24F]: eS24F,
+  [ES24Model.Es24N]: eS24N,
+  [ES60Model.Es60G2]: eS60G2,
+  [ES102Model.Es102G2]: eS102G2,
 };
