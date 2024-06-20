8622edb712d6e3ea60c50d26a5d06e6d43c34b04
Denys Butenko <ua13dark@gmail.com>
2024-06-20 13:31:20 +0700

    NAS-129624 / 24.10 / Fix Cloud Sync Bandwidth limit increases on every task edit (#10209)
    * NAS-129624: Fix Cloud Sync Bandwidth limit increases on every task edit
    
    * NAS-129624: Fix Cloud Sync Bandwidth limit increases on every task edit
    
    * NAS-129624: Fix Cloud Sync Bandwidth limit increases on every task edit
    
    * NAS-129624: Fix Cloud Sync Bandwidth limit increases on every task edit
    
    * NAS-129624: Fix Cloud Sync Bandwidth limit increases on every task edit
    
    * NAS-129624: Fix Cloud Sync Bandwidth limit increases on every task edit

---
+++ src/app/helpers/bwlimit.utils.spec.ts
@@ -0,0 +1,48 @@
+import { prepareBwlimit } from './bwlimit.utils';
+
+describe('prepareBwlimit', () => {
+  it('should return an empty array if bwlimit is undefined', () => {
+    const bwlimit: string[] = undefined;
+    const result = prepareBwlimit(bwlimit);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an empty array if bwlimit is an empty array', () => {
+    const bwlimit: string[] = [];
+    const result = prepareBwlimit(bwlimit);
+    expect(result).toEqual([]);
+  });
+
+  it('should add default time if bwlimit has only one limit without time', () => {
+    const bwlimit: string[] = ['100'];
+    const result = prepareBwlimit(bwlimit);
+    expect(result).toEqual([{ time: '00:00', bandwidth: '100' }]);
+  });
+
+  it('should convert bandwidth to bytes if it ends with "/s"', () => {
+    const bwlimit: string[] = ['10M'];
+    const result = prepareBwlimit(bwlimit);
+    expect(result).toEqual([{ time: '00:00', bandwidth: '10485760' }]);
+  });
+
+  it('should convert bandwidth to bytes if it ends with "/S"', () => {
+    const bwlimit: string[] = ['10MB/S'];
+    const result = prepareBwlimit(bwlimit);
+    expect(result).toEqual([{ time: '00:00', bandwidth: '10485760' }]);
+  });
+
+  it('should return null bandwidth if it is "off"', () => {
+    const bwlimit: string[] = ['00:00, off'];
+    const result = prepareBwlimit(bwlimit);
+    expect(result).toEqual([{ time: '00:00', bandwidth: null }]);
+  });
+
+  it('should return an array of BwLimitUpdate objects', () => {
+    const bwlimit: string[] = ['00:00, 10MB/s', '12:00, 5GB/s'];
+    const result = prepareBwlimit(bwlimit);
+    expect(result).toEqual([
+      { time: '00:00', bandwidth: '10485760' },
+      { time: '12:00', bandwidth: '5368709120' },
+    ]);
+  });
+});
