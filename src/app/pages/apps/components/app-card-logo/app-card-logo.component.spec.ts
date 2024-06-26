e7990c58624a704a58bb3e5445b5d84fbe9f1f4c
Denys Butenko <ua13dark@gmail.com>
2024-06-26 21:38:52 +0700

    NAS-129427: Implement App widget

--- src/app/pages/apps/components/app-card-logo/app-card-logo.component.spec.ts
+++ src/app/pages/apps/components/app-card-logo/app-card-logo.component.spec.ts
@@ -1,6 +1,5 @@
 import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
-import { LazyLoadImageDirective } from 'ng-lazyload-image';
-import { MockDirective } from 'ng-mocks';
+import { LazyLoadImageDirective, LazyLoadImageModule } from 'ng-lazyload-image';
 import { AppCardLogoComponent } from 'app/pages/apps/components/app-card-logo/app-card-logo.component';
 
 describe('AppCardLogoComponent', () => {
@@ -8,9 +7,7 @@ describe('AppCardLogoComponent', () => {
 
   const createComponent = createComponentFactory({
     component: AppCardLogoComponent,
-    declarations: [
-      MockDirective(LazyLoadImageDirective),
-    ],
+    imports: [LazyLoadImageModule],
   });
 
   beforeEach(() => {
