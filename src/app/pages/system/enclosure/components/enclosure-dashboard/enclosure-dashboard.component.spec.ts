661966f10abb3782cbfcff87bb00272b029dfc9e
RehanY147 <rehanyousaf147@gmail.com>
2024-07-09 01:52:22 +0500

    NAS-128920: Update enclosure-dashboard.component.spec.ts

--- src/app/pages/system/enclosure/components/enclosure-dashboard/enclosure-dashboard.component.spec.ts
+++ src/app/pages/system/enclosure/components/enclosure-dashboard/enclosure-dashboard.component.spec.ts
@@ -10,6 +10,7 @@ import {
   EnclosureDashboardComponent,
 } from 'app/pages/system/enclosure/components/enclosure-dashboard/enclosure-dashboard.component';
 import { EnclosureStore } from 'app/pages/system/enclosure/services/enclosure.store';
+import { WebSocketService } from 'app/services/ws.service';
 
 describe('EnclosureDashboardComponent', () => {
   let spectator: SpectatorRouting<EnclosureDashboardComponent>;
@@ -28,6 +29,7 @@ describe('EnclosureDashboardComponent', () => {
       }),
     ],
     providers: [
+      mockProvider(WebSocketService),
       // mockWebSocket([
       //   mockCall('jbof.licensed', 5),
       // ]),
