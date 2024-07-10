8c9ac14fb12fc690caadafdeffa368a55e9a8ca7
RehanY147 <rehanyousaf147@gmail.com>
2024-07-10 18:21:04 +0500

    NAS-128920: Fix code style issue

--- src/test-utils/missing-injection-factories.ts
+++ src/test-utils/missing-injection-factories.ts
@@ -1,7 +1,7 @@
 import { Observable, throwError } from 'rxjs';
 
 export function getMissingInjectionErrorFactory(className: string): () => Observable<unknown> {
-  return function (): Observable<unknown> {
+  return function getMissingInjectionError(): Observable<unknown> {
     return getMissingInjectionErrorObservable(className);
   };
 }
