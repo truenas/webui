aea155feda5569acc90c7a888147a1442c1a2089
Evgeny Stepanovych <undsoft@gmail.com>
2024-07-22 17:17:50 +0200

    NAS-129950: Preset in scheduler does not update with the rest of the form

--- src/app/pages/data-protection/cloud-backup/cloud-backup-form/cloud-backup-form.component.spec.ts
+++ src/app/pages/data-protection/cloud-backup/cloud-backup-form/cloud-backup-form.component.spec.ts
@@ -236,7 +236,7 @@ describe('CloudBackupFormComponent', () => {
         Password: '1234',
         'Post-script': '',
         'Pre-script': '',
-        Schedule: 'Weekly (0 0 * * 0)  On Sundays at 00:00 (12:00 AM)',
+        Schedule: 'Weekly (0 0 * * sun)  On Sundays at 00:00 (12:00 AM)',
         'Source Path': '/mnt/my pool',
         'Take Snapshot': false,
         Transfers: '5',
