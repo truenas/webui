766bba0c48301b8a442a25204bc77de276cbb916
Alex Karpov <akarpov@ixsystems.com>
2024-07-25 12:42:27 +0300

    NAS-127279: ix-table2 Expandable Details Row - Cells are incorrectly rendered, as plain string despite the cell type (#10352)

---
+++ src/app/modules/ix-table/components/ix-table-details-row/ix-table-details-row.component.ts
@@ -0,0 +1,15 @@
+import {
+  Component, ChangeDetectionStrategy, input,
+} from '@angular/core';
+import { Column, ColumnComponent } from 'app/modules/ix-table/interfaces/table-column.interface';
+
+@Component({
+  selector: 'ix-table-details-row',
+  templateUrl: './ix-table-details-row.component.html',
+  styleUrls: ['./ix-table-details-row.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IxTableDetailsRowComponent<T> {
+  readonly hiddenColumns = input<Column<T, ColumnComponent<T>>[]>();
+  readonly row = input<T>();
+}
