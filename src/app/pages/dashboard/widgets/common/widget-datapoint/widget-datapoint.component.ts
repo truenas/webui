0e13bc006aba6145f2a325061273ffbc02485075
Denys Butenko <ua13dark@gmail.com>
2024-07-25 15:51:53 +0700

    NAS-130129: Update datapoint widget and arbitary settings

--- src/app/pages/dashboard/widgets/common/widget-datapoint/widget-datapoint.component.ts
+++ src/app/pages/dashboard/widgets/common/widget-datapoint/widget-datapoint.component.ts
@@ -15,15 +15,15 @@ export class WidgetDatapointComponent {
   text = input<string>();
   subText = input<string>();
 
-  getMaxFontSize(text = ''): number {
+  get maxFontSize(): number {
     const isQuarter = this.size() === SlotSize.Quarter;
     let fontSize = isQuarter ? 15 : 20;
 
-    if (text.length <= 15) {
+    if (this.text().length <= 15) {
       fontSize = isQuarter ? 30 : 49;
-    } else if (text.length <= 30) {
+    } else if (this.text().length <= 30) {
       fontSize = isQuarter ? 20 : 30;
-    } else if (text.length <= 40) {
+    } else if (this.text().length <= 40) {
       fontSize = isQuarter ? 18 : 22;
     }
 
