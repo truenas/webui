465446550ebb98cb120a72b48c2251679d83f485
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-08-15 04:57:25 -0700

    NAS-130296 / 24.10-RC.1 / Update copyright-line (by denysbutenko) (#10463)

--- src/app/modules/layout/components/copyright-line/copyright-line.component.ts
+++ src/app/modules/layout/components/copyright-line/copyright-line.component.ts
@@ -3,8 +3,8 @@ import {
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
-import { map } from 'rxjs';
-import { productTypeLabels } from 'app/enums/product-type.enum';
+import { map, startWith } from 'rxjs';
+import { ProductType, productTypeLabels } from 'app/enums/product-type.enum';
 import { AppState } from 'app/store';
 import { selectBuildYear, selectProductType } from 'app/store/system-info/system-info.selectors';
 
@@ -16,7 +16,10 @@ import { selectBuildYear, selectProductType } from 'app/store/system-info/system
 })
 export class CopyrightLineComponent {
   readonly withIxLogo = input(false);
-  readonly product = toSignal(this.store$.select(selectProductType).pipe(map((type) => productTypeLabels.get(type))));
+  readonly product = toSignal(this.store$.select(selectProductType).pipe(
+    startWith(ProductType.Scale),
+    map((type) => productTypeLabels.get(type)),
+  ));
   readonly copyrightYear = toSignal(this.store$.select(selectBuildYear));
 
   constructor(
