c1245401fa2652ebcde4c602bbf38822ce98b7bb
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-08-18 14:41:27 -0700

    NAS-130332: Restore pod logs (#10474)
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/apps/components/installed-apps/pod-logs/pod-logs.component.ts
+++ src/app/pages/apps/components/installed-apps/pod-logs/pod-logs.component.ts
@@ -4,11 +4,13 @@ import {
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { UUID } from 'angular2-uuid';
-import { combineLatest, map, Subscription } from 'rxjs';
+import {
+  combineLatest, map, Subscription, switchMap, tap,
+} from 'rxjs';
 import { WebSocketError } from 'app/interfaces/websocket-error.interface';
 import { DialogService } from 'app/modules/dialog/dialog.service';
 import { AppLoaderService } from 'app/modules/loader/app-loader.service';
+import { LogsDetailsDialogComponent } from 'app/pages/apps/components/logs-details-dialog/logs-details-dialog.component';
 import { DownloadService } from 'app/services/download.service';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
 import { ShellService } from 'app/services/shell.service';
@@ -35,12 +37,12 @@ export class PodLogsComponent implements OnInit {
   fontSize = 14;
   appName: string;
   containerId: string;
-  podLogSubscriptionId: string = null;
   podLogSubName = '';
   isLoadingPodLogs = false;
+  defaultTailLines = 500;
 
   private podLogsChangedListener: Subscription;
-  podLogs: PodLogEvent[];
+  podLogs: PodLogEvent[] = [];
 
   constructor(
     private ws: WebSocketService,
@@ -66,16 +68,22 @@ export class PodLogsComponent implements OnInit {
 
   // subscribe pod log for selected app, pod and container.
   reconnect(): void {
-    this.podLogs = [];
-    this.isLoadingPodLogs = true;
-
     if (this.podLogsChangedListener && !this.podLogsChangedListener.closed) {
       this.podLogsChangedListener.unsubscribe();
     }
 
-    this.podLogSubName = `app.container_log_follow:{"app_name": "${this.appName}", "container_id": "${this.containerId}"}`;
-    this.podLogSubscriptionId = UUID.UUID();
-    this.podLogsChangedListener = this.ws.subscribeToLogs(this.podLogSubName).pipe(
+    this.podLogsChangedListener = this.matDialog.open(LogsDetailsDialogComponent, { width: '400px' }).afterClosed().pipe(
+      tap((details: LogsDetailsDialogComponent['form']['value']) => {
+        this.podLogSubName = `app.container_log_follow: ${JSON.stringify({
+          app_name: this.appName,
+          container_id: this.containerId,
+          tail_lines: details.tail_lines || this.defaultTailLines,
+        })}`;
+
+        this.podLogs = [];
+        this.isLoadingPodLogs = true;
+      }),
+      switchMap(() => this.ws.subscribeToLogs(this.podLogSubName)),
       map((apiEvent) => apiEvent.fields),
       untilDestroyed(this),
     ).subscribe({
