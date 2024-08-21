a9c3d6b4fdbe99c9edd3f9337f95d6bf84f5c6e8
Alex Karpov <karpov4you@gmail.com>
2024-08-21 10:28:59 +0300

    NAS-130656: Portal group IDs on iSCSI target are shown as incorrect

--- src/app/pages/sharing/iscsi/target/target-form/target-form.component.ts
+++ src/app/pages/sharing/iscsi/target/target-form/target-form.component.ts
@@ -46,14 +46,11 @@ export class TargetFormComponent implements OnInit {
   readonly modes$ = of(this.helptext.target_form_enum_mode);
   readonly portals$ = this.iscsiService.listPortals().pipe(
     map((portals) => {
-      const opts: Option[] = [];
-      portals.forEach((portal) => {
-        let label = String(portal.tag);
-        if (portal.comment) {
-          label += ' (' + portal.comment + ')';
-        }
-        opts.push({ label, value: portal.id });
+      const opts: Option[] = portals.map((portal) => {
+        const label = portal.comment ? `${portal.id} (${portal.comment})` : String(portal.id);
+        return { label, value: portal.id };
       });
+
       return opts;
     }),
   );
