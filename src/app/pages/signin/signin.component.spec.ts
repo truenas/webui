9d10d73bd5f5bb54d3dd393fa07cce4697365b05
Evgeny Stepanovych <undsoft@gmail.com>
2024-09-04 13:48:51 +0200

    NAS-130976: Migrate LayoutModule to standalone components

--- src/app/pages/signin/signin.component.spec.ts
+++ src/app/pages/signin/signin.component.spec.ts
@@ -6,6 +6,7 @@ import { FailoverDisabledReason } from 'app/enums/failover-disabled-reason.enum'
 import { FailoverStatus } from 'app/enums/failover-status.enum';
 import { DialogService } from 'app/modules/dialog/dialog.service';
 import { IxIconModule } from 'app/modules/ix-icon/ix-icon.module';
+import { CopyrightLineComponent } from 'app/modules/layout/copyright-line/copyright-line.component';
 import {
   DisconnectedMessageComponent,
 } from 'app/pages/signin/disconnected-message/disconnected-message.component';
@@ -48,6 +49,7 @@ describe('SigninComponent', () => {
         SetAdminPasswordFormComponent,
         TrueCommandStatusComponent,
         FailoverStatusComponent,
+        CopyrightLineComponent,
       ),
     ],
     componentProviders: [
