7774e52542af2a0c7b9d076e00001508ab4433fb
Boris Vasilenko <bvasilenko@ixsystems.com>
2024-09-05 20:21:36 +0300

    NAS-130645: Remove unnecessary import

--- src/app/pages/storage/modules/pool-manager/components/pool-manager/tests/step-changing.spec.ts
+++ src/app/pages/storage/modules/pool-manager/components/pool-manager/tests/step-changing.spec.ts
@@ -11,7 +11,6 @@ import { DiskType } from 'app/enums/disk-type.enum';
 import { VdevType } from 'app/enums/v-dev-type.enum';
 import { DetailsDisk } from 'app/interfaces/disk.interface';
 import { Enclosure } from 'app/interfaces/enclosure.interface';
-import { IxFormsModule } from 'app/modules/forms/ix-forms/ix-forms.module';
 import { CastPipe } from 'app/modules/pipes/cast/cast.pipe';
 import { FileSizePipe } from 'app/modules/pipes/file-size/file-size.pipe';
 import { MapValuePipe } from 'app/modules/pipes/map-value/map-value.pipe';
@@ -35,7 +34,6 @@ describe('PoolManagerComponent – step changing', () => {
   const createComponent = createComponentFactory({
     component: PoolManagerComponent,
     imports: [
-      IxFormsModule,
       ReactiveFormsModule,
       MatStepperModule,
       FileSizePipe,
