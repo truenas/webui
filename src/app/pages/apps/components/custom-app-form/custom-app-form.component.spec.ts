abc2820c9d77e5657bc716e27ea57b7882323434
Evgeny Stepanovych <undsoft@gmail.com>
2024-09-09 14:05:57 +0200

    NAS-129579: Add custom app form

--- src/app/pages/apps/components/custom-app-form/custom-app-form.component.spec.ts
+++ src/app/pages/apps/components/custom-app-form/custom-app-form.component.spec.ts
@@ -10,11 +10,10 @@ import { mockJob, mockWebSocket } from 'app/core/testing/utils/mock-websocket.ut
 import { CatalogAppState } from 'app/enums/catalog-app-state.enum';
 import { App } from 'app/interfaces/app.interface';
 import { DialogService } from 'app/modules/dialog/dialog.service';
-import { IxCodeEditorComponent } from 'app/modules/forms/ix-forms/components/ix-code-editor/ix-code-editor.component';
 import { IxCodeEditorHarness } from 'app/modules/forms/ix-forms/components/ix-code-editor/ix-code-editor.harness';
-import { IxInputComponent } from 'app/modules/forms/ix-forms/components/ix-input/ix-input.component';
 import { IxInputHarness } from 'app/modules/forms/ix-forms/components/ix-input/ix-input.harness';
 import { IxSlideInRef } from 'app/modules/forms/ix-forms/components/ix-slide-in/ix-slide-in-ref';
+import { IxFormsModule } from 'app/modules/forms/ix-forms/ix-forms.module';
 import { PageHeaderModule } from 'app/modules/page-header/page-header.module';
 import { CustomAppFormComponent } from 'app/pages/apps/components/custom-app-form/custom-app-form.component';
 import { ApplicationsService } from 'app/pages/apps/services/applications.service';
@@ -42,8 +41,7 @@ describe('CustomAppFormComponent', () => {
   const createComponent = createComponentFactory({
     component: CustomAppFormComponent,
     imports: [
-      IxInputComponent,
-      IxCodeEditorComponent,
+      IxFormsModule,
       MockModule(PageHeaderModule),
       ReactiveFormsModule,
     ],
