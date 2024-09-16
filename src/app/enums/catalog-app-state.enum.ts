439cb40715f8202ee1e336ed3fb9ff510b6f870b
Evgeny Stepanovych <undsoft@gmail.com>
2024-09-16 10:03:12 +0200

    NAS-131146: Crashed state for apps

--- src/app/enums/catalog-app-state.enum.ts
+++ src/app/enums/catalog-app-state.enum.ts
@@ -2,10 +2,12 @@ export enum CatalogAppState {
   Running = 'RUNNING',
   Deploying = 'DEPLOYING',
   Stopped = 'STOPPED',
+  Crashed = 'CRASHED',
 }
 
 export const appStateIcons = new Map<CatalogAppState, string>([
   [CatalogAppState.Running, 'mdi-check-circle'],
   [CatalogAppState.Deploying, 'mdi-progress-wrench'],
   [CatalogAppState.Stopped, 'mdi-stop-circle'],
+  [CatalogAppState.Crashed, 'mdi-alert-circle'],
 ]);
