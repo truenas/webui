8ae8beb60c9e38cd0870dfdb6a4656eca2c64815
Boris Vasilenko <bvasilenko@ixsystems.com>
2024-09-17 23:01:43 +0300

    NAS-130955: Add invalidate method

--- src/app/services/global-store/global-store.service.ts
+++ src/app/services/global-store/global-store.service.ts
@@ -1,5 +1,7 @@
 import { inject, Injectable, Type } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  BehaviorSubject, Observable, of, switchMap, tap,
+} from 'rxjs';
 import { ApiCallAndSubscribeMethod, ApiCallAndSubscribeResponse } from 'app/interfaces/api/api-call-and-subscribe-directory.interface';
 import { ApiCallMethod, ApiCallParams, ApiCallResponse } from 'app/interfaces/api/api-call-directory.interface';
 import { ApiEventMethod, ApiEventTyped } from 'app/interfaces/api-message.interface';
@@ -13,6 +15,7 @@ interface GlobalStoreMembers<
   call: Observable<ApiCallResponse<M1>>;
   subscribe: Observable<ApiEventTyped<M2>>;
   callAndSubscribe: Observable<ApiCallAndSubscribeResponse<M3>[]>;
+  invalidate: () => void;
 }
 
 export function globalStore<
@@ -26,10 +29,48 @@ export function globalStore<
   @Injectable({ providedIn: 'root' })
   class GlobalStore implements GlobalStoreMembers<M1, M2, M3> {
     private ws = inject(WebSocketService);
+    private callResult$ = new BehaviorSubject<ApiCallResponse<M1>>(undefined);
+    private subscribeResult$ = new BehaviorSubject<ApiEventTyped<M2>>(undefined);
+    private callAndSubscribeResult$ = new BehaviorSubject<ApiCallAndSubscribeResponse<M3>[]>(undefined);
 
-    call = this.ws.call(method as M1, params as ApiCallParams<M1>);
-    subscribe = this.ws.subscribe(method as M2);
-    callAndSubscribe = this.ws.callAndSubscribe(method as M3, params as ApiCallParams<M3>);
+    call = this.callResult$.pipe(
+      switchMap((callResult) => {
+        if (callResult === undefined) {
+          return this.ws
+            .call(method as M1, params as ApiCallParams<M1>)
+            .pipe(tap((result) => this.callResult$.next(result)));
+        }
+        return of(callResult);
+      }),
+    );
+
+    subscribe = this.subscribeResult$.pipe(
+      switchMap((subscribeResult) => {
+        if (subscribeResult === undefined) {
+          return this.ws
+            .subscribe(method as M2)
+            .pipe(tap((result) => this.subscribeResult$.next(result)));
+        }
+        return of(subscribeResult);
+      }),
+    );
+
+    callAndSubscribe = this.callAndSubscribeResult$.pipe(
+      switchMap((callAndSubscribeResult) => {
+        if (callAndSubscribeResult === undefined) {
+          return this.ws
+            .callAndSubscribe(method as M3, params as ApiCallParams<M3>)
+            .pipe(tap((result) => this.callAndSubscribeResult$.next(result)));
+        }
+        return of(callAndSubscribeResult);
+      }),
+    );
+
+    invalidate(): void {
+      this.callResult$.next(undefined);
+      this.subscribeResult$.next(undefined);
+      this.callAndSubscribeResult$.next(undefined);
+    }
   }
   return GlobalStore;
 }
