db283e7cdc2fcc3dac249e6b16c18dd1d248d82b
Evgeny Stepanovych <undsoft@gmail.com>
2024-09-19 09:03:45 +0200

    NAS-131139 / 25.04 / Updates to Running toggle in services (#10695)

---
+++ src/app/pages/services/components/service-state-column/service-state-column.component.spec.ts
@@ -0,0 +1,101 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatSlideToggleHarness } from '@angular/material/slide-toggle/testing';
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { of } from 'rxjs';
+import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
+import { mockCall, mockWebSocket } from 'app/core/testing/utils/mock-websocket.utils';
+import { ServiceName } from 'app/enums/service-name.enum';
+import { ServiceStatus } from 'app/enums/service-status.enum';
+import { IscsiGlobalSession } from 'app/interfaces/iscsi-global-config.interface';
+import { ServiceRow } from 'app/interfaces/service.interface';
+import { DialogService } from 'app/modules/dialog/dialog.service';
+import {
+  ServiceStateColumnComponent,
+} from 'app/pages/services/components/service-state-column/service-state-column.component';
+import { IscsiService } from 'app/services/iscsi.service';
+import { WebSocketService } from 'app/services/ws.service';
+
+describe('ServiceStateColumnComponent', () => {
+  let spectator: Spectator<ServiceStateColumnComponent>;
+  let toggle: MatSlideToggleHarness;
+  const service = {
+    service: ServiceName.Cifs,
+    state: ServiceStatus.Running,
+  } as ServiceRow;
+  const createComponent = createComponentFactory({
+    component: ServiceStateColumnComponent,
+    providers: [
+      mockProvider(IscsiService, {
+        getGlobalSessions: jest.fn(() => of([])),
+      }),
+      mockWebSocket([
+        mockCall('service.start', true),
+        mockCall('service.stop', true),
+      ]),
+      mockProvider(DialogService, {
+        confirm: jest.fn(() => of(true)),
+      }),
+      mockAuth(),
+    ],
+  });
+
+  beforeEach(async () => {
+    spectator = createComponent();
+    spectator.component.setRow(service);
+
+    const loader = TestbedHarnessEnvironment.loader(spectator.fixture);
+    toggle = await loader.getHarness(MatSlideToggleHarness);
+  });
+
+  it('shows whether service is currently running or not', async () => {
+    expect(await toggle.isChecked()).toBe(true);
+
+    spectator.component.setRow({ ...service, state: ServiceStatus.Stopped });
+    expect(await toggle.isChecked()).toBe(false);
+  });
+
+  describe('stopping a service', () => {
+    it('asks for confirmation when user tries to stop a service', async () => {
+      await toggle.toggle();
+
+      expect(spectator.inject(DialogService).confirm).toHaveBeenCalledWith(
+        expect.objectContaining({
+          message: 'Stop SMB?',
+        }),
+      );
+    });
+
+    it('asks for a different confirmation when user tries to stop iSCSI and there are active sessions', async () => {
+      spectator
+        .inject(IscsiService)
+        .getGlobalSessions
+        .mockReturnValueOnce(of([{ target: '123' }] as IscsiGlobalSession[]));
+
+      spectator.component.setRow({ ...service, service: ServiceName.Iscsi });
+
+      await toggle.toggle();
+
+      expect(spectator.inject(DialogService).confirm).toHaveBeenCalledWith(
+        expect.objectContaining({
+          message: expect.stringContaining('There is an active iSCSI connection.'),
+        }),
+      );
+    });
+
+    it('stops the service when user confirms', async () => {
+      await toggle.toggle();
+
+      expect(spectator.inject(WebSocketService).call).toHaveBeenCalledWith('service.stop', [service.service, { silent: false }]);
+    });
+  });
+
+  describe('starting a service', () => {
+    it('starts the service when user changes toggle on a non-running service', async () => {
+      spectator.component.setRow({ ...service, state: ServiceStatus.Stopped });
+
+      await toggle.toggle();
+
+      expect(spectator.inject(WebSocketService).call).toHaveBeenCalledWith('service.start', [service.service, { silent: false }]);
+    });
+  });
+});
