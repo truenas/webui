03fcbec90f8aad89c52b971006c9cf0230573d0c
Denys Butenko <ua13dark@gmail.com>
2024-09-19 14:09:54 +0700

    NAS-131073: Add app-version helper

--- src/app/pages/apps/components/installed-apps/app-update-cell/app-update-cell.component.ts
+++ src/app/pages/apps/components/installed-apps/app-update-cell/app-update-cell.component.ts
@@ -9,6 +9,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { App } from 'app/interfaces/app.interface';
 import { IxIconModule } from 'app/modules/ix-icon/ix-icon.module';
 import { MapValuePipe } from 'app/modules/pipes/map-value/map-value.pipe';
+import { AppVersionPipe } from 'app/pages/dashboard/widgets/apps/common/utils/app-version.pipe';
 
 @Component({
   selector: 'ix-app-update-cell',
@@ -16,7 +17,7 @@ import { MapValuePipe } from 'app/modules/pipes/map-value/map-value.pipe';
   styleUrls: ['./app-update-cell.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [TranslateModule, MapValuePipe, MatTooltipModule, IxIconModule],
+  imports: [TranslateModule, MapValuePipe, MatTooltipModule, IxIconModule, AppVersionPipe],
 })
 export class AppUpdateCellComponent {
   app = input.required<App>();
