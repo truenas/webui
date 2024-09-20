5e715629044cceb47da771e1d5065562ee8f367a
Evgeny Stepanovych <undsoft@gmail.com>
2024-09-20 18:25:47 +0200

    NAS-131228: Fix validate-translations CI job (#10692)

--- scripts/validate_translations.js
+++ scripts/validate_translations.js
@@ -21,18 +21,16 @@ fs.readdir(translationDir, function (err, files) {
     const messages = JSON.parse(fs.readFileSync(translationDir + file, { encoding: 'utf-8' }));
 
     // Validate line by line because it gives better error messages
+    let line = 2; // First line is opening bracket
     Object.entries(messages).forEach(([key, translation]) => {
       try {
         parse(key);
         parse(translation);
+        line++;
       } catch (error) {
         hadErrors = true;
 
-        if (error.location) {
-          console.error(`${language}.json, line ${error.location.start.line}: ${error.message}`);
-        } else {
-          console.error(`${language}.json: ${error.message}`);
-        }
+        console.error(`${language}.json, line ${line}: ${error.message}`);
       }
     });
   });
