7eb4baabf7d1a1b1e9b92e8c5fc33451c312a9aa
Evgeny Stepanovych <undsoft@gmail.com>
2024-09-22 11:07:23 +0200

    NAS-131273: Update packages

--- src/app/modules/ix-tree/testing/tree-node.harness.ts
+++ src/app/modules/ix-tree/testing/tree-node.harness.ts
@@ -1,6 +1,6 @@
 import { coerceBooleanProperty, coerceNumberProperty } from '@angular/cdk/coercion';
 import { ComponentHarnessConstructor, ContentContainerComponentHarness, HarnessPredicate } from '@angular/cdk/testing';
-import { TreeNodeHarnessFilters } from '@angular/material/tree/testing';
+import { MatTreeNodeHarness, TreeNodeHarnessFilters } from '@angular/material/tree/testing';
 
 function getNodePredicate<T extends TreeNodeHarness>(
   type: ComponentHarnessConstructor<T>,
@@ -25,56 +25,20 @@ function getNodePredicate<T extends TreeNodeHarness>(
     );
 }
 
-export class TreeNodeHarness extends ContentContainerComponentHarness {
-  static hostSelector = '.ix-tree-node, .ix-nested-tree-node';
-  _toggle = this.locatorForOptional('[treeNodeToggle]');
+export class TreeNodeHarness extends MatTreeNodeHarness {
+  static override hostSelector = '.ix-tree-node, .ix-nested-tree-node';
+  override _toggle = this.locatorForOptional('[treeNodeToggle]');
 
   /**
    * Gets a `HarnessPredicate` that can be used to search for a tree node with specific attributes.
    * @param options Options for narrowing the search
    * @return a `HarnessPredicate` configured with the given options.
    */
-  static with(options: TreeNodeHarnessFilters = {}): HarnessPredicate<TreeNodeHarness> {
+  static override with(options: TreeNodeHarnessFilters = {}): HarnessPredicate<TreeNodeHarness> {
     return getNodePredicate(TreeNodeHarness, options);
   }
 
-  async isExpanded(): Promise<boolean> {
-    return coerceBooleanProperty(await (await this.host()).getAttribute('aria-expanded'));
-  }
-
-  async isDisabled(): Promise<boolean> {
-    return coerceBooleanProperty(await (await this.host()).getProperty('aria-disabled'));
-  }
-
-  async getLevel(): Promise<number> {
-    return coerceNumberProperty(await (await this.host()).getAttribute('aria-level'));
-  }
-
-  async getText(): Promise<string> {
+  override async getText(): Promise<string> {
     return (await this.host()).text({ exclude: '.ix-tree-node, .ix-nested-tree-node, button' });
-  }
-
-  /** Toggles node between expanded/collapsed. Only works when node is not disabled. */
-  async toggle(): Promise<void> {
-    const toggle = await this._toggle();
-    if (toggle) {
-      return toggle.click();
-    }
-
-    return Promise.resolve();
-  }
-
-  /** Expands the node if it is collapsed. Only works when node is not disabled. */
-  async expand(): Promise<void> {
-    if (!(await this.isExpanded())) {
-      await this.toggle();
-    }
-  }
-
-  /** Collapses the node if it is expanded. Only works when node is not disabled. */
-  async collapse(): Promise<void> {
-    if (await this.isExpanded()) {
-      await this.toggle();
-    }
   }
 }
