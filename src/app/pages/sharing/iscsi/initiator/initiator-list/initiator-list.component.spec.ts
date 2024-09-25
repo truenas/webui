cc3d8505f2457688687f9339b960147d8807e9ec
Evgeny Stepanovych <undsoft@gmail.com>
2024-09-25 15:24:09 +0200

    NAS-131364: Move more components to standalone

--- src/app/pages/sharing/iscsi/initiator/initiator-list/initiator-list.component.spec.ts
+++ src/app/pages/sharing/iscsi/initiator/initiator-list/initiator-list.component.spec.ts
@@ -18,6 +18,7 @@ import { IxTableHarness } from 'app/modules/ix-table/components/ix-table/ix-tabl
 import {
   IxTableColumnsSelectorComponent,
 } from 'app/modules/ix-table/components/ix-table-columns-selector/ix-table-columns-selector.component';
+import { FakeProgressBarComponent } from 'app/modules/loader/components/fake-progress-bar/fake-progress-bar.component';
 import { InitiatorListComponent } from 'app/pages/sharing/iscsi/initiator/initiator-list/initiator-list.component';
 import { IxSlideInService } from 'app/services/ix-slide-in.service';
 import { WebSocketService } from 'app/services/ws.service';
@@ -39,6 +40,7 @@ describe('InitiatorListComponent', () => {
     imports: [
       SearchInput1Component,
       IxTableColumnsSelectorComponent,
+      FakeProgressBarComponent,
     ],
     providers: [
       mockAuth(),
