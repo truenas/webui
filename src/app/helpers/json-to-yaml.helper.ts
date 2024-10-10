07149afaf64c7cc9f611c6cd8bc2556c825daeac
Denys Butenko <ua13dark@gmail.com>
2024-10-10 19:31:39 +0700

    NAS-131147 / 25.04 / Allow to edit yaml installed apps (#10804)
    * NAS-131147: Allow to edit yaml installed apps
    
    * NAS-131147: Redirect to installed apps after custom app install
    
    * NAS-131147: Use custom app form component to edit custom apps
    
    * NAS-131147: Reload installed apps when custom app installed
    
    * NAS-131147: Reload installed app when custom app is changed
    
    * NAS-131147: Reduce delay

--- src/app/helpers/json-to-yaml.helper.ts
+++ src/app/helpers/json-to-yaml.helper.ts
@@ -1,5 +1,5 @@
 import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
-import { dump } from 'js-yaml';
+import { dump, load } from 'js-yaml';
 
 export function jsonToYaml(jsonData: unknown): string {
   try {
@@ -13,6 +13,15 @@ export function jsonToYaml(jsonData: unknown): string {
         '!!map': 'camelcase',
       },
     });
+  } catch (error) {
+    console.error(error);
+    return T('Error occurred');
+  }
+}
+
+export function yamlToJson(value: string): unknown {
+  try {
+    return load(value);
   } catch (error) {
     console.error(error);
     return T('Error occurred');
