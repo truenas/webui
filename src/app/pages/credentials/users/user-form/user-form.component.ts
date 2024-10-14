31be2ea8a8596396f9323b211ac7aff0af7eb004
undsoft <undsoft@gmail.com>
2024-10-14 18:33:18 +0200

    NAS-128039: Disable toggle for SMB authentication for built-in users

--- src/app/pages/credentials/users/user-form/user-form.component.ts
+++ src/app/pages/credentials/users/user-form/user-form.component.ts
@@ -104,6 +104,10 @@ export class UserFormComponent implements OnInit {
     return this.isNewUser ? this.translate.instant('Add User') : this.translate.instant('Edit User');
   }
 
+  get isEditingBuiltinUser(): boolean {
+    return !this.isNewUser && this.editingUser.builtin;
+  }
+
   form = this.fb.group({
     full_name: ['', [Validators.required]],
     username: ['', [
@@ -163,6 +167,7 @@ export class UserFormComponent implements OnInit {
     shell: helptextUsers.user_form_shell_tooltip,
     locked: helptextUsers.user_form_lockuser_tooltip,
     smb: helptextUsers.user_form_smb_tooltip,
+    smbBuiltin: helptextUsers.smbBuiltin,
   };
 
   readonly groupOptions$ = this.ws.call('group.query').pipe(
@@ -449,6 +454,10 @@ export class UserFormComponent implements OnInit {
       this.form.controls.home.disable();
       this.form.controls.home_create.disable();
       this.form.controls.username.disable();
+    }
+
+    if (user.builtin) {
+      this.form.controls.smb.disable();
     }
 
     this.setNamesInUseValidator(user.username);
