1fb3a32c825d635249dbf08372688d2e0abde61f
Denys Butenko <ua13dark@gmail.com>
2024-10-15 21:33:44 +0700

    NAS-131407: adapt feedback

--- src/app/services/bottom-sheet.service.ts
+++ src/app/services/bottom-sheet.service.ts
@@ -1,6 +1,7 @@
 import { ComponentType } from '@angular/cdk/portal';
 import { Injectable } from '@angular/core';
 import { MatBottomSheet, MatBottomSheetConfig, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { AuthService } from 'app/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,16 @@ import { MatBottomSheet, MatBottomSheetConfig, MatBottomSheetRef } from '@angula
 export class BottomSheetService {
   private bottomSheetRef: MatBottomSheetRef;
 
-  constructor(private bottomSheet: MatBottomSheet) {}
+  constructor(
+    private bottomSheet: MatBottomSheet,
+    private authService: AuthService,
+  ) {
+    this.authService.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (!isAuthenticated && this.bottomSheetRef) {
+        this.bottomSheetRef.dismiss();
+      }
+    });
+  }
 
   open<T, D, R>(
     component: ComponentType<T>,
