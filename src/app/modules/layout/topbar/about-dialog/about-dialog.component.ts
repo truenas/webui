4ae75d22d32599dd27b754f0c28596815a8fa328
Alex Karpov <akarpov@ixsystems.com>
2024-10-24 15:56:39 +0300

    NAS-127228 / 25.04 / Add system version to About dialog (#10910)
    * NAS-127228: Add system version to About dialog
    
    * NAS-127228: PR Update

--- src/app/modules/layout/topbar/about-dialog/about-dialog.component.ts
+++ src/app/modules/layout/topbar/about-dialog/about-dialog.component.ts
@@ -1,7 +1,10 @@
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatDialogRef, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
+import { map } from 'rxjs';
 import { LetDirective } from 'app/directives/app-let.directive';
 import { productTypeLabels } from 'app/enums/product-type.enum';
 import { helptextAbout } from 'app/helptext/about';
@@ -10,6 +13,8 @@ import { CopyrightLineComponent } from 'app/modules/layout/copyright-line/copyri
 import { MapValuePipe } from 'app/modules/pipes/map-value/map-value.pipe';
 import { TestDirective } from 'app/modules/test-id/test.directive';
 import { SystemGeneralService } from 'app/services/system-general.service';
+import { AppState } from 'app/store';
+import { selectSystemInfoState } from 'app/store/system-info/system-info.selectors';
 
 @Component({
   selector: 'ix-about-dialog',
@@ -27,15 +32,18 @@ import { SystemGeneralService } from 'app/services/system-general.service';
     MapValuePipe,
     LetDirective,
     TestDirective,
+    AsyncPipe,
   ],
 })
 export class AboutDialogComponent {
   systemType = this.systemGeneralService.getProductType();
+  readonly systemVersion$ = this.store$.select(selectSystemInfoState).pipe(map((system) => system.systemInfo?.version));
   readonly helptext = helptextAbout;
   readonly productTypeLabels = productTypeLabels;
 
   constructor(
     public dialogRef: MatDialogRef<AboutDialogComponent>,
     private systemGeneralService: SystemGeneralService,
+    private store$: Store<AppState>,
   ) {}
 }
