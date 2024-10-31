01943d59b8452951875de44745692d2f94021459
Alex Karpov <karpov4you@gmail.com>
2024-10-31 17:34:59 +0200

    NAS-132095: Initial

--- src/app/pages/virtualization/components/create-instance-form/create-instance-form.component.ts
+++ src/app/pages/virtualization/components/create-instance-form/create-instance-form.component.ts
@@ -7,15 +7,22 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { map } from 'rxjs';
 import { RequiresRolesDirective } from 'app/directives/requires-roles/requires-roles.directive';
-import { VirtualizationRemote } from 'app/enums/virtualization.enum';
 import {
+  VirtualizationDeviceType, VirtualizationGpuType, VirtualizationRemote, VirtualizationType,
+} from 'app/enums/virtualization.enum';
+import {
+  AvailableGpu,
+  AvailableUsb,
   CreateVirtualizationInstance,
+  VirtualizationDevice,
 } from 'app/interfaces/virtualization.interface';
 import { DialogService } from 'app/modules/dialog/dialog.service';
 import { IxCheckboxComponent } from 'app/modules/forms/ix-forms/components/ix-checkbox/ix-checkbox.component';
 import { IxFieldsetComponent } from 'app/modules/forms/ix-forms/components/ix-fieldset/ix-fieldset.component';
 import { IxInputComponent } from 'app/modules/forms/ix-forms/components/ix-input/ix-input.component';
+import { IxSelectComponent } from 'app/modules/forms/ix-forms/components/ix-select/ix-select.component';
 import { FormErrorHandlerService } from 'app/modules/forms/ix-forms/services/form-error-handler.service';
 import { IxFormatterService } from 'app/modules/forms/ix-forms/services/ix-formatter.service';
 import { PageHeaderComponent } from 'app/modules/page-header/page-title-header/page-header.component';
@@ -40,6 +47,7 @@ import { WebSocketService } from 'app/services/ws.service';
     RequiresRolesDirective,
     TestDirective,
     IxFieldsetComponent,
+    IxSelectComponent,
   ],
   templateUrl: './create-instance-form.component.html',
   styleUrls: ['./create-instance-form.component.scss'],
@@ -48,9 +56,31 @@ import { WebSocketService } from 'app/services/ws.service';
 export class CreateInstanceFormComponent {
   protected readonly isLoading = signal(false);
 
+  usbDevices$ = this.ws.call('virt.device.usb_choices').pipe(
+    map((choices: Record<string, AvailableUsb>) => Object.values(choices).map((choice) => ({
+      label: choice.product,
+      value: choice.product_id,
+    }))),
+    untilDestroyed(this),
+  );
+
+  // TODO: MV supports only [Container, Physical] for now (based on the response)
+  gpuDevices$ = this.ws.call(
+    'virt.device.gpu_choices',
+    [VirtualizationType.Container, VirtualizationGpuType.Physical],
+  ).pipe(
+    map((choices: Record<string, AvailableGpu>) => Object.values(choices).map((choice) => ({
+      label: choice.description,
+      value: choice.vendor,
+    }))),
+    untilDestroyed(this),
+  );
+
   protected readonly form = this.formBuilder.nonNullable.group({
     name: ['', Validators.required],
     cpu: ['', Validators.required],
+    usb_devices: [null as string[]],
+    gpu_devices: [null as string[]],
     autostart: [false],
     memory: [null as number, Validators.required],
     image: ['', Validators.required],
@@ -91,13 +121,11 @@ export class CreateInstanceFormComponent {
   }
 
   protected onSubmit(): void {
-    const values = this.form.value;
+    const payload = this.getPayload();
+    const job$ = this.ws.job('virt.instance.create', [payload]);
 
-    const job$ = this.ws.job('virt.instance.create', [values as CreateVirtualizationInstance]);
-
-    this.dialogService.jobDialog(job$, {
-      title: this.translate.instant('Saving Instance'),
-    })
+    this.dialogService
+      .jobDialog(job$, { title: this.translate.instant('Saving Instance') })
       .afterClosed()
       .pipe(untilDestroyed(this))
       .subscribe({
@@ -109,5 +137,23 @@ export class CreateInstanceFormComponent {
           this.formErrorHandler.handleWsFormError(error, this.form);
         },
       });
+  }
+
+  private getPayload(): CreateVirtualizationInstance {
+    return {
+      name: this.form.controls.name.value,
+      cpu: this.form.controls.cpu.value,
+      autostart: this.form.controls.autostart.value,
+      memory: this.form.controls.memory.value,
+      image: this.form.controls.image.value,
+      devices: [
+        ...(this.form.controls.usb_devices.value || []).map((productId) => ({
+          dev_type: VirtualizationDeviceType.Usb, product_id: productId,
+        })),
+        ...(this.form.controls.gpu_devices.value || []).map((gpuType) => ({
+          dev_type: VirtualizationDeviceType.Gpu, gpu_type: gpuType,
+        })),
+      ] as VirtualizationDevice[],
+    } as CreateVirtualizationInstance;
   }
 }
