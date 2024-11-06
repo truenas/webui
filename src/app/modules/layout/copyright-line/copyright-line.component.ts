948188e1a70a9ea403978af402ab133628f47a63
Denys Butenko <ua13dark@gmail.com>
2024-11-06 19:31:45 +0700

    NAS-132171: Update initial value for productType

--- src/app/modules/layout/copyright-line/copyright-line.component.ts
+++ src/app/modules/layout/copyright-line/copyright-line.component.ts
@@ -3,8 +3,8 @@ import {
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
-import { map, startWith } from 'rxjs';
-import { ProductType, productTypeLabels } from 'app/enums/product-type.enum';
+import { map } from 'rxjs';
+import { productTypeLabels } from 'app/enums/product-type.enum';
 import { TestDirective } from 'app/modules/test-id/test.directive';
 import { AppState } from 'app/store';
 import { selectBuildYear, selectProductType } from 'app/store/system-info/system-info.selectors';
@@ -20,7 +20,6 @@ import { selectBuildYear, selectProductType } from 'app/store/system-info/system
 export class CopyrightLineComponent {
   readonly withIxLogo = input(false);
   readonly product = toSignal(this.store$.select(selectProductType).pipe(
-    startWith(ProductType.Scale),
     map((type) => productTypeLabels.get(type)),
   ));
 
