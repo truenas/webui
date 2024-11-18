06eb4d01198fed7e19b585db6030e0d82912577e
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-11-18 05:48:10 -0800

    NAS-132517 / 24.10.1 / Cloud credentials storJ text does not render link correctly (by AlexKarpov98) (#11061)
    * NAS-132517: PR update
    
    ---------
    
    Co-authored-by: Alex Karpov <akarpov@ixsystems.com>

--- src/app/pages/data-protection/cloudsync/cloudsync-provider-description/cloudsync-provider-description.component.spec.ts
+++ src/app/pages/data-protection/cloudsync/cloudsync-provider-description/cloudsync-provider-description.component.spec.ts
@@ -17,8 +17,13 @@ describe('CloudSyncProviderDescriptionComponent', () => {
   it('should have the correct details for Storj', () => {
     spectator.setInput('provider', CloudSyncProviderName.Storj);
 
-    expect(spectator.component.image).toBe('/assets/images/cloudsync/STORJ_IX.png');
-    expect(spectator.component.name).toBe('Storj');
-    expect(spectator.component.description).toBe('StorJ is an S3 compatible, fault tolerant, globally distributed cloud storage platform with a security first approach to backup and recovery - delivering extreme resilience and performance both sustainably and economically. <a href="https://truenas.com/storj" target="_blank">TrueNAS and Storj</a> have partnered to streamline delivery of Hybrid Cloud solutions globally.');
+    const image = spectator.query('.image img');
+    expect(image).toHaveAttribute('src', '/assets/images/cloudsync/STORJ_IX.png');
+
+    const name = spectator.query('.name');
+    expect(name).toHaveText('Storj');
+
+    const description = spectator.query('.body');
+    expect(description).not.toContain('<a href');
   });
 });
