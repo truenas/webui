4fa7de7a98bac20b5aeb2eb2aa349b5b5bddefbf
Evgeny Stepanovych <undsoft@gmail.com>
2024-11-23 09:29:03 +0100

    NAS-132641: Validation errors are not shown on corresponding form elements (#11074)

--- src/app/modules/forms/ix-forms/components/ix-checkbox/ix-checkbox.component.ts
+++ src/app/modules/forms/ix-forms/components/ix-checkbox/ix-checkbox.component.ts
@@ -1,6 +1,5 @@
 import {
-  AfterViewInit,
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnDestroy,
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, Input,
 } from '@angular/core';
 import {
   ControlValueAccessor, NgControl,
@@ -10,7 +9,6 @@ import { MatHint } from '@angular/material/form-field';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { IxErrorsComponent } from 'app/modules/forms/ix-forms/components/ix-errors/ix-errors.component';
 import { WarningComponent } from 'app/modules/forms/ix-forms/components/warning/warning.component';
-import { IxFormService } from 'app/modules/forms/ix-forms/services/ix-form.service';
 import { TestDirective } from 'app/modules/test-id/test.directive';
 import { TooltipComponent } from 'app/modules/tooltip/tooltip.component';
 
@@ -30,7 +28,7 @@ import { TooltipComponent } from 'app/modules/tooltip/tooltip.component';
     TestDirective,
   ],
 })
-export class IxCheckboxComponent implements ControlValueAccessor, AfterViewInit, OnDestroy {
+export class IxCheckboxComponent implements ControlValueAccessor {
   @Input() label: string;
   @Input() hint: string;
   @Input() tooltip: string;
@@ -43,18 +41,8 @@ export class IxCheckboxComponent implements ControlValueAccessor, AfterViewInit,
   constructor(
     public controlDirective: NgControl,
     private cdr: ChangeDetectorRef,
-    private formService: IxFormService,
-    private elementRef: ElementRef<HTMLElement>,
   ) {
     this.controlDirective.valueAccessor = this;
-  }
-
-  ngAfterViewInit(): void {
-    this.formService.registerControl(this.controlDirective, this.elementRef);
-  }
-
-  ngOnDestroy(): void {
-    this.formService.unregisterControl(this.controlDirective);
   }
 
   onChange: (value: boolean) => void = (): void => {};
