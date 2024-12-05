5275e76469500c0012a2de72b14a8cdf4ec850e0
Alex Karpov <karpov4you@gmail.com>
2024-12-05 14:39:23 +0200

    NAS-132779: PR Update

--- src/app/interfaces/fibre-channel.interface.ts
+++ src/app/interfaces/fibre-channel.interface.ts
@@ -1,11 +1,18 @@
-import { IscsiTarget } from 'app/interfaces/iscsi.interface';
-
 export interface FibreChannelPort {
   id: number;
   port: string;
   wwpn: string | null;
   wwpn_b: string | null;
-  target: IscsiTarget;
+  target: FibreChannelTarget;
+}
+
+export interface FibreChannelTarget {
+  id: number;
+  iscsi_target_name: string;
+  iscsi_target_alias: string | null;
+  iscsi_target_mode: string;
+  iscsi_target_auth_networks: string[];
+  iscsi_target_rel_tgt_id: number;
 }
 
 export interface FibreChannelPortUpdate {
