7392d5c5ea4953bb710235d81e2a35ce885d2d31
RehanY147 <rehanyousaf147@gmail.com>
2024-12-07 05:46:55 +0500

    NAS-132300: Create ix-form-glossary.component.ts

---
+++ src/app/modules/forms/ix-forms/components/ix-form-glossary/ix-form-glossary.component.ts
@@ -0,0 +1,187 @@
+/* eslint-disable sonarjs/no-dead-store, sonarjs/sonar-no-unused-vars, unused-imports/no-unused-vars */
+
+import { AsyncPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
+import {
+  FormBuilder, NgControl, ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { TranslateModule } from '@ngx-translate/core';
+import { Subscription, timer } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+} from 'rxjs/operators';
+import { RequiresRolesDirective } from 'app/directives/requires-roles/requires-roles.directive';
+import { Option } from 'app/interfaces/option.interface';
+import { IxFormSectionComponent } from 'app/modules/forms/ix-forms/components/ix-form-section/ix-form-section.component';
+import { IxInputComponent } from 'app/modules/forms/ix-forms/components/ix-input/ix-input.component';
+import { ReadOnlyComponent } from 'app/modules/forms/ix-forms/components/readonly-badge/readonly-badge.component';
+import { IxFormService } from 'app/modules/forms/ix-forms/services/ix-form.service';
+import { iconMarker } from 'app/modules/ix-icon/icon-marker.util';
+import { IxIconComponent } from 'app/modules/ix-icon/ix-icon.component';
+import { PageHeaderComponent } from 'app/modules/page-header/page-title-header/page-header.component';
+import { TestDirective } from 'app/modules/test-id/test.directive';
+import { AppMetadataCardComponent } from 'app/pages/apps/components/installed-apps/app-metadata-card/app-metadata-card.component';
+
+@UntilDestroy()
+@Component({
+  selector: 'ix-form-glossary',
+  templateUrl: './ix-form-glossary.component.html',
+  styleUrls: ['./ix-form-glossary.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    PageHeaderComponent,
+    ReadOnlyComponent,
+    IxInputComponent,
+    AppMetadataCardComponent,
+    MatButton,
+    RequiresRolesDirective,
+    AsyncPipe,
+    TranslateModule,
+    TestDirective,
+    ReactiveFormsModule,
+    IxIconComponent,
+  ],
+})
+export class IxFormGlossaryComponent implements OnInit {
+  protected searchControl = this.formBuilder.control('');
+  protected searchOptions = signal<Option[]>([]);
+  protected iconMarker = iconMarker;
+
+  protected sections = signal<IxFormSectionComponent[]>([]);
+  protected sectionsValidity = new Map<IxFormSectionComponent, boolean>();
+  private sectionControlsSubscriptions = new Map<
+    IxFormSectionComponent,
+    Record<string, Subscription>
+  >();
+
+  private sectionControlsValidities = new Map<
+    IxFormSectionComponent,
+    Record<string, boolean>
+  >();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private formService: IxFormService,
+    private cdr: ChangeDetectorRef,
+  ) {
+    this.formService.controlNamesWithlabels$.pipe(
+      map((controlsWithLabels) => controlsWithLabels.map(
+        (nameWithLabel) => ({ label: nameWithLabel.label, value: nameWithLabel.name }),
+      )),
+      untilDestroyed(this),
+    ).subscribe({
+      next: (options) => this.searchOptions.set(options),
+    });
+    this.formService.controlSections$.pipe(
+      untilDestroyed(this),
+    ).subscribe({
+      next: (sectionsWithControls) => {
+        for (const { section, controls } of sectionsWithControls) {
+          this.resetSubscriptionsAndValidities(section);
+
+          this.initializeSectionValidity(
+            section,
+            Array.from(controls).map(([, control]) => control),
+          );
+
+          const sectionControlSubscriptions = this.sectionControlsSubscriptions.get(section) || {};
+          for (const [name, control] of controls) {
+            this.setControlValidity(section, name, control ? control.valid : true);
+
+            sectionControlSubscriptions[name] = control?.statusChanges.pipe(
+              untilDestroyed(this),
+            ).subscribe({
+              next: () => {
+                this.setControlValidity(section, name, control.valid);
+              },
+            });
+          }
+          this.sectionControlsSubscriptions.set(section, sectionControlSubscriptions);
+        }
+        this.sections.set(sectionsWithControls.map(({ section }) => section));
+      },
+    });
+  }
+
+  private initializeSectionValidity(section: IxFormSectionComponent, controls: NgControl[]): void {
+    const isAtLeastOneControlInvalid = controls.some((control) => {
+      if (!control) {
+        return false;
+      }
+      return !control.valid;
+    });
+    const isSectionValid = !isAtLeastOneControlInvalid;
+    this.sectionsValidity.set(section, isSectionValid);
+  }
+
+  private setControlValidity(
+    section: IxFormSectionComponent,
+    control: string,
+    valid: boolean,
+  ): void {
+    const sectionControlValidities = this.sectionControlsValidities.get(section) || {};
+    sectionControlValidities[control] = valid;
+
+    this.sectionControlsValidities.set(section, sectionControlValidities);
+    this.sectionsValidity.set(section, Object.values(sectionControlValidities).every(Boolean));
+    this.cdr.markForCheck();
+  }
+
+  private resetSubscriptionsAndValidities(section: IxFormSectionComponent): void {
+    this.sectionControlsValidities.set(section, {});
+    for (const subscription of Object.values(this.sectionControlsSubscriptions.get(section) || {})) {
+      subscription?.unsubscribe();
+    }
+    this.sectionControlsSubscriptions.set(section, {});
+  }
+
+  protected isSectionValid(section: IxFormSectionComponent): boolean {
+    const valid = this.sectionsValidity.get(section);
+    return this.sectionsValidity.get(section);
+  }
+
+  ngOnInit(): void {
+    this.handleSearchControl();
+  }
+
+  protected onSectionClick(id: string, label: string = null): void {
+    const nextElement = this.formService.getElementByControlName(id)
+      || this.formService.getElementByLabel(label);
+    if (!nextElement) {
+      return;
+    }
+
+    nextElement?.scrollIntoView({ block: 'center' });
+    nextElement.classList.add('highlighted');
+
+    timer(999)
+      .pipe(untilDestroyed(this))
+      .subscribe(() => nextElement.classList.remove('highlighted'));
+  }
+
+  private handleSearchControl(): void {
+    this.searchControl.valueChanges.pipe(
+      debounceTime(100),
+      distinctUntilChanged(),
+      untilDestroyed(this),
+    ).subscribe((value: string) => {
+      const option = this.searchOptions().find((opt) => opt.value === value)
+        || this.searchOptions().find((opt) => opt.label.toLocaleLowerCase() === value.toLocaleLowerCase());
+
+      if (option) {
+        this.onSectionClick(option.value.toString(), option.label);
+      }
+    });
+  }
+}
