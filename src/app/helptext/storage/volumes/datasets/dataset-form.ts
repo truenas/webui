e95082002785060ceb86897ec193e7bff3f42298
undsoft <undsoft@gmail.com>
2024-12-15 17:04:58 +0100

    NAS-132458: Expose dedup_table* fields in UI

--- src/app/helptext/storage/volumes/datasets/dataset-form.ts
+++ src/app/helptext/storage/volumes/datasets/dataset-form.ts
@@ -58,7 +58,7 @@ export const helptextDatasetForm = {
  a one-way process. <b>Deduplicated data cannot be undeduplicated!</b>.'),
   dataset_form_deduplication_warning: T('This feature is memory-intensive and <b>permanently affects how the data is stored</b>. It is recommended to be very familiar with the benefits and drawbacks of deduplication before activating this feature.'),
 
-  deduplicationWarning: T('Deduplication is experimental in 24.10 and not fully supported. When enabled, data is permanently stored with this memory-intensive method and cannot be undone. Take extreme caution and ensure you have adequate data backups before enabling this feature.'),
+  deduplicationWarning: T('Deduplication is experimental and not fully supported. When enabled, data is permanently stored with this memory-intensive method and cannot be undone. Take extreme caution and ensure you have adequate data backups before enabling this feature.'),
   deduplicationChecksumWarning: T(`The default "Checksum" value for datasets with deduplication used to be SHA256.
        Our testing has shown that SHA512 performs better for such datasets.
        We've changed the checksum value from SHA256 to SHA512. You can change it back in "Advanced Options".`),
