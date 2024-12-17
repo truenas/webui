90fe3fa4d0eb965ceba1a1ed7e7c0aad7942023f
Alex Karpov <karpov4you@gmail.com>
2024-12-17 14:15:59 +0200

    NAS-132965: Shell authenticator option for ACME DNS produces invalid fields

--- src/app/pages/credentials/certificates-dash/forms/acmedns-form/acmedns-form.component.ts
+++ src/app/pages/credentials/certificates-dash/forms/acmedns-form/acmedns-form.component.ts
@@ -141,20 +141,19 @@ export class AcmednsFormComponent implements OnInit {
   private createAuthenticatorControls(schemas: AuthenticatorSchema[]): void {
     schemas.forEach((schema) => {
       Object.values(schema.schema.properties).forEach((input) => {
-        this.form.controls.attributes.addControl(input._name_, new FormControl('', input._required_ ? [Validators.required] : []));
+        this.form.controls.attributes.addControl(
+          input._name_,
+          new FormControl(input.const || '', input._required_ ? [Validators.required] : []),
+        );
       });
     });
 
     this.dynamicSection = [{
       name: '',
       description: '',
-      schema: Array.from(
-        new Map(
-          schemas
-            .flatMap((schema) => this.parseSchemaForDynamicSchema(schema))
-            .map((item) => [item.controlName, item]),
-        ).values(),
-      ),
+      schema: schemas
+        .map((schema) => this.parseSchemaForDynamicSchema(schema))
+        .reduce((all, val) => all.concat(val), []),
     }];
 
     this.dnsAuthenticatorList = schemas.map((schema) => this.parseSchemaForDnsAuthList(schema));
@@ -162,7 +161,9 @@ export class AcmednsFormComponent implements OnInit {
   }
 
   parseSchemaForDynamicSchema(schema: AuthenticatorSchema): DynamicFormSchemaNode[] {
-    return Object.values(schema.schema.properties).map((input) => getDynamicFormSchemaNode(input));
+    return Object.values(schema.schema.properties)
+      .filter((input) => !input.const)
+      .map((input) => getDynamicFormSchemaNode(input));
   }
 
   parseSchemaForDnsAuthList(schema: AuthenticatorSchema): DnsAuthenticatorList {
