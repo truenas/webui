84f10df17fe825773204f8c902316fd0f1a4097a
Damian Szidiropulosz <damianos@knoxbug.org>
2025-01-09 09:36:05 -0500

    NAS-133494: WIP enclosure color fixes

--- src/app/pages/system/enclosure/components/enclosure-side/enclosure-svg/enclosure-svg.component.ts
+++ src/app/pages/system/enclosure/components/enclosure-side/enclosure-svg/enclosure-svg.component.ts
@@ -42,6 +42,8 @@ export type TintingFunction = (slot: DashboardEnclosureSlot | null) => string | 
   imports: [NgxSkeletonLoaderModule],
 })
 export class EnclosureSvgComponent implements OnDestroy {
+  readonly emptyOpacity = 0.15;
+  readonly unselectedOpacity = 0.3;
   readonly svgUrl = input.required<string>();
   readonly slots = input<DashboardEnclosureSlot[]>();
   readonly enableMouseEvents = input(true);
@@ -132,6 +134,7 @@ export class EnclosureSvgComponent implements OnDestroy {
   });
 
   private clearSelectionStylesFromAllSlots(): void {
+    this.resetDimValues();
     Object.values(this.overlayRects).forEach((overlay) => {
       overlay.classList.remove('selected');
       overlay.classList.remove('selected-vdev-disk');
@@ -161,8 +164,11 @@ export class EnclosureSvgComponent implements OnDestroy {
     for (const slot of allSlots) {
       if (selectedVdevDisks.includes(slot.dev)) {
         this.renderer.addClass(this.overlayRects[slot.drive_bay_number], 'selected-vdev-disk');
+        if (slot.type) {
+          this.dimSlot(slot.drive_bay_number, 1);
+        }
       } else if (slot.drive_bay_number !== selectedSlot.drive_bay_number) {
-        this.renderer.addClass(this.overlayRects[slot.drive_bay_number], 'not-selected-vdev-disk');
+        this.dimSlot(slot.drive_bay_number, this.unselectedOpacity);
       }
     }
   }
@@ -245,7 +251,6 @@ export class EnclosureSvgComponent implements OnDestroy {
     const prevSlotExists = !!selectedSlot;
 
     if (!isNewSlotEmpty && prevSlotExists && slot.dev === selectedSlot.dev) {
-      this.selectedSlot.set(null);
       return;
     }
 
@@ -253,17 +258,58 @@ export class EnclosureSvgComponent implements OnDestroy {
   };
 
   private addTint(slot: DashboardEnclosureSlot): void {
-    const overlay = this.overlayRects[slot.drive_bay_number];
-
-    this.renderer.removeClass(overlay, 'tinted');
-
+    const tintTargets: NodeListOf<SVGGElement> = this.getTintTarget(slot.drive_bay_number);
     const slotTint = this.slotTintFn()(slot);
-    if (!slotTint) {
+
+    if (slotTint) {
+      tintTargets.forEach((tintTarget: SVGGElement) => {
+        this.renderer.addClass(tintTarget, 'tinted');
+        this.renderer.setStyle(tintTarget, 'fill', slotTint);
+        this.renderer.setStyle(tintTarget, 'filter', 'brightness(1.25)');
+      });
+      this.dimSlot(slot.drive_bay_number, 1);
+    } else {
+      tintTargets.forEach((tintTarget: SVGGElement) => {
+        this.renderer.removeClass(tintTarget, 'tinted');
+        this.renderer.setStyle(tintTarget, 'fill', null);
+        this.renderer.setStyle(tintTarget, 'filter', 'brightness(1)');
+      });
+      this.dimSlot(slot.drive_bay_number, this.emptyOpacity);
+    }
+  }
+
+  private dimSlot(slotNumber: number, opacity: number): void {
+    const slotId: string = 'DRIVE_CAGE_' + slotNumber.toString();
+    const slotSvg = this.svgContainer().nativeElement.querySelectorAll<SVGGElement>(`svg [id^=${slotId}]`);
+
+    this.renderer.setStyle(slotSvg[0], 'opacity', opacity.toString());
+  }
+
+  private resetDimValues(): void {
+    if (!this.svgContainer()) {
       return;
     }
 
-    this.renderer.addClass(overlay, 'tinted');
-    this.renderer.setStyle(overlay, 'fill', slotTint);
+    const driveTrays = this.svgContainer().nativeElement.querySelectorAll<SVGGElement>('svg [id^="DRIVE_CAGE_"]');
+    driveTrays.forEach((tray) => {
+      const slot = this.getSlotForTray(tray);
+      if (!slot) {
+        return;
+      }
+
+      if (!slot.dev) {
+        this.dimSlot(slot.drive_bay_number, this.emptyOpacity);
+      } else {
+        this.dimSlot(slot.drive_bay_number, 1);
+      }
+    });
+  }
+
+  private getTintTarget(slotNumber: number): NodeListOf<SVGGElement> {
+    const slotId: string = 'DRIVE_CAGE_' + slotNumber.toString();
+    const driveTrays = this.svgContainer().nativeElement.querySelectorAll<SVGGElement>(`svg [id^=${slotId}] .tint-target`);
+
+    return driveTrays;
   }
 
   private getSlotForTray(tray: SVGGElement): DashboardEnclosureSlot {
