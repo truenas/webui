43fff2fb3d1cec02e56463a85a9125ad27280c82
undsoft <undsoft@gmail.com>
2025-01-14 15:48:32 -0500

    NAS-133566: UI is not reloaded after license is applied

--- src/app/pages/system/general-settings/support/license/license.component.spec.ts
+++ src/app/pages/system/general-settings/support/license/license.component.spec.ts
@@ -6,6 +6,12 @@ import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectat
 import { of } from 'rxjs';
 import { mockCall, mockApi } from 'app/core/testing/utils/mock-api.utils';
 import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
+import { mockWindow } from 'app/core/testing/utils/mock-window.utils';
+import { WINDOW } from 'app/helpers/window.helper';
+import { helptextSystemSupport as helptext } from 'app/helptext/system/support';
+import {
+  DialogWithSecondaryCheckboxResult,
+} from 'app/interfaces/dialog.interface';
 import { DialogService } from 'app/modules/dialog/dialog.service';
 import { FormErrorHandlerService } from 'app/modules/forms/ix-forms/services/form-error-handler.service';
 import { IxFormHarness } from 'app/modules/forms/ix-forms/testing/ix-form.harness';
@@ -37,10 +43,17 @@ describe('LicenseComponent', () => {
       mockProvider(SlideIn, {
         components$: of([]),
       }),
-      mockProvider(DialogService),
+      mockProvider(DialogService, {
+        confirm: jest.fn(() => of()),
+      }),
       mockProvider(FormErrorHandlerService),
       mockProvider(SlideInRef, slideInRef),
       mockAuth(),
+      mockWindow({
+        location: {
+          reload: jest.fn(),
+        },
+      }),
     ],
   });
 
@@ -60,5 +73,24 @@ describe('LicenseComponent', () => {
     await saveButton.click();
 
     expect(api.call).toHaveBeenCalledWith('system.license_update', ['test-license']);
+  });
+
+  it('shows a warning that UI needs to be reloaded and reloads it after dialog is closed', async () => {
+    jest.spyOn(spectator.inject(DialogService), 'confirm').mockReturnValue(of({} as DialogWithSecondaryCheckboxResult));
+
+    const form = await loader.getHarness(IxFormHarness);
+    await form.fillForm({
+      License: 'test-license',
+    });
+
+    const saveButton = await loader.getHarness(MatButtonHarness.with({ text: 'Save' }));
+    await saveButton.click();
+
+    expect(spectator.inject(DialogService).confirm).toHaveBeenCalledWith(expect.objectContaining({
+      title: helptext.update_license.reload_dialog_title,
+      message: helptext.update_license.reload_dialog_message,
+    }));
+
+    expect(spectator.inject<Window>(WINDOW).location.reload).toHaveBeenCalled();
   });
 });
