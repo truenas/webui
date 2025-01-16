b3318bf5e6b0c93bd8e7632dacaac4828a95c2d4
Denys Butenko <ua13dark@gmail.com>
2025-01-16 11:49:14 +0700

    NAS-132330: Update TrueNAS Brand

--- src/app/helpers/copyright-text.helper.spec.ts
+++ src/app/helpers/copyright-text.helper.spec.ts
@@ -1,16 +1,19 @@
+import { environment } from 'environments/environment';
 import { ProductType } from 'app/enums/product-type.enum';
 import { getCopyrightText } from 'app/helpers/copyright-text.helper';
 
+const buildYear = environment.buildYear;
+
 describe('getCopyrightText', () => {
   it('general: copyright text', () => {
-    expect(getCopyrightText()).toBe('TrueNAS ® © 2024');
+    expect(getCopyrightText()).toBe(`TrueNAS ® © ${buildYear}`);
   });
 
   it('community edition: copyright text', () => {
-    expect(getCopyrightText(ProductType.Scale)).toBe('TrueNAS Community Edition ® © 2024');
+    expect(getCopyrightText(ProductType.Scale)).toBe(`TrueNAS Community Edition ® © ${buildYear}`);
   });
 
   it('enterprise: copyright text', () => {
-    expect(getCopyrightText(ProductType.ScaleEnterprise)).toBe('TrueNAS Enterprise ® © 2024');
+    expect(getCopyrightText(ProductType.ScaleEnterprise)).toBe(`TrueNAS Enterprise ® © ${buildYear}`);
   });
 });
