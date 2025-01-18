361ecf9252136c746d806d1bd3b75b89f40cdd53
Boris Vasilenko <bvasilenko@ixsystems.com>
2025-01-18 10:01:15 +0300

    NAS-133433: Fix remarks

--- src/app/pages/virtualization/virtualization.constants.ts
+++ src/app/pages/virtualization/virtualization.constants.ts
@@ -1,12 +1,10 @@
 /**
- * @type {number}
  * @description
  * Latest 10 minutes on the chart (2 seconds API interval)
  */
 export const metricsMaxItemsCount = 60 * 10 / 2;
 
 /**
- * @type {number}
  * @description
  * Default VNC port
  */
