f83c2e506554e4bb55554e569c3eaf5a647e0c6c
RehanY147 <rehanyousaf147@gmail.com>
2025-01-31 12:50:06 +0500

    NAS-133697 / 25.04 / Adapts to the latest changes in CPU live stats reporting by middleware (#11372)

--- src/app/pages/dashboard/widgets/cpu/interfaces/cpu-params.interface.ts
+++ src/app/pages/dashboard/widgets/cpu/interfaces/cpu-params.interface.ts
@@ -3,4 +3,8 @@ export interface CpuParams {
   usageMax: number;
   usageMinThreads: number[];
   usageMaxThreads: number[];
+  tempMin: number;
+  tempMax: number;
+  tempMinCores: number[];
+  tempMaxCores: number[];
 }
