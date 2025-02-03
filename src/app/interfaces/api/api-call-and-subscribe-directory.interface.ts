9232f2881f26d433d2bca8314f71f60ba02f58b3
Denys Butenko <ua13dark@gmail.com>
2025-02-03 22:37:39 +0700

    NAS-133891: Remove old VM section

--- src/app/interfaces/api/api-call-and-subscribe-directory.interface.ts
+++ src/app/interfaces/api/api-call-and-subscribe-directory.interface.ts
@@ -3,14 +3,12 @@ import { ContainerImage } from 'app/interfaces/container-image.interface';
 import { Group } from 'app/interfaces/group.interface';
 import { Pool } from 'app/interfaces/pool.interface';
 import { User } from 'app/interfaces/user.interface';
-import { VirtualMachine } from 'app/interfaces/virtual-machine.interface';
 import { VirtualizationInstance } from 'app/interfaces/virtualization.interface';
 
 /**
  * Directory of compatible API call and subscribe methods.
  */
 export interface ApiCallAndSubscribeEventDirectory {
-  'vm.query': { response: VirtualMachine };
   'user.query': { response: User };
   'pool.query': { response: Pool };
   'group.query': { response: Group };
