35e482c2066fa300fe0d410a7a5975085edda4e2
Denys Butenko <ua13dark@gmail.com>
2025-02-04 20:38:43 +0700

    NAS-132330: Update TrueNAS Brand (#11318)
    * NAS-133354: QA iSCSI pages
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    * NAS-132330: Update TrueNAS Brand
    
    (cherry picked from commit 921ef500b3d031b4e3cabcc25b52dd4a80c39b5b)

--- src/app/pages/dashboard/widgets/system/widget-sys-info-passive/widget-sys-info-passive.component.spec.ts
+++ src/app/pages/dashboard/widgets/system/widget-sys-info-passive/widget-sys-info-passive.component.spec.ts
@@ -21,7 +21,6 @@ import { selectCanFailover, selectIsHaEnabled, selectIsHaLicensed } from 'app/st
 import {
   selectIsIxHardware,
   selectProductType,
-  selectIsEnterprise,
   selectHasEnclosureSupport,
 } from 'app/store/system-info/system-info.selectors';
 
@@ -67,10 +66,6 @@ describe('WidgetSysInfoPassiveComponent', () => {
           {
             selector: selectProductType,
             value: ProductType.Enterprise,
-          },
-          {
-            selector: selectIsEnterprise,
-            value: true,
           },
           {
             selector: selectHasEnclosureSupport,
@@ -132,7 +127,8 @@ describe('WidgetSysInfoPassiveComponent', () => {
       const items = await parallel(() => matListItems.map((item) => item.getFullText()));
       expect(items).toEqual([
         'Platform: TRUENAS-M40-HA',
-        'Version: ElectricEel-24.10.0-MASTER-20240301-233006',
+        'Edition: Enterprise',
+        'Version: Goldeye-25.10.0-MASTER-20250126-184805',
         'Support License: Gold Contract,  Expires on 2025-01-01',
         'System Serial: AA-00002',
         'Hostname: test-hostname-b',
