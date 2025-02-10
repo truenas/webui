c6e875277e040572308e05df76f5b4aa3c5f771b
Evgeny Stepanovych <undsoft@gmail.com>
2025-02-10 09:18:22 -0500

    NAS-134102: More strict null checks (#11519)

--- src/app/modules/tooltip/tooltip.component.ts
+++ src/app/modules/tooltip/tooltip.component.ts
@@ -2,9 +2,8 @@ import {
   ChangeDetectionStrategy, Component, input,
 } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgxPopperjsModule, NgxPopperjsPlacements } from 'ngx-popperjs';
+import { NgxPopperjsModule, NgxPopperjsPlacements, NgxPopperjsTriggers } from 'ngx-popperjs';
 import { IxIconComponent } from 'app/modules/ix-icon/ix-icon.component';
-import { CastPipe } from 'app/modules/pipes/cast/cast.pipe';
 import { TestDirective } from 'app/modules/test-id/test.directive';
 
 @Component({
@@ -17,13 +16,14 @@ import { TestDirective } from 'app/modules/test-id/test.directive';
     NgxPopperjsModule,
     TranslateModule,
     IxIconComponent,
-    CastPipe,
     TestDirective,
   ],
 })
 export class TooltipComponent {
   readonly message = input<string>('');
   readonly header = input<string>('');
+
+  protected readonly clickTrigger = 'click' as NgxPopperjsTriggers;
 
   // Conversion here just allows us to use enum values as string without having to import the enum
   readonly placement = input<NgxPopperjsPlacements, `${NgxPopperjsPlacements}`>(NgxPopperjsPlacements.AUTO, {
