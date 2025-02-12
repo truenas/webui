82e0c57d6b8ce2d7f76e260866a5dddf3ffb986d
Alex Karpov <akarpov@ixsystems.com>
2025-02-12 09:18:29 +0200

    NAS-133963: PR update

--- src/app/interfaces/virtualization.interface.ts
+++ src/app/interfaces/virtualization.interface.ts
@@ -190,6 +190,7 @@ export interface VirtualizationImage {
   release: string;
   variant: string;
   instance_types: VirtualizationType[];
+  secureboot: boolean | null;
 }
 
 export interface VirtualizationStopParams {
