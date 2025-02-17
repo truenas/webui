79f22ab38cead0ef65a6e69b397f8c3dc8cdc2f3
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-02-17 00:05:18 -0500

    NAS-134036 / 25.04-RC.1 / Add logs to Cloud Sync Task card (by bvasilenko) (#11564)
    * NAS-134036: Add logs to Cloud Sync Task card
    
    (cherry picked from commit 4577d8cc0bad7f9f157e141f4d1655cae5131e23)
    
    * NAS-134036: Fix updating row in IxCellStateButton
    
    (cherry picked from commit c17699abbaf3152e3f0f03663bc7a829c709a7f8)
    
    ---------
    
    Co-authored-by: Boris Vasilenko <bvasilenko@ixsystems.com>

--- src/app/pages/data-protection/cloudsync/cloudsync-task-card/cloudsync-task-card.component.ts
+++ src/app/pages/data-protection/cloudsync/cloudsync-task-card/cloudsync-task-card.component.ts
@@ -109,6 +109,7 @@ export class CloudSyncTaskCardComponent implements OnInit {
     stateButtonColumn({
       title: this.translate.instant('State'),
       getValue: (row) => row.state.state,
+      getJob: (row) => row.job,
       cssClass: 'state-button',
     }),
     actionsColumn({
