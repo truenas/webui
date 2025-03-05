cceb8dd10529702fe5e3d823978db646cc0a97d9
Denys Butenko <ua13dark@gmail.com>
2025-03-05 19:02:42 +0700

    NAS-134595: Fix instances in ui search (#11668)
    * NAS-134595: Fix instances in ui search
    
    * NAS-134595: Fix tests
    
    (cherry picked from commit 0724134d89b8f9da62ae91c33306ee531033d9cf)

--- src/app/pages/instances/components/all-instances/all-instances.elements.ts
+++ src/app/pages/instances/components/all-instances/all-instances.elements.ts
@@ -2,13 +2,13 @@ import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
 
 export const allInstancesElements = {
-  hierarchy: [T('Virtualization'), T('Containers')],
-  synonyms: [T('VM'), T('Virtual Machines')],
-  anchorRouterLink: ['/virtualization/view/def'],
+  hierarchy: [T('Instances')],
+  synonyms: [T('VM'), T('Virtual Machines'), T('Instances'), T('Incus'), T('Containers')],
+  anchorRouterLink: ['/instances/view/def'],
   elements: {
     list: {
-      hierarchy: [T('Instances')],
-      synonyms: [T('Containers')],
+      hierarchy: [T('List')],
+      synonyms: [T('Containers'), T('Instances'), T('Incus')],
       anchor: 'containers-list',
     },
   },
