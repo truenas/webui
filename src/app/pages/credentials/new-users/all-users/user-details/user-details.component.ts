b177f85b5d07b131eb33fae76dad204b69eb498d
Alex Karpov <akarpov@ixsystems.com>
2025-03-06 12:16:34 +0200

    NAS-134475 / 25.10 / Implement master-detail layout for users list (#11684)
    * NAS-134475: Implement master-detail layout for users list
    
    * NAS-134475: PR Update

---
+++ src/app/pages/credentials/new-users/all-users/user-details/user-details.component.ts
@@ -0,0 +1,25 @@
+import {
+  ChangeDetectionStrategy, Component, input,
+} from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { UiSearchDirective } from 'app/directives/ui-search.directive';
+import { User } from 'app/interfaces/user.interface';
+import { userDetailsElements } from 'app/pages/credentials/new-users/all-users/user-details/user-details.elements';
+import { UserGeneralInfoComponent } from 'app/pages/credentials/new-users/all-users/user-details/user-general-info/user-general-info.component';
+
+@Component({
+  selector: 'ix-user-details',
+  templateUrl: './user-details.component.html',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    TranslateModule,
+    UserGeneralInfoComponent,
+    UiSearchDirective,
+  ],
+})
+export class UserDetailsComponent {
+  user = input.required<User>();
+
+  protected readonly searchableElements = userDetailsElements;
+}
