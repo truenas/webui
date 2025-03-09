6538f0515f2057593f824b77be5a4bf31de6feb7
Alex Karpov <akarpov@ixsystems.com>
2025-03-09 21:45:14 +0200

    NAS-134575 / 25.10 / Refactor user/group providers (#11685)
    * NAS-134566: Unexpected warning next to Security in Instances
    
    * NAS-134566: Refactor user/group providers
    
    * NAS-134575: PR Update
    
    * NAS-134575: PR Update
    
    * NAS-134575: PR update

--- src/app/modules/forms/ix-forms/classes/user-combobox-provider.ts
+++ src/app/modules/forms/ix-forms/classes/user-combobox-provider.ts
@@ -1,41 +1,55 @@
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ComboboxQueryType } from 'app/enums/combobox.enum';
 import { Option } from 'app/interfaces/option.interface';
 import { User } from 'app/interfaces/user.interface';
 import { IxComboboxProvider } from 'app/modules/forms/ix-forms/components/ix-combobox/ix-combobox-provider';
 import { UserService } from 'app/services/user.service';
 
+interface UserComboboxOptions {
+  valueField?: keyof Pick<User, 'username' | 'uid' | 'id'>;
+  queryType?: ComboboxQueryType;
+}
+
 export class UserComboboxProvider implements IxComboboxProvider {
   protected page = 1;
   readonly pageSize = 50;
+  protected valueField: keyof Pick<User, 'username' | 'uid' | 'id'>;
+  protected queryType: ComboboxQueryType;
+
+  constructor(
+    protected userService: UserService,
+    options: UserComboboxOptions = {},
+  ) {
+    this.valueField = options.valueField ?? 'username';
+    this.queryType = options.queryType ?? ComboboxQueryType.Default;
+  }
 
   fetch(filterValue: string): Observable<Option[]> {
     this.page = 0;
-    const offset = this.page * this.pageSize;
-
-    return this.userService.userQueryDsCache(filterValue, offset)
-      .pipe(
-        map((users) => this.userQueryResToOptions(users)),
-      );
-  }
-
-  userQueryResToOptions(users: User[]): Option[] {
-    return users.map((user) => {
-      return { label: user.username, value: user[this.optionsValueField] };
-    });
+    return this.queryUsers(filterValue);
   }
 
   nextPage(filterValue: string): Observable<Option[]> {
     this.page++;
-    const offset = this.page * this.pageSize;
-    return this.userService.userQueryDsCache(filterValue, offset)
-      .pipe(
-        map((users) => this.userQueryResToOptions(users)),
-      );
+    return this.queryUsers(filterValue);
   }
 
-  constructor(
-    protected userService: UserService,
-    protected optionsValueField: 'username' | 'uid' | 'id' = 'username',
-  ) {}
+  private queryUsers(filterValue: string): Observable<Option[]> {
+    const offset = this.page * this.pageSize;
+    const queryMethod = this.queryType === ComboboxQueryType.Smb
+      ? this.userService.smbUserQueryDsCache.bind(this.userService)
+      : this.userService.userQueryDsCache.bind(this.userService);
+
+    return queryMethod(filterValue, offset).pipe(
+      map((users) => this.userQueryResToOptions(users)),
+    );
+  }
+
+  private userQueryResToOptions(users: User[]): Option[] {
+    return users.map((user) => ({
+      label: user.username,
+      value: user[this.valueField],
+    }));
+  }
 }
