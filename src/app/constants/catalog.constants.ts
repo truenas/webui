e52ac6d5e04a1e3ef3eb41a8825a403c5d837d03
Alex Karpov <karpov4you@gmail.com>
2025-03-18 13:39:24 +0200

    NAS-134844: Discover Search, with Category filter returns broken App icons
    (cherry picked from commit a3930aa354b29487efa127ad002d157d424bd975)

--- src/app/constants/catalog.constants.ts
+++ src/app/constants/catalog.constants.ts
@@ -1,4 +1,4 @@
 export const customApp = 'ix-app';
 export const customAppTrain = 'stable';
-export const appImagePlaceholder = 'assets/images/truenas_scale_ondark_favicon.png';
+export const appImagePlaceholder = 'assets/images/truenas_favicon.png';
 export const latestVersion = 'latest';
