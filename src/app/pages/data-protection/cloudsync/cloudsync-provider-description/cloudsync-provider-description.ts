705ed273c74235831987a527cbcea6fc51b155ed
Evgeny Stepanovych <undsoft@gmail.com>
2025-03-19 13:47:45 -0400

    NAS-134889: Storj text updates (#11764)

--- src/app/pages/data-protection/cloudsync/cloudsync-provider-description/cloudsync-provider-description.ts
+++ src/app/pages/data-protection/cloudsync/cloudsync-provider-description/cloudsync-provider-description.ts
@@ -2,5 +2,5 @@ import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { CloudSyncProviderName } from 'app/enums/cloudsync-provider.enum';
 
 export const cloudsyncProviderDescriptionMap = new Map<CloudSyncProviderName, string>([
-  [CloudSyncProviderName.Storj, T('StorJ is an S3 compatible, fault tolerant, globally distributed cloud storage platform with a security first approach to backup and recovery - delivering extreme resilience and performance both sustainably and economically. <a href="https://truenas.com/storj" target="_blank">TrueNAS and Storj</a> have partnered to streamline delivery of Hybrid Cloud solutions globally.')],
+  [CloudSyncProviderName.Storj, T('Storj is an S3 compatible, fault tolerant, globally distributed cloud storage platform with a security first approach to backup and recovery - delivering extreme resilience and performance both sustainably and economically. <a href="https://truenas.com/storj" target="_blank">TrueNAS and Storj</a> have partnered to streamline delivery of Hybrid Cloud solutions globally.')],
 ]);
