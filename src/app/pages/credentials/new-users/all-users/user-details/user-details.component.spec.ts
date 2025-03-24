a5967fc9bc4a45f3f70265dc044c4794548ccb21
Boris Vasilenko <dreaktor0@gmail.com>
2025-03-24 15:07:54 +0300

    NAS-134529: Add user profile card (#11737)

--- src/app/pages/credentials/new-users/all-users/user-details/user-details.component.spec.ts
+++ src/app/pages/credentials/new-users/all-users/user-details/user-details.component.spec.ts
@@ -4,6 +4,7 @@ import { User } from 'app/interfaces/user.interface';
 import { UserAccessCardComponent } from 'app/pages/credentials/new-users/all-users/user-details/user-access-card/user-access-card.component';
 import { UserDetailsComponent } from 'app/pages/credentials/new-users/all-users/user-details/user-details.component';
 import { UserGeneralInfoComponent } from 'app/pages/credentials/new-users/all-users/user-details/user-general-info/user-general-info.component';
+import { UserProfileCardComponent } from 'app/pages/credentials/new-users/all-users/user-details/user-profile-card/user-profile-card.component';
 
 describe('UserDetailsComponent', () => {
   let spectator: Spectator<UserDetailsComponent>;
@@ -11,6 +12,7 @@ describe('UserDetailsComponent', () => {
     component: UserDetailsComponent,
     imports: [
       MockComponents(UserGeneralInfoComponent),
+      MockComponents(UserProfileCardComponent),
       MockComponents(UserAccessCardComponent),
     ],
   });
@@ -28,6 +30,7 @@ describe('UserDetailsComponent', () => {
 
   it('should render the cards', () => {
     expect(spectator.query(UserGeneralInfoComponent)).toExist();
+    expect(spectator.query(UserProfileCardComponent)).toExist();
     expect(spectator.query(UserAccessCardComponent)).toExist();
   });
 });
