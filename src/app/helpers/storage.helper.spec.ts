7c25528c37d941797bbaa203f533e3e40341eb32
Evgeny Stepanovych <undsoft@gmail.com>
2025-03-31 14:19:52 +0200

    NAS-135044 / 25.10 / Too many quotas are reported in Dataset Space Management card (#11812)
    (cherry picked from commit 025a1ab7a22013dcbe5e387e65e5ede7c2211c8a)

---
+++ src/app/helpers/storage.helper.spec.ts
@@ -0,0 +1,45 @@
+import { VdevType } from 'app/enums/v-dev-type.enum';
+import { DatasetQuota } from 'app/interfaces/dataset-quota.interface';
+import { isQuotaSet, isTopologyLimitedToOneLayout } from './storage.helper';
+
+describe('Storage Helper', () => {
+  describe('isTopologyLimitedToOneLayout', () => {
+    it('returns true for Spare type', () => {
+      expect(isTopologyLimitedToOneLayout(VdevType.Spare)).toBe(true);
+    });
+
+    it('returns true for Cache type', () => {
+      expect(isTopologyLimitedToOneLayout(VdevType.Cache)).toBe(true);
+    });
+
+    it('returns false for other types', () => {
+      expect(isTopologyLimitedToOneLayout(VdevType.Dedup)).toBe(false);
+    });
+  });
+
+  describe('isQuotaSet', () => {
+    it('returns true when quota > 0', () => {
+      const quota = {
+        quota: 10,
+        obj_quota: 0,
+      } as DatasetQuota;
+      expect(isQuotaSet(quota)).toBe(true);
+    });
+
+    it('returns true when obj_quota > 0', () => {
+      const quota = {
+        quota: 0,
+        obj_quota: 10,
+      } as DatasetQuota;
+      expect(isQuotaSet(quota)).toBe(true);
+    });
+
+    it('returns false when both quota and obj_quota = 0', () => {
+      const quota = {
+        quota: 0,
+        obj_quota: 0,
+      } as DatasetQuota;
+      expect(isQuotaSet(quota)).toBe(false);
+    });
+  });
+});
