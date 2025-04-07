a0ffe47959c00d3e17044b7892cb4dd4e8b5e201
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-04-07 06:51:06 -0400

    NAS-134539 / 25.04.1 / Re-initialize `VirtualizationInstancesStore` after config form closes (by RehanY147) (#11813)

--- src/app/pages/instances/components/common/device-actions-menu/device-actions-menu.component.ts
+++ src/app/pages/instances/components/common/device-actions-menu/device-actions-menu.component.ts
@@ -19,6 +19,7 @@ import { TestDirective } from 'app/modules/test-id/test.directive';
 import { ApiService } from 'app/modules/websocket/api.service';
 import { getDeviceDescription } from 'app/pages/instances/components/common/utils/get-device-description.utils';
 import { VirtualizationDevicesStore } from 'app/pages/instances/stores/virtualization-devices.store';
+import { VirtualizationInstancesStore } from 'app/pages/instances/stores/virtualization-instances.store';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
 
 @UntilDestroy()
@@ -55,7 +56,7 @@ export class DeviceActionsMenuComponent {
       return this.translate.instant('This device is read-only and cannot be edited.');
     }
 
-    const isInstanceStopped = this.deviceStore.selectedInstance().status === VirtualizationStatus.Stopped;
+    const isInstanceStopped = this.instancesStore.selectedInstance().status === VirtualizationStatus.Stopped;
     if (this.device().dev_type === VirtualizationDeviceType.Tpm && !isInstanceStopped) {
       return this.translate.instant('This device cannot be edited while the instance is running.');
     }
@@ -68,6 +69,7 @@ export class DeviceActionsMenuComponent {
     private api: ApiService,
     private errorHandler: ErrorHandlerService,
     private translate: TranslateService,
+    private instancesStore: VirtualizationInstancesStore,
     private snackbar: SnackbarService,
     private deviceStore: VirtualizationDevicesStore,
     private loader: AppLoaderService,
@@ -95,7 +97,7 @@ export class DeviceActionsMenuComponent {
   }
 
   private deleteDevice(): Observable<unknown> {
-    const selectedInstanceId = this.deviceStore.selectedInstance()?.id;
+    const selectedInstanceId = this.instancesStore.selectedInstance()?.id;
     if (!selectedInstanceId) {
       return NEVER;
     }
