0fc4e20b62f4d8fa10d3eb787f9c105a1432f8d2
Evgeny Stepanovych <undsoft@gmail.com>
2025-04-09 14:53:17 +0200

    NAS-135259: Be more strict about unused icons (#11872)

--- src/app/constants/catalog.constants.ts
+++ src/app/constants/catalog.constants.ts
@@ -1,4 +1,4 @@
 export const customApp = 'ix-app';
 export const customAppTrain = 'stable';
-export const appImagePlaceholder = 'assets/icons/custom/image-preloader.svg';
+export const appImagePlaceholder = 'assets/images/image-preloader.svg';
 export const latestVersion = 'latest';
