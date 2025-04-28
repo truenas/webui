2f5730f42c8d82f55746a9bdce6e9c85936f46ab
Alex Karpov <karpov4you@gmail.com>
2025-04-28 14:29:06 +0300

    NAS-135556: Simplify Datasets and Storage – pass 2

--- src/app/pages/storage/components/dashboard-pool/vdevs-card/vdevs-card.component.spec.ts
+++ src/app/pages/storage/components/dashboard-pool/vdevs-card/vdevs-card.component.spec.ts
@@ -2367,30 +2367,11 @@ describe('VDevsCardComponent', () => {
       const values = spectator.queryAll('.vdev-line .vdev-value');
 
       expect(spectator.queryAll('.vdev-line .warning ix-icon')).toHaveLength(1);
-      expect(captions).toHaveLength(6);
-      expect(values).toHaveLength(5);
+      expect(captions).toHaveLength(1);
+      expect(values).toHaveLength(0);
 
       expect(captions[0]).toHaveText('Data VDEVs');
       expect(spectator.query('.offline-data-vdevs')!.textContent).toBe('Offline VDEVs');
-
-      // Redundancy level should match data VDEVs
-      expect(captions[1]).toHaveText('Metadata');
-      expect(values[0]).toHaveText('N/A');
-
-      // Can be Disk or MIRROR
-      expect(captions[2]).toHaveText('Log VDEVs');
-      expect(values[1]).toHaveText('N/A');
-
-      // Can be DISK Only
-      expect(captions[3]).toHaveText('Cache VDEVs');
-      expect(values[2]).toHaveText('N/A');
-
-      // Can be DISK only but should also be same size or larger than disk sizes used in data VDEVs
-      expect(captions[4]).toHaveText('Spare VDEVs');
-      expect(values[3]).toHaveText('N/A');
-
-      expect(captions[5]).toHaveText('Dedup VDEVs');
-      expect(values[4]).toHaveText('N/A');
     });
   });
 });
