4840691123b2363b8096542ec218009754976586
Denys Butenko <ua13dark@gmail.com>
2025-04-28 20:06:10 +0700

    NAS-135046: Make login page more resilient

--- src/app/pages/signin/signin.component.spec.ts
+++ src/app/pages/signin/signin.component.spec.ts
@@ -63,6 +63,7 @@ describe('SigninComponent', () => {
       mockProvider(WebSocketStatusService, {
         isConnected$,
         isReconnectAllowed$,
+        setReconnect: jest.fn(),
       }),
     ],
   });
