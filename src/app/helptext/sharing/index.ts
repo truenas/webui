cc6da6955ba018da63a4995481d00958b6733eb1
Evgeny Stepanovych <undsoft@gmail.com>
2025-05-13 04:03:13 -0400

    NAS-135827: Cleanup more helptext (#11988)

--- src/app/helptext/sharing/index.ts
+++ src/app/helptext/sharing/index.ts
@@ -5,6 +5,6 @@ export * from './nfs/nfs';
 export * from './smb/smb';
 
 export const shared = {
-  delete_share_message: T('The sharing configuration will be removed.\
+  deleteShareMessage: T('The sharing configuration will be removed.\
     Data in the share dataset will not be affected.'),
 };
