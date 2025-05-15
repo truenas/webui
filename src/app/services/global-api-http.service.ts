fddebea8a70a8b4bd652a31e708c0477ee70f016
Alex Karpov <karpov4you@gmail.com>
2025-05-15 21:39:43 +0300

    NAS-135734: PR update

--- src/app/services/global-api-http.service.ts
+++ src/app/services/global-api-http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { WINDOW } from 'app/helpers/window.helper';
 
 @Injectable({
@@ -23,7 +23,6 @@ export class GlobalApiHttpService {
   }
 
   getBootId(): Observable<string> {
-    // return this.get<string>('boot_id');
-    return of('12345678-1234-1234-1234-123456789012');
+    return this.get<string>('boot_id');
   }
 }
