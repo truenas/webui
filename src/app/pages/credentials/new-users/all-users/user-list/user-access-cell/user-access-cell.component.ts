cc7dadd08dc92dbe092565ee9093bb035ea50cc5
Denys Butenko <ua13dark@gmail.com>
2025-06-02 16:07:16 +0700

    NAS-135745: Add user-access-cell

--- src/app/pages/credentials/new-users/all-users/user-list/user-access-cell/user-access-cell.component.ts
+++ src/app/pages/credentials/new-users/all-users/user-list/user-access-cell/user-access-cell.component.ts
@@ -3,7 +3,7 @@ import {
   computed,
 } from '@angular/core';
 import { MatTooltip } from '@angular/material/tooltip';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { roleNames } from 'app/enums/role.enum';
 import { User } from 'app/interfaces/user.interface';
 import { IxIconComponent } from 'app/modules/ix-icon/ix-icon.component';
@@ -13,7 +13,7 @@ import { IxIconComponent } from 'app/modules/ix-icon/ix-icon.component';
   templateUrl: './user-access-cell.component.html',
   styleUrls: ['./user-access-cell.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [IxIconComponent, MatTooltip],
+  imports: [IxIconComponent, MatTooltip, TranslateModule],
 })
 export class UserAccessCellComponent {
   readonly user = input.required<User>();
