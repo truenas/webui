290141789de68628f7383df54b7234fa6eebf5d5
Alex Karpov <akarpov@ixsystems.com>
2025-06-04 12:45:59 +0300

    NAS-136083: PR update

--- src/app/interfaces/audit/audit.interface.ts
+++ src/app/interfaces/audit/audit.interface.ts
@@ -3,6 +3,7 @@ import { ApiTimestamp } from 'app/interfaces/api-date.interface';
 import { MiddlewareAuditEntry } from 'app/interfaces/audit/middleware-audit-entry.interface';
 import { SmbAuditEntry } from 'app/interfaces/audit/smb-audit-entry.interface';
 import { SudoAuditEntry } from 'app/interfaces/audit/sudo-audit-entry.interface';
+import { SystemAuditEntry } from 'app/interfaces/audit/system-audit-entry.interface';
 import { QueryFilters, QueryOptions } from 'app/interfaces/query-api.interface';
 
 export interface AuditQueryParams {
@@ -31,7 +32,7 @@ export interface AuditConfig {
   quota_fill_critical: number;
 }
 
-export type AuditEntry = SmbAuditEntry | MiddlewareAuditEntry | SudoAuditEntry;
+export type AuditEntry = SmbAuditEntry | MiddlewareAuditEntry | SudoAuditEntry | SystemAuditEntry;
 
 export interface AuditVersions {
   major: number;
