1587010a7f1b82a7f1a26ee68da3decbf6f8d0c9
Alex Karpov <akarpov@ixsystems.com>
2025-06-07 12:55:17 +0300

    NAS-136118 / 25.10 / Add STIG related logic for new users (#12089)
    * NAS-136118: Add STIG related logic for new users
    
    * NAS-136118: PR update

--- src/app/pages/credentials/new-users/all-users/user-details/user-password-card/user-password-card.component.spec.ts
+++ src/app/pages/credentials/new-users/all-users/user-details/user-password-card/user-password-card.component.spec.ts
@@ -1,9 +1,21 @@
-import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { of } from 'rxjs';
+import { mockApi, mockCall } from 'app/core/testing/utils/mock-api.utils';
+import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
+import { SystemSecurityConfig } from 'app/interfaces/system-security-config.interface';
 import { User } from 'app/interfaces/user.interface';
+import { DialogService } from 'app/modules/dialog/dialog.service';
+import { ApiService } from 'app/modules/websocket/api.service';
 import { UserPasswordCardComponent } from 'app/pages/credentials/new-users/all-users/user-details/user-password-card/user-password-card.component';
+import { OneTimePasswordCreatedDialog } from 'app/pages/credentials/users/one-time-password-created-dialog/one-time-password-created-dialog.component';
 
 const user = {
   uid: 2937,
+  username: 'test-user',
   password_age: 1,
   password_history: [],
   password_change_required: false,
@@ -14,9 +26,28 @@ const user = {
 
 describe('UserPasswordCardComponent', () => {
   let spectator: Spectator<UserPasswordCardComponent>;
+  let loader: HarnessLoader;
+  let api: ApiService;
 
   const createComponent = createComponentFactory({
     component: UserPasswordCardComponent,
+    providers: [
+      mockProvider(DialogService, {
+        confirm: jest.fn(() => of(true)),
+      }),
+      mockApi([
+        mockCall('auth.generate_onetime_password', 'test-password'),
+        mockCall('system.security.config', {
+          enable_gpos_stig: false,
+        } as SystemSecurityConfig),
+      ]),
+      mockProvider(MatDialog, {
+        open: jest.fn(() => ({
+          afterClosed: () => of(true),
+        })),
+      }),
+      mockAuth(),
+    ],
   });
 
   function getRows(): Record<string, string> {
@@ -32,6 +63,8 @@ describe('UserPasswordCardComponent', () => {
     spectator = createComponent({
       props: { user },
     });
+    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
+    api = spectator.inject(ApiService);
   });
 
   it('shows header', () => {
@@ -67,6 +100,25 @@ describe('UserPasswordCardComponent', () => {
       'History:': '4 entries',
       'Age:': '4 days',
       'Last Change:': '2023-10-11 16:20:00',
+    });
+  });
+
+  it('generates Generate One-Time Password when Generate One-Time Password button is pressed', async () => {
+    const button = await loader.getHarness(MatButtonHarness.with({ text: /Generate One-Time Password/ }));
+    await button.click();
+
+    expect(spectator.inject(DialogService).confirm).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: 'Generate One-Time Password',
+        message: 'Are you sure you want to generate a one-time password for "test-user" user?',
+        hideCheckbox: true,
+      }),
+    );
+
+    expect(api.call).toHaveBeenCalledWith('auth.generate_onetime_password', [{ username: 'test-user' }]);
+
+    expect(spectator.inject(MatDialog).open).toHaveBeenLastCalledWith(OneTimePasswordCreatedDialog, {
+      data: 'test-password',
     });
   });
 });
