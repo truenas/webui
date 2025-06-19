a79d7407f21787549273c5d1edce6f418e6c26e9
Alex Karpov <Karpov4you@gmail.com>
2025-06-19 14:04:13 +0300

    NAS-00000: close slide-ins on logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,6 +77,7 @@ export class AppComponent implements OnInit {
   }
 
   private logOutExpiredUser(): void {
+    this.slideIn.closeAll();
     this.authService.clearAuthToken();
     this.router.navigate(['/signin']);
     this.snackbar.open(
