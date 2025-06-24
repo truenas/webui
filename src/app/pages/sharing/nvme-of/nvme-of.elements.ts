cc550723e4c4b09ee1f414256723edd26a914aed
Alex Karpov <karpov4you@gmail.com>
2025-06-24 14:58:38 +0300

    NAS-136387: PR update

--- src/app/pages/sharing/nvme-of/nvme-of.elements.ts
+++ src/app/pages/sharing/nvme-of/nvme-of.elements.ts
@@ -4,10 +4,16 @@ import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-sea
 export const nvmeOfElements = {
   hierarchy: [T('Shares'), T('NVMe-oF')],
   anchorRouterLink: ['/sharing', 'nvme-of'],
+  synonyms: [T('NVMeOF')],
   elements: {
     config: {
       hierarchy: [T('Global Configuration')],
       anchor: 'global-configuration',
+    },
+    addSubsystem: {
+      hierarchy: [T('Add Subsystem')],
+      synonyms: [T('Create Subsystem'), T('New Subsystem')],
+      anchor: 'add-subsystem',
     },
   },
 } satisfies UiSearchableElement;
