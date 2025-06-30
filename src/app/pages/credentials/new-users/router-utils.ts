e2a11448f4e8110cc2334f38b277e7081f5d16ba
RehanY147 <rehanyousaf147@gmail.com>
2025-06-30 06:53:50 +0500

    NAS-135333: Update router-utils.ts

--- src/app/pages/credentials/new-users/router-utils.ts
+++ src/app/pages/credentials/new-users/router-utils.ts
@@ -1,11 +1,12 @@
 import { Location } from '@angular/common';
 
 export function setUsernameInUrl(location: Location, username: string): void {
-  if (!username) {
-    return;
+  let newUrl = 'credentials/users-new';
+  if (username) {
+    const queryParams = new URLSearchParams();
+    queryParams.set('username', username);
+    newUrl = `${newUrl}?${queryParams.toString()}`;
   }
-  const queryParams = new URLSearchParams();
-  queryParams.set('username', username);
-  const newUrl = `credentials/users-new?${queryParams.toString()}`;
+
   location.replaceState(newUrl);
 }
