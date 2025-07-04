83937e8e0d85296b60cb62a70dc6db5a7407241c
Evgeny Stepanovych <undsoft@gmail.com>
2025-07-04 08:14:27 +0200

    NAS-136495: Replace "Instance(s)" with "Container(s)" on Containers screens (#12220)

--- src/app/pages/instances/components/all-instances/all-instances.elements.ts
+++ src/app/pages/instances/components/all-instances/all-instances.elements.ts
@@ -2,8 +2,8 @@ import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
 
 export const allInstancesElements = {
-  hierarchy: [T('Instances')],
-  synonyms: [T('VM'), T('Virtual Machines'), T('Instances'), T('Incus'), T('Containers')],
+  hierarchy: [T('Containers')],
+  synonyms: [T('Instances'), T('Incus'), T('Containers')],
   anchorRouterLink: ['/instances/view/def'],
   elements: {
     list: {
--- src/app/pages/instances/components/all-instances/instance-details/instance-details.elements.ts
+++ src/app/pages/instances/components/all-instances/instance-details/instance-details.elements.ts
@@ -2,8 +2,8 @@ import { marker as T } from '@biesbjerg/ngx-translate-extract-marker';
 import { UiSearchableElement } from 'app/modules/global-search/interfaces/ui-searchable-element.interface';
 
 export const instanceDetailsElements = {
-  hierarchy: [T('Instances')],
-  synonyms: [T('VM'), T('Virtual Machines'), T('Instances'), T('Virtualization'), T('Containers')],
+  hierarchy: [T('Containers')],
+  synonyms: [T('Instances'), T('Containers')],
   anchorRouterLink: ['/instances/view/def'],
   elements: {
     generalInfo: {
