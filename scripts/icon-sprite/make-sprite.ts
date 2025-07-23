71ef6d278aab10bceb19042d3362f5f508978451
William Grzybowski <56250+william-gr@users.noreply.github.com>
2025-07-23 10:28:46 -0300

    NAS-136829 / 25.10 / Update to latest Node LTS (22.17.1) (#12291)

--- scripts/icon-sprite/make-sprite.ts
+++ scripts/icon-sprite/make-sprite.ts
@@ -1,7 +1,5 @@
 import crypto from 'crypto';
 import fs from 'fs';
-import * as path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { resolve } from 'path';
 import { addCustomIcons } from './lib/add-custom-icons';
 import { buildSprite } from './lib/build-sprite';
@@ -12,13 +10,9 @@ import { warnAboutDuplicates } from './lib/warn-about-duplicates';
 
 async function makeSprite(): Promise<void> {
   try {
-    // TODO: Can be simplified in node 20.11+
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-    const srcDir = resolve(__dirname, '../../src');
-    const targetPath = resolve(__dirname, '../../src/assets/icons/sprite.svg');
-    const configPath = resolve(__dirname, '../../src/assets/icons/sprite-config.json');
+    const srcDir = resolve(import.meta.dirname, '../../src');
+    const targetPath = resolve(import.meta.dirname, '../../src/assets/icons/sprite.svg');
+    const configPath = resolve(import.meta.dirname, '../../src/assets/icons/sprite-config.json');
 
     const templateIcons = findIconsInTemplates(srcDir);
     const markerIcons = findIconsWithMarker(srcDir);
