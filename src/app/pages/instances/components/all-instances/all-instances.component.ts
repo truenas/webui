3e5f8bf2f3312fe9b7dea1463eeba21a228b5ab9
Alex Karpov <karpov4you@gmail.com>
2025-07-25 14:38:34 +0300

    NAS-136852: Remove all mentions of Incus

--- src/app/pages/instances/components/all-instances/all-instances.component.ts
+++ src/app/pages/instances/components/all-instances/all-instances.component.ts
@@ -67,18 +67,5 @@ export class AllInstancesComponent implements OnInit {
       distinctUntilChanged(),
       untilDestroyed(this),
     ).subscribe(() => this.instancesStore.initialize());
-
-    const showVmInstancesWarning = !this.window.localStorage.getItem('showNewVmInstancesWarning');
-
-    if (showVmInstancesWarning) {
-      this.dialogService.closeAllDialogs();
-
-      this.dialogService.warn(
-        'Warning',
-        'Containers powered by Incus are experimental and only recommended for advanced users. Make all configuration changes using the TrueNAS UI. Operations using the command line are not supported.',
-      ).pipe(untilDestroyed(this)).subscribe(() => {
-        this.window.localStorage.setItem('showNewVmInstancesWarning', 'true');
-      });
-    }
   }
 }
