20db44611189289155224991d1cb4def436120c7
Alex Karpov <karpov4you@gmail.com>
2025-07-29 11:18:47 +0300

    NAS-136879: Snackbar is overlapped by the backdrop

--- src/app/modules/snackbar/services/snackbar.service.ts
+++ src/app/modules/snackbar/services/snackbar.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarRef } from '@angular/material/snack-bar';
 import { iconMarker } from 'app/modules/ix-icon/icon-marker.util';
 import { SnackbarComponent } from 'app/modules/snackbar/components/snackbar/snackbar.component';
 import { TranslatedString } from 'app/modules/translate/translate.helper';
@@ -17,7 +17,7 @@ export class SnackbarService {
   ) {}
 
   success(message: TranslatedString): MatSnackBarRef<SnackbarComponent> {
-    return this.matSnackBar.openFromComponent(SnackbarComponent, {
+    const config: MatSnackBarConfig = {
       announcementMessage: message,
       politeness: 'assertive',
       data: {
@@ -25,6 +25,22 @@ export class SnackbarService {
         icon: iconMarker('mdi-check'),
         iconCssColor: 'var(--green)',
       },
-    });
+    };
+
+    const ref = this.matSnackBar.openFromComponent(SnackbarComponent, config);
+
+    this.forceOverlayIndex(ref, 3000);
+
+    return ref;
+  }
+
+  // This is a workaround to force the snackbar to be on top of other overlays.
+  private forceOverlayIndex(ref: MatSnackBarRef<SnackbarComponent>, zIndex: number): void {
+    const containerEl = ref.containerInstance._elementRef.nativeElement as HTMLElement;
+    const overlayPane = containerEl.closest('.cdk-global-overlay-wrapper') as HTMLElement | null;
+
+    if (overlayPane) {
+      overlayPane.style.zIndex = String(zIndex);
+    }
   }
 }
