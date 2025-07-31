067226092bec2127d93e63ba5c05741dd3287748
William Grzybowski <william@grzy.org>
2025-07-31 19:14:20 -0300

    Hopefully last fix

--- src/app/modules/layout/topbar/change-password-dialog/change-password-form/change-password-form.component.spec.ts
+++ src/app/modules/layout/topbar/change-password-dialog/change-password-form/change-password-form.component.spec.ts
@@ -1,15 +1,17 @@
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { TestBed } from '@angular/core/testing';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatDialogRef } from '@angular/material/dialog';
 import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
-import { of, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { MockAuthService } from 'app/core/testing/classes/mock-auth.service';
 import { mockCall, mockApi } from 'app/core/testing/utils/mock-api.utils';
 import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
 import { Role } from 'app/enums/role.enum';
 import { LoggedInUser } from 'app/interfaces/ds-cache.interface';
+import { AuthService } from 'app/modules/auth/auth.service';
 import { FormErrorHandlerService } from 'app/modules/forms/ix-forms/services/form-error-handler.service';
 import { IxFormHarness } from 'app/modules/forms/ix-forms/testing/ix-form.harness';
 import { ChangePasswordDialog } from 'app/modules/layout/topbar/change-password-dialog/change-password-dialog.component';
@@ -21,58 +23,62 @@ describe('ChangePasswordDialogComponent', () => {
   let spectator: Spectator<ChangePasswordDialog>;
   let loader: HarnessLoader;
   let api: ApiService;
-  
+
   const createComponent = createComponentFactory({
     component: ChangePasswordDialog,
     imports: [
       ReactiveFormsModule,
     ],
     providers: [
+      mockAuth({
+        pw_name: 'root',
+        privilege: {
+          roles: {
+            $set: [],
+          },
+        },
+      } as LoggedInUser),
       mockApi([
         mockCall('user.set_password'),
       ]),
       mockProvider(FormErrorHandlerService),
       mockProvider(MatDialogRef),
       mockProvider(LoaderService, {
-        withLoader: () => (source$: any) => source$,
+        withLoader: () => <T>(source$: T) => source$,
       }),
       mockProvider(SnackbarService),
     ],
   });
 
   it('does not show current password field for full admin', async () => {
-    spectator = createComponent({
-      providers: [
-        mockAuth(),
-      ],
+    // Override the mockAuth to provide admin user
+    TestBed.overrideProvider(AuthService, {
+      useFactory: () => {
+        const mockService = new MockAuthService();
+        mockService.setUser({
+          pw_name: 'root',
+          privilege: {
+            roles: {
+              $set: [Role.FullAdmin],
+            },
+          },
+        } as LoggedInUser);
+        return mockService;
+      },
     });
+
+    spectator = createComponent();
     loader = TestbedHarnessEnvironment.loader(spectator.fixture);
     api = spectator.inject(ApiService);
-    
-    // Set up the auth mock to return true for FullAdmin
-    const authMock = spectator.inject(MockAuthService);
-    authMock.hasRole.mockReturnValue(of(true));
-    
-    spectator.detectChanges();
 
     const form = await loader.getHarness(IxFormHarness);
     expect(await form.getControl('Current Password')).toBeUndefined();
   });
 
   it('checks current password, updates to new password and closes the dialog when form is saved', async () => {
-    spectator = createComponent({
-      providers: [
-        mockAuth(),
-      ],
-    });
+    spectator = createComponent();
     loader = TestbedHarnessEnvironment.loader(spectator.fixture);
     api = spectator.inject(ApiService);
-    
-    // Set up the auth mock to return false for FullAdmin (i.e., not a full admin)
-    const authMock = spectator.inject(MockAuthService);
-    authMock.hasRole.mockReturnValue(of(false));
-    
-    spectator.detectChanges();
 
     const form = await loader.getHarness(IxFormHarness);
     await form.fillForm({
@@ -93,19 +99,9 @@ describe('ChangePasswordDialogComponent', () => {
   });
 
   it('shows error if any happened during password change request', async () => {
-    spectator = createComponent({
-      providers: [
-        mockAuth(),
-      ],
-    });
+    spectator = createComponent();
     loader = TestbedHarnessEnvironment.loader(spectator.fixture);
     api = spectator.inject(ApiService);
-    
-    // Set up the auth mock to return false for FullAdmin (i.e., not a full admin)
-    const authMock = spectator.inject(MockAuthService);
-    authMock.hasRole.mockReturnValue(of(false));
-    
-    spectator.detectChanges();
 
     const error = new Error('error');
     jest.spyOn(api, 'call').mockReturnValue(throwError(() => error));
