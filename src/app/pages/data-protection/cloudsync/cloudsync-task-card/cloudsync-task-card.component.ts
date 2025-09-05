fcbeccd46a3512d37f841d9eb6008745419fc6d5
Gerald Miral <gm.boknat@gmail.com>
2025-09-05 15:05:43 +0800

    NAS-137120: updated unit test

--- src/app/pages/data-protection/cloudsync/cloudsync-task-card/cloudsync-task-card.component.ts
+++ src/app/pages/data-protection/cloudsync/cloudsync-task-card/cloudsync-task-card.component.ts
@@ -324,9 +324,6 @@ export class CloudSyncTaskCardComponent implements OnInit {
       }),
       untilDestroyed(this),
     ).subscribe((job: Job) => {
-      if (job.state === JobState.Success) {
-        this.snackbar.success(this.translate.instant('Cloud Sync Task «{name}» completed successfully.', { name: row.description }));
-      }
       this.updateRowStateAndJob(row, job.state, job);
       if (this.jobStates.get(job.id) !== job.state) {
         this.getCloudSyncTasks();
