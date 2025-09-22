c103c668d354ed7f5fbb0337119e270f4d6e86ff
William Grzybowski <william@grzy.org>
2025-09-22 10:35:17 -0300

    NAS-137636: always send inherit_encryption inz vol form

--- src/app/pages/datasets/components/zvol-form/zvol-form.component.spec.ts
+++ src/app/pages/datasets/components/zvol-form/zvol-form.component.spec.ts
@@ -193,6 +193,7 @@ describe('ZvolFormComponent', () => {
         volblocksize: '16K',
         readonly: OnOff.On,
         snapdev: DatasetSnapdev.Visible,
+        inherit_encryption: false,
         encryption: true,
         encryption_options: {
           algorithm: 'AES-128-CCM',
@@ -282,9 +283,9 @@ describe('ZvolFormComponent', () => {
       expect(createCall[1][0]).toMatchObject({
         name: 'parentId/encrypted-zvol',
         type: DatasetType.Volume,
+        inherit_encryption: true,
         // Should NOT include encryption field when inherit_encryption is true
       });
-      expect(createCall[1][0].inherit_encryption).toBeUndefined(); // inherit_encryption should be deleted from payload
       expect(createCall[1][0].encryption).toBeUndefined(); // encryption should not be sent when inheriting
     });
   });
@@ -334,6 +335,7 @@ describe('ZvolFormComponent', () => {
         compression: 'LZ4',
         deduplication: 'OFF',
         force_size: false,
+        inherit_encryption: true,
         readonly: 'INHERIT',
         snapdev: 'INHERIT',
         sync: 'STANDARD',
