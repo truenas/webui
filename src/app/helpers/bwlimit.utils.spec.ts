8e0f526bfc18d40fa00dc84b63f2b2351fb2c485
matt wyatt <matt.wyatt@truenas.com>
2025-10-10 12:12:44 -0400

    fix: send numeric bandwidth limit to API
    the API was giving us a validation error when trying to set an rclone
    bandwidth limit no matter what we put in. this was because we were
    sending a string to the API instead of a number.

--- src/app/helpers/bwlimit.utils.spec.ts
+++ src/app/helpers/bwlimit.utils.spec.ts
@@ -15,19 +15,19 @@ describe('prepareBwlimit', () => {
   it('should add default time if bwlimit has only one limit without time', () => {
     const bwlimit: string[] = ['100'];
     const result = prepareBwlimit(bwlimit);
-    expect(result).toEqual([{ time: '00:00', bandwidth: '100' }]);
+    expect(result).toEqual([{ time: '00:00', bandwidth: 100 }]);
   });
 
   it('should convert bandwidth to bytes if it ends with "/s"', () => {
     const bwlimit: string[] = ['10M'];
     const result = prepareBwlimit(bwlimit);
-    expect(result).toEqual([{ time: '00:00', bandwidth: '10485760' }]);
+    expect(result).toEqual([{ time: '00:00', bandwidth: 10485760 }]);
   });
 
   it('should convert bandwidth to bytes if it ends with "/S"', () => {
     const bwlimit: string[] = ['10MB/S'];
     const result = prepareBwlimit(bwlimit);
-    expect(result).toEqual([{ time: '00:00', bandwidth: '10485760' }]);
+    expect(result).toEqual([{ time: '00:00', bandwidth: 10485760 }]);
   });
 
   it('should return null bandwidth if it is "off"', () => {
@@ -40,8 +40,8 @@ describe('prepareBwlimit', () => {
     const bwlimit: string[] = ['00:00, 10MB/s', '12:00, 5GB/s'];
     const result = prepareBwlimit(bwlimit);
     expect(result).toEqual([
-      { time: '00:00', bandwidth: '10485760' },
-      { time: '12:00', bandwidth: '5368709120' },
+      { time: '00:00', bandwidth: 10485760 },
+      { time: '12:00', bandwidth: 5368709120 },
     ]);
   });
 });
