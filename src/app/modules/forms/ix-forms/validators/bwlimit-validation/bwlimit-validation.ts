4d5bcf0385b3b2e3224ec374d0be16e47462c5b2
matt wyatt <matt.wyatt@truenas.com>
2025-10-13 13:46:02 -0400

    feat: add time validation to `bwlimitValidator`
    * add client-side time validation to the `bwlimitValidator` used in the
      cloud sync form
    * add a new error message to the list of default error messages
      validators can produce for invalid rclone bandwidth limits
    * add test coverage for time validation

--- src/app/modules/forms/ix-forms/validators/bwlimit-validation/bwlimit-validation.ts
+++ src/app/modules/forms/ix-forms/validators/bwlimit-validation/bwlimit-validation.ts
@@ -6,8 +6,8 @@ import { prepareBwlimit } from 'app/helpers/bwlimit.utils';
  * match the specification described here:
  * https://rclone.org/docs/#bwlimit-bandwidth-spec
  *
- * on error, the validator returns the *pattern* error object which displays:
- * 'Invalid format or character'.
+ * on error, the validator returns the *invalidRcloneBandwidthLimit* error object with
+ * the original invalid value string.
  */
 export function bwlimitValidator(): ValidatorFn {
   return (control: AbstractControl<string[]>): ValidationErrors | null => {
@@ -22,13 +22,47 @@ export function bwlimitValidator(): ValidatorFn {
 
     // a parsed bandwidth limit is invalid if it is `NaN`, since that means
     // that `prepareBwlimit` failed to parse it as a number at all.
-    // TODO: probably want to validate the time here too? just for consistency?
-    const hasInvalidBandwidth = parsed.some((limit) => {
+    const invalidBandwidthIndex = parsed.findIndex((limit) => {
       return Number.isNaN(limit.bandwidth);
     });
 
-    if (hasInvalidBandwidth) {
-      return { pattern: true };
+    // a parsed time is in valid if its hour or minute components are NaN, non-integer, or outside
+    // of the 24 hour time range. 0 <= hour <= 23 and 0 <= minute <= 59
+    const invalidTimeIndex = parsed.findIndex((limit) => {
+      const timeComponents = limit.time.split(':');
+      if (timeComponents.length !== 2) {
+        return true;
+      }
+
+      const hour = Number(timeComponents[0]);
+      const minute = Number(timeComponents[1]);
+      if (Number.isNaN(hour) || !Number.isInteger(hour) || hour < 0 || hour > 23) {
+        return true;
+      }
+
+      if (Number.isNaN(minute) || !Number.isInteger(minute) || minute < 0 || minute > 59) {
+        return true;
+      }
+
+      return false;
+    });
+
+    // we're only able to return one validation error of one kind at a time, so we check for invalid times first,
+    // then we check for invalid bandwidths.
+    if (invalidTimeIndex !== -1) {
+      return {
+        invalidRcloneBandwidthLimit: {
+          value: value[invalidTimeIndex],
+        },
+      };
+    }
+
+    if (invalidBandwidthIndex !== -1) {
+      return {
+        invalidRcloneBandwidthLimit: {
+          value: value[invalidBandwidthIndex],
+        },
+      };
     }
 
     return null;
