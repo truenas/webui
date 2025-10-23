d8e6c9e1cae76cf8bac24e55192b2c0a67dc6bc4
Alex Karpov <akarpov@ixsystems.com>
2025-10-23 11:42:46 +0300

    NAS-138096 / 26.04 / Improve snackbars (#12722)
    * NAS-138096: Snackbar improvements
    
    * NAS-138096: PR Update
    
    * NAS-138096: PR Update

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -8,6 +8,7 @@ import {
   catchError,
   combineLatest,
   filter,
+  finalize,
   map,
   Observable,
   of,
@@ -60,6 +61,10 @@ export class AuthService implements OnDestroy {
 
   // Flag to prevent premature adminUiInitialized dispatch
   private sessionInitialized = false;
+
+  // Track whether logout was manual to avoid showing "session expired" message
+  private isManualLogout = new BehaviorSubject<boolean>(false);
+  readonly isManualLogout$ = this.isManualLogout.asObservable();
 
   /**
    * This is 10 seconds less than 300 seconds which is the default life
@@ -234,6 +239,7 @@ export class AuthService implements OnDestroy {
   }
 
   logout(): Observable<void> {
+    this.isManualLogout.next(true);
     return this.api.call('auth.logout').pipe(
       tap(() => {
         this.clearAuthToken();
@@ -245,6 +251,7 @@ export class AuthService implements OnDestroy {
         this.loggedInUser$.next(null); // Clear user data on logout
         this.cachedGlobalTwoFactorConfig$.next(null); // Clear cached 2FA config
       }),
+      finalize(() => this.isManualLogout.next(false)),
     );
   }
 
