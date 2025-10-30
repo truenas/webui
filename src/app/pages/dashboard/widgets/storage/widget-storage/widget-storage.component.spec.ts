7dbe46b4b71d9506a1ec8993d468fc8d6ca7f9b9
matt wyatt <matt.wyatt@truenas.com>
2025-10-30 15:04:32 -0500

    NAS-138173: refactor: test file clarity improvements
    * re-add removed test case `sets poolsInfo`.
    
    * better handle testing differences between having multiple pools in the
    component vs. having a single pool in the component.

--- src/app/pages/dashboard/widgets/storage/widget-storage/widget-storage.component.spec.ts
+++ src/app/pages/dashboard/widgets/storage/widget-storage/widget-storage.component.spec.ts
@@ -5,7 +5,7 @@ import { format } from 'date-fns';
 import { MockDirective } from 'ng-mocks';
 import { BaseChartDirective } from 'ng2-charts';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-import { of } from 'rxjs';
+import { of, BehaviorSubject } from 'rxjs';
 import { mockAuth } from 'app/core/testing/utils/mock-auth.utils';
 import { PoolScanFunction } from 'app/enums/pool-scan-function.enum';
 import { PoolScanState } from 'app/enums/pool-scan-state.enum';
@@ -16,6 +16,12 @@ import { FormatDateTimePipe } from 'app/modules/dates/pipes/format-date-time/for
 import { WidgetResourcesService } from 'app/pages/dashboard/services/widget-resources.service';
 import { SlotSize } from 'app/pages/dashboard/types/widget.interface';
 import { WidgetStorageComponent } from 'app/pages/dashboard/widgets/storage/widget-storage/widget-storage.component';
+
+// ~3.63GiB
+const totalData = 3892314112;
+// ~2.11GiB
+const usedData = 2267242496;
+
 
 /**
  * helper function to create a fake pool for testing. in this file, we make a few different
@@ -84,7 +90,7 @@ const poolWithOngoingScrub = makeFakePool('poolWithOngoingScrub', 2, {
   end_time: null,
   pause: null,
   errors: 0,
-  percentage: 58.25,
+  percentage: 65.19,
   total_secs_left: 1200,
 } as PoolScanUpdate);
 
@@ -99,15 +105,9 @@ const poolWithNeverRunScrub = makeFakePool('poolWithNeverRunScrub', 3, {
   total_secs_left: null,
 } as PoolScanUpdate);
 
-const fakePools = [
-  poolWithFinishedScrub,
-  poolWithOngoingScrub,
-  poolWithNeverRunScrub,
-];
-
 describe('WidgetStorageComponent', () => {
   let spectator: Spectator<WidgetStorageComponent>;
-
+  const pools$ = new BehaviorSubject<Pool[]>([]);
   const createComponent = createComponentFactory({
     component: WidgetStorageComponent,
     imports: [
@@ -121,23 +121,15 @@ describe('WidgetStorageComponent', () => {
       mockProvider(
         WidgetResourcesService,
         {
-          pools$: of(fakePools),
+          pools$,
           poolUpdatesWithStaleDetection: () => of({
             value: {
+              // since we only ever check the size values of the pool with a finished scrub,
+              // we only define the size for the pool with a finished scrub.
               poolWithFinishedScrub: {
-                available: 1625071616,
-                used: 2267242496,
-                total: 3892314112,
-              },
-              poolWithOngoingScrub: {
-                available: 1625071616,
-                used: 2267242496,
-                total: 3892314112,
-              },
-              poolWithNeverRunScrub: {
-                available: 1625071616,
-                used: 2267242496,
-                total: 3892314112,
+                available: totalData - usedData,
+                used: usedData,
+                total: totalData,
               },
             },
             isStale: false,
@@ -149,30 +141,115 @@ describe('WidgetStorageComponent', () => {
           return format(typeof date === 'string' ? Date.parse(date) : date as number | Date, 'yyyy-MM-dd HH:mm:ss');
         }),
       }),
-      mockProvider(PercentPipe, {
-        transform: jest.fn((value: number) => `${(value * 100).toFixed(2)}%`),
-      }),
+      mockProvider(PercentPipe),
       mockAuth(),
     ],
   });
 
-  beforeEach(() => {
-    spectator = createComponent({
-      props: {
-        size: SlotSize.Full,
-      },
+  describe('Single Pool Configuration', () => {
+    // within each describe block, we change the `pools$` subject to contain whichever pools
+    // we want to test with. for this specific block, we
+    // *only* want the pool with a finished scrub (and a size defined).
+    beforeEach(() => {
+      pools$.next([poolWithFinishedScrub]);
+      spectator = createComponent({
+        props: {
+          size: SlotSize.Full,
+        },
+      });
+    });
+
+    it('sets poolsInfo', () => {
+      const poolsInfo = spectator.component.poolsInfo();
+      expect(poolsInfo).toMatchObject([{
+        name: 'poolWithFinishedScrub',
+        status: {
+          icon: 'check_circle',
+          label: 'Pool Status',
+          level: 'safe',
+          value: 'ONLINE',
+        },
+        usedSpace: {
+          icon: 'check_circle',
+          label: 'Used Space',
+          level: 'safe',
+          value: '58.25%',
+        },
+        disksWithError: {
+          icon: 'error',
+          label: 'Disks with Errors',
+          level: 'warn',
+          value: '1 of 2',
+        },
+        scan: {
+          icon: 'check_circle',
+          label: 'Last Scrub',
+          level: 'safe',
+          value: '2024-05-05 10:00:13',
+        },
+        freeSpace: '1.51 GiB',
+        totalDisks: '2',
+      }]);
+    });
+
+    it('shows storage tiles', () => {
+      const tiles = spectator.queryAll('.tile');
+      expect(tiles).toHaveLength(1);
+
+      const headers = tiles.map((tile) => tile.querySelector('.tile-header-title')!.textContent!.trim());
+      expect(headers).toEqual(['poolWithFinishedScrub']);
+
+      const contents = tiles.map((tile) => {
+        const labels: (string | null)[] = [];
+        const values: (string | null)[] = [];
+        tile.querySelectorAll('li').forEach((row) => {
+          const label = row.querySelector('.label')!;
+          const value = row.querySelector('.value')!;
+          labels.push(label ? label.textContent!.trim() : null);
+          values.push(value ? value.getAttribute('ng-reflect-content')! || value.textContent!.trim() : null);
+        });
+        return { labels, values };
+      });
+
+      expect(contents[0]).toEqual(
+        {
+          labels: [
+            'Pool Status:',
+            'Used Space:',
+            'Disks with Errors:',
+            'Last Scrub:',
+            'Free Space:',
+            'Total Disks:',
+            'Data:',
+            'Caches:',
+            'Spares:',
+          ],
+          values: [
+            'ONLINE',
+            '58.25%',
+            '1 of 2',
+            null,
+            '1.51 GiB',
+            '2',
+            '2 vdev',
+            '0',
+            '1',
+          ],
+        },
+      );
     });
   });
 
-  it('shows storage tiles', () => {
-    const tiles = spectator.queryAll('.tile');
-    expect(tiles).toHaveLength(3);
+  describe('Multiple Pools - Scrub Status', () => {
+    beforeEach(() => {
+      pools$.next([poolWithFinishedScrub, poolWithOngoingScrub, poolWithNeverRunScrub]);
+      spectator = createComponent({
+        props: {
+          size: SlotSize.Full,
+        },
+      });
+    });
 
-    const headers = tiles.map((tile) => tile.querySelector('.tile-header-title')!.textContent!.trim());
-    expect(headers).toEqual(['poolWithFinishedScrub', 'poolWithOngoingScrub', 'poolWithNeverRunScrub']);
-  });
-
-  describe('scan status', () => {
     it('shows completed scrub with date', () => {
       const poolsInfo = spectator.component.poolsInfo();
       // fakePools[0] - poolWithFinishedScrub
@@ -191,7 +268,7 @@ describe('WidgetStorageComponent', () => {
         icon: 'arrow_circle_right',
         label: 'Last Scrub',
         level: 'safe',
-        value: '58.25%',
+        value: '65.19%',
       });
     });
 
