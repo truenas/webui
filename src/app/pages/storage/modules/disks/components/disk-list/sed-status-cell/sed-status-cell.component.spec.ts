d3bfca40af9b50ef7279de311a12664b309d2ea0
William Grzybowski <william@grzy.org>
2025-12-01 11:26:58 -0300

    NAS-138704: remove icons

--- src/app/pages/storage/modules/disks/components/disk-list/sed-status-cell/sed-status-cell.component.spec.ts
+++ src/app/pages/storage/modules/disks/components/disk-list/sed-status-cell/sed-status-cell.component.spec.ts
@@ -1,15 +1,11 @@
-import { HarnessLoader } from '@angular/cdk/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { Spectator } from '@ngneat/spectator';
 import { createComponentFactory } from '@ngneat/spectator/jest';
 import { SedStatus } from 'app/enums/sed-status.enum';
 import { Disk } from 'app/interfaces/disk.interface';
-import { IxIconHarness } from 'app/modules/ix-icon/ix-icon.harness';
 import { SedStatusCellComponent } from './sed-status-cell.component';
 
 describe('SedStatusCellComponent', () => {
   let spectator: Spectator<SedStatusCellComponent<Disk>>;
-  let loader: HarnessLoader;
 
   const createComponent = createComponentFactory({
     component: SedStatusCellComponent<Disk>,
@@ -18,80 +14,63 @@ describe('SedStatusCellComponent', () => {
 
   beforeEach(() => {
     spectator = createComponent();
-    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
     spectator.component.propertyName = 'sed_status';
     spectator.component.uniqueRowTag = (row) => row.name;
   });
 
-  it('shows "Unsupported" for disks without SED support', async () => {
+  it('shows "Unsupported" for disks without SED support', () => {
     spectator.component.setRow({ name: 'sda', sed: false } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Unsupported');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-close' }));
-    expect(icon).toBeTruthy();
   });
 
-  it('shows "Unsupported" for disks with null sed property', async () => {
+  it('shows "Unsupported" for disks with null sed property', () => {
     spectator.component.setRow({ name: 'sda', sed: null } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Unsupported');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-close' }));
-    expect(icon).toBeTruthy();
   });
 
-  it('shows "Unsupported" for disks with undefined sed property', async () => {
+  it('shows "Unsupported" for disks with undefined sed property', () => {
     spectator.component.setRow({ name: 'sda' } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Unsupported');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-close' }));
-    expect(icon).toBeTruthy();
   });
 
-  it('shows "Unlocked" for unlocked SED disks', async () => {
+  it('shows "Unlocked" for unlocked SED disks', () => {
     spectator.component.setRow({ name: 'sda', sed: true, sed_status: SedStatus.Unlocked } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Unlocked');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-lock-open-variant' }));
-    expect(icon).toBeTruthy();
   });
 
-  it('shows "Locked" for locked SED disks', async () => {
+  it('shows "Locked" for locked SED disks', () => {
     spectator.component.setRow({ name: 'sda', sed: true, sed_status: SedStatus.Locked } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Locked');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-lock' }));
-    expect(icon).toBeTruthy();
   });
 
-  it('shows "Uninitialized" for uninitialized SED disks', async () => {
+  it('shows "Uninitialized" for uninitialized SED disks', () => {
     spectator.component.setRow({ name: 'sda', sed: true, sed_status: SedStatus.Uninitialized } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Uninitialized');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-checkbox-blank-circle-outline' }));
-    expect(icon).toBeTruthy();
   });
 
-  it('shows "Failed" for failed SED disks', async () => {
+  it('shows "Failed" for failed SED disks', () => {
     spectator.component.setRow({ name: 'sda', sed: true, sed_status: SedStatus.Failed } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Failed');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-alert-circle' }));
-    expect(icon).toBeTruthy();
   });
 
-  it('shows "Unknown" for unknown SED status', async () => {
+  it('shows "Unknown" for unknown SED status', () => {
     spectator.component.setRow({ name: 'sda', sed: true, sed_status: 'SOMETHING_ELSE' as SedStatus } as Disk);
     spectator.detectChanges();
 
     expect(spectator.element.textContent!.trim()).toBe('Unknown');
-    const icon = await loader.getHarness(IxIconHarness.with({ name: 'mdi-help-circle' }));
-    expect(icon).toBeTruthy();
   });
 });
