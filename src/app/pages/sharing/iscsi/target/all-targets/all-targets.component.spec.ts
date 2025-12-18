cd17f49397d528063b5368431d9a4a7cab69a1e5
matt wyatt <matt.wyatt@truenas.com>
2025-12-18 12:46:04 -0600

    NAS-138487: test: introduce minimal tests for `all-targets` component

---
+++ src/app/pages/sharing/iscsi/target/all-targets/all-targets.component.spec.ts
@@ -0,0 +1,77 @@
+import { signal } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { of } from 'rxjs';
+import { IscsiTarget } from 'app/interfaces/iscsi.interface';
+import { AsyncDataProvider } from 'app/modules/ix-table/classes/async-data-provider/async-data-provider';
+import { SlideIn } from 'app/modules/slide-ins/slide-in';
+import { AllTargetsComponent } from 'app/pages/sharing/iscsi/target/all-targets/all-targets.component';
+import { IscsiService } from 'app/services/iscsi.service';
+
+// this test suite is pretty minimal, since this component doesn't have a lot of user-facing
+// functionality we need to test. regardless, since we introduced `onMobileDetailsClosed`,
+// we need to test it to prevent regressions.
+describe('AllTargetsComponent', () => {
+  let spectator: Spectator<AllTargetsComponent>;
+  const createComponent = createComponentFactory({
+    component: AllTargetsComponent,
+    providers: [
+      mockProvider(IscsiService, {
+        getTargets: jest.fn(() => of([])),
+        listenForDataRefresh: jest.fn(() => of(null)),
+      }),
+      mockProvider(MatDialog),
+      mockProvider(SlideIn),
+    ],
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
+  });
+
+  it('should create', () => {
+    expect(spectator.component).toBeTruthy();
+  });
+
+  describe('onMobileDetailsClosed', () => {
+    it('clears the selected target when in mobile view', () => {
+      // create a mock data provider and rig it to the component
+      // we have to use `Object.defineProperty` here since they're protected values, and we don't
+      // have a way to mock them otherwise. this is generally ill-advised, but in such a small case it's permissible.
+      const mockTarget = { id: 1, name: 'iSCSI-1' } as IscsiTarget;
+      const dataProvider = new AsyncDataProvider(of([mockTarget]));
+      dataProvider.expandedRow = mockTarget;
+      Object.defineProperty(spectator.component, 'dataProvider', {
+        value: dataProvider,
+      });
+
+      // and create a mock master detail view and also add it to the component
+      const mockMasterDetailView = signal({ isMobileView: () => true });
+      Object.defineProperty(spectator.component, 'masterDetailView', {
+        value: mockMasterDetailView,
+      });
+
+      // ensure the component nullifies the expanded row when we're in the mobile view
+      spectator.component.onMobileDetailsClosed();
+      expect(dataProvider.expandedRow).toBeNull();
+    });
+
+    it('keeps the selected target when not in mobile view', () => {
+      const mockTarget = { id: 1, name: 'iSCSI-1' } as IscsiTarget;
+      const dataProvider = new AsyncDataProvider(of([mockTarget]));
+      dataProvider.expandedRow = mockTarget;
+      Object.defineProperty(spectator.component, 'dataProvider', {
+        value: dataProvider,
+      });
+
+      const mockMasterDetailView = signal({ isMobileView: () => false });
+      Object.defineProperty(spectator.component, 'masterDetailView', {
+        value: mockMasterDetailView,
+      });
+
+      spectator.component.onMobileDetailsClosed();
+      expect(dataProvider.expandedRow).not.toBeNull();
+      expect(dataProvider.expandedRow).toBe(mockTarget);
+    });
+  });
+});
