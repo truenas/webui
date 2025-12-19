650a8fcd354026fec832485329c80d039223832c
mattwyatt-ix <matt.wyatt@truenas.com>
2025-12-19 12:43:09 -0600

    NAS-138600 / 26.04 / feat: add dynamic return nav to ACL editor (#13013)

--- src/app/pages/sharing/components/shares-dashboard/smb-card/smb-card.component.ts
+++ src/app/pages/sharing/components/shares-dashboard/smb-card/smb-card.component.ts
@@ -215,9 +215,11 @@ export class SmbCardComponent implements OnInit {
     if (row.locked) {
       this.showLockedPathDialog(row.path);
     } else {
+      const returnUrl = this.router.url;
       this.router.navigate(['/', 'datasets', 'acl', 'edit'], {
         queryParams: {
           path: row.path,
+          returnUrl,
         },
       });
     }
