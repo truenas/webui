87fb5c7bc197da7e7d16f2fa6309071ebc92b2ac
matt wyatt <matt.wyatt@truenas.com>
2026-01-06 13:18:25 -0600

    NAS-139093: test: improve coverage

--- src/app/services/errors/error-parser.service.spec.ts
+++ src/app/services/errors/error-parser.service.spec.ts
@@ -105,9 +105,18 @@ describe('ErrorParserService', () => {
     });
 
     it('parses a failed job', () => {
-      const errorReport = spectator.service.parseError(new FailedJobError(failedJob));
+      const job1 = failedJob;
+      const job2 = {
+        ...failedJob,
+        error: undefined,
+      } as Job;
+      const job3 = {
+        ...failedJob,
+        error: undefined,
+        exception: undefined,
+      } as Job;
 
-      expect(errorReport).toEqual({
+      expect(spectator.service.parseError(new FailedJobError(job1))).toEqual({
         title: 'FAILED',
         message: '<pre>DUMMY_ERROR</pre>',
         stackTrace: 'LOGS',
@@ -116,7 +125,31 @@ describe('ErrorParserService', () => {
           route: '/jobs',
           params: { jobId: failedJob.id },
         }],
-        logs: failedJob,
+        logs: job1,
+      });
+
+      expect(spectator.service.parseError(new FailedJobError(job2))).toEqual({
+        title: 'FAILED',
+        message: '<pre>EXCEPTION</pre>',
+        stackTrace: 'LOGS',
+        actions: [{
+          label: 'View Details',
+          route: '/jobs',
+          params: { jobId: failedJob.id },
+        }],
+        logs: job2,
+      });
+
+      expect(spectator.service.parseError(new FailedJobError(job3))).toEqual({
+        title: 'FAILED',
+        message: 'Unknown error',
+        stackTrace: 'LOGS',
+        actions: [{
+          label: 'View Details',
+          route: '/jobs',
+          params: { jobId: failedJob.id },
+        }],
+        logs: job3,
       });
     });
 
