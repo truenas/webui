013a9182423921426983a8a5280c010c73ffb494
William Grzybowski <william@grzy.org>
2026-01-15 09:47:21 -0300

    NAS-00000: Remove Sentry integration

--- src/app/store/system-config/system-config.effects.spec.ts
+++ src/app/store/system-config/system-config.effects.spec.ts
@@ -1,4 +1,4 @@
-import { createServiceFactory, mockProvider, SpectatorService } from '@ngneat/spectator/jest';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator/jest';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { firstValueFrom, ReplaySubject } from 'rxjs';
 import { mockApi, mockCall } from 'app/core/testing/utils/mock-api.utils';
@@ -6,8 +6,6 @@ import { mockWindow } from 'app/core/testing/utils/mock-window.utils';
 import { AdvancedConfig } from 'app/interfaces/advanced-config.interface';
 import { SystemGeneralConfig } from 'app/interfaces/system-config.interface';
 import { ApiService } from 'app/modules/websocket/api.service';
-import { ErrorHandlerService } from 'app/services/errors/error-handler.service';
-import { waitForConsent$ } from 'app/services/errors/wait-for-sentry-consent';
 import { adminUiInitialized } from 'app/store/admin-panel/admin.actions';
 import { systemConfigLoaded } from 'app/store/system-config/system-config.actions';
 import { SystemConfigEffects } from 'app/store/system-config/system-config.effects';
@@ -32,7 +30,6 @@ describe('SystemConfigEffects', () => {
           setItem: jest.fn(),
         },
       }),
-      mockProvider(ErrorHandlerService),
       provideMockActions(actions$),
     ],
   });
@@ -60,36 +57,6 @@ describe('SystemConfigEffects', () => {
         generalConfig,
         advancedConfig,
       }));
-    });
-  });
-
-  describe('disableSentryIfNeeded$', () => {
-    it('disables Sentry if usage_collection is false', () => {
-      actions$.next(systemConfigLoaded({
-        generalConfig: {
-          usage_collection: false,
-        } as SystemGeneralConfig,
-        advancedConfig,
-      }));
-
-      spectator.service.disableSentryIfNeeded$.subscribe();
-
-      expect(spectator.inject(ErrorHandlerService).disableSentry).toHaveBeenCalled();
-    });
-
-    it('emits usage_collection value to waitForConsent$', () => {
-      jest.spyOn(waitForConsent$, 'next').mockImplementation();
-
-      actions$.next(systemConfigLoaded({
-        generalConfig: {
-          usage_collection: false,
-        } as SystemGeneralConfig,
-        advancedConfig,
-      }));
-
-      spectator.service.disableSentryIfNeeded$.subscribe();
-
-      expect(waitForConsent$.next).toHaveBeenCalledWith(false);
     });
   });
 });
