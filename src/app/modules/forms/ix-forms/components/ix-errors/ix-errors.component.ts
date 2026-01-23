b94d7ca7cd85d7b4a86ac979a22bba2f2aac6640
Alex Karpov <akarpov@ixsystems.com>
2026-01-23 11:25:56 +0200

    NAS-139321 / 26.04 / Confusing validation messages for Key length in Dataset Creation screen (#13152)

--- src/app/modules/forms/ix-forms/components/ix-errors/ix-errors.component.ts
+++ src/app/modules/forms/ix-forms/components/ix-errors/ix-errors.component.ts
@@ -85,6 +85,12 @@ export class IxErrorsComponent implements OnChanges, OnDestroy {
     invalidRcloneBandwidthLimit: (value: string) => this.translate.instant('Invalid Rclone bandwidth limit: {value}', { value }),
     selectionMustBeFile: () => this.translate.instant('Selected path must be a file and not a directory'),
     empty: () => this.translate.instant('Value cannot be empty or whitespace only'),
+    exactLength: (requiredLength: number, actualLength: number) => this.translate.instant(
+      this.label()
+        ? T('The length of {field} must be exactly {requiredLength} (current length: {actualLength})')
+        : T('The length must be exactly {requiredLength} (current length: {actualLength})'),
+      { field: this.label(), requiredLength, actualLength },
+    ),
     minArrayLength: (minLength: number) => {
       const message = minLength === 1
         ? T('List should have at least {minLength} item')
@@ -237,6 +243,11 @@ export class IxErrorsComponent implements OnChanges, OnDestroy {
         return this.defaultErrMessages.selectionMustBeFile();
       case DefaultValidationError.Empty:
         return this.defaultErrMessages.empty();
+      case DefaultValidationError.ExactLength:
+        return this.defaultErrMessages.exactLength(
+          (errors.exactLength as SomeError).requiredLength as number,
+          (errors.exactLength as SomeError).actualLength as number,
+        );
       case DefaultValidationError.MinArrayLength:
         return this.defaultErrMessages.minArrayLength(
           (errors.minArrayLength as ArrayLengthValidationError).requiredLength,
