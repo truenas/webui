b6584a5db97f1063b859161f6b78a34675ec47cc
William Grzybowski <william@grzy.org>
2026-01-23 11:25:32 -0300

    NAS-137480: Suppress reboot info dialog during security updates
    (cherry picked from commit 44a4c6bb8bffbc0a5de2a99935f092d26f9328ef)

---
+++ src/app/services/reboot-info-dialog-suppression.service.spec.ts
@@ -0,0 +1,30 @@
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator/jest';
+import { RebootInfoDialogSuppressionService } from './reboot-info-dialog-suppression.service';
+
+describe('RebootInfoDialogSuppressionService', () => {
+  let spectator: SpectatorService<RebootInfoDialogSuppressionService>;
+  const createService = createServiceFactory(RebootInfoDialogSuppressionService);
+
+  beforeEach(() => {
+    spectator = createService();
+  });
+
+  it('should be created', () => {
+    expect(spectator.service).toBeTruthy();
+  });
+
+  it('should start with suppression disabled', () => {
+    expect(spectator.service.isSuppressed()).toBe(false);
+  });
+
+  it('should enable suppression when suppress() is called', () => {
+    spectator.service.suppress();
+    expect(spectator.service.isSuppressed()).toBe(true);
+  });
+
+  it('should disable suppression when unsuppress() is called', () => {
+    spectator.service.suppress();
+    spectator.service.unsuppress();
+    expect(spectator.service.isSuppressed()).toBe(false);
+  });
+});
