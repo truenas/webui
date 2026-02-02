92820f72f9107ae484b24886ed289b4dd9885944
William Grzybowski <william@grzy.org>
2026-02-02 11:07:17 -0300

    NAS-139503: Add refreshRebootInfo action and refresh after security settings save
    (cherry picked from commit b140fb05e3bcac0140df16e7768bbe8c8a6a6b7d)

--- src/app/store/reboot-info/reboot-info.effects.ts
+++ src/app/store/reboot-info/reboot-info.effects.ts
@@ -1,18 +1,44 @@
 import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import {
+  map, mergeMap, switchMap, take,
+} from 'rxjs/operators';
 import { ApiService } from 'app/modules/websocket/api.service';
 import { failoverLicensedStatusLoaded } from 'app/store/ha-info/ha-info.actions';
-import { rebootInfoLoaded } from 'app/store/reboot-info/reboot-info.actions';
+import { selectIsHaLicensed } from 'app/store/ha-info/ha-info.selectors';
+import { rebootInfoLoaded, refreshRebootInfo } from 'app/store/reboot-info/reboot-info.actions';
 
 @Injectable()
 export class RebootInfoEffects {
   private actions$ = inject(Actions);
   private api = inject(ApiService);
+  private store$ = inject(Store);
 
   loadRebootInfo = createEffect(() => this.actions$.pipe(
     ofType(failoverLicensedStatusLoaded),
     mergeMap(({ isHaLicensed }) => {
+      if (isHaLicensed) {
+        return this.api.call('failover.reboot.info').pipe(
+          map((info) => rebootInfoLoaded({
+            thisNodeRebootInfo: info.this_node,
+            otherNodeRebootInfo: info.other_node,
+          })),
+        );
+      }
+      return this.api.call('system.reboot.info').pipe(
+        map((info) => rebootInfoLoaded({
+          thisNodeRebootInfo: info,
+          otherNodeRebootInfo: null,
+        })),
+      );
+    }),
+  ));
+
+  refreshRebootInfo = createEffect(() => this.actions$.pipe(
+    ofType(refreshRebootInfo),
+    switchMap(() => this.store$.select(selectIsHaLicensed).pipe(take(1))),
+    mergeMap((isHaLicensed) => {
       if (isHaLicensed) {
         return this.api.call('failover.reboot.info').pipe(
           map((info) => rebootInfoLoaded({
