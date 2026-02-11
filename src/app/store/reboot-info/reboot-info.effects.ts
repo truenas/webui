185badc616c4e0375aff9db2c4d0e5fa8e26c847
bugclerk <40872210+bugclerk@users.noreply.github.com>
2026-02-11 11:53:35 -0500

    NAS-139503 / 25.10.3 / Add refreshRebootInfo action and refresh after security settings save (by william-gr) (#13221)
    * NAS-139503: Add refreshRebootInfo action and refresh after security settings save
    
    (cherry picked from commit b140fb05e3bcac0140df16e7768bbe8c8a6a6b7d)
    
    * NAS-139503: Refactor reboot info effects to reduce duplication and add HA test
    
    (cherry picked from commit 568f1af805a069aba8e98412368fab0b63e83eae)
    
    ---------
    
    Co-authored-by: William Grzybowski <william@grzy.org>

--- src/app/store/reboot-info/reboot-info.effects.ts
+++ src/app/store/reboot-info/reboot-info.effects.ts
@@ -1,33 +1,30 @@
 import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {
+  map, mergeMap, switchMap, take,
+} from 'rxjs/operators';
 import { ApiService } from 'app/modules/websocket/api.service';
 import { failoverLicensedStatusLoaded } from 'app/store/ha-info/ha-info.actions';
-import { rebootInfoLoaded } from 'app/store/reboot-info/reboot-info.actions';
+import { selectIsHaLicensed } from 'app/store/ha-info/ha-info.selectors';
+import { rebootInfoLoaded, refreshRebootInfo } from 'app/store/reboot-info/reboot-info.actions';
 
 @Injectable()
 export class RebootInfoEffects {
   private actions$ = inject(Actions);
   private api = inject(ApiService);
+  private store$ = inject(Store);
 
   loadRebootInfo = createEffect(() => this.actions$.pipe(
     ofType(failoverLicensedStatusLoaded),
-    mergeMap(({ isHaLicensed }) => {
-      if (isHaLicensed) {
-        return this.api.call('failover.reboot.info').pipe(
-          map((info) => rebootInfoLoaded({
-            thisNodeRebootInfo: info.this_node,
-            otherNodeRebootInfo: info.other_node,
-          })),
-        );
-      }
-      return this.api.call('system.reboot.info').pipe(
-        map((info) => rebootInfoLoaded({
-          thisNodeRebootInfo: info,
-          otherNodeRebootInfo: null,
-        })),
-      );
-    }),
+    mergeMap(({ isHaLicensed }) => this.fetchRebootInfo(isHaLicensed)),
+  ));
+
+  refreshRebootInfo = createEffect(() => this.actions$.pipe(
+    ofType(refreshRebootInfo),
+    switchMap(() => this.store$.select(selectIsHaLicensed).pipe(take(1))),
+    mergeMap((isHaLicensed) => this.fetchRebootInfo(isHaLicensed)),
   ));
 
   subscribeToRebootInfo = createEffect(() => this.actions$.pipe(
@@ -49,4 +46,21 @@ export class RebootInfoEffects {
       );
     }),
   ));
+
+  private fetchRebootInfo(isHaLicensed: boolean): Observable<ReturnType<typeof rebootInfoLoaded>> {
+    if (isHaLicensed) {
+      return this.api.call('failover.reboot.info').pipe(
+        map((info) => rebootInfoLoaded({
+          thisNodeRebootInfo: info.this_node,
+          otherNodeRebootInfo: info.other_node,
+        })),
+      );
+    }
+    return this.api.call('system.reboot.info').pipe(
+      map((info) => rebootInfoLoaded({
+        thisNodeRebootInfo: info,
+        otherNodeRebootInfo: null,
+      })),
+    );
+  }
 }
