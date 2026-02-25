3a15fc159cb11346e4beb3e4d9750285e7854634
matt wyatt <matt.wyatt@truenas.com>
2026-02-25 10:22:02 -0500

    Revert "fix: make cards on app details scrollable"
    This reverts commit f76ba49cf646b27adb78eaf2055c5d5fa064c30a.

--- src/app/pages/apps/components/installed-apps/app-details-panel/app-details-panel.component.ts
+++ src/app/pages/apps/components/installed-apps/app-details-panel/app-details-panel.component.ts
@@ -11,7 +11,6 @@ import { AppWorkloadsCardComponent } from 'app/pages/apps/components/installed-a
 @Component({
   selector: 'ix-app-details-panel',
   templateUrl: './app-details-panel.component.html',
-  styleUrls: ['./app-details-panel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     TranslateModule,
