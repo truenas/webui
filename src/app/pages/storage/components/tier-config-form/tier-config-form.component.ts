cc461d3be8ddfa03a0a2c5f6f860b3e6f03606d2
William Grzybowski <william@grzy.org>
2026-02-27 11:24:43 -0300

    NAS-140003: Add Tiering button and config slide-in form to storage page

---
+++ src/app/pages/storage/components/tier-config-form/tier-config-form.component.ts
@@ -0,0 +1,92 @@
+import {
+  ChangeDetectionStrategy, Component, DestroyRef, OnInit, signal, inject,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { ZfsTierConfig } from 'app/interfaces/zfs-tier.interface';
+import { FormActionsComponent } from 'app/modules/forms/ix-forms/components/form-actions/form-actions.component';
+import { IxCheckboxComponent } from 'app/modules/forms/ix-forms/components/ix-checkbox/ix-checkbox.component';
+import { IxFieldsetComponent } from 'app/modules/forms/ix-forms/components/ix-fieldset/ix-fieldset.component';
+import { IxInputComponent } from 'app/modules/forms/ix-forms/components/ix-input/ix-input.component';
+import { FormErrorHandlerService } from 'app/modules/forms/ix-forms/services/form-error-handler.service';
+import { ModalHeaderComponent } from 'app/modules/slide-ins/components/modal-header/modal-header.component';
+import { SlideInRef } from 'app/modules/slide-ins/slide-in-ref';
+import { TestDirective } from 'app/modules/test-id/test.directive';
+import { ApiService } from 'app/modules/websocket/api.service';
+
+@Component({
+  selector: 'ix-tier-config-form',
+  templateUrl: './tier-config-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ModalHeaderComponent,
+    MatCard,
+    MatCardContent,
+    ReactiveFormsModule,
+    IxFieldsetComponent,
+    IxCheckboxComponent,
+    IxInputComponent,
+    FormActionsComponent,
+    MatButton,
+    TestDirective,
+    TranslateModule,
+  ],
+})
+export class TierConfigFormComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private api = inject(ApiService);
+  private errorHandler = inject(FormErrorHandlerService);
+  slideInRef = inject<SlideInRef<void, boolean>>(SlideInRef);
+  private destroyRef = inject(DestroyRef);
+
+  isFormLoading = signal(false);
+
+  formGroup = this.fb.nonNullable.group({
+    enabled: [false],
+    max_concurrent_jobs: [1],
+    min_available_space: [0],
+  });
+
+  constructor() {
+    this.slideInRef.requireConfirmationWhen(() => {
+      return of(this.formGroup.dirty);
+    });
+  }
+
+  ngOnInit(): void {
+    this.isFormLoading.set(true);
+    this.api.call('zfs.tier.config').pipe(
+      takeUntilDestroyed(this.destroyRef),
+    ).subscribe({
+      next: (config: ZfsTierConfig) => {
+        this.formGroup.patchValue(config);
+        this.isFormLoading.set(false);
+      },
+      error: () => {
+        this.isFormLoading.set(false);
+      },
+    });
+  }
+
+  protected submit(): void {
+    const values = this.formGroup.getRawValue();
+    this.isFormLoading.set(true);
+
+    this.api.call('zfs.tier.update', [values]).pipe(
+      takeUntilDestroyed(this.destroyRef),
+    ).subscribe({
+      next: () => {
+        this.isFormLoading.set(false);
+        this.slideInRef.close({ response: true });
+      },
+      error: (error: unknown) => {
+        this.isFormLoading.set(false);
+        this.errorHandler.handleValidationErrors(error, this.formGroup);
+      },
+    });
+  }
+}
