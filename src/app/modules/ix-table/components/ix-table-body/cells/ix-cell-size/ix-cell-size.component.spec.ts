b5f9ccd2ff091c103b3e209546ed419bbe438897
Alex Karpov <karpov4you@gmail.com>
2026-04-13 13:42:19 +0300

    NAS-140637: Broken storage usage estimate in snapshot page
    (cherry picked from commit 8322ea57626fd3c064c6e69faeb406c6c3338a2e)

--- src/app/modules/ix-table/components/ix-table-body/cells/ix-cell-size/ix-cell-size.component.spec.ts
+++ src/app/modules/ix-table/components/ix-table-body/cells/ix-cell-size/ix-cell-size.component.spec.ts
@@ -1,5 +1,6 @@
 import { Spectator } from '@ngneat/spectator';
 import { createComponentFactory } from '@ngneat/spectator/jest';
+import { TranslateModule } from '@ngx-translate/core';
 import { IxCellSizeComponent } from 'app/modules/ix-table/components/ix-table-body/cells/ix-cell-size/ix-cell-size.component';
 
 interface TestTableData { numberField: number }
@@ -9,18 +10,28 @@ describe('IxCellSizeComponent', () => {
 
   const createComponent = createComponentFactory({
     component: IxCellSizeComponent<TestTableData>,
+    imports: [TranslateModule.forRoot()],
     detectChanges: false,
   });
 
   beforeEach(() => {
     spectator = createComponent();
-    spectator.component.propertyName = 'numberField';
-    spectator.component.setRow({ numberField: 5 * 1024 * 1024 * 1024 });
     spectator.component.uniqueRowTag = () => '';
-    spectator.detectChanges();
   });
 
   it('shows file size in template', () => {
+    spectator.component.propertyName = 'numberField';
+    spectator.component.setRow({ numberField: 5 * 1024 * 1024 * 1024 });
+    spectator.detectChanges();
+
     expect(spectator.element.textContent!.trim()).toBe('5 GiB');
+  });
+
+  it('shows N/A when value is null', () => {
+    spectator.component.propertyName = 'numberField';
+    spectator.component.setRow({ numberField: null } as unknown as TestTableData);
+    spectator.detectChanges();
+
+    expect(spectator.element.textContent!.trim()).toBe('N/A');
   });
 });
