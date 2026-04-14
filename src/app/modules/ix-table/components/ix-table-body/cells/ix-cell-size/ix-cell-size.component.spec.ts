8494bfe3c8d9be8b35b0cc78404f412470d921e2
bugclerk <40872210+bugclerk@users.noreply.github.com>
2026-04-14 10:02:37 -0400

    NAS-140637 / 26.0.0-BETA.2 / Broken storage usage estimate in snapshot page (by AlexKarpov98) (#13477)
    * NAS-140637: Broken storage usage estimate in snapshot page
    
    (cherry picked from commit 8322ea57626fd3c064c6e69faeb406c6c3338a2e)
    
    * NAS-140637: Broken storage usage estimate in snapshot page
    
    (cherry picked from commit 100fb5bb38b20f714e1b2a7dcda8ae7da592bbc8)
    
    ---------
    
    Co-authored-by: Alex Karpov <karpov4you@gmail.com>

--- src/app/modules/ix-table/components/ix-table-body/cells/ix-cell-size/ix-cell-size.component.spec.ts
+++ src/app/modules/ix-table/components/ix-table-body/cells/ix-cell-size/ix-cell-size.component.spec.ts
@@ -1,26 +1,37 @@
 import { Spectator } from '@ngneat/spectator';
 import { createComponentFactory } from '@ngneat/spectator/jest';
+import { TranslateModule } from '@ngx-translate/core';
 import { IxCellSizeComponent } from 'app/modules/ix-table/components/ix-table-body/cells/ix-cell-size/ix-cell-size.component';
 
-interface TestTableData { numberField: number }
+interface TestTableData { numberField: number | null }
 
 describe('IxCellSizeComponent', () => {
   let spectator: Spectator<IxCellSizeComponent<TestTableData>>;
 
   const createComponent = createComponentFactory({
     component: IxCellSizeComponent<TestTableData>,
+    imports: [TranslateModule.forRoot()],
     detectChanges: false,
   });
 
   beforeEach(() => {
     spectator = createComponent();
-    spectator.component.propertyName = 'numberField';
-    spectator.component.setRow({ numberField: 5 * 1024 * 1024 * 1024 });
     spectator.component.uniqueRowTag = () => '';
-    spectator.detectChanges();
   });
 
   it('shows file size in template', () => {
+    spectator.component.propertyName = 'numberField';
+    spectator.component.setRow({ numberField: 5 * 1024 * 1024 * 1024 });
+    spectator.detectChanges();
+
     expect(spectator.element.textContent!.trim()).toBe('5 GiB');
+  });
+
+  it('shows N/A when value is null', () => {
+    spectator.component.propertyName = 'numberField';
+    spectator.component.setRow({ numberField: null });
+    spectator.detectChanges();
+
+    expect(spectator.element.textContent!.trim()).toBe('N/A');
   });
 });
