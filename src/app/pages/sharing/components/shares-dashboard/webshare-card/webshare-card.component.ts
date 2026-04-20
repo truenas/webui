9029b2e69e73712cdd0f87927506499d618a9537
Alex Karpov <karpov4you@gmail.com>
2026-04-20 16:58:11 +0300

    NAS-140730: PR Update

--- src/app/pages/sharing/components/shares-dashboard/webshare-card/webshare-card.component.ts
+++ src/app/pages/sharing/components/shares-dashboard/webshare-card/webshare-card.component.ts
@@ -78,7 +78,7 @@ import { selectService } from 'app/store/services/services.selectors';
   ],
 })
 export class WebShareCardComponent implements OnInit {
-  readonly requiredRoles = [Role.SharingWebshareWrite];
+  protected readonly requiredRoles = [Role.SharingWebshareWrite];
   protected readonly cardMenuPath = ['sharing', 'webshare'];
 
   private api = inject(ApiService);
