05e34e31fc7ef402f39b8e4a9ebe25c67d9d46c5
Alex Karpov <claude_101@ixsystems.com>
2026-06-01 13:56:24 +0300

    NAS-141022: Migrate feedback dialogs (FeedbackDialog, FileTicket, FileReview, FileTicketLicensed)

--- src/app/modules/feedback/components/feedback-dialog/feedback-dialog.component.spec.ts
+++ src/app/modules/feedback/components/feedback-dialog/feedback-dialog.component.spec.ts
@@ -1,11 +1,12 @@
+import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {
   createComponentFactory, mockProvider, Spectator,
 } from '@ngneat/spectator/jest';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { TnDialogHarness } from '@truenas/ui-components';
 import { MockComponents } from 'ng-mocks';
 import { BehaviorSubject } from 'rxjs';
 import { mockApi, mockCall } from 'app/core/testing/utils/mock-api.utils';
@@ -51,12 +52,12 @@ describe('FeedbackDialogComponent', () => {
         checkIfReviewAllowed: () => isReviewAllowed$,
       }),
       mockProvider(SystemGeneralService),
-      mockProvider(MatDialogRef),
+      mockProvider(DialogRef),
       mockApi([
         mockCall('support.attach_ticket_max_size', 5),
       ]),
       {
-        provide: MAT_DIALOG_DATA,
+        provide: DIALOG_DATA,
         useValue: null,
       },
       provideMockStore({
@@ -86,8 +87,9 @@ describe('FeedbackDialogComponent', () => {
       await setupTest();
     });
 
-    it('shows the header', () => {
-      expect(spectator.query('h1')).toHaveText('Send Feedback');
+    it('shows the header', async () => {
+      const dialog = await loader.getHarness(TnDialogHarness);
+      expect(await dialog.getTitle()).toBe('Send Feedback');
     });
 
     describe('type selector', () => {
@@ -125,7 +127,7 @@ describe('FeedbackDialogComponent', () => {
 
         const visibleForm = spectator.query(FileReviewComponent);
         expect(visibleForm).toExist();
-        expect(visibleForm!.dialogRef).toBe(spectator.inject(MatDialogRef));
+        expect(visibleForm!.dialogRef).toBe(spectator.inject(DialogRef));
 
         expect(spectator.query(FileTicketComponent)).not.toExist();
         expect(spectator.query(FileTicketLicensedComponent)).not.toExist();
@@ -137,7 +139,7 @@ describe('FeedbackDialogComponent', () => {
 
         let visibleForm = spectator.query(FileTicketComponent);
         expect(visibleForm).toExist();
-        expect(visibleForm!.dialogRef()).toBe(spectator.inject(MatDialogRef));
+        expect(visibleForm!.dialogRef()).toBe(spectator.inject(DialogRef));
         expect(visibleForm!.type()).toBe(FeedbackType.Bug);
 
         expect(spectator.query(FileReviewComponent)).not.toExist();
@@ -145,7 +147,7 @@ describe('FeedbackDialogComponent', () => {
 
         visibleForm = spectator.query(FileTicketComponent);
         expect(visibleForm).toExist();
-        expect(visibleForm!.dialogRef()).toBe(spectator.inject(MatDialogRef));
+        expect(visibleForm!.dialogRef()).toBe(spectator.inject(DialogRef));
         expect(visibleForm!.type()).toBe(FeedbackType.Bug);
 
         expect(spectator.query(FileReviewComponent)).not.toExist();
@@ -163,7 +165,7 @@ describe('FeedbackDialogComponent', () => {
 
         const visibleForm = spectator.query(FileTicketLicensedComponent);
         expect(visibleForm).toExist();
-        expect(visibleForm!.dialogRef).toBe(spectator.inject(MatDialogRef));
+        expect(visibleForm!.dialogRef).toBe(spectator.inject(DialogRef));
 
         expect(spectator.query(FileReviewComponent)).not.toExist();
         expect(spectator.query(FileTicketComponent)).not.toExist();
@@ -180,7 +182,7 @@ describe('FeedbackDialogComponent', () => {
         spectator.detectChanges();
 
         const visibleForm = spectator.query(FileTicketComponent);
-        expect(visibleForm!.dialogRef()).toBe(spectator.inject(MatDialogRef));
+        expect(visibleForm!.dialogRef()).toBe(spectator.inject(DialogRef));
 
         spectator.component.onIsLoadingChange(true);
         expect(visibleForm!.dialogRef()).toHaveProperty('disableClose', true);
@@ -195,7 +197,7 @@ describe('FeedbackDialogComponent', () => {
     spectator = createComponent({
       providers: [
         {
-          provide: MAT_DIALOG_DATA,
+          provide: DIALOG_DATA,
           useValue: FeedbackType.Bug,
         },
       ],
