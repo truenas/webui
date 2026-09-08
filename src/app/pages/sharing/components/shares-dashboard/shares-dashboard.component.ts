14ee425d81f46866986e76da4e677a805856eaa1
William Grzybowski <56250+william-gr@users.noreply.github.com>
2026-09-08 13:26:36 -0300

    NAS-143227: Show WebShare on non-Enterprise systems regardless of the WEBSHARE entitlement (#14044)

--- src/app/pages/sharing/components/shares-dashboard/shares-dashboard.component.ts
+++ src/app/pages/sharing/components/shares-dashboard/shares-dashboard.component.ts
@@ -5,7 +5,6 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { map } from 'rxjs';
 import { sharesEmptyConfig } from 'app/constants/empty-configs';
 import { UiSearchDirective } from 'app/directives/ui-search.directive';
-import { EntitlementFeature } from 'app/enums/entitlement-feature.enum';
 import { Role } from 'app/enums/role.enum';
 import { EmptyConfig } from 'app/interfaces/empty-config.interface';
 import { EmptyComponent } from 'app/modules/empty/empty.component';
@@ -14,8 +13,8 @@ import { NvmeOfCardComponent } from 'app/pages/sharing/components/shares-dashboa
 import { S3CardComponent } from 'app/pages/sharing/components/shares-dashboard/s3-card/s3-card.component';
 import { sharesDashboardElements } from 'app/pages/sharing/components/shares-dashboard/shares-dashboard.elements';
 import { WebShareCardComponent } from 'app/pages/sharing/components/shares-dashboard/webshare-card/webshare-card.component';
-import { EntitlementsService } from 'app/services/entitlements.service';
 import { poolStore } from 'app/services/global-store/stores.constant';
+import { LicenseService } from 'app/services/license.service';
 import { IscsiCardComponent } from './iscsi-card/iscsi-card.component';
 import { NfsCardComponent } from './nfs-card/nfs-card.component';
 import { SmbCardComponent } from './smb-card/smb-card.component';
@@ -42,7 +41,7 @@ export class SharesDashboardComponent {
   private poolStoreService = inject(poolStore);
   private translate = inject(TranslateService);
   private router = inject(Router);
-  private entitlements = inject(EntitlementsService);
+  private license = inject(LicenseService);
 
   protected readonly searchableElements = sharesDashboardElements;
 
@@ -58,5 +57,5 @@ export class SharesDashboardComponent {
 
   readonly pools = toSignal(this.poolStoreService.call.pipe(map((pools) => pools.length)), { initialValue: null });
 
-  protected readonly shouldShowWebshare = this.entitlements.entitled(EntitlementFeature.Webshare);
+  protected readonly shouldShowWebshare = toSignal(this.license.shouldShowWebshare$, { initialValue: false });
 }
